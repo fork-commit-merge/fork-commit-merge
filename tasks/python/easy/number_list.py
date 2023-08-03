5c35fc0508c8a46147e57b472034518f5735dd5d
python-fuse <129158431+python-fuse@users.noreply.github.com>
2023-08-02 23:14:37 -1200

    Update number_list.py

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -1,3 +1,11 @@
 # Python - Easy
-
+import math
 # TODO: Implement the list
+
+# Creating a list of the first 10 natural numbers(1 through 10)
+number_list = [x for x in range(1,11)]
+
+# Printing the contents of the list to the console
+print(number_list)
+#Printing the sum of the numbers using the sum method from the math module
+print(sum(number_list))
