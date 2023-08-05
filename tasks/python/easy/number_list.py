3a947becb32a2d15c8b8fb35ada726e10a2a635e
Fulosophy <michael.colandrea@outlook.com>
2023-08-05 02:08:01 -0400

    Fixed Python Easy Issue

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -1,3 +1,18 @@
 # Python - Easy
 
 # TODO: Implement the list
+
+# define the function to calculate the sum of a list.
+def calculate_sum_list(_list: any):
+    # the value that is being printed to the console using the built in sum function from python
+    value = sum(_list)
+    print(value)
+
+# Creating our natural numbers test list 1-10
+natural_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+#printing the natural numbers list
+print(natural_numbers)
+
+# printing the sum of the list
+calculate_sum_list(natural_numbers)
