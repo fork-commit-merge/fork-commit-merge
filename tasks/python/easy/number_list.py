e3d96b68ebe72c7023bf07e2e85ace4fd54990d6
GitHub Action <action@github.com>
2023-08-06 08:14:23 +0000

    Revert "Merge pull request #100 from avivk9/fix-issue"
    This reverts commit df3d794adb4bd0b5fcad3fed12a62a92f17cb1ff, reversing
    changes made to 2d60c30c23d77a6d23401c96e1769a2ac1310675.

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -1,11 +1,1 @@
 # Python - Easy
-
-# The program should create a list of the first 10 natural numbers.
-nums = [x for x in range(1,11)]
-
-# The program should print the created list to the console.
-print(nums)
-
-# The program should print the sum of the numbers in the list.
-print(sum(nums))
-
