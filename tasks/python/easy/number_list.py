43cef68cb3c1cc7df2d70f4e24c9b8fdc3b2dce2
Hannah Lin <ht.techx@gmail.com>
2023-08-07 21:01:38 -0400

    fixed issue #79

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -3,6 +3,6 @@
 # TODO: Create the Simple Python Program to Interact with Lists
 
 numbers = list(range(1,11))
-print("List of the first 10 natural numbers:", numbers)
+print("List of the first 10 numbers:", numbers)
 total_sum = sum(numbers)
 print("Sum of the numbers in the list:", total_sum)
