283ec8359ce56127359728e2ecb0cf7f63b5a348
GitHub Action <action@github.com>
2023-08-08 03:48:14 +0000

    Revert "Merge pull request #107 from ht-l1/fix-issue"
    This reverts commit bb8914547b680dd1a91fd39592cedc44ee9ea62c, reversing
    changes made to 15e3c5edde6dadbebcbd841824d12ba05d950366.

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -1,8 +1,3 @@
 # Python - Easy
 
 # TODO: Create the Simple Python Program to Interact with Lists
-
-numbers = list(range(1,11))
-print("List of the first 10 numbers:", numbers)
-total_sum = sum(numbers)
-print("Sum of the numbers in the list:", total_sum)
