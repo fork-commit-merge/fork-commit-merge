a74e3d0c1576e27379df820aee05b00df2a5c88f
Michael Gikunda <108736931+michaelmuthomi@users.noreply.github.com>
2023-08-09 22:53:04 +0300

    Fix: Easy Issues (Python)

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -1,3 +1,14 @@
 # Python - Easy
 
 # TODO: Create the Simple Python Program to Interact with Lists
+
+list = []
+num = -1
+sum = 0
+
+while num < 9:
+    num += 1
+    list.append(num)
+    sum += num
+    print(list)
+print(sum)
