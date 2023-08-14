ba85e1ae3f177a99c6993e353bb74a1d9e55dbba
GitHub Action <action@github.com>
2023-08-14 08:27:34 +0000

    Revert "Merge pull request #144 from yaten2302/main"
    This reverts commit 61fbeb20a3ccbab43aaa8d2f9b226e5ace89a4d1, reversing
    changes made to 99bbc66c36eb5eb4d08a4ec59f60ca769524468c.

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -1,9 +1,3 @@
 # Python - Easy
 
 # TODO: Create the Simple Python Program to Interact with Lists
-
-number_list = []
-for i in range(1, 11):
-    number_list.append(i)
-print(number_list)
-print(sum(number_list))
