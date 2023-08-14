6de47494ec63308a5efb7ea3fe1204dfdff8d0c3
GitHub Action <action@github.com>
2023-08-14 05:40:55 +0000

    Revert "Merge pull request #143 from vrtineu/fix-issue"
    This reverts commit cbe8a945ac029c82bfd2a1be7711fb547a0b2084, reversing
    changes made to 5df38f2fdca5816d0660cb11213446c7f63a044b.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,11 +1,10 @@
+// Java - Easy
+
 import java.util.Scanner;
 
 public class GreetingApp {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name:");
-        String name = scanner.nextLine();
-        System.out.println("Hello, " + name + "!" + " Nice to meet you!");
+        // TODO: Implement a Simple Greeting Application
     }
 }
