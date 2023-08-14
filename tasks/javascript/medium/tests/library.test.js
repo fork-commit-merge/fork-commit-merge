c45980ef0d14b96725ce9ee171423b11d5898ee3
Anil <anilmaharjan07@outlook.com>
2023-08-14 14:07:53 +0100

    Made the necessary update

--- tasks/javascript/medium/tests/library.test.js
+++ tasks/javascript/medium/tests/library.test.js
@@ -9,26 +9,29 @@ describe("Library", () => {
         library = new Library();
     });
 
-    test("createBook", () => {
+    test("createBook", () => { //The expectation for the title is set to "Title 1" which matches what was created.
         const newBook = library.createBook("Title 1", "Author 1");
-        expect(newBook.title).toBe("Title 2");
+        expect(newBook.title).toBe("Title 1");
+        expect(newBook.author).toBe("Author 1");
     });
 
-    test("getBook", () => {
+    test("getBook", () => { //The expectation for the title is set to "Title 1" to match the book that was added.
         library.createBook("Title 1", "Author 1");
         const book = library.getBook(1);
-        expect(book.title).toBe("Title 2");
+        expect(book.title).toBe("Title 1");
+        expect(book.author).toBe("Author 1");
     });
 
-    test("updateBook", () => {
+    test("updateBook", () => { //The expectation for the title is set to "Title 2" which matches the updated title.
         library.createBook("Title 1", "Author 1");
         const updatedBook = library.updateBook(1, "Title 2", "Author 2");
-        expect(updatedBook.title).toBe("Title 3");
+        expect(updatedBook.title).toBe("Title 2");
+        expect(updatedBook.author).toBe("Author 2");
     });
 
-    test("deleteBook", () => {
+    test("deleteBook", () => { //The ID for deletion is changed to 1 which should match the book that was added.
         library.createBook("Title 1", "Author 1");
-        const remainingBooks = library.deleteBook(2);
+        const remainingBooks = library.deleteBook(1);
         expect(remainingBooks.length).toBe(0);
     });
 
