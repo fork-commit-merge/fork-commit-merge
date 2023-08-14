f4e6fe366fd79f43acda9e4d9bd05a665076e9cd
Alan James <112971020+alanJames00@users.noreply.github.com>
2023-08-14 17:55:07 +0530

    Do number_list.py

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -1,3 +1,6 @@
 # Python - Easy
 
 # TODO: Create the Simple Python Program to Interact with Lists
+numLs = list(range(1,11))
+print("Natural numbers upto 10:",numLs)
+print("Sum of natural numbers upto:",sum(numLs))
