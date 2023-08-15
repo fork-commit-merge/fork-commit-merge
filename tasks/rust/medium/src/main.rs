89027757fad61b4549f36d564ec254e4a8bedbc6
vasfvitor <61759797+vasfvitor@users.noreply.github.com>
2023-08-15 14:16:40 -0300

    factor the logic in functions

--- tasks/rust/medium/src/main.rs
+++ tasks/rust/medium/src/main.rs
@@ -1,7 +1,80 @@
 // Rust - Medium
 
 use std::io;
+/*
+Task Details:
+
+    Prompt the user for the temperature value.
+    Prompt the user for the current unit (Fahrenheit or Celsius).
+    Convert the given temperature to the opposite unit.
+    Display the converted temperature to the user.
+
+Acceptance Criteria:
+
+    The program should handle both Fahrenheit to Celsius and Celsius to Fahrenheit conversions.
+    The program should be properly documented with comments.
+    Ensure error handling for invalid inputs.
+ */
 
 fn main() {
-    // TODO: Create a program in Rust that converts temperatures between Fahrenheit and Celsius
+    let input_temp: f32 = get_temperature();
+    let input_unit: char = get_unit();
+    let output_temp: f32 = convert_temperature(input_unit, input_temp);
+    print_result(input_temp, input_unit, output_temp)
+}
+
+fn get_temperature() -> f32 {
+    println!("Insert the temperature value:");
+    // loop while input isn't a valid number
+    let input_temp = loop {
+        let mut input = String::new();
+        // unwarp because the 'match' handles the error
+        io::stdin().read_line(&mut input).unwrap();
+        match input.trim().parse::<f32>() {
+            Ok(temp) => break temp,
+            Err(_) => println!("Please enter a valid temperature:"),
+        }
+    };
+    input_temp
+}
+
+fn get_unit() -> char {
+    println!("What is the current unit: Fahrenheit (F) or Celsius (C)?");
+    // loop while input isn't in C or Celsius format (case insensitive)
+    let input_unit = loop {
+        let mut input = String::new();
+        // unwarp because the 'match' handles the error
+        io::stdin().read_line(&mut input).unwrap();
+        match input.trim().to_uppercase().as_str() {
+            "C" | "CELSIUS" => break 'C',
+            "F" | "FAHRENHEIT" => break 'F',
+            _ => println!("Please enter a valid unit - Fahrenheit (F) or Celsius (C):"),
+        };
+    };
+    input_unit
+}
+
+fn convert_temperature(unit: char, temp: f32) -> f32 {
+    let output = match unit {
+        'C' => c_to_f(temp),
+        'F' => f_to_c(temp),
+        _ => None,
+    };
+    output.unwrap()
+}
+
+fn print_result(in_temp: f32, in_unit: char, out_temp: f32) {
+    match in_unit {
+        'C' => println!("{}°C is equal to {}°F", in_temp, out_temp),
+        'F' => println!("{}°F is equal to {}°C", in_temp, out_temp),
+        _ => println!("{}", out_temp),
+    }
+}
+
+fn f_to_c(val: f32) -> Option<f32> {
+    Some((val - 32.0) * 5.0 / 9.0)
+}
+
+fn c_to_f(val: f32) -> Option<f32> {
+    Some((val * 9.0 / 5.0) + 32.0)
 }
