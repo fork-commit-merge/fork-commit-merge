cad2db2a4556bc7528140cc735f4344f9db8e466
Rahul Chauhan <rc950470@gmail.com>
2023-08-15 16:06:38 +0530

    Fixed issue

--- tasks/javascript/medium/tests/library.test.js
+++ tasks/javascript/medium/tests/library.test.js
@@ -11,25 +11,25 @@ describe("Library", () => {
 
     test("createBook", () => {
         const newBook = library.createBook("Title 1", "Author 1");
-        expect(newBook.title).toBe("Title 2");
+        expect(newBook.title).toBe("Title 1");
     });
 
     test("getBook", () => {
         library.createBook("Title 1", "Author 1");
         const book = library.getBook(1);
-        expect(book.title).toBe("Title 2");
+        expect(book.title).toBe("Title 1");
     });
 
     test("updateBook", () => {
         library.createBook("Title 1", "Author 1");
         const updatedBook = library.updateBook(1, "Title 2", "Author 2");
-        expect(updatedBook.title).toBe("Title 3");
+        expect(updatedBook.title).toBe("Title 2");
     });
 
     test("deleteBook", () => {
         library.createBook("Title 1", "Author 1");
         const remainingBooks = library.deleteBook(2);
-        expect(remainingBooks.length).toBe(0);
+        expect(remainingBooks.length).toBe(1);
     });
 
 });
