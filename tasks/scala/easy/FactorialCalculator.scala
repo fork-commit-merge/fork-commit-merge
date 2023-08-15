e3cfa7e657de1156a013b6de1e44556eb8f6a699
Niko Hoffrén <niko.hoffren@gmail.com>
2023-08-15 22:09:41 +0300

    Add Scala to languages with Easy issue

---
+++ tasks/scala/easy/FactorialCalculator.scala
@@ -0,0 +1,11 @@
+// Scala - Easy
+
+object FactorialCalculator {
+  def main(args: Array[String]): Unit = {
+    // TODO: Create a simple Scala program that takes a number from the user and prints the factorial of that number
+  }
+
+  def factorial(n: Int): BigInt = {
+    // TODO: Create factorial function
+  }
+}
