f45773d7145b18cc0557010bfd696d83c5dca221
GitHub Action <action@github.com>
2023-08-15 14:32:35 +0000

    Revert "Merge pull request #158 from Aneal07/main"
    This reverts commit 21f1ec947dd0eb1412b78bdfcc107f9477a1e9ed, reversing
    changes made to 32144081e175fc46f7b091938966bf3261aae069.

--- tasks/javascript/medium/tests/library.test.js
+++ tasks/javascript/medium/tests/library.test.js
@@ -9,29 +9,26 @@ describe("Library", () => {
         library = new Library();
     });
 
-    test("createBook", () => { //The expectation for the title is set to "Title 1" which matches what was created.
+    test("createBook", () => {
         const newBook = library.createBook("Title 1", "Author 1");
-        expect(newBook.title).toBe("Title 1");
-        expect(newBook.author).toBe("Author 1");
+        expect(newBook.title).toBe("Title 2");
     });
 
-    test("getBook", () => { //The expectation for the title is set to "Title 1" to match the book that was added.
+    test("getBook", () => {
         library.createBook("Title 1", "Author 1");
         const book = library.getBook(1);
-        expect(book.title).toBe("Title 1");
-        expect(book.author).toBe("Author 1");
+        expect(book.title).toBe("Title 2");
     });
 
-    test("updateBook", () => { //The expectation for the title is set to "Title 2" which matches the updated title.
+    test("updateBook", () => {
         library.createBook("Title 1", "Author 1");
         const updatedBook = library.updateBook(1, "Title 2", "Author 2");
-        expect(updatedBook.title).toBe("Title 2");
-        expect(updatedBook.author).toBe("Author 2");
+        expect(updatedBook.title).toBe("Title 3");
     });
 
-    test("deleteBook", () => { //The ID for deletion is changed to 1 which should match the book that was added.
+    test("deleteBook", () => {
         library.createBook("Title 1", "Author 1");
-        const remainingBooks = library.deleteBook(1);
+        const remainingBooks = library.deleteBook(2);
         expect(remainingBooks.length).toBe(0);
     });
 
