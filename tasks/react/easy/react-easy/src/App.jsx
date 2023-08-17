02e49ca7a7d10097fde8c83fc86cba015f9c09e9
Eyal Haimov <eyalhaimoon@gmail.com>
2023-08-17 14:44:50 +0300

    Add eyal-haim to App.jsx

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -6,6 +6,14 @@ function App() {
         <>
             <div className="center">
                 {/* TODO: Implement the card component */}
+            <h1>React/Vite - Easy</h1>
+            <div className="card">
+                <img src="https://loremflickr.com/320/240" alt="image" />
+                <h2>Card Title</h2>
+                <p>
+                    Card Descrioption
+                </p>
+            </div>
             </div>
         </>
     );
