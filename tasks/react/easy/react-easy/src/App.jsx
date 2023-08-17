3ceef304209976f8f662dd6c516a03f296c97d98
Niko Hoffrén <niko.hoffren@gmail.com>
2023-08-17 09:56:52 +0300

    Add React to the project with Easy issue

---
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -0,0 +1,14 @@
+// React/Vite - Easy
+import "./index.css";
+
+function App() {
+    return (
+        <>
+            <div className="center">
+                {/* TODO: Implement the card component */}
+            </div>
+        </>
+    );
+}
+
+export default App;
