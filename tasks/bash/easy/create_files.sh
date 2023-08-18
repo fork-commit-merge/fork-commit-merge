3de899a2fce3678666c04881df7c3b00ece2ca2a
GitHub Action <action@github.com>
2023-08-18 13:10:33 +0000

    Revert "Merge pull request #185 from Nancy64-bit/easy/hello-shell"
    This reverts commit 6ce27872b432f88ff0a13632d53d61c136da9a27, reversing
    changes made to 81aa381482de2af3da80ba9ca295cd9949063eb6.

--- tasks/bash/easy/create_files.sh
+++ tasks/bash/easy/create_files.sh
@@ -3,16 +3,3 @@
 # Bash - Easy
 
 # TODO: Write the Bash script requested in the issue description
-
-# Navigate to the 'files' directory
-cd files
-
-# Create three files and write "Hello Shell!" to each
-echo "Hello Shell!" > file1.txt
-echo "Hello Shell!" > file2.txt
-echo "Hello Shell!" > file3.txt
-
-# Print the contents of each file
-cat file1.txt
-cat file2.txt
-cat file3.txt
