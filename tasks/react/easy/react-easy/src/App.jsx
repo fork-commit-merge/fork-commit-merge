babbe63a2f42a0936d82446d3c2285dde56d877e
Dante Passalacqua <dante.l.pass@gmail.com>
2023-08-18 00:27:21 -0600

    Created card component

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -1,14 +1,19 @@
 // React/Vite - Easy
-import "./index.css";
+import './index.css';
 
 function App() {
-    return (
-        <>
-            <div className="center">
-                {/* TODO: Implement the card component */}
-            </div>
-        </>
-    );
+	return (
+		<>
+			<div className="center">
+				<h1>React/Vite - Easy</h1>
+				<div className="card">
+					<img src="https://loremflickr.com/320/240" alt="" />
+					<h2>Card Title</h2>
+					<p>Card Description</p>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App;
