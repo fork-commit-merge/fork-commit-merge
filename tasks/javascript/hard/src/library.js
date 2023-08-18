24a76dcd25f0604405354d563e691f95bdbe4b2d
GitHub Action <action@github.com>
2023-08-18 08:00:16 +0000

    Revert "Merge pull request #181 from dpass47/resolved-javascript-hard-issue"
    This reverts commit 480b62ffd3a167fb0cc8c7c325a926f9d40c06aa, reversing
    changes made to 5e6241460941890745423bbdf77680ed17219ff1.

--- tasks/javascript/hard/src/library.js
+++ tasks/javascript/hard/src/library.js
@@ -1,58 +1,47 @@
 // JavaScript - Hard
 
 class Library {
-	constructor() {
-		this.books = [];
-	}
+    constructor() {
+        this.books = [];
+    }
 
-	createBook(title, author) {
-		const book = {
-			title,
-			author,
-			id: this.books.length + 1,
-		};
-		this.books.push(book);
-		return book;
-	}
+    createBook(title, author) {
+        const book = {
+            title,
+            author,
+            id: this.books.length + 1,
+        };
+        this.books.push(book);
+        return book;
+    }
 
-	getBook(id) {
-		return this.books.find((book) => book.id === id);
-	}
+    getBook(id) {
+        return this.books.find((book) => book.id === id);
+    }
 
-	updateBook(id, title, author) {
-		const bookIndex = this.books.findIndex((book) => book.id === id);
-		if (bookIndex !== -1) {
-			this.books[bookIndex] = { id, title, author };
-		}
-		return this.books[bookIndex];
-	}
+    updateBook(id, title, author) {
+        const bookIndex = this.books.findIndex((book) => book.id === id);
+        if (bookIndex !== -1) {
+            this.books[bookIndex] = { id, title, author };
+        }
+        return this.books[bookIndex];
+    }
 
-	deleteBook(id) {
-		const bookIndex = this.books.findIndex((book) => book.id === id);
-		if (bookIndex !== -1) {
-			this.books.splice(bookIndex, 1);
-		}
-		return this.books;
-	}
+    deleteBook(id) {
+        const bookIndex = this.books.findIndex((book) => book.id === id);
+        if (bookIndex !== -1) {
+            this.books.splice(bookIndex, 1);
+        }
+        return this.books;
+    }
 
-	searchBooks(query) {
-		return this.books.filter(
-			(book) =>
-				book.id === query ||
-				book.title === query ||
-				book.author === query ||
-				book.title.includes(query) ||
-				book.author.includes(query)
-		);
-	}
+    searchBooks(query) {
+        // TODO: Implement search functionality
+    }
 
-	filterBooks(criteria) {
-		return this.books.filter(
-			(books) =>
-				books.title === criteria.title ||
-				books.author === criteria.author
-		);
-	}
+    filterBooks(criteria) {
+        // TODO: Implement filter functionality
+    }
 }
 
 module.exports = Library;
