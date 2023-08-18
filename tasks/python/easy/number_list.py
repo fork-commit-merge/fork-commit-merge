fb97fdcd8e641645969ea3ff41d5e5398429eb7c
GitHub Action <action@github.com>
2023-08-18 15:17:27 +0000

    Revert "Merge pull request #186 from TheBarr/number_list_issue"
    This reverts commit 5658c931ecaca659458cfbe17234cdf2c23190a1, reversing
    changes made to dfff2c790b9a5ebebe0fdfdb6091a282befeda35.

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -1,4 +1,3 @@
-natural_numbers = list(range(1, 11))
-sum_of_numbers = sum(natural_numbers)
-print(natural_numbers)
-print(sum_of_numbers)
+# Python - Easy
+
+# TODO: Create the Simple Python Program to Interact with Lists
