50cf74ddc05c8d231b9748f0439fe0001ba0ab2e
Niko Hoffrén <niko.hoffren@gmail.com>
2023-08-19 10:12:44 +0300

    Add Vue.js to the project with Medium Issue

---
+++ tasks/vue/medium/vue-task-list/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    root: true,
+    env: {
+      node: true
+    },
+    extends: [
+      'plugin:vue/vue3-recommended',
+      'eslint:recommended'
+    ],
+    parserOptions: {
+      parser: 'babel-eslint'
+    },
+    rules: {
+    }
+  };
---
+++ tasks/vue/medium/vue-task-list/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <TaskList />
+  </div>
+</template>
+
+<script>
+import TaskList from './components/TaskList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TaskList
+  }
+}
+</script>
