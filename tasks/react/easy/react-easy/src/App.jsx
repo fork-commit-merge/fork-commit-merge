349d357647600b6ded1c3caf2629e05a254b570a
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-08-19 22:16:53 +0530

    Add img tag with random image inside div-card

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
             <div className="center">
                 <h1>React/Vite - Easy</h1>
                 <div className="card">
-                    
+                    <img src="https://loremflickr.com/320/240" alt="random-image" />
                 </div>
             </div>
         </>
