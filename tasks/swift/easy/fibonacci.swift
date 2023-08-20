1a8d451eec2e11c68679f51938ec030f30ac0c19
GitHub Action <action@github.com>
2023-08-20 10:14:56 +0000

    Revert "Merge pull request #206 from Aloneking789/main"
    This reverts commit 8097fb8464d6b961d5545d35e9fc973024503faf, reversing
    changes made to 6f2dd8fd7d3e0033a98ed9bfca756eed0d895564.

--- tasks/swift/easy/fibonacci.swift
+++ tasks/swift/easy/fibonacci.swift
@@ -1,21 +1,12 @@
+// Swift - Easy
+
 import Foundation
 
 func fibonacci(n: Int) -> Int {
-    if n <= 0 {
-        return 0
-    } else if n == 1 {
-        return 1
-    } else {
-        var fib = [0, 1]
-        for i in 2...n {
-            let nextFib = fib[i - 1] + fib[i - 2]
-            fib.append(nextFib)
-        }
-        return fib[n]
-    }
+    // TODO: Implement the fibonacci sequence function
 }
 
-// Test cases
+// Test cases (Don't touch!)
 let testCases: [Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 for test in testCases {
