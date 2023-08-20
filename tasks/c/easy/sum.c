8f82e2130ecf9f43f927f1e1b89d00a44b72ee7c
nipanko <nipanko@protonmail.com>
2023-08-20 11:22:04 +0530

    FIX #196 added sum function that takes two integers as input form the user and display their sum. ty.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -2,10 +2,20 @@
 
 #include <stdio.h>
 
-// TODO: Implement the sum function
+int sum(int a,int b) {
+    return a + b;
+}
 
 int main() {
-    // TODO: Implement the main function
+    int a = 0;
+    int b = 0;
+    int total = 0;
+    printf("Enter the first integer : ");
+    scanf("%d", &a);
+    printf("Enter the second integer : ");
+    scanf("%d", &b);
+    total = sum(a, b);
+    printf("Sum of %d and %d is : %d", a, b, total);
 
     return 0;
 }
