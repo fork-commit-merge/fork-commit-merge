3616afd2aea9a8dd0582aa52e2079a6222a3e72c
Aloneking789 <116704975+Aloneking789@users.noreply.github.com>
2023-08-20 15:35:42 +0530

    Add Fibonacci sequence function implementation

--- tasks/swift/easy/fibonacci.swift
+++ tasks/swift/easy/fibonacci.swift
@@ -1,12 +1,21 @@
-// Swift - Easy
-
 import Foundation
 
 func fibonacci(n: Int) -> Int {
-    // TODO: Implement the fibonacci sequence function
+    if n <= 0 {
+        return 0
+    } else if n == 1 {
+        return 1
+    } else {
+        var fib = [0, 1]
+        for i in 2...n {
+            let nextFib = fib[i - 1] + fib[i - 2]
+            fib.append(nextFib)
+        }
+        return fib[n]
+    }
 }
 
-// Test cases (Don't touch!)
+// Test cases
 let testCases: [Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 for test in testCases {
