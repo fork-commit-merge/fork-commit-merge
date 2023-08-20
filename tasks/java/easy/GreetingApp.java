866b0b98b145bd44addd90638936fcc23219270f
GitHub Action <action@github.com>
2023-08-20 07:59:44 +0000

    Revert "Merge pull request #205 from Omanshu209/add-code"
    This reverts commit 9e9c70ec87926874c894630fc29d98e16555d506, reversing
    changes made to 3b7eb5ce4e935756acbdd84b8b69c687e6173f5c.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,17 +1,8 @@
 // Java - Easy
 
-import java.util.Scanner;
-
 public class GreetingApp {
-    
-    static void greet(String name){
-    	System.out.println("Hello " + name + "!");
-    }
-    
+
     public static void main(String[] args) {
         // TODO: Implement a Simple Greeting Application
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your name : ");
-        greet(sc.nextLine());
     }
 }
