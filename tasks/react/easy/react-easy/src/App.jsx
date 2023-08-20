0a33cd18686dcd59535779660c66c8e466b60149
GitHub Action <action@github.com>
2023-08-20 10:17:27 +0000

    Revert "Merge pull request #207 from PranshuBasak/main"
    This reverts commit 756778ae1f5628a63fc8d3985079170c2f4c3383, reversing
    changes made to 242d32ffe4d3b3bd1c4ee0e508f52ca7ee0bb116.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -5,10 +5,7 @@ function App() {
     return (
         <>
             <div className="center">
-                <h1>React/Vite - Easy</h1>
-                <div className="class"><img src="https://loremflickr.com/320/240" alt="Random Cat Images" /></div>
-                <h2>Card Title</h2>
-                <p>Card Discription</p>
+                {/* TODO: Implement the card component */}
             </div>
         </>
     );
