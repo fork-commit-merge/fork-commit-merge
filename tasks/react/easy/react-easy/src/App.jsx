d74247c53d27d9a2194e37b542788e2727cdacfb
PranshuBasak <pranshubasak@gmail.com>
2023-08-20 15:39:34 +0530

    Created Card Component

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -5,7 +5,10 @@ function App() {
     return (
         <>
             <div className="center">
-                {/* TODO: Implement the card component */}
+                <h1>React/Vite - Easy</h1>
+                <div className="class"><img src="https://loremflickr.com/320/240" alt="Random Cat Images" /></div>
+                <h2>Card Title</h2>
+                <p>Card Discription</p>
             </div>
         </>
     );
