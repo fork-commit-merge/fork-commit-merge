b777ec1a8facac69d14ababdfdca546721aa3470
Yashasvi <yashasvivaidya13@gmail.com>
2023-08-20 10:45:51 +0530

    made card component

---
+++ tasks/react/easy/react-easy/src/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Card() {
+  return (
+      <div className='center'>
+          <div className="card"><img src="https://loremflickr.com/320/240" alt="img"/>
+              <h2>card title</h2>
+          <p>Card description</p></div>
+      
+    </div>
+  )
+}
+
+export default Card
