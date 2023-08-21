89f3263430838ff3759301c5fcb07342520786aa
GitHub Action <action@github.com>
2023-08-21 16:51:48 +0000

    Revert "Merge pull request #215 from PlasticPath/main"
    This reverts commit b1b56c4d4e6124687ea73ed885e42a6dc6137264, reversing
    changes made to d7f84737b045ead8e57ed3f011172ef35de330ef.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,25 +1,11 @@
 // C - Easy
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// // TODO: Implement the sum function
+// TODO: Implement the sum function
 
-// int main() {
-//     // TODO: Implement the main function
+int main() {
+    // TODO: Implement the main function
 
-//    
-#include<iostream>
-using namespace std;
-
-
-int add(int a,int b)
-{
-    return a+b;
-}
-int main()
-{
-    cout<<"Enter two numbers: ";
-    int a,b;
-    cin>>a>>b;
-    cout<<"Sum of two numbers is "<<add(a,b);
+    return 0;
 }
