7d1893058ca0a77ae033abfbe4e619a9d9775672
GitHub Action <action@github.com>
2023-08-21 16:31:18 +0000

    Revert "Merge pull request #217 from FreakWolf/main"
    This reverts commit 58692fb29cd59f2a2af87edd056ed8adafce4846, reversing
    changes made to 603fbc7aa99416ce09a341a0c2bcea907ee55603.

--- tasks/c#/easy/HelloUser/Program.cs
+++ tasks/c#/easy/HelloUser/Program.cs
@@ -1,18 +1,3 @@
 ﻿// C# - Easy
-using System;
-
-namespace GreetingApp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Please enter your name:");
-            string userName = Console.ReadLine();
-
-            Console.WriteLine($"Hello, {userName}!");
-        }
-    }
-}
 
 // TODO: Implement the C# program requested in the Issue description
