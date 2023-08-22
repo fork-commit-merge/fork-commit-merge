20e23bc5daf1b5d94f5003334cdb4397b1592cec
GitHub Action <action@github.com>
2023-08-22 13:32:21 +0000

    Revert "Merge pull request #224 from Ayushparui/sampleProducts"
    This reverts commit d884e60d41eac4c33f320ab7380bee44547e94a8, reversing
    changes made to 61c5c8f918c506f098b388acece6c209c5d87bfb.

--- tasks/nextjs/easy/nextjs-easy/src/pages/index.tsx
+++ tasks/nextjs/easy/nextjs-easy/src/pages/index.tsx
@@ -3,10 +3,6 @@
 export default function Home() {
     const sampleProducts = [
         // TODO: Create a sample product list with names and prices
-        {"name": "Watches", "price": "$1000"},
-        {"name": "Bags", "price": "$500"},
-        {"name": "Shoes", "price": "$800"}
-        
     ];
 
     return (
