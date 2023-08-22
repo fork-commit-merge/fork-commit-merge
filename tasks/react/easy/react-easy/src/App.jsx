bb03e2c2e0548ac673292fae206560812725feb7
GitHub Action <action@github.com>
2023-08-22 13:29:13 +0000

    Revert "Merge pull request #226 from ananyajain10/fix-issue"
    This reverts commit c50647815419c6fac0df39bf3e20c9bc4515a9dd, reversing
    changes made to 7d9fb20f9254d23f72ca1fe3c746be5bb72d9974.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -1,47 +1,14 @@
-import React, { useState } from "react";
+// React/Vite - Easy
 import "./index.css";
+
 function App() {
-  const [meme, setMeme] = useState({
-   
-    memes: "https://i.imgflip.com/8p0a.jpg",
-  });
-
-  function handleChange(event) {
-    const { name, value } = event.target;
-    setMeme((preValue) => ({
-      ...preValue,
-      [name]: value,
-    }));
-  }
-  const [memeData, setMemeData] = useState([]);
-  React.useEffect(() => {
-    fetch("https://api.imgflip.com/get_memes")
-      .then((res) => res.json())
-      .then((data) => setMemeData(data.data.memes));
-  }, []);
-
-  function getMemeImage() {
-    const randomNumber = Math.floor(Math.random() * memeData.length);
-    const url = memeData[randomNumber].url;
-    setMeme((preValue) => ({
-      ...preValue,
-      memes: url,
-    }));
-    console.log(url);
-  }
-
-  return (
-    <div className="center">
-      
-      
-      <button className="card" onClick={getMemeImage}>
-        Get New Image
-      </button>
-      
-        <img className="card img" alt="Meme" src={meme.memes} />
-       
-      
-    </div>
-  );
+    return (
+        <>
+            <div className="center">
+                {/* TODO: Implement the card component */}
+            </div>
+        </>
+    );
 }
+
 export default App;
