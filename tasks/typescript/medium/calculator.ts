f24cfe79dd730203af7325d59079b5097cbba4e1
GitHub Action <action@github.com>
2023-08-22 04:44:09 +0000

    Revert "Merge pull request #222 from alxhal/fix-issue"
    This reverts commit 73a46ab6f257af4d4293d7b7ce715064113d1446, reversing
    changes made to 703c8ffd8bb5c82d49f0650c3cea09e0fa7e1dd3.

--- tasks/typescript/medium/calculator.ts
+++ tasks/typescript/medium/calculator.ts
@@ -1,24 +1,7 @@
 // TypeScript - Medium
 
 namespace Calculator {
-  // Returns the sum of a and b
-  export const add = (a: number, b: number): number => a + b;
-
-  // Returns the result of a minus b
-  export const subtract = (a: number, b: number): number => a - b;
-
-  // Returns the product of a and b
-  export const multiply = (a: number, b: number): number => a * b;
-
-  // Returns the quotient of a divided by b.
-  // If b is 0, it should return null and log a warning
-  export const divide = (a: number, b: number): number | null => {
-    if (b === 0) {
-      console.warn("Warning: Cannot divide by zero");
-      return null;
-    }
-    return a / b;
-  };
+    // TODO: Implement a Simple TypeScript Calculator
 }
 
 // Test cases
