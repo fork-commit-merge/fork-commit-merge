773ec7135bb697932143a79ba96210337b40cfe4
Vishesh Rawal <92795514+visheshrwl@users.noreply.github.com>
2023-08-22 22:44:37 +0530

    Update sum.c

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -3,9 +3,14 @@
 #include <stdio.h>
 
 // TODO: Implement the sum function
+int sum(int a, int b){
+    return a - (-b);
+}
 
 int main() {
     // TODO: Implement the main function
-
+    int num1 = 5;
+    int num2 = 10;
+    printf("%d", sum(num1, num2));
     return 0;
 }
