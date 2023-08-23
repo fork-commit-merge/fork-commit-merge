e913d67000f8079a3efb0c03abc61e270ec44588
jaypee15 <okoyejohnpaul15@gmail.com>
2023-08-23 16:03:39 +0100

    Fixed issue

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -1,3 +1,11 @@
 # Python - Easy
 
 # TODO: Create the Simple Python Program to Interact with Lists
+
+natural_nums = []
+
+for i in range(1, 11):
+    natural_nums.append(i)
+
+print(natural_nums)
+print(sum(natural_nums))
