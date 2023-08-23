8e3e4ba8a9b87de51a9175f2cbd12f9c57c90f12
GitHub Action <action@github.com>
2023-08-23 15:27:40 +0000

    Revert "Merge pull request #249 from jaypee15/fix-issue"
    This reverts commit 1dbda7ba81534e6472da7ca310f06b19d1920820, reversing
    changes made to 43f2aee3dbbe6db72eb9ecb964d602af522d9e09.

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -1,11 +1,3 @@
 # Python - Easy
 
 # TODO: Create the Simple Python Program to Interact with Lists
-
-natural_nums = []
-
-for i in range(1, 11):
-    natural_nums.append(i)
-
-print(natural_nums)
-print(sum(natural_nums))
