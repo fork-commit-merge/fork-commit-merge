b6f43d79fd3789d3b4f3b83ee75bbbee66837af8
ObsidianAsphodel <terencep.dixon@gmail.com>
2023-08-22 22:55:12 -0700

    "Fixed issue"

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -1,3 +1,10 @@
 # Python - Easy
 
 # TODO: Create the Simple Python Program to Interact with Lists
+# create list with first 10 natural numbers
+firstTenNaturalNumbers = []
+for i in range(11):
+    if i != 0:
+        firstTenNaturalNumbers.append(i)
+        print(firstTenNaturalNumbers)
+print("The sum of First Ten Natural Numbers is:",sum(firstTenNaturalNumbers))
