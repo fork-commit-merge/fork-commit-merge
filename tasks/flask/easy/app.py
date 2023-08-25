38a76d90ca8035327c7498658552a8b0e319c033
Haitham AbuGhaida <108552334+Haitham-ghaida@users.noreply.github.com>
2023-08-25 20:53:24 +0200

    added a hello world print to /

--- tasks/flask/easy/app.py
+++ tasks/flask/easy/app.py
@@ -5,3 +5,10 @@ from flask import Flask
 app = Flask(__name__)
 
 # TODO: Implement rest of the code here
+
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
