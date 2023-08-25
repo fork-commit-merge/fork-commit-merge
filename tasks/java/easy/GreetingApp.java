3267e4fbe013035897fc34de8fc04eb13a134dcb
dshunter107 <david.shane.hunter@gmail.com>
2023-08-24 20:46:39 -0400

    A HelloWorld application where the user has to input their name.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,8 +1,24 @@
 // Java - Easy
 
+import java.util.Scanner;
+
 public class GreetingApp {
 
     public static void main(String[] args) {
-        // TODO: Implement a Simple Greeting Application
+        // Getting the scanner and then closing it afterwards
+    	
+    	Scanner ask = new Scanner(System.in);
+    	System.out.println("What is your name: ");
+    	String name = ask.nextLine();
+    	ask.close();
+    	
+    	//putting the response in a string builder and then sending it out
+    	StringBuilder output = new StringBuilder();
+    	output.append("Hello ");
+    	output.append(name);
+    	output.append("! Nice to meet you.");
+    	
+    	System.out.println(output.toString());
+
     }
 }
