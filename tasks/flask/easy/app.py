675f12d0cf7d7470b043e67155413e561f3ef7ac
GitHub Action <action@github.com>
2023-08-25 18:58:47 +0000

    Revert "Merge pull request #295 from Haitham-ghaida/hello_world_flask"
    This reverts commit 5e422247825969385359481d491d810f55340942, reversing
    changes made to e419ca3fca12fc8b7d66d20669a6e207085c0085.

--- tasks/flask/easy/app.py
+++ tasks/flask/easy/app.py
@@ -5,10 +5,3 @@ from flask import Flask
 app = Flask(__name__)
 
 # TODO: Implement rest of the code here
-
-@app.route('/')
-def index():
-    return 'Hello, World!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
