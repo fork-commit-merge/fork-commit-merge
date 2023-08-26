33b63cb01ce477cd4efd9e64fbb0b204b838272d
Tanishavats19 <tanishavats01@gmail.com>
2023-08-26 19:50:33 +0530

    Fixed issue

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -5,7 +5,12 @@ function App() {
     return (
         <>
             <div className="center">
-                {/* TODO: Implement the card component */}
+                <h1>React/Vite - Easy</h1>
+                <div className="card">
+                    <img src="https://loremflickr.com/320/240"></img>
+                    <h2>Card Title</h2>
+                    <p>Card Description</p>
+                </div>
             </div>
         </>
     );
