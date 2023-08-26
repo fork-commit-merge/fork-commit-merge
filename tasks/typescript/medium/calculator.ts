faf6b532fe92e3d3540e34a01bb488694c1b8034
Tanishavats19 <tanishavats01@gmail.com>
2023-08-26 20:11:00 +0530

    Fixed issue

--- tasks/typescript/medium/calculator.ts
+++ tasks/typescript/medium/calculator.ts
@@ -1,7 +1,24 @@
 // TypeScript - Medium
 
 namespace Calculator {
-    // TODO: Implement a Simple TypeScript Calculator
+    export function add(a: number, b: number): number {
+        return a + b;
+      }
+    
+      export function subtract(a: number, b: number): number {
+        return a - b;
+      }
+    
+      export function multiply(a: number, b: number): number {
+        return a * b;
+      }
+    
+      export function divide(a: number, b: number): number | null {
+        if (b === 0) {
+          return null;
+        }
+        return a / b;
+      }
 }
 
 // Test cases
