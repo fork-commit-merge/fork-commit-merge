0d2c326443360a415ce8c30982c78621f4717453
Dudi Varaprasad <94173498+bezalel-7@users.noreply.github.com>
2023-08-27 16:29:10 +0530

    Update tempConverter.cpp
    added cpp solution

--- tasks/c++/easy/tempConverter.cpp
+++ tasks/c++/easy/tempConverter.cpp
@@ -1,3 +1,22 @@
 // C++ - Easy
 
 // TODO:  Create a Basic C++ Program to Convert Temperature from Celsius to Fahrenheit
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float celsius, fahrenheit;
+
+    // Input temperature in Celsius
+    cout << "Enter temperature in Celsius: ";
+    cin >> celsius;
+
+    // Convert Celsius to Fahrenheit
+    fahrenheit = (celsius * 9/5) + 32;
+
+    // Display the result
+    cout << "Temperature in Fahrenheit: " << fahrenheit << endl;
+
+    return 0;
+}
