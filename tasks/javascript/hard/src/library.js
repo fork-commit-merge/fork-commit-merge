b0c73762063335d2f882c60fa83d33bc5d6f0795
fanmadestudio <110520844+fanmadestudio@users.noreply.github.com>
2023-08-27 18:19:17 +0700

    Fixed issue

--- tasks/javascript/hard/src/library.js
+++ tasks/javascript/hard/src/library.js
@@ -1,47 +1,64 @@
 // JavaScript - Hard
 
 class Library {
-    constructor() {
-        this.books = [];
-    }
+  constructor() {
+    this.books = [];
+  }
 
-    createBook(title, author) {
-        const book = {
-            title,
-            author,
-            id: this.books.length + 1,
-        };
-        this.books.push(book);
-        return book;
-    }
+  createBook(title, author) {
+    const book = {
+      title,
+      author,
+      id: this.books.length + 1,
+    };
+    this.books.push(book);
+    return book;
+  }
 
-    getBook(id) {
-        return this.books.find((book) => book.id === id);
-    }
+  getBook(id) {
+    return this.books.find((book) => book.id === id);
+  }
 
-    updateBook(id, title, author) {
-        const bookIndex = this.books.findIndex((book) => book.id === id);
-        if (bookIndex !== -1) {
-            this.books[bookIndex] = { id, title, author };
-        }
-        return this.books[bookIndex];
+  updateBook(id, title, author) {
+    const bookIndex = this.books.findIndex((book) => book.id === id);
+    if (bookIndex !== -1) {
+      this.books[bookIndex] = { id, title, author };
     }
+    return this.books[bookIndex];
+  }
 
-    deleteBook(id) {
-        const bookIndex = this.books.findIndex((book) => book.id === id);
-        if (bookIndex !== -1) {
-            this.books.splice(bookIndex, 1);
-        }
-        return this.books;
+  deleteBook(id) {
+    const bookIndex = this.books.findIndex((book) => book.id === id);
+    if (bookIndex !== -1) {
+      this.books.splice(bookIndex, 1);
     }
+    return this.books;
+  }
 
-    searchBooks(query) {
-        // TODO: Implement search functionality
-    }
 
-    filterBooks(criteria) {
-        // TODO: Implement filter functionality
-    }
+//   searchBooks(query) {
+//     // TODO: Implement search functionality
+    
+//   }
+
+  searchBooks(query) {
+    // TODO: Implement search functionality
+    return this.books.filter(book => book.title.includes(query) || book.author.includes(query));
+  }
+
+  filterBooks(criteria) {
+    // TODO: Implement filter functionality
+    return this.books.filter(book => {
+      let match = true;
+      for (const key in criteria) {
+          if (book[key] !== criteria[key]) {
+              match = false;
+              break;
+          }
+      }
+      return match;
+  });
+  }
 }
 
 module.exports = Library;
