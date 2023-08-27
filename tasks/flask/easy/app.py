20d4bfdc2e9024957122713f4a5ac0b750819795
GitHub Action <action@github.com>
2023-08-27 10:49:11 +0000

    Revert "Merge pull request #314 from sjadhav07/patch-1"
    This reverts commit 5669eab703691894f5d0ba0fd2d46b8292514553, reversing
    changes made to d4a41f9d3ffafcd3dbb429411cf21ca53d9ce736.

--- tasks/flask/easy/app.py
+++ tasks/flask/easy/app.py
@@ -1,14 +1,7 @@
-# Import the Flask class from the flask module
+# Flask - Easy
+
 from flask import Flask
 
-# Create an instance of the Flask class
 app = Flask(__name__)
 
-# Define a route for the root url ("/")
-@app.route("/")
-def hello():
-  return "Hello, Falsk!"
-
-# Run the Flask app if this script is executed directly
-if __name__ == "__main__":
-  app.run()
+# TODO: Implement rest of the code here
