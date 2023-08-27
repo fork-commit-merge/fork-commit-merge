551bf42138921becf33c733893d48751f81037a0
Niko Hoffrén <niko.hoffren@gmail.com>
2023-08-27 17:51:58 +0300

    Update Django - Easy issue - Hello World - Settings

--- tasks/django/easy/hello_world/hello_world/settings.py
+++ tasks/django/easy/hello_world/hello_world/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=i7itqe#e13f=u97*99+@yrj!saeke=ctb2zf(o2e#p^v#n*01'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
