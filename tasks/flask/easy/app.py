924175d8167dd61227388194299dc328856bc111
Sudarshan Jadhav <117652787+sjadhav07@users.noreply.github.com>
2023-08-27 15:37:36 +0530

    feat: Add route for root URL returning "Hello, Flask!"
    In this commit, a new route has been added to the Flask app to
    handle the root URL ("/"). The route is defined using the @app.route("/") decorator, and it maps to the hello() function. When the root URL is accessed, the hello() function responds with a simple "Hello, Flask!" message. This enhancement improves the initial functionality of the app by providing a basic response to the root URL.
    
    Changes Made:
    - Added route for root URL
    - Implemented hello() function to handle the route
    
    To test the changes, run the app and navigate to http://localhost:5000/ in a web browser. The expected response should be "Hello, Flask!" displayed on the page.

--- tasks/flask/easy/app.py
+++ tasks/flask/easy/app.py
@@ -1,7 +1,14 @@
-# Flask - Easy
-
+# Import the Flask class from the flask module
 from flask import Flask
 
+# Create an instance of the Flask class
 app = Flask(__name__)
 
-# TODO: Implement rest of the code here
+# Define a route for the root url ("/")
+@app.route("/")
+def hello():
+  return "Hello, Falsk!"
+
+# Run the Flask app if this script is executed directly
+if __name__ == "__main__":
+  app.run()
