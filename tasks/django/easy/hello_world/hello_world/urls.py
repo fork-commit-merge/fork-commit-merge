8ad7e6406031b3b8f628d413692b13e59b85d531
Alexpeain <tonghsengsom@gmail.com>
2023-08-27 23:52:04 +0630

    add contact form to html medium and fixing django easy

--- tasks/django/easy/hello_world/hello_world/urls.py
+++ tasks/django/easy/hello_world/hello_world/urls.py
@@ -5,5 +5,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # TODO: Add /hello path here
+    #TODO: Add /hello path here 
+    path('hello/',include('hello_world_app.urls')),
 ]
--- tasks/django/easy/hello_world/hello_world_app/views.py
+++ tasks/django/easy/hello_world/hello_world_app/views.py
@@ -4,3 +4,4 @@ from django.http import HttpResponse
 
 def hello_world(request):
     # TODO: Add return statement here
+    return HttpResponse("Hello, World!")
