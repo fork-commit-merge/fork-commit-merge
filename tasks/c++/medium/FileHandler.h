778b7f09f4d6f6dffdd33d9dc20a9d8c41bf039c
Dudi Varaprasad <94173498+bezalel-7@users.noreply.github.com>
2023-08-29 00:02:17 +0530

    Update FileHandler.h
    added filehandler.h

--- tasks/c++/medium/FileHandler.h
+++ tasks/c++/medium/FileHandler.h
@@ -2,8 +2,17 @@
 
 #pragma once
 #include <string>
+#ifndef FILEHANDLER_H
+#define FILEHANDLER_H
+
+#include <string>
 
 class FileHandler {
 public:
-    // TODO: Implement the FileHandler class
+    static std::string ReadFile(const std::string& filePath);
+    static bool WriteFile(const std::string& filePath, const std::string& content);
+    static bool AppendFile(const std::string& filePath, const std::string& content);
+    static bool FileExists(const std::string& filePath);
 };
+
+
