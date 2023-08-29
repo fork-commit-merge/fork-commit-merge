b50b8f16c015fd94137f02da22741247bc9329be
Niko Hoffrén <niko.hoffren@gmail.com>
2023-08-29 20:28:09 +0300

    Add Flutter to the project with Easy level issue

---
+++ tasks/flutter/easy/hello_flutter/android/app/src/main/kotlin/com/example/hello_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hello_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tasks/flutter/easy/hello_flutter/lib/main.dart
@@ -0,0 +1,20 @@
+// Flutter - Easy
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ForkCommitMerge());
+}
+
+class ForkCommitMerge extends StatelessWidget {
+  const ForkCommitMerge({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          // TODO: Implement the appbar and body properties
+      ),
+    );
+  }
+}
