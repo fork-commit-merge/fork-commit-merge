6a350a3362b633f70e59db162c1c4a187b717438
GitHub Action <action@github.com>
2023-08-29 04:38:30 +0000

    Revert "Merge pull request #340 from bezalel-7/bezalel-7-patch-1"
    This reverts commit 1b308a5bd7c5d4485750ff42360c83ba9c68050b, reversing
    changes made to a98897ef406cb4093203fec2cf84ec5fd34c2e59.

--- tasks/c++/medium/FileHandler.h
+++ tasks/c++/medium/FileHandler.h
@@ -2,17 +2,8 @@
 
 #pragma once
 #include <string>
-#ifndef FILEHANDLER_H
-#define FILEHANDLER_H
-
-#include <string>
 
 class FileHandler {
 public:
-    static std::string ReadFile(const std::string& filePath);
-    static bool WriteFile(const std::string& filePath, const std::string& content);
-    static bool AppendFile(const std::string& filePath, const std::string& content);
-    static bool FileExists(const std::string& filePath);
+    // TODO: Implement the FileHandler class
 };
-
-
