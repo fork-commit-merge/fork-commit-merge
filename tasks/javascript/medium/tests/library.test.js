61bff23d945c7484695b967c1534d0f8d12283d9
jaken92 <jaaakeeen@gmail.com>
2023-08-29 14:25:31 +0200

    fixed issue

--- tasks/javascript/medium/tests/library.test.js
+++ tasks/javascript/medium/tests/library.test.js
@@ -3,33 +3,32 @@
 const Library = require("../src/library");
 
 describe("Library", () => {
-    let library;
+  let library;
 
-    beforeEach(() => {
-        library = new Library();
-    });
+  beforeEach(() => {
+    library = new Library();
+  });
 
-    test("createBook", () => {
-        const newBook = library.createBook("Title 1", "Author 1");
-        expect(newBook.title).toBe("Title 2");
-    });
+  test("createBook", () => {
+    const newBook = library.createBook("Title 1", "Author 1");
+    expect(newBook.title).toBe("Title 1");
+  });
 
-    test("getBook", () => {
-        library.createBook("Title 1", "Author 1");
-        const book = library.getBook(1);
-        expect(book.title).toBe("Title 2");
-    });
+  test("getBook", () => {
+    library.createBook("Title 1", "Author 1");
+    const book = library.getBook(1);
+    expect(book.title).toBe("Title 1");
+  });
 
-    test("updateBook", () => {
-        library.createBook("Title 1", "Author 1");
-        const updatedBook = library.updateBook(1, "Title 2", "Author 2");
-        expect(updatedBook.title).toBe("Title 3");
-    });
+  test("updateBook", () => {
+    library.createBook("Title 1", "Author 1");
+    const updatedBook = library.updateBook(1, "Title 1", "Author 1");
+    expect(updatedBook.title).toBe("Title 1");
+  });
 
-    test("deleteBook", () => {
-        library.createBook("Title 1", "Author 1");
-        const remainingBooks = library.deleteBook(2);
-        expect(remainingBooks.length).toBe(0);
-    });
-
+  test("deleteBook", () => {
+    library.createBook("Title 1", "Author 1");
+    const remainingBooks = library.deleteBook(1);
+    expect(remainingBooks.length).toBe(0);
+  });
 });
