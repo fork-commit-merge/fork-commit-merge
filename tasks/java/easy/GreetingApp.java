7ed081394f314f6707cdbca292ca8f0d53701a84
shayan-shaikh <65915426+shayan-shaikh@users.noreply.github.com>
2023-08-31 16:05:29 +0530

    feat: Added greeting in Java

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,8 +1,14 @@
+
 // Java - Easy
+import java.util.Scanner;
 
 public class GreetingApp {
 
     public static void main(String[] args) {
-        // TODO: Implement a Simple Greeting Application
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name : ");
+        String name = sc.nextLine();
+        System.out.println(String.format("Hello %s ! Nice to meet you!", name));
+        sc.close();
     }
 }
