4bb478a53b239c87423a3cc7164078cdeaa63554
GitHub Action <action@github.com>
2023-08-31 19:56:42 +0000

    Revert "Merge pull request #383 from mvoorhes/max-branch"
    This reverts commit c7e53d7c48fc3d74acaf7a27d869d915e977de5c, reversing
    changes made to 63f9fd6682ae0dd4094b2b0a2e07d3257e0dba6b.

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,12 +1,7 @@
 # Python - Medium
 
 def bubble_sort(arr):
-    for x in range(len(arr)):
-        for y in range(0, len(arr) - x - 1, 1):
-            if arr[y] > arr[y + 1]:
-                temp = arr[y]
-                arr[y] = arr[y + 1]
-                arr[y + 1] = temp
+    # TODO: Implement the bubble sort algorithm
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
