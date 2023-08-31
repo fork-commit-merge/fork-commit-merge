59cac1b648f08991073d8738b9013006d38b35bb
GitHub Action <action@github.com>
2023-08-31 10:58:26 +0000

    Revert "Merge pull request #367 from shayan-shaikh/main"
    This reverts commit fce80d5e6b1a78e113e6834c2b7e2d5d667d564e, reversing
    changes made to 451fe08097d0a1aba1cd12444801bff8e1951b08.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,14 +1,8 @@
-
 // Java - Easy
-import java.util.Scanner;
 
 public class GreetingApp {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name : ");
-        String name = sc.nextLine();
-        System.out.println(String.format("Hello %s ! Nice to meet you!", name));
-        sc.close();
+        // TODO: Implement a Simple Greeting Application
     }
 }
