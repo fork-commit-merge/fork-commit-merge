e2103ad0c6b5b7b228258a7088fb30a82b599e69
Sherman <shermantaymk@gmail.com>
2023-09-01 16:29:29 +0800

    add descriptive print statements

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -3,6 +3,6 @@
 # TODO: Create the Simple Python Program to Interact with Lists
 
 num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(num_list)
-print(sum(num_list))
+print(f"The list of numbers contain: {num_list}")
+print(f"The sum of all the numbers is: {sum(num_list)}")
 
