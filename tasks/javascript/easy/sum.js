b94220b2a776f9038b9cc3e6eb2aeb6438aaab8b
GitHub Action <action@github.com>
2023-09-01 18:14:16 +0000

    Revert "Merge pull request #400 from ronitsharma03/patch-2"
    This reverts commit 7a77a21610745ed93377c569ec26f20ddebc7e00, reversing
    changes made to 756cab125f8eeab0280f281ffb093516bbb4755c.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,8 +1,4 @@
 // JavaScript - Easy
 
 // TODO: Write a function that returns the sum of two numbers
-function sum(n1, n2){
-  let ans = n1+n2
-  console.log("The sum of " +n1+ " and " +n2+ " is " +ans)
-}
-sum(1, 2);
+
