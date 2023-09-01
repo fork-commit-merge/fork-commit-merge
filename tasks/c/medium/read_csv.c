7284cf9d369501ac128cc25853ce8f9e95aaf620
GitHub Action <action@github.com>
2023-09-01 06:11:15 +0000

    Revert "Merge pull request #384 from mvoorhes/max-branch"
    This reverts commit c5b03d04b956053c25eb3bbd1068a10b1b01534e, reversing
    changes made to 45e9de38c9d40c0ed653c0aad7bd6df3be080909.

--- tasks/c/medium/read_csv.c
+++ tasks/c/medium/read_csv.c
@@ -5,35 +5,6 @@
 #include <string.h>
 
 int main() {
-    FILE* f = fopen("data.csv", "r");
-    char word[128];
-    char name[128];
-    int age;
-    char ageChar[128];
-    int i = 0;
-    while ((fscanf(f, "%s\n", word)) != EOF) {
-        if (i == 0) {
-            printf("Header: %s\n", word);
-            i++;
-            continue;
-        }
-        printf("\n");
-        memset(name, 0, strlen(name));
-        memset(ageChar, 0, strlen(ageChar));
-        printf("Reading line: %s\n", word);
-        int comma;   // Find comma
-        for (comma = strlen(word) - 1; word[comma] != ','; comma--);
-        strncpy(name, word, comma); // Get name
-        printf("Name: %s\n", name);
-        int index = 0;
-        // Get age
-        for (comma = comma + 1; comma < strlen(word); comma++) {
-            ageChar[index] = word[comma];
-            index++;
-        }
-        ageChar[index] = '\0';
-        printf("Age: %s\n", ageChar);
-    }
-    fclose(f);
+    // TODO: Implement the program that reads and displays the contents of data.csv file
     return 0;
 }
