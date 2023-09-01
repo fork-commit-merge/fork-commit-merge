2cf66d959f9899760df9aa8f1e4529b9661af319
GitHub Action <action@github.com>
2023-09-01 14:00:23 +0000

    Revert "Merge pull request #392 from iamsirenstorm/fix-256"
    This reverts commit 0faf31b35bd80475b86f92c5a5989abcd80ae30f, reversing
    changes made to eedaf01176df0ef5538b97406cfd96d3abce2b60.

--- tasks/c++/medium/FileHandler.cpp
+++ tasks/c++/medium/FileHandler.cpp
@@ -1,88 +1,14 @@
 // C++ - Medium
 
-#include "FileHandler.h"  // FileHandler class
-#include <fstream>        // std::ifstream, std::ofstream
-#include <sstream>        // std::stringstream
+#include "FileHandler.h"
+#include <fstream>
+#include <sstream>
 
-std::string FileHandler::ReadFile(const std::string& filePath)
-{
-  // Input stream
-  std::ifstream file;
+// TODO: Implement the FileHandler::ReadFile method
 
-  file.open(filePath);
+// TODO: Implement the FileHandler::WriteFile method
 
-  if (file.is_open())
-  {
-    // Stringstream to build contents of file
-    std::stringstream ss;
-    // Temporary string to hold each line
-    std::string linestr;
-    // State flag so we don't start with a newline
-    bool bFirstLine = true;
+// TODO: Implement the FileHandler::AppendFile method
 
-    // Concatenate each line to stringstream
-    while (std::getline(file, linestr))
-    {
-      if (bFirstLine)
-      {
-        ss << linestr;
-        bFirstLine = false;
-      }
-      else ss << '\n' << linestr;
-    }
-    file.close();
+// TODO: Implement the FileHandler::FileExists method
 
-    return ss.str();
-  }
-  else return "File could not be read.";
-}
-
-bool FileHandler::WriteFile(const std::string& filePath, const char* toWrite)
-{
-  // Output stream
-  std::ofstream file;
-
-  file.open(filePath);
-
-  if (file.is_open())
-  {
-    file << toWrite;
-    // Check if writing is successful.
-    bool bSuccess = file.good();
-    file.close();
-    return bSuccess;
-  }
-  else return false;
-}
-
-bool FileHandler::AppendFile(const std::string& filePath, const char* toAppend)
-{
-  // Output stream
-  std::ofstream file;
-
-  file.open(filePath, std::ios::app);
-
-  if (file.is_open())
-  {
-    file << toAppend;
-    // Check if writing is successful.
-    bool bSuccess = file.good();
-    file.close();
-    return bSuccess;
-  }
-  else return false;
-}
-
-bool FileHandler::FileExists(const std::string& filePath)
-{
-  // Input stream
-  std::ifstream file;
-
-  file.open(filePath);
-  if (file.good())
-  {
-    file.close();
-    return true;
-  }
-  else return false;
-}
