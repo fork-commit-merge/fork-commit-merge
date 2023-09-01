7fd5e3095610f2d0f656ab1774c00c39760cf40f
GitHub Action <action@github.com>
2023-09-01 19:24:39 +0000

    Revert "Merge pull request #404 from aditya-garg-09-01-2002/python"
    This reverts commit f7000f43453a6038774987f27d16a8903670acb5, reversing
    changes made to c73c9ab46c5c927f39c457dceda9d2baddb4ab85.

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,15 +1,7 @@
 # Python - Medium
 
 def bubble_sort(arr):
-    for i in range(len(arr)):
-        swapped=False
-        for j in range(len(arr)-i-1):
-            if arr[j]>arr[j+1]:
-                swapped=True
-                arr[j],arr[j+1]=arr[j+1],arr[j]
-        if swapped==False:
-            break
-    
+    # TODO: Implement the bubble sort algorithm
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
