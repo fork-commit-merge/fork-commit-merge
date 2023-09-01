9dd36abcb8d639bb0a829c61ab9c869d9ba10d59
GitHub Action <action@github.com>
2023-09-01 14:35:52 +0000

    Revert "Merge pull request #393 from Sweetdevil144/main"
    This reverts commit 981be995a937438f2dda38f2503c963b9ad94d19, reversing
    changes made to 9c24a22b1cfc195d969c3d3416680ab71dcbebc2.

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,15 +1,7 @@
 # Python - Medium
 
 def bubble_sort(arr):
-    n = len(arr)
-  
-    # Traverse through all elements in the list
-    for i in range(n):
-        # Last i elements are already sorted, so we skip them
-        for j in range(0, n-i-1):
-            # Swap if the element found is greater than the next element
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+    # TODO: Implement the bubble sort algorithm
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
