f75066b4f5d07742aaf4187b444d3343da254050
safiya siddiqui <safiyaqazi517@gmail.com>
2023-09-01 11:55:59 +0530

    make card in tailwindcss

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,4 +1,14 @@
 // JavaScript - Easy
 
-// TODO: Write a function that returns the sum of two numbers
+// TODO: Write a function that returns the sum of two num
 
+let a = 2;
+let b = 4;
+const sumoftwoNumber = (a, b) => {
+  const sum = a + b;
+  console.log(sum);
+};
+sumoftwoNumber(a, b);
+
+//
+//
