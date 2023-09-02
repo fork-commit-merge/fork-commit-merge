a54c5162f41b91075f9a403efeb6fa96e2914926
Sahil Pannu <81547349+sahilpannu@users.noreply.github.com>
2023-09-02 23:11:13 +0530

    Update sum.js
    solved #378 
    
    Easy & good for beginners

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,4 +1,9 @@
 // JavaScript - Easy
 
 // TODO: Write a function that returns the sum of two numbers
-
+ function sum(a, b){
+            var c = a+b;
+            return c;
+        }
+        
+ alert(sum(2,2));
