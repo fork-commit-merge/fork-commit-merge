9ded9cd28fd4a21688ba9fcffcd39e830f0460b0
GitHub Action <action@github.com>
2023-09-02 17:15:43 +0000

    Revert "Merge pull request #413 from sahilpannu/patch-1"
    This reverts commit 7aca273831b01e370fe5197405386427b1b42b16, reversing
    changes made to 7a7c6c9d7f22ccfee901d7848104a92a4764eaf9.

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -2,30 +2,6 @@
 
 def bubble_sort(arr):
     # TODO: Implement the bubble sort algorithm
-    # Python program for implementation of Bubble Sort
-	n = len(arr)
-	# optimize code, so if the array is already sorted, it doesn't need
-	# to go through the entire process
-	swapped = False
-	# Traverse through all array elements
-	for i in range(n-1):
-		# range(n) also work but outer loop will
-		# repeat one time more than needed.
-		# Last i elements are already in place
-		for j in range(0, n-i-1):
-
-			# traverse the array from 0 to n-i-1
-			# Swap if the element found is greater
-			# than the next element
-			if arr[j] > arr[j + 1]:
-				swapped = True
-				arr[j], arr[j + 1] = arr[j + 1], arr[j]
-		
-		if not swapped:
-			# if we haven't needed to make a single swap, we
-			# can just exit the main loop.
-			return
-
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
