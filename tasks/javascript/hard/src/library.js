acefec2eb0a5e6935c8e3cc1a0eae554f0c67634
safiya siddiqui <safiyaqazi517@gmail.com>
2023-09-03 22:25:10 +0530

    fix issue #369

--- tasks/javascript/hard/src/library.js
+++ tasks/javascript/hard/src/library.js
@@ -1,47 +1,48 @@
 // JavaScript - Hard
 
 class Library {
-    constructor() {
-        this.books = [];
-    }
+  constructor() {
+    this.books = [];
+  }
 
-    createBook(title, author) {
-        const book = {
-            title,
-            author,
-            id: this.books.length + 1,
-        };
-        this.books.push(book);
-        return book;
-    }
+  createBook(title, author) {
+    const book = {
+      title,
+      author,
+      id: this.books.length + 1,
+    };
+    this.books.push(book);
+    return book;
+  }
 
-    getBook(id) {
-        return this.books.find((book) => book.id === id);
-    }
+  getBook(id) {
+    return this.books.find((book) => book.id === id);
+  }
 
-    updateBook(id, title, author) {
-        const bookIndex = this.books.findIndex((book) => book.id === id);
-        if (bookIndex !== -1) {
-            this.books[bookIndex] = { id, title, author };
-        }
-        return this.books[bookIndex];
+  updateBook(id, title, author) {
+    const bookIndex = this.books.findIndex((book) => book.id === id);
+    if (bookIndex !== -1) {
+      this.books[bookIndex] = { id, title, author };
     }
+    return this.books[bookIndex];
+  }
 
-    deleteBook(id) {
-        const bookIndex = this.books.findIndex((book) => book.id === id);
-        if (bookIndex !== -1) {
-            this.books.splice(bookIndex, 1);
-        }
-        return this.books;
+  deleteBook(id) {
+    const bookIndex = this.books.findIndex((book) => book.id === id);
+    if (bookIndex !== -1) {
+      this.books.splice(bookIndex, 1);
     }
+    return this.books;
+  }
 
-    searchBooks(query) {
-        // TODO: Implement search functionality
-    }
+  searchBooks(query) {
+    // TODO: Implement search functionality
+  }
 
-    filterBooks(criteria) {
-        // TODO: Implement filter functionality
-    }
+  filterBooks(criteria) {
+    // TODO: Implement filter functionality
+    const filterbooks = this.books.filter((book) => book);
+  }
 }
 
 module.exports = Library;
