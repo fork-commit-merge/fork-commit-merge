e5b35d69dccd652fbd43e9a0804ed7abc397a615
Siddant Tripathi <sidtripathi95@gmail.com>
2023-09-04 19:06:53 +0530

    Fixed Issue

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -5,6 +5,29 @@ package tasks.java.medium;
 public class Calculator {
     // TODO: Implement the Calculator class
 
+    //addition method
+
+    public static double add(double num1, double num2){
+        return num1 + num2;
+    }
+
+    //subtraction method
+    public static double subtract(double num1, double num2){
+        return num1 - num2;
+    }
+
+    //multiplication method
+    public static double multiply(double num1, double num2){
+        return num1 * num2;
+    }
+
+    //division method
+    public static double divide(double num1, double num2){
+        if(num2 == 0){
+            throw new ArithmeticException("Division by zero is not allowed");
+        }
+        return num1 / num2;
+    }
     public static void main(String[] args) {
         //! Dont't change the tests in the main method!
         Calculator calc = new Calculator();
