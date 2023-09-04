1a8cbf1436b946cc38f78f2f776e5df2ec2a68bc
Siddant Tripathi <sidtripathi95@gmail.com>
2023-09-04 18:41:49 +0530

    greeting added changes made

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,8 +1,23 @@
 // Java - Easy
 
+import java.util.Scanner;
+
 public class GreetingApp {
 
     public static void main(String[] args) {
         // TODO: Implement a Simple Greeting Application
+        //create a scanner object to read user name
+        Scanner sc = new Scanner(System.in);
+    
+        //prompt the user to enter name
+        System.out.println("Enter your name: ");
+
+        //read the user name as string
+        String name = sc.nextLine();
+
+        //closing scanner object
+        sc.close();
+        //print the message
+        System.out.println("Hello, "+name+"! Nice to meet you!");
     }
 }
