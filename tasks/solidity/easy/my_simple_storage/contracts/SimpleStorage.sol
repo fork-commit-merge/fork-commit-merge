51940003f45d48122f0282f46108c67724e51fa5
Niko Hoffrén <niko.hoffren@gmail.com>
2023-09-04 20:57:14 +0300

    Add Solidity to the languages with Easy level issue

---
+++ tasks/solidity/easy/my_simple_storage/contracts/SimpleStorage.sol
@@ -0,0 +1,10 @@
+// Solidity - Easy
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    //TODO: Implement function to set the number
+
+    //TODO: Implement function to get the number
+}
