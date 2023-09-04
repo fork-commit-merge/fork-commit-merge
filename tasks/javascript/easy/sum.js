1f96dcc83f6f829e51c949293b4f035d2c177241
Nishant <nishantnl2003@outlook.com>
2023-09-04 18:21:16 +0530

    Update sum.js

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,4 +1,11 @@
 // JavaScript - Easy
 
 // TODO: Write a function that returns the sum of two numbers
+function sum(a,b){
+    var c = a + b;
+    return c;
+}
 
+var a = 1, b = 2;
+
+console.log("The sum of "+a+" and "+b+" is "+sum(1, 2));
