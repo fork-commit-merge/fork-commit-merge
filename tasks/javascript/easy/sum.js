ac72c024a2b81d89bf6074c0f1f125a6c694633b
GitHub Action <action@github.com>
2023-09-05 05:18:56 +0000

    Revert "Merge pull request #432 from pradeeptosarkar/issue-fix"
    This reverts commit bb88e9b1a92459c6c2d766eb3eaf94aa6daa41af, reversing
    changes made to b0b7a576f0ce2039fafbd6a30cc5c501572d6619.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -2,11 +2,3 @@
 
 // TODO: Write a function that returns the sum of two numbers
 
-const num1 = 1;
-const num2 = 2;
-
-// add two numbers
-const sum = num1 + num2;
-
-// display the sum
-console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + sum + '.');
