480152ae21be252c34eab00ac9684098170e8bb8
GitHub Action <action@github.com>
2023-09-06 05:46:27 +0000

    Revert "Merge pull request #437 from samba2001/main"
    This reverts commit 0204df7c089e3b497c6bd60f51d7dfc67fb31512, reversing
    changes made to 8c756086bfe316a1f75b06277483d76c5fe5b0e5.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,8 +1,4 @@
 // JavaScript - Easy
+
 // TODO: Write a function that returns the sum of two numbers
-function sum(a, b) {
-    sum = a+b;
-    console.log(" The sum of ${a} and ${b} is ${sum}");
-    return sum;
-}
 
