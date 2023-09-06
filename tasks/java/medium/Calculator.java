57d60dea74464a5e65b74219ca2aaddf0fad6eea
Bhavya <110487270+bhaavvya@users.noreply.github.com>
2023-09-06 13:34:39 +0530

    Update Calculator.java

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -2,9 +2,8 @@
 
 package tasks.java.medium;
 
-public class Calculator {
-    // TODO: Implement the Calculator class
-
+public class Main{
+    
     public static void main(String[] args) {
         //! Dont't change the tests in the main method!
         Calculator calc = new Calculator();
@@ -14,5 +13,26 @@ public class Calculator {
         System.out.println("Multiplication: " + calc.multiply(5, 3));
         System.out.println("Division: " + calc.divide(5, 3));
     }
+    
 }
+
+
+class Calculator {
+    // TODO: Implement the Calculator class
+    public static int add(int a,int b){
+        return (a+b);
+    }
+    public static int subtract(int a,int b){
+        return (a-b);
+    }
+    public static int multiply(int a,int b){
+        return (a*b);
+    }
+    public static double divide(int a,int b){
+    return (double)(a/b);
+}
+}
+
+    
+
 
