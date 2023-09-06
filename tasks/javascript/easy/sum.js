f0f99120ff850905de98e1d81cb4dbc01f8af0d8
samba2001 <reddy200101@gmail.com>
2023-09-06 10:28:30 +0530

    added sum function in js file

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,4 +1,8 @@
 // JavaScript - Easy
-
 // TODO: Write a function that returns the sum of two numbers
+function sum(a, b) {
+    sum = a+b;
+    console.log(" The sum of ${a} and ${b} is ${sum}");
+    return sum;
+}
 
