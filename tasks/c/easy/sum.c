f4892b620f9f14d6b48044c5666f55f9d1cdd9b4
GitHub Action <action@github.com>
2023-09-07 17:35:48 +0000

    Revert "Merge pull request #456 from LloydAAmaranto/sum_function"
    This reverts commit 0e9f7cf4601fb9c4644664e8bf468c69857fecac, reversing
    changes made to 1d72ae69f4c23675445154a3201384c7d2d26d7e.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,17 +1,11 @@
+// C - Easy
+
 #include <stdio.h>
 
-int sum(int a, int b);
+// TODO: Implement the sum function
 
 int main() {
-    int a = 0;
-    int b = 0;
-    printf("Enter in two numbers: ");
-    scanf("%d", &a);
-    scanf("%d", &b);
-    printf("the sum is: %d\n", sum(a,b));
-    return 0;
-}
+    // TODO: Implement the main function
 
-int sum(int a, int b){
-    return a+b;
+    return 0;
 }
