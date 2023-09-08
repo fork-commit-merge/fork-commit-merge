3386d36700bd18fb8d0c2235edd82c79d8830d2d
GitHub Action <action@github.com>
2023-09-08 06:05:01 +0000

    Revert "Merge pull request #462 from RaiZiStyle/fix-issue-c-sum"
    This reverts commit 0be06757a3ded54c460300a515f32a7949b1fe43, reversing
    changes made to 6bc0b4981163b7f39d6a93f5dfc8a64009c822c0.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -2,28 +2,10 @@
 
 #include <stdio.h>
 
-int sum(int a, int b){
-    int total  = a + b;
-    return total;
-}
-
-// This is from "https://www.geeksforgeeks.org/how-to-sum-two-integers-without-using-arithmetic-operators-in-cc/"
-// I won't try to explain it as I don't fully understand it.
-int sum_without_plus(int a, int b){
-    char *p = a;
-    return (int)&p[b];
-}
+// TODO: Implement the sum function
 
 int main() {
-    int b=0;
-    int a = 0;
-    int total = 0;
-    printf("Enter two numbers: ");
-    scanf("%d %d", &a, &b);
-    total = sum(a,b);
-    printf("The sum is : %d\n", total);
-    total = sum_without_plus(a,b);
-    printf("The sum_without_plus is : %d\n", total);
+    // TODO: Implement the main function
 
     return 0;
 }
