9c6c4a4bfc513c2fadde85b3cecd899ffa3de9e6
BuildTools <unconfigured@null.spigotmc.org>
2023-09-08 18:16:01 -0500

    Added comments to fixed issue

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -2,12 +2,18 @@
 
 # TODO: Create the Simple Python Program to Interact with Lists
 def main():
+
+    # Create a List
     num_list = []
+
+    # Use a for loop to populate the list of the first 10 natural numbers
     for i in range(1,11):
         num_list.append(i)
     
+    # Print the List
     print(num_list)
 
+    # Print the sum of the numbers in the list using sum()
     print(sum(num_list))
 
 main()
