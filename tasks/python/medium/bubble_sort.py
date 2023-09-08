f1fecfb616aab95e906daa93197fc81d15e75996
GitHub Action <action@github.com>
2023-09-08 19:11:30 +0000

    Revert "Merge pull request #469 from BernEsports/fix-issue"
    This reverts commit 08472ca3a6239ed75f66219388d74bfb8007e748, reversing
    changes made to 8d54b7a3258b103e3e08b24363beff0eeeefc7f5.

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -2,14 +2,6 @@
 
 def bubble_sort(arr):
     # TODO: Implement the bubble sort algorithm
-    """This func implements bubble sort"""
-    
-    # starts from i=4, j=0, i-1 for each iteration
-    for i in range(len(arr)-1, 0, -1):
-        for j in range(i):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-        
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
