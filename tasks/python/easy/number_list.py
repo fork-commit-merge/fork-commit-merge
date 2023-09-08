75da3428a2922721654b2410a547090d7e30f4cf
BuildTools <unconfigured@null.spigotmc.org>
2023-09-08 18:11:47 -0500

    Fixed Easy Python Issue

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -1,3 +1,13 @@
 # Python - Easy
 
 # TODO: Create the Simple Python Program to Interact with Lists
+def main():
+    num_list = []
+    for i in range(1,11):
+        num_list.append(i)
+    
+    print(num_list)
+
+    print(sum(num_list))
+
+main()
