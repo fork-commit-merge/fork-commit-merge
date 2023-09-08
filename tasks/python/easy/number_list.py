8e04ad0090d3787485e59d8d307655eba5ff03a2
GitHub Action <action@github.com>
2023-09-08 17:29:30 +0000

    Revert "Merge pull request #467 from BernEsports/fix-issue"
    This reverts commit f3522a8081a4a574f0f5f8c0de75acbb94acf24d, reversing
    changes made to 640a72480bcffc5a2692b3f172f29a0da30c8cf1.

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -1,13 +1,3 @@
 # Python - Easy
 
 # TODO: Create the Simple Python Program to Interact with Lists
-
-def sum_list():
-    """Creates list from 1 to 10 and returns it"""
-    arr_of_nums = []
-    for i in range(1, 11):
-        arr_of_nums.append(i)
-    return arr_of_nums
-
-print(sum_list())
-print(sum(sum_list()))
