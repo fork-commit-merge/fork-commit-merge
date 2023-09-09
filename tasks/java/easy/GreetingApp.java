96702ece52fb55bc2f1ff043014678ebd6574462
GitHub Action <action@github.com>
2023-09-09 05:54:19 +0000

    Revert "Merge pull request #473 from mokshanirugutti/fix/create-greeting-application-java"
    This reverts commit bddb2da59b9408823381c917ddbbcdd8cb46aeb2, reversing
    changes made to f76d8f7bdff1e3a99e4c568e6284434b4b75f215.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,13 +1,8 @@
 // Java - Easy
-import java.util.Scanner;
+
 public class GreetingApp {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter your name : ");
-        String name = in.nextLine();
-        System.out.println("Hello, "+name+"! Nice to meet you!");
-
-
+        // TODO: Implement a Simple Greeting Application
     }
 }
