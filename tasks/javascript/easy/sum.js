9991dd11eaea4a8ff820a8f4573a7905cfcaa6b5
GitHub Action <action@github.com>
2023-09-11 20:18:41 +0000

    Revert "Merge pull request #503 from Sweetdevil144/main"
    This reverts commit e8944d6b8afb78f0361d480c6fd2f6c185ab05ca, reversing
    changes made to 9dec2179f870d0c19b224cbd8b0bd482897564d3.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,9 +1,4 @@
-function addNumbers(a, b) {
-  return a + b;
-}
+// JavaScript - Easy
 
-// Example usage:
-const num1 = 1;
-const num2 = 2;
-const sum = addNumbers(num1, num2);
-console.log(`The sum of ${num1} and ${num2} is ${sum}.`); // This will output 3
+// TODO: Write a function that returns the sum of two numbers
+
