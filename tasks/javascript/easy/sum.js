7b35badd5d270e83d3ce1a5127e9eea94872bd72
Umair Amir <91899494+CodeWithUmair@users.noreply.github.com>
2023-09-11 22:13:49 +0500

    Update sum.js

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,4 +1,13 @@
 // JavaScript - Easy
-
 // TODO: Write a function that returns the sum of two numbers
 
+function sum(a, b) {
+  const result = a + b;
+  console.log(`The sum of ${a} and ${b} is ${result}.`);
+  return result;
+}
+
+// Test the sum function
+const num1 = 1;
+const num2 = 2;
+const total = sum(num1, num2);
