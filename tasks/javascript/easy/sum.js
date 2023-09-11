d5f8cff5b45419d012613c581c10e129c9153294
Abhinav Pandey <abhinavpandey1230@gmail.com>
2023-09-12 01:34:54 +0530

    Update sum.js

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -6,4 +6,4 @@ function addNumbers(a, b) {
 const num1 = 1;
 const num2 = 2;
 const sum = addNumbers(num1, num2);
-console.log(`The sum of ${num1} and ${num2} is ${sum}`); // This will output 3
+console.log(`The sum of ${num1} and ${num2} is ${sum}.`); // This will output 3
