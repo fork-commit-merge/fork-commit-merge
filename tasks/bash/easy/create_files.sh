6d96142c965208df06bcbd62736ae9fcc453e650
GitHub Action <action@github.com>
2023-09-12 17:34:52 +0000

    Revert "Merge pull request #517 from sanjay14073/patch-6"
    This reverts commit 63d877ce1bdf105d4488344442a30806ef83b5c5, reversing
    changes made to 2ccd93677f4b262fbe49c12ff11cc974d12b0cc9.

--- tasks/bash/easy/create_files.sh
+++ tasks/bash/easy/create_files.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
 # Bash - Easy
-touch file1.txt
-echo "echo Hello Shell!">file1.txt
-touch file2.txt
-echo "echo Hello Shell!">file2.txt
-touch file3.txt
-echo "echo Hello Shell!">file3.txt
-chmod +x file1.txt
-./file1.txt
-chmod +x file2.txt
-./file2.txt
-chmod +x file3.txt
-./file3.txt
+
 # TODO: Write the Bash script requested in the issue description
