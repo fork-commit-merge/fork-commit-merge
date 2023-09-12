7159dcd6b20613e9856418645912362222e17e39
Daniel Toba <danieloloruntoba681@gmail.com>
2023-09-12 23:53:00 +0100

    Update sum.js

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -2,3 +2,8 @@
 
 // TODO: Write a function that returns the sum of two numbers
 
+function sum(num1, num2) {
+  const total = num1 + num2;
+  console.log(`The sum of ${num1} and ${num2} is ${total}`);
+}
+sum(2, 3);
