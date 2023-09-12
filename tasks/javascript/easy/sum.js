6907c0168a90dfdb738cda7d4fc4f373785e9db0
GitHub Action <action@github.com>
2023-09-12 11:35:23 +0000

    Revert "Merge pull request #509 from ignaszd/html-hard"
    This reverts commit 1c8336071555416613fb7f8eeb49a9014027e07c, reversing
    changes made to af144e6ca7dc11c47404a3655ddfb764526e32db.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,8 +1,4 @@
-function sumOfTwoNumbers($a, $b)
-{
-    return $a + $b;
-}
-$a = 1;
-$b = 2;
-$sum = sumOfTwoNumbers($a, $b);
-console.log('The sum of ' + $a + ' and ' + $b + ' is ' + $sum)
+// JavaScript - Easy
+
+// TODO: Write a function that returns the sum of two numbers
+
