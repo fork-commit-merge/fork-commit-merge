16e4ff9acf2d3b19c3ce2a5f0cddf1b40071a3a9
GitHub Action <action@github.com>
2023-09-13 18:40:16 +0000

    Revert "Merge pull request #527 from ghulamali16/main"
    This reverts commit 91a1a0a5c0a0e1a35db407884efbec8b0576670e, reversing
    changes made to 789f50b8669d8f33ba82259972730678bb3b361c.

--- tasks/rust/easy/fibonacci.rs
+++ tasks/rust/easy/fibonacci.rs
@@ -2,28 +2,4 @@
 
 use std::io;
 
-fn main () {
-   loop {
-       println!("Please input a positive number");
-       let mut int = String::new();
-        io::stdin()
-       .read_line(&mut int)
-       .expect("");
-       let int: u32= match int.trim()
-       .parse() {
-           Ok(int) => int,
-           Err(_) => continue,
-       };
-       println!("Fibonacci ({}) => {}", int, fib(int));
-
-   }
-
-}
-
-fn fib (n: u32) -> u32 {
-   if n <= 0 {
-       return 0;
-   } else if n == 1 {
-       return 1;
-   }   fib(n - 1) + fib(n - 2)
-}
+// TODO: Implement fibonacci sequence in Rust
