d2d6739acc9c343eb83e1238c33920348e78c75c
Arjun <110415386+Arjun-A3@users.noreply.github.com>
2023-09-13 23:15:45 +0530

    added a recursive funtion to find the Nth fibonacci number

--- tasks/rust/easy/fibonacci.rs
+++ tasks/rust/easy/fibonacci.rs
@@ -3,3 +3,23 @@
 use std::io;
 
 // TODO: Implement fibonacci sequence in Rust
+use std::io;
+fn main () {
+        let mut num:u32=0;
+    let mut input = String::new();
+    
+    println!("Enter number: ");
+    io::stdin().read_line(&mut input).expect("Not a valid string");
+    num = input.trim().parse().expect("Not a valid number");
+       println!("The Fibonacci num is {}",fib(num));
+
+}
+
+
+fn fib (n: u32) -> u32 {
+   if n <= 0 {
+       return 0;
+   } else if n == 1 {
+       return 1;
+   }   fib(n - 1) + fib(n - 2)
+}
