7f873cd4b70915e83019413df2d6b44edf318869
GitHub Action <action@github.com>
2023-09-14 08:12:01 +0000

    Revert "Merge pull request #531 from sarafurd/fix-issue-497"
    This reverts commit 96062aeaa54887fdf4cd381d6541d48d9a014e48, reversing
    changes made to 33dc23a9babd0b5b8b415de6e274f14cde3612fc.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,12 +1,4 @@
 // JavaScript - Easy
-function sum(num1, num2) {
-    return num1 + num2;
-}
-
-const num1 = 1;
-const num2 = 2;
-const result = sum(num1, num2);
-console.log(`The sum of ${num1} and ${num2} is ${result}`)
-
 
 // TODO: Write a function that returns the sum of two numbers
+
