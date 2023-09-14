053aa6c8cb69fc481f6d7ebf72cd5b568de413c1
obiwan <109742515+obiwan04kanobi@users.noreply.github.com>
2023-09-14 11:49:54 +0530

    Update Calculator.java
    A simple calculator  in Java that performs basic operations: addition, subtraction, multiplication, and division.

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -5,6 +5,22 @@ package tasks.java.medium;
 public class Calculator {
     // TODO: Implement the Calculator class
 
+    public float add(float x, float y){
+        return x+y;
+    }
+
+    public float subtract(float x, float y){
+        return x-y;
+    }
+
+    public float multiply(float x, float y){
+        return x*y;
+    }
+
+    public double divide(double x, double y){
+        return x/y;
+    }
+
     public static void main(String[] args) {
         //! Dont't change the tests in the main method!
         Calculator calc = new Calculator();
