593704f4964f8ced4c09d17e53d574d5caaace48
GitHub Action <action@github.com>
2023-09-14 06:26:24 +0000

    Revert "Merge pull request #529 from obiwan04kanobi/obiwan04kanobi_#447"
    This reverts commit c80824bcec30c7992fdc25a297d80ad9b9734ec6, reversing
    changes made to 69d1e8e7a2c9a68738e8db05fcbaab9eae889eb7.

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -1,21 +1,3 @@
 # Python - Easy
 
 # TODO: Create the Simple Python Program to Interact with Lists
-
-# creating an empty list
-natural_nums = []
-
-# using loop to add numbers
-for i in range(1,11):
-    natural_nums.append(i)
-    
-print(f"List of Natural Numbers: {natural_nums}")
-
-# initialising sum variable
-sum = 0
-
-# using loop to add numbers from the list
-for i in range(len(natural_nums)):
-    sum += i
-
-print(f"Sum of first 10 Natural Numbers is: {sum}")
