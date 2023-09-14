b63fc28a145b561ff3f8f681bfc35f08b4ae088a
sara <sruthfd@csu.fullerton.edu>
2023-09-14 00:59:07 -0700

    Fix issue 497

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,4 +1,12 @@
 // JavaScript - Easy
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+const num1 = 1;
+const num2 = 2;
+const result = sum(num1, num2);
+console.log(`The sum of ${num1} and ${num2} is ${result}`)
+
 
 // TODO: Write a function that returns the sum of two numbers
-
