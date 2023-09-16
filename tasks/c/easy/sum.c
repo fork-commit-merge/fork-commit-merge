afff3ef8408f967a6c3435dd9991b4b60f46a6b2
GitHub Action <action@github.com>
2023-09-16 05:29:57 +0000

    Revert "Merge pull request #545 from sedna08/sedna-branch"
    This reverts commit b90e7ccf1aad97251a9f2546c590873d4f1e04f3, reversing
    changes made to bfd36b896ed5d63624af73e3ba7f73a05c2bd016.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -2,18 +2,10 @@
 
 #include <stdio.h>
 
-int sum(int a, int b);
-
+// TODO: Implement the sum function
 
 int main() {
-    int num1,num2;
-    printf("Enter two numbers: ");
-    scanf("%d %d",&num1,&num2);
-    printf("The sum is: %d ",sum(num1,num2));
+    // TODO: Implement the main function
 
     return 0;
-}
-
-int sum(int a, int b) {
-    return a + b;
 }
