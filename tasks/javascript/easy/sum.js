04d3c58db6bae807e60155f5780f253bef19a7ce
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2023-09-16 22:52:41 +0100

    feat: sum of numbers

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,4 +1,9 @@
 // JavaScript - Easy
 
 // TODO: Write a function that returns the sum of two numbers
+const sum = (num1, num2) => {
+  const total = num1 + num2;
+  return `The sum of ${num1} and ${num2} is ${total}`;
+};
 
+console.log(sum(2, 3));
