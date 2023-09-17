3bc546d3393a85bdbdb31adc4719050506185730
GitHub Action <action@github.com>
2023-09-17 12:05:53 +0000

    Revert "Merge pull request #576 from Plktz/c++-easy"
    This reverts commit 835fca3fc4434c65b36d719d3ffed73f4434dd78, reversing
    changes made to d6df36438068704dcfc789105b579f2215e9992f.

--- tasks/c++/easy/tempConverter.cpp
+++ tasks/c++/easy/tempConverter.cpp
@@ -1,12 +1,3 @@
 // C++ - Easy
 
 // TODO:  Create a Basic C++ Program to Convert Temperature from Celsius to Fahrenheit
-#include <iostream>
-
-int main(){
-    float celsius, fahrenheit;
-    std::cout << "Enter temperature in Celsius: ";
-    std::cin >> celsius;
-    fahrenheit = (celsius*1.8) + 32;
-    std::cout << "Temperature in Fahrenheit: " << fahrenheit;
-}
