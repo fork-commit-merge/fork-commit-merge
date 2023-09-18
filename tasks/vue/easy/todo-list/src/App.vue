27a7e51205696b9bbff6545a514c1f6d1eedb873
Niko Hoffrén <niko.hoffren@gmail.com>
2023-09-18 11:16:09 +0300

    Add Vue - Easy issue (587)

---
+++ tasks/vue/easy/todo-list/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <TodoList />
+  </div>
+</template>
+
+<script>
+import TodoList from "@/components/TodoList.vue";
+
+export default {
+  components: {
+    TodoList,
+  },
+};
+</script>
