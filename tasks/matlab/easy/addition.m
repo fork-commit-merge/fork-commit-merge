53b06862cd1b9ca87d024db987a9a0e55a8d165f
GitHub Action <action@github.com>
2023-09-18 03:55:47 +0000

    Revert "Merge pull request #585 from Vedant91827/Vedant91827-patch-1"
    This reverts commit 5f290b6c0965ade094df41a5e181294a33ccbc4c, reversing
    changes made to 9d49c3c9eb6fc3c486e47eef6f922e57692434d5.

--- tasks/matlab/easy/addition.m
+++ tasks/matlab/easy/addition.m
@@ -1,10 +1,3 @@
 % MATLAB - Easy
 
-% MATLAB Script that adds two numbers
-
-num1 = input('Enter the first number: ');
-num2 = input('Enter the second number: ');
-
-result = num1 + num2;
-
-fprintf('The sum of %f and %f is: %f\n', num1, num2, result);
+% TODO: Create a MATLAB script that adds two numbers together and prints the result
