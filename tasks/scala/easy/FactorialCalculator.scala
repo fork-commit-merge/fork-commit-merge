3bc4bd403c5e92643dd065b174f0ab89b8cd5c00
GitHub Action <action@github.com>
2023-09-19 08:53:44 +0000

    Revert "Merge pull request #594 from ARYAN105/ARYAN105-patch-1"
    This reverts commit 11f6716cb5d4414a9c76cb71577291a0cb9a78bb, reversing
    changes made to e875ef25d27b0f6195eaa066aadd1cc97e607e64.

--- tasks/scala/easy/FactorialCalculator.scala
+++ tasks/scala/easy/FactorialCalculator.scala
@@ -1,19 +1,11 @@
 // Scala - Easy
-import scala.io.StdIn
+
 object FactorialCalculator {
   def main(args: Array[String]): Unit = {
-    println("Enter a number: ")
-    val input = StdIn.readInt() 
-    val result = factorial(input) 
-    println(s"The factorial of $input is $result")
+    // TODO: Create a simple Scala program that takes a number from the user and prints the factorial of that number
   }
 
   def factorial(n: Int): BigInt = {
-     if (n <= 1) 1 
-    else n * factorial(n - 1) 
+    // TODO: Create factorial function
   }
 }
-
-
-
-
