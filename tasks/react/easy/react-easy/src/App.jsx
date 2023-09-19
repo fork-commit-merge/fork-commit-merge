dfe34fefcaa98a85d879c63312ba8ffce8fe6152
ratishjain <ratishjain6@gmail.com>
2023-09-19 14:01:45 +0530

    added react card component

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -1,14 +1,16 @@
 // React/Vite - Easy
+import Card from "./Card";
 import "./index.css";
 
 function App() {
-    return (
-        <>
-            <div className="center">
-                {/* TODO: Implement the card component */}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="center">
+        {/* TODO: Implement the card component */}
+        <Card />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ tasks/react/easy/react-easy/src/Card.jsx
@@ -0,0 +1,10 @@
+function Card() {
+  return (
+    <div className="card">
+      <img src="https://loremflickr.com/320/240" />
+      <h2>Card Title</h2>
+      <p>Card Description</p>
+    </div>
+  );
+}
+export default Card;
