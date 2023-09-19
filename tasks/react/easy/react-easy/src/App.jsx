98ae77bd80ec524cb143e2812b1b7e9b7ea48654
GitHub Action <action@github.com>
2023-09-19 08:55:30 +0000

    Revert "Merge pull request #596 from ratishjain12/react-card-component"
    This reverts commit 2c359deb3055d3fe730c6313daaa46703d34f50f, reversing
    changes made to e033946d1e465554ccc8565ce074f5b052d5408c.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -1,16 +1,14 @@
 // React/Vite - Easy
-import Card from "./Card";
 import "./index.css";
 
 function App() {
-  return (
-    <>
-      <div className="center">
-        {/* TODO: Implement the card component */}
-        <Card />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="center">
+                {/* TODO: Implement the card component */}
+            </div>
+        </>
+    );
 }
 
 export default App;
