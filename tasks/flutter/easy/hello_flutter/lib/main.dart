d61aabb4c26bc5968192869ec6f4c82a96ab221c
GitHub Action <action@github.com>
2023-09-19 08:56:17 +0000

    Revert "Merge pull request #597 from kaushal1717/easy-flutter"
    This reverts commit 4ded2b87c3a50b985187c826a38663ca2c0f504c, reversing
    changes made to f1e203a71767795bf368babb32fa12d1bda463e9.

--- tasks/flutter/easy/hello_flutter/lib/main.dart
+++ tasks/flutter/easy/hello_flutter/lib/main.dart
@@ -13,11 +13,7 @@ class ForkCommitMerge extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        // TODO: Implement the appbar and body properties
-        appBar: AppBar(
-          title: Text('Fork, Commit, Merge'),
-        ),
-        body: Center(child: Text('Hello, Flutter!')),
+          // TODO: Implement the appbar and body properties
       ),
     );
   }
