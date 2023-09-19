86fadca93af2651e057218fe689ffa25690a16f9
GitHub Action <action@github.com>
2023-09-19 13:33:14 +0000

    Revert "Merge pull request #600 from GasparBonari/main"
    This reverts commit 76a3a00f16fc8b60950229af73f50bc675718863, reversing
    changes made to 17e7b3ba02a10845ba2d84129d16ce04f3fdb2f3.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -2,9 +2,3 @@
 
 // TODO: Write a function that returns the sum of two numbers
 
-function sumOfNumbers(...numbers)
-{
-    return numbers.reduce((acc, e) => acc + e);
-}
-
-console.log(sumOfNumbers(1, 1))
--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -5,12 +5,7 @@ function App() {
     return (
         <>
             <div className="center">
-                <h1>React/Vite - Easy</h1>
-                <div className="card">
-                    <img src="https://loremflickr.com/320/240"></img>
-                </div>
-                <h2>"Card Title"</h2>
-                <p>"Card Description"</p>
+                {/* TODO: Implement the card component */}
             </div>
         </>
     );
