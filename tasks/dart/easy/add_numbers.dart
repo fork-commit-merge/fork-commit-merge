4f0b122d0ddea74a6c0d23a417a07bbefce4cbfd
GitHub Action <action@github.com>
2023-09-20 17:54:21 +0000

    Revert "Merge pull request #609 from sourav091/fix-issue"
    This reverts commit 17e435914a26a544b7633798eb6a4b4888b6a00d, reversing
    changes made to 3329bc7307b4fde9c9c0f72b11616ec00e28f539.

--- tasks/dart/easy/add_numbers.dart
+++ tasks/dart/easy/add_numbers.dart
@@ -1,19 +1,8 @@
 // Dart - Easy
 
-// Define a function addNumbers that takes two integers as parameters
-// and returns their sum.
-int addNumbers(int a, int b) {
-  return a + b;
-}
-
 void main() {
-  // Input two numbers
-  int num1 = 5;
-  int num2 = 10;
-
-  // Calculate the sum by calling the addNumbers function
-  int sum = addNumbers(num1, num2);
-
-  // Print the sum to the console
-  print("The sum of $num1 and $num2 is: $sum");
+  // int sum = addNumbers(5, 10);
+  // print('The sum is: $sum');
 }
+
+// TODO: Implement addNumbers function
