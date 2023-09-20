a57d979759b475b5dd00a6adcdbee720acd0207e
Jessy Baki <bakijessy@gmail.com>
2023-09-20 11:57:43 +0000

    Update - Add h1 element

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -2,13 +2,13 @@
 import "./index.css";
 
 function App() {
-    return (
-        <>
-            <div className="center">
-                {/* TODO: Implement the card component */}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="center">
+        <h1>React/Vite - Easy</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
