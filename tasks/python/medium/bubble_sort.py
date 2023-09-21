8a75631c6451eb443a447b3d321e94b04bbfe0c0
Arya <aishwaryanair430@gmail.com>
2023-09-21 20:17:18 +0530

    My first open-source commit. Solved :)

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -2,6 +2,22 @@
 
 def bubble_sort(arr):
     # TODO: Implement the bubble sort algorithm
+    n = len(arr)
+
+    swapped = False
+    # Traverse through all array elements
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            # Swap if the element found is greater than the next element
+            if arr[j] > arr[j+1]:
+                swapped = True
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+            # If there is no need for swapping any element; all elements are sorted.
+            # We can just exit the main loop.
+            if not swapped:
+                return
+
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
