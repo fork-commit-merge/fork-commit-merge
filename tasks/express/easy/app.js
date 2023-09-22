05589d662e245db210a754640a5726aa6352ef89
Niko Hoffrén <niko.hoffren@gmail.com>
2023-09-22 10:53:43 +0300

    Add ExpressJS to the project with Easy issue number 621

---
+++ tasks/express/easy/app.js
@@ -0,0 +1,10 @@
+// ExpressJS - Easy
+
+const { get } = require("cheerio/lib/api/traversing");
+const express = require("express");
+const app = express();
+const port = 6969;
+
+// TODO: Implement `get` method for / route
+
+// TODO: Implement `listen` method
