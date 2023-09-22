d5f5707f7ab1962810fbe58e74bfe51aa73e6b72
SanjayNithin2002 <sanjaynithin.s2020@vitstudent.ac.in>
2023-09-22 22:20:07 +0530

    issue 621 solved

--- tasks/express/easy/app.js
+++ tasks/express/easy/app.js
@@ -1,9 +1,15 @@
 // ExpressJS - Easy
 
-const { get } = require("cheerio/lib/api/traversing");
 const express = require("express");
 const app = express();
 const port = 6969;
+
+app.get("/", (req, res) => {
+    res.send("Hello, Express!");
+});
+app.listen(port, () => {
+    console.log("Server is running on port 6969");
+})
 
 // TODO: Implement `get` method for / route
 
