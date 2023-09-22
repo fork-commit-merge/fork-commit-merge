1ece272a29cbc250d5d4b647d05e3257b5a8970f
GitHub Action <action@github.com>
2023-09-22 05:12:48 +0000

    Revert "Merge pull request #615 from ivan-rm/fix-issue-#256"
    This reverts commit e44b57d80c1ff5b6e894445ed5fd0dd582507eb1, reversing
    changes made to 6735752173dcb395c5dfb21cfdb05e8cf9bafbc7.

--- tasks/c++/medium/FileHandler.cpp
+++ tasks/c++/medium/FileHandler.cpp
@@ -1,53 +1,14 @@
-// This class is responsible for reading and writing to files using the C++ fstream library
+// C++ - Medium
 
 #include "FileHandler.h"
 #include <fstream>
 #include <sstream>
 
-std::string FileHandler::ReadFile(std::string &filePath)
-{
-    std::ifstream file;
-    file.open (filePath, std::ifstream::in);
-    if (file.is_open()) {
-        std::stringstream buffer;
-        buffer << file.rdbuf();
-        file.close();
-        return buffer.str();
-    }
-    return "";
-};
+// TODO: Implement the FileHandler::ReadFile method
 
-bool FileHandler::WriteFile(std::string &filePath, std::string content)
-{
-    std::ofstream file;
-    file.open (filePath, std::ofstream::out | std::ofstream::trunc);
-    if (file.is_open()) {
-        file << content;
-        file.close();
-        return true;
-    }
-    return false;
-};
+// TODO: Implement the FileHandler::WriteFile method
 
-bool FileHandler::AppendFile(std::string &filePath, std::string content)
-{
-    std::ofstream file;
-    file.open (filePath, std::ofstream::out | std::ofstream::app);
-    if (file.is_open()) {
-        file << content;
-        file.close();
-        return true;
-    }
-    return false;
-};
+// TODO: Implement the FileHandler::AppendFile method
 
-bool FileHandler::FileExists(std::string &filePath)
-{
-    std::ifstream file;
-    file.open (filePath, std::ifstream::in);
-    if (file.is_open()) {
-        file.close();
-        return true;
-    }
-    return false;
-};
+// TODO: Implement the FileHandler::FileExists method
+
