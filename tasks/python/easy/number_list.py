d0d3012488ec5ec3fd6d537b91c889177d8914c8
alpakaka0o0 <alpakaka000808@gmail.com>
2023-09-22 22:38:41 +0900

    fix #447

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -1,3 +1,10 @@
 # Python - Easy
 
 # TODO: Create the Simple Python Program to Interact with Lists
+num_lst = [0] * 10
+sum = 0
+for i in range(1, 11):
+    num_lst[i-1] = i
+    sum += i
+print(num_lst)
+print(sum)
