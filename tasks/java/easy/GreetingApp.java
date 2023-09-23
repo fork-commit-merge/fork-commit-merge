159c98fecce904cdc0488225d88c791a2613c4e7
GitHub Action <action@github.com>
2023-09-23 07:18:59 +0000

    Revert "Merge pull request #633 from OrganicBeetle/fix-issue"
    This reverts commit b126f0e80529c74764acbe13dfcd0b951f7cd147, reversing
    changes made to 8860bc4294fc2a514d344112eeb4c500b2dc28b1.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,11 +1,8 @@
-import java.util.Scanner;
+// Java - Easy
 
 public class GreetingApp {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name:");
-        String str = sc.nextLine();
-        System.out.println("Hello, " + str + "!" + " Nice to meet you!");
+        // TODO: Implement a Simple Greeting Application
     }
 }
