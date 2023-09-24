ec3ff7c7fd4664e78948d14e491353f0a09729ad
GitHub Action <action@github.com>
2023-09-24 06:09:24 +0000

    Revert "Merge pull request #661 from robertlent/python-medium-task"
    This reverts commit c6a6bfcd82d3ee6674dab1ffec685429bc721785, reversing
    changes made to dc26b24e8d4fa3d154abaa682c521820bf443947.

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,13 +1,7 @@
 # Python - Medium
 
 def bubble_sort(arr):
-    for i in range(len(arr) - 1):
-        for j in range(len(arr) - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-
-    return arr
-
+    # TODO: Implement the bubble sort algorithm
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
