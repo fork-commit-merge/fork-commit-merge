082321ce89334eabf1ca7c482aefb7ff60b48865
GitHub Action <action@github.com>
2023-09-24 12:01:00 +0000

    Revert "Merge pull request #673 from kani-29/main"
    This reverts commit 8c9e02c333392f3386ccd5448ef67a83280f0a80, reversing
    changes made to acfc69e34f7a439f6bef2ed2c3a80d046a5efb8f.

--- tasks/flask/easy/app.py
+++ tasks/flask/easy/app.py
@@ -5,9 +5,3 @@ from flask import Flask
 app = Flask(__name__)
 
 # TODO: Implement rest of the code here
-@app.route("/")
-def hello_world():
-  return "hello world"
-
-if __name__=='__main__':
-  app.run()
