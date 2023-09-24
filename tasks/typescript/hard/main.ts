acfc69e34f7a439f6bef2ed2c3a80d046a5efb8f
GitHub Action <action@github.com>
2023-09-24 11:59:30 +0000

    Revert "Merge pull request #672 from jeremyhuang035/typescript-hard-issue"
    This reverts commit 7b6026d127e8d845f944084982ba47f4d52183b1, reversing
    changes made to 94534af7f0af73fd098a0908a63ca4d1719000ae.

--- tasks/typescript/hard/main.ts
+++ tasks/typescript/hard/main.ts
@@ -1,87 +1,42 @@
 // TypeScript - Hard
 
 interface Product {
-  id: number;
-  title: string;
-  price: number;
-  rating: number;
+    id: number;
+    title: string;
+    price: number;
+    rating: number;
 }
 
 let products: Product[] = [];
 
 async function fetchData() {
-  // TODO: Implement the fetch function
-  try {
-    const response = await fetch('https://dummyjson.com/products');
-    if (!response.ok) {
-      throw new Error('Failed to fetch products');
-    }
-
-    const data = await response.json();
-    products = data.products;
+    // TODO: Implement the fetch function
     displayProducts(products);
-  } catch (error) {
-    console.error(error);
-  }
 }
 
 function displayProducts(products: Product[]) {
-  // TODO: Implement the display function
-  const productBody = document.getElementById('productBody');
-
-  if (productBody) {
-    productBody.textContent = '';
-  }
-
-  products.forEach((product) => {
-    const productEl = document.createElement('tr');
-
-    const titleEl = document.createElement('td');
-    titleEl.textContent = product.title;
-
-    const priceEl = document.createElement('td');
-    priceEl.textContent = `${product.price}`;
-
-    const ratingEl = document.createElement('td');
-    ratingEl.textContent = `${product.rating}`;
-
-    productEl.appendChild(titleEl);
-    productEl.appendChild(priceEl);
-    productEl.appendChild(ratingEl);
-
-    productBody?.appendChild(productEl);
-  });
+    // TODO: Implement the display function
 }
 
 function applyFilters() {
-  const minPrice = parseFloat(
-    (document.getElementById('minPrice') as HTMLInputElement).value,
-  );
-  const maxPrice = parseFloat(
-    (document.getElementById('maxPrice') as HTMLInputElement).value,
-  );
-  const minRating = parseFloat(
-    (document.getElementById('minRating') as HTMLInputElement).value,
-  );
-  const maxRating = parseFloat(
-    (document.getElementById('maxRating') as HTMLInputElement).value,
-  );
+    const minPrice = parseFloat(
+        (document.getElementById("minPrice") as HTMLInputElement).value
+    );
+    const maxPrice = parseFloat(
+        (document.getElementById("maxPrice") as HTMLInputElement).value
+    );
+    const minRating = parseFloat(
+        (document.getElementById("minRating") as HTMLInputElement).value
+    );
+    const maxRating = parseFloat(
+        (document.getElementById("maxRating") as HTMLInputElement).value
+    );
 
-  const filteredProducts = products.filter(
-    // TODO: Implement the filter function
-    (product) => {
-      const filteredProductsByPrice =
-        (isNaN(minPrice) || product.price >= minPrice) &&
-        (isNaN(maxPrice) || product.price <= maxPrice);
-      const filteredProductsByRating =
-        (isNaN(minRating) || product.rating >= minRating) &&
-        (isNaN(maxRating) || product.rating <= maxRating);
+    const filteredProducts = products.filter(
+        // TODO: Implement the filter function
+    );
 
-      return filteredProductsByPrice && filteredProductsByRating;
-    },
-  );
-
-  displayProducts(filteredProducts);
+    displayProducts(filteredProducts);
 }
 
 fetchData();
