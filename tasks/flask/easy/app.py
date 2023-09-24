44e8971676113285b8ab5fd2caa2c8aed2a5df23
kani-29 <92940669+kani-29@users.noreply.github.com>
2023-09-24 16:39:02 +0530

    Update app.py
    i have corrected flask app python code

--- tasks/flask/easy/app.py
+++ tasks/flask/easy/app.py
@@ -5,3 +5,9 @@ from flask import Flask
 app = Flask(__name__)
 
 # TODO: Implement rest of the code here
+@app.route("/")
+def hello_world():
+  return "hello world"
+
+if __name__=='__main__':
+  app.run()
