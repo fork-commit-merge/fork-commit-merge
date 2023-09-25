a8b1a6466c364b2f633ee1845ee1f5480f57ccab
GitHub Action <action@github.com>
2023-09-25 06:02:25 +0000

    Revert "Merge pull request #676 from yaralviana/main"
    This reverts commit f38b4f8ae80327f764b0df3c3d6ea96bccc08369, reversing
    changes made to 5be6ba64dc312bef4cacc36cc3a435d8bef3984e.

--- tasks/vitest/easy/src/sum.test.ts
+++ tasks/vitest/easy/src/sum.test.ts
@@ -4,8 +4,3 @@ import sum from "./sum";
 import { describe, expect, it } from "vitest";
 
 // TODO: Create the test required in the task
-describe('sum function test', () => {
-    it('should return 0 when no numbers are provided', () => {
-      expect(sum()).toBe(0);
-    });
-  });
