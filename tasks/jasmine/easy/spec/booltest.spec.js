3a74154d44a38e0381b58e082e01144591a6ed08
Oscar Vaquera <vaqueraoscar0@gmail.com>
2023-09-25 12:51:28 -0700

    jasmine test case create

--- tasks/jasmine/easy/spec/booltest.spec.js
+++ tasks/jasmine/easy/spec/booltest.spec.js
@@ -3,5 +3,6 @@
 describe("Jasmine - Easy - Boolean Test", function () {
     it("should pass this test", function () {
         // TODO: Add `expect` and `toBe` statements to pass this test
+        expect(true).toBe(true);
     });
 });
