dc3fe664041b027d879516110fcaddec0575630a
Bhavya <110487270+bhaavvya@users.noreply.github.com>
2023-09-25 19:38:39 +0530

    Update read_csv.c

--- tasks/c/medium/read_csv.c
+++ tasks/c/medium/read_csv.c
@@ -6,5 +6,24 @@
 
 int main() {
     // TODO: Implement the program that reads and displays the contents of data.csv file
+    char filename="data.csv";
+    FILE *file=fopen(filename,"r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return;
+    }
+    char line[1024];
+    while (fgets(line, sizeof(line), file)) {
+        // Tokenize the line using a comma as the delimiter
+        char *token = strtok(line, ",");
+        while (token != NULL) {
+            // Print each token (field) with appropriate formatting
+            printf("%-15s", token); // Adjust the field width as needed
+            token = strtok(NULL, ",");
+        }
+        printf("\n"); // Newline after processing one line
+    }
+
+    fclose(file);
     return 0;
 }
