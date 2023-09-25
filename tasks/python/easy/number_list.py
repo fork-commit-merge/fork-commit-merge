312b60f13ea10781c7c0e7699bcc6debf8546a09
GitHub Action <action@github.com>
2023-09-25 06:03:27 +0000

    Revert "Merge pull request #677 from karthikchalla7/python-easy"
    This reverts commit 7c4e49ff24b8df50b6bf06cba183b089570fbfa4, reversing
    changes made to 3c80a42e9572d964ad0b9688d1aae664daa2616c.

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -1,7 +1,3 @@
 # Python - Easy
 
 # TODO: Create the Simple Python Program to Interact with Lists
-
-list = [i for i in range(1,11)]
-print(f"The first 10 numbers of the list are",list)
-print(f"The sum of first 10 numbers of the list",sum(list))
