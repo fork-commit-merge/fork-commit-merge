93fb61df5b6f26ee3201709f8b9e2a8d2e870ddc
Bhavya <110487270+bhaavvya@users.noreply.github.com>
2023-09-25 19:18:06 +0530

    Update App.jsx

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -6,6 +6,12 @@ function App() {
         <>
             <div className="center">
                 {/* TODO: Implement the card component */}
+                <h1>"React/Vite - Easy</h1>
+                <div class="card">
+                <img src="https://loremflickr.com/cache/resized/65535_52422330809_c86d4f09f3_320_240_nofilter.jpg" alt="image" />
+                    <h2>Card Title</h2>
+                    <p>Card Description</p>
+                </div>
             </div>
         </>
     );
