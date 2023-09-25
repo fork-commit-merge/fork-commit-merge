a19245a69e5a7bf74eb0947eea8fa33e816965db
Niko Hoffrén <niko.hoffren@gmail.com>
2023-09-25 11:12:52 +0300

    Add Electron.js to the project with Easy level issue (679)

---
+++ tasks/electron/easy/main.js
@@ -0,0 +1,9 @@
+// Electron - Easy
+
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+    // TODO: Create a new BrowserWindow that displays the index.html file
+}
+
+app.on("ready", createWindow);
