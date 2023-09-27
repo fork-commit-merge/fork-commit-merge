98aaf8a9cf7cdcf538edbb3884e4f40af76bd2b7
Niko Hoffrén <niko.hoffren@gmail.com>
2023-09-27 12:51:24 +0300

    Add Pascal - Medium issue (705)

---
+++ tasks/pascal/medium/AverageCalculator.pas
@@ -0,0 +1,3 @@
+// Pascal - Medium
+
+// TODO: Create a Pascal program that reads a sequence of numbers from the user until a negative number is entered. Then, calculate and display the average of all the positive numbers entered.
