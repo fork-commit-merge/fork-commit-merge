5248de5fc6314722216f05332ce25393ff5151f0
vivekBoii <yadav100adu@gmail.com>
2023-09-27 16:10:56 +0530

    Added Countdown Timer

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,3 +1,19 @@
 // JavaScript - Medium
 
 // TODO: Create functionality for a countdown timer that counts down from 10 seconds to zero
+
+const timer=document.getElementById("timer");
+let time=10;
+
+function updateTime(){
+    time=time-1;
+    if(time){
+        timer.innerHTML=`00:${time}`;
+    }
+    else{
+        timer.innerHTML="Time's up!";
+        clearInterval(go);
+    }
+}
+
+let go=setInterval(updateTime,1000);
