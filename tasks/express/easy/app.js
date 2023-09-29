c52a9738802d71527fb67e4fb277e172936f4560
GitHub Action <action@github.com>
2023-09-29 14:15:33 +0000

    Revert "Merge pull request #723 from sanjay14073/patch-7"
    This reverts commit 326a5bc2ebc5871c70bbeb6e74a13bca0ffee6af, reversing
    changes made to cedc0984d1e8b945ab6bfbd9afd8cca2560efe66.

--- tasks/express/easy/app.js
+++ tasks/express/easy/app.js
@@ -6,10 +6,5 @@ const app = express();
 const port = 6969;
 
 // TODO: Implement `get` method for / route
-app.get('/',(req,res,next)=>{
-  res.send("Hello, Express!");
-})
-app.listen(port,(req,res)=>{
-  console.log("Server is running on port 6969");
-});
+
 // TODO: Implement `listen` method
