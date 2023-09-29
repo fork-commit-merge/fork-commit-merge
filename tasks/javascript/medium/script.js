30e06b876bc43d757f28dee51bede9498062d1c9
GitHub Action <action@github.com>
2023-09-29 16:13:01 +0000

    Revert "Merge pull request #728 from vidhan63-soe/main"
    This reverts commit 10d4fce753d1be7bbef81232396c00947438b90e, reversing
    changes made to d415b0f867273171a551f3ab5312c53299639204.

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,24 +1,3 @@
-// Get the timer element
-const timerElement = document.getElementById('timer');
+// JavaScript - Medium
 
-// Set the time for the countdown (in seconds)
-let timeInSeconds = 10;
-
-// Update the timer every second
-const timerInterval = setInterval(() => {
-    // Check if the time has run out
-    if (timeInSeconds <= 0) {
-        clearInterval(timerInterval);  // Stop the timer
-        timerElement.innerHTML = 'Time`s Up !';  // Display 
-    } else {
-        // Format the time to display in Task format
-        const minutes = Math.floor(timeInSeconds / 60).toString().padStart(2, '0');
-        const seconds = (timeInSeconds % 60).toString().padStart(2, '0');
-
-        // Update the timer element
-        timerElement.innerHTML = `${minutes}:${seconds}`;
-
-        
-        timeInSeconds--;
-    }
-}, 1000);  // Update every 1 second (1000 milliseconds)
+// TODO: Create functionality for a countdown timer that counts down from 10 seconds to zero
