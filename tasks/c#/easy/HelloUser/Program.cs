8e35b1dbeb42a75e3f93e79eae36d744f352a508
BanulaKumarage <banula.19@cse.mrt.ac.lk>
2023-09-30 13:34:21 +0530

    Addressing #577

--- tasks/c#/easy/HelloUser/Program.cs
+++ tasks/c#/easy/HelloUser/Program.cs
@@ -1,3 +1,16 @@
 ﻿// C# - Easy
 
 // TODO: Implement the C# program requested in the Issue description
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter your name:");
+        string name = Console.ReadLine();
+
+        Console.WriteLine($"Hello, {name}!");
+    }
+}
