199f726f178a8f07c1132c0adf2191ca72738af9
sagnik3788 <116512372+sagnik3788@users.noreply.github.com>
2023-10-01 01:15:06 +0530

    update Ts file

--- tasks/typescript/easy/index.ts
+++ tasks/typescript/easy/index.ts
@@ -2,8 +2,9 @@
 
 function greet(name: string): string {
     // TODO: Add return statement
+    return `Hello, Contributors!`;
 }
 
 let user: string = 'John Doe';
 
-console.log(greet(user));
+console.log(greet(user));// Output: Hello, Contributors!
