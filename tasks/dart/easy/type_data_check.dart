81ce293f4cda463f761655a353cc173c3953615a
GitHub Action <action@github.com>
2023-10-01 11:51:50 +0000

    Revert "Merge pull request #796 from VoidVampire/dart-easy-typefunction"
    This reverts commit 4950cbc35dc60416b99aaba2fcf89137ca6fe247, reversing
    changes made to ad19ead12fff07f9ec9fedb2c061ec7e80562228.

--- tasks/dart/easy/type_data_check.dart
+++ tasks/dart/easy/type_data_check.dart
@@ -1,25 +1,9 @@
-String typeDataCheck(dynamic value) {
-  if (value == null) {
-    return "Null";
-  } else if (value is int) {
-    return "Integer";
-  } else if (value is double) {
-    return "Double";
-  } else if (value is String) {
-    return "String";
-  } else if (value is bool) {
-    return "Boolean";
-  } else {
-    return "Unknown";
-  }
-}
-
 void main() {
-  print(typeDataCheck(5));         // Output: Integer
-  print(typeDataCheck(3.14));      // Output: Double
-  print(typeDataCheck("Hello"));   // Output: String
-  print(typeDataCheck(true));      // Output: Boolean
-  print(typeDataCheck(null));      // Output: Null
-  print(typeDataCheck([]));        // Output: Unknown
+  // print(typeDataCheck(5));
+  // print(typeDataCheck(3.14));
+  // print(typeDataCheck("Hello"));
+  // print(typeDataCheck(true));
+  // print(typeDataCheck(null));
 }
 
+// TODO: Implement a function that accepts a parameter of any type and returns a string describing its data type.
