5d3b724cb439a618045ad364c34b4bfdce4f3e06
chiku <chintukumar0460@gmail.com>
2023-10-01 16:55:37 +0530

    created functiion

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -3,9 +3,17 @@
 #include <stdio.h>
 
 // TODO: Implement the sum function
+int sum(int a,int b){
+    return a+b;
+
+}
 
 int main() {
     // TODO: Implement the main function
-
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    int res=sum(a,b);
+    printf(res);
     return 0;
 }
