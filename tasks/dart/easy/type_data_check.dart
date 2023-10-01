0ca6410990df0b2bd2041ba0a0579efba82172de
GitHub Action <action@github.com>
2023-10-01 15:28:58 +0000

    Revert "Merge pull request #809 from abhinaba-dash/patch-1"
    This reverts commit 77534666bbf55dcd1a5c00e94372776ae05f7882, reversing
    changes made to 0ace98f61f908c1d87d66af51ac33cd1e50dd345.

--- tasks/dart/easy/type_data_check.dart
+++ tasks/dart/easy/type_data_check.dart
@@ -1,13 +1,9 @@
-// TODO: Implement a function that accepts a parameter of any type and returns a string describing its data type.
-
-String typeDataCheck(dynamic data) {
-  return data.runtimeType.toString();
-}
-
 void main() {
-  print(typeDataCheck(5));
-  print(typeDataCheck(3.14));
-  print(typeDataCheck("Hello"));
-  print(typeDataCheck(true));
-  print(typeDataCheck(null));
+  // print(typeDataCheck(5));
+  // print(typeDataCheck(3.14));
+  // print(typeDataCheck("Hello"));
+  // print(typeDataCheck(true));
+  // print(typeDataCheck(null));
 }
+
+// TODO: Implement a function that accepts a parameter of any type and returns a string describing its data type.
