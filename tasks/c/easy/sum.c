b3c0a24a41dd34c0673d60d98bad6e3fd89c4337
Suprabhat Das <98036987+suprabhatdas@users.noreply.github.com>
2023-10-01 10:52:23 +0530

    Update sum.c

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,11 +1,14 @@
-// C - Easy
-
 #include <stdio.h>
 
-// TODO: Implement the sum function
-
 int main() {
-    // TODO: Implement the main function
+  int num1, num2, sum;
 
-    return 0;
+  printf("Enter two numbers: ");
+  scanf("%d %d", &num1, &num2);
+
+  sum = num1 + num2;
+
+  printf("The sum of %d and %d is %d\n", num1, num2, sum);
+
+  return 0;
 }
