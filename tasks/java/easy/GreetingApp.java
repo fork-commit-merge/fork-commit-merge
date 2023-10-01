6633552725069a13b70958fa891d584b0527e82a
GitHub Action <action@github.com>
2023-10-01 13:33:22 +0000

    Revert "Merge pull request #805 from shrey141102/main"
    This reverts commit f79a3bd7b9f151402b39b64308edb14d29943e92, reversing
    changes made to 78f705fce0070fe8b95460828f2999655300938f.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,18 +1,8 @@
-//Java - easy
-import java.util.Scanner;
+// Java - Easy
 
 public class GreetingApp {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-        scanner.close();
-
-        String greeting = generateGreeting(name);
-        System.out.println(greeting);
-    }
-
-    public static String generateGreeting(String name) {
-        return "Hello, " + name + "! Welcome to the Greeting Application!";
+        // TODO: Implement a Simple Greeting Application
     }
 }
