a4efb42ce5054ee3de4e6836a01307040236eba4
KAMBOJI-AKHILESH <kambojiakhilesh@gmail.com>
2023-10-01 16:59:22 +0530

    solved-calculator

--- tasks/typescript/medium/calculator.ts
+++ tasks/typescript/medium/calculator.ts
@@ -1,12 +1,28 @@
 // TypeScript - Medium
 
 namespace Calculator {
-    // TODO: Implement a Simple TypeScript Calculator
+  // TODO: Implement a Simple TypeScript Calculator
+  export function add(a: number, b: number) {
+    return a + b;
+  }
+  export function subtract(a: number, b: number) {
+    return a - b;
+  }
+  export function multiply(a: number, b: number) {
+    return a * b;
+  }
+  export function divide(a: number, b: number) {
+    if (a == 0 || b == 0) {
+      console.error("zero division error");
+      return null;
+    }
+    return a / b;
+  }
 }
 
 // Test cases
-console.log(Calculator.add(5, 3));       // Expected: 8
-console.log(Calculator.subtract(5, 3));  // Expected: 2
-console.log(Calculator.multiply(5, 3));  // Expected: 15
-console.log(Calculator.divide(5, 3));    // Expected: 1.666...
-console.log(Calculator.divide(5, 0));    // Expected: Warning message & null
+console.log(Calculator.add(5, 3)); // Expected: 8
+console.log(Calculator.subtract(5, 3)); // Expected: 2
+console.log(Calculator.multiply(5, 3)); // Expected: 15
+console.log(Calculator.divide(5, 3)); // Expected: 1.666...
+console.log(Calculator.divide(5, 0)); // Expected: Warning message & null
