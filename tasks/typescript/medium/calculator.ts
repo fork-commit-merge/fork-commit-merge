e467043d65c7033e0b9ba7faed8a107d183834da
GitHub Action <action@github.com>
2023-10-01 08:51:27 +0000

    Revert "Merge pull request #776 from Hekt000r/patch-2"
    This reverts commit 8bfce2d6419163ce95d7b9a8056748ef941e0e48, reversing
    changes made to 8c2f1d5ce6abbf6571c0622fac977ed8af491ca8.

--- tasks/typescript/medium/calculator.ts
+++ tasks/typescript/medium/calculator.ts
@@ -1,26 +1,9 @@
 // TypeScript - Medium
-// Takes input of 2 numbers and operates on them
+
 namespace Calculator {
-    export function add(number1: number, number2: number) {
-        return number1 + number2;
-    }
-    export function subtract(number1: number, number2: number) {
-        // Notice: if subtracted by lower number, will return a negative number
-        return number1 - number2;
-    }
-    export function multiply(number1: number, number2: number) {
-        return number1 * number2;
-    }
-    export function divide(number1: number, number2: number) {
-        if (number1 == 0 || number2 == 0) {
-            console.error("Attempted to divide by 0")
-            return null;
-        }
-        else{
-            return number1 / number2;
-        }
-    }
+    // TODO: Implement a Simple TypeScript Calculator
 }
+
 // Test cases
 console.log(Calculator.add(5, 3));       // Expected: 8
 console.log(Calculator.subtract(5, 3));  // Expected: 2
