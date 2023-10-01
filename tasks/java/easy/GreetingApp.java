75ca7f50483fa79cc83ad75555db348d65b2e28b
KAMBOJI-AKHILESH <kambojiakhilesh@gmail.com>
2023-10-01 17:19:10 +0530

    solved-java-easy

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,8 +1,14 @@
 // Java - Easy
 
+import java.util.Scanner;
+
 public class GreetingApp {
 
     public static void main(String[] args) {
         // TODO: Implement a Simple Greeting Application
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello "+name+", nice to meet you");
+        sc.close();
     }
 }
