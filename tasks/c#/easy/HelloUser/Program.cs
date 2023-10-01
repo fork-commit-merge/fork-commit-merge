77a5a088681635dfd18b00d10e34bee1e831e278
GitHub Action <action@github.com>
2023-10-01 04:31:28 +0000

    Revert "Merge pull request #744 from BanulaKumarage/Addressing-577"
    This reverts commit 83b9b5fae8431a5bfb5e02572063b24c0256390d, reversing
    changes made to a431b0af1d7488798b870108c994c10c43dba8e9.

--- tasks/c#/easy/HelloUser/Program.cs
+++ tasks/c#/easy/HelloUser/Program.cs
@@ -1,16 +1,3 @@
 ﻿// C# - Easy
 
 // TODO: Implement the C# program requested in the Issue description
-
-using System;
-
-class Program
-{
-    static void Main()
-    {
-        Console.WriteLine("Please enter your name:");
-        string name = Console.ReadLine();
-
-        Console.WriteLine($"Hello, {name}!");
-    }
-}
