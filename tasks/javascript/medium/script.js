97848e15bf49cd201b0614a3084d45e9031c0f5b
GitHub Action <action@github.com>
2023-10-01 04:49:50 +0000

    Revert "Merge pull request #752 from anuraggopalakrishna/js-medium-submission"
    This reverts commit f9d9366b783d54d114b415003ddd54b3f5e745e0, reversing
    changes made to f2120ef291a753f29560254d7b1a0f296356cb09.

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,20 +1,3 @@
 // JavaScript - Medium
 
 // TODO: Create functionality for a countdown timer that counts down from 10 seconds to zero
-
-document.addEventListener("DOMContentLoaded", function () {
-    const timer = document.getElementById("timer");
-
-    function updateTimer() {
-        const seconds = timer.textContent.slice(-2);
-        if (seconds > 0) {
-            timer.textContent = `00:${(seconds - 1).toString().padStart(2, "0")}`;
-        } else {
-            clearInterval(countdownInterval);
-            timer.textContent = "Time's up!";
-        }
-    }
-
-    let countdownInterval = setInterval(updateTimer, 1000);
-    sleep(1000).then(() => updateTimer());
-});
