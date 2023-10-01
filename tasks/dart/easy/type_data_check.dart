2a3d2eef3209cbb366d60a6887915c3e7d224042
micielkirsten <micielkirsten@gmail.com>
2023-10-01 13:10:40 -0400

    completed typeDataCheck function

--- tasks/dart/easy/type_data_check.dart
+++ tasks/dart/easy/type_data_check.dart
@@ -1,9 +1,11 @@
 void main() {
-  // print(typeDataCheck(5));
-  // print(typeDataCheck(3.14));
-  // print(typeDataCheck("Hello"));
-  // print(typeDataCheck(true));
-  // print(typeDataCheck(null));
+  print(typeDataCheck(5));
+  print(typeDataCheck(3.14));
+  print(typeDataCheck("Hello"));
+  print(typeDataCheck(true));
+  print(typeDataCheck(null));
 }
 
-// TODO: Implement a function that accepts a parameter of any type and returns a string describing its data type.
+String typeDataCheck(dynamic data) {
+  return data.runtimeType.toString();
+}
