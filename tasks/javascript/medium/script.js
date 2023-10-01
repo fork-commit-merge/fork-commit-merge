5fb4863422a2e4e761722f8a1dd6db244d86d10f
soumik_ghosh <soumikghosh5225@gmail.com>
2023-10-01 15:02:21 +0530

    Stopwatch using Javascript

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,3 +1,11 @@
 // JavaScript - Medium
 
 // TODO: Create functionality for a countdown timer that counts down from 10 seconds to zero
+
+var timer = document.getElementById("timer");
+var time = 10;
+setInterval(() => {
+  time--;
+  if (time > 0) timer.innerHTML = "00:" + time;
+  else timer.innerHTML = "Time's up!";
+}, 1000);
