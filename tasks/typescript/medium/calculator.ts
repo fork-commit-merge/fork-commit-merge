2e02d3aa080475e9eaae38c62b98fc83244e9bca
ratishjain <ratishjain6@gmail.com>
2023-10-01 18:19:03 +0530

    calculator using typescript:Hacktoberfest

--- tasks/typescript/medium/calculator.ts
+++ tasks/typescript/medium/calculator.ts
@@ -1,12 +1,24 @@
 // TypeScript - Medium
 
 namespace Calculator {
-    // TODO: Implement a Simple TypeScript Calculator
+  // TODO: Implement a Simple TypeScript Calculator
+  export function add(a: number, b: number): number {
+    return a + b;
+  }
+  export function multiply(a: number, b: number): number {
+    return a * b;
+  }
+  export function subtract(a: number, b: number): number {
+    return a - b;
+  }
+  export function divide(a: number, b: number): number {
+    return a / b;
+  }
 }
 
 // Test cases
-console.log(Calculator.add(5, 3));       // Expected: 8
-console.log(Calculator.subtract(5, 3));  // Expected: 2
-console.log(Calculator.multiply(5, 3));  // Expected: 15
-console.log(Calculator.divide(5, 3));    // Expected: 1.666...
-console.log(Calculator.divide(5, 0));    // Expected: Warning message & null
+console.log(Calculator.add(5, 3)); // Expected: 8
+console.log(Calculator.subtract(5, 3)); // Expected: 2
+console.log(Calculator.multiply(5, 3)); // Expected: 15
+console.log(Calculator.divide(5, 3)); // Expected: 1.666...
+console.log(Calculator.divide(5, 0)); // Expected: Warning message & null
