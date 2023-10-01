c0b6e0cec3e656f5afeb348541e1603d425cd812
GitHub Action <action@github.com>
2023-10-01 18:36:08 +0000

    Revert "Merge pull request #820 from Jhanvi-Singhal/patch-1"
    This reverts commit d9482ba5011a8e2a7e412ecf1f75f65808db790c, reversing
    changes made to f9f151c996a04732f969b237ba75732eb760f800.

--- tasks/c++/easy/tempConverter.cpp
+++ tasks/c++/easy/tempConverter.cpp
@@ -1,9 +1,3 @@
 // C++ - Easy
 
 // TODO:  Create a Basic C++ Program to Convert Temperature from Celsius to Fahrenheit
-float cel;
-cin>>cel;
-  float far;
-far=(1.8*cel)+32;
-cout<< far;
-return 0;
