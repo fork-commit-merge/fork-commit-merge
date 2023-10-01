3b3b10d757a9600eaf0e209da5dd38bb229ecf0f
GitHub Action <action@github.com>
2023-10-01 18:53:58 +0000

    Revert "Merge pull request #822 from 29deepanshutyagi/main"
    This reverts commit 38986cebb4b0cd52ad8ec826e328f7cd73ac6f24, reversing
    changes made to 72c6a966967b88433f1ffcc15dd1d45138984a39.

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,29 +1,3 @@
+// JavaScript - Medium
 
-const timerElement = document.getElementById('timer');
-
-
-let countdownTime = 10;
-
-
-function startCountdown() {
-  
-  const interval = setInterval(() => {
-    
-    countdownTime--;
-
-    
-    timerElement.textContent = countdownTime;
-
-  
-    if (countdownTime === 0) {
-    
-      clearInterval(interval);
-
-   
-      timerElement.textContent = 'Time\'s up!';
-    }
-  }, 1000);
-}
-
-
-startCountdown();
+// TODO: Create functionality for a countdown timer that counts down from 10 seconds to zero
