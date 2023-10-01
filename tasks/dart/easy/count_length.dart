5b50c2822b2c1bcf5052a50b6908c29eb8b4e78a
GitHub Action <action@github.com>
2023-10-01 09:25:44 +0000

    Revert "Merge pull request #783 from mokshanirugutti/fix/Dart-Code-to-count-lenght"
    This reverts commit 22c24439e2c7b6d867a9a8aeee51a90accaf1c00, reversing
    changes made to 0afd307b97a81f8454da385517bd2cb785cf7e38.

--- tasks/dart/easy/count_length.dart
+++ tasks/dart/easy/count_length.dart
@@ -1,14 +1,7 @@
 void main() {
-  String text = "Example text";
-  int lengthData = countLength(text);
-  print('Length: $lengthData');
+  // String text = "Example text";
+  // int lengthData = countLength(text);
+  // print('Length: $lengthData');
 }
 
 // TODO: Create a function for counting the length of a string
-int countLength(String text) {
-  int count = 0;
-  for (int i = 0; i < text.runes.length; i++) {
-    count++;
-  }
-  return count;
-}
