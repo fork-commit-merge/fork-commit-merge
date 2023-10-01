1d61f0abe9d0c64a15cd089d7e37445ac8b383fc
mokshanirgutti <mokshasai910@gmail.com>
2023-10-01 09:13:47 +0530

    "fix : Implement a Simple File Reader/Writer Class and Methods in C++"

--- tasks/c++/medium/FileHandler.cpp
+++ tasks/c++/medium/FileHandler.cpp
@@ -3,12 +3,55 @@
 #include "FileHandler.h"
 #include <fstream>
 #include <sstream>
-
+#include <iostream>
 // TODO: Implement the FileHandler::ReadFile method
 
-// TODO: Implement the FileHandler::WriteFile method
 
+
+
+
+
+
+std::string FileHandler::ReadFile(const std::string& filePath) {
+    std::ifstream inputFile(filePath);
+    if (!inputFile.is_open()) {
+        std::cerr << "Error: Could not open file for reading: " << filePath << std::endl;
+        return "";
+    }
+
+    std::stringstream fileContent;
+    fileContent << inputFile.rdbuf();
+    inputFile.close();
+
+    return fileContent.str();
+}
+// TODO: Implement the FileHandler::WriteFile method
+bool FileHandler::WriteFile(const std::string& filePath, const std::string& content) {
+    std::ofstream outputFile(filePath);
+    if (!outputFile.is_open()) {
+        std::cerr << "Error: Could not open file for writing: " << filePath << std::endl;
+        return false;
+    }
+
+    outputFile << content;
+    outputFile.close();
+    return true;
+}
 // TODO: Implement the FileHandler::AppendFile method
+bool FileHandler::AppendFile(const std::string& filePath, const std::string& content) {
+    std::ofstream outputFile(filePath, std::ios::app);
+    if (!outputFile.is_open()) {
+        std::cerr << "Error: Could not open file for appending: " << filePath << std::endl;
+        return false;
+    }
+
+    outputFile << content;
+    outputFile.close();
+    return true;
+}
 
 // TODO: Implement the FileHandler::FileExists method
-
+bool FileHandler::FileExists(const std::string& filePath) {
+    std::ifstream file(filePath);
+    return file.good();
+}
--- tasks/c++/medium/FileHandler.h
+++ tasks/c++/medium/FileHandler.h
@@ -5,5 +5,8 @@
 
 class FileHandler {
 public:
-    // TODO: Implement the FileHandler class
+     static std::string ReadFile(const std::string& filePath);
+    static bool WriteFile(const std::string& filePath, const std::string& content);
+    static bool AppendFile(const std::string& filePath, const std::string& content);
+    static bool FileExists(const std::string& filePath);
 };
