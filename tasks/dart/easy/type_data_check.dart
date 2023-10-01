9a012bf49176ad5190904f2a18f56525011fecd5
GitHub Action <action@github.com>
2023-10-01 19:18:01 +0000

    Revert "Merge pull request #825 from manelk/fix774"
    This reverts commit a95b723e8fab282a9d1a2300de79d5f6ea97f12a, reversing
    changes made to ea5685fafbfff0b81af46a98fe62373c00069608.

--- tasks/dart/easy/type_data_check.dart
+++ tasks/dart/easy/type_data_check.dart
@@ -1,27 +1,9 @@
 void main() {
-  print(typeDataCheck(5));
-  print(typeDataCheck(3.14));
-  print(typeDataCheck("Hello"));
-  print(typeDataCheck(true));
-  print(typeDataCheck(null));
+  // print(typeDataCheck(5));
+  // print(typeDataCheck(3.14));
+  // print(typeDataCheck("Hello"));
+  // print(typeDataCheck(true));
+  // print(typeDataCheck(null));
 }
 
-String? typeDataCheck(arg) {
-  if (arg is int) {
-    return "int";
-  } else if (arg is double) {
-    return "double";
-  } else if (arg is String) {
-    return "String";
-  } else if (arg is bool) {
-    return "bool";
-  } else if (arg is List) {
-    return "List";
-  } else if (arg is Map) {
-    return "Map";
-  } else if (arg == null) {
-    return "null";
-  } else {
-    return "Unknown";
-  }
-}
+// TODO: Implement a function that accepts a parameter of any type and returns a string describing its data type.
