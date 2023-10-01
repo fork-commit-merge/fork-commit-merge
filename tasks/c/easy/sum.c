531a5650615e37db7143f758ae2f2223bf5a29df
GitHub Action <action@github.com>
2023-10-01 11:54:23 +0000

    Revert "Merge pull request #798 from chiku011/sum"
    This reverts commit fb8bf85d9d2a9735f517e12e95562d551b1ec070, reversing
    changes made to ac38bbf1f491b825649c8aa78a1924166ee8adfa.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -3,17 +3,9 @@
 #include <stdio.h>
 
 // TODO: Implement the sum function
-int sum(int a,int b){
-    return a+b;
-
-}
 
 int main() {
     // TODO: Implement the main function
-    int a,b;
-    scanf("%d",&a);
-    scanf("%d",&b);
-    int res=sum(a,b);
-    printf(res);
+
     return 0;
 }
