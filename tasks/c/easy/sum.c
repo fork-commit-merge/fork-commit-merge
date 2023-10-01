286cf7efb6631300fb5d4f95c51f1b3e3175e81d
GitHub Action <action@github.com>
2023-10-01 05:27:29 +0000

    Revert "Merge pull request #762 from suprabhatdas/main"
    This reverts commit 5d7b05636480cdd3825b41354aac638bd5af82c0, reversing
    changes made to 80c60bed70f65ade567c6f7ebcb98e30ae7f932f.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,14 +1,11 @@
+// C - Easy
+
 #include <stdio.h>
 
+// TODO: Implement the sum function
+
 int main() {
-  int num1, num2, sum;
+    // TODO: Implement the main function
 
-  printf("Enter two numbers: ");
-  scanf("%d %d", &num1, &num2);
-
-  sum = num1 + num2;
-
-  printf("The sum of %d and %d is %d\n", num1, num2, sum);
-
-  return 0;
+    return 0;
 }
