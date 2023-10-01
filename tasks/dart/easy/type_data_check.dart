9fc4f61f4492d8b8dd79c1d7a4fb07815d47e267
GitHub Action <action@github.com>
2023-10-01 17:28:32 +0000

    Revert "Merge pull request #815 from micielkirsten/main"
    This reverts commit 60f8c7ffad5de3f287980cd2217f506650af8f0b, reversing
    changes made to f5a67458a961c2e2079bf1868f6912d996bb84d1.

--- tasks/dart/easy/type_data_check.dart
+++ tasks/dart/easy/type_data_check.dart
@@ -1,11 +1,9 @@
 void main() {
-  print(typeDataCheck(5));
-  print(typeDataCheck(3.14));
-  print(typeDataCheck("Hello"));
-  print(typeDataCheck(true));
-  print(typeDataCheck(null));
+  // print(typeDataCheck(5));
+  // print(typeDataCheck(3.14));
+  // print(typeDataCheck("Hello"));
+  // print(typeDataCheck(true));
+  // print(typeDataCheck(null));
 }
 
-String typeDataCheck(dynamic data) {
-  return data.runtimeType.toString();
-}
+// TODO: Implement a function that accepts a parameter of any type and returns a string describing its data type.
