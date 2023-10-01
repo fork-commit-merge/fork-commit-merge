80c60bed70f65ade567c6f7ebcb98e30ae7f932f
GitHub Action <action@github.com>
2023-10-01 05:26:07 +0000

    Revert "Merge pull request #761 from joel2607/main"
    This reverts commit 04532074fa30dbebf50591fd95ec0dd2d700c78a, reversing
    changes made to 239154c36fbd642f6e89c32d653fa27e0f27625f.

--- tasks/typescript/medium/calculator.ts
+++ tasks/typescript/medium/calculator.ts
@@ -1,14 +1,7 @@
 // TypeScript - Medium
 
 namespace Calculator {
-    export const add = (a: number,b: number) : number => a + b;
-    
-    export const subtract = (a: number,b: number) : number => a - b;
-    
-    export const multiply = (a: number,b: number) : number => a * b;
-    
-    export const divide = (a: number, b: number) : number | null => (b===0)?null:(a/b);
-    
+    // TODO: Implement a Simple TypeScript Calculator
 }
 
 // Test cases
