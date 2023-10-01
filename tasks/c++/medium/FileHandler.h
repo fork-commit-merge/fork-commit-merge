229ff17f4a249e5d02c41ebedd0ac55a393c8dd4
GitHub Action <action@github.com>
2023-10-01 04:51:39 +0000

    Revert "Merge pull request #753 from mokshanirugutti/fix/Implement-Simple-File-Reader/Writer"
    This reverts commit f6feff3f3fed6aa61d8b72f1e1100c9452576f17, reversing
    changes made to 97848e15bf49cd201b0614a3084d45e9031c0f5b.

--- tasks/c++/medium/FileHandler.h
+++ tasks/c++/medium/FileHandler.h
@@ -5,8 +5,5 @@
 
 class FileHandler {
 public:
-     static std::string ReadFile(const std::string& filePath);
-    static bool WriteFile(const std::string& filePath, const std::string& content);
-    static bool AppendFile(const std::string& filePath, const std::string& content);
-    static bool FileExists(const std::string& filePath);
+    // TODO: Implement the FileHandler class
 };
