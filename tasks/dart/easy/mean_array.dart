e8f60d53378a816621d85482533c5782b4b02496
GitHub Action <action@github.com>
2023-10-02 05:54:02 +0000

    Revert "Merge pull request #827 from manelk/fix773"
    This reverts commit ae0f4d0455d670370bdda2cee704669cc979d7c2, reversing
    changes made to 181d43bb7c04af9d11abda4beba7287b1ae57c53.

--- tasks/dart/easy/mean_array.dart
+++ tasks/dart/easy/mean_array.dart
@@ -1,14 +1,8 @@
 void main() {
-  List<int> data = [5, 10, 15, 20, 25];
-  double mean = calculateMean(data);
-  print('Mean: $mean');  // Expected output: Mean: 15.0
+  // List<int> data = [5, 10, 15, 20, 25];
+  // double mean = calculateMean(data);
+  // print('Mean: $mean');  // Expected output: Mean: 15.0
 }
 
-double calculateMean(List<int> arg){
-  int sum = 0;
-  for(var i=0; i<arg.length; i++){
-    sum = arg[i] + sum;
-  }
-  return sum/arg.length;
+// TODO: Implement a function called 'calculateMean' that takes a list of integers as input and returns the mean as a double.
 
-}
