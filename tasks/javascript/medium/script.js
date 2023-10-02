a1d8d68cdac2eda40ee4021b4a5baf35fb7a6d32
GitHub Action <action@github.com>
2023-10-02 06:07:48 +0000

    Revert "Merge pull request #831 from sonvir249/fix-703"
    This reverts commit d9f8f5b8ef6b2e1fc826925787fc3027c7b3df9f, reversing
    changes made to 321e77a03f49d98c7a64578ddd5ece2c3a1207a0.

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,11 +1,3 @@
 // JavaScript - Medium
 
 // TODO: Create functionality for a countdown timer that counts down from 10 seconds to zero
-
-const timer = document.getElementById("timer");
-let time = 10;
-
-setInterval(() => {
-  timer.innerHTML = (time < 0) ? "Time's up!" : `OO:${time}`;
-  time--;
-}, 1000);
