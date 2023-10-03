f8e8557d70ca2c89041dc345e8323d0d11194963
GitHub Action <action@github.com>
2023-10-03 09:59:23 +0000

    Revert "Merge pull request #850 from harshil-junakiya/c_program_changes"
    This reverts commit 0bd17316524be79f45554eb9678bf3b6f1990f22, reversing
    changes made to 7a2e93554fab61d362d0ee010883b83ee26e8804.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -4,15 +4,8 @@
 
 // TODO: Implement the sum function
 
-int sum(int a,int b){
-    return a+b;
-}
-
 int main() {
     // TODO: Implement the main function
-    int a , b;
-    printf("Enter two numbers: ");
-    scanf("%d %d",&a,&b);
-    printf("The sum is %d",sum(a,b));
+
     return 0;
 }
