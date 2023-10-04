ae2d43783edf867412929626a2b78f684242836c
GitHub Action <action@github.com>
2023-10-04 20:01:26 +0000

    Revert "Merge pull request #863 from alpharosto/dev-saurav"
    This reverts commit c5e37f0b8cb6a2aaa7678e52751ff1bd38ebb4e8, reversing
    changes made to 9f9ac1dd182cf2d46f732fa0288191be525f578f.

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,33 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const timerDisplay = document.getElementById('timer');
-    
-    let countdown; 
-    let seconds = 10;
+// JavaScript - Medium
 
-    function formatTime(time) {
-        const minutes = Math.floor(time / 60);
-        const seconds = time % 60;
-        return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-    }
-
-    function updateTimerDisplay() {
-        timerDisplay.textContent = formatTime(seconds);
-    }
-
-    function startCountdown() {
-        updateTimerDisplay();
-
-        countdown = setInterval(function () {
-            seconds--;
-
-            if (seconds < 0) {
-                clearInterval(countdown);
-                seconds = 0;
-            }
-
-            updateTimerDisplay();
-        }, 1000);
-    }
-
-    startCountdown(); 
-});
+// TODO: Create functionality for a countdown timer that counts down from 10 seconds to zero
