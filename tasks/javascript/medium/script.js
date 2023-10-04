5de5ea77b7564dcfd61dc9424ee096ba86339473
GitHub Action <action@github.com>
2023-10-04 15:15:26 +0000

    Revert "Merge pull request #860 from NsCracker/feat/832-Countdown-Timer"
    This reverts commit 6fb148c61c48c9b0eee423a8eccf068118c09081, reversing
    changes made to 71ab19f3002978e90ab8cc03c4bd8b32b382d7a0.

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,24 +1,3 @@
 // JavaScript - Medium
 
 // TODO: Create functionality for a countdown timer that counts down from 10 seconds to zero
-const timerElement = document.getElementById('timer');
-
-let countdown = 10;
-function updateTimer() {
-    const minutes = Math.floor(countdown / 60);
-    const seconds = countdown % 60;
-    timerElement.textContent = `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-}
-
-function countdownTick() {
-    updateTimer();
-
-    if (countdown === 0) {
-        timerElement.textContent = "Time's up!";
-        clearInterval(timerInterval);
-    } else {
-        countdown--;
-    }
-}
-
-const timerInterval = setInterval(countdownTick, 1000);
