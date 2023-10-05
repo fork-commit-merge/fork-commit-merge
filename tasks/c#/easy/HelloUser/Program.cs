0808a0eabc31c32935911403b32090aa7227551a
Mrudang Vora <mrudang-vora@users.noreply.github.com>
2023-10-05 18:25:59 +0530

    Fixed issue
    Signed-off-by: Mrudang Vora <mrudang-vora@users.noreply.github.com>

--- tasks/c#/easy/HelloUser/Program.cs
+++ tasks/c#/easy/HelloUser/Program.cs
@@ -1,3 +1,12 @@
 ﻿// C# - Easy
-
 // TODO: Implement the C# program requested in the Issue description
+using System;
+
+class Program{
+    static void Main(string[] args){
+        Console.WriteLine("Please enter your name:");
+        string? strInput = string.Empty;
+        strInput = Console.ReadLine();
+        Console.WriteLine("Hello, " + strInput);
+    }
+}
