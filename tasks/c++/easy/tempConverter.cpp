42840303dfe9d54ce02d164b4ca35ed3f48699bf
GitHub Action <action@github.com>
2023-10-06 19:00:27 +0000

    Revert "Merge pull request #910 from GD0910/main"
    This reverts commit d8a75c89f3ff5cef87f33780fd1c6ad5ef445de6, reversing
    changes made to 9d06446a8298b51d70ab8eba33102be32ce4c181.

--- tasks/c++/easy/tempConverter.cpp
+++ tasks/c++/easy/tempConverter.cpp
@@ -1,23 +1,3 @@
 // C++ - Easy
 
 // TODO:  Create a Basic C++ Program to Convert Temperature from Celsius to Fahrenheit
-
-#include <iostream>
-using namespace std;
-  
-int main() {
-    float fahren, celsius;
-  
-    cout << "Enter the temperature in celsius\n";
-    cin >> celsius;
-  
-    // convert celsius to fahreneheit 
-    // Multiply by 9, then divide by 5, then add 32
-    fahren =(9.0/5.0) * celsius + 32;
-  
-    cout << celsius <<"Centigrade is equal to " << fahren 
-        <<"Fahrenheit";
-      
-    return 0;
-}
-
