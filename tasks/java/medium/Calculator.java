3ec90f428605b2d4a13cc96812e85035d163acb5
GitHub Action <action@github.com>
2023-10-06 05:17:43 +0000

    Revert "Merge pull request #898 from jhonDoe15/main"
    This reverts commit 49335ee862c34d49012a9cdfdb70aa00023553da, reversing
    changes made to 794d69f702dcd7551f346526860dd8c86d6dc2ce.

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -4,21 +4,6 @@ package tasks.java.medium;
 
 public class Calculator {
     // TODO: Implement the Calculator class
-    private static float add(float a, float b){
-        return a + b;
-    }
-
-    private static float subtract(float a, float b){
-        return a - b;
-    }
-
-    private static float multiply(float a, float b){
-        return a * b;
-    }
-
-    private static float divide(float a, float b){
-        return a / b;
-    }
 
     public static void main(String[] args) {
         //! Dont't change the tests in the main method!
