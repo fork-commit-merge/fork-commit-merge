4b28581c58d26c47bc3fd32b49dce9e93e128965
GitHub Action <action@github.com>
2023-10-06 05:21:07 +0000

    Revert "Merge pull request #901 from manelk/fix832"
    This reverts commit 4357304d997c90df9f53f022b88f40b6ad096faf, reversing
    changes made to 79b2777dc617b2a6680542e68b78d8e18d78b766.

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,24 +1,3 @@
-// Create functionality for a countdown timer that counts down from 10 seconds to zero then display a message "Time's up!"
+// JavaScript - Medium
 
-// Logic:
-// Set a variable for the seconds
-// Repeat code every 1s using setInterval method
-// Decrement the seconds
-// When seconds is zero clear time and display "Time's up!";
-// If seconds not zero then display the remaining seconds
-
-const counterDown = () => {
-  let s = 10;
-  setInterval(function () {
-    if (s === 0) {
-      clearInterval(timer);
-      document.getElementById("timer").textContent = "Time's up!";
-    } else {
-      const formattedTime = `00:${s.toString().padStart(2, "0")}`;
-      document.getElementById("timer").textContent = formattedTime;
-      s--;
-    }
-  }, 1000);
-};
-
-window.onload = counterDown;
+// TODO: Create functionality for a countdown timer that counts down from 10 seconds to zero
