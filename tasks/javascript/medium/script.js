4f1acd8ee2c44e329dba8b80cb742c82e1b96e6a
Elcio Yoshiaki Sato <elciosato@gmail.com>
2023-10-06 22:40:38 +0200

    implementation Medium Issue (JavaScript)

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,3 +1,28 @@
 // JavaScript - Medium
 
 // TODO: Create functionality for a countdown timer that counts down from 10 seconds to zero
+let seconds = 10
+let intervalId = null
+
+function displayTimer(text) {
+  const timerText = document.querySelector('#timer')
+  timerText.textContent=text
+}
+
+function countdownSeconds() {
+  const text = `00:${String(seconds).padStart(2,'0')}`
+  if (seconds < 0) {
+    if (intervalId) {
+      clearInterval(intervalId)
+      intervalId = null
+    }
+    displayTimer('Time\'s up!')
+  } else {
+    console.log(text)
+    displayTimer(text)
+
+  }
+  seconds--
+}
+
+intervalId = setInterval(countdownSeconds,1000)
