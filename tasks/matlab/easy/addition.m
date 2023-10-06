66b6aa60a7c0e1ee57a42f3ae663e1a2e65629f7
P VASU NAYAK <pvasunayak@Ps-MacBook-Air.local>
2023-10-06 16:41:27 +0530

    Added two numbers

--- tasks/matlab/easy/addition.m
+++ tasks/matlab/easy/addition.m
@@ -1,3 +1,15 @@
-% MATLAB - Easy
+% Define two numbers
+num1 = 5;
+num2 = 7;
 
-% TODO: Create a MATLAB script that adds two numbers together and prints the result
+% Add the numbers
+result = num1 + num2;
+
+% Print the result
+fprintf('The sum of %d and %d is %d\n', num1, num2, result);
+
+
+
+
+
+
