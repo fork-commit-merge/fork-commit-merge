ccd8e75548fe5bc3b797e78e03a2203da4097aff
GitHub Action <action@github.com>
2023-10-07 14:42:27 +0000

    Revert "Merge pull request #920 from krish-0510/main"
    This reverts commit 1c68e580fc906cfb55c517ca2685c0bec1810a25, reversing
    changes made to 6fdf243b14c7b1fbfe2379f3e2cc8295679fccb5.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,16 +1,11 @@
+// C - Easy
+
 #include <stdio.h>
 
-int sum(int a,int b);  //funtion declaration
+// TODO: Implement the sum function
 
 int main() {
-    int a,b;
-    printf("Enter two numbers:");
-    scanf("%d%d",&a,&b);
-    printf("The sum is: %d",sum(a,b));    //function calling
-    return 0;
-}
+    // TODO: Implement the main function
 
-int sum(int a,int b)        //function definition
-{
-    return a+b;
+    return 0;
 }
