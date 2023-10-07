7bc765a937fbb905d0749a524cd2dbac2790dd99
GitHub Action <action@github.com>
2023-10-07 05:17:59 +0000

    Revert "Merge pull request #913 from elciosato/js-easy"
    This reverts commit 40e5a55c2266c30286642012b99067b8ad8e1d90, reversing
    changes made to 400f2dcf6442fc6d097a9726d114112a577afdff.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -2,11 +2,3 @@
 
 // TODO: Write a function that returns the sum of two numbers
 
-function randomNum(maxNum = 100 ) {
-  return Math.round(Math.random()*100) + 1
-}
-
-const num1 = randomNum()
-const num2 = randomNum();
-
-console.log(`The sum of ${num1} and ${num2} is ${num1 + num2}`)
