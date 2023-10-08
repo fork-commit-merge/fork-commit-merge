08ab00fab4667e7ad10be64cfcfe7be210e7c1e7
shree-77 <shreedhoni7@gmail.com>
2023-10-08 14:47:07 +0530

    Sql hard questions changed

--- tasks/sql/hard/selfJoin.sql
+++
@@ -1,8 +0,0 @@
--- TABLE : 
--- stops(id, name)
--- route(num, company, pos, stop)
-
--- Problem : 
--- Give a distinct list of the stops which may be reached from 'Craiglockhart'
---  by taking one bus, including 'Craiglockhart' itself, offered by the LRT
---   company. Include the company and bus no. of the relevant services.
