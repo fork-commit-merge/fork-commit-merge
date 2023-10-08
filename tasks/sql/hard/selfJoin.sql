48de8dd074cf9d6a542cf39ae3e6e54a01e55895
shree-77 <shreedhoni7@gmail.com>
2023-10-08 12:34:38 +0530

    new tasks - Java(hard),Sql(medium,hard) are added

---
+++ tasks/sql/hard/selfJoin.sql
@@ -0,0 +1,8 @@
+-- TABLE : 
+-- stops(id, name)
+-- route(num, company, pos, stop)
+
+-- Problem : 
+-- Give a distinct list of the stops which may be reached from 'Craiglockhart'
+--  by taking one bus, including 'Craiglockhart' itself, offered by the LRT
+--   company. Include the company and bus no. of the relevant services.
---
+++ tasks/sql/medium/Select(select).sql
@@ -0,0 +1,14 @@
+-- # Which country has a population that is more than Canada but
+-- # less than Poland? Show the name and the population.
+-- # use select within select 
+
+-- sample table : 
+
+-- World (Name,Continent,Area,Population,GDP);
+
+-- |   Name       |  Continent  |     Area    |  Population  |     GDP     |
+-- |--------------|-------------|-------------|--------------|-------------|
+
+
+
+
