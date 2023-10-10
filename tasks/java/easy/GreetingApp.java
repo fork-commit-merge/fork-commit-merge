349a35c21475d3ded0335af5d0ed6bb354fbc8e2
GitHub Action <action@github.com>
2023-10-10 10:19:01 +0000

    Revert "Merge pull request #970 from VAIBHAVPANT07/main"
    This reverts commit d96a5c55b319332e910dc8976f409fd59af96348, reversing
    changes made to 38a57f1c41b4cfe0ead9a1c6ace578eb294b3010.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,16 +1,8 @@
 // Java - Easy
-import java.util.*;
+
 public class GreetingApp {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("ENTER YOUR NAME: ");
-        String k= sc.nextLine();
-        System.out.println("Hello, " + k + " !Nice to meet you! "  );
-
-
-        
-        
         // TODO: Implement a Simple Greeting Application
     }
 }
