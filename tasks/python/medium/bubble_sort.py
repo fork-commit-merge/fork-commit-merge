0d1632524bd5f7a9c37db52ea509c573e424b054
GitHub Action <action@github.com>
2023-10-10 11:23:15 +0000

    Revert "Merge pull request #971 from hoalasama/main"
    This reverts commit 300f32624eac3b9829f68aec98b1975421c87a19, reversing
    changes made to 2899a8af2acfba88a873b7863039f8f14ea77731.

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -2,15 +2,7 @@
 
 def bubble_sort(arr):
     # TODO: Implement the bubble sort algorithm
-    n = len(arr)
-    swapped = False
-    for i in range(n-1):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j + 1]:
-                swapped = True
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-        if not swapped:
-            return
+
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
 print("Original array:", arr)
