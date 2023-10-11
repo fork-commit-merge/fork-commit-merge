d3fd4ae13c04f63f87cf53e956247070af93abf4
BuildTools <unconfigured@null.spigotmc.org>
2023-10-11 19:20:36 -0400

    sql medium task issue

--- tasks/sql/medium/Select(select).sql
+++ tasks/sql/medium/Select(select).sql
@@ -10,6 +10,18 @@
 -- |   Name       |  Continent  |     Area    |  Population  |     GDP     |
 -- |--------------|-------------|-------------|--------------|-------------|
 
-
+SELECT Name,
+    Population
+FROM World
+WHERE Population > (
+        SELECT Population
+        FROM World
+        WHERE Name = Canada
+    )
+    AND Population < (
+        SELECT Population
+        FROM World
+        WHERE Name = Poland
+    );
 
 
