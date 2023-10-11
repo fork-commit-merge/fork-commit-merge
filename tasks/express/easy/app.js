3766e859e7d8188f451115b9a22e19005eda606b
pick-cee <akinloluwaolumuyide@gmail.com>
2023-10-11 12:05:04 +0100

    fixed issue

--- tasks/express/easy/app.js
+++ tasks/express/easy/app.js
@@ -6,5 +6,11 @@ const app = express();
 const port = 6969;
 
 // TODO: Implement `get` method for / route
+app.get('/', (request, response) => {
+    response.json('Get route')
+})
 
 // TODO: Implement `listen` method
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
