f7d277c4ffb7633309dc3fc3721eea468cfeadc8
GitHub Action <action@github.com>
2023-10-12 05:40:57 +0000

    Revert "Merge pull request #989 from SaayanBiswas98/fix/#962-display-a-random-image"
    This reverts commit 2f92fe9274167923fc6876813aed86e0a05716a8, reversing
    changes made to ea86d15e8c5ac896f4dba3e72ca5f9cc724349b7.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -6,14 +6,6 @@ function App() {
         <>
             <div className="center">
                 {/* TODO: Implement the card component */}
-                <div>
-                    <h1>React/Vite - Easy</h1>
-                    <div className="card">
-                        <img src="https://loremflickr.com/320/240" alt=""/>
-                        <h2>Card Title</h2>
-                        <p>Card Description</p>
-                    </div>
-                </div>
             </div>
         </>
     );
