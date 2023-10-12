09e8f73900fbc1badd9de0171559543d11dde58c
Zeeshan Hassan <mzeeshanhassan125@gmail.com>
2023-10-12 11:12:07 +0500

    Scipt for Reversing a String in Pearl

--- tasks/perl/easy/reverse_string.pl
+++ tasks/perl/easy/reverse_string.pl
@@ -4,4 +4,14 @@
 use strict;
 use warnings;
 
+use strict;
+use warnings;
+
+print "Enter a string: ";
+my $input_str = <STDIN>;
+chomp $input_str;
+
+my $reversed_str = reverse $input_str;
+print "Reversed string: $reversed_str\n";
+
 # TODO: Write a Perl script that accepts a string and prints it in reversed order
