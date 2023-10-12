abbf88b64794c13857f4dcea6d795fe1fbffd3a9
GitHub Action <action@github.com>
2023-10-12 05:39:27 +0000

    Revert "Merge pull request #988 from tkmunir/sql-task"
    This reverts commit 55306930d83196b3dc46b2d57c247f55291926c5, reversing
    changes made to cc24943936c29fc4d20dcbad2e06d5c148e8352d.

--- tasks/sql/medium/Select(select).sql
+++ tasks/sql/medium/Select(select).sql
@@ -10,18 +10,6 @@
 -- |   Name       |  Continent  |     Area    |  Population  |     GDP     |
 -- |--------------|-------------|-------------|--------------|-------------|
 
-SELECT Name,
-    Population
-FROM World
-WHERE Population > (
-        SELECT Population
-        FROM World
-        WHERE Name = Canada
-    )
-    AND Population < (
-        SELECT Population
-        FROM World
-        WHERE Name = Poland
-    );
+
 
 
