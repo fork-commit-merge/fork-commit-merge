305876f3c86de3e5cf60c979bd792cdb3dda3f9e
GitHub Action <action@github.com>
2023-10-12 19:04:34 +0000

    Revert "Merge pull request #999 from Milagre10/milo_issue"
    This reverts commit 30ffaf808db6cb2793d3172bc823105a22c156c6, reversing
    changes made to 979085144f7e3efdf0ea57892730cf7cddd7018d.

--- tasks/bash/easy/create_files.sh
+++ tasks/bash/easy/create_files.sh
@@ -3,8 +3,3 @@
 # Bash - Easy
 
 # TODO: Write the Bash script requested in the issue description
-cd tasks/bash/easy
- for i in {1..3}; do touch file$i.txt; done
-for i in {1..3}; do echo 'Hello Shell' > file$i.txt; done
- for i in {1..3}; do cat file$i.txt; done
-
