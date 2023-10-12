0213ab17e67c82adad3a07a2d12e469f2f39fc82
Atorretti <98433038+Atorretti@users.noreply.github.com>
2023-10-12 22:54:04 +0100

    fixed issue

--- tasks/bash/easy/create_files.sh
+++ tasks/bash/easy/create_files.sh
@@ -3,3 +3,11 @@
 # Bash - Easy
 
 # TODO: Write the Bash script requested in the issue description
+for i in 1 2 3; do
+
+name="file${i}.txt"
+message="hello Shell!"
+
+echo "$message" > "$name"
+cat "$name"
+done
