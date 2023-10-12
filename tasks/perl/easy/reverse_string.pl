dd2412eee9febb62bb239479126c2b9ef5360857
GitHub Action <action@github.com>
2023-10-12 06:26:00 +0000

    Revert "Merge pull request #990 from thezeeshanhassan/main"
    This reverts commit 710ed0819f9b126942b2c57695edd3a6e2c546da, reversing
    changes made to 0d941be46e0d8a18411bf927fc5919fb1f3f3e13.

--- tasks/perl/easy/reverse_string.pl
+++ tasks/perl/easy/reverse_string.pl
@@ -4,14 +4,4 @@
 use strict;
 use warnings;
 
-use strict;
-use warnings;
-
-print "Enter a string: ";
-my $input_str = <STDIN>;
-chomp $input_str;
-
-my $reversed_str = reverse $input_str;
-print "Reversed string: $reversed_str\n";
-
 # TODO: Write a Perl script that accepts a string and prints it in reversed order
