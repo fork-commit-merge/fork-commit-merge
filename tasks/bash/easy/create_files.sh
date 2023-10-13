1f00d567089e620f259c378aba110af2457b52a8
GitHub Action <action@github.com>
2023-10-13 05:25:32 +0000

    Revert "Merge pull request #1000 from Atorretti/fix-issue"
    This reverts commit 5381a56f1e338c44a2d3aaf091821baa57d763d7, reversing
    changes made to f87d7db732c2b6249170361b51a1e84d9fcbcd0c.

--- tasks/bash/easy/create_files.sh
+++ tasks/bash/easy/create_files.sh
@@ -3,11 +3,3 @@
 # Bash - Easy
 
 # TODO: Write the Bash script requested in the issue description
-for i in 1 2 3; do
-
-name="file${i}.txt"
-message="hello Shell!"
-
-echo "$message" > "$name"
-cat "$name"
-done
