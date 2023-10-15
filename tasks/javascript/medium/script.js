e4971d30e3382f2d7055935580d9d0a18d108afe
Divyam Sharma <113417008+div02-afk@users.noreply.github.com>
2023-10-15 13:41:28 +0530

    Fixed issue

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,3 +1,14 @@
 // JavaScript - Medium
+let time = 9;
+const interval = setInterval(() => {
+    console.log(time);
+    const timer = document.getElementById('timer');
+    
+    timer.innerHTML = "00:0" + time;
+    time-=1;
+    if(time < 0){
+        clearInterval(interval);
+    }
+}, 1000);
 
 // TODO: Create functionality for a countdown timer that counts down from 10 seconds to zero
