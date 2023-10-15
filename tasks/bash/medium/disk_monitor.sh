d46f2125934b5ee58ba646995cf8e4d18306c3f5
GitHub Action <action@github.com>
2023-10-15 05:50:32 +0000

    Revert "Merge pull request #1013 from Atorretti/fix-issue"
    This reverts commit 6f7bb43f64ea35b53f420354b7d81f3fb173ab40, reversing
    changes made to a8c985b12a92cd8d69d800ebc3bc75b954c6c1a2.

--- tasks/bash/medium/disk_monitor.sh
+++ tasks/bash/medium/disk_monitor.sh
@@ -3,19 +3,3 @@
 # Bash - Medium
 
 # TODO: Implement the Disk Monitor script
-#!/bin/bash
-
-threshold=20
-
-
-disk_space=$(df --output=pcent / | tail -n 1)
-disk_space=${disk_space%\%*} 
-
-if ((disk_space < threshold)); then
-   
-    recipient="email@example.com"
-    subject="Low Disk Space Alert on $(hostname)"
-    message="Available disk space is ${disk_space}%, which is below the threshold of ${threshold}%."
-
-    echo "$message" | mail -s "$subject" "$recipient"
-fi
