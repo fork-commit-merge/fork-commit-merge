28074533bdabfaa745a1eb7940984fe3631e32cb
GitHub Action <action@github.com>
2023-10-15 08:18:25 +0000

    Revert "Merge pull request #1019 from div02-afk/fix-issue"
    This reverts commit 42e746b39dd341bc76dbff4dc3b47bb252f38a14, reversing
    changes made to 2bbccae789d3fbdce4e80b3247454eeea9df4fc5.

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,14 +1,3 @@
 // JavaScript - Medium
-let time = 9;
-const interval = setInterval(() => {
-    console.log(time);
-    const timer = document.getElementById('timer');
-    
-    timer.innerHTML = "00:0" + time;
-    time-=1;
-    if(time < 0){
-        clearInterval(interval);
-    }
-}, 1000);
 
 // TODO: Create functionality for a countdown timer that counts down from 10 seconds to zero
