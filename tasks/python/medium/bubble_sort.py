da8dd1820c48d733e8f049ec8133e39cf0a020f2
Thanh <0ngminhthanh0@gmail.com>
2023-10-16 20:24:00 +0700

    Wrote code for easy and medium

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -2,6 +2,11 @@
 
 def bubble_sort(arr):
     # TODO: Implement the bubble sort algorithm
+    for i in range(len(arr)):
+        for j in range(len(arr) - i - 1):
+            if arr[i] > arr[j + 1]:
+                arr[j], arr[i] = arr[i], arr[j]
+    return arr
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
