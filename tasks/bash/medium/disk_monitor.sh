03733782d26d26b50b08529ab126541539d18439
GitHub Action <action@github.com>
2023-10-18 16:14:25 +0000

    Revert "Merge pull request #1097 from Devanshukoli/bash_easy"
    This reverts commit 1580067a24cce0cab6e5b5171f375822914b7bc6, reversing
    changes made to 3eee7f8b130000358bd5a6a4d36c98fb512f7789.

--- tasks/bash/medium/disk_monitor.sh
+++ tasks/bash/medium/disk_monitor.sh
@@ -2,23 +2,4 @@
 
 # Bash - Medium
 
-#!/bin/bash
-
-threshold=20 # Percentage threshold for low disk space 
-email="alerts@example.com"
-
-# Get disk usage
-df -h | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }' > disk_usage.txt
-
-# Now Checking if usage exceeds threshold
-while read usage mount; do
-  use=$(echo $usage | sed 's/%//g')
-  if [ $use -ge $threshold ]; then
-    echo "Disk usage on $mount has exceeded $threshold%" 
-  fi
-done < disk_usage.txt
-
-# Then if any disks exceeded threshold, send email alert
-if [ $? -eq 0 ]; then
-  echo "Disk space alert" | mail -s "Disk Space Alert" $email
-fi
+# TODO: Implement the Disk Monitor script
