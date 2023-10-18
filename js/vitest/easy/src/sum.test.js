d11bcad996c56377e474f7d69ffbd7c1fd722c14
Sehar Batool <97355349+seharbat00l@users.noreply.github.com>
2023-10-18 22:17:51 +0000

    Easy Issue 1 (Ruby) #1086 solved

---
+++ js/vitest/easy/src/sum.test.js
@@ -0,0 +1,4 @@
+"use strict";
+// Vitest - Easy
+Object.defineProperty(exports, "__esModule", { value: true });
+// TODO: Create the test required in the task
--- tasks/ruby/easy/index.rb
+++ tasks/ruby/easy/index.rb
@@ -2,4 +2,11 @@
 
 # TODO: Implement a function named sum_of_two_numbers that calculates the sum of two numbers
 
-puts(sum_of_two_numbers(2,4))
+def sum_of_two_numbers(num1, num2)
+    result = num1 + num2
+    return result
+  end
+  
+  # Function call with sample numbers (2 and 4)
+  result = sum_of_two_numbers(2, 4)
+  puts result
