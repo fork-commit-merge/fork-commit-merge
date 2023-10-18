3d53d3b2218e4d501f98ec6f9d896f0ce04bad7c
Utkarsh Srivastava <srivastavautkarsh539@gmail.com>
2023-10-18 22:43:34 +0530

    Implemented Simple HTTP Server with Express.js

--- tasks/express/easy/app.js
+++ tasks/express/easy/app.js
@@ -1,10 +1,12 @@
-// ExpressJS - Easy
-
-const { get } = require("cheerio/lib/api/traversing");
-const express = require("express");
+const express = require('express');
 const app = express();
+
 const port = 6969;
 
-// TODO: Implement `get` method for / route
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
 
-// TODO: Implement `listen` method
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
