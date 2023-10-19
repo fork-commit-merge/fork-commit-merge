3693140791efd1f0f6f9eefe37f6351c635a0290
GitHub Action <action@github.com>
2023-10-19 05:28:08 +0000

    Revert "Merge pull request #1101 from seharbat00l/easyIssue1PHP"
    This reverts commit 8f6e7bbedcbebb977ee2bacdabbf381db6e2aca2, reversing
    changes made to b8d222b2c975a824042bbd61370be87fcfa6dff4.

--- js/easy/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// TypeScript - Easy
-function greet(name) {
-    // TODO: Add return statement
-}
-var user = 'John Doe';
-console.log(greet(user));
--- tasks/php/easy/index.php
+++ tasks/php/easy/index.php
@@ -6,7 +6,3 @@ $hello = "Hello";
 $world = "World!";
 
 // TODO: Implement rest of the string concatenation program
-$concatenatedString = $hello . $world;
-echo $concatenatedString;
-
-?>
