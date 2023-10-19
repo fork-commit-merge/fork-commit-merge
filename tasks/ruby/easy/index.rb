5146fde329ef75730fdb0cf893ca008461a9c4af
GitHub Action <action@github.com>
2023-10-19 05:29:32 +0000

    Revert "Merge pull request #1102 from seharbat00l/easyissueruby"
    This reverts commit 28ec425e9e23020cd3ce483fa50a270ddd6cf301, reversing
    changes made to 3693140791efd1f0f6f9eefe37f6351c635a0290.

--- tasks/ruby/easy/index.rb
+++ tasks/ruby/easy/index.rb
@@ -2,11 +2,4 @@
 
 # TODO: Implement a function named sum_of_two_numbers that calculates the sum of two numbers
 
-def sum_of_two_numbers(num1, num2)
-    result = num1 + num2
-    return result
-  end
-  
-  # Function call with sample numbers (2 and 4)
-  result = sum_of_two_numbers(2, 4)
-  puts result
+puts(sum_of_two_numbers(2,4))
