cfdcf4683f5c6441a7115f0fe91eff75b59eebec
Angad <80452785+Angad-Godara@users.noreply.github.com>
2023-10-21 02:57:59 +0530

    Delete tasks/react/cloudbook directory

--- tasks/react/cloudbook/src/Context/Alert/alertContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const alertContext = createContext()
-
-export default alertContext
--- tasks/react/cloudbook/src/Context/LoadingBar/loaderContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const loaderContext = createContext()
-
-export default loaderContext
