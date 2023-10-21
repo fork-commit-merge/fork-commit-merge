bb354235514384e8a33c08702fed52d31c4d6c31
Bratajit Das <106532791+Bratajit-03@users.noreply.github.com>
2023-10-21 12:50:49 +0530

    add_numbers.dart

--- tasks/dart/easy/add_numbers.dart
+++ tasks/dart/easy/add_numbers.dart
@@ -1,11 +1,9 @@
-// add_numbers.dart
-
 void main() {
-  // Function call and print statement
-  print(addNumbers(5, 10)); // Uncomment this line to check the function with 5 and 10.
+  
+  print(addNumbers(5, 10));
 }
 
 int addNumbers(int a, int b) {
-  // Function to add two numbers
+  
   return a + b;
 }
