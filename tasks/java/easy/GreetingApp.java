224234044888c4019fd86acfe7905de8fbc45791
GitHub Action <action@github.com>
2023-10-21 10:24:27 +0000

    Revert "Merge pull request #1119 from FarukhS52/patch-1"
    This reverts commit 2d2956a12d27c22a8960a60add157822bcdf35c0, reversing
    changes made to 8712574bfd17c4a74f016cafed2713377af19342.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -3,16 +3,6 @@
 public class GreetingApp {
 
     public static void main(String[] args) {
-        
-        System.out.println("Enter your name:");
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.next();
-        scanner.close();
-        greetUser(name);
-    }
-
-    public static void greetUser(String name){
-
-        System.out.println("Hi there, " + name);
+        // TODO: Implement a Simple Greeting Application
     }
 }
