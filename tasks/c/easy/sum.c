09e7ad80ed0aeccd0f85bae5ac273260079bfaa1
GitHub Action <action@github.com>
2023-10-21 17:35:56 +0000

    Revert "Merge pull request #1123 from amandeep-singh-parihar/main"
    This reverts commit 7403e4468c521f4624c0264e8d5d41e1b2889d4a, reversing
    changes made to 6c812179f6abda0071c5f849da1ab5b2b55f7de4.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -3,23 +3,9 @@
 #include <stdio.h>
 
 // TODO: Implement the sum function
-int sum(int a, int b) {
-    return a + b;
-}
 
 int main() {
     // TODO: Implement the main function
-     int num1, num2;
-
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
-
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
-
-    int result = sum(num1, num2);
-
-    printf("The sum of %d and %d is: %d\n", num1, num2, result);
 
     return 0;
 }
