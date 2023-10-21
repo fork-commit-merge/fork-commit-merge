d6afdfe1a387b91e4ceb26982fbce8cde7f0bfda
Kunal Sharma <cse.190320027@silicon.ac.in>
2023-10-21 15:14:56 +0530

    Update GreetingApp.java
    Written a simple program to take input from user, its name and print the desired task

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,8 +1,12 @@
-// Java - Easy
+import java.util.Scanner;
 
 public class GreetingApp {
 
     public static void main(String[] args) {
-        // TODO: Implement a Simple Greeting Application
+        // Implement a Simple Greeting Application
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String a = sc.nextLine();
+        System.out.println("Hello, " + a + "! Nice to meet you!");
     }
 }
