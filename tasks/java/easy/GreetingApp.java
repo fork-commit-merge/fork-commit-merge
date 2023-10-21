08edbbd8adce25ed93b30e519a41b09a4accd48f
Farookh Zaheer Siddiqui <129654632+FarukhS52@users.noreply.github.com>
2023-10-21 15:45:13 +0530

    Update GreetingApp.java

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -3,6 +3,18 @@
 public class GreetingApp {
 
     public static void main(String[] args) {
-        // TODO: Implement a Simple Greeting Application
+        
+        System.out.println("Enter your name:");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        scanner.close();
+        greetUser(name);
+    }
+
+    public static void greetUser(String name){
+
+        System.out.println("Hi there, " + name);
+    }
+
     }
 }
