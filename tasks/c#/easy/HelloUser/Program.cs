6ff452c8caaa0e8edbeabbe0e3f811025fa58755
GitHub Action <action@github.com>
2023-10-22 17:08:01 +0000

    Revert "Merge pull request #1142 from RS-labhub/patch-1"
    This reverts commit 7edbc8793823e996a44897d11936a560f8bf93ed, reversing
    changes made to 7b0597efd325fca377d2f128b61eadf7e245f2d2.

--- tasks/c#/easy/HelloUser/Program.cs
+++ tasks/c#/easy/HelloUser/Program.cs
@@ -1,13 +1,3 @@
 ﻿// C# - Easy
 
 // TODO: Implement the C# program requested in the Issue description
-
-using System;
-
-class MainClass {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Please enter your name:");
-    string name = Console.ReadLine();
-    Console.WriteLine("Hello {0}", name); 
-   }
-}
