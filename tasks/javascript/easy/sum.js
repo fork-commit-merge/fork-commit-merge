2856571c90aead43cd86c9247c9dc8a392c3127f
amandeep singh parihar <amandeepparihar24@gmail.com>
2023-10-22 09:38:59 +0530

    implementing the sum function in javascript

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -2,3 +2,10 @@
 
 // TODO: Write a function that returns the sum of two numbers
 
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+// Example
+const result = addNumbers(5, 3);
+console.log(result); // This will output 8
