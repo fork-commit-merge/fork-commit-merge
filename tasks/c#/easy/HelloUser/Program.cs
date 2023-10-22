f5a2001d81909840be4b85c35fc2db9dc048e8c6
Rohan Sharma <117426013+RS-labhub@users.noreply.github.com>
2023-10-22 22:25:42 +0530

    Update Program.cs

--- tasks/c#/easy/HelloUser/Program.cs
+++ tasks/c#/easy/HelloUser/Program.cs
@@ -1,3 +1,25 @@
 ﻿// C# - Easy
 
 // TODO: Implement the C# program requested in the Issue description
+
+using System; 
+
+// namespace declaration 
+namespace HelloWorldApp { 
+	
+	// Class declaration 
+	class Geeks { 
+		
+		// Main Method 
+		static void Main(string[] args) { 
+			
+			// statement 
+			// printing Hello World! 
+			Console.WriteLine("Hello World!"); 
+			
+			// To prevents the screen from 
+			// running and closing quickly 
+			Console.ReadKey(); 
+		} 
+	} 
+} 
