8548f9e942612a08bc5f55223a90d7da8e919d15
GitHub Action <action@github.com>
2023-10-22 10:21:32 +0000

    Revert "Merge pull request #1134 from amandeep-singh-parihar/main"
    This reverts commit f12ef6fa43a2461b86c6903e020a496f715b5e91, reversing
    changes made to 77cbefbe7cd7fb8310f170057f4ad8a77e8341b2.

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,23 +1,3 @@
 // JavaScript - Medium
 
 // TODO: Create functionality for a countdown timer that counts down from 10 seconds to zero
-document.addEventListener("DOMContentLoaded", function() {
-    const timer = document.getElementById("timer");
-    let timeLeft = 10;
-  
-    function updateTimer() {
-      const minutes = Math.floor(timeLeft / 60);
-      const seconds = timeLeft % 60;
-      timer.textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-  
-      if (timeLeft === 0) {
-        clearInterval(timerInterval);
-        timer.textContent = "00:00";
-      } else {
-        timeLeft--;
-      }
-    }
-  
-    updateTimer();
-    const timerInterval = setInterval(updateTimer, 1000);
-  });  
