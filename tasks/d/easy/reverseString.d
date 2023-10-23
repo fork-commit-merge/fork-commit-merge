7c5ed60cc72a2eb82ee3b25acfe4a226059fd60a
GitHub Action <action@github.com>
2023-10-23 06:33:25 +0000

    Revert "Merge pull request #1148 from romitp4l/main"
    This reverts commit 8b091a304b1d0aef3377a6544dd60b8949cfc395, reversing
    changes made to 6ff452c8caaa0e8edbeabbe0e3f811025fa58755.

--- tasks/d/easy/reverseString.d
+++ tasks/d/easy/reverseString.d
@@ -1,17 +1,7 @@
+// D - Easy
+
 import std.stdio;
 
-string reverseString(string input) {
-    string reversed = "";
-    for (int i = input.length - 1; i >= 0; i--) {
-        reversed ~= input[i];
-    }
-    return reversed;
-}
-
 void main() {
-    string originalString = "Hello, World!";
-    string reversedString = reverseString(originalString);
-    
-    writeln("Original: ", originalString);
-    writeln("Reversed: ", reversedString);
+    // TODO: Implement program to reverse a string
 }
--- tasks/kotlin/easy/GreetingApp.kt
+++ tasks/kotlin/easy/GreetingApp.kt
@@ -2,12 +2,4 @@
 
 fun main() {
     // TODO: Create a Simple Kotlin Program to Print User's Input
-    val scanner = Scanner(System.`in`)
-
-    print("Enter something: ")
-    val userInput = scanner.nextLine()
-
-    println("You entered: $userInput")
-
-    scanner.close()
 }
