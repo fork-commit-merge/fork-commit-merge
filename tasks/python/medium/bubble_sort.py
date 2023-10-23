fefc86a37e02a76702e8c76fad128745637c4d20
GitHub Action <action@github.com>
2023-10-23 12:57:50 +0000

    Revert "Merge pull request #1158 from amandeep-singh-parihar/main"
    This reverts commit b03f064b9bf97c27de14d0a8c3d5c23d4cb602a7, reversing
    changes made to 4c82ca0dcb15137e3127474e4bfc7dc7fa1bdec3.

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -2,19 +2,6 @@
 
 def bubble_sort(arr):
     # TODO: Implement the bubble sort algorithm
-    n = len(arr)
-
-    for i in range(n):
-        swapped = False
-
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-                swapped = True
-
-        if not swapped:
-            break
-
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
