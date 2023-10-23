2761bd3630b92b73cf50d470d33bf08f4f3c8d7d
GitHub Action <action@github.com>
2023-10-23 13:47:41 +0000

    Revert "Merge pull request #1161 from Daoist-Ye/main"
    This reverts commit e9c3d54d2a6403b84326044465e56c9709a13011, reversing
    changes made to dd3e86aa8b9665733399173ef13e94c54ce544f4.

--- tasks/c++/medium/FileHandler.cpp
+++ tasks/c++/medium/FileHandler.cpp
@@ -2,68 +2,13 @@
 
 #include "FileHandler.h"
 #include <fstream>
-#include <iostream>
 #include <sstream>
 
-// Implemented the FileHandler::ReadFile method
-std::string FileHandler::ReadFile(const std::string &filePath)
-{
-    std::string content;
-    std::ifstream file(filePath);
+// TODO: Implement the FileHandler::ReadFile method
 
-    if (file)
-    {
-        // Read the content of the file into a string
-        content.assign((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-        file.close();
-    }
-    else
-    {
-        std::cerr << "Error: Unable to read the file '" << filePath << "'.\n";
-    }
+// TODO: Implement the FileHandler::WriteFile method
 
-    return content;
-}
+// TODO: Implement the FileHandler::AppendFile method
 
-// Implemented the FileHandler::WriteFile method
-bool FileHandler::WriteFile(const std::string &filePath, const std::string &content)
-{
-    std::ofstream file(filePath, std::ios::out);
+// TODO: Implement the FileHandler::FileExists method
 
-    if (file)
-    {
-        file << content;
-        file.close();
-        return true;
-    }
-    else
-    {
-        std::cerr << "Error: Unable to write to the file '" << filePath << "'.\n";
-        return false;
-    }
-}
-
-// Implemented the FileHandler::AppendFile method
-bool FileHandler::AppendFile(const std::string &filePath, const std::string &content)
-{
-    std::ofstream file(filePath, std::ios::out | std::ios::app);
-
-    if (file)
-    {
-        file << content;
-        file.close();
-        return true;
-    }
-    else
-    {
-        std::cerr << "Error: Unable to append to the file '" << filePath << "'.\n";
-        return false;
-    }
-}
-
-// Implemented the FileHandler::FileExists method
-bool FileHandler::FileExists(const std::string &filePath)
-{
-    std::ifstream file(filePath);
-    return file.good();
-}
--- tasks/c++/medium/FileHandler.h
+++ tasks/c++/medium/FileHandler.h
@@ -1,19 +1,9 @@
 // C++ - Medium
 
 #pragma once
-#ifndef FILEHANDLER_H
-#define FILEHANDLER_H
-
 #include <string>
 
-class FileHandler
-{
+class FileHandler {
 public:
-    // Implement the FileHandler class
-    static std::string ReadFile(const std::string &filePath);
-    static bool WriteFile(const std::string &filePath, const std::string &content);
-    static bool AppendFile(const std::string &filePath, const std::string &content);
-    static bool FileExists(const std::string &filePath);
+    // TODO: Implement the FileHandler class
 };
-
-#endif // FILEHANDLER_H
