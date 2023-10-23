dcadb69e596640a3a5b65e033b8be5c70b935373
GitHub Action <action@github.com>
2023-10-23 10:45:20 +0000

    Revert "Merge pull request #1155 from FarukhS52/main"
    This reverts commit f0de8701031e21c33a42711c446d6cd57ae37de9, reversing
    changes made to 265a2a7dec3ba4149043e3e6c147408180d67acf.

--- tasks/c++/easy/tempConverter.cpp
+++ tasks/c++/easy/tempConverter.cpp
@@ -1,14 +1,3 @@
 // C++ - Easy
 
 // TODO:  Create a Basic C++ Program to Convert Temperature from Celsius to Fahrenheit
-int main()
-{
-    float fahrenheit, celsius;
- 
-    cout << "Enter the temperature in Celsius : ";
-    cin >> celsius;
-    fahrenheit = (celsius * 9.0) / 5.0 + 32;
-    cout << "The temperature in Celsius    : " << celsius << endl;
-    cout << "The temperature in Fahrenheit : " << fahrenheit << endl;
-    return 0;
-}
