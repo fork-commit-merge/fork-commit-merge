ec33b7320512ffe6e415c461e787321871de7dc0
ROMIT PAL <85341342+romitp4l@users.noreply.github.com>
2023-10-23 10:07:07 +0530

    Update reverseString.d
     Implement program to reverse a string

--- tasks/d/easy/reverseString.d
+++ tasks/d/easy/reverseString.d
@@ -1,7 +1,17 @@
-// D - Easy
-
 import std.stdio;
 
+string reverseString(string input) {
+    string reversed = "";
+    for (int i = input.length - 1; i >= 0; i--) {
+        reversed ~= input[i];
+    }
+    return reversed;
+}
+
 void main() {
-    // TODO: Implement program to reverse a string
+    string originalString = "Hello, World!";
+    string reversedString = reverseString(originalString);
+    
+    writeln("Original: ", originalString);
+    writeln("Reversed: ", reversedString);
 }
