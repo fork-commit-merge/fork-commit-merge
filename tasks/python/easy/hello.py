4c82ca0dcb15137e3127474e4bfc7dc7fa1bdec3
GitHub Action <action@github.com>
2023-10-23 12:17:26 +0000

    Revert "Merge pull request #1157 from amandeep-singh-parihar/main"
    This reverts commit 488d72b7786c06dcdc35b2b19ee6043e08763863, reversing
    changes made to c9f07dcf4f21054a1d22d56cbaa767e42c561644.

--- tasks/python/easy/hello.py
+++ tasks/python/easy/hello.py
@@ -1,7 +1,3 @@
 # Python - Easy
 
 # TODO: Write a function that prints "Hello, Python!" to the console
-def say_hello():
-    print("Hello, Python!")
-
-say_hello()
--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,5 +1,3 @@
 # Python - Easy
 
 # TODO: Write a function that returns the sum of two numbers
-def add_numbers(a, b):
-    return a + b
