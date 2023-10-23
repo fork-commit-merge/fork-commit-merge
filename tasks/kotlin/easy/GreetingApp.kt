d59a234cc3bb20e1e805e60c6875a97941d2f684
ROMIT PAL <85341342+romitp4l@users.noreply.github.com>
2023-10-23 09:59:39 +0530

    Update GreetingApp.kt
    Created a Simple Kotlin Program to Print User's Input

--- tasks/kotlin/easy/GreetingApp.kt
+++ tasks/kotlin/easy/GreetingApp.kt
@@ -2,4 +2,12 @@
 
 fun main() {
     // TODO: Create a Simple Kotlin Program to Print User's Input
+    val scanner = Scanner(System.`in`)
+
+    print("Enter something: ")
+    val userInput = scanner.nextLine()
+
+    println("You entered: $userInput")
+
+    scanner.close()
 }
