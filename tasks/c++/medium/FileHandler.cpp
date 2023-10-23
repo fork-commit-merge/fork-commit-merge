7e106546d98b8c344f3eead8daaa95953a4e0ac8
Daoist-Ye <yashanand598@gmail.com>
2023-10-23 18:54:05 +0530

    Medium Issue (C++) #1039
    Worked on C++ Medium Level Implemented sile system

--- tasks/c++/medium/FileHandler.cpp
+++ tasks/c++/medium/FileHandler.cpp
@@ -2,13 +2,68 @@
 
 #include "FileHandler.h"
 #include <fstream>
+#include <iostream>
 #include <sstream>
 
-// TODO: Implement the FileHandler::ReadFile method
+// Implemented the FileHandler::ReadFile method
+std::string FileHandler::ReadFile(const std::string &filePath)
+{
+    std::string content;
+    std::ifstream file(filePath);
 
-// TODO: Implement the FileHandler::WriteFile method
+    if (file)
+    {
+        // Read the content of the file into a string
+        content.assign((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+        file.close();
+    }
+    else
+    {
+        std::cerr << "Error: Unable to read the file '" << filePath << "'.\n";
+    }
 
-// TODO: Implement the FileHandler::AppendFile method
+    return content;
+}
 
-// TODO: Implement the FileHandler::FileExists method
+// Implemented the FileHandler::WriteFile method
+bool FileHandler::WriteFile(const std::string &filePath, const std::string &content)
+{
+    std::ofstream file(filePath, std::ios::out);
 
+    if (file)
+    {
+        file << content;
+        file.close();
+        return true;
+    }
+    else
+    {
+        std::cerr << "Error: Unable to write to the file '" << filePath << "'.\n";
+        return false;
+    }
+}
+
+// Implemented the FileHandler::AppendFile method
+bool FileHandler::AppendFile(const std::string &filePath, const std::string &content)
+{
+    std::ofstream file(filePath, std::ios::out | std::ios::app);
+
+    if (file)
+    {
+        file << content;
+        file.close();
+        return true;
+    }
+    else
+    {
+        std::cerr << "Error: Unable to append to the file '" << filePath << "'.\n";
+        return false;
+    }
+}
+
+// Implemented the FileHandler::FileExists method
+bool FileHandler::FileExists(const std::string &filePath)
+{
+    std::ifstream file(filePath);
+    return file.good();
+}
