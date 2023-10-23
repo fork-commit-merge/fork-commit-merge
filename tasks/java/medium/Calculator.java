83ff35675b0671dcaac7e9de2f940dfd61e8e030
amandeep singh parihar <amandeepparihar24@gmail.com>
2023-10-23 18:35:47 +0530

    implemented the Calculator class in java

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -4,6 +4,28 @@ package tasks.java.medium;
 
 public class Calculator {
     // TODO: Implement the Calculator class
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Implement the subtract method
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Implement the multiply method
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Implement the divide method
+    public double divide(int a, int b) {
+        if (b == 0) {
+            System.out.println("Division by zero is not allowed.");
+            return Double.NaN;
+        }
+        return (double) a / b;
+    }
 
     public static void main(String[] args) {
         //! Dont't change the tests in the main method!
