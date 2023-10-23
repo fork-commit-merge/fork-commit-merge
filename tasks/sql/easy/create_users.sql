96ea2a80a7d3bea9f1cdbe62ae0efcb43883132c
GitHub Action <action@github.com>
2023-10-23 16:40:39 +0000

    Revert "Merge pull request #1165 from amandeep-singh-parihar/main"
    This reverts commit 667247d35dcd4dd9944965403815f46b9f960081, reversing
    changes made to cd72211d3715586e47e919f1f36d906d982e7447.

--- tasks/sql/easy/create_users.sql
+++ tasks/sql/easy/create_users.sql
@@ -2,11 +2,5 @@
 
 CREATE TABLE users (
     -- TODO: Add all the requested fields
-    user_id INT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    password_hash CHAR(64) NOT NULL,
-    registration_date DATE,
-    is_active BOOLEAN DEFAULT true
 );
 
