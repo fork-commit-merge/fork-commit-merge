ca34264f80a18185797a29b3ec6d9d515bc1aadf
Mahraib Fatima <mahraibfatima5@gmail.com>
2023-10-25 00:47:23 +0500

    modify hello.asm

--- tasks/asm/easy/hello.asm
+++ tasks/asm/easy/hello.asm
@@ -1,3 +1,30 @@
 ; ASM (Assembly) - Easy
 
 ; TODO: Implement a Basic "Hello, World!" Program
+
+
+
+
+org 100h
+
+.model small
+.stack 100h
+.data
+    bval db 'Hello, World!$'
+
+.code
+main proc
+    mov dx, offset bval   ; Load the address of bval into DX
+    mov ah, 9
+    int 21h             ; DOS print string function
+    mov ah, 4Ch         ; Exit program function
+    int 21h             ; DOS terminate program function
+main endp
+end main
+
+
+ret
+
+
+
+
