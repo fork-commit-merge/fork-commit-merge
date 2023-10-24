ba9f8b95283b711306abb025465ed012c7929438
GitHub Action <action@github.com>
2023-10-24 08:31:02 +0000

    Revert "Merge pull request #1171 from SoulNikhar/patch-1"
    This reverts commit c8ce232f8db59aad418a473444e8eb53ab226196, reversing
    changes made to b5fe59457f6cf8e614fec82961565c9f731e5d06.

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,18 +1,3 @@
 // JavaScript - Medium
 
 // TODO: Create functionality for a countdown timer that counts down from 10 seconds to zero
-
-const timer = document.getElementById('timer');
-let timeLeft = 10;
-function updateTimer() {
-    const minutes = Math.floor(timeLeft / 60);
-    const seconds = timeLeft % 60;
-    timer.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-    if (timeLeft === 0) {
-        timer.textContent = "Time's up!";
-        clearInterval(interval);
-    } else {
-        timeLeft--;
-    }
-}
-const interval = setInterval(updateTimer, 1000);
