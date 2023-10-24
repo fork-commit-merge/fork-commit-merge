6b231d59b39f8a10fb4786e6bdc0c5e8886d739c
Pratham Mahajan <111378171+Killer2OP@users.noreply.github.com>
2023-10-24 23:37:25 +0530

    [ADDED]: add_numbers in dart

--- tasks/dart/easy/add_numbers.dart
+++ tasks/dart/easy/add_numbers.dart
@@ -1,6 +1,20 @@
-void main() {
-  // int sum = addNumbers(5, 10);
-  // print('The sum is: $sum');
+import 'dart:io';
+
+// Function to add two numbers
+int addNumbers(int a, int b) {
+  return a + b;
 }
 
-// TODO: Implement addNumbers function that returns the sum of two numbers
+void main() {
+  // Read the first number from the user
+  stdout.write('Enter the first number: ');
+  int firstNumber = int.parse(stdin.readLineSync()!);
+
+  // Read the second number from the user
+  stdout.write('Enter the second number: ');
+  int secondNumber = int.parse(stdin.readLineSync()!);
+
+  // Call the addNumbers function and print the result
+  int sum = addNumbers(firstNumber, secondNumber);
+  print('The sum of $firstNumber and $secondNumber is: $sum');
+}
