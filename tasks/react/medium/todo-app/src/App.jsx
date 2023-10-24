7dbf3172e7b47df1fcbad690b2d047222ce812e2
GitHub Action <action@github.com>
2023-10-24 14:44:26 +0000

    Revert "Merge pull request #1195 from shraddha761/patch-1"
    This reverts commit 3bf702e2c8abc45b16427deed4900fc3ec7cb1ab, reversing
    changes made to 1c5f8e67c618d5fc09610b0ad6aefc472e1f9e43.

--- tasks/react/medium/todo-app/src/App.jsx
+++ tasks/react/medium/todo-app/src/App.jsx
@@ -1,67 +1,15 @@
 // React - Medium
 
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import './App.css';
 
-const TodoList = () => {
-  const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState("");
-
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem("todos") || "[]");
-    setTodos(storedTodos);
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
-
-  const addTodo = () => {
-    if (newTodo.trim() !== "") {
-      setTodos([...todos, { id: Date.now(), text: newTodo }]);
-      setNewTodo("");
-    }
-  };
-
-  const updateTodo = (id, newText) => {
-    const updatedTodos = todos.map((todo) =>
-      todo.id === id ? { ...todo, text: newText } : todo
-    );
-    setTodos(updatedTodos);
-  };
-
-  const deleteTodo = (id) => {
-    const updatedTodos = todos.filter((todo) => todo.id !== id);
-    setTodos(updatedTodos);
-  };
-
+function App() {
+  // TODO: Add the necessary functionality such as hooks here
   return (
-    <div className="todo-list">
+    <div className="App">
       <h1>To-Do List</h1>
-      <div className="input-container">
-        <input
-          type="text"
-          value={newTodo}
-          onChange={(e) => setNewTodo(e.target.value)}
-          placeholder="Enter your todo"
-        />
-        <button onClick={addTodo}>Add</button>
-      </div>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            <input
-              type="text"
-              value={todo.text}
-              onChange={(e) => updateTodo(todo.id, e.target.value)}
-            />
-            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+      {/* TODO: Add the To-Do list here */}
     </div>
   );
-};
+}
 
-export default TodoList;
-
+export default App;
