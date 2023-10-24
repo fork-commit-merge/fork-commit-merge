afc6ffc8b931f0169f39d16b6a9b7857f7db43d8
GitHub Action <action@github.com>
2023-10-24 19:33:52 +0000

    Revert "Merge pull request #1210 from amandeep-singh-parihar/main"
    This reverts commit d7940606ecd2d68b55088168e5718304bccda07a, reversing
    changes made to 30d30852e17ce4c0ba016ef6f314295553284d6f.

--- tasks/ruby/easy/index.rb
+++ tasks/ruby/easy/index.rb
@@ -2,9 +2,4 @@
 
 # TODO: Implement a function named sum_of_two_numbers that calculates the sum of two numbers
 
-def sum_of_two_numbers(a, b)
-    sum = a + b
-    return sum
-  end
-
 puts(sum_of_two_numbers(2,4))
