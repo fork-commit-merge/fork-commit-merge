3e45baa9d09c5c6802cd1730ddd2ea3a3454ba07
GitHub Action <action@github.com>
2023-10-24 14:34:31 +0000

    Revert "Merge pull request #1191 from Shubhamkashyap1601/main"
    This reverts commit 272cee6978ee5793d6144510d0cd104f1cedb370, reversing
    changes made to e6b7efeaec67fc2ca1c5646d01b237f4ffbc797f.

--- tasks/html/hard/clock/index.js
+++ tasks/html/hard/clock/index.js
@@ -1,30 +1,12 @@
 // Implement the setClock function to update the positions of the hour, minute, and second hands on the clock based on the current time.
-function updateClock() {
-    console.log("Running");
-    
-    const now = new Date();
-    const hours = now.getHours() % 12;
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
+setInterval(() => {
+  // TODO: Calculate the current time and update the clock hands' positions here.
+  // You need to calculate the rotation ratios and update the hand rotations.
+}, 1000);
 
-    const hourHand = document.querySelector('.hour-hand');
-    const minuteHand = document.querySelector('.minute-hand');
-    const secondHand = document.querySelector('.second-hand');
-
-    const hourRotation = (hours * 30) + (0.5 * minutes);
-    const minuteRotation = (minutes * 6) + (0.1 * seconds);
-    const secondRotation = seconds * 6;
-
-    console.log(hourRotation, minuteRotation, secondRotation);
-
-    hourHand.style.transform = `translateX(-50%) translateY(0) rotate(${hourRotation}deg)`;
-    minuteHand.style.transform = `translateX(-50%) translateY(0) rotate(${minuteRotation}deg)`;
-    secondHand.style.transform = `translateX(-50%) translateY(0) rotate(${secondRotation}deg)`;
-    
-}
-
-
-setInterval(updateClock, 1);
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
 
 // Create setRotation function to rotate a DOM element around its center point. This function will be used to rotate the hour, minute, and second hands on the clock.
 // TODO: Implement the setRotation function to update the rotation of a DOM element based on the provided rotationRatio.
