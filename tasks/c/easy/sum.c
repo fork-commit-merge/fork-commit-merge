b9bc38df881e4c833f6097c9cb846ce5b738d22a
unknown <dineshgarje102@gmail.com>
2023-10-24 23:45:28 +0530

    Create a function to sum two numbers in c

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,11 +1,20 @@
-// C - Easy
+    // C - Easy
 
-#include <stdio.h>
+    #include <stdio.h>
 
-// TODO: Implement the sum function
+    // TODO: Implement the sum function
+    int sum(int a, int b)
+    {
+        return a + b;
+    }
 
-int main() {
-    // TODO: Implement the main function
-
-    return 0;
-}
+    int main()
+    {
+        // TODO: Implement the main function
+        int num1, num2, result;
+        printf("Enter two numbers: ");
+        scanf("%d %d", &num1, &num2);
+        result = sum(num1, num2);
+        printf("The sum is: %d\n", result);
+        return 0;
+    }
