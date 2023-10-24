08aa191350d98de1f273f5c4574593dfdc3ff124
GitHub Action <action@github.com>
2023-10-24 18:51:07 +0000

    Revert "Merge pull request #1208 from shraddha761/patch-2"
    This reverts commit b4c763ca0123cc1cd007a4ca0a9fd3dfff235ae8, reversing
    changes made to e3dccffcb2aebe8a5321af0544897f8215266aeb.

--- tasks/swift/easy/fibonacci.swift
+++ tasks/swift/easy/fibonacci.swift
@@ -4,27 +4,6 @@ import Foundation
 
 func fibonacci(n: Int) -> Int {
     // TODO: Implement the fibonacci sequence function
-    if n == 0 {
-        return 0
-    } else if n == 1 {
-        return 1
-    } else {
-        var a = 0
-        var b = 1
-        var result = 0
-        for _ in 2...n {
-            result = a + b
-            a = b
-            b = result
-        }
-        return result
-    }
-}
-
-// Example usage
-let fibonacciNumber = fibonacci(n: 10) // This will calculate the 10th Fibonacci number, which is 55
-print("The 10th Fibonacci number is \(fibonacciNumber)")
-
 }
 
 // Test cases (Don't touch!)
