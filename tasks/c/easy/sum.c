d7f3360488679282c07ab09f61d063353cabd25c
GitHub Action <action@github.com>
2023-10-24 18:26:43 +0000

    Revert "Merge pull request #1206 from DicoderRG/fun-sum-two-num-in-c"
    This reverts commit 6b09fceec473407b932ebcbd629cadc1a90f5845, reversing
    changes made to f5e08ba924eff37f3d6bd6bec4d032e79d71fd66.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,20 +1,11 @@
-    // C - Easy
+// C - Easy
 
-    #include <stdio.h>
+#include <stdio.h>
 
-    // TODO: Implement the sum function
-    int sum(int a, int b)
-    {
-        return a + b;
-    }
+// TODO: Implement the sum function
 
-    int main()
-    {
-        // TODO: Implement the main function
-        int num1, num2, result;
-        printf("Enter two numbers: ");
-        scanf("%d %d", &num1, &num2);
-        result = sum(num1, num2);
-        printf("The sum is: %d\n", result);
-        return 0;
-    }
+int main() {
+    // TODO: Implement the main function
+
+    return 0;
+}
