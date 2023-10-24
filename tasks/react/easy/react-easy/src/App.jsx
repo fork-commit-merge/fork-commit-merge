7aef5b7d397e44f055ce81eb69727898f1139e4b
GitHub Action <action@github.com>
2023-10-24 20:02:43 +0000

    Revert "Merge pull request #1212 from shraddha761/patch-4"
    This reverts commit bcd46c0247a85875b6273b1fa599d7107368ad90, reversing
    changes made to 289331de0489de625abc8bcd962b8fae198d3498.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -1,23 +1,14 @@
 // React/Vite - Easy
-import React from "react";
-import "./index.css"; 
+import "./index.css";
 
-const getRandomImageUrl = () => {
-  return "https://loremflickr.com/320/240";
-};
+function App() {
+    return (
+        <>
+            <div className="center">
+                {/* TODO: Implement the card component */}
+            </div>
+        </>
+    );
+}
 
-const Card = () => {
-  return (
-    <div className="center">
-      <h1>React/Vite - Easy</h1>
-      <div className="card">
-        <img src={getRandomImageUrl()} alt="Random" />
-        <h2>Card Title</h2>
-        <p>Card Description</p>
-      </div>
-    </div>
-  );
-};
-
-export default Card;
-
+export default App;
