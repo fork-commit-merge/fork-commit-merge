26f68c320b745b314c754296e4e28ee8a6fae378
shraddha761 <106100728+shraddha761@users.noreply.github.com>
2023-10-25 01:13:04 +0530

    Fork, Commit, Merge - Easy Issue (React) #962

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -1,14 +1,23 @@
 // React/Vite - Easy
-import "./index.css";
+import React from "react";
+import "./index.css"; 
 
-function App() {
-    return (
-        <>
-            <div className="center">
-                {/* TODO: Implement the card component */}
-            </div>
-        </>
-    );
-}
+const getRandomImageUrl = () => {
+  return "https://loremflickr.com/320/240";
+};
 
-export default App;
+const Card = () => {
+  return (
+    <div className="center">
+      <h1>React/Vite - Easy</h1>
+      <div className="card">
+        <img src={getRandomImageUrl()} alt="Random" />
+        <h2>Card Title</h2>
+        <p>Card Description</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
+
