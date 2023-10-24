f58f7e85b3fce30c539e22bd4ac91c147b10dc2b
amandeep singh parihar <amandeepparihar24@gmail.com>
2023-10-25 00:26:05 +0530

    Implemented a function named sum_of_two_numbers in ruby

--- tasks/ruby/easy/index.rb
+++ tasks/ruby/easy/index.rb
@@ -2,4 +2,9 @@
 
 # TODO: Implement a function named sum_of_two_numbers that calculates the sum of two numbers
 
+def sum_of_two_numbers(a, b)
+    sum = a + b
+    return sum
+  end
+
 puts(sum_of_two_numbers(2,4))
