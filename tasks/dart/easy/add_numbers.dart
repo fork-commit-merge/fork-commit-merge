f5e08ba924eff37f3d6bd6bec4d032e79d71fd66
GitHub Action <action@github.com>
2023-10-24 18:14:51 +0000

    Revert "Merge pull request #1205 from Killer2OP/patch-5"
    This reverts commit ed3bb71ad5e5df549881132874198d90fa1aff6f, reversing
    changes made to 3a7d1f59b3ac073d09cfa9e9832e5060b87787b2.

--- tasks/dart/easy/add_numbers.dart
+++ tasks/dart/easy/add_numbers.dart
@@ -1,20 +1,6 @@
-import 'dart:io';
-
-// Function to add two numbers
-int addNumbers(int a, int b) {
-  return a + b;
-}
-
 void main() {
-  // Read the first number from the user
-  stdout.write('Enter the first number: ');
-  int firstNumber = int.parse(stdin.readLineSync()!);
-
-  // Read the second number from the user
-  stdout.write('Enter the second number: ');
-  int secondNumber = int.parse(stdin.readLineSync()!);
-
-  // Call the addNumbers function and print the result
-  int sum = addNumbers(firstNumber, secondNumber);
-  print('The sum of $firstNumber and $secondNumber is: $sum');
+  // int sum = addNumbers(5, 10);
+  // print('The sum is: $sum');
 }
+
+// TODO: Implement addNumbers function that returns the sum of two numbers
