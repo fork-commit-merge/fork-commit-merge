94a28c7b6e99674284aa44b472139159cb54bfbf
paman6415 <114017588+paman6415@users.noreply.github.com>
2023-10-24 17:39:19 +0530

    Update index.php

--- tasks/php/easy/index.php
+++ tasks/php/easy/index.php
@@ -1,8 +1,23 @@
-<?php
-
-// PHP - Easy 1
-
-$hello = "Hello";
-$world = "World!";
-
-// TODO: Implement rest of the string concatenation program
+<?php   
+// PHP code to get the Fibonacci series 
+// Recursive function for fibonacci series. 
+function Fibonacci($number){ 
+      
+    // if and else if to generate first two numbers 
+    if ($number == 0) 
+        return 0;     
+    else if ($number == 1) 
+        return 1;     
+      
+    // Recursive Call to get the upcoming numbers 
+    else
+        return (Fibonacci($number-1) +  
+                Fibonacci($number-2)); 
+} 
+  
+// Driver Code 
+$number = 10; 
+for ($counter = 0; $counter < $number; $counter++){   
+    echo Fibonacci($counter),' '; 
+} 
+?> 
