923b111a7809978d161ca9e6675b207b90fd7b9a
GitHub Action <action@github.com>
2023-10-25 15:21:05 +0000

    Revert "Merge pull request #1247 from predator3813/kami"
    This reverts commit 3b26efc6daae9ee2eed1173380ce5f70cef29ccd, reversing
    changes made to 837db64a422a8272c7c4ce71ba19a1a2725ad229.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -3,14 +3,9 @@
 #include <stdio.h>
 
 // TODO: Implement the sum function
-sum (int a, int b) {
-    return a + b;
-}
+
 int main() {
     // TODO: Implement the main function
-    int a, b;
-    scanf("%d %d", &a, &b);
-    printf("%d\n", sum(a, b));
 
     return 0;
 }
