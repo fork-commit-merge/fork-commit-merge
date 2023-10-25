29e676d3c36cbce14e57433f61e6cbb139418e62
GitHub Action <action@github.com>
2023-10-25 15:24:10 +0000

    Revert "Merge pull request #1249 from Abhishekgupta204/abhi2"
    This reverts commit 8c7032e815e44d8627769e026ba8d757e7b22f17, reversing
    changes made to 316f351e2934eec0929b2b4498fb9ea42372436c.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -2,12 +2,3 @@
 
 // TODO: Write a function that returns the sum of two numbers
 
-// store input numbers
-const num1 = parseInt(prompt('Enter the first number '));
-const num2 = parseInt(prompt('Enter the second number '));
-
-//add two numbers
-const sum = num1 + num2;
-
-// display the sum
-console.log(`The sum of ${num1} and ${num2} is ${sum}`);
