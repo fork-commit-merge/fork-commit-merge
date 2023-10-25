74fe416926e1832827933bf66ac47caac38c4396
predator3813 <kanishkreddy3813@gmail.com>
2023-10-25 19:22:19 +0530

    Fix Issue

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -3,9 +3,17 @@
 #include <stdio.h>
 
 // TODO: Implement the sum function
-
+int sum(int a, int b) {
+    int c;
+    c=a+b;
+    return c;
+}
 int main() {
     // TODO: Implement the main function
+     int a,b;
+        scanf("%d %d",&a,&b);
+        printf("%d",sum(a,b));
+        
 
     return 0;
 }
