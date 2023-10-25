57b72a4c4bd7e9e0c6ac833e3d734e5d4808fabe
amandeep singh parihar <amandeepparihar24@gmail.com>
2023-10-25 14:12:12 +0530

    Implemented the fibonacci sequence in Rust

--- tasks/rust/easy/fibonacci.rs
+++ tasks/rust/easy/fibonacci.rs
@@ -3,3 +3,36 @@
 use std::io;
 
 // TODO: Implement fibonacci sequence in Rust
+fn main() {
+    println!("Enter the number of Fibonacci numbers to generate: ");
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let n: u32 = input.trim().parse().expect("Please enter a valid number");
+
+    for i in 0..n {
+        let fib_number = fibonacci(i);
+        println!("Fibonacci({}) = {}", i, fib_number);
+    }
+}
+
+fn fibonacci(n: u32) -> u32 {
+    if n == 0 {
+        return 0;
+    } else if n == 1 {
+        return 1;
+    } else {
+        let mut prev = 0;
+        let mut current = 1;
+        for _ in 2..=n {
+            let next = prev + current;
+            prev = current;
+            current = next;
+        }
+        return current;
+    }
+}
