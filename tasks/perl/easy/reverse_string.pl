7a7a5aed02108390ad16f7ed640322fe99db27f3
GitHub Action <action@github.com>
2023-10-25 16:04:16 +0000

    Revert "Merge pull request #1261 from amandeep-singh-parihar/main"
    This reverts commit 27fedddb1e072bced5dc36c8e573fc5f9c2d35a8, reversing
    changes made to b42c0d7ac6a327b74e806bb04f6aadd577eff53c.

--- tasks/perl/easy/reverse_string.pl
+++ tasks/perl/easy/reverse_string.pl
@@ -5,12 +5,3 @@ use strict;
 use warnings;
 
 # TODO: Write a Perl script that accepts a string and prints it in reversed order
-print "Enter a string: ";
-my $input = <STDIN>;
-chomp($input); # Remove the newline character
-
-# Reverse the string
-my $reversed = reverse $input;
-
-# Print the reversed string
-print "Reversed string: $reversed\n";
