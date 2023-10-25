316f351e2934eec0929b2b4498fb9ea42372436c
GitHub Action <action@github.com>
2023-10-25 15:23:10 +0000

    Revert "Merge pull request #1248 from Ruhi14/c-1"
    This reverts commit 7a30b6f0044595e705998307b9934bfba6ff7813, reversing
    changes made to 923b111a7809978d161ca9e6675b207b90fd7b9a.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,17 +1,11 @@
+// C - Easy
+
 #include <stdio.h>
 
-int sumNumbers(int numbers[], int size) {
-    int sum = 0;
-    for (int i = 0; i < size; ++i) {
-        sum += numbers[i];
-    }
-    return sum;
-}
+// TODO: Implement the sum function
 
 int main() {
-    int numbers[] = {1, 2, 3, 4, 5};
-    int size = sizeof(numbers) / sizeof(numbers[0]);
-    printf("Sum: %d\n", sumNumbers(numbers, size));
+    // TODO: Implement the main function
+
     return 0;
 }
-
