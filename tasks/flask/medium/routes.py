d832320fecb6e74e6e4799e59c4cd0af4c311cd3
Abhishek Mallick <mallickabhishek97@gmail.com>
2023-10-25 21:07:44 +0530

    Flask config setup --staged

---
+++ tasks/flask/medium/routes.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# two decorators, same function
+@app.route('/')
+@app.route('/index.html')
+def index():
+    return render_template('index.html', the_title='Tiger Home Page')
+
+@app.route('/symbol.html')
+def symbol():
+    return render_template('symbol.html', the_title='Tiger As Symbol')
+
+@app.route('/myth.html')
+def myth():
+    return render_template('myth.html', the_title='Tiger in Myth and Legend')
+
+if __name__ == '__main__':
+    app.run(debug=True)
