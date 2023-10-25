1c11c170372638de2016ea9ffd8cc12cbc47070c
Vikash <114097162+ivikashgorai@users.noreply.github.com>
2023-10-25 12:57:55 +0530

    Update Frequency.java

--- tasks/java/hard/Frequency.java
+++ tasks/java/hard/Frequency.java
@@ -2,6 +2,25 @@
 
 package tasks.java.hard;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Frequency {
-    // TODO: Frequency Count - Given an array of integers, write a Java program to find the frequency of each element using a Map
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 2, 3, 5, 6, 4, 7, 7};
+
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+
+        for (int num : arr) {
+            if (frequencyMap.containsKey(num)) {
+                frequencyMap.put(num, frequencyMap.get(num) + 1);
+            } else {
+                frequencyMap.put(num, 1);
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            System.out.println("Element: " + entry.getKey() + ", Frequency: " + entry.getValue());
+        }
+    }
 }
