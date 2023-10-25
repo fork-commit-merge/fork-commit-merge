b42c0d7ac6a327b74e806bb04f6aadd577eff53c
GitHub Action <action@github.com>
2023-10-25 15:58:09 +0000

    Revert "Merge pull request #1257 from Adidem23/main"
    This reverts commit 3f86208f66b14d07d46b3593ad6fe6aa803e48c4, reversing
    changes made to a2237e7781c179bc84f23811ec074f50bb91ad95.

--- tasks/solidity/easy/my_simple_storage/contracts/SimpleStorage.sol
+++ tasks/solidity/easy/my_simple_storage/contracts/SimpleStorage.sol
@@ -1,14 +1,10 @@
+// Solidity - Easy
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract SimpleStorage {
-    uint256 number;
+    //TODO: Implement function to set the number
 
-    function set(uint256 _numtoset) public {
-        number=_numtoset;
-    }
-
-    function get() public view returns (uint256) {
-        return number;
-    }
+    //TODO: Implement function to get the number
 }
