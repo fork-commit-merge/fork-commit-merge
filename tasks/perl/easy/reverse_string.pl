cb4d51ae367617e3c71f16ffdcceb2e677448129
amandeep singh parihar <amandeepparihar24@gmail.com>
2023-10-25 21:26:11 +0530

    added a perl script that accepts a tring and reverse it

--- tasks/perl/easy/reverse_string.pl
+++ tasks/perl/easy/reverse_string.pl
@@ -5,3 +5,12 @@ use strict;
 use warnings;
 
 # TODO: Write a Perl script that accepts a string and prints it in reversed order
+print "Enter a string: ";
+my $input = <STDIN>;
+chomp($input); # Remove the newline character
+
+# Reverse the string
+my $reversed = reverse $input;
+
+# Print the reversed string
+print "Reversed string: $reversed\n";
