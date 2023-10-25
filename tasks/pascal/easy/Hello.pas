7881a678c929898083406a16bb41afddabea443c
GitHub Action <action@github.com>
2023-10-25 13:47:52 +0000

    Revert "Merge pull request #1239 from sivangbagri/dev"
    This reverts commit c4ee6dc1fbd88636ae5f8e5540b354426162f615, reversing
    changes made to d9f321ca0f6d7fa2146dc04d576d8870cc89be81.

--- tasks/pascal/easy/Hello.pas
+++ tasks/pascal/easy/Hello.pas
@@ -1,9 +1,3 @@
 // Pascal - Easy
 
 // TODO: Create a simple Pascal program that prints "Hello, Pascal!" to the console when executed
-
-
-program HelloWorld;
-begin
-  writeln('Hello, Pascal!');
-end.
