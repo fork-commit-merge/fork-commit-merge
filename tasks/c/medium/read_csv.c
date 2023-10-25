20f9b6aac52468cadbfa64a6cafd0dda88a15465
GitHub Action <action@github.com>
2023-10-25 07:13:13 +0000

    Revert "Merge pull request #1228 from ivikashgorai/patch-4"
    This reverts commit e3ef73c27eb1015ed5ed04d317098e0efacc5c2f, reversing
    changes made to 9e4f18443b9452eafb8bcbaeea725aa57bba3395.

--- tasks/c/medium/read_csv.c
+++ tasks/c/medium/read_csv.c
@@ -1,51 +1,10 @@
+// C - Medium
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void readCSV();
-void writeCSV();
 
 int main() {
-    writeCSV();
-
-    readCSV();
-
+    // TODO: Implement the program that reads and displays the contents of data.csv file
     return 0;
 }
-void readCSV() {
-    FILE *file = fopen("data.csv", "r");
-    if (file == NULL) {
-        perror("Error opening the file");
-        return;
-    }
-
-    printf("Name\tAge\n");
-
-    char line[100];
-    while (fgets(line, sizeof(line), file)) {
-        char *name = strtok(line, ",");
-        char *age = strtok(NULL, ",");
-        
-        if (name != NULL && age != NULL) {
-            printf("%s\t%s\n", name, age);
-        }
-    }
-
-    fclose(file);
-}
-
-void writeCSV() {
-    FILE *file = fopen("data.csv", "w");
-    if (file == NULL) {
-        perror("Error opening the file");
-        return;
-    }
-
-    fprintf(file, "Name,Age\n");
-
-    fprintf(file, "Alice,30\n");
-    fprintf(file, "Bob,40\n");
-    fprintf(file, "Charlie,50\n");
-
-    fclose(file);
-}
-
