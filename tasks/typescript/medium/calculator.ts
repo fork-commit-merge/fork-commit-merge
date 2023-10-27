2b4895606d19a20e7e570502c74078aac194712d
Aloisi Matteo <99matteoaloisi@gmail.com>
2023-10-27 16:23:25 +0200

    typescript/calculator added

--- tasks/typescript/medium/calculator.ts
+++ tasks/typescript/medium/calculator.ts
@@ -5,9 +5,7 @@ namespace Calculator {
     export function add(a: number, b: number): number { return a + b }
     export function subtract(a: number, b: number): number { return a - b }
     export function multiply(a: number, b: number): number { return a * b }
-    export function divide(a: number, b: number): number | null {
-        return b == 0 ? null : a / b
-     }
+    export function divide(a: number, b: number): number | null { return b == 0 ? null : a / b }
 }
 
 // Test cases
