2dee3e2070607203910c5e7ab637d42596d7ba7f
GitHub Action <action@github.com>
2023-10-27 14:35:08 +0000

    Revert "Merge pull request #1285 from AloisiMatteo/typescript_calculator_issue"
    This reverts commit ba2470cd6e51f5d67b96f8e0d142acd3b06a87ff, reversing
    changes made to c754728e6c4a3c2054c1817698b20c62e4aa8986.

--- tasks/typescript/medium/calculator.ts
+++ tasks/typescript/medium/calculator.ts
@@ -1,11 +1,7 @@
 // TypeScript - Medium
 
 namespace Calculator {
-    
-    export function add(a: number, b: number): number { return a + b }
-    export function subtract(a: number, b: number): number { return a - b }
-    export function multiply(a: number, b: number): number { return a * b }
-    export function divide(a: number, b: number): number | null { return b == 0 ? null : a / b }
+    // TODO: Implement a Simple TypeScript Calculator
 }
 
 // Test cases
