c77e0dca5bfd447e57e69af4bdf22b0aafe8b233
Yachika9925 <147185379+Yachika9925@users.noreply.github.com>
2023-10-28 00:08:28 +0530

    Update array.php

--- tasks/php/easy/array.php
+++ tasks/php/easy/array.php
@@ -1,7 +1,12 @@
 <?php
-
-// PHP - Easy 3
-
 $data = ['Pizza', 'Chicken', 'Beef', 'Rice', 'Bread'];
 
-// TODO: Display data "Beef" from the array
+// Find and display "Beef" from the array
+foreach ($data as $item) {
+    if ($item === 'Beef') {
+        echo $item;
+        break; // Exit the loop once "Beef" is found
+    }
+}
+?>
+
