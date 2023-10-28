ae264e10f8e53ab91199f157e48e4468ea3c3762
reximitis <reximitis@gmail.com>
2023-10-28 18:15:39 +0530

    File Handler task #1039

--- tasks/c++/medium/FileHandler.h
+++ tasks/c++/medium/FileHandler.h
@@ -1,9 +1,10 @@
-// C++ - Medium
-
 #pragma once
 #include <string>
 
 class FileHandler {
 public:
-    // TODO: Implement the FileHandler class
+	static int 		   	WriteFile	(const std::string& filePath, const std::string& text);
+	static int 		   	AppendFile	(const std::string& filePath, const std::string& text);
+	static std::string 	ReadFile	(const std::string& filePath);
+	static bool 		FileExists	(const std::string& filePath);
 };
