a0053e6e32f27e135060c968c0496462c2c32e43
GitHub Action <action@github.com>
2023-10-28 15:27:22 +0000

    Revert "Merge pull request #1299 from Ruhi14/java-2"
    This reverts commit d1834e25e575bea6bdfa34a20872c6b69f22c14d, reversing
    changes made to 633a9804d359675cad6309674da7569d177cff23.

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -4,26 +4,6 @@ package tasks.java.medium;
 
 public class Calculator {
     // TODO: Implement the Calculator class
-    public int subtract(int num1, int num2) {
-        return num1 - num2;
-    }
-
-    // Method to multiply two numbers
-    public int multiply(int num1, int num2) {
-        return num1 * num2;
-    }
-
-    // Method to divide two numbers (returns a double to handle division with decimals)
-    public double divide(int num1, int num2) {
-        if (num2 != 0) {
-            // Avoid division by zero
-            return (double) num1 / num2;
-        } else {
-            // Handle division by zero error
-            System.out.println("Error: Division by zero!");
-            return Double.NaN; // NaN (Not a Number) represents an undefined or unrepresentable value
-        }
-    }
 
     public static void main(String[] args) {
         //! Dont't change the tests in the main method!
@@ -35,4 +15,3 @@ public class Calculator {
         System.out.println("Division: " + calc.divide(5, 3));
     }
 }
-
