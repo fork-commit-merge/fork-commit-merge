dc0429222b30d164ae3f564d968e09383363354f
GitHub Action <action@github.com>
2023-10-28 15:44:18 +0000

    Revert "Merge pull request #1304 from rexmitis/fix-cpp-hard"
    This reverts commit 5dc62768a5f967209081df77beca464b133d5a78, reversing
    changes made to e778ad076655d40d37d47099317c99dd45fdb4f7.

--- tasks/c++/hard/mystring.cpp
+++ tasks/c++/hard/mystring.cpp
@@ -5,82 +5,51 @@
 #include <iostream>
 
 // Default constructor
-MyString::MyString() : cstr(nullptr), len(0), size(0) {}
+MyString::MyString() : str(nullptr), len(0) {}
 
 // Parameterized constructor
 MyString::MyString(const char* s) {
-	len = 0;
-	while(s[len]) ++len;
-	size = len+1;
-	cstr = new char[size];
-	for(unsigned int i=0; i<size; ++i) cstr[i] = s[i];
+    // TODO: Implement this
 }
 
 // Copy constructor
 MyString::MyString(const MyString& other) {
-    len = other.len; size = len+1;
-	cstr = new char[size];
-	for(unsigned int i=0; i<size; ++i) cstr[i] = other.cstr[i];
+    // TODO: Implement this
 }
 
 // Destructor
 MyString::~MyString() {
-    delete[] cstr;
+    delete[] str;
 }
 
-unsigned int MyString::length() const {
-    return len;
+int MyString::length() const {
+    // TODO: Implement this
 }
 
 const char* MyString::c_str() const {
-    return cstr;
+    // TODO: Implement this
 }
 
 void MyString::append(const MyString& other) {
-	if(size < len+other.len+1) {
-		size = 1; while(size<len+other.len) size <<= 1;
-		char* temp = new char[++size];
-		for(unsigned int i=0; i<len; ++i) temp[i] = cstr[i];
-		delete[] cstr;
-		cstr = temp;
-	}
-	for(unsigned int i=0; i<other.len+1; ++i) cstr[i+len] = other.cstr[i];
-	len += other.len;
+    // TODO: Implement this
 }
 
 int MyString::compare(const MyString& other) const {
-    int res = 0;
-	for(unsigned int i=0; i<len+1; ++i) {
-		if(cstr[i] < other.cstr[i]) { res = -1; break; }
-		else if(cstr[i] > other.cstr[i]) { res = 1; break; }
-	}
-	return res;
+    // TODO: Implement this
 }
 
-void MyString::operator=(const MyString& other) {
-    len = other.len; size = len+1;
-	delete[] cstr;
-	cstr = new char[size];
-	for(unsigned int i=0; i<size; ++i) cstr[i] = other.cstr[i];
+MyString& MyString::operator=(const MyString& other) {
+    // TODO: Implement this
 }
 
 MyString MyString::operator+(const MyString& other) const {
-    MyString res;
-	res.len = len + other.len;
-	res.size = res.len + 1;
-	res.cstr = new char[res.size];
-	for(unsigned int i=0; i<len; ++i) res.cstr[i] = cstr[i];
-	for(unsigned int i=0; i<other.len+1; ++i) res.cstr[len+i] = other.cstr[i];
-	return res;
+    // TODO: Implement this
 }
 
 bool MyString::operator==(const MyString& other) const {
-	if(len != other.len) return 0;
-    for(unsigned int i=0; i<len; ++i) if(cstr[i] != other.cstr[i]) return 0;
-	return 1;
+    // TODO: Implement this
 }
 
 std::ostream& operator<<(std::ostream& os, const MyString& s) {
-    os << s.c_str();
-	return os;
+    // TODO: Implement this
 }
--- tasks/c++/hard/mystring.h
+++ tasks/c++/hard/mystring.h
@@ -5,27 +5,8 @@
 #include <iostream>
 
 class MyString {
-public:
-	MyString();
-	~MyString();
-	MyString(const char* data);
-	MyString(const MyString& str);
-	
-	unsigned int length() const;
-	const char* c_str() const;
-	void append(const MyString& str);
-	int compare(const MyString& str) const;
-	
-	void	 operator=	(const MyString& other);
-	MyString operator+  (const MyString& str) const;
-	bool 	 operator== (const MyString& str) const;
-	
-private:
-	char* cstr;
-	unsigned int len, size;
+    // TODO: Implement MyString class
 };
-
-std::ostream& operator<<(std::ostream& os, const MyString& s);
 
 #endif
 
