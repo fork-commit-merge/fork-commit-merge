9f844d46b6189d9fed5f572c95ff9ae88ac1b85e
DawmosTomie <88043739+DawmosTomie@users.noreply.github.com>
2023-10-29 15:06:59 +0800

    'python'

--- tasks/python/hard/decision_tree.py
+++ tasks/python/hard/decision_tree.py
@@ -1,5 +1,141 @@
-# Python - Hard
-
 import numpy as np
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
 
-# TODO: Implement the Decision Tree Classifier using NumPy
+class Node:
+    def __init__(self, predicted_class):
+        self.predicted_class = predicted_class
+        self.feature_index = 0
+        self.threshold = 0
+        self.left = None
+        self.right = None
+
+class DecisionTree:
+    def __init__(self, max_depth=None):
+        self.max_depth = max_depth
+
+    def fit(self, X, y):
+        self.n_classes_ = len(set(y))
+        self.n_features_ = X.shape[1]
+        self.tree_ = self._grow_tree(X, y)
+
+    def predict(self, X):
+        return [self._predict(inputs) for inputs in X]
+
+    def _best_split(self, X, y):
+        m = y.size
+        if m <= 1:
+            return None, None
+        num_parent = [np.sum(y == c) for c in range(self.n_classes_)]
+        best_gini = 1.0 - sum((n / m) ** 2 for n in num_parent)
+        best_idx, best_thr = None, None
+        for idx in range(self.n_features_):
+            thresholds, classes = zip(*sorted(zip(X[:, idx], y)))
+            num_left = [0] * self.n_classes_
+            num_right = num_parent.copy()
+            for i in range(1, m):
+                c = classes[i - 1]
+                num_left[c] += 1
+                num_right[c] -= 1
+                gini_left = 1.0 - sum(
+                    (num_left[x] / i) ** 2 for x in range(self.n_classes_)
+                )
+                gini_right = 1.0 - sum(
+                    (num_right[x] / (m - i)) ** 2 for x in range(self.n_classes_)
+                )
+                gini = (i * gini_left + (m - i) * gini_right) / m
+                if thresholds[i] == thresholds[i - 1]:
+                    continue
+                if gini < best_gini:
+                    best_gini = gini
+                    best_idx = idx
+                    best_thr = (thresholds[i] + thresholds[i - 1]) / 2
+        return best_idx, best_thr
+
+    def _grow_tree(self, X, y, depth=0):
+        num_samples_per_class = [np.sum(y == i) for i in range(self.n_classes_)]
+        predicted_class = np.argmax(num_samples_per_class)
+        node = Node(predicted_class=predicted_class)
+        if depth < self.max_depth:
+            idx, thr = self._best_split(X, y)
+            if idx is not None:
+                indices_left = X[:, idx] < thr
+                X_left, y_left = X[indices_left], y[indices_left]
+                X_right, y_right = X[~indices_left], y[~indices_left]
+                node.feature_index = idx
+                node.threshold = thr
+                node.left = self._grow_tree(X_left, y_left, depth + 1)
+                node.right = self._grow_tree(X_right, y_right, depth + 1)
+        return node
+
+    def _predict(self, inputs):
+        node = self.tree_
+        while node.left:
+            if inputs[node.feature_index] < node.threshold:
+                node = node.left
+            else:
+                node = node.right
+        return node.predicted_class
+
+    def visualize_tree(self, node=None, indent=""):
+        """
+        Visualize the decision tree structure.
+
+        Parameters:
+        node (Node, optional): The current node being visualized.
+        indent (str, optional): Indentation for tree visualization.
+
+        Returns:
+        None
+        """
+        if node is None:
+            node = self.tree_
+
+        if node.left is not None:
+            print(indent + f"Feature {node.feature_index} <= {node.threshold}")
+            self.visualize_tree(node.left, indent + "  ")
+            print(indent + f"Feature {node.feature_index} > {node.threshold}")
+            self.visualize_tree(node.right, indent + "  ")
+        else:
+            print(indent + f"Class {node.predicted_class}")
+
+    # Add a method to calculate feature importance
+    def feature_importance(self):
+        """
+        Calculate feature importance for the decision tree.
+
+        Returns:
+        array-like: Feature importance scores, shape (n_features,).
+        """
+        # Implement feature importance calculation here
+
+
+# 加载数据集并划分为训练集和测试集
+iris_data = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(iris_data.data,
+                                                    iris_data.target,
+                                                    test_size=0.2,
+                                                    random_state=42)
+
+# 创建并训练决策树分类器
+clf = DecisionTree(max_depth=10)
+clf.fit(X_train, y_train)
+
+# 可视化决策树
+print("Decision Tree Structure:")
+clf.visualize_tree()
+
+# 进行预测并计算性能指标
+predictions = clf.predict(X_test)
+accuracy = accuracy_score(y_test, predictions)
+precision = precision_score(y_test, predictions, average='macro')
+recall = recall_score(y_test, predictions, average='macro')
+
+print(f'Accuracy: {accuracy}')
+print(f'Precision: {precision}')
+print(f'Recall: {recall}')
+
+# 计算特征重要性
+feature_importance = clf.feature_importance()
+print(f'Feature Importance: {feature_importance}')
