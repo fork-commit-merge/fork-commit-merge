df843d183121ba5cd73b7acbd5c6df3d849725f7
GitHub Action <action@github.com>
2023-10-29 08:02:48 +0000

    Revert "Merge pull request #1319 from R4Rohit23/express_easy_solution"
    This reverts commit 55bdfaad7092938f7102e7a3295ada0e688ebdfd, reversing
    changes made to 3a12dab454747ad818c7c474061816c7f67c5a2d.

--- tasks/express/easy/app.js
+++ tasks/express/easy/app.js
@@ -1,15 +1,10 @@
 // ExpressJS - Easy
 
+const { get } = require("cheerio/lib/api/traversing");
 const express = require("express");
 const app = express();
 const port = 6969;
 
 // TODO: Implement `get` method for / route
-app.get("/", (req, res) => {
-  res.status(200).send("Hello, Express!");
-});
 
 // TODO: Implement `listen` method
-app.listen(port, () => {
-  console.log("Server is running on port 6969");
-});
