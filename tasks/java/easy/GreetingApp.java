8df333d4e08ec5338fc8250fe1524e0f8b41b21c
GitHub Action <action@github.com>
2023-10-29 08:01:17 +0000

    Revert "Merge pull request #1316 from R4Rohit23/java_easy_solution"
    This reverts commit 99d8756d12b892ab9d49e67ef724b9e4f93b8e41, reversing
    changes made to 1b09b2288704ee23222292dfaadf58aafe67ed7a.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,13 +1,8 @@
 // Java - Easy
-import java.util.Scanner;
 
 public class GreetingApp {
 
     public static void main(String[] args) {
         // TODO: Implement a Simple Greeting Application
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = scan.nextLine();
-        System.out.println("Hello " + name + "! Nice to meet you!");
     }
 }
