ea0958e555b56f62ac39196af344a263ea801a6a
Rohit Chaware <80148306+R4Rohit23@users.noreply.github.com>
2023-10-29 12:06:15 +0530

    Solved Java Medium Problem.

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -4,6 +4,18 @@ package tasks.java.medium;
 
 public class Calculator {
     // TODO: Implement the Calculator class
+    public static float add(float num1, float num2) {
+        return num1 + num2;
+    }
+    public static float subtract(float num1, float num2) {
+        return num1 - num2;
+    }
+    public static float multiply(float num1, float num2) {
+        return num1 * num2;
+    }
+    public static float divide(float num1, float num2) {
+        return num1 / num2;
+    }
 
     public static void main(String[] args) {
         //! Dont't change the tests in the main method!
