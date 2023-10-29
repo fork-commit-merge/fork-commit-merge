edde9aa9f035c7c8f2b4f958c25889a1c215f2de
GitHub Action <action@github.com>
2023-10-29 07:56:42 +0000

    Revert "Merge pull request #1314 from DawmosTomie/branch"
    This reverts commit 5d33c5264e0a5cbeebea82fc7ab9bca2d4c524df, reversing
    changes made to 16b57536f97ea25e2c30d9bee50827c730ae452c.

--- tasks/java/hard/Frequency.java
+++ tasks/java/hard/Frequency.java
@@ -1,37 +1,7 @@
+// Java - Hard
+
 package tasks.java.hard;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Frequency {
-    // Function to find the frequency of each element in the array
-    public static Map<Integer, Integer> findElementFrequency(int[] arr) {
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-
-        // Iterate through the array
-        for (int num : arr) {
-            // Check if the element is already in the map
-            if (frequencyMap.containsKey(num)) {
-                // If it is, increment the frequency count
-                int frequency = frequencyMap.get(num);
-                frequencyMap.put(num, frequency + 1);
-            } else {
-                // If not, add it to the map with a frequency of 1
-                frequencyMap.put(num, 1);
-            }
-        }
-
-        return frequencyMap;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 2, 3, 1, 3, 1, 4, 5, 4};
-
-        Map<Integer, Integer> frequencyMap = findElementFrequency(arr);
-
-        // Print the frequency of each element
-        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
-            System.out.println("Element: " + entry.getKey() + ", Frequency: " + entry.getValue());
-        }
-    }
+    // TODO: Frequency Count - Given an array of integers, write a Java program to find the frequency of each element using a Map
 }
