d18c23fac41a701fafb3e4760cc0129b955ae996
GitHub Action <action@github.com>
2023-10-29 16:51:26 +0000

    Revert "Merge pull request #1322 from DevanshKyada27/Temperature-converter"
    This reverts commit 4e334fe4f78571cad82648422eb8821ac5b75528, reversing
    changes made to dbee7ac155d02bb65b42b759d1faa395a3d352bd.

--- tasks/c++/easy/tempConverter.cpp
+++ tasks/c++/easy/tempConverter.cpp
@@ -1,20 +1,3 @@
 // C++ - Easy 1
 
 // TODO:  Create a Basic C++ Program to Convert Temperature from Celsius to Fahrenheit
-
-#include <iostream>
-using namespace std;
-
-//Function for temperature convertion
-void TemperatureConverter(float cel) {
-    float fah;
-    fah = ((9 * cel) / 5) + 32;     //Formula for calculating the fahrenhit temperature
-    cout << "The temperature in Fahrenhit is: " << fah;
-}
-
-int main() {
-    float cel;
-    cout << "Enter the temperature in Celsius: ";
-    cin >> cel;                     //Input temperature from user
-    TemperatureConverter(cel);      //Function call
-}
