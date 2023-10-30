bedd74a1ee638668728fc68a8043528fa3b70716
GitHub Action <action@github.com>
2023-10-30 18:13:22 +0000

    Revert "Merge pull request #1355 from auth-02/sum"
    This reverts commit 0383cc88d95ebcaabc259bde1a3d625deba02e25, reversing
    changes made to 4b7a6b418c5c201a45b8245487d1d04a552bfd7a.

--- tasks/c++/easy/sum.cpp
+++ tasks/c++/easy/sum.cpp
@@ -1,22 +1,3 @@
-#include <iostream>
+// C++ - Easy 2
 
-int main() {
-    // Declare two variables to store the numbers
-    int num1, num2;
-
-    // Prompt the user to enter the first number
-    std::cout << "Enter the first number: ";
-    std::cin >> num1;
-
-    // Prompt the user to enter the second number
-    std::cout << "Enter the second number: ";
-    std::cin >> num2;
-
-    // Add the two numbers and store the result in a variable
-    int sum = num1 + num2;
-
-    // Display the result
-    std::cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << std::endl;
-
-    return 0;
-}
+// TODO: Create a Basic C++ Program to Add Two Numbers
