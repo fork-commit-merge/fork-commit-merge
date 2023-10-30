4d8ea5558cb07400908ae27697903b9b5718284b
GitHub Action <action@github.com>
2023-10-30 12:45:52 +0000

    Revert "Merge pull request #1325 from Gauravtb2253/patch-1"
    This reverts commit 252adc2e8da46bc771620f742bbb00f808818401, reversing
    changes made to 5a18d681a4cd64ec3061f5bd9c73b2f240e785d2.

--- tasks/c++/easy/ReverseString.cpp
+++ tasks/c++/easy/ReverseString.cpp
@@ -1,34 +1,11 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
-std::string reverseString(const string &str) {
-    string reversed = str;
-    int left = 0;
-    int right = reversed.size() - 1;
-
-    while (left < right) {
-        char temp = reversed[left];
-        reversed[left] = reversed[right];
-        reversed[right] = temp;
-
-        left++;
-        right--;
-    }
-
-    return reversed;
+std::string reverseString(const std::string &str) {
+    // TODO: Implement the reverseString function
 }
 
 int main() {
-    string input;
-    cout << "Enter a string: ";
-    cin >> input;
-
-    string reversed = reverseString(input);
-    cout << "Reversed string: " << reversed << endl;
-
-    return 0;
+    // TODO: Implement the main method
 }
-
 
