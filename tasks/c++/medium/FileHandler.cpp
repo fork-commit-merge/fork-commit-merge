9fa7b71dc0acf1e311d47c1345535df70b7e36b6
Gaurav Bomble <114927759+Gauravtb2253@users.noreply.github.com>
2023-10-30 18:44:41 +0530

    Update FileHandler.cpp

--- tasks/c++/medium/FileHandler.cpp
+++ tasks/c++/medium/FileHandler.cpp
@@ -1,14 +1,44 @@
-// C++ - Medium
-
 #include "FileHandler.h"
 #include <fstream>
 #include <sstream>
 
-// TODO: Implement the FileHandler::ReadFile method
+FileHandler::FileHandler(const std::string& filePath) : filePath(filePath) {}
 
-// TODO: Implement the FileHandler::WriteFile method
+std::string FileHandler::ReadFile() {
+    std::ifstream file(filePath);
+    if (file.is_open()) {
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+        file.close();
+        return buffer.str();
+    } else {
+        return "Error: File not found or could not be opened.";
+    }
+}
 
-// TODO: Implement the FileHandler::AppendFile method
+bool FileHandler::WriteFile(const std::string& content) {
+    std::ofstream file(filePath);
+    if (file.is_open()) {
+        file << content;
+        file.close();
+        return true;
+    } else {
+        return false;
+    }
+}
 
-// TODO: Implement the FileHandler::FileExists method
+bool FileHandler::AppendFile(const std::string& content) {
+    std::ofstream file(filePath, std::ios::app);
+    if (file.is_open()) {
+        file << content;
+        file.close();
+        return true;
+    } else {
+        return false;
+    }
+}
 
+bool FileHandler::FileExists() {
+    std::ifstream file(filePath);
+    return file.good();
+}
