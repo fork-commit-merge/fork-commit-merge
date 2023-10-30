be2802aef4fb4075074beeb2fb6a1b57348850b5
GitHub Action <action@github.com>
2023-10-30 19:00:44 +0000

    Revert "Merge pull request #1357 from auth-02/hard"
    This reverts commit 0abd61dc5af416c162084af07fec599d492fc999, reversing
    changes made to 7e29d569e52a9d7acd2aac8d102bf575010a781d.

--- tasks/c++/hard/mystring.cpp
+++ tasks/c++/hard/mystring.cpp
@@ -1,151 +1,55 @@
-// // C++ - Hard
-
-// #include "mystring.h"
-// #include <cstring>
-// #include <iostream>
-
-// // Default constructor
-// MyString::MyString() : str(nullptr), len(0) {}
-
-// // Parameterized constructor
-// MyString::MyString(const char* s) {
-//     // TODO: Implement this
-// }
-
-// // Copy constructor
-// MyString::MyString(const MyString& other) {
-//     // TODO: Implement this
-// }
-
-// // Destructor
-// MyString::~MyString() {
-//     delete[] str;
-// }
-
-// int MyString::length() const {
-//     // TODO: Implement this
-// }
-
-// const char* MyString::c_str() const {
-//     // TODO: Implement this
-// }
-
-// void MyString::append(const MyString& other) {
-//     // TODO: Implement this
-// }
-
-// int MyString::compare(const MyString& other) const {
-//     // TODO: Implement this
-// }
-
-// MyString& MyString::operator=(const MyString& other) {
-//     // TODO: Implement this
-// }
-
-// MyString MyString::operator+(const MyString& other) const {
-//     // TODO: Implement this
-// }
-
-// bool MyString::operator==(const MyString& other) const {
-//     // TODO: Implement this
-// }
-
-// std::ostream& operator<<(std::ostream& os, const MyString& s) {
-//     // TODO: Implement this
-// }
+// C++ - Hard
 
 #include "mystring.h"
 #include <cstring>
 #include <iostream>
 
-// Constructors and Destructor
-MyString::MyString() : data(nullptr), size(0) {}
+// Default constructor
+MyString::MyString() : str(nullptr), len(0) {}
 
-MyString::MyString(const char* cstr) : data(nullptr), size(0) {
-    copyData(cstr);
+// Parameterized constructor
+MyString::MyString(const char* s) {
+    // TODO: Implement this
 }
 
-MyString::MyString(const MyString& other) : data(nullptr), size(0) {
-    copyData(other.data);
+// Copy constructor
+MyString::MyString(const MyString& other) {
+    // TODO: Implement this
 }
 
+// Destructor
 MyString::~MyString() {
-    clear();
+    delete[] str;
 }
 
-// Member functions
 int MyString::length() const {
-    return size;
+    // TODO: Implement this
 }
 
 const char* MyString::c_str() const {
-    return data;
+    // TODO: Implement this
 }
 
 void MyString::append(const MyString& other) {
-    MyString temp(*this);
-    clear();
-    size = temp.size + other.size;
-    data = new char[size + 1];
-    strcpy(data, temp.data);
-    strcat(data, other.data);
+    // TODO: Implement this
 }
 
 int MyString::compare(const MyString& other) const {
-    return strcmp(data, other.data);
+    // TODO: Implement this
 }
 
-MyString MyString::substring(int start, int end) const {
-    if (start < 0) start = 0;
-    if (end > size) end = size;
-    if (start >= end) return MyString();
-
-    MyString sub;
-    sub.size = end - start;
-    sub.data = new char[sub.size + 1];
-    strncpy(sub.data, data + start, sub.size);
-    sub.data[sub.size] = '\0';
-    return sub;
-}
-
-// Overloaded operators
 MyString& MyString::operator=(const MyString& other) {
-    if (this != &other) {
-        clear();
-        copyData(other.data);
-    }
-    return *this;
+    // TODO: Implement this
 }
 
 MyString MyString::operator+(const MyString& other) const {
-    MyString result(*this);
-    result.append(other);
-    return result;
+    // TODO: Implement this
 }
 
 bool MyString::operator==(const MyString& other) const {
-    return compare(other) == 0;
+    // TODO: Implement this
 }
 
-// Helper function to copy data
-void MyString::copyData(const char* cstr) {
-    if (cstr) {
-        size = strlen(cstr);
-        data = new char[size + 1];
-        strcpy(data, cstr);
-    }
-}
-
-// Helper function to clear data
-void MyString::clear() {
-    delete[] data;
-    data = nullptr;
-    size = 0;
-}
-
-// Overloaded stream insertion operator
-std::ostream& operator<<(std::ostream& os, const MyString& str) {
-    if (str.data)
-        os << str.data;
-    return os;
+std::ostream& operator<<(std::ostream& os, const MyString& s) {
+    // TODO: Implement this
 }
--- tasks/c++/hard/mystring.h
+++ tasks/c++/hard/mystring.h
@@ -1,49 +1,12 @@
-// // C++ - Hard
-
-// #ifndef MYSTRING_H
-// #define MYSTRING_H
-// #include <iostream>
-
-// class MyString {
-//     // TODO: Implement MyString class
-// };
-
-// #endif
+// C++ - Hard
 
 #ifndef MYSTRING_H
 #define MYSTRING_H
-
 #include <iostream>
 
 class MyString {
-public:
-    // Constructors and Destructor
-    MyString();
-    MyString(const char* cstr);
-    MyString(const MyString& other);
-    ~MyString();
-
-    // Member functions
-    int length() const;
-    const char* c_str() const;
-    void append(const MyString& other);
-    int compare(const MyString& other) const;
-
-    // Overloaded operators
-    MyString& operator=(const MyString& other);
-    MyString operator+(const MyString& other) const;
-    bool operator==(const MyString& other) const;
-    friend std::ostream& operator<<(std::ostream& os, const MyString& str);
-
-    // Bonus feature
-    MyString substring(int start, int end) const;
-
-private:
-    char* data;
-    int size;
-
-    void copyData(const char* cstr);
-    void clear();
+    // TODO: Implement MyString class
 };
 
 #endif
+
