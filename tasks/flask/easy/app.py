37f271f49ebe46d3e5422d5404dacae0d0a17b76
GitHub Action <action@github.com>
2023-10-30 13:27:49 +0000

    Revert "Merge pull request #1332 from Gauravtb2253/patch-8"
    This reverts commit a8e1289ca25d3e52e7539d918440a88035131cec, reversing
    changes made to 5072f7abbe48c6478ee0229d8883aed8b05b9907.

--- tasks/flask/easy/app.py
+++ tasks/flask/easy/app.py
@@ -1,10 +1,7 @@
+# Flask - Easy
+
 from flask import Flask
 
-app = Flask(__name)
+app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, Flask!"
-
-if __name__ == "__main__":
-    app.run(debug=True)
+# TODO: Implement rest of the code here
