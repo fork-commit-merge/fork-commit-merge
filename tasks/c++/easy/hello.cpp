cc1cf2586906b5045d749dc26104cfb1d15a705a
GitHub Action <action@github.com>
2023-10-30 12:46:38 +0000

    Revert "Merge pull request #1326 from Gauravtb2253/patch-2"
    This reverts commit 05197008d19389a5ab3afb2ecf0ee1365ed3b9a6, reversing
    changes made to 811c82af258ff4a732b3efcbf85fca789d616196.

--- tasks/c++/easy/hello.cpp
+++ tasks/c++/easy/hello.cpp
@@ -1,11 +1,9 @@
 // C++ - Easy 3
 
 #include <iostream>
-using namespace std;
 
 int main() {
     // TODO: Create a Basic C++ Program that Prints "Hello World"
-    cout<<"Hello World"<<endl;
     return 0;
 }
 
