14445f850bd513003b81e945904ddda6c38d6943
GitHub Action <action@github.com>
2023-10-30 16:25:22 +0000

    Revert "Merge pull request #1350 from AlexHoop140/flask#1050"
    This reverts commit ab2572a1aec07dccc87bdc68412011b2dea2acfe, reversing
    changes made to e0c0e85b1ea02429a1d34e40540ab73aea1e27e7.

--- tasks/flask/easy/app.py
+++ tasks/flask/easy/app.py
@@ -1,12 +1,8 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name)
 
-
-@app.route('/')
-def hello():
-    return 'Hello, Flask!'
-
+# TODO: Implement the task requested in the issue description
 
 if __name__ == "__main__":
     app.run(debug=True)
