8225699d2ff3402174a6b1723ffa20cc25705d42
GitHub Action <action@github.com>
2023-10-30 14:31:26 +0000

    Revert "Merge pull request #1336 from Gauravtb2253/patch-12"
    This reverts commit 166bb174403ec85fbaa4d353ac247db53c03e39a, reversing
    changes made to bc0ec517238de33defed38c16d7a31c2942c77ff.

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,10 +1,9 @@
+# Python - Medium
+
 def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                
+    # TODO: Implement the bubble sort algorithm
+
+#! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
 print("Original array:", arr)
 
