3965f9f7e1496f4975957cdda460ed3f391a78cd
Gaurav Bomble <114927759+Gauravtb2253@users.noreply.github.com>
2023-10-30 17:46:30 +0530

    sum of two numbers
    sum of two numbers

--- tasks/c++/easy/sum.cpp
+++ tasks/c++/easy/sum.cpp
@@ -1,3 +1,17 @@
-// C++ - Easy 2
+#include <iostream>
 
-// TODO: Create a Basic C++ Program to Add Two Numbers
+int main() {
+    double num1, num2;
+
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+
+    double sum = num1 + num2;
+
+    std::cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << std::endl;
+
+    return 0;
+}
