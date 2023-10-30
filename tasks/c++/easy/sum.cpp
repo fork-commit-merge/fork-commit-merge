55c4807acd0c1b44bfffdd3e415c7703fb7473c2
GitHub Action <action@github.com>
2023-10-30 12:48:06 +0000

    Revert "Merge pull request #1327 from Gauravtb2253/patch-3"
    This reverts commit 6e2961582a43e2602aadec79f7a67ca664bf4668, reversing
    changes made to cc1cf2586906b5045d749dc26104cfb1d15a705a.

--- tasks/c++/easy/sum.cpp
+++ tasks/c++/easy/sum.cpp
@@ -1,17 +1,3 @@
-#include <iostream>
+// C++ - Easy 2
 
-int main() {
-    double num1, num2;
-
-    std::cout << "Enter the first number: ";
-    std::cin >> num1;
-
-    std::cout << "Enter the second number: ";
-    std::cin >> num2;
-
-    double sum = num1 + num2;
-
-    std::cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << std::endl;
-
-    return 0;
-}
+// TODO: Create a Basic C++ Program to Add Two Numbers
