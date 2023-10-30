b04a7adea80ddb591a779ea6e774e90aeea05a04
Gaurav Bomble <114927759+Gauravtb2253@users.noreply.github.com>
2023-10-30 21:52:14 +0530

    DataType Check

--- tasks/dart/easy/type_data_check.dart
+++ tasks/dart/easy/type_data_check.dart
@@ -1,9 +1,23 @@
 void main() {
-  // print(typeDataCheck(5));
-  // print(typeDataCheck(3.14));
-  // print(typeDataCheck("Hello"));
-  // print(typeDataCheck(true));
-  // print(typeDataCheck(null));
+  print(typeDataCheck(5));
+  print(typeDataCheck(3.14));
+  print(typeDataCheck("Hello"));
+  print(typeDataCheck(true));
+  print(typeDataCheck(null));
 }
 
-// TODO: Implement a function that accepts a parameter of any type and returns a string describing its data type.
+String typeDataCheck(dynamic variable) {
+  if (variable is int) {
+    return 'int';
+  } else if (variable is double) {
+    return 'double';
+  } else if (variable is String) {
+    return 'String';
+  } else if (variable is bool) {
+    return 'bool';
+  } else if (variable == null) {
+    return 'null';
+  } else {
+    return 'Unknown';
+  }
+}
