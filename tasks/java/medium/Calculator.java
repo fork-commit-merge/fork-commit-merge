9ee0704692322aa43a7cc323d4fdf5728b12a308
Gaurav Bomble <114927759+Gauravtb2253@users.noreply.github.com>
2023-10-30 20:26:11 +0530

    Calculator

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -1,17 +1,31 @@
-// Java - Medium
-
 package tasks.java.medium;
 
 public class Calculator {
-    // TODO: Implement the Calculator class
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        if (b == 0.0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
 
     public static void main(String[] args) {
-        //! Dont't change the tests in the main method!
         Calculator calc = new Calculator();
 
-        System.out.println("Addition: " + calc.add(5, 3));
-        System.out.println("Subtraction: " + calc.subtract(5, 3));
-        System.out.println("Multiplication: " + calc.multiply(5, 3));
-        System.out.println("Division: " + calc.divide(5, 3));
+        System.out.println("Addition: " + calc.add(5.0, 3.0));
+        System.out.println("Subtraction: " + calc.subtract(5.0, 3.0));
+        System.out.println("Multiplication: " + calc.multiply(5.0, 3.0));
+        System.out.println("Division: " + calc.divide(5.0, 3.0));
     }
 }
