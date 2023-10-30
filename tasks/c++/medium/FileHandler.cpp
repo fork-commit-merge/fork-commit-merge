7e29d569e52a9d7acd2aac8d102bf575010a781d
GitHub Action <action@github.com>
2023-10-30 18:53:31 +0000

    Revert "Merge pull request #1356 from auth-02/file"
    This reverts commit 2653493b22d97f9d99d8938d9d9236f3c7c51c64, reversing
    changes made to bedd74a1ee638668728fc68a8043528fa3b70716.

--- tasks/c++/medium/FileHandler.cpp
+++ tasks/c++/medium/FileHandler.cpp
@@ -1,63 +1,14 @@
-// // C++ - Medium
-
-// #include "FileHandler.h"
-// #include <fstream>
-// #include <sstream>
-
-// // TODO: Implement the FileHandler::ReadFile method
-
-// // TODO: Implement the FileHandler::WriteFile method
-
-// // TODO: Implement the FileHandler::AppendFile method
-
-// // TODO: Implement the FileHandler::FileExists method
+// C++ - Medium
 
 #include "FileHandler.h"
 #include <fstream>
-#include <iostream>
+#include <sstream>
 
-std::string FileHandler::ReadFile(const std::string& filename) {
-    std::string content;
-    std::ifstream file(filename);
-    if (file.is_open()) {
-        std::string line;
-        while (std::getline(file, line)) {
-            content += line + "\n";
-        }
-        file.close();
-    } else {
-        std::cerr << "Error: Cannot open file for reading - " << filename << std::endl;
-    }
-    return content;
-}
+// TODO: Implement the FileHandler::ReadFile method
 
-bool FileHandler::WriteFile(const std::string& filename, const std::string& content) {
-    std::ofstream file(filename);
-    if (file.is_open()) {   
-        file << content;
-        file.close();
-        std::cout << "Write successful." << std::endl;
-        return true;
-    } else {
-        std::cerr << "Error: Cannot open file for writing - " << filename << std::endl;
-        return false;
-    }
-}
+// TODO: Implement the FileHandler::WriteFile method
 
-bool FileHandler::AppendFile(const std::string& filename, const std::string& content) {
-    std::ofstream file(filename, std::ios::app);
-    if (file.is_open()) {
-        file << content;
-        file.close();
-        std::cout << "Append successful." << std::endl;
-        return true;
-    } else {
-        std::cerr << "Error: Cannot open file for appending - " << filename << std::endl;
-        return false;
-    }
-}
+// TODO: Implement the FileHandler::AppendFile method
 
-bool FileHandler::FileExists(const std::string& filename) {
-    std::ifstream file(filename);
-    return file.good();
-}
+// TODO: Implement the FileHandler::FileExists method
+
