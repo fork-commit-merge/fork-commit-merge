71bbde51ba6290e6820424f7bba1d81266a23d61
Atharva Shinde <shindeathrv02@gmail.com>
2023-10-30 23:53:49 +0530

    Implement a Simple File Reader/Writer Class and Methods in C++

--- tasks/c++/medium/FileHandler.cpp
+++ tasks/c++/medium/FileHandler.cpp
@@ -1,14 +1,63 @@
-// C++ - Medium
+// // C++ - Medium
+
+// #include "FileHandler.h"
+// #include <fstream>
+// #include <sstream>
+
+// // TODO: Implement the FileHandler::ReadFile method
+
+// // TODO: Implement the FileHandler::WriteFile method
+
+// // TODO: Implement the FileHandler::AppendFile method
+
+// // TODO: Implement the FileHandler::FileExists method
 
 #include "FileHandler.h"
 #include <fstream>
-#include <sstream>
+#include <iostream>
 
-// TODO: Implement the FileHandler::ReadFile method
+std::string FileHandler::ReadFile(const std::string& filename) {
+    std::string content;
+    std::ifstream file(filename);
+    if (file.is_open()) {
+        std::string line;
+        while (std::getline(file, line)) {
+            content += line + "\n";
+        }
+        file.close();
+    } else {
+        std::cerr << "Error: Cannot open file for reading - " << filename << std::endl;
+    }
+    return content;
+}
 
-// TODO: Implement the FileHandler::WriteFile method
+bool FileHandler::WriteFile(const std::string& filename, const std::string& content) {
+    std::ofstream file(filename);
+    if (file.is_open()) {
+        file << content;
+        file.close();
+        std::cout << "Write successful." << std::endl;
+        return true;
+    } else {
+        std::cerr << "Error: Cannot open file for writing - " << filename << std::endl;
+        return false;
+    }
+}
 
-// TODO: Implement the FileHandler::AppendFile method
+bool FileHandler::AppendFile(const std::string& filename, const std::string& content) {
+    std::ofstream file(filename, std::ios::app);
+    if (file.is_open()) {
+        file << content;
+        file.close();
+        std::cout << "Append successful." << std::endl;
+        return true;
+    } else {
+        std::cerr << "Error: Cannot open file for appending - " << filename << std::endl;
+        return false;
+    }
+}
 
-// TODO: Implement the FileHandler::FileExists method
-
+bool FileHandler::FileExists(const std::string& filename) {
+    std::ifstream file(filename);
+    return file.good();
+}
--- tasks/c++/medium/FileHandler.h
+++ tasks/c++/medium/FileHandler.h
@@ -1,9 +1,24 @@
-// C++ - Medium
+// // C++ - Medium
 
-#pragma once
+// #pragma once
+// #include <string>
+
+// class FileHandler {
+// public:
+//     // TODO: Implement the FileHandler class
+// };
+
+#ifndef FILEHANDLER_H
+#define FILEHANDLER_H
+
 #include <string>
 
 class FileHandler {
 public:
-    // TODO: Implement the FileHandler class
+    static std::string ReadFile(const std::string& filename);
+    static bool WriteFile(const std::string& filename, const std::string& content);
+    static bool AppendFile(const std::string& filename, const std::string& content);
+    static bool FileExists(const std::string& filename);
 };
+
+#endif
