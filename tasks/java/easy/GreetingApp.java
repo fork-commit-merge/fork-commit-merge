4bff3deaa44b575661b7b5a932c2ee23719c917a
GitHub Action <action@github.com>
2023-10-30 14:48:38 +0000

    Revert "Merge pull request #1340 from Gauravtb2253/patch-15"
    This reverts commit 42b2ff20c4e7d8fcf3cd037534aa23def72043b4, reversing
    changes made to e31a9887226221c62506647fcb3ff7cb03939e8d.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,14 +1,8 @@
-import java.util.Scanner;
+// Java - Easy
 
-public class GreetingApp{
+public class GreetingApp {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-
-        System.out.println("Hello, " + name + "! Nice to meet you!");
-
-        scanner.close();
+        // TODO: Implement a Simple Greeting Application
     }
 }
