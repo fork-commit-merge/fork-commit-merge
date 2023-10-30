16c359e096e1a11c128aca2058337c59ae587edc
GitHub Action <action@github.com>
2023-10-30 14:34:07 +0000

    Revert "Merge pull request #1338 from Gauravtb2253/patch-14"
    This reverts commit 9db569b09d6dfcad3135ee94abe16169db5305a9, reversing
    changes made to 55dc1e5142e57a8e0077fff776320fac4122438c.

--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,4 +1,3 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-result = num1 + num2
-print("The sum of", num1, "and", num2, "is:", result)
+# Python - Easy
+
+# TODO: Write a function that returns the sum of two numbers
