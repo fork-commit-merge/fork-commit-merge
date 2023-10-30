1f721e4b988736123cad03b778849d9f4bd44fe7
Atharva Shinde <shindeathrv02@gmail.com>
2023-10-30 23:36:22 +0530

    sum.cpp

--- tasks/c++/easy/ReverseString.cpp
+++ tasks/c++/easy/ReverseString.cpp
@@ -2,21 +2,10 @@
 #include <string>
 
 std::string reverseString(const std::string &str) {
-    std::string reversedStr;
-    for (int i = str.length() - 1; i >= 0; i--) {
-        reversedStr += str[i];
-    }
-    return reversedStr;
+    // TODO: Implement the reverseString function
 }
 
 int main() {
-    std::string input;
-    std::cout << "Enter a string: ";
-    std::cin >> input;
-
-    std::string reversed = reverseString(input);
-
-    std::cout << "Reversed string: " << reversed << std::endl;
-
-    return 0;
+    // TODO: Implement the main method
 }
+
