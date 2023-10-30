cb718e3285316747f7ee982f8e923e97e34c721d
GitHub Action <action@github.com>
2023-10-30 15:39:19 +0000

    Revert "Merge pull request #1345 from Gauravtb2253/patch-19"
    This reverts commit 2be6247a6774fcef37f5b2e2fa92de64aff50504, reversing
    changes made to 2e0a28e4b4c3a18eea70aa6679b78a6e3f458266.

--- tasks/dart/easy/mean_array.dart
+++ tasks/dart/easy/mean_array.dart
@@ -1,18 +1,8 @@
 void main() {
-  List<int> data = [5, 10, 15, 20, 25];
-  double mean = calculateMean(data);
-  print('Mean: $mean');
+  // List<int> data = [5, 10, 15, 20, 25];
+  // double mean = calculateMean(data);
+  // print('Mean: $mean');  // Expected output: Mean: 15.0
 }
 
-double calculateMean(List<int> data) {
-  if (data.isEmpty) {
-    return 0.0;
-  }
+// TODO: Implement a function called 'calculateMean' that takes a list of integers as input and returns the mean as a double.
 
-  double sum = 0.0;
-  for (int value in data) {
-    sum += value.toDouble();
-  }
-
-  return sum / data.length;
-}
