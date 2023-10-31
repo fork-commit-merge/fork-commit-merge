44e1b845d5ecf81cc57b45cf4b6a7f4ce99bfcea
GitHub Action <action@github.com>
2023-10-31 12:17:21 +0000

    Revert "Merge pull request #1365 from wwwsyi/patch-3"
    This reverts commit 2bcdef2e72960955a7c307b3dc269b0bb3407cee, reversing
    changes made to 1f403f3dd5fc140b45a68b7d159ce8525f12a89e.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,14 +1,8 @@
 // Java - Easy
-import java.util.Scanner;
 
 public class GreetingApp {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.println("Hello, " + name + "! Nice to meet you!");
-        scanner.close();
         // TODO: Implement a Simple Greeting Application
     }
 }
