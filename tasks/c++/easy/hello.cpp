088bb51503eae470928e853a6f422589954bc554
abc-345 <136461577+abc-345@users.noreply.github.com>
2023-10-31 20:52:19 +0800

    Update hello.cpp

--- tasks/c++/easy/hello.cpp
+++ tasks/c++/easy/hello.cpp
@@ -1,9 +1,9 @@
 // C++ - Easy 3
-
 #include <iostream>
 
 int main() {
-    // TODO: Create a Basic C++ Program that Prints "Hello World"
+    std::cout << "Hello World!" << std::endl;
     return 0;
 }
+
 
