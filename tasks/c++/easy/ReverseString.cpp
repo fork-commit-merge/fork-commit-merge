de2c01add7c6aa746130e3d71fc2e2e8580df230
GitHub Action <action@github.com>
2023-10-31 14:19:37 +0000

    Revert "Merge pull request #1369 from luv2027/fix/reverseString.cpp"
    This reverts commit f40b2e46c1896dac61f30480d20604392f7c3438, reversing
    changes made to 66ff2750a947703aaa306ed7c3698145f461282e.

--- tasks/c++/easy/ReverseString.cpp
+++ tasks/c++/easy/ReverseString.cpp
@@ -3,22 +3,9 @@
 
 std::string reverseString(const std::string &str) {
     // TODO: Implement the reverseString function
-    std::string reversedString;
-    reversedString.resize(str.length());
-
-    for (int i = str.length() - 1, j = 0; i >= 0; i--, j++) {
-        reversedString[j] = str[i];
-    }
-
-    return reversedString;
 }
 
 int main() {
     // TODO: Implement the main method
-           std::string str, reversedString;
-   std::cout << "Enter a string: ";
-   std::getline(std::cin, str);
-   reversedString = reverseString(str);
-   std::cout << "Reversed string: " << reversedString << std::endl;
 }
 
