169ca7cd7709a3e550941bd0d9fcaffcc0e1f82e
GitHub Action <action@github.com>
2023-10-31 17:07:20 +0000

    Revert "Merge pull request #1383 from shu-qian/main"
    This reverts commit 16e1248b1ef4f2385d09ea49a0a34d36b28f0a33, reversing
    changes made to 84c992fff3e03fd801f5d73f852c5df64b1d66a9.

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -3,26 +3,8 @@
 package tasks.java.medium;
 
 public class Calculator {
-    public double add(double num1, double num2) {
-        return num1 + num2;
-    }
+    // TODO: Implement the Calculator class
 
-    public double subtract(double num1, double num2) {
-        return num1 - num2;
-    }
-
-    public double multiply(double num1, double num2) {
-        return num1 * num2;
-    }
-
-    public double divide(double num1, double num2) {
-        if (num2 == 0) {
-            System.out.println("Error: Division by zero");
-            return Double.NaN; // Not-a-Number
-        }
-        return num1 / num2;
-    }
-    
     public static void main(String[] args) {
         //! Dont't change the tests in the main method!
         Calculator calc = new Calculator();
