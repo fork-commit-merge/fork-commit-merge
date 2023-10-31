6886c66b48f5c966a4c18d4afb2bfeee0eca4a33
abc-345 <136461577+abc-345@users.noreply.github.com>
2023-10-31 20:46:15 +0800

    Update tempConverter.cpp

--- tasks/c++/easy/tempConverter.cpp
+++ tasks/c++/easy/tempConverter.cpp
@@ -1,3 +1,20 @@
 // C++ - Easy 1
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double celsius, fahrenheit;
+
+    cout << "请输入摄氏温度：";
+    cin >> celsius;
+
+    fahrenheit = (celsius * 1.8) + 32;
+
+    cout << "摄氏" << celsius << "度转换为华氏温度为：" << fahrenheit << "度。" << endl;
+
+    return 0;
+}
 
 // TODO:  Create a Basic C++ Program to Convert Temperature from Celsius to Fahrenheit
