cfdbed96dd70d9b09f06830cee0d1acf03bd37b9
Fzzffz <1214397309@qq.com>
2023-11-01 00:25:44 +0800

    解决了java hard,js medium 和 sql hard

--- tasks/java/hard/Frequency.java
+++ tasks/java/hard/Frequency.java
@@ -1,3 +1,28 @@
-// Java - Hard
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-// TODO: Given an array of integers, write a Java program to find the frequency of each element using a Map
+public class Frequency {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter integers separated by spaces");
+
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+
+        while (scanner.hasNextInt()) {
+            int element = scanner.nextInt();
+
+            if (frequencyMap.containsKey(element)) {
+                frequencyMap.put(element, frequencyMap.get(element) + 1);
+            } else {
+                frequencyMap.put(element, 1);
+            }
+        }
+
+        scanner.close();
+
+        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            System.out.println("Element: " + entry.getKey() + ", Frequency: " + entry.getValue());
+        }
+    }
+}
