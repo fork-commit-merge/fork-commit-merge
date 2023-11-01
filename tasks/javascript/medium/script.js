82f5c6828b22e761fc40baa0c3853c69b8885077
Anil <anilmaharjan07@outlook.com>
2023-11-01 11:11:12 +0000

    Create functionality for a countdown timer

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,4 +1,18 @@
-// JavaScript - Medium
 
-// TODO: Create functionality for a countdown timer that counts down from 10 seconds to zero
+const timerDisplay = document.getElementById('timer');
 
+let countdownTime = 10;
+
+function updateTimerDisplay() {
+  if (countdownTime >= 0) {
+    timerDisplay.textContent = countdownTime;
+    countdownTime--;
+  } else {
+    timerDisplay.textContent = "Time's up!";
+    clearInterval(timerInterval);
+  }
+}
+
+updateTimerDisplay();
+
+const timerInterval = setInterval(updateTimerDisplay, 1000);
