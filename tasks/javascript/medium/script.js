f4a6854c3f35d6a1535aee2617207dd4ccc15430
GitHub Action <action@github.com>
2023-11-01 11:31:11 +0000

    Revert "Merge pull request #1386 from Aneal07/main"
    This reverts commit e67a205ff57ed0197d9b140e77b2014b094ba298, reversing
    changes made to 02d0424d4d038f847de440db8ce9c0f89f427798.

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,18 +1,4 @@
+// JavaScript - Medium
 
-const timerDisplay = document.getElementById('timer');
+// TODO: Create functionality for a countdown timer that counts down from 10 seconds to zero
 
-let countdownTime = 10;
-
-function updateTimerDisplay() {
-  if (countdownTime >= 0) {
-    timerDisplay.textContent = countdownTime;
-    countdownTime--;
-  } else {
-    timerDisplay.textContent = "Time's up!";
-    clearInterval(timerInterval);
-  }
-}
-
-updateTimerDisplay();
-
-const timerInterval = setInterval(updateTimerDisplay, 1000);
