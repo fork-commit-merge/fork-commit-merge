3dfbd04a2060899af0c6ac2cbf6f0fb0d37f00c6
GitHub Action <action@github.com>
2023-11-02 09:22:43 +0000

    Revert "Merge pull request #1394 from LeMinhMan2809/fix-issue-javascript"
    This reverts commit ef8c36d5b5deab320afce0a69db09220a37e9243, reversing
    changes made to d61d18ded76df88fcf67d6d4acd8ba16c6944dc5.

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -2,19 +2,3 @@
 
 // TODO: Create functionality for a countdown timer that counts down from 10 seconds to zero
 
-let time = 10
-let timer = document.getElementById("timer");
-
-var setTimeCountDown = setInterval(CountDown, 1000)
-function CountDown (){
-    let seconds = time % 60
-    if(seconds < 10){
-        seconds = `0${seconds}`
-    }
-    timer.innerHTML = `00:${seconds}`
-    time-- 
-    if(seconds == '0-1'){
-        clearInterval(setTimeCountDown)
-        timer.innerHTML = 'Time\'s Up!'
-    }
-}
