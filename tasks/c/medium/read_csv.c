9e9749a041b753009bc1f31d1bb0db538446ac9f
GitHub Action <action@github.com>
2023-11-03 20:00:22 +0000

    Revert "Merge pull request #1406 from toanb2005897/solution"
    This reverts commit 29fee9798057bb4abb73438ea45f5e2da2f95af8, reversing
    changes made to 7044aad47b159516e7dcebfaa9a90bb81de3c585.

--- tasks/c/medium/read_csv.c
+++ tasks/c/medium/read_csv.c
@@ -4,43 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_LINE_LENGTH 1024
-#define MAX_FIELD_LENGTH 256
-
-int main()
-{
-    FILE *csv_file = fopen("data.csv", "r"); // Change "data.csv" to your CSV file's name
-    if (csv_file == NULL)
-    {
-        perror("Error opening the CSV file");
-        return 1;
-    }
-
-    char line[MAX_LINE_LENGTH];
-    char *token;
-
-    // Read and print the header
-    if (fgets(line, sizeof(line), csv_file) != NULL)
-    {
-        printf("Header: %s", line);
-    }
-
-    // Read and print each data line
-    while (fgets(line, sizeof(line), csv_file) != NULL)
-    {
-        printf("Reading line: %s", line);
-
-        // Tokenize the line using ',' as the delimiter
-        token = strtok(line, ",");
-        while (token != NULL)
-        {
-            char field[MAX_FIELD_LENGTH];
-            strncpy(field, token, sizeof(field));
-            printf("%s: %s\n", (token == line) ? "Name" : "Age", field);
-            token = strtok(NULL, ",");
-        }
-    }
-
-    fclose(csv_file);
+int main() {
+    // TODO: Implement the program that reads and displays the contents of data.csv file
     return 0;
 }
