8de6b5ebad0913cdb1fb473d7c9675aa82b54f40
GitHub Action <action@github.com>
2023-11-04 14:54:43 +0000

    Revert "Merge pull request #1420 from GauravSamanta/fix-issue"
    This reverts commit bd8466c03f090c4999f9b731b0a580a663cad438, reversing
    changes made to b22cfa2d02e8440754ac44920a5c6290cd2ca728.

--- tasks/express/easy/app.js
+++ tasks/express/easy/app.js
@@ -1,26 +1,10 @@
 // ExpressJS - Easy
 
-
+const { get } = require("cheerio/lib/api/traversing");
 const express = require("express");
 const app = express();
 const port = 6969;
 
+// TODO: Implement `get` method for / route
 
-app.get('/', (req, res) => {
-
-    try {
-
-        res.status(200).send("Hello, Express!");
-
-    } catch (error) {
-
-        res.status(400).send(error);
-
-    }
-
-})
-
-
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-})
+// TODO: Implement `listen` method
