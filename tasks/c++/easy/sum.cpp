508b4d6885acb4bb3c048108192b97e07ec6e57d
GitHub Action <action@github.com>
2023-11-04 10:21:48 +0000

    Revert "Merge pull request #1410 from williebonavente/fix-issue"
    This reverts commit b75173e42d84179fc209c618458d18635d29f8bf, reversing
    changes made to fb44608d85f5e45e39c58416cccb3f9c10339a76.

--- tasks/c++/easy/sum.cpp
+++ tasks/c++/easy/sum.cpp
@@ -1,19 +1,3 @@
 // C++ - Easy 2
 
 // TODO: Create a Basic C++ Program to Add Two Numbers
-#include<iostream>
-
-int sum(int a, int b);
-int main(void)
-{
-    int num1, num2;
-    std::cout << "Enter two numbers: ";
-    std::cin >> num1, std::cin >> num2;
-    std::cout << sum(num1, num2);
-    return 0;
-}
-
-int sum(int a, int b)
-{
-    return a + b;
-}
