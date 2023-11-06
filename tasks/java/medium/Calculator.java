3d91e4ce345ad703a73ca2dc4627647402ef00cc
nhdh41 <danghuy.nguyenhoang0401@gmail.com>
2023-11-06 22:21:35 +0700

    Calculator

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -1,12 +1,30 @@
 // Java - Medium
 
-package tasks.java.medium;
-
 public class Calculator {
     // TODO: Implement the Calculator class
 
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            System.out.println("Division by zero is not allowed.");
+            return Double.NaN; // Return NaN to represent an error
+        }
+        return (double) a / b;
+    }
+
     public static void main(String[] args) {
-        //! Dont't change the tests in the main method!
+        //! Don't change the tests in the main method!
         Calculator calc = new Calculator();
 
         System.out.println("Addition: " + calc.add(5, 3));
