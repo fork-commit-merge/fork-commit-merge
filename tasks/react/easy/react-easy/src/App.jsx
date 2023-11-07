7d25c7377d51bc9229f5b3b5bc88c257ff771c55
GitHub Action <action@github.com>
2023-11-07 12:34:37 +0000

    Revert "Merge pull request #1469 from TheHolyRoller/main"
    This reverts commit 25ad6768309a5f915bcda1f2f6270c347761ca90, reversing
    changes made to e2350c9ba29148647e3d8d7c69723ac4dd48a464.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -5,16 +5,6 @@ function App() {
   return (
     <div className="center">
       {/* TODO: Implement the card component */}
-      <h1>React/Vite - Easy</h1>
-      <div className='card' >
-      <img src="https://loremflickr.com/320/240" alt='alt'/>
-      
-      <h2>Card Title</h2>
-      
-      <p>Card Description</p>
-      
-      </div>
-      
     </div>
   );
 }
