01dbdd8885ce9f5f7b7baf123c5cbea8a01cad66
GitHub Action <action@github.com>
2023-11-07 18:10:43 +0000

    Revert "Merge pull request #1476 from DicoderRG/simple-storage-solidity-smart-contract"
    This reverts commit c3664036d420c830cb553ef22c829b618946906d, reversing
    changes made to a63cb260acdf97f2698825d8975640cf102fdb0a.

--- tasks/solidity/easy/my_simple_storage/contracts/SimpleStorage.sol
+++ tasks/solidity/easy/my_simple_storage/contracts/SimpleStorage.sol
@@ -2,15 +2,9 @@
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-    
+
 contract SimpleStorage {
-    uint256 _num;
     //TODO: Implement function to set the number
-    function store(uint256 num) public {
-       _num = num;
-    }
+
     //TODO: Implement function to get the number
-    function retrieve() public view returns(uint256) {
-        return _num;
-    }
 }
