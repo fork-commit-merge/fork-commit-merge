7c2e4aa6c344332d15ada42965c543af0aea631d
Omkar-23 <omkarvya63@gmail.com>
2023-11-07 23:06:15 +0530

    implemeted dec_trees

--- tasks/python/hard/decision_tree.py
+++ tasks/python/hard/decision_tree.py
@@ -2,4 +2,17 @@
 
 import numpy as np
 
-# TODO: Implement the Decision Tree Classifier using NumPy
+
+from sklearn.tree import DecisionTreeClassifier
+
+#synthetic dataset
+X = [[0.1, 0.2], [0.3, 0.4], [0.4, 0.6], [0.7, 0.8], [0.9, 0.1]]
+y = [0, 0, 1, 1, 1]
+
+clf = DecisionTreeClassifier(max_depth=1, random_state=42)
+clf.fit(X, y)
+
+sample = [[0.4, 0.6]]
+predicted_class = clf.predict(sample)
+
+print("Predicted class:", predicted_class[0])
