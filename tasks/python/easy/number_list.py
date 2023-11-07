969af1a42321b6d78ff5352cdcd3704a87d0ffb8
GitHub Action <action@github.com>
2023-11-07 14:12:52 +0000

    Revert "Merge pull request #1471 from Tashuuuu/add-interact-with-lists-file"
    This reverts commit 2fde4d2c6d7d9f669277bf6e915222a147563750, reversing
    changes made to 19997164d37851bded35bb2c545a277aa6619d39.

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -5,11 +5,5 @@
 # prints that list to the console and,
 # prints the sum of the numbers of that list
 
-first_10_natural_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(f"List of first 10 natural numbers is {first_10_natural_numbers}")
-
-sum_of_numbers = 0
-for i in first_10_natural_numbers:
-    sum_of_numbers += i
-
-print(f"The sum of first 10 natural numbers is {sum_of_numbers}")
+def function(numbers):
+    pass
