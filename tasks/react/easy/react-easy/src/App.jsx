fa1787dbf10a774e0aba8b2d56e8ee5a91a18aa5
Kamrin Hurley <128550647+KamH333@users.noreply.github.com>
2023-11-07 07:18:19 -0500

    random image card changes

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -4,7 +4,12 @@ import "./index.css";
 function App() {
   return (
     <div className="center">
-      {/* TODO: Implement the card component */}
+      <h1> React/Vite - Easy </h1>
+      <div className="card">
+        <img src="https://loremflickr.com/320/240"></img>
+        <h2> Card Title </h2>
+        <p> Card Description </p>
+      </div>
     </div>
   );
 }
