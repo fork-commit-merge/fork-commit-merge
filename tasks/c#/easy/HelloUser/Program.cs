b07d759981a285d7ef0810406616e9572867d8b9
GitHub Action <action@github.com>
2023-11-08 13:46:34 +0000

    Revert "Merge pull request #1479 from Andre118/console-program-branch"
    This reverts commit 4e043f34e69043fcb2f52a3885aa390457e10ce5, reversing
    changes made to b7f89049ba5b4a24acbd8033034c3b5951bd6955.

--- tasks/c#/easy/HelloUser/Program.cs
+++ tasks/c#/easy/HelloUser/Program.cs
@@ -1,9 +1,3 @@
 ﻿// C# - Easy
 
 // TODO: Implement the C# program requested in the Issue description
-//read user name
-Console.WriteLine("Please enter your name:");
-string userName = Console.ReadLine();
-
-//output welcome message
-Console.WriteLine($"Hello, {userName}");
