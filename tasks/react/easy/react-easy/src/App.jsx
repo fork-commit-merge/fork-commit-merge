1e85c75acdcea1e4a0561bd9b8f0766a143fe0da
GitHub Action <action@github.com>
2023-11-08 16:34:49 +0000

    Revert "Merge pull request #1481 from satyam-mishra-07/fix-issue"
    This reverts commit 2ab3d36df473b0bd60b4cd1a18bf7023da37a18f, reversing
    changes made to 0aa3bb1870ce9be5df5c2a73c4c2216be7051d24.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -4,12 +4,7 @@ import "./index.css";
 function App() {
   return (
     <div className="center">
-      <h1>React/Vite - Easy</h1>
-      <div className="card">
-        <h2>Random Image Generator</h2>
-        <img src="https://loremflickr.com/320/240" alt="Random Image" />
-        <p>Here is your random image</p>
-      </div>
+      {/* TODO: Implement the card component */}
     </div>
   );
 }
