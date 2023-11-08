065b47e218ee21cb9d3a526f9273edfc4326dde8
Satyam <satyammishrakumar39@gmail.com>
2023-11-08 21:31:39 +0530

    Fissue Issue

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -6,9 +6,9 @@ function App() {
     <div className="center">
       <h1>React/Vite - Easy</h1>
       <div className="card">
-        <h2>Card Title</h2>
+        <h2>Random Image Generator</h2>
         <img src="https://loremflickr.com/320/240" alt="Random Image" />
-        <p>Card Description</p>
+        <p>Here is your Random Image</p>
       </div>
     </div>
   );
