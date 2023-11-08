c8d72a597c488df2743d934811c7ba09f9b420ab
MacHongVu-B2013343 <machongvu1804@gmail.com>
2023-11-08 23:47:35 +0700

    Fixed issue

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,8 +1,20 @@
 // Java - Easy
 
-public class GreetingApp {
+import java.util.Scanner;
 
+public class GreetingApp {
     public static void main(String[] args) {
-        // TODO: Implement a Simple Greeting Application
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter their name
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        // Print the greeting message
+        System.out.println("Hello, " + name + "! Nice to meet you!");
+
+        // Close the scanner to release resources
+        scanner.close();
     }
 }
+
