8cc71dc099411660741f2aaa55dfa26e6be5e5f7
Satyam <satyammishrakumar39@gmail.com>
2023-11-08 21:28:18 +0530

    Fissue Issue

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -4,7 +4,12 @@ import "./index.css";
 function App() {
   return (
     <div className="center">
-      {/* TODO: Implement the card component */}
+      <h1>React/Vite - Easy</h1>
+      <div className="card">
+        <h2>Card Title</h2>
+        <img src="https://loremflickr.com/320/240" alt="Random Image" />
+        <p>Card Description</p>
+      </div>
     </div>
   );
 }
