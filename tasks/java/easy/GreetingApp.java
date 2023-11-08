b180893c8307d56c5957c521ae21de33e11bd064
GitHub Action <action@github.com>
2023-11-08 16:51:39 +0000

    Revert "Merge pull request #1485 from HHongVu/fix-issue"
    This reverts commit 5cbf94ada2c761bdf594e26b97f68e90966153d7, reversing
    changes made to 45763b9cf05185a5479294a7a092eb6e049ab6e4.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,20 +1,8 @@
 // Java - Easy
 
-import java.util.Scanner;
-
 public class GreetingApp {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Prompt the user to enter their name
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-
-        // Print the greeting message
-        System.out.println("Hello, " + name + "! Nice to meet you!");
-
-        // Close the scanner to release resources
-        scanner.close();
+        // TODO: Implement a Simple Greeting Application
     }
 }
-
