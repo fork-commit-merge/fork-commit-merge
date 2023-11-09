c20253d0c102c95cc2e3da2a12499349f0a71c80
GitHub Action <action@github.com>
2023-11-09 06:30:16 +0000

    Revert "Merge pull request #1487 from Bishalsingh153/main"
    This reverts commit 9b367cc7235f5b6ca481a0e258b7fc4e3d190712, reversing
    changes made to c88e7b8a717e871f0f0e52803f24654926ac5f30.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,19 +1,3 @@
 #include <stdio.h>
 
 // TODO: Implement the sum and main functions
-int sum(int a,int b)
-{
-    //return a+b;
-    return printf("The sum is: %d",a+b);
-}
- int main()
- {
-     int a,b;
-     printf("Enter the two numbers\n");
-     scanf("%d %d",&a,&b);
-
-
-     sum(a,b);
-
-    // printf("The sum is: %d",sum(a,b));
- }
