decbd2512a16bf4ecd7d24e654009f43ddf1cb96
GitHub Action <action@github.com>
2023-11-10 18:46:28 +0000

    Revert "Merge pull request #1496 from Omkar-23/jva_map"
    This reverts commit 0b739d6b97ad6f7d66567e9129f1091491847b18, reversing
    changes made to 65bf58c69a561aacf7e04c36d8ab4b84d45c7852.

--- tasks/java/hard/Frequency.java
+++ tasks/java/hard/Frequency.java
@@ -1,19 +1,8 @@
-import java.util.HashMap;
-import java.util.Map;
+// Java - Hard
 
-public class ElementFrequency {
+public class Frequency {
 
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 2, 1, 3, 5, 5, 6, 7, 7};
-
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-        for (int num : array) {
-            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
-        }
-        //kanye tha goat no cap
-        System.out.println("Element's freq:");
-        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue() + " times");
-        }
+        // TODO: Given an array of integers, write a Java program to find the frequency of each element using a Map
     }
 }
