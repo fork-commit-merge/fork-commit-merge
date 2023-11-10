07270c06668cbb1100d2b656ff74388da62f41dd
GitHub Action <action@github.com>
2023-11-10 10:51:42 +0000

    Revert "Merge pull request #1495 from Tashuuuu/add-sum-of-two-numbers"
    This reverts commit b9dd964bba0b30eadfde6991be3d2c288f952cc9, reversing
    changes made to 02b99347a13852d63180279fdd051950581befe8.

--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,22 +1,3 @@
 # Python - Easy
 
 # TODO: Write a function that returns the sum of two numbers
-
-def sum(a, b):
-    c = a + b
-    return c
-
-while True:
-    num1 = int(input("Please enter the first number: "))
-    num2 = int(input("Please enter the second number: "))
-
-    print(f"The sum of {num1} and {num2} is {sum(num1, num2)}")
-
-    again = input("Do you want to sum numbers again 'yes' or 'no'?: ")
-
-    if again == "yes":
-        continue
-    elif again == "no":
-        break
-    else:
-        print("Please write either 'yes' or 'no'!")
