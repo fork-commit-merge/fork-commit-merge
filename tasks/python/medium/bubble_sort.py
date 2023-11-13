16eaf16df8473d45d84ea427f09dbf090d1415d4
GitHub Action <action@github.com>
2023-11-13 13:50:12 +0000

    Revert "Merge pull request #1529 from 3lle4/implement_bubbleSort"
    This reverts commit 63cd17e1d8713d52c3f97e12e1d6f190bc35193e, reversing
    changes made to c7e29d0bdb7dff87328597e3c821d23f66d3de7b.

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,17 +1,7 @@
 # Python - Medium
 
 def bubble_sort(arr):
-    n = len(arr)
-    swapped = False
-
-    for i in range(n-1):
-        for j in range(0,n-i-1):
-            if arr[j] > arr[j+1]:
-                swapped = True
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-        
-        if not swapped:
-            return
+    # TODO: Implement the bubble sort algorithm
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
