8d25b99e57ee4d51f3bb80d27298d897685ee6ee
GitHub Action <action@github.com>
2023-11-14 07:01:17 +0000

    Revert "Merge pull request #1539 from tusharkc/bugfix/medium-issue-1-react"
    This reverts commit 49a30d93e0d6abc2749c2103a25af7cff15d64cb, reversing
    changes made to 94869c86e361be30da3df3c92e57d181736ed048.

--- tasks/react/medium/todo-app/src/App.jsx
+++ tasks/react/medium/todo-app/src/App.jsx
@@ -1,14 +1,12 @@
 // React - Medium
 
-import "./App.css";
-import TodoList from "./components/TodoList";
+import './App.css';
 
 function App() {
   // TODO: Add the necessary functionality such as hooks here
   return (
     <div className="App">
       <h1>To-Do List</h1>
-      <TodoList />
       {/* TODO: Add the To-Do list here */}
     </div>
   );
