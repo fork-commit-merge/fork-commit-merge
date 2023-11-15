448e3843cd33a85d520f6e97ad9f48707639848b
PavanCheruvupally <pavancheruvupally5@gmail.com>
2023-11-15 13:29:52 +0530

    Fixed issue

--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,3 +1,10 @@
 # Python - Easy
 
 # TODO: Write a function that returns the sum of two numbers
+print("First Number")
+a=int(input())
+print("Second Number")
+b=int(input())
+s=a+b
+print("Sum of "+ str(a)+ " and  " + str(b)+ " is " +str(s))
+
