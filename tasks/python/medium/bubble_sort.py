e16f8c7a0521ebf9f90af15d8105920d04da3761
PavanCheruvupally <pavancheruvupally5@gmail.com>
2023-11-15 13:17:12 +0530

    Fixed Issue

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,7 +1,15 @@
 # Python - Medium
 
 def bubble_sort(arr):
-    # TODO: Implement the bubble sort algorithm
+    n=len(arr)
+    for i in range(n):
+        swapped=False
+        for j in range(0,n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+                swapped=True
+        if swapped==False:
+            break
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
