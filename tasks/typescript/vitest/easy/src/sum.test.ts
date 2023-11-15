da562d7d4d7f4fc4450dd2afc3152aff150e657d
GitHub Action <action@github.com>
2023-11-15 19:52:23 +0000

    Revert "Merge pull request #1557 from cleecoloma/fix-issue"
    This reverts commit 0dc24c7be86a39291df3e27ec27a616bf9d3ab38, reversing
    changes made to fd1774debbf3c21cda1dd46ef0fd5ae3dcd44b74.

--- tasks/typescript/vitest/easy/src/sum.test.ts
+++ tasks/typescript/vitest/easy/src/sum.test.ts
@@ -4,27 +4,3 @@ import sum from "./sum";
 import { describe, expect, it } from "vitest";
 
 // TODO: Create the test required in the task
-describe('Sum Function', () => {
-  it('Should add an array of numbers correctly', () => {
-    const numbers: number[] = [3, 4, 2];
-    const result = sum(...numbers);
-    expect(result).toBe(9);
-  });
-
-  it('Should handle negative numbers correctly', () => {
-    const numbers: number[] = [-2, 5, -1];
-    const result = sum(...numbers);
-    expect(result).toBe(2);
-  });
-
-  it('Should return 0 for an empty array', () => {
-    const numbers: number[] = [];
-    const result = sum(...numbers);
-    expect(result).toBe(0);
-  });
-
-  it('Should return 0 when no numbers are provided', () => {
-    const result = sum();
-    expect(result).toBe(0);
-  });
-});
