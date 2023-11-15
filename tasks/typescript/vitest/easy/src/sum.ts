dca244ea51d6ee3e54b33d10d5cb32a1791829a1
Quasark <q@quasark.xyz>
2023-11-15 11:33:02 -0800

    updates

--- tasks/typescript/vitest/easy/src/sum.ts
+++ tasks/typescript/vitest/easy/src/sum.ts
@@ -5,6 +5,3 @@
 export default function sum(...numbers: number[]) {
     return numbers.reduce((total, number) => total + number, 0);
 }
-
-let result = sum(1, 2, 3, 4, 5);  // result will be 15
-console.log(result)
