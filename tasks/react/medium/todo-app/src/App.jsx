f2bbf10201cc815fbba1caa2a6e4552ed53597d3
ChristianRhey Tojot <christiantojot@gmail.com>
2023-11-15 11:45:17 -0800

    finished feature tasks

--- tasks/react/medium/todo-app/src/App.jsx
+++ tasks/react/medium/todo-app/src/App.jsx
@@ -1,14 +1,57 @@
 // React - Medium
 
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
   // TODO: Add the necessary functionality such as hooks here
+  const [newItem, setNewItem] = useState('');
+  const [items, setItems] = useState([]);
+
+  function addItem() {
+    console.log(newItem);
+
+    if (!newItem) {
+      alert('Enter an item.');
+      return;
+    }
+
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem
+    }
+
+    setItems(oldList => [...oldList,item])
+
+    setNewItem('')
+  }
+
+  function deleteItem(id) {
+    const newArray = items.filter(item => item.id !== id);
+    setItems(newArray);
+  }
+
   return (
     <div className="App">
       <h1>To-Do List</h1>
-      {/* TODO: Add the To-Do list here */}
+      
+      <input 
+        type='text'
+        placeholder='Add an item...'
+        value={newItem}
+        onChange={e => setNewItem(e.target.value)}
+      />
+      <button onClick={() => addItem()}>Add</button>
+      <ul>
+        {items.map(item => {
+          return(
+            <li key={item.id}>{item.value} <button onClick={() => deleteItem(item.id)}>Delete</button></li>
+          )
+        })}
+      </ul>
     </div>
+    
   );
 }
 
