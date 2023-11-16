b5785ba8d8c710d1e976fd51898980a744160c6d
GitHub Action <action@github.com>
2023-11-16 06:25:24 +0000

    Revert "Merge pull request #1538 from alfabeat/string-class"
    This reverts commit 0a498fefe838eacdbe2d6e0df721cd63ffa132ab, reversing
    changes made to f9f75d57a230833564f8c42ca8504a56c3e3d9bf.

--- tasks/c++/hard/mystring.h
+++ tasks/c++/hard/mystring.h
@@ -5,27 +5,8 @@
 #include <iostream>
 
 class MyString {
-    // TODO: Implement MyString class 
-    char* str;
-    int len;
-public:
-    MyString();
-    ~MyString();
-    MyString(const MyString& other);
-    MyString(const char* s);
-
-    int length()  const;
-    const char* c_str() const;
-    void append(const MyString& other);
-    int compare(const MyString& other) const;
-    MyString& operator=(const MyString& other);
-    MyString operator+(const MyString& other) const;
-    bool operator==(const MyString& other) const;
-
-
-   
+    // TODO: Implement MyString class
 };
 
-    std::ostream& operator<<(std::ostream& os, const MyString& s);
 #endif
 
