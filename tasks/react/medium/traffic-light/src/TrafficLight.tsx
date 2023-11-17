c4069e1d9a500c1efc7c827ff42af5ba91bb5707
GitHub Action <action@github.com>
2023-11-17 14:10:18 +0000

    Revert "Merge pull request #1578 from M-n0ur/traffic-light"
    This reverts commit 48eee087171d10c2ffe1e966bb8a0c317a69f808, reversing
    changes made to 949ea97ae6e6830955e8dd2afd212ffa7fd37c3e.

--- tasks/react/medium/traffic-light/src/TrafficLight.tsx
+++ tasks/react/medium/traffic-light/src/TrafficLight.tsx
@@ -1,51 +1,11 @@
 // React.js - Medium 2
-import { useEffect,useState } from "react";
 import "./App.css";
-
 
 // TODO: Implement  the TrafficLight component
 
-const timer ={
-  red  : 4,
-  yellow : 2,
-  green: 3
-}
-const theTotalTime :number =timer.red + timer.yellow + timer.green; 
-const red = timer.red;
-const yellow = timer.yellow +red;
-const green = timer.green + yellow 
-console.log(red ,yellow ,green)
-
-
-
 export function TrafficLight() {
-  const [counter, setCounter] = useState(0)
-  useEffect(() => {
-   const intervalId =  setInterval(()=>{
-      setCounter((prev)=>{
-        
-        console.log(prev)
-        if (prev>=theTotalTime) {
-          return 0
-          
-        }
-
-        
-        return prev+1})
-    },1000)
-  
-    return () => {
-      clearInterval(intervalId)
-    }
-  }, [])
-  
 
   return (
-    <>
-      <div className={`traffic-light  light  red ${(counter>=0 && counter< red ? "active":"unactive")}`} ></div>
-      <div className={`traffic-light light  yellow ${(counter>=red && counter < yellow ? "active":"unactive")} `} ></div>
-      <div className={`traffic-light light  green ${(counter>=yellow && counter < green ? "active":"unactive")} `} ></div>
-    </>
     // TODO: return JSX here
   );
 }
