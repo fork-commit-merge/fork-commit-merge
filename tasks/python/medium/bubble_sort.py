fef12f6df2dc40e2ace2c43172c5542716c34628
Nay Lin Tun <naylin.ucsy.99@gmail.com>
2023-11-17 22:07:02 +0630

    Implement Bubble Sort Algorithm in Python

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,7 +1,26 @@
 # Python - Medium
 
 def bubble_sort(arr):
-    # TODO: Implement the bubble sort algorithm
+    """
+    Sorts an array in ascending order using the bubble sort algorithm.
+
+    Parameters:
+        arr (list): The list to be sorted.
+
+    Returns:
+        None: This function modifies the original list in place.
+    """
+    # Get the length of the array
+    n = len(arr)
+
+    # Iterate over the array, comparing adjacent elements
+    for i in range(n):
+        # Iterate from the beginning to the end of the unsorted portion
+        for j in range(0, n-i-1):
+            # If the current element is greater than the next element, swap them
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
