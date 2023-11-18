4a3f97398734745e82ddbcc26b528c2df46d6873
Pratik <pratikmore6588@gmail.com>
2023-11-18 10:03:43 +0530

    GreetingApp.java

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,8 +1,15 @@
 // Java - Easy
 
+import java.util.Scanner;
+
 public class GreetingApp {
 
     public static void main(String[] args) {
         // TODO: Implement a Simple Greeting Application
+        String name;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        name = sc.nextLine();
+        System.out.println("Hello, " +name+ "! Nice to meet you!");
     }
 }
