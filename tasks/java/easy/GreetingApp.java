08d6a06d2f5514c4f7bb6e0420e1d6c8aa555c98
GitHub Action <action@github.com>
2023-11-18 06:56:29 +0000

    Revert "Merge pull request #1587 from PratikMore55/java"
    This reverts commit e263e692ebf9111716029e198b226be162ac772c, reversing
    changes made to a40e138795689cdea91832a25af946a55245446d.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,15 +1,8 @@
 // Java - Easy
 
-import java.util.Scanner;
-
 public class GreetingApp {
 
     public static void main(String[] args) {
         // TODO: Implement a Simple Greeting Application
-        String name;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name: ");
-        name = sc.nextLine();
-        System.out.println("Hello, " +name+ "! Nice to meet you!");
     }
 }
