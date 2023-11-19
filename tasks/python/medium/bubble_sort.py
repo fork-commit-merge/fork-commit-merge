f008481af97490994508d882f6fdb8049cff67a9
Owais <warsimuhammadowais@gmail.com>
2023-11-19 23:12:43 +0530

    Bubble Sort added

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -2,6 +2,11 @@
 
 def bubble_sort(arr):
     # TODO: Implement the bubble sort algorithm
+    for i in range(len(arr)):
+        for j in range(i+1,len(arr)):
+            if(arr[i] > arr[j]):
+                arr[i],arr[j] = arr[j],arr[i];
+
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
