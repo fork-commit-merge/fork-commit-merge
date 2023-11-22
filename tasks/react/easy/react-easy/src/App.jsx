02c54fc6eb16fa73f7ecf872430011a925d0223c
GitHub Action <action@github.com>
2023-11-22 19:14:41 +0000

    Revert "Merge pull request #1621 from DicoderRG/add/card-component-in-react"
    This reverts commit 5909cda1783b32f8100a9ef1f55858ab6335b377, reversing
    changes made to 03021228049ee3d672449f5334206556e894c7ff.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -1,15 +1,10 @@
 // React/Vite - Easy
-import './index.css';
+import "./index.css";
 
 function App() {
   return (
     <div className="center">
-      <h1>React/Vite-Easy</h1>
-      <div className="card">
-        <img src={'https://loremflickr.com/320/240'} />
-        <h2>Card Title</h2>
-        <p>Card Description </p>
-      </div>
+      {/* TODO: Implement the card component */}
     </div>
   );
 }
