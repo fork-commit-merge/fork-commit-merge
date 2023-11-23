4b8a4e62d737a6a779fe5eaa10eb294d8fb318b4
GitHub Action <action@github.com>
2023-11-23 07:26:17 +0000

    Revert "Merge pull request #1623 from DilushaHesaranga/patch-1"
    This reverts commit 8b7c8b03053ff17c881faefe9e73009b5dcbfd86, reversing
    changes made to 02c54fc6eb16fa73f7ecf872430011a925d0223c.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -4,12 +4,7 @@ import "./index.css";
 function App() {
   return (
     <div className="center">
-      <h1>React/Vite- Easy</h1>
-      <div class="card">
-      <h2>Card Title</h2>
-      <img src="https://loremflickr.com/320/240" alt="random image">
-      <p>Card Description</p> 
-      </div>
+      {/* TODO: Implement the card component */}
     </div>
   );
 }
