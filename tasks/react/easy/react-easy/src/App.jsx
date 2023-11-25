b4aa41ebe3007c586ce2b7a56a7430dfbeb340c3
GitHub Action <action@github.com>
2023-11-25 14:13:50 +0000

    Revert "Merge pull request #1633 from dhruvjindal555/main"
    This reverts commit a1e2d9d246ab15a66899de76d5cf2d063360e9ef, reversing
    changes made to c4324190393d4d8bab3c1936738d126974af66dc.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -4,12 +4,7 @@ import "./index.css";
 function App() {
   return (
     <div className="center">
-      <h1>React/Vite - Easy </h1>
-      <div class="card">
-        <img src="https://loremflickr.com/320/240"/>
-        <h2>Cart Title</h2>
-        <p>Cart Description</p>
-      </div>
+      {/* TODO: Implement the card component */}
     </div>
   );
 }
