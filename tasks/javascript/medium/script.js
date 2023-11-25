ac812821e80c1fc5447e9fa650412c5b3fcf22d9
GitHub Action <action@github.com>
2023-11-25 18:33:21 +0000

    Revert "Merge pull request #1637 from HrithikHarsha/medJs"
    This reverts commit 11c1b0d75ac2263fe73940137b660ea5ed738f6e, reversing
    changes made to b4aa41ebe3007c586ce2b7a56a7430dfbeb340c3.

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,21 +1,4 @@
-var timerElement = document.getElementById("timer");
+// JavaScript - Medium
 
+// TODO: Create functionality for a countdown timer that counts down from 10 seconds to zero
 
-var countdownTime = 10;
-
-function updateTimerDisplay() {
-    var minutes = Math.floor(countdownTime / 60);
-    var seconds = countdownTime % 60;
-    timerElement.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-}
-function countdown() {
-    updateTimerDisplay();
-    if (countdownTime === 0) {
-        timerElement.textContent = "Time's up!";
-    } else {
-        countdownTime--;
-        
-        setTimeout(countdown, 1000);
-    }
-}
-countdown();
