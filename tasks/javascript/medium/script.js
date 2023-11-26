5fc77f95a7d671fa1c98edcc9e678490f0903281
Niko Hoffrén <niko.hoffren@gmail.com>
2023-11-26 14:02:29 +0200

    Update JavaScript and Tailwind task files

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,21 +1,3 @@
-var timerElement = document.getElementById("timer");
+// JavaScript - Medium
 
-
-var countdownTime = 10;
-
-function updateTimerDisplay() {
-    var minutes = Math.floor(countdownTime / 60);
-    var seconds = countdownTime % 60;
-    timerElement.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-}
-function countdown() {
-    updateTimerDisplay();
-    if (countdownTime === 0) {
-        timerElement.textContent = "Time's up!";
-    } else {
-        countdownTime--;
-        
-        setTimeout(countdown, 1000);
-    }
-}
-countdown();
+// Create functionality for a countdown timer that counts down from 10 seconds to zero using JavaScript
