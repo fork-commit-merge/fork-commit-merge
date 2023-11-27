4a0e326edb113570995b69374c926b0f0dbf1a9f
GitHub Action <action@github.com>
2023-11-27 12:28:27 +0000

    Revert "Merge pull request #1647 from kogura1995/test"
    This reverts commit 92579e6abb5f4d1a6c483cefb4b3321ea6cdb8c2, reversing
    changes made to 58c2f50f94de589aa525088727f4c13be7438aa0.

--- tasks/rust/easy/fibonacci.rs
+++ tasks/rust/easy/fibonacci.rs
@@ -1,30 +1,5 @@
 // Rust - Easy
 
 use std::io;
-use std::io::Write;
 
-fn main() {
-
-    // Input Number
-    print!("Input integer number(1 or over): ");
-    io::stdout().flush().unwrap();
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).ok();
-    let number: u128 = input.trim().parse().ok().unwrap();
-    if number == 0 {
-        println!("Input number is 1 or over.");
-    }
-
-    // Calculate　Fibonacci Numbers
-    let mut fibonacci_numbers = (0, 1);
-    for loop_number in 1..number+1 {
-        if number == loop_number {
-            let answer = fibonacci_numbers.0 + fibonacci_numbers.1;
-            println!("{}", answer);
-        }
-        else {
-            fibonacci_numbers.1 = fibonacci_numbers.0 + fibonacci_numbers.1;
-            fibonacci_numbers.0 = fibonacci_numbers.1 - fibonacci_numbers.0;
-        }
-    }
-}
+// TODO: Implement fibonacci sequence in Rust
