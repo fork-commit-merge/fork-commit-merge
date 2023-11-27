5647ea7a4a3aac88c82559fbd291c036a2a3cfa0
GitHub Action <action@github.com>
2023-11-27 07:20:15 +0000

    Revert "Merge pull request #1642 from mani1soni/mani1soni/bash-disk-monitoring-script-#1063"
    This reverts commit fe19e88b81fc31dbffe968e6d6940d941768163d, reversing
    changes made to 30fe7e1039206b6c6d9a829da84347fec661617e.

--- tasks/bash/medium/disk_monitor.sh
+++ tasks/bash/medium/disk_monitor.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-# Configuration
-THRESHOLD=20  # Set the threshold as 20% (can be changed)
-EMAIL="your_email@example.com"  # Replace with the email address where alerts should be sent
+# Bash - Medium
 
-# Function to check disk space and send email if needed
-check_disk_space() {
-    # Using 'df' command to get the root filesystem disk usage and extracting the usage percentage
-    usage=$(df -h / | grep -v Filesystem | awk '{print $5}' | sed 's/%//g')
-
-    # Check if usage exceeds threshold
-    if [ $usage -ge $THRESHOLD ]; then
-        # Preparing the disk space report
-        report=$(df -h /)
-
-        # Sending the email alert
-        echo -e "Subject: Disk Space Alert\n\nDisk space is critically low on the server. Current status:\n\n$report" | mail -s "Disk Space Alert" $EMAIL
-    fi
-}
-
-# Run the check
-check_disk_space
-
+# TODO: Implement the Disk Monitor script
