233dba62aabec779f5bb30de9d865be3aa5ae08d
GitHub Action <action@github.com>
2023-11-27 17:08:51 +0000

    Revert "Merge pull request #1649 from Rabbok/main"
    This reverts commit a276d1d98cc3e31b1981f13bb090df4a995a6f3f, reversing
    changes made to a5e966bc86c8aea089beaa0a800dd3bf869e7e8d.

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,25 +1,3 @@
 // JavaScript - Medium
+
 // Create functionality for a countdown timer that counts down from 10 seconds to zero using JavaScript
-// Set the countdown time in seconds
-
-function updateCountdownDisplay(seconds) {
-    const countdownElement = document.getElementById('timer');
-    countdownElement.innerHTML = seconds;
-}
-
-function countdown() {
-    let i = 10;
-
-    const countdownInterval = setInterval(function () {
-        updateCountdownDisplay(i);
-
-        if (i <= 0) {
-            clearInterval(countdownInterval);
-            document.getElementById('timer').textContent = "Time's up!";
-        }
-
-        i--;
-    }, 1000);
-}
-
-window.onload = countdown;
