1bfda3228bb91410411b44d2266fa731f79ae5a9
GitHub Action <action@github.com>
2023-11-28 07:58:23 +0000

    Revert "Merge pull request #1654 from HarikaB11/fix_issue_#1516"
    This reverts commit 3c9f8f8908a628811495227565a83024384ccaac, reversing
    changes made to 29b9128eaad67bb1ea5e664ed44dba64691d0ec7.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,13 +1,8 @@
 // Java - Easy
-import java.util.Scanner;
+
 public class GreetingApp {
 
     public static void main(String[] args) {
         // TODO: Implement a Simple Greeting Application
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.println("Hello, " + name + "! Nice to meet you!");
-        scanner.close();
     }
 }
