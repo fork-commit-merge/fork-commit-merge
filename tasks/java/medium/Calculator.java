103e784dae31ed991bca7d1beab79b8ee0264d38
LakshmiGayathri19 <rlgayathri99@gmail.com>
2023-11-28 22:43:36 -0500

    Complete calculator.java code

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -3,7 +3,69 @@
 package tasks.java.medium;
 
 public class Calculator {
-    // TODO: Implement the Calculator class
+    public int add(int number_1, int number_2) {
+        return number_1 + number_2;
+    }
+
+    public float add(int number_1, float number_2) {
+        return number_1 + number_2;
+    } 
+
+    public float add(float number_1, int number_2) {
+        return number_1 + number_2;
+    } 
+
+    public float add(float number_1, float number_2) {
+        return number_1 + number_2;
+    }
+
+    public int subtract(int number_1, int number_2) {
+        return number_1 - number_2;
+    }
+
+    public float subtract(int number_1, float number_2) {
+        return number_1 - number_2;
+    } 
+
+    public float subtract(float number_1, int number_2) {
+        return number_1 - number_2;
+    } 
+
+    public float subtract(float number_1, float number_2) {
+        return number_1 - number_2;
+    }
+
+    public int multiply(int number_1, int number_2) {
+        return number_1 * number_2;
+    }
+
+    public float multiply(int number_1, float number_2) {
+        return number_1 * number_2;
+    } 
+
+    public float multiply(float number_1, int number_2) {
+        return number_1 * number_2;
+    } 
+
+    public float multiply(float number_1, float number_2) {
+        return number_1 * number_2;
+    }
+
+    public int divide(int number_1, int number_2) {
+        return number_1 / number_2;
+    }
+
+    public float divide(int number_1, float number_2) {
+        return number_1 / number_2;
+    } 
+
+    public float divide(float number_1, int number_2) {
+        return number_1 / number_2;
+    } 
+
+    public float divide(float number_1, float number_2) {
+        return number_1 / number_2;
+    }
 
     public static void main(String[] args) {
         //! Don't change the tests in the main method!
