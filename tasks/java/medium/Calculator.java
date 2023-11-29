ee2b74c14b8fb4cc344dbecfd6b4186afe530e5e
Manasa Thatipamula <manasa7862@gmail.com>
2023-11-29 16:00:40 -0500

    Updated Calculator.java

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -5,6 +5,32 @@ package tasks.java.medium;
 public class Calculator {
     // TODO: Implement the Calculator class
 
+    // Addition
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Subtraction
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Multiplication
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Division
+    public double divide(int a, int b) {
+        // Check for division by zero
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+
+        // Perform division and return the result
+        return (double) a / b;
+    }
+
     public static void main(String[] args) {
         //! Don't change the tests in the main method!
         Calculator calc = new Calculator();
@@ -15,3 +41,4 @@ public class Calculator {
         System.out.println("Division: " + calc.divide(5, 3));
     }
 }
+
