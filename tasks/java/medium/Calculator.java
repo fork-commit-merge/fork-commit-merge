5de51f057862ab06b06a328f79b4e711643ab1a7
LakshmiGayathri19 <rlgayathri99@gmail.com>
2023-11-28 22:47:48 -0500

    Change data type to double to match test cases

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -3,67 +3,19 @@
 package tasks.java.medium;
 
 public class Calculator {
-    public int add(int number_1, int number_2) {
+    public double add(double number_1, double number_2) {
         return number_1 + number_2;
     }
 
-    public float add(int number_1, float number_2) {
-        return number_1 + number_2;
-    } 
-
-    public float add(float number_1, int number_2) {
-        return number_1 + number_2;
-    } 
-
-    public float add(float number_1, float number_2) {
-        return number_1 + number_2;
-    }
-
-    public int subtract(int number_1, int number_2) {
-        return number_1 - number_2;
-    }
-
-    public float subtract(int number_1, float number_2) {
+    public double subtract(double number_1, double number_2) {
         return number_1 - number_2;
     } 
 
-    public float subtract(float number_1, int number_2) {
-        return number_1 - number_2;
-    } 
-
-    public float subtract(float number_1, float number_2) {
-        return number_1 - number_2;
-    }
-
-    public int multiply(int number_1, int number_2) {
+    public double multiply(double number_1, double number_2) {
         return number_1 * number_2;
     }
 
-    public float multiply(int number_1, float number_2) {
-        return number_1 * number_2;
-    } 
-
-    public float multiply(float number_1, int number_2) {
-        return number_1 * number_2;
-    } 
-
-    public float multiply(float number_1, float number_2) {
-        return number_1 * number_2;
-    }
-
-    public int divide(int number_1, int number_2) {
-        return number_1 / number_2;
-    }
-
-    public float divide(int number_1, float number_2) {
-        return number_1 / number_2;
-    } 
-
-    public float divide(float number_1, int number_2) {
-        return number_1 / number_2;
-    } 
-
-    public float divide(float number_1, float number_2) {
+    public double divide(double number_1, double number_2) {
         return number_1 / number_2;
     }
 
