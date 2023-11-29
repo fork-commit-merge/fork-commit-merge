19c6027a5e6fcfbd8096876c15e8925e396871ae
HarikaB11 <hbanda@clemson.edu>
2023-11-28 22:12:57 -0500

    Fixed issue #1443

--- tasks/sql/medium/Select(select).sql
+++ tasks/sql/medium/Select(select).sql
@@ -9,3 +9,8 @@
 
 -- |   Name       |  Continent  |     Area    |  Population  |     GDP     |
 -- |--------------|-------------|-------------|--------------|-------------|
+SELECT Name, Population
+FROM World
+WHERE Population > (SELECT Population FROM World WHERE Name = 'Canada') 
+ AND Population < (SELECT Population FROM World WHERE Name = 'Poland');
+
