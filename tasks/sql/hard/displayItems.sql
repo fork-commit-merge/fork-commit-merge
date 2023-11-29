f8f1797c4150a58b27912299df20086814c26ebd
GitHub Action <action@github.com>
2023-11-29 06:06:53 +0000

    Revert "Merge pull request #1665 from Fshahnaj/fix-hard-issue-SQL-#1444"
    This reverts commit d29f2d46a49a86c88d41cef0176488e990dac58e, reversing
    changes made to 624702bd0f15f7e233dfe9ac535733527f19a923.

--- tasks/sql/hard/displayItems.sql
+++ tasks/sql/hard/displayItems.sql
@@ -10,29 +10,14 @@
 
 CREATE TABLE SELLER (
     -- TODO: Add all the requested fields (S_id, Name, Bank_acc_no, Email, Phone)
-    S_id INT PRIMARY KEY,
-    Name VARCHAR(255),
-    Bank_acc_no VARCHAR(20),
-    Email VARCHAR(255),
-    Phone VARCHAR(15)
 );
 
 CREATE TABLE STOCKS (
     -- TODO: Add all the requested fields (S_id, Prod_id, S_Date)
-     S_id INT,
-    Prod_id INT,
-    S_Date DATE,
-    FOREIGN KEY (S_id) REFERENCES SELLER(S_id),
-    FOREIGN KEY (Prod_id) REFERENCES PRODUCT(Prod_id)
 );
 
 CREATE TABLE PRODUCT (
     -- TODO: Add all the requested fields (Prod_id, Prod_name, Description, Price, Expiry_date)
-    Prod_id INT PRIMARY KEY,
-    Prod_name VARCHAR(255),
-    Description TEXT,
-    Price DECIMAL(10, 2),
-    Expiry_date DATE
 );
 
 
@@ -61,48 +46,11 @@ VALUES
 -- Display the following :
 
 -- (a) Display name of the sellers and their bank account number who stocked at least 24 different products with price higher than 1000 rupees in the last one year
-SELECT S.Name, S.Bank_acc_no
-FROM SELLER S
-JOIN STOCKS STO ON S.S_id = STO.S_id
-JOIN PRODUCT P ON STO.Prod_id = P.Prod_id
-WHERE P.Price > 1000
-  AND STO.S_Date >= DATE(CURRENT_DATE, '-1 year')
-GROUP BY S.S_id, S.Name, S.Bank_acc_no
-HAVING COUNT(DISTINCT STO.Prod_id) >= 24;
 
 -- (b) Use nested query to display name of the sellers and their email address; who have in stock products with expiry date past today’s date
-SELECT S.Name, S.Email
-FROM SELLER S
-WHERE S.S_id IN (
-    SELECT ST.S_id
-    FROM STOCKS ST
-    JOIN PRODUCT P ON ST.Prod_id = P.Prod_id
-    WHERE P.Expiry_date > DATE('now')
-);
+
 -- (c) Display the name of the products that are in stock by at least one seller and also those that are not in stock by any of the sellers
-SELECT DISTINCT P.Prod_name
-FROM PRODUCT P
-LEFT JOIN STOCKS ST ON P.Prod_id = ST.Prod_id
-WHERE ST.S_id IS NOT NULL;
 
 -- (d) Use join query to display name of the sellers and name of the products stocked by them during March 23, 2020 and June 30, 2020.
-SELECT S.Name, P.Prod_name
-FROM SELLER S
-JOIN STOCKS ST ON S.S_id = ST.S_id
-JOIN PRODUCT P ON ST.Prod_id = P.Prod_id
-WHERE ST.S_Date BETWEEN '2020-03-23' AND '2020-06-30';
 
 -- (e) Display name of sellers who have in stock maximum number of different products and minimum number of different products
-SELECT S.Name, COUNT(DISTINCT ST.Prod_id) AS NumProducts
-FROM SELLER S
-JOIN STOCKS ST ON S.S_id = ST.S_id
-GROUP BY S.S_id
-ORDER BY NumProducts DESC
-LIMIT 1;  -- Seller with the maximum number of different products
-
-SELECT S.Name, COUNT(DISTINCT ST.Prod_id) AS NumProducts
-FROM SELLER S
-JOIN STOCKS ST ON S.S_id = ST.S_id
-GROUP BY S.S_id
-ORDER BY NumProducts ASC
-LIMIT 1;  -- Seller with the minimum number of different products
