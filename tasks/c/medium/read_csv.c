b17153ffddb67d1a754f3a64d47e869755e40097
GitHub Action <action@github.com>
2023-11-29 06:03:02 +0000

    Revert "Merge pull request #1663 from LakshmiGayathri19/C"
    This reverts commit 5c15e798cba0cc16b84d86b9a6dfcd64967b4481, reversing
    changes made to 14a4327163a2148e32ff6c54726436062548ed43.

--- tasks/c/medium/read_csv.c
+++ tasks/c/medium/read_csv.c
@@ -4,40 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_LINE_SIZE 1024
-#define DELIMITER ","
-
 int main() {
     // TODO: Implement the program that reads and displays the contents of data.csv file
-    FILE *file = fopen("data.csv", "r");
-    if (!file) {
-        perror("Error opening file");
-        return 1;
-    }
-
-    char line[MAX_LINE_SIZE];
-
-    // Read and print the header
-    if (fgets(line, sizeof(line), file)) {
-        printf("Header: %s", line);
-    }
-
-    // Read the file line by line (skipping the header)
-    while (fgets(line, sizeof(line), file)) {
-        printf("\nReading line: %s", line);
-
-        char *token = strtok(line, DELIMITER);
-
-        // Print the content in a human-readable format
-        while (token != NULL) {
-            printf("Name: %s \nAge: %s", strtok(token, "\n"), strtok(NULL, DELIMITER));
-            token = strtok(NULL, DELIMITER);
-        }
-        
-        printf("\n");
-    }
-
-    fclose(file);
-
     return 0;
 }
