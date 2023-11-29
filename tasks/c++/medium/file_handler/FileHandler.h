4e573069dc2260e7ece2f61846b682b1b226f3ce
GitHub Action <action@github.com>
2023-11-29 05:42:19 +0000

    Revert "Merge pull request #1657 from HemanthVasireddy/main"
    This reverts commit 9d0799b0da6db337e8aa6c6a46d82b85ff813d88, reversing
    changes made to bdf9219156b47b4db9209be32bc91d1b7af82cb1.

--- tasks/c++/medium/file_handler/FileHandler.h
+++ tasks/c++/medium/file_handler/FileHandler.h
@@ -5,15 +5,5 @@
 
 class FileHandler {
 public:
-    // Reads the content of a given text file and returns it as a string
-    static std::string ReadFile(const std::string& filePath);
-
-    // Writes a given string to a specified text file
-    static bool WriteFile(const std::string& filePath, const std::string& content);
-
-    // Appends a given string to the end of a specified text file
-    static bool AppendFile(const std::string& filePath, const std::string& content);
-
-    // Checks if a file exists at a given path
-    static bool FileExists(const std::string& filePath);
+    // TODO: Implement the FileHandler class
 };
