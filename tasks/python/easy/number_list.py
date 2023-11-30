67aa4a792cb54c0afd894b5566d6d70c3b6f0953
GitHub Action <action@github.com>
2023-11-30 09:19:38 +0000

    Revert "Merge pull request #1675 from sandalinemmaniwar/sandalinemmaniwar-patch-1"
    This reverts commit 476367727539a880909211f040783a9821101bf8, reversing
    changes made to ad47f6b04bedefdacd2e6e0f3250a4baf166d05f.

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -5,17 +5,5 @@
 # prints that list to the console and,
 # prints the sum of the numbers of that list
 
-def function():
-    # Create a list of the first 10 natural numbers
-    numbers = list(range(1, 11))
-
-    # Print the list
-    print("List of the first 10 natural numbers:", numbers)
-
-    # Calculate and print the sum of the numbers in the list
-    sum_of_numbers = sum(numbers)
-    print("Sum of the numbers in the list:", sum_of_numbers)
-
-# Call the function
-function()
-
+def function(numbers):
+    pass
