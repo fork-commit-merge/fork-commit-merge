2b3f67f6ea7f89ef7d756bf1b64ffe75325bc6e4
Sreeram Paladugu <paladugusreeram@outlook.com>
2023-11-30 18:57:08 -0500

    added bubblesort

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,9 +1,13 @@
 # Python - Medium
 
 def bubble_sort(arr):
-    # TODO: Implement the bubble sort algorithm
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
-#! Test cases (Don't edit):
+# Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
 print("Original array:", arr)
 
