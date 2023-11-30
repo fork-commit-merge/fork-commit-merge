9910cfeff7bfb18ea7ab115ccbc04ea62cd47107
GitHub Action <action@github.com>
2023-11-30 10:07:25 +0000

    Revert "Merge pull request #1695 from surajsolanki724/surajsolanki724-#1533"
    This reverts commit 31e5a7dd1749371a5ec0ff6563703d7d4c929ddb, reversing
    changes made to 612f1d434ab18191f4acec737624ae1d60619368.

--- tasks/bash/easy/create_files.sh
+++ tasks/bash/easy/create_files.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-cd tasks/bash/easy/files || exit
+# Bash - Easy
 
-# Create three files named file1.txt, file2.txt, and file3.txt and write "Hello Shell!" to each
-echo "Hello Shell!" > file1.txt
-echo "Hello Shell!" > file2.txt
-echo "Hello Shell!" > file3.txt
-
-echo "Contents of file1.txt:"
-cat file1.txt
-
-echo "Contents of file2.txt:"
-cat file2.txt
-
-echo "Contents of file3.txt:"
-cat file3.txt
+# TODO: Write the Bash script requested in the issue description
