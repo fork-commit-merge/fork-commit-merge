6fa07dc6597c713c8c0c76ae1ae6d4d3acfeaaf7
GitHub Action <action@github.com>
2023-11-30 14:05:15 +0000

    Revert "Merge pull request #1697 from mac-Git1/main"
    This reverts commit 2df2ecd6448378a0fd3536e46909e49d0c6f646a, reversing
    changes made to bd765e8dd95389f3a6e8085d06850223ed3cdab6.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -3,24 +3,6 @@
 public class GreetingApp {
 
     public static void main(String[] args) {
- 
-
-        // Create a Scanner object to read user input
-        Scanner scanner = new Scanner(System.in);
-
-        // Prompt the user for their name
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-
-        // Greet the user
-        String greeting = generateGreeting(name);
-        System.out.println(greeting);
-
-        // Close the scanner to avoid resource leaks
-        scanner.close();
-    }
-    // Function to generate a personalized greeting
-    private static String generateGreeting(String name) {
-        return "Hello, " + name + "! Welcome to the Greeting App.";
+        // TODO: Implement a Simple Greeting Application
     }
 }
