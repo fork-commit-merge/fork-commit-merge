995724019db216ae2779c3781b495d30e0579aa4
SURAJ SOLANKI <36067876+surajsolanki724@users.noreply.github.com>
2023-11-30 15:09:37 +0530

    Added shell script for issue #1533
    Added shell script for issue #1533

--- tasks/bash/easy/create_files.sh
+++ tasks/bash/easy/create_files.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-# Bash - Easy
+cd tasks/bash/easy/files || exit
 
-# TODO: Write the Bash script requested in the issue description
+# Create three files named file1.txt, file2.txt, and file3.txt and write "Hello Shell!" to each
+echo "Hello Shell!" > file1.txt
+echo "Hello Shell!" > file2.txt
+echo "Hello Shell!" > file3.txt
+
+echo "Contents of file1.txt:"
+cat file1.txt
+
+echo "Contents of file2.txt:"
+cat file2.txt
+
+echo "Contents of file3.txt:"
+cat file3.txt
