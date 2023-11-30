3c1d359562f2a475808e32714d216250eb847795
GitHub Action <action@github.com>
2023-11-30 09:52:27 +0000

    Revert "Merge pull request #1686 from gayatri-t/btstrp"
    This reverts commit f85b9693c97a3e8b7289ea7c8f8eb262aa9b87ca, reversing
    changes made to fe33a50658d63bf711a1467c327ed335b8c318fc.

--- tasks/sql/hard/displayItems.sql
+++ tasks/sql/hard/displayItems.sql
@@ -1,28 +1,25 @@
+-- SQL - Hard
+
+-- Consider the following relational database schema:
+
+-- SELLER (S_id, Name, Bank_acc_no, Email, Phone)
+-- STOCKS (S_id, Prod_id, S_Date)
+-- PRODUCT (Prod_id, Prod_name, Description, Price, Expiry_date)
+
 -- Create the Following Tables:
 
 CREATE TABLE SELLER (
-    S_id INT PRIMARY KEY,
-    Name VARCHAR(255),
-    Bank_acc_no VARCHAR(255),
-    Email VARCHAR(255),
-    Phone VARCHAR(255)
+    -- TODO: Add all the requested fields (S_id, Name, Bank_acc_no, Email, Phone)
 );
 
 CREATE TABLE STOCKS (
-    S_id INT,
-    Prod_id INT,
-    S_Date DATE,
-    FOREIGN KEY (S_id) REFERENCES SELLER(S_id),
-    FOREIGN KEY (Prod_id) REFERENCES PRODUCT(Prod_id)
+    -- TODO: Add all the requested fields (S_id, Prod_id, S_Date)
 );
 
 CREATE TABLE PRODUCT (
-    Prod_id INT PRIMARY KEY,
-    Prod_name VARCHAR(255),
-    Description TEXT,
-    Price DECIMAL(10, 2),
-    Expiry_date DATE
+    -- TODO: Add all the requested fields (Prod_id, Prod_name, Description, Price, Expiry_date)
 );
+
 
 -- Insert values into the SELLER table
 INSERT INTO SELLER (S_id, Name, Bank_acc_no, Email, Phone)
@@ -37,6 +34,7 @@ VALUES
     (1, 102, '2023-02-20'),
     (2, 103, '2023-03-10');
 
+
 -- Insert values into the PRODUCT table
 INSERT INTO PRODUCT (Prod_id, Prod_name, Description, Price, Expiry_date)
 VALUES
@@ -44,77 +42,15 @@ VALUES
     (102, 'Product B', 'Description for Product B', 19.99, '2023-11-30'),
     (103, 'Product C', 'Description for Product C', 39.99, '2023-10-15');
 
--- (a) Display name of the sellers and their bank account number who stocked at least 24 different products with a price higher than 1000 rupees in the last one year
-SELECT S.Name, S.Bank_acc_no
-FROM SELLER S
-WHERE S.S_id IN (
-    SELECT ST.S_id
-    FROM STOCKS ST
-    JOIN PRODUCT P ON ST.Prod_id = P.Prod_id
-    WHERE P.Price > 1000
-    AND ST.S_Date >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR)
-    GROUP BY ST.S_id
-    HAVING COUNT(DISTINCT P.Prod_id) >= 24
-);
 
--- (b) Use a nested query to display the name of the sellers and their email address; who have in stock products with an expiry date past today’s date
-SELECT S.Name, S.Email
-FROM SELLER S
-WHERE S.S_id IN (
-    SELECT DISTINCT ST.S_id
-    FROM STOCKS ST
-    JOIN PRODUCT P ON ST.Prod_id = P.Prod_id
-    WHERE P.Expiry_date > CURDATE()
-);
+-- Display the following :
+
+-- (a) Display name of the sellers and their bank account number who stocked at least 24 different products with price higher than 1000 rupees in the last one year
+
+-- (b) Use nested query to display name of the sellers and their email address; who have in stock products with expiry date past today’s date
 
 -- (c) Display the name of the products that are in stock by at least one seller and also those that are not in stock by any of the sellers
--- Products in stock by at least one seller
-SELECT DISTINCT P.Prod_name
-FROM PRODUCT P
-JOIN STOCKS ST ON P.Prod_id = ST.Prod_id
 
-UNION
+-- (d) Use join query to display name of the sellers and name of the products stocked by them during March 23, 2020 and June 30, 2020.
 
--- Products not in stock by any seller
-SELECT DISTINCT P.Prod_name
-FROM PRODUCT P
-WHERE P.Prod_id NOT IN (
-    SELECT DISTINCT ST.Prod_id
-    FROM STOCKS ST
-);
-
--- (d) Use a join query to display the name of the sellers and the name of the products stocked by them during March 23, 2020, and June 30, 2020
-SELECT S.Name, P.Prod_name
-FROM SELLER S
-JOIN STOCKS ST ON S.S_id = ST.S_id
-JOIN PRODUCT P ON ST.Prod_id = P.Prod_id
-WHERE ST.S_Date BETWEEN '2020-03-23' AND '2020-06-30';
-
--- (e) Display the name of sellers who have in stock the maximum number of different products and the minimum number of different products
--- Sellers with the maximum number of different products in stock
-SELECT S.Name, COUNT(DISTINCT ST.Prod_id) AS num_products
-FROM SELLER S
-JOIN STOCKS ST ON S.S_id = ST.S_id
-GROUP BY S.Name
-HAVING COUNT(DISTINCT ST.Prod_id) = (
-    SELECT MAX(num_products)
-    FROM (
-        SELECT COUNT(DISTINCT ST1.Prod_id) AS num_products
-        FROM STOCKS ST1
-        GROUP BY ST1.S_id
-    ) AS max_products
-);
-
--- Sellers with the minimum number of different products in stock
-SELECT S.Name, COUNT(DISTINCT ST.Prod_id) AS num_products
-FROM SELLER S
-JOIN STOCKS ST ON S.S_id = ST.S_id
-GROUP BY S.Name
-HAVING COUNT(DISTINCT ST.Prod_id) = (
-    SELECT MIN(num_products)
-    FROM (
-        SELECT COUNT(DISTINCT ST1.Prod_id) AS num_products
-        FROM STOCKS ST1
-        GROUP BY ST1.S_id
-    ) AS min_products
-);
+-- (e) Display name of sellers who have in stock maximum number of different products and minimum number of different products
