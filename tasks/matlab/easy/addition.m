629febfde650fcfc6834caec1e910e693896a1eb
GitHub Action <action@github.com>
2023-12-01 09:01:50 +0000

    Revert "Merge pull request #1700 from sandalinemmaniwar/sandalinemmaniwar-patch-3"
    This reverts commit c75e24ca34f1df73cc3d4266d216078a7a96d118, reversing
    changes made to 72224263e941e1104595cbf37c23dbb2909f3d47.

--- tasks/matlab/easy/addition.m
+++ tasks/matlab/easy/addition.m
@@ -1,12 +1,3 @@
 % MATLAB - Easy
 
 % TODO: Create a MATLAB script that adds two numbers together and prints the result
-% Define the numbers
-num1 = 5;
-num2 = 10;
-
-% Add the numbers
-result = num1 + num2;
-
-% Print the result
-fprintf('The sum of %d and %d is %d.\n', num1, num2, result);
