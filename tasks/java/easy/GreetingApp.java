0e51282625ff889ac9d34ba8ed406e8a9ad3c415
GitHub Action <action@github.com>
2023-12-02 08:08:26 +0000

    Revert "Merge pull request #1712 from hsmanik/main"
    This reverts commit 9dd6b765daf75dabb92c9ce54523d405ec39f117, reversing
    changes made to 3fc49cfa601b2d669a1de40b225df149e3f1878e.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,17 +1,8 @@
 // Java - Easy
 
-import java.util.Scanner;
-
 public class GreetingApp {
 
     public static void main(String[] args) {
         // TODO: Implement a Simple Greeting Application
-
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter you name: ");
-        String name = sc.nextLine();
-
-        System.out.println("Hello, "+ name + "!" +" Nice to meet you!"); //Hello, John! Nice to meet you!
-        sc.close();
     }
 }
