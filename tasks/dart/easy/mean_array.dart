17d6b27608c138bfeae134e4f3734c1e50c2f8d0
sanjay14073 <103362239+sanjay14073@users.noreply.github.com>
2023-12-02 22:35:12 +0530

    Update mean_array.dart

--- tasks/dart/easy/mean_array.dart
+++ tasks/dart/easy/mean_array.dart
@@ -1,7 +1,17 @@
 void main() {
-  // List<int> data = [5, 10, 15, 20, 25];
-  // double mean = calculateMean(data);
-  // print('Mean: $mean');  // Expected output: Mean: 15.0
+  List<int> data = [5, 10, 15, 20, 25];
+  double mean = calculateMean(data);
+  print('Mean: $mean');  // Expected output: Mean: 15.0
+}
+double calculateMean(List<int>a){
+  double ans=0;
+  for(int i=0;i<a.length;i++){
+    ans+=a[i];
+  }
+  if(a.length!=0){
+    return (ans/a.length).toDouble();
+  }
+  return 0.0
 }
 
 // TODO: Implement a function called 'calculateMean' that takes a list of integers as input and returns the mean as a double.
