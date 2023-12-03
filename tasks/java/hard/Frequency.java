d4b9206f5819413f01e1707c82d22d334960c958
GitHub Action <action@github.com>
2023-12-03 13:31:04 +0000

    Revert "Merge pull request #1723 from qq905575048/fix"
    This reverts commit 28905bab7a2f9fc78886fb9a088e15054ac279da, reversing
    changes made to eb85bd6ff14315a46278febabf3b7c508f71d7f9.

--- tasks/java/hard/Frequency.java
+++ tasks/java/hard/Frequency.java
@@ -1,38 +1,8 @@
 // Java - Hard
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+
 public class Frequency {
 
-    public static Map<Integer, Integer> findFrequency(int[] arr) {
-        // Create an empty map to store the frequencies
-        Map<Integer, Integer> freqMap = new HashMap<>();
-
-        // Loop through the array elements
-        for (int num : arr) {
-            // If the map already contains the element, increment its frequency by 1
-            if (freqMap.containsKey(num)) {
-                freqMap.put(num, freqMap.get(num) + 1);
-            }
-            // Otherwise, add the element to the map with frequency 1
-            else {
-                freqMap.put(num, 1);
-            }
-        }
-
-        // Return the map of frequencies
-        return freqMap;
-    }
     public static void main(String[] args) {
         // TODO: Given an array of integers, write a Java program to find the frequency of each element using a Map
-        // Create an array of integers
-        int[] arr = {1, 2, 3, 4, 5, 1, 2, 3, 1, 2, 1};
-
-        // Print the array
-        System.out.println("Array: " + Arrays.toString(arr));
-
-        // Call the findFrequency method and print the result
-        Map<Integer, Integer> freqMap = findFrequency(arr);
-        System.out.println("Frequencies: " + freqMap);
     }
 }
