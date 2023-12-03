28aa3ee3e545f2d3c03579f9d79b6dfebe55b12e
Sun Tianyang <jsjtsty@126.com>
2023-12-03 20:18:02 +0800

    feat: implement class MyString

--- tasks/c++/hard/mystring.h
+++ tasks/c++/hard/mystring.h
@@ -2,11 +2,32 @@
 
 #ifndef MYSTRING_H
 #define MYSTRING_H
-#include <iostream>
+#include <ostream>
 
 class MyString {
-    // TODO: Implement MyString class
+public:
+	MyString();
+	MyString(const char* str);
+	MyString(const MyString& other);
+	~MyString() noexcept;
+
+	int length() const;
+	const char* c_str() const;
+	int compare(const MyString& other) const;
+	void append(const MyString& other);
+
+	MyString& operator=(const MyString& other);
+	MyString operator+(const MyString& other);
+	bool operator==(const MyString& other);
+
+	friend std::ostream& operator<<(std::ostream& os, const MyString& s);
+
+protected:
+
+	void clear();
+
+	char* data;
+	int len;
 };
 
 #endif
-
