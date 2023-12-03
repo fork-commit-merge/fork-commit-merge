adc63766a7081ac5c33be21011b6573eb2fa4536
GitHub Action <action@github.com>
2023-12-03 13:29:04 +0000

    Revert "Merge pull request #1722 from jsjtsty/fix-issue"
    This reverts commit 6e56b0418a4c1672cc661b3c9700272d1ed56651, reversing
    changes made to 918b7f71b6fa3ce3e7cae57200fce9475b44e935.

--- tasks/c++/hard/mystring.cpp
+++ tasks/c++/hard/mystring.cpp
@@ -1,140 +1,55 @@
+// C++ - Hard
+
 #include "mystring.h"
 #include <cstring>
+#include <iostream>
 
-MyString::MyString() {
-	this->clear();
+// Default constructor
+MyString::MyString() : str(nullptr), len(0) {}
+
+// Parameterized constructor
+MyString::MyString(const char* s) {
+    // TODO: Implement this
 }
 
-MyString::MyString(const char* str) {
-	if (!str) {
-		this->clear();
-		return;
-	}
-
-	int len = (int)strlen(str);
-	char* data = new char[len + 1];
-	if (data) {
-		this->len = len;
-		strcpy(data, str);
-		this->data = data;
-	} else {
-		this->clear();
-	}
-}
-
+// Copy constructor
 MyString::MyString(const MyString& other) {
-	int len = other.len;
-	char* data = new char[len + 1];
-	if (data) {
-		this->len = len;
-		strcpy(data, other.data);
-		this->data = data;
-	} else {
-		this->clear();
-	}
+    // TODO: Implement this
 }
 
-MyString::~MyString() noexcept {
-	if (data != nullptr) {
-		delete[] data;
-	}
+// Destructor
+MyString::~MyString() {
+    delete[] str;
 }
 
 int MyString::length() const {
-	return len;
+    // TODO: Implement this
 }
 
 const char* MyString::c_str() const {
-	return data;
-}
-
-int MyString::compare(const MyString& other) const {
-	if (!this->data && !other.data) {
-		return 0;
-	} else if (this->data && !other.data) {
-		return 1;
-	} else if (!this->data && other.data) {
-		return -1;
-	}
-
-	return strcmp(this->data, other.data);
-}
-
-MyString& MyString::operator=(const MyString& other) {
-	if (this != &other) {
-		if (other.len <= 0 || !other.data) {
-			this->clear();
-			return *this;
-		}
-
-		char* data = new char[other.len + 1];
-		if (data) {
-			this->len = other.len;
-			strcpy(data, other.data);
-			this->data = data;
-		} else {
-			this->clear();
-		}
-	}
-
-	return *this;
+    // TODO: Implement this
 }
 
 void MyString::append(const MyString& other) {
-	int len = this->len + other.len;
-	char* buffer = new char[len + 1];
-	if (!buffer) {
-		return;
-	}
-
-	if (this->len) {
-		memcpy(buffer, this->data, this->len);
-	}
-	if (other.len) {
-		memcpy(buffer + this->len, other.data, other.len);
-	}
-	buffer[len] = 0;
-
-	delete[] this->data;
-	this->data = buffer;
-	this->len = len;
+    // TODO: Implement this
 }
 
-MyString MyString::operator+(const MyString& other) {
-	int len = this->len + other.len;
-	char* buffer = new char[len + 1];
-	if (!buffer) {
-		return MyString();
-	}
-
-	if (this->len) {
-		memcpy(buffer, this->data, this->len);
-	}
-	if (other.len) {
-		memcpy(buffer + this->len, other.data, other.len);
-	}
-	buffer[len] = 0;
-	MyString result(buffer);
-	delete[] buffer;
-
-	return result;
+int MyString::compare(const MyString& other) const {
+    // TODO: Implement this
 }
 
-bool MyString::operator==(const MyString& other) {
-	return this->compare(other) == 0;
+MyString& MyString::operator=(const MyString& other) {
+    // TODO: Implement this
 }
 
-void MyString::clear() {
-	this->len = 0;
-	this->data = new char[1];
-	if (this->data) {
-		this->data[0] = 0;
-	}
+MyString MyString::operator+(const MyString& other) const {
+    // TODO: Implement this
+}
+
+bool MyString::operator==(const MyString& other) const {
+    // TODO: Implement this
 }
 
 std::ostream& operator<<(std::ostream& os, const MyString& s) {
-	if (s.data) {
-		os << s.data;
-	}
-	return os;
+    // TODO: Implement this
 }
--- tasks/c++/hard/mystring.h
+++ tasks/c++/hard/mystring.h
@@ -2,32 +2,11 @@
 
 #ifndef MYSTRING_H
 #define MYSTRING_H
-#include <ostream>
+#include <iostream>
 
 class MyString {
-public:
-	MyString();
-	MyString(const char* str);
-	MyString(const MyString& other);
-	~MyString() noexcept;
-
-	int length() const;
-	const char* c_str() const;
-	int compare(const MyString& other) const;
-	void append(const MyString& other);
-
-	MyString& operator=(const MyString& other);
-	MyString operator+(const MyString& other);
-	bool operator==(const MyString& other);
-
-	friend std::ostream& operator<<(std::ostream& os, const MyString& s);
-
-protected:
-
-	void clear();
-
-	char* data;
-	int len;
+    // TODO: Implement MyString class
 };
 
 #endif
+
