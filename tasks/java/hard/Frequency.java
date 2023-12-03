83cd7213dd24d7d225018300f07a6d55c11bb403
GitHub Action <action@github.com>
2023-12-03 09:12:37 +0000

    Revert "Merge pull request #1716 from HIT0638/main"
    This reverts commit 9f404d295e61dd894ad0b639dbfd6091e1e1893c, reversing
    changes made to 207048c6b00c6a8d7bd1b2675bd876e2ecf1f253.

--- tasks/java/hard/Frequency.java
+++ tasks/java/hard/Frequency.java
@@ -1,40 +1,8 @@
 // Java - Hard
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-
 public class Frequency {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter the number of elements in the array:");
-        int n = scanner.nextInt();
-
-        int[] array = new int[n];
-
-        System.out.println("Enter " + n + " integers:");
-        for (int i = 0; i < n; i++) {
-            array[i] = scanner.nextInt();
-        }
-
-        Map<Integer, Integer> frequencyMap = findFrequency(array);
-
-        // 打印结果
-        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
-            System.out.println("Element " + entry.getKey() + " occurs " + entry.getValue() + " times");
-        }
-
-        scanner.close();
-    }
-
-    public static Map<Integer, Integer> findFrequency(int[] array) {
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-
-        for (int num : array) {
-            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
-        }
-
-        return frequencyMap;
+        // TODO: Given an array of integers, write a Java program to find the frequency of each element using a Map
     }
 }
