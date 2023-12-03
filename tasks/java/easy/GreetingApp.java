3e3a79ead03935cc17e00a5558a9cf51b5b106aa
GitHub Action <action@github.com>
2023-12-03 09:16:47 +0000

    Revert "Merge pull request #1719 from Shubham-2110/Java-Easy"
    This reverts commit 7791af7ffb425bc420dc12db60973e5064d53809, reversing
    changes made to 9d5bb9a5ca6cbf8cf18463d487f921511d12c240.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,18 +1,8 @@
-import java.util.Scanner;
+// Java - Easy
 
 public class GreetingApp {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Prompt the user to enter their name
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-
-        // Print a greeting using the provided name
-        System.out.println("Hello, " + name + "! Nice to meet you!");
-
-        // Close the scanner
-        scanner.close();
+        // TODO: Implement a Simple Greeting Application
     }
 }
