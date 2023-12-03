9d5bb9a5ca6cbf8cf18463d487f921511d12c240
GitHub Action <action@github.com>
2023-12-03 09:15:36 +0000

    Revert "Merge pull request #1718 from Shubham-2110/Java-Hard"
    This reverts commit 72b48e5044a787623a08fa49d4231665ed5ba944, reversing
    changes made to 86f5e0135dc477fbf6741e602396b3603d87cd51.

--- tasks/java/hard/Frequency.java
+++ tasks/java/hard/Frequency.java
@@ -1,21 +1,8 @@
-import java.util.*;
+// Java - Hard
 
 public class Frequency {
 
     public static void main(String[] args) {
-        int[] numbers = { 2, 4, 5, 2, 6, 7, 4, 8, 2, 5, 4, 7, 9, 2 };
-
-        // Create a Map to store the frequency of each element
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-
-        // Calculate the frequency of each element in the array
-        for (int num : numbers) {
-            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
-        }
-
-        // Display the frequency of each element
-        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
-            System.out.println("Element: " + entry.getKey() + ", Frequency: " + entry.getValue());
-        }
+        // TODO: Given an array of integers, write a Java program to find the frequency of each element using a Map
     }
 }
