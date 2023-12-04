587268dc5bfc9344053b1a45d494391bbd868511
GitHub Action <action@github.com>
2023-12-04 08:18:34 +0000

    Revert "Merge pull request #1728 from MounikaVemparala/fix-issue"
    This reverts commit 77dfb454f97277bcf0b690357ac9f798f49336e9, reversing
    changes made to 30dcc72ed53b81e6d5a03471ebf4fc51b86939d0.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,35 +1,3 @@
 #include <stdio.h>
 
 // TODO: Implement the sum and main functions
-int sum(int a, int b) {
-    while (b != 0) {
-        // Carry contains common set bits of a and b
-        int carry = a & b;
-
-        // Sum of bits of a and b where at least one of the bits is not set
-        a = a ^ b;
-
-        // Carry is shifted by one so that adding it to a gives the required sum
-        b = carry << 1;
-    }
-    return a;
-}
-
-int main() {
-    // Variables to store user input
-    int num1, num2;
-
-    // Prompt the user to enter two integers
-    printf("Enter two numbers: ");
-
-    // Read the input using scanf
-    scanf("%d %d", &num1, &num2);
-
-    // Call the sum function to get the sum
-    int result = sum(num1, num2);
-
-    // Print the result using printf
-    printf("The sum is: %d\n", result);
-
-    return 0;
-}
