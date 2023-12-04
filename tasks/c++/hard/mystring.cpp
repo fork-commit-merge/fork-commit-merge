9b0c7e7a3a42f1ca3518090aec44a4273e8323b1
Mounika Vemparala <mounikavemparala@Mounikas-MacBook-Pro.local>
2023-12-03 20:48:39 -0500

    new-issue-branch2

--- tasks/c++/hard/mystring.cpp
+++ tasks/c++/hard/mystring.cpp
@@ -10,11 +10,22 @@ MyString::MyString() : str(nullptr), len(0) {}
 // Parameterized constructor
 MyString::MyString(const char* s) {
     // TODO: Implement this
+    if (s) {
+        len = strlen(s);
+        str = new char[len + 1];
+        strcpy(str, s);
+    } else {
+        str = nullptr;
+        len = 0;
+    }
 }
 
 // Copy constructor
 MyString::MyString(const MyString& other) {
     // TODO: Implement this
+     len = other.len;
+    str = new char[len + 1];
+    strcpy(str, other.str);
 }
 
 // Destructor
@@ -24,32 +35,57 @@ MyString::~MyString() {
 
 int MyString::length() const {
     // TODO: Implement this
+    return len;
 }
 
 const char* MyString::c_str() const {
     // TODO: Implement this
+    return str;
 }
 
 void MyString::append(const MyString& other) {
     // TODO: Implement this
+    int newLen = len + other.len;
+    char* newStr = new char[newLen + 1];
+    
+    strcpy(newStr, str);
+    strcat(newStr, other.str);
+
+    delete[] str;
+    str = newStr;
+    len = newLen;
 }
 
 int MyString::compare(const MyString& other) const {
     // TODO: Implement this
+    return strcmp(str, other.str);
 }
 
 MyString& MyString::operator=(const MyString& other) {
     // TODO: Implement this
+    if (this != &other) {  // Check for self-assignment
+        delete[] str;
+        len = other.len;
+        str = new char[len + 1];
+        strcpy(str, other.str);
+    }
+    return *this;
 }
 
 MyString MyString::operator+(const MyString& other) const {
     // TODO: Implement this
+    MyString result = *this;  // Copy constructor
+    result.append(other);
+    return result;
 }
 
 bool MyString::operator==(const MyString& other) const {
     // TODO: Implement this
+     return compare(other) == 0;
 }
 
 std::ostream& operator<<(std::ostream& os, const MyString& s) {
     // TODO: Implement this
+     os << s.str;
+    return os;
 }
