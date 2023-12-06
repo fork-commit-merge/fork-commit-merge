a0468369e5387311a77813c04cbf8abeacc7aaa5
Harishankar Ravindran <31157448+harishankarr7@users.noreply.github.com>
2023-12-06 05:04:03 -0500

    now the function sorts the input

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,8 +1,17 @@
-# Python - Medium
 
-# TODO: Create the Bubble Sort algorithm in Python
+def bubble_sort(arr):
+    n = len(arr)
+    # Traverse through all array elements
+    for i in range(n):
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+            # Traverse the array from 0 to n-i-1
+            # Swap if the element found is greater than the next element
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
-# Test cases (Don't edit):
+
+# Test cases
 arr = [64, 25, 12, 22, 11]
 print("Original array:", arr)
 
