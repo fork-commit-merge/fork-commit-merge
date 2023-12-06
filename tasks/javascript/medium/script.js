145da0fa24080abeedd61c405dfd6f31b9761876
Skandesh <42321593+Skandesh@users.noreply.github.com>
2023-12-06 19:46:46 +0530

    Fix Issue: Javascript-medium

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,3 +1,14 @@
 // JavaScript - Medium
 
 // Create functionality for a countdown timer that counts down from 10 seconds to zero using JavaScript
+
+let currentTime = document.getElementById("timer").innerHTML;
+let timeParts = currentTime.split(":");
+let timeNum = parseFloat(timeParts[1]);
+
+setInterval(() => {
+  if (timeNum > 0) {
+    timeNum = timeNum - 1;
+  }
+  document.getElementById("timer").innerHTML = "00 : " + timeNum;
+}, 1000);
