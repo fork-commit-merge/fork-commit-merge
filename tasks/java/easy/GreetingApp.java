b7c206d785f5062516149d3c3fe1bafccce69774
Pavs1605 <pavs1605@gmail.com>
2023-12-08 18:00:53 +0400

    Created greeting app

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,8 +1,17 @@
 // Java - Easy
+import java.util.Scanner;
 
 public class GreetingApp {
 
     public static void main(String[] args) {
         // TODO: Implement a Simple Greeting Application
+        Scanner userScannerObj = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+
+        String username = userScannerObj.nextLine();
+        System.out.println("Hello, " + username + "!nice to meet you!");
+
+        userScannerObj.close();
+
     }
 }
