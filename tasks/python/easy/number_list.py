b29df250b777eb956cb426496385231d5f75949a
Rupa-Rd <roopa.69.spr@gmail.com>
2023-12-09 16:58:28 +0530

    Solved Python Easy Task

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -6,4 +6,10 @@
 # prints the sum of the numbers of that list
 
 def function(numbers):
-    pass
+    natural_numbers = []
+    for number in range(numbers+1):
+        natural_numbers.append(number)
+    print(natural_numbers)
+    print(f"Sum of first {numbers} natural numbers is {sum(natural_numbers)}")
+
+function(10)
