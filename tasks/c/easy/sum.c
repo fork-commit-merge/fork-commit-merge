fe266ff7702467d047b01e9976e0b6295f8e0917
GitHub Action <action@github.com>
2023-12-09 08:15:50 +0000

    Revert "Merge pull request #1765 from DilushaHesaranga/main"
    This reverts commit b3133e8afa2beceeef3f027584c3fd77e63cb030, reversing
    changes made to ee5d80e1457cdd337f9367b041025716df92c73e.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,24 +1,3 @@
 #include <stdio.h>
 
-// Function to calculate the sum of two integers
-int sum(int a, int b) {
-    return a + b;
-}
-
-int main() {
-    int num1, num2;
-
-    // Prompt the user to enter two integers
-    printf("Enter two numbers: ");
-    
-    // Read the integers using scanf
-    scanf("%d %d", &num1, &num2);
-
-    // Call the sum function to get the sum
-    int result = sum(num1, num2);
-
-    // Print the result using printf
-    printf("The sum is: %d\n", result);
-
-    return 0;
-}
+// TODO: Implement the sum and main functions
