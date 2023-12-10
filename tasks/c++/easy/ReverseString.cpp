f837285cd7e66840e16b7de48ac157f9570ce2d0
GitHub Action <action@github.com>
2023-12-10 15:31:45 +0000

    Revert "Merge pull request #1773 from Rupa-Rd/main"
    This reverts commit 094ad4d2c9440bb4e84aa9bb0d6afad747fe3ebd, reversing
    changes made to 4abfdaeffd1602b5ac5440b8ad33253daf645b2c.

--- tasks/c++/easy/ReverseString.cpp
+++ tasks/c++/easy/ReverseString.cpp
@@ -1,27 +1,11 @@
 #include <iostream>
 #include <string>
-using namespace std;
-string reverseString(string &str) {
+
+std::string reverseString(const std::string &str) {
     // TODO: Implement the reverseString function
-    int i, len, temp;
-    len = str.length();
-
-
-    for(i = 0;i < len/2;i++)
-    {
-       
-        temp = str[i];
-        str[i] = str[len - i - 1];
-        str[len - i - 1] = temp;
-    }
-        
-    return str;
 }
 
 int main() {
     // TODO: Implement the main method
-    
-    string str = "fork-commit-merge";
-    reverseString(str);
-    cout<<str;
 }
+
--- tasks/c++/easy/sum.cpp
+++ tasks/c++/easy/sum.cpp
@@ -1,16 +1,3 @@
 // C++ - Easy 2
 
 // TODO: Create a Basic C++ Program to Add Two Numbers
-#include <iostream>
-using namespace std;
-int main() {
-  
-    int a,b,sum;
-    cout<<"Enter a: ";
-    cin>>a;
-    cout<<"Enter b: ";
-    cin>>b;
-    sum = a + b;
-    cout<<"Sum:"<<sum;
-    return 0;
-}
