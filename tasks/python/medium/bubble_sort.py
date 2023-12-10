6894a8785a7b3f2ab1fc629ab6634f85c6196ad3
GitHub Action <action@github.com>
2023-12-10 10:06:02 +0000

    Revert "Merge pull request #1768 from Rupa-Rd/main"
    This reverts commit 48f17e30bd99cb2286467d49713a314f076d00d3, reversing
    changes made to a090766793d9f74a8e7b96cb107381f9e2e30072.

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,10 +1,5 @@
 def bubble_sort(arr):
     # TODO: Implement the bubble sort algorithm
-    for i in range(len(arr) - 1,0,-1):
-        for j in range(i):
-            if arr[j] > arr[j+1]:
-                arr[j],arr[j+1] = arr[j+1],arr[j]
-            
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
