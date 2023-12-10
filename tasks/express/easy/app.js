33f5efc7e5db4bb53a0d6c1fcc7b50d83cfefab9
GitHub Action <action@github.com>
2023-12-10 15:47:45 +0000

    Revert "Merge pull request #1774 from DilushaHesaranga/main"
    This reverts commit 35ede89a623271bcc184455a3d7af268c4cfd862, reversing
    changes made to f837285cd7e66840e16b7de48ac157f9570ce2d0.

--- tasks/express/easy/app.js
+++ tasks/express/easy/app.js
@@ -1,22 +1,10 @@
-// TODO: Implement `get` method for / route
-// Import the Express.js module
-const express = require('express');
+// ExpressJS - Easy
 
-// Create an instance of the Express application
+const { get } = require("cheerio/lib/api/traversing");
+const express = require("express");
 const app = express();
-
-// Define the port number
 const port = 6969;
 
-// Set up a route that responds with "Hello, Express!" when accessed
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
-});
-
-// Start the server and listen on the specified port
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
+// TODO: Implement `get` method for / route
 
 // TODO: Implement `listen` method
