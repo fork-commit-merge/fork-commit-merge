cefb01ab279e83b0f04f0191378ad334ea29a011
GitHub Action <action@github.com>
2023-12-11 14:03:11 +0000

    Revert "Merge pull request #1781 from Rabbok/main"
    This reverts commit 81ab56821e17d62d79150cd2c4eda696534748c7, reversing
    changes made to 89812c3501fab2d5ab291e420b53dc4e66473566.

--- tasks/typescript/hard/main.ts
+++ tasks/typescript/hard/main.ts
@@ -10,30 +10,14 @@ interface Product {
 let products: Product[] = [];
 
 async function fetchData() {
-    const response = await fetch('https://dummyjson.com/products');
-    const data = await response.json();
-    const products = data;
+    // TODO: Implement the fetch function
     displayProducts(products);
 }
 
 function displayProducts(products: Product[]) {
-    // Clear previous rows in the table
-    const tableBody = document.getElementById('productBody')! as HTMLTableSectionElement;
-    tableBody.innerHTML = '';
-  
-    // Populate the table with products
-    products.forEach((product) => {
-      const row = tableBody.insertRow();
-      const titleCell = row.insertCell(0);
-      const priceCell = row.insertCell(1);
-      const ratingCell = row.insertCell(2);
-  
-      titleCell.textContent = product.title;
-      priceCell.textContent = product.price.toString();
-      ratingCell.textContent = product.rating.toString();
-    });
-  }
-  
+    // TODO: Implement the display function
+}
+
 function applyFilters() {
     const minPrice = parseFloat(
         (document.getElementById("minPrice") as HTMLInputElement).value
@@ -48,19 +32,11 @@ function applyFilters() {
         (document.getElementById("maxRating") as HTMLInputElement).value
     );
 
-    const filteredProducts = products.filter((product) => {
-        const priceInRange =
-          (isNaN(minPrice) || product.price >= minPrice) &&
-          (isNaN(maxPrice) || product.price <= maxPrice);
-    
-        const ratingInRange =
-          (isNaN(minRating) || product.rating >= minRating) &&
-          (isNaN(maxRating) || product.rating <= maxRating);
-    
-        return priceInRange && ratingInRange;
-      });
-    
-      displayProducts(filteredProducts);
+    const filteredProducts = products.filter(
+        // TODO: Implement the filter function
+    );
+
+    displayProducts(filteredProducts);
 }
 
 fetchData();
