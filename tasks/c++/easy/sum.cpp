13bca1ad8e0e14f26c597b321f679bd6c509e87c
GitHub Action <action@github.com>
2023-12-12 16:08:05 +0000

    Revert "Merge pull request #1787 from Ruhifarhath/patch-4"
    This reverts commit fcfe3fe29035cc0d7b08c3d61d31a467efe280da, reversing
    changes made to b278551f2f3ec6f30a466329d9d8a7fb1164db2b.

--- tasks/c++/easy/sum.cpp
+++ tasks/c++/easy/sum.cpp
@@ -1,16 +1,3 @@
 // C++ - Easy 2
 
 // TODO: Create a Basic C++ Program to Add Two Numbers
-int main()
-{
-  int num1;
-  int num2;
-  cout<<"Enter first number 1: "<<endl;
-  cin>>num1;
-  cout<<"Enter the second number : "<<endl;
-  cin>>num2;
-  cout<<"Sum of the two numbers is: "<<num1+num2<<endl;
-  return 0;
-  
-}
-
