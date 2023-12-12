ccb7ddc26ad91a52dae289625d66137201d74c8d
GitHub Action <action@github.com>
2023-12-12 06:36:00 +0000

    Revert "Merge pull request #1783 from harishankarr7/modifying-the-traffic-light-code"
    This reverts commit cbb2071a742fb8cb87fa32e3189fbeadda9f4b14, reversing
    changes made to c3a12255370cfd3f47eae80edebe19ec8aa3f92f.

--- tasks/react/medium/traffic-light/src/TrafficLight.tsx
+++ tasks/react/medium/traffic-light/src/TrafficLight.tsx
@@ -1,40 +1,11 @@
-import React, { useState, useEffect } from "react";
+// React.js - Medium 2
 import "./App.css";
 
-type Light = "red" | "yellow" | "green";
+// TODO: Implement  the TrafficLight component
 
 export function TrafficLight() {
-  const [activeLight, setActiveLight] = useState<Light>("red");
-
-  useEffect(() => {
-    const cycle = {
-      red: 4000, // Red light duration in milliseconds
-      green: 3000, // Green light duration
-      yellow: 1000, // Yellow light duration
-    };
-
-    const nextLight = {
-      red: "green",
-      green: "yellow",
-      yellow: "red",
-    };
-
-    const timeoutId = setTimeout(() => {
-      setActiveLight((current) => nextLight[current]);
-    }, cycle[activeLight]);
-
-    return () => clearTimeout(timeoutId); // Clean up the timeout
-  }, [activeLight]);
 
   return (
-    <div className="traffic-light">
-      <div className={`light red ${activeLight === "red" ? "active" : ""}`} />
-      <div
-        className={`light yellow ${activeLight === "yellow" ? "active" : ""}`}
-      />
-      <div
-        className={`light green ${activeLight === "green" ? "active" : ""}`}
-      />
-    </div>
+    // TODO: return JSX here
   );
 }
