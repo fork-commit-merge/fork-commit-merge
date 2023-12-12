b48b8fd380620bac809424243cb8485a1d67f70a
Harishankar Ravindran <31157448+harishankarr7@users.noreply.github.com>
2023-12-11 23:17:04 -0500

    added a todo list using the  vite server and react

--- tasks/react/medium/todo-app/src/App.jsx
+++ tasks/react/medium/todo-app/src/App.jsx
@@ -1,13 +1,61 @@
-// React - Medium
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  // TODO: Add the necessary functionality such as hooks here
+  const [todos, setTodos] = useState(() => {
+    // Retrieve the stored todos or initialize an empty array
+    const savedTodos = localStorage.getItem('todos');
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
+  const [input, setInput] = useState('');
+
+  // Function to add a new todo
+  const addTodo = () => {
+    if (!input) return; // Don't add empty todos
+    const newTodo = {
+      id: Date.now(),
+      text: input,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+    setInput(''); // Clear the input field after adding
+  };
+
+  // Function to delete a todo
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  // Function to update a todo
+  const updateTodo = (id, newText) => {
+    setTodos(todos.map(todo => todo.id === id ? { ...todo, text: newText } : todo));
+  };
+
+  // Store the todos in localStorage whenever they change
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
   return (
     <div className="App">
       <h1>To-Do List</h1>
-      {/* TODO: Add the To-Do list here */}
+      <input 
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Enter a new task"
+      />
+      <button onClick={addTodo}>Add Task</button>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <input 
+              value={todo.text}
+              onChange={(e) => updateTodo(todo.id, e.target.value)}
+            />
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
