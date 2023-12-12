322639e17daebd39173c5263c19cf6efac6ca3ce
Harishankar Ravindran <31157448+harishankarr7@users.noreply.github.com>
2023-12-12 00:25:25 -0500

    modified the traffic light code

--- tasks/react/medium/traffic-light/src/TrafficLight.tsx
+++ tasks/react/medium/traffic-light/src/TrafficLight.tsx
@@ -1,11 +1,40 @@
-// React.js - Medium 2
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-// TODO: Implement  the TrafficLight component
+type Light = "red" | "yellow" | "green";
 
 export function TrafficLight() {
+  const [activeLight, setActiveLight] = useState<Light>("red");
+
+  useEffect(() => {
+    const cycle = {
+      red: 4000, // Red light duration in milliseconds
+      green: 3000, // Green light duration
+      yellow: 1000, // Yellow light duration
+    };
+
+    const nextLight = {
+      red: "green",
+      green: "yellow",
+      yellow: "red",
+    };
+
+    const timeoutId = setTimeout(() => {
+      setActiveLight((current) => nextLight[current]);
+    }, cycle[activeLight]);
+
+    return () => clearTimeout(timeoutId); // Clean up the timeout
+  }, [activeLight]);
 
   return (
-    // TODO: return JSX here
+    <div className="traffic-light">
+      <div className={`light red ${activeLight === "red" ? "active" : ""}`} />
+      <div
+        className={`light yellow ${activeLight === "yellow" ? "active" : ""}`}
+      />
+      <div
+        className={`light green ${activeLight === "green" ? "active" : ""}`}
+      />
+    </div>
   );
 }
