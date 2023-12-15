59351e245a9ca639fac688d6b29850e4fa362d79
GitHub Action <action@github.com>
2023-12-15 07:57:21 +0000

    Revert "Merge pull request #1792 from manikanta799/forkmrge"
    This reverts commit d0b9573d91945d58affde08431bd14aa1d20d0ad, reversing
    changes made to a2d1bb7266c2e9913ac9a8b6ba1a71c99b4edc06.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,17 +1,3 @@
 #include <stdio.h>
 
-int sum(int a, int b) {
-    return a + b;
-}
-
-int main() {
-    int num1, num2;
-    printf("Enter two numbers: ");
-    scanf("%d %d", &num1, &num2);
-
-    int result = sum(num1, num2);
-
-    printf("The sum is: %d\n", result);
-
-    return 0;
-}
+// TODO: Implement the sum and main functions
