6a54838922f55472cd902b01ac40afafc30e0ab6
GitHub Action <action@github.com>
2023-12-17 18:53:08 +0000

    Revert "Merge pull request #1802 from Rupa-Rd/main"
    This reverts commit c27ff45c740d701318b5b102dff25c4fadd95082, reversing
    changes made to 9be372a80b9487d6341793c151fb2ad176215d90.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,13 +1,3 @@
 #include <stdio.h>
 
-void main() {
-  
-    int a,b,sum;
-    printf("A:");
-    scanf("%d",&a);
-    printf("B:");
-    scanf("%d",&b);
-    sum = a + b;
-    printf("Sum: %d",sum);
-    
-}
+// TODO: Implement the sum and main functions
