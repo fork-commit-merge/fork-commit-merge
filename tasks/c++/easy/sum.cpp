919b6d2c4f21904fc97cfca8d018049a17eb8076
Riyazul555 <riyazulislam2003@gmail.com>
2023-12-22 19:25:02 +0530

    Fixed C++ issue #1306 and other issues in the same folder

--- tasks/c++/easy/sum.cpp
+++ tasks/c++/easy/sum.cpp
@@ -1,3 +1,23 @@
 // C++ - Easy 2
 
 // TODO: Create a Basic C++ Program to Add Two Numbers
+
+#include <iostream>
+
+int main() {
+
+ int n, m;
+
+ std::cout<<"Enter your first number";
+ std::cin>>n;
+
+ std::cout<<"Enter your second number";
+ std::cin>>m;
+
+ int sum = n + m;
+
+ std::cout<<"the sum is "<<sum<< std::endl;
+
+ return 0;
+
+}
