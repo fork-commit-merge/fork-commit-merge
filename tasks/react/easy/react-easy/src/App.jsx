5340ec3ab71f76b5f843aa34733131f57b04ccb8
GitHub Action <action@github.com>
2023-12-22 19:07:56 +0000

    Revert "Merge pull request #1818 from Vince-arch/create-card-component"
    This reverts commit a824a8d1b47399ea7df5e147ac8eccf800d727d8, reversing
    changes made to d1ce2005cae4385486f229b6622a19a18ebc01e4.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -4,15 +4,7 @@ import "./index.css";
 function App() {
   return (
     <div className="center">
-      {<>
-        <h1>"React/Vite-Easy" in it</h1>
-        <div className= "card">
-          <h2>"Card Title"</h2>
-          <img src="https://loremflickr.com/320/240" alt="random image"></img>
-          <p> "Card Description"</p>
-        </div>
-      </>
-      }
+      {/* TODO: Implement the card component */}
     </div>
   );
 }
