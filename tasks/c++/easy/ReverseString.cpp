41c0382974a709d37bfe17dd4bcbf5033dce303c
GitHub Action <action@github.com>
2023-12-22 19:10:47 +0000

    Revert "Merge pull request #1820 from Riyazul555/Riyaz3"
    This reverts commit bc4e2858f0ca8a9d75791d676aa3b4c2d783d904, reversing
    changes made to df29bdc9ccb003d0e835463f934ece1d01196f49.

--- tasks/c++/easy/ReverseString.cpp
+++ tasks/c++/easy/ReverseString.cpp
@@ -2,28 +2,10 @@
 #include <string>
 
 std::string reverseString(const std::string &str) {
-    std::string reversedStr = str; // Copy the input string
-
-    // Reverse the copied string
-    for (size_t i = 0, j = reversedStr.length() - 1; i < j; ++i, --j) {
-        std::swap(reversedStr[i], reversedStr[j]);
-    }
-
-    return reversedStr;
+    // TODO: Implement the reverseString function
 }
 
 int main() {
-    // Get input string from the user
-    std::string inputStr;
-    std::cout << "Enter a string: ";
-    std::getline(std::cin, inputStr);
-
-    // Reverse the string
-    std::string reversedStr = reverseString(inputStr);
-
-    // Display the reversed string
-    std::cout << "Reversed string: " << reversedStr << std::endl;
-
-    return 0;
+    // TODO: Implement the main method
 }
 
--- tasks/c++/easy/tempConverter.cpp
+++ tasks/c++/easy/tempConverter.cpp
@@ -1,26 +1,3 @@
 // C++ - Easy 1
 
 // TODO:  Create a Basic C++ Program to Convert Temperature from Celsius to Fahrenheit
-
-
-#include <iostream>
-
-double celsiusToFahrenheit(double celsius) {
-    // Convert Celsius to Fahrenheit using the formula: F = (C * 9/5) + 32
-    return (celsius * 9.0 / 5.0) + 32.0;
-}
-
-int main() {
-    // Get temperature in Celsius from the user
-    double celsius;
-    std::cout << "Enter temperature in Celsius: ";
-    std::cin >> celsius;
-
-    // Convert Celsius to Fahrenheit
-    double fahrenheit = celsiusToFahrenheit(celsius);
-
-    // Display the converted temperature
-    std::cout << "Temperature in Fahrenheit: " << fahrenheit << std::endl;
-
-    return 0;
-}
