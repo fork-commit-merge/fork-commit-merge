5a6f831f46eebf4c696434e11288f0994229f4e2
GitHub Action <action@github.com>
2023-12-22 19:11:52 +0000

    Revert "Merge pull request #1821 from Riyazul555/Riyaz4"
    This reverts commit 272632dcd437be0c8271e359ff46fc46d8a18b0f, reversing
    changes made to 41c0382974a709d37bfe17dd4bcbf5033dce303c.

--- tasks/java/hard/Frequency.java
+++ tasks/java/hard/Frequency.java
@@ -1,26 +1,8 @@
 // Java - Hard
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Frequency {
 
     public static void main(String[] args) {
-        // Given an array of integers
-        int[] numbers = {1, 2, 3, 1, 2, 3, 4, 5, 1, 2, 2};
-
-        // Create a Map to store the frequency of each element
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-
-        // Count the frequency of each element
-        for (int num : numbers) {
-            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
-        }
-
-        // Print the frequency of each element
-        System.out.println("Frequency of each element:");
-        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue() + " times");
-        }
+        // TODO: Given an array of integers, write a Java program to find the frequency of each element using a Map
     }
 }
--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -2,18 +2,3 @@
 
 // TODO: Write a function that returns the sum of two numbers
 
-// Function to calculate the sum of two numbers
-function addNumbers(num1, num2) {
-    return num1 + num2;
-}
-
-// Example usage
-const number1 = 5;
-const number2 = 10;
-
-const sum = addNumbers(number1, number2);
-
-// Display the result
-console.log(`The sum of ${number1} and ${number2} is: ${sum}`);
-
-
