eb4ec20985b35232348d6353fc6a53563d3365e3
LandaRashmi <129966632+LandaRashmi@users.noreply.github.com>
2023-12-23 23:01:20 +0530

    Update ReverseString.cpp

--- tasks/c++/easy/ReverseString.cpp
+++ tasks/c++/easy/ReverseString.cpp
@@ -3,9 +3,16 @@
 
 std::string reverseString(const std::string &str) {
     // TODO: Implement the reverseString function
+    reverse(str.begin(),str.end());
+    return str;
 }
 
 int main() {
     // TODO: Implement the main method
+    string str;
+    cout<<"Enter a string:";
+    cin>>str;
+    reverseString(str);
+    cout<<"Reversed String:"<<str<<endl;
 }
 
