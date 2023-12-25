43f635c555fdf039ae6333f03f20b051e3782ecc
Vru007 <vrushik7143work@gmail.com>
2023-12-25 11:00:57 +0530

    Issue-solved

--- tasks/react/medium/traffic-light/src/TrafficLight.tsx
+++ tasks/react/medium/traffic-light/src/TrafficLight.tsx
@@ -1,11 +1,56 @@
 // React.js - Medium 2
+import { useEffect, useState } from "react";
 import "./App.css";
 
 // TODO: Implement  the TrafficLight component
 
 export function TrafficLight() {
+  const [Redcolor, setRed] = useState("");
+  const [Yellowcolor, setYellow] = useState("");
+  const [Greencolor, setGreen] = useState("");
+  
+  
+  const settingRed=()=> {
+    
+    setRed("Red");
+    setGreen("");
+      setTimeout(() => {
+        settingYellow();
+      }, 4000)};
+
+      const settingYellow=()=> {
+      
+          setRed("");
+          setYellow("Yellow");
+        setTimeout(() => {
+          
+          settingGreen();
+        }, 3000)};
+
+        const settingGreen=() => {
+        
+            setYellow("");
+            setGreen("Green");
+          setTimeout(() => {
+            
+            settingRed();
+          }, 1000)};
+       
+    useEffect(()=>{
+       settingRed();
+    },[]);    
+    
+    
 
   return (
     // TODO: return JSX here
+  
+    <div className="traffic-light-div">
+      <div className="traffic-light">
+        <div className={`light red ${(Redcolor=="Red" ?"active":"unactive")}`}></div>
+        <div className={`light yellow ${(Yellowcolor=="Yellow" ?"active":"unactive")}`}></div>
+        <div className={`light green ${(Greencolor=="Green" ?"active":"unactive")}`}></div>
+      </div>
+    </div>
   );
 }
