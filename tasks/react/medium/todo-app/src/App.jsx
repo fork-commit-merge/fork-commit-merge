ccfd3b9175a588b0770ac40869cc07ab02dd120d
GitHub Action <action@github.com>
2023-12-25 19:18:21 +0000

    Revert "Merge pull request #1833 from DaveSimoes/main"
    This reverts commit aee3c36677eb286cc4775c967d79bd0a3ec77aac, reversing
    changes made to cbe1356113c3046d4e7b39847bbfe5114c103f8f.

--- tasks/react/medium/todo-app/src/App.jsx
+++ tasks/react/medium/todo-app/src/App.jsx
@@ -1,76 +1,13 @@
 // React - Medium
-import "./App.css";
-import React, { useState, useEffect } from 'react';
 
+import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState('');
-  const [editingId, setEditingId] = useState(null);
-
-  // Load todos from localStorage on initial render
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem('todos'));
-    if (storedTodos) {
-      setTodos(storedTodos);
-    }
-  }, []);
-
-  // Save todos to localStorage whenever todos state changes
-  useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos));
-  }, [todos]);
-
-  const addTodo = () => {
-    if (newTodo.trim() !== '') {
-      setTodos([...todos, { id: Date.now(), text: newTodo }]);
-      setNewTodo('');
-    }
-  };
-
-  const deleteTodo = (id) => {
-    const updatedTodos = todos.filter((todo) => todo.id !== id);
-    setTodos(updatedTodos);
-  };
-
-  const updateTodo = (id, newText) => {
-    const updatedTodos = todos.map((todo) =>
-      todo.id === id ? { ...todo, text: newText } : todo
-    );
-    setTodos(updatedTodos);
-    setEditingId(null);
-  };
-
+  // TODO: Add the necessary functionality such as hooks here
   return (
     <div className="App">
       <h1>To-Do List</h1>
-      <input
-        type="text"
-        value={newTodo}
-        onChange={(e) => setNewTodo(e.target.value)}
-        placeholder="Enter a new task"
-      />
-      <button onClick={addTodo}>Add</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            {editingId === todo.id ? (
-              <input
-                type="text"
-                value={todo.text}
-                onChange={(e) => updateTodo(todo.id, e.target.value)}
-                onBlur={() => setEditingId(null)}
-              />
-            ) : (
-              <>
-                {todo.text}
-                <button onClick={() => setEditingId(todo.id)}>Edit</button>
-                <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-              </>
-            )}
-          </li>
-        ))}
-      </ul>
+      {/* TODO: Add the To-Do list here */}
     </div>
   );
 }
