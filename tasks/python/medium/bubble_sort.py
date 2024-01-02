edfd8987126ce84b49bd245ed3b75371da558fc8
GitHub Action <action@github.com>
2024-01-02 20:47:14 +0000

    Revert "Merge pull request #1850 from izumigoto/main"
    This reverts commit 49ae27124e2bd29446f00e5af08cd66a88e1792d, reversing
    changes made to 8c7953ef56745ab5c8ccd30f34ef8c7c6f87be27.

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,14 +1,7 @@
 def bubble_sort(arr):
-    n = len(arr)
+    # TODO: Implement the bubble sort algorithm
 
-    # Traverse through all elements in the list
-    for i in range(n):
-        # Last i elements are already in place, so we don't need to check them
-        for j in range(0, n-i-1):
-            # Swap if the element found is greater than the next element
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    return arr
+#! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
 print("Original array:", arr)
 
