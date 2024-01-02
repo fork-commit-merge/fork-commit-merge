6b5deabf2701ae144b69f2bc117a65bf08afe41a
GitHub Action <action@github.com>
2024-01-02 17:16:25 +0000

    Revert "Merge pull request #1849 from mohitmahur/fix-issue"
    This reverts commit b8bf773895cff3a26ce50edf8be4ee4bebe9ac69, reversing
    changes made to 8f06204e538b5ed8a959beed20d09672b1bab054.

--- tasks/java/hard/Frequency.java
+++ tasks/java/hard/Frequency.java
@@ -1,21 +1,8 @@
 // Java - Hard
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Frequency {
 
     public static void main(String[] args) {
-        int[] array = {1,2,1,1,2,2,3,3,4,4,5,5,5,1,1,3};
-        Map<Integer, Integer> FreqMap = new HashMap<>();
-        for ( int element : array){
-            FreqMap.put(element, FreqMap.getOrDefault(element, 0)+1);
-
-        }
-        System.out.println("element : frequency");
-        for ( Map.Entry<Integer, Integer> entry : FreqMap.entrySet()){
-            System.out.println(entry.getKey()+ " : " + entry.getValue());
-        }
         // TODO: Given an array of integers, write a Java program to find the frequency of each element using a Map
     }
 }
--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -3,18 +3,6 @@
 package tasks.java.medium;
 
 public class Calculator {
-    public int add(int a, int b){
-        return a+b;
-    }
-    public int subtract(int a , int b){
-        return a-b;
-    }
-    public int multiply(int a , int b){
-        return a*b;
-    }
-    public float divide(int a , int b){
-        return a/b;
-    }
     // TODO: Implement the Calculator class
 
     public static void main(String[] args) {
