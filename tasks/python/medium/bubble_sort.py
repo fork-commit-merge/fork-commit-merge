3f7e4906c39cdeb58f47c0b7c8b3f3af37a4ebe9
S.Lakshmi Vignesh <23f1002745@ds.study.iitm.ac.in>
2024-01-02 23:17:55 +0530

    added bubblesort.py

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,7 +1,14 @@
 def bubble_sort(arr):
-    # TODO: Implement the bubble sort algorithm
+    n = len(arr)
 
-#! Test cases (Don't edit):
+    # Traverse through all elements in the list
+    for i in range(n):
+        # Last i elements are already in place, so we don't need to check them
+        for j in range(0, n-i-1):
+            # Swap if the element found is greater than the next element
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
 arr = [64, 25, 12, 22, 11]
 print("Original array:", arr)
 
