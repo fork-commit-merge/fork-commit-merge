7aa7893fb0b5f0590a8daf9697f06e68f6518679
nitinya9av <nitinya9av@gmail.com>
2024-01-03 21:38:17 +0530

    Fixed issue

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -5,5 +5,9 @@
 # prints that list to the console and,
 # prints the sum of the numbers of that list
 
-def function(numbers):
-    pass
+def function(numbers):  
+    print(numbers)  
+    print(sum(numbers))
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+function(numbers)
