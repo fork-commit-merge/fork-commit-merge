3f04f42cdd327e7f8dde68c96ea3d5685a900083
GitHub Action <action@github.com>
2024-01-03 15:42:32 +0000

    Revert "Merge pull request #1854 from Darshanroy/esay_python1"
    This reverts commit 88e65fd31d1934bf73ade8f9b107a5dfc440e609, reversing
    changes made to ab98c1e6bcfcf96aff6b5b50602a3330d39cf61a.

--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,10 +1,3 @@
 # Python - Easy
 
 # TODO: Write a function that returns the sum of two numbers
-
-# Python function to return the sum of two numbers
-def add(a,b):
-    return a + b
-
-# Function call
-print(add(103,30))
