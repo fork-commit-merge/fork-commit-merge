080b464fdde139cc1e7d7c885a9252fba2d9d02c
GitHub Action <action@github.com>
2024-01-03 16:22:18 +0000

    Revert "Merge pull request #1859 from nitinya9av/fix-issue"
    This reverts commit d2719c763f68e961c345415b08b9c9671eece833, reversing
    changes made to 3f6577494d77bfece0a7c19ea3bc56520213f322.

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -5,9 +5,5 @@
 # prints that list to the console and,
 # prints the sum of the numbers of that list
 
-def function(numbers):  
-    print(numbers)  
-    print(sum(numbers))
-
-numbers = [1,2,3,4,5,6,7,8,9,10]
-function(numbers)
+def function(numbers):
+    pass
