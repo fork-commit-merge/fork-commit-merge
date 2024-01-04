2bf3dda841d27a4e1a13725958b5bbe32b2910ad
Karthik110505 <bvvkarthik1155@gmail.com>
2024-01-04 16:27:32 +0530

    Bubble sort in python3-medium issue

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,5 +1,13 @@
 def bubble_sort(arr):
-    # TODO: Implement the bubble sort algorithm
+    n = len(arr)
+    if n <=1:
+        return 
+    for i in range(1,n):
+        for j in range(0,n-i):
+            if arr[j] > arr[j+1]:
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1]= temp
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
