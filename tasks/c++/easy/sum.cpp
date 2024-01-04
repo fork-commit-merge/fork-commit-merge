e27925db9d5410dca9b47ae5df25f3a130fc0966
trisha <bl.en.u4cse22065@bl.students.amrita.edu>
2024-01-05 01:00:58 +0530

    sum using c++ commit

--- tasks/c++/easy/sum.cpp
+++ tasks/c++/easy/sum.cpp
@@ -1,3 +1,14 @@
 // C++ - Easy 2
 
 // TODO: Create a Basic C++ Program to Add Two Numbers
+#include <iostream>
+using namespace std;
+int main(){
+	int a, b;
+	cout << "Enter First Number"<<endl;
+	cin >> a;
+	cout << "Enter Second Number"<<endl;
+	cin >> b;
+	cout << "The sum is:" << a+b << endl;
+	return 0;
+}
