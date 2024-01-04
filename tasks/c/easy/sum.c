ab6ca89ecc66cd0506f6b5bf6c1580a5228bb562
trisha <bl.en.u4cse22065@bl.students.amrita.edu>
2024-01-05 00:44:03 +0530

    sum c program commit

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 
 // TODO: Implement the sum and main functions
+int sum(int a, int b){
+	int res = a + b;
+	return res;
+}
+int main(){
+	int x; 
+	printf("enter first number\n");
+	scanf("%d", &x);
+	int y;
+	printf("enter second number\n");
+	scanf("%d", &y);
+	int s = sum(x,y);
+	printf("the sum is %d", s);
+}
