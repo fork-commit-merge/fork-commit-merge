cf99ace4edde5ce59edeeb8024d4bde9e5b2fbd0
GitHub Action <action@github.com>
2024-01-05 22:10:17 +0000

    Revert "Reapply "Reapply "Merge pull request #1915 from izumigoto/octave"""
    This reverts commit 56de01a991b0efde5a3a4c5dad8e137fa9462d45.

--- tasks/kotlin/easy/GreetingApp.kt
+++ tasks/kotlin/easy/GreetingApp.kt
@@ -1,10 +1,5 @@
+// Kotlin - Easy
+
 fun main() {
-    // Prompt the user to enter a string
-    print("Enter something: ")
-
-    // Read user input from the console
-    val userInput = readLine()
-
-    // Print the user's input
-    println("You entered: $userInput")
+    // TODO: Create a Simple Kotlin Program to Print User's Input
 }
