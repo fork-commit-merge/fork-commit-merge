908f14ffb92fc97a8a02694e51776932033e92d2
GitHub Action <action@github.com>
2024-01-05 22:20:33 +0000

    Revert "Merge pull request #1905 from nitinya9av/fix-issue-8"
    This reverts commit 3865d12cd9e1f5aee9ded5117c9b4cd540e10b66, reversing
    changes made to 305711d9a302ffd35695de2c24e884ad2a104618.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,13 +1,3 @@
 #include <stdio.h>
 
-int sum(int a, int b) {
-    return a + b;
-}
-
-int main() {
-    int a = 5;
-    int b = 3;
-    int result = sum(a, b);
-    printf("The sum of %d and %d is: %d\n", a, b, result);
-    return 0;
-}
+// TODO: Implement the sum and main functions
