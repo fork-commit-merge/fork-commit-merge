5d5b00a3d01eca61ec698f8a75a70ca2133e65d2
GitHub Action <action@github.com>
2024-01-05 07:55:39 +0000

    Revert "Merge pull request #1878 from Karthik110505/new_branch5"
    This reverts commit ea24d2702cfca9ac8c6b1d1df41b3b69558febcc, reversing
    changes made to fb78829e9234f7aa64e2a1bdd43cbde10c84933f.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,17 +1,3 @@
 #include <stdio.h>
 
-int sum(int a, int b){
-    return a+b;
-}
-
-void main(){
-    int num1,num2;
-    printf("Enter 1st number ");
-    scanf("%d",&num1);
-    printf("Enter 2nd number ");
-    scanf("%d",&num2);
-    int added;
-    added = sum(num1,num2);
-    printf("Sum of %d and %d is %d \n",num1,num2,added);
-
-}
+// TODO: Implement the sum and main functions
