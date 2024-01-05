7a93316d674a3c5d799f7bb365ae031cf7631d5a
nitinya9av <nitinya9av@gmail.com>
2024-01-05 15:14:57 +0530

    Fixed issue

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,3 +1,20 @@
 // JavaScript - Medium
 
 // Create functionality for a countdown timer that counts down from 10 seconds to zero using JavaScript
+
+let countdown = 10;
+const timerElement = document.getElementById('timer');
+const messageElement = document.getElementById('timer');
+
+const timerId = setInterval(() => {
+    let formattedCountdown = `00:${String(countdown).padStart(2, '0')}`;
+    timerElement.textContent = formattedCountdown;
+
+    if (countdown <= 0) {
+        clearInterval(timerId);
+        timerElement.textContent = '';
+        messageElement.textContent = "Time's up!";
+    } else {
+        countdown--;
+    }
+}, 1000);
