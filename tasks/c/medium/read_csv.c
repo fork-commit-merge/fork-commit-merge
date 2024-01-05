424380bfdbb3bd1aaa5eeb3924e62bd25b829f08
nitinya9av <nitinya9av@gmail.com>
2024-01-05 23:29:20 +0530

    Fixed issue

--- tasks/c/medium/read_csv.c
+++ tasks/c/medium/read_csv.c
@@ -4,7 +4,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int main() {
-    // TODO: Implement the program that reads and displays the contents of data.csv file
+    FILE *file = fopen("data.csv", "r");
+    if (file == NULL) {
+        printf("Cannot open file\n");
+        return 1;
+    }
+
+    char line[100];
+    while (fgets(line, sizeof(line), file)) {
+        printf("%s", line);
+    }
+
+    fclose(file);
     return 0;
 }
