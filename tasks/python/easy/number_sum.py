ed81bdfa22a7a290c12376efeaa528034333558f
GitHub Action <action@github.com>
2024-01-05 07:39:03 +0000

    Revert "Merge pull request #1868 from trish404/easytsk"
    This reverts commit 4a0f54b63b595eaf6c0cc2c7e82cf1f8b2731eb0, reversing
    changes made to d5b176315fc47239d09ad1e3f9728b6ca6ef28aa.

--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,12 +1,3 @@
 # Python - Easy
 
 # TODO: Write a function that returns the sum of two numbers
-def sum_of_two_nos(n,m):
-	res = n + m
-	return res
-
-a = int(input("enter the first number\n"))
-b = int(input("enter the second number\n"))
-
-c = sum_of_two_nos(a,b)
-print("sum =", c)
