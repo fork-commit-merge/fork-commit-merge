faffd7ffb502ae3161b6e51d7f08f0c30db26c74
GitHub Action <action@github.com>
2024-01-05 07:47:25 +0000

    Revert "Merge pull request #1873 from himanshu-garg-ds/fix-issue"
    This reverts commit 6f37545246adec9ce50af72508a4c90366382ebd, reversing
    changes made to 8fe221b2cdf15b484fc915819d1a603c0118b35b.

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,24 +1,7 @@
 def bubble_sort(arr):
-  # inital info
-  n = len(arr)
+    # TODO: Implement the bubble sort algorithm
 
-  for i in range(n - 1):
-    is_sorted = True
-
-    # putting the (i + 1) largest element at (n - i - 1) position
-    for j in range(n - i - 1):
-      if arr[j] > arr[j + 1]:
-        arr[j], arr[j + 1] = arr[j + 1], arr[j]
-        is_sorted = False
-	
-		# since this prefix is sorted, its subprefixes too hence we don't need to check further
-    if is_sorted: 
-      break
-
-	#sorted arr
-  return arr
-
-# Test cases (Don't edit):
+#! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
 print("Original array:", arr)
 
