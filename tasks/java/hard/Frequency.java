58deb0c25f4e15f4886f1ec77930659ad1d1f06b
GitHub Action <action@github.com>
2024-01-05 21:30:22 +0000

    Revert "Merge pull request #1886 from nitinya9av/fix-issue-3"
    This reverts commit 7745b9e459d6488ef1b61864b6b290dd0afb5ce1, reversing
    changes made to 3d0482d1ca6eab1da2594a2de1919a68b26e1eea.

--- tasks/java/hard/Frequency.java
+++ tasks/java/hard/Frequency.java
@@ -1,20 +1,8 @@
-import java.util.HashMap;
-import java.util.Map;
+// Java - Hard
 
 public class Frequency {
 
     public static void main(String[] args) {
-        int[] array = {10, 20, 20, 30, 10, 10, 20};  // Sample array for demonstration
-
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-
-        for (int num : array) {
-            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
-        }
-
-        // Print the frequency of each element
-        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
-            System.out.println("Element " + entry.getKey() + " occurs " + entry.getValue() + " times");
-        }
+        // TODO: Given an array of integers, write a Java program to find the frequency of each element using a Map
     }
 }
