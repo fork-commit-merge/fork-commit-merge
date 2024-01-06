e0cf44062b286c80478eee0e24e24910a1d6a9c8
nitinya9av <nitinya9av@gmail.com>
2024-01-06 14:55:30 +0530

    Fixed issue

--- tasks/rust/medium/src/main.rs
+++ tasks/rust/medium/src/main.rs
@@ -4,4 +4,25 @@ use std::io;
 
 fn main() {
     // TODO: Create a program in Rust that converts temperatures between Fahrenheit and Celsius
+    let mut input = String::new();
+    println!("Enter 1 to convert from Fahrenheit to Celsius, or 2 to convert from Celsius to Fahrenheit: ");
+    io::stdin().read_line(&mut input).unwrap();
+    let choice: u32 = input.trim().parse().unwrap();
+
+    input.clear();
+    println!("Enter temperature: ");
+    io::stdin().read_line(&mut input).unwrap();
+    let temperature: f32 = input.trim().parse().unwrap();
+
+    match choice {
+        1 => {
+            let celsius = (temperature - 32.0) * 5.0/9.0;
+            println!("Temperature in Celsius: {}", celsius);
+        },
+        2 => {
+            let fahrenheit = temperature * 9.0/5.0 + 32.0;
+            println!("Temperature in Fahrenheit: {}", fahrenheit);
+        },
+        _ => println!("Invalid choice"),
+    }
 }
