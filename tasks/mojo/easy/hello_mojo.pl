1486cb550372a637985a48f16c9850041b6cb49a
slvignesh05@gmail.com <slvignesh05@gmail.com>
2024-01-06 20:34:09 +0530

    added hello_mojo.pl

--- tasks/mojo/easy/hello_mojo.pl
+++ tasks/mojo/easy/hello_mojo.pl
@@ -1,6 +1,9 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite -signatures;
 
-# TODO: Create a Route leading to a "Hello, Mojo!" text
+# Route leading to a "Hello, Mojo!" text
+get '/' => sub ($c) {
+    $c->render(text => 'Hello, Mojo!');
+};
 
 app->start;
