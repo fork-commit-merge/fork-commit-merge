c9376c92aeaeb1813611532feaca55f8707f2de7
slvignesh05@gmail.com <slvignesh05@gmail.com>
2024-01-06 09:53:47 +0530

    added Triangle_area.f90

--- tasks/fortran/easy/triangle_area.f90
+++ tasks/fortran/easy/triangle_area.f90
@@ -1,5 +1,19 @@
-! Fortran - Easy
-
 program triangle_area
-    ! TODO: Implement the program here
+    implicit none
+
+    real :: base, height, area
+
+    ! TODO: Get input for base and height
+    write(*,*) "Enter the base of the triangle:"
+    read(*,*) base
+
+    write(*,*) "Enter the height of the triangle:"
+    read(*,*) height
+
+    ! TODO: Calculate the area
+    area = 0.5 * base * height
+
+    ! TODO: Display the result
+    write(*,*) "The area of the triangle is:", area
+
 end program triangle_area
