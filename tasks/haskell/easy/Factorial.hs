ac60f92c8a1cdf9b5e59ce94beab17a7e0112ab3
slvignesh05@gmail.com <slvignesh05@gmail.com>
2024-01-06 10:34:50 +0530

    added Factorial.hs

--- tasks/haskell/easy/Factorial.hs
+++ tasks/haskell/easy/Factorial.hs
@@ -3,3 +3,14 @@
 main :: IO ()
 main = do
     -- TODO: Implement a Haskell program that calculates the factorial of a number
+main :: IO ()
+main = do
+    putStrLn "Enter a number:"
+    input <- getLine
+    let number = read input :: Integer
+        factorialResult = factorial number
+    putStrLn $ "Factorial of " ++ show number ++ " is: " ++ show factorialResult
+
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
