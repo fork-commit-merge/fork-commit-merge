4cb6c75a8daee3f66476ea0240d7ecefa5913b88
Karthik110505 <bvvkarthik1155@gmail.com>
2024-01-06 14:57:29 +0530

    MATLAB EASY

--- tasks/matlab/easy/addition.m
+++ tasks/matlab/easy/addition.m
@@ -1,3 +1,6 @@
 % MATLAB - Easy
 
-% TODO: Create a MATLAB script that adds two numbers together and prints the result
+n1 =12;
+n2 =234;
+
+fprintf('The sum of numbers %d , %d : %d \n',n1,n2,n1+n2);
