224d8ec7c4e532104b67f02cd540bd7aa9a24fc7
GitHub Action <action@github.com>
2024-01-06 14:38:16 +0000

    Revert "Merge pull request #1952 from nitinya9av/fix-issue-34"
    This reverts commit 7ac12ec2d3c93162fa25cc7d33bb5887de60c98a, reversing
    changes made to 05902194a7c9807c17c3adb0eac133ee2d2ce1a0.

--- tasks/rust/easy/fibonacci.rs
+++ tasks/rust/easy/fibonacci.rs
@@ -3,18 +3,3 @@
 use std::io;
 
 // TODO: Implement fibonacci sequence in Rust
-
-fn fibonacci(n: u32) -> u32 {
-    match n {
-        0 => 0,
-        1 => 1,
-        _ => fibonacci(n - 1) + fibonacci(n - 2),
-    }
-}
-
-fn main() {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    let n: u32 = input.trim().parse().unwrap();
-    println!("{}", fibonacci(n));
-}
