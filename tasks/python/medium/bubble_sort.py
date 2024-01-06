649cee8b78de8b48220a35628904df427f8c2e19
christiano-developer <christiano.developer05@gmail.com>
2024-01-07 00:06:42 +0530

    fixed-issue

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,5 +1,15 @@
 def bubble_sort(arr):
     # TODO: Implement the bubble sort algorithm
+    noswap = False
+    while(not noswap):
+        noswap = True
+        for i in range(len(arr)-1):
+            if arr[i] > arr[i+1]:
+                noswap = False
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
