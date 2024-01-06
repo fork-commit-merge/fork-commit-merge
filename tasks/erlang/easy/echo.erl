bf772e067efe7cbbc7038da505c2a9d61158e606
slvignesh05@gmail.com <slvignesh05@gmail.com>
2024-01-06 09:52:05 +0530

    added echo.erl

--- tasks/erlang/easy/echo.erl
+++ tasks/erlang/easy/echo.erl
@@ -1,7 +1,16 @@
-% Erlang - Easy
-
 -module(echo).
 -export([start/0]).
 
 start() ->
-    % TODO: Implement the echo program
+    io:format("Welcome to the echo program!~n"),
+    echo_loop().
+
+echo_loop() ->
+    {ok, Input} = io:get_line("Enter text (or 'exit' to quit): "),
+    case Input of
+        "exit\n" ->
+            io:format("Goodbye!~n");
+        _ ->
+            io:format("You entered: ~s~n", [Input]),
+            echo_loop()
+    end.
