331c23a5641b2bf1d03016de7332ae996cdf6207
Jitesh Pahwa <jiteshofficial2004@gmail.com>
2024-01-06 22:59:52 +0530

    Written the temperature converter in C++

--- tasks/c++/easy/tempConverter.cpp
+++ tasks/c++/easy/tempConverter.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+using namespace std;
+
+float convertToFarenheit(float temp){
+  float newTemp = temp*(9.0/5) + 32.0;
+  return newTemp;
+}
 
 int main() {
   // TODO: Implement the temperature converter
+  float temp_celcius;
+  cout<<"Enter the temperature in celcius: ";
+  cin>>temp_celcius;
+  float farenheit_temp = convertToFarenheit(temp_celcius);
+  cout<<"The temperature in farenheit is: "<<farenheit_temp<<endl;
+  return 0;
 }
