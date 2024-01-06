fe36d5ce61b80a67e11185528a51091489a6246e
GitHub Action <action@github.com>
2024-01-06 14:52:01 +0000

    Revert "Merge pull request #1945 from Karthik110505/new_branch3"
    This reverts commit 6ac62c052857a68f1fdafae6a244cd48338a59c5, reversing
    changes made to 81d3f30ef4ef5cc84c35ab591fc53dcacbe946f9.

--- tasks/c/medium/read_csv.c
+++ tasks/c/medium/read_csv.c
@@ -4,17 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void main() {
-    FILE *data;
-    data  = fopen("data.csv", "r");
-    if (data == NULL) {
-        printf("Error while opening the file or File doesn't exists\n");
-    }
 
-    char text[100];
-    while (fgets(text, sizeof(text), data)) {
-        printf("%s", text);
-    }
-
-    fclose(data);
+int main() {
+//    TODO: Implement the program requested in the task description
 }
