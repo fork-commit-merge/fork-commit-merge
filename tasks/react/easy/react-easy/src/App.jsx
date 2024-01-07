730cf921786df0545c2b7e9c1f9b889b7880a6c1
kishlay42 <kishlayk7357@gmail.com>
2024-01-08 00:28:15 +0530

    fifth  commit

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -2,11 +2,14 @@
 import "./index.css";
 
 function App() {
-  return (
-    <div className="center">
-      {/* TODO: Implement the card component */}
+  return <div className="center">
+    <h1>React/Vite-Easy</h1>
+    <div className="card">
+      <img src="https://loremflickr.com/320/240" alt="randomImage" />
+      <h2>Card Title</h2>
+      <p>Card Description</p>
     </div>
-  );
+  </div>;
 }
 
 export default App;
