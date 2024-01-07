e1ffd70e5758e533a26ad661b901ae1245e0c0ef
GitHub Action <action@github.com>
2024-01-07 19:16:52 +0000

    Revert "Merge pull request #1968 from kishlay42/main"
    This reverts commit 2a58b84af84c1497eb6e7724d373946362bcdf1e, reversing
    changes made to 28ac83350f0eb1ec93f2dce0a28fa3eaf4a37c16.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -2,14 +2,11 @@
 import "./index.css";
 
 function App() {
-  return <div className="center">
-    <h1>React/Vite-Easy</h1>
-    <div className="card">
-      <img src="https://loremflickr.com/320/240" alt="randomImage" />
-      <h2>Card Title</h2>
-      <p>Card Description</p>
+  return (
+    <div className="center">
+      {/* TODO: Implement the card component */}
     </div>
-  </div>;
+  );
 }
 
 export default App;
