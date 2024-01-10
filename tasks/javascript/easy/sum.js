f771643a1430315ae8da9c070acfda1589ef283c
GitHub Action <action@github.com>
2024-01-10 09:28:53 +0000

    Revert "Merge pull request #1980 from gyanavardhana/main"
    This reverts commit e34ec74bf424deb64f7c55d9724c4e146f57641d, reversing
    changes made to 5fc35f89910d3a72e88735ce5384dc1e7c1239cb.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -2,9 +2,3 @@
 
 // TODO: Write a function that returns the sum of two numbers
 
-function sum(a,b){
-  return a+b;
-} 
-var a = 1;
-var b = 2;
-console.log("the sum of %d and %d is %d",a,b,sum(a,b));
