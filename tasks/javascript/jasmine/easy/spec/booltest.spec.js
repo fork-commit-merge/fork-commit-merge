33c41bf99fe85658c314e59635be803552c46f79
Amir <amirm034@gmail.com>
2024-01-13 11:28:48 -0800

    20240113_JavasScript/Jasmine_1128

--- tasks/javascript/jasmine/easy/spec/booltest.spec.js
+++ tasks/javascript/jasmine/easy/spec/booltest.spec.js
@@ -1,7 +1,8 @@
 // Jasmine - Easy
 
-describe("Jasmine - Easy - Boolean Test", function () {
-    it("should pass this test", function () {
-        // TODO: Add `expect` and `toBe` statements to pass this test
-    });
+describe('Jasmine - Easy - Boolean Test', function () {
+  it('should pass this test', function () {
+    // TODO: Add `expect` and `toBe` statements to pass this test
+    expect(a).toBe(true);
+  });
 });
