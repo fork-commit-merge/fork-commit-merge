013d511350dccdcf9737eb34d6c3b3a93f194d5d
GitHub Action <action@github.com>
2024-01-20 14:50:27 +0000

    Revert "Merge pull request #2017 from azharKhalid/fix-issue"
    This reverts commit 2c53f222cf9938c69068df0212b4dfaf34ac5284, reversing
    changes made to d43239ca12510d7a1bd990a0f87fdd9ad79293ff.

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,22 +1,3 @@
 // JavaScript - Medium
 
 // Create functionality for a countdown timer that counts down from 10 seconds to zero using JavaScript
-
-function handleCountDown() {
-  let time = 10;
-  let timerEl = document.getElementById("timer");
-  
-  function updateTimeAndDOM() {
-    time -= 1;
-    timerEl.innerText = "00:0" + time;
-
-    if (time === 0) {
-      timerEl.innerText = "Times up!";
-      clearInterval(countdownInterval);
-    }
-  }
-
-  const countdownInterval = setInterval(updateTimeAndDOM, 1000);
-}
-
-document.addEventListener("DOMContentLoaded", handleCountDown());
