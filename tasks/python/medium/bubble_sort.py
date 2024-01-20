14ecda4ce6133bc86e99420a6d6ce95a8bad0c83
Madhu_Sri <123156637+Madhusri02@users.noreply.github.com>
2024-01-20 23:30:01 +0530

    Update bubble_sort.py

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -2,31 +2,17 @@
 
 def bubbleSort(arr):
 	n = len(arr)
-	# optimize code, so if the array is already sorted, it doesn't need
-	# to go through the entire process
 	swapped = False
-	# Traverse through all array elements
 	for i in range(n-1):
-		# range(n) also work but outer loop will
-		# repeat one time more than needed.
-		# Last i elements are already in place
 		for j in range(0, n-i-1):
-
-			# traverse the array from 0 to n-i-1
-			# Swap if the element found is greater
-			# than the next element
 			if arr[j] > arr[j + 1]:
 				swapped = True
 				arr[j], arr[j + 1] = arr[j + 1], arr[j]
 		
 		if not swapped:
-			# if we haven't needed to make a single swap, we 
-			# can just exit the main loop.
 			return
 
 arr = [64,25, 12, 22, 11]
 print("Original array : ",arr)
-
 bubbleSort(arr)
-
 print("Sorted array: ",arr)
