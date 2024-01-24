25935d2125da4c354d42736f82c55e468ba67d24
GitHub Action <action@github.com>
2024-01-24 14:40:26 +0000

    Revert "Merge pull request #2039 from dikshabroota/fix-issue"
    This reverts commit eb5649fe56835f6f41cc93bcfa4fef8279b7c015, reversing
    changes made to d4460b81b93a29075de3c4fc7b1522b2d4c6ec01.

--- tasks/c++/easy/sum.cpp
+++ tasks/c++/easy/sum.cpp
@@ -1,12 +1,3 @@
 // C++ - Easy 2
-#include <iostream>
 
-int main()
-{
-    int a = 10;
-    int b = 5;
-
-    int sum = a + b;
-    return sum;
-}
 // TODO: Create a Basic C++ Program to Add Two Numbers
