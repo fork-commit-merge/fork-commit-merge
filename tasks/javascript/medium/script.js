e30b2a65ed2b4157f05936220fca068824bb4bd6
GitHub Action <action@github.com>
2024-01-24 10:22:37 +0000

    Revert "Merge pull request #2033 from AndresPaulino/medium-issue-fix"
    This reverts commit ea8395161c3ba00a2c07b9ec3eac1c8ebe8e6543, reversing
    changes made to a59dd15cdd1b363e0cab90f784ef4bb567c6b299.

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,16 +1,3 @@
 // JavaScript - Medium
 
 // Create functionality for a countdown timer that counts down from 10 seconds to zero using JavaScript
-
-const timer = document.querySelector('#timer');
-let count = 10;
-
-const interval = setInterval(() => {
-  if (count === 0) {
-    clearInterval(interval);
-    timer.innerHTML = '<h1>Done</h1>';
-  } else {
-    timer.innerHTML = count;
-    count--;
-  }
-}, 1000);
