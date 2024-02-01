1cd5709677a5e04ec6351d51a648507f01422fa7
GitHub Action <action@github.com>
2024-02-01 19:37:42 +0000

    Revert "Merge pull request #2059 from DicoderRG/Javascript-two-number-addition-program"
    This reverts commit f44ac2c92601ddedeba67eb9edddff5d735c5637, reversing
    changes made to cc2c015f87fbc3d8c353787aa9196f5b1490feaa.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,8 +1,4 @@
 // JavaScript - Easy
 
 // TODO: Write a function that returns the sum of two numbers
-function sum(a, b) {
-  return a + b;
-}
 
-console.log(`The sum of 1 and 2 is ${sum(1, 2)}.`);
