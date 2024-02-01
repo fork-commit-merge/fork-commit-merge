5b16e6688587fb0baa5ae594915bee6dea825a52
GitHub Action <action@github.com>
2024-02-01 16:54:49 +0000

    Revert "Merge pull request #2057 from andref12/fix-issue"
    This reverts commit dbbe36e6574441766d1181b5be8461f304bb51d2, reversing
    changes made to fb0a86c1ecbba230b71bfe57aae2acb48bd32823.

--- tasks/c/medium/read_csv.c
+++ tasks/c/medium/read_csv.c
@@ -7,21 +7,4 @@
 
 int main() {
 //    TODO: Implement the program requested in the task description
-    
-    FILE *csv_file = fopen("data.csv", "r");
-    char c_list[1000];
-    char *str_ptr;
-    fgets(c_list, sizeof(c_list), csv_file);
-    printf("Header: %s\n", c_list);
-
-    while(fgets(c_list, sizeof(c_list), csv_file)){
-        printf("Reading line: %s",c_list);
-        str_ptr = strtok(c_list,",");
-        printf("Name: %s\n", str_ptr);
-        str_ptr = strtok(NULL,",");
-        printf("Age: %s\n", str_ptr);
-    }
-    fclose(csv_file);
-    
-    return 0;
 }
