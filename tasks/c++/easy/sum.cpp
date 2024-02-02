52474ae391aab06df7923961da355b738de5533f
Nikola <128964319+NikolaM4@users.noreply.github.com>
2024-02-02 14:49:20 +0100

    Finished task

--- tasks/c++/easy/sum.cpp
+++ tasks/c++/easy/sum.cpp
@@ -1,3 +1,15 @@
 // C++ - Easy 2
+#include <iostream>
+
+using namespace std;
 
 // TODO: Create a Basic C++ Program to Add Two Numbers
+
+int main()
+{
+  int a,b;
+  cin >> a;
+  cin >> b;
+  cout << a + b;
+  return 0;
+}
