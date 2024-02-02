173390f32fef4f10af3b8853148f5fcd4122354d
GitHub Action <action@github.com>
2024-02-02 15:43:30 +0000

    Revert "Merge pull request #2062 from NikolaM4/fix-issue"
    This reverts commit 9e28bb30d7677fafc23b60357a5964b8de1feeb3, reversing
    changes made to d012bbb0594b600dc690b30de68ee2dc675d2d35.

--- tasks/c++/easy/sum.cpp
+++ tasks/c++/easy/sum.cpp
@@ -1,15 +1,3 @@
 // C++ - Easy 2
-#include <iostream>
-
-using namespace std;
 
 // TODO: Create a Basic C++ Program to Add Two Numbers
-
-int main()
-{
-  int a,b;
-  cin >> a;
-  cin >> b;
-  cout << a + b;
-  return 0;
-}
