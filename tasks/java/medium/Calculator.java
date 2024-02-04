afa63753b1532f82d9574269503b59849b1386f7
GitHub Action <action@github.com>
2024-02-04 16:21:48 +0000

    Revert "Merge pull request #2069 from DongJu-Na/main"
    This reverts commit 25b1d7add3b7cb800b87494415b6ae49dbaed157, reversing
    changes made to 2e05f5c6ac40a481996811d72646e80e52855b75.

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -14,31 +14,4 @@ public class Calculator {
         System.out.println("Multiplication: " + calc.multiply(5, 3));
         System.out.println("Division: " + calc.divide(5, 3));
     }
-
-    // Addition method
-    public double add(double num1, double num2) {
-        return num1 + num2;
-    }
-
-    // Subtraction method
-    public double subtract(double num1, double num2) {
-        return num1 - num2;
-    }
-
-    // Multiplication method
-    public double multiply(double num1, double num2) {
-        return num1 * num2;
-    }
-
-    // Division method
-    public double divide(double num1, double num2) {
-        if (num2 != 0) {
-            return num1 / num2;
-        } else {
-            System.out.println("Error: Division by zero");
-            return Double.NaN;
-        }
-    }
-
-
 }
