b3339f671ebc58f8668b3f355e241c20ad33f1c8
Amitabh Kumar <21dec004@nith.ac.in>
2024-02-04 12:49:06 +0530

    Update TextFileParser.cpp
    Added the commented functionality

--- tasks/c++/medium/text_file_parser/TextFileParser.cpp
+++ tasks/c++/medium/text_file_parser/TextFileParser.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+using namespace std;
 
 // TODO: Create the necessary functions for the program to work
+void printFun(){
+    cout<<"Hello World!"<<endl;
+    cout<<"The world is a beautiful place."<<endl;
+    cout<<"The weather is wonderful."<<endl;
+}
 
 int main() {
     // TODO: Implement the main method
+    printFun();
     return 0;
 }
