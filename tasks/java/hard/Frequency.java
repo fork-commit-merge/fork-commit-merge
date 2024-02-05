0c7408b0e24ecc7fdf06fcdb2f47450823eaf1fc
GitHub Action <action@github.com>
2024-02-05 14:12:02 +0000

    Revert "Merge pull request #2072 from DongJu-Na/main"
    This reverts commit 825cbc5fdfbbd2e745cb0eca82e890e22a206ced, reversing
    changes made to 99141fd138b1e15e80a426d44a9d31f0a73005c6.

--- tasks/java/hard/Frequency.java
+++ tasks/java/hard/Frequency.java
@@ -1,21 +1,8 @@
 // Java - Hard
-import java.util.HashMap;
-import java.util.Map;
-
 
 public class Frequency {
 
     public static void main(String[] args) {
         // TODO: Given an array of integers, write a Java program to find the frequency of each element using a Map
-        Map<Integer,Integer> frequencyMap = new HashMap<Integer,Integer>();
-        int[] array = {1, 2, 2, 3, 1, 1, 2};
-
-        for (int num:array) {
-            System.out.println("key > " + num + " value > " + (frequencyMap.getOrDefault(num,0) + 1));
-            frequencyMap.put(num,frequencyMap.getOrDefault(num,0) + 1);
-
-        }
-
-        System.out.println(frequencyMap);
     }
 }
