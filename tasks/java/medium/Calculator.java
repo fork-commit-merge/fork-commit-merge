532292363c778c8621d83bca176214c4f4ada76a
GitHub Action <action@github.com>
2024-02-06 17:40:30 +0000

    Revert "Merge pull request #2076 from sinha-sanchay/patch-1"
    This reverts commit 32ffee45297d7fb79ed8d56212c269b7a34f4a04, reversing
    changes made to 55b02d81caeb3454208615f629df42e2d02b5fc9.

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -3,30 +3,7 @@
 package tasks.java.medium;
 
 public class Calculator {
-     // TODO: Implement the Calculator class
-    
-    // Method to add two numbers
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-    // Method to subtract two numbers
-    public int subtract(int a, int b) {
-        return a - b;
-    }
-
-    // Method to multiply two numbers
-    public int multiply(int a, int b) {
-        return a * b;
-    }
-
-    // Method to divide two numbers
-    public double divide(int a, int b) {
-        if (b == 0) {
-            throw new ArithmeticException("Cannot divide by zero");
-        }
-        return (double) a / b;
-    }
+    // TODO: Implement the Calculator class
 
     public static void main(String[] args) {
         //! Don't change the tests in the main method!
