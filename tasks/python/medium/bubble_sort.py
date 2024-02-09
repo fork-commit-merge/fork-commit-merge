fd3ad4c1552863602f2ca84bcd851c1ea5cda9dc
GitHub Action <action@github.com>
2024-02-09 18:55:49 +0000

    Revert "Merge pull request #2086 from saquib000/new"
    This reverts commit 983d5f1aa80e5dcfc75a472ac802f37890eb8e67, reversing
    changes made to 4c7c7dd76571d6e73a14853d6b61b9cd058f09a7.

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,17 +1,5 @@
 def bubble_sort(arr):
     # TODO: Implement the bubble sort algorithm
-    def bubbleSort(arr):
-    n = len(arr)
-    swapped = False
-    for i in range(n - 1):
-        for j in range(0, n - i - 1):
-            if arr[j] > arr[j + 1]:
-                # Swap the elements
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                swapped = True
-        # If no swaps were made during this pass, the array is already sorted
-        if not swapped:
-            break
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
