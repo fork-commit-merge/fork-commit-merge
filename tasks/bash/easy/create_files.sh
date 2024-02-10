37fe8c2fd9eb676167b43cd0c510aca4b82691c4
rajanarahul93 <rajanarahul93@gmail.com>
2024-02-10 15:03:10 +0530

    Bash Script: File Creation & Content Display#1533

--- tasks/bash/easy/create_files.sh
+++ tasks/bash/easy/create_files.sh
@@ -3,3 +3,25 @@
 # Bash - Easy
 
 # TODO: Write the Bash script requested in the issue description
+#!/bin/bash
+
+# Navigate into the "files" directory
+cd tasks/bash/easy/files || exit
+
+# Create three files
+touch file1.txt file2.txt file3.txt
+
+# Write "Hello Shell!" to each file
+echo "Hello Shell!" > file1.txt
+echo "Hello Shell!" > file2.txt
+echo "Hello Shell!" > file3.txt
+
+# Print the contents of each file
+echo "Contents of file1.txt:"
+cat file1.txt
+
+echo "Contents of file2.txt:"
+cat file2.txt
+
+echo "Contents of file3.txt:"
+cat file3.txt
