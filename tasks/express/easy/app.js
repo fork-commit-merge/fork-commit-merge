9c3d90744d940cca3b39848a394e62b5c44e8577
GitHub Action <action@github.com>
2024-02-12 15:01:49 +0000

    Revert "Merge pull request #2094 from Riyazul555/Riyaz2"
    This reverts commit f2b739e11e35b8ea9cb1b6589b1b4e4f6e03b0c4, reversing
    changes made to 565f7c810b225c6aea9cd1d3c466399f02b7c400.

--- tasks/express/easy/app.js
+++ tasks/express/easy/app.js
@@ -1,15 +1,10 @@
 // ExpressJS - Easy
 
+const { get } = require("cheerio/lib/api/traversing");
 const express = require("express");
 const app = express();
 const port = 6969;
 
-// Implement `get` method for / route
-app.get("/", (req, res) => {
-    res.send("Hello, Express!");
-});
+// TODO: Implement `get` method for / route
 
-// Implement `listen` method
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+// TODO: Implement `listen` method
