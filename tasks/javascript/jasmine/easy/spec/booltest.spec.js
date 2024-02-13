5b5e726437818baedf3cbeae9cf5a103bbe23dcd
GitHub Action <action@github.com>
2024-02-13 17:40:07 +0000

    Revert "Merge pull request #2102 from VAIBHAVPANT07/main"
    This reverts commit b71ed5ff784be1433f6ea52d977a66c041464710, reversing
    changes made to 45ca3359e0f8e12b428dc04d797a8901a3878d90.

--- tasks/javascript/jasmine/easy/spec/booltest.spec.js
+++ tasks/javascript/jasmine/easy/spec/booltest.spec.js
@@ -1,6 +1,7 @@
+// Jasmine - Easy
+
 describe("Jasmine - Easy - Boolean Test", function () {
     it("should pass this test", function () {
-        // Adding `expect` and `toBe` statements to pass this test
-        expect(true).toBe(true);
+        // TODO: Add `expect` and `toBe` statements to pass this test
     });
 });
