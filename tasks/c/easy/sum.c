7abc37554d39adb5e5c12a4cbf40609d572d891d
rajanarahul93 <rajanarahul93@gmail.com>
2024-02-14 23:16:23 +0530

    solved issue

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 
-// TODO: Implement the sum and main functions
+int main() {
+    int num1, num2;
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    int sum = num1 + num2;
+
+    printf("The sum of %d and %d is: %d\n", num1, num2, sum);
+
+    return 0;
+}
