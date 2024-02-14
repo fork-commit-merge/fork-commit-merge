44764f111c19fcf5c59359c245a5a74a2c38048e
rajanarahul93 <rajanarahul93@gmail.com>
2024-02-14 08:20:32 +0530

    Solved the issue

--- tasks/ada/easy/hello.adb
+++ tasks/ada/easy/hello.adb
@@ -2,5 +2,6 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Hello is
 begin
-  -- Print "Hello, World!" to the console
+   -- Print "Hello, World!" to the console
+   Put_Line("Hello, World!");
 end Hello;
