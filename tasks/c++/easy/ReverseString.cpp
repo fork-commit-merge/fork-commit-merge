cc01c31caebb3df110cd00b7bf82cc315bedece1
rajanarahul93 <rajanarahul93@gmail.com>
2024-02-15 19:41:54 +0530

    solved issues

--- tasks/c++/easy/ReverseString.cpp
+++ tasks/c++/easy/ReverseString.cpp
@@ -2,10 +2,28 @@
 #include <string>
 
 std::string reverseString(const std::string &str) {
-    // TODO: Implement the reverseString function
+    // Create a copy of the input string
+    std::string reversed = str;
+
+    // Reverse the copy using standard library functions
+    std::reverse(reversed.begin(), reversed.end());
+
+    return reversed;
 }
 
 int main() {
-    // TODO: Implement the main method
-}
+    // Input string
+    std::string inputString;
 
+    // Get user input
+    std::cout << "Enter a string: ";
+    std::getline(std::cin, inputString);
+
+    // Call the reverseString function
+    std::string reversedString = reverseString(inputString);
+
+    // Display the reversed string
+    std::cout << "Reversed string: " << reversedString << std::endl;
+
+    return 0;
+}
