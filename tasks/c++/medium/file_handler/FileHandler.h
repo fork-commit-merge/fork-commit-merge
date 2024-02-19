9592fab724a7a08eb2728167f10fb68435ee456b
rajanarahul93 <rajanarahul93@gmail.com>
2024-02-19 11:43:49 +0530

    Solved issue

--- tasks/c++/medium/file_handler/FileHandler.h
+++ tasks/c++/medium/file_handler/FileHandler.h
@@ -1,9 +1,45 @@
-// C++ - Medium
-
 #pragma once
 #include <string>
+#include <fstream>
 
 class FileHandler {
 public:
-    // TODO: Implement the FileHandler class
+    FileHandler(const std::string& filePath) : filePath(filePath) {}
+
+    bool openFile() {
+        fileStream.open(filePath);
+        return fileStream.is_open();
+    }
+
+    void closeFile() {
+        fileStream.close();
+    }
+
+    bool writeToFile(const std::string& content) {
+        if (!fileStream.is_open()) {
+            return false;
+        }
+
+        fileStream << content;
+        return true;
+    }
+
+    std::string readFromFile() {
+        std::string content;
+        std::string line;
+
+        if (!fileStream.is_open()) {
+            return content;
+        }
+
+        while (std::getline(fileStream, line)) {
+            content += line + "\n";
+        }
+
+        return content;
+    }
+
+private:
+    std::string filePath;
+    std::fstream fileStream;
 };
