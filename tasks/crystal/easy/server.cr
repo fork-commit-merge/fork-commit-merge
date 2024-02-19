45cd8818c331a47414e8c83e3f3f4f7b6e513669
rajanarahul93 <rajanarahul93@gmail.com>
2024-02-20 02:08:51 +0530

    Solved issue

--- tasks/crystal/easy/server.cr
+++ tasks/crystal/easy/server.cr
@@ -1,5 +1,10 @@
-# Crystal - Easy
-
 require "http/server"
 
-# TODO: Implement the HTTP server
+server = HTTP::Server.new do |context|
+  context.response.content_type = "text/plain"
+  context.response.print "Hello, World!"
+end
+
+address = server.bind_tcp "0.0.0.0", 8080
+puts "Listening on http://#{address}"
+server.listen
