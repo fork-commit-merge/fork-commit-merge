8c458af20370f9807095cd3101ffd9fd01ae8c14
GitHub Action <action@github.com>
2024-02-19 19:43:17 +0000

    Revert "Merge pull request #2115 from rajanarahul93/main"
    This reverts commit 6b2407d2dc4ae28f49e64b140f5442bdb2139c7b, reversing
    changes made to 69badf56f03051c4ec65244dc5c1cd4c1e5013cd.

--- tasks/c++/hard/mystring.h
+++ tasks/c++/hard/mystring.h
@@ -1,58 +1,11 @@
+// C++ - Hard
+
+#ifndef MYSTRING_H
+#define MYSTRING_H
+#include <iostream>
 
 class MyString {
-private:
-    char* str; // Pointer to the string data
-    size_t len; // Length of the string
-
-public:
-    // Default constructor
-    MyString() : str(nullptr), len(0) {}
-
-    // Parameterized constructor
-    MyString(const char* s);
-
-    // Copy constructor
-    MyString(const MyString& other);
-
-    // Move constructor
-    MyString(MyString&& other) noexcept;
-
-    // Destructor
-    ~MyString();
-
-    // Copy assignment operator
-    MyString& operator=(const MyString& other);
-
-    // Move assignment operator
-    MyString& operator=(MyString&& other) noexcept;
-
-    // Concatenation operator
-    MyString operator+(const MyString& other) const;
-
-    // Equality operator
-    bool operator==(const MyString& other) const;
-
-    // Inequality operator
-    bool operator!=(const MyString& other) const;
-
-    // Less than operator
-    bool operator<(const MyString& other) const;
-
-    // Greater than operator
-    bool operator>(const MyString& other) const;
-
-    // Function to append to the current string
-    void append(const MyString& other);
-
-    // Function to compare two strings
-    int compare(const MyString& other) const;
-
-    // Function to return C-style string
-    const char* c_str() const;
-
-    // Function to get the length of the string
-    size_t length() const;
-
-    // Friend function to output the string using ostream
-    friend std::ostream& operator<<(std::ostream& os, const MyString& s);
+    // TODO: Implement MyString class
 };
+
+#endif
--- tasks/c++/medium/file_handler/FileHandler.cpp
+++ tasks/c++/medium/file_handler/FileHandler.cpp
@@ -1,36 +1,14 @@
-bool FileHandler::WriteFile(const std::string& filePath, const std::string& content) {
-    std::ofstream out(filePath);
-    if (!out.is_open()) {
-        return false;
-    }
-    out << content;
-    out.close();
-    return true;
-}
+// C++ - Medium
 
-bool FileHandler::AppendFile(const std::string& filePath, const std::string& additionalContent) {
-    std::ofstream out(filePath, std::ios_base::app);
-    if (!out.is_open()) {
-        return false;
-    }
-    out << additionalContent;
-    out.close();
-    return true;
-}
+#include "FileHandler.h"
+#include <fstream>
+#include <sstream>
 
-std::string FileHandler::ReadFile(const std::string& filePath) {
-    std::ifstream in(filePath);
-    std::stringstream buffer;
-    if (!in.is_open()) {
-        return "";
-    }
-    buffer << in.rdbuf();
-    in.close();
-    return buffer.str();
-}
+// TODO: Implement the FileHandler::ReadFile method
 
-bool FileHandler::FileExists(const std::string& filePath) {
-    std::ifstream in(filePath);
-    return in.good();
-}
+// TODO: Implement the FileHandler::WriteFile method
+
+// TODO: Implement the FileHandler::AppendFile method
+
+// TODO: Implement the FileHandler::FileExists method
 
