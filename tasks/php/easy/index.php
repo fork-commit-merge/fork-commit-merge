cd6b85a8725b5aebc977560f78bde549953ac6fe
GitHub Action <action@github.com>
2024-02-21 10:04:03 +0000

    Revert "Merge pull request #2123 from Tashuuuu/main"
    This reverts commit 391b22f0ce31a273c0b362b9d9c6e7f70bd50608, reversing
    changes made to 624ace1f7189062e999b8001d6f0e55180d1f76b.

--- tasks/php/easy/index.php
+++ tasks/php/easy/index.php
@@ -6,8 +6,3 @@ $hello = "Hello";
 $world = "World!";
 
 // TODO: Implement rest of the string concatenation program
-
-$result = $hello . ' ' . $world; // Concatenate the strings
-
-echo $result; // Print the result to the terminal
-?>
