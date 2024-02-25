67acff0bd790888b694b1817462e42c1126187be
rajanarahul93 <rajanarahul93@gmail.com>
2024-02-25 08:07:45 +0530

    Solved issue #1029

--- tasks/fortran/easy/triangle_area.f90
+++ tasks/fortran/easy/triangle_area.f90
@@ -1,4 +1,18 @@
 program triangle_area
-! TODO: Implement the triangle_area program
+    ! Declaration of variables
+    real :: base, height, area
+    
+    ! Input: Read the base and height of the triangle from the user
+    write(*,*) "Enter the base of the triangle: "
+    read(*,*) base
+
+    write(*,*) "Enter the height of the triangle: "
+    read(*,*) height
+
+    ! Calculate the area of the triangle
+    area = 0.5 * base * height
+
+    ! Output: Display the calculated area
+    write(*,*) "The area of the triangle is: ", area
 
 end program triangle_area
