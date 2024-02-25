7f8332a0f212802f0a52f4815f1c13c6134b14cb
GitHub Action <action@github.com>
2024-02-25 13:05:19 +0000

    Revert "Merge pull request #2139 from rajanarahul93/main"
    This reverts commit 0d3221d92af541fe1d037a5c2bdedc1d22291ad0, reversing
    changes made to 225c2205521a19ae74e01a0fe60a54bc2b973a0d.

--- tasks/fortran/easy/triangle_area.f90
+++ tasks/fortran/easy/triangle_area.f90
@@ -1,18 +1,4 @@
 program triangle_area
-    ! Declaration of variables
-    real :: base, height, area
-    
-    ! Input: Read the base and height of the triangle from the user
-    write(*,*) "Enter the base of the triangle: "
-    read(*,*) base
-
-    write(*,*) "Enter the height of the triangle: "
-    read(*,*) height
-
-    ! Calculate the area of the triangle
-    area = 0.5 * base * height
-
-    ! Output: Display the calculated area
-    write(*,*) "The area of the triangle is: ", area
+! TODO: Implement the triangle_area program
 
 end program triangle_area
