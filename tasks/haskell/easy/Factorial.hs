2f1eee89cc2be27e85d7991c2bd67a29b31106f1
GitHub Action <action@github.com>
2024-02-26 13:07:31 +0000

    Revert "Merge pull request #2143 from rajanarahul93/main"
    This reverts commit e8a48fc20c3263eed58f1db6e269f77e4eb78198, reversing
    changes made to 58e97877b22a0bf325c4e0f22d64ddf1929d008e.

--- tasks/haskell/easy/Factorial.hs
+++ tasks/haskell/easy/Factorial.hs
@@ -2,11 +2,4 @@
 
 main :: IO ()
 main = do
-    putStrLn "Enter a number:"
-    input <- getLine
-    let number = read input :: Integer
-    putStrLn $ "Factorial of " ++ show number ++ " is: " ++ show (factorial number)
-
-factorial :: Integer -> Integer
-factorial 0 = 1
-factorial n = n * factorial (n - 1)
+    -- TODO: Implement a Haskell program that calculates the factorial of a number
