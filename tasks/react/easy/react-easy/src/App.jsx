7ae417ecb362c85adaac5285d6b1222d9ae697c8
GitHub Action <action@github.com>
2024-02-28 15:53:52 +0000

    Revert "Merge pull request #2156 from gurjeetsinghvirdee/react-todo"
    This reverts commit 2e00e2642f827dcde107c94ca4f38d3118ba38b6, reversing
    changes made to 59351b6bea68b0aa410de9c224b726a16558906a.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -4,12 +4,7 @@ import "./index.css";
 function App() {
   return (
     <div className="center">
-      <div className="card">
-        <h1>React/Vite - Easy</h1>
-        <img src="https://loremflickr.com/320/240" alt="Card" />
-        <h2>Lorem Flick</h2>
-        <p>Image Generator</p>
-      </div>
+      {/* TODO: Implement the card component */}
     </div>
   );
 }
