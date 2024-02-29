0127eca09f0bc7337e79defa79176aeba6ceb967
GitHub Action <action@github.com>
2024-02-29 17:23:28 +0000

    Revert "Merge pull request #2160 from rajanarahul93/main"
    This reverts commit efa3826030793f7f2ed7869ae779853d14c87058, reversing
    changes made to 18d9299c9a3d0890c8f11559ec67cd60b8b7f916.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,9 +1,4 @@
-// Function to add two numbers
-function addNumbers(num1, num2) {
-    return num1 + num2;
-  }
-  
-  // Example usage
-  const result = addNumbers(num1,num2);
-  console.log(result); // Output: 12
-  
+// JavaScript - Easy
+
+// TODO: Write a function that returns the sum of two numbers
+
