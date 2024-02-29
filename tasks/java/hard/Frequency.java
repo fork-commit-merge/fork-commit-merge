12f368f27829550932ade38f916d240c9bf13dff
GitHub Action <action@github.com>
2024-02-29 06:59:31 +0000

    Revert "Merge pull request #2157 from rajanarahul93/main"
    This reverts commit 134c7bbff0189156acc062a94c743d1bc030c620, reversing
    changes made to 7ae417ecb362c85adaac5285d6b1222d9ae697c8.

--- tasks/java/hard/Frequency.java
+++ tasks/java/hard/Frequency.java
@@ -1,19 +1,8 @@
-import java.util.HashMap;
-import java.util.Map;
+// Java - Hard
 
 public class Frequency {
 
     public static void main(String[] args) {
-        int[] array = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4}; // Replace with the actual array
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-
-        for (int number : array) {
-            frequencyMap.put(number, frequencyMap.getOrDefault(number, 0) + 1);
-        }
-
-        // Print the frequency of each element
-        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
-            System.out.println("Element: " + entry.getKey() + ", Frequency: " + entry.getValue());
-        }
+        // TODO: Given an array of integers, write a Java program to find the frequency of each element using a Map
     }
 }
--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -1,30 +1,9 @@
+// Java - Medium
+
 package tasks.java.medium;
 
 public class Calculator {
     // TODO: Implement the Calculator class
-
-    public int add(int a, int b) {
-        // TODO: Implement addition logic
-        return a + b;
-    }
-
-    public int subtract(int a, int b) {
-        // TODO: Implement subtraction logic
-        return a - b;
-    }
-
-    public int multiply(int a, int b) {
-        // TODO: Implement multiplication logic
-        return a * b;
-    }
-
-    public double divide(int a, int b) {
-        // TODO: Implement division logic
-        if (b == 0) {
-            throw new ArithmeticException("Cannot divide by zero");
-        }
-        return (double) a / b;
-    }
 
     public static void main(String[] args) {
         //! Don't change the tests in the main method!
