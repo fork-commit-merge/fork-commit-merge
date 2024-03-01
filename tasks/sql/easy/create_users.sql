e1f7eddae3e7a487c2e1387a750629a9134fe554
Riyazul555 <riyazulislam2003@gmail.com>
2024-03-01 12:02:09 +0530

    SQL issue solved

--- tasks/sql/easy/create_users.sql
+++ tasks/sql/easy/create_users.sql
@@ -4,3 +4,11 @@ CREATE TABLE users (
     -- TODO: Add all the requested fields
 );
 
+CREATE TABLE users (
+    user_id INT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    birthdate DATE,
+    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    is_active BOOLEAN DEFAULT true
+);
