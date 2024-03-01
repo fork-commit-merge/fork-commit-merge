aa4195a6ca67fe006118c6b55ceb2b416f27afd1
GitHub Action <action@github.com>
2024-03-01 09:27:01 +0000

    Revert "Merge pull request #2166 from Riyazul555/Riyaz8"
    This reverts commit 351b726c9c2b89267d23401348e65ddee67fbf71, reversing
    changes made to 8b703e41a44888582d7101aeeb06cfcde4744108.

--- tasks/sql/easy/create_users.sql
+++ tasks/sql/easy/create_users.sql
@@ -4,11 +4,3 @@ CREATE TABLE users (
     -- TODO: Add all the requested fields
 );
 
-CREATE TABLE users (
-    user_id INT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    birthdate DATE,
-    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    is_active BOOLEAN DEFAULT true
-);
