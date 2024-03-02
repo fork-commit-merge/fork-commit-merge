b9bfabac1cac5e58ebf688326d16fe84ddde75bb
rajanarahul93 <rajanarahul93@gmail.com>
2024-03-02 08:16:24 +0530

    solved issue

--- tasks/javascript/jest/hard/src/library.js
+++ tasks/javascript/jest/hard/src/library.js
@@ -1,47 +1,13 @@
-// JavaScript - Hard
-
-class Library {
-    constructor() {
-        this.books = [];
-    }
-
-    createBook(title, author) {
-        const book = {
-            title,
-            author,
-            id: this.books.length + 1,
-        };
-        this.books.push(book);
-        return book;
-    }
-
-    getBook(id) {
-        return this.books.find((book) => book.id === id);
-    }
-
-    updateBook(id, title, author) {
-        const bookIndex = this.books.findIndex((book) => book.id === id);
-        if (bookIndex !== -1) {
-            this.books[bookIndex] = { id, title, author };
-        }
-        return this.books[bookIndex];
-    }
-
-    deleteBook(id) {
-        const bookIndex = this.books.findIndex((book) => book.id === id);
-        if (bookIndex !== -1) {
-            this.books.splice(bookIndex, 1);
-        }
-        return this.books;
-    }
-
     searchBooks(query) {
-        // TODO: Implement search functionality
+        const queryRegex = new RegExp(query, "i");
+        return this.books.filter((book) => 
+            queryRegex.test(book.title) || queryRegex.test(book.author)
+        );
     }
 
     filterBooks(criteria) {
-        // TODO: Implement filter functionality
+        return this.books.filter((book) => 
+            Object.keys(criteria).every((key) => book[key] === criteria[key])
+        );
     }
-}
 
-module.exports = Library;
