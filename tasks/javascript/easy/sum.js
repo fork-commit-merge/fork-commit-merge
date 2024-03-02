a54808d98c613391a7e55e5760a3b0e48d2a0bc7
thangbui3914 <thangbui040603@gmail.com>
2024-03-02 11:08:50 +0700

    Fixed issue

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,4 +1,10 @@
 // JavaScript - Easy
 
 // TODO: Write a function that returns the sum of two numbers
+function sum(a, b){
+	const total = a + b;
+	console.log(`The sum of ${a} and ${b} is ${total}.`);
+	return total;
+}
 
+sum(1, 2);
