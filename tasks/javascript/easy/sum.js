8798ce1c4514762a6ae1be278255c30b6e8bd059
GitHub Action <action@github.com>
2024-03-02 07:11:14 +0000

    Revert "Merge pull request #2169 from HighEffortMeme/fix-issue"
    This reverts commit 7a5d7576d32fcfc195a6554a5ed9b52e4c3ea493, reversing
    changes made to c39d5548f5831c262eb127da88bc2bdcd5cd2fbf.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,10 +1,4 @@
 // JavaScript - Easy
 
 // TODO: Write a function that returns the sum of two numbers
-function sum(a, b){
-	const total = a + b;
-	console.log(`The sum of ${a} and ${b} is ${total}.`);
-	return total;
-}
 
-sum(1, 2);
