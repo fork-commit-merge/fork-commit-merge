68ec17d3f582c43e63dcc5a432f1a84eb142346a
GitHub Action <action@github.com>
2024-03-03 05:28:46 +0000

    Revert "Merge pull request #2172 from Riyazul555/Riyaz11"
    This reverts commit 52c9cfed8d54392e85b20c85b3e3684bfdcee8bd, reversing
    changes made to 2fa31c3530f5f06dbd15823ee2f321001d215af7.

--- tasks/bash/medium/disk_monitor.sh
+++ tasks/bash/medium/disk_monitor.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-# Set the threshold for low disk space (in percentage)
-THRESHOLD=20
+# Bash - Medium
 
-# Set the recipient email address
-RECIPIENT_EMAIL="<YOUR_EMAIL>"
-
-# Get disk space information using df command
-DISK_SPACE=$(df -h /)
-
-# Extract the percentage of available space
-AVAIL_PERCENTAGE=$(echo "$DISK_SPACE" | awk 'NR==2 {print $(NF-1)}' | sed 's/%//')
-
-# Check if available space is below the threshold
-if [ "$AVAIL_PERCENTAGE" -lt "$THRESHOLD" ]; then
-    # Compose the email message
-    SUBJECT="Low Disk Space Alert"
-    MESSAGE="Disk space is running low on the system.\n\nDisk Space Information:\n$DISK_SPACE"
-
-    # Send email using mail command
-    echo -e "$MESSAGE" | mail -s "$SUBJECT" "$RECIPIENT_EMAIL"
-
-    echo "Email alert sent. Disk space is below $THRESHOLD%."
-else
-    echo "Disk space is above $THRESHOLD%. No action required."
-fi
+# TODO: Implement the Disk Monitor script
