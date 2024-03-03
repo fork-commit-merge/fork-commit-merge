2fa31c3530f5f06dbd15823ee2f321001d215af7
GitHub Action <action@github.com>
2024-03-03 05:27:48 +0000

    Revert "Merge pull request #2171 from Riyazul555/Riyaz10"
    This reverts commit 8b401aac52e7bf55df9ecf1a1631007ae69f91d0, reversing
    changes made to 6b8f74cd8492cb760e8bfe93deb96d6d98e16922.

--- tasks/rust/easy/fibonacci.rs
+++ tasks/rust/easy/fibonacci.rs
@@ -1,41 +1,5 @@
+// Rust - Easy
+
 use std::io;
 
-fn fibonacci(n: u32) -> u64 {
-    if n == 0 {
-        return 0;
-    } else if n == 1 {
-        return 1;
-    }
-
-    let mut a = 0;
-    let mut b = 1;
-    let mut result = 0;
-
-    for _ in 2..=n {
-        result = a + b;
-        a = b;
-        b = result;
-    }
-
-    result
-}
-
-fn main() {
-    println!("Fibonacci Sequence Calculator");
-
-    println!("Enter the number of terms in the sequence:");
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    let n: u32 = match input.trim().parse() {
-        Ok(num) => num,
-        Err(_) => {
-            println!("Invalid input. Please enter a valid number.");
-            return;
-        }
-    };
-
-    println!("The Fibonacci sequence up to {} terms is:", n);
-    for i in 0..n {
-        print!("{} ", fibonacci(i));
-    }
-}
+// TODO: Implement fibonacci sequence in Rust
