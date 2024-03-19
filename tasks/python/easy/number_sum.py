f5d8fc1764397e2941512b6aef1e42a5dc5d7c92
RithikaReddyK <rithikakuchanpally@gmail.com>
2024-03-19 15:01:17 -0400

    Adding Two numbers using Python

--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,3 +1,17 @@
 # Python - Easy
 
 # TODO: Write a function that returns the sum of two numbers
+def add_numbers(num1, num2):
+    sum = num1 + num2
+    return sum
+
+def main():
+    a = input("Enter the first number: ")
+    b = input("Enter the second number: ")
+
+    result = add_numbers(a, b)
+
+    print(f"The sum of {a} and {b} is {result}.")
+
+if __name__ == "__main__":
+    main()
