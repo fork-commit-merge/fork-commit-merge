8d17a90a7b27d43192cbf4486516aa7c207287d3
GitHub Action <action@github.com>
2024-03-20 14:01:45 +0000

    Revert "Merge pull request #2199 from RithikaReddyK/Calculator"
    This reverts commit 764b8ef025430f7e9f438cec2b756713533c8aad, reversing
    changes made to 97c8ba2822b6d19f48b11c48b8ba3599a9f3f86f.

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -3,31 +3,10 @@
 package tasks.java.medium;
 
 public class Calculator {
-    // Method to add two numbers
-    public int add(int num1, int num2) {
-        return num1 + num2;
-    }
-
-    // Method to subtract two numbers
-    public int subtract(int num1, int num2) {
-        return num1 - num2;
-    }
-
-    // Method to multiply two numbers
-    public int multiply(int num1, int num2) {
-        return num1 * num2;
-    }
-
-    // Method to divide two numbers
-    public double divide(int num1, int num2) {
-        if (num2 == 0) {
-            throw new ArithmeticException("Cannot divide by zero");
-        }
-        return (double) num1 / num2;
-    }
+    // TODO: Implement the Calculator class
 
     public static void main(String[] args) {
-        // ! Don't change the tests in the main method!
+        //! Don't change the tests in the main method!
         Calculator calc = new Calculator();
 
         System.out.println("Addition: " + calc.add(5, 3));
