a80b1c7a478baa17628dbf674dca66104bb638dc
rajanarahul93 <rajanarahul93@gmail.com>
2024-03-23 12:00:30 +0530

    Implement code to print HELLO WORLD

--- tasks/vue/easy/todo-list/src/components/TodoList.vue
+++ tasks/vue/easy/todo-list/src/components/TodoList.vue
@@ -1,13 +1,53 @@
 <template>
-    <!-- TODO: Implement the Vue component code here -->
+    <div>
+        <h2>Todo List</h2>
+        <ul>
+            <li v-for="(item, index) in todos" :key="index">
+                {{ item }}
+            </li>
+        </ul>
+        <input type="text" v-model="newTodo" @keyup.enter="addTodo" placeholder="Enter a new todo" />
+    </div>
 </template>
 
 <script>
 export default {
-    // TODO: Implement the JavaScript code here
+    data() {
+        return {
+            todos: [],
+            newTodo: '',
+        };
+    },
+    methods: {
+        addTodo() {
+            if (this.newTodo.trim() !== '') {
+                this.todos.push(this.newTodo);
+                this.newTodo = '';
+            }
+        },
+    },
 };
 </script>
 
 <style scoped>
-/* TODO: Implement the CSS code here */
+h2 {
+    font-size: 24px;
+    margin: 10px 0;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    margin: 5px 0;
+}
+
+input[type='text'] {
+    width: 100%;
+    padding: 5px;
+    margin-top: 10px;
+}
 </style>
+
