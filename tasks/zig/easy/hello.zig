2adf1e05355846ea811d4d2215e85d51d9100193
GitHub Action <action@github.com>
2024-03-24 13:06:27 +0000

    Revert "Merge pull request #2205 from rajanarahul93/main"
    This reverts commit 2b430c459e7c46e3fd4c96f35678757764f13fdd, reversing
    changes made to 318f755418622400c5d99bf9fe5888c1bd276c2c.

--- tasks/zig/easy/hello.zig
+++ tasks/zig/easy/hello.zig
@@ -3,8 +3,5 @@
 const std = @import("std");
 
 pub fn main() void {
-    const hello = "Hello, world!";
-    const stdout = std.io.getStdOut().writer();
-    stdout.print(hello, .{}) catch unreachable;
-    stdout.print("\n", .{}) catch unreachable;
+    // TODO: Implement the code for printing the string "Hello, world!"
 }
