8237f456437378b2a781a665a19f5eba55bb7237
GitHub Action <action@github.com>
2024-03-28 15:57:20 +0000

    Revert "Merge pull request #2217 from uHan2/feature/greeting"
    This reverts commit 494b60959e92f4d21127e064066f9fd68cf29023, reversing
    changes made to 65891764471ad7b6ffd6b12fbb658ba33cb8e7f7.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,13 +1,8 @@
-import java.util.Scanner;
+// Java - Easy
 
 public class GreetingApp {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-
-		System.out.print("Enter your name: "); // input from user
-		String name = scanner.nextLine();
-
-		System.out.println("Hello, " + name + "! Nice to meet you!"); // 인사말 출력
-	}
+    public static void main(String[] args) {
+        // TODO: Implement a Simple Greeting Application
+    }
 }
