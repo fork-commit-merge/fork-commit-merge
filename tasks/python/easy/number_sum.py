e951cc08b5ee873926ca40f7335a1c7ebdca9ec8
Keerthana K <125170456+k-komati@users.noreply.github.com>
2024-04-02 20:09:23 -0400

    Update number_sum.py

--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,3 +1,11 @@
 # Python - Easy
 
 # TODO: Write a function that returns the sum of two numbers
+
+def sum(num1, num2):
+  return num1 + num2
+
+num1 = float(input('Enter number 1'))
+num2 = float(input('Enter number 2'))
+
+print(f'Sum of {num1} and {num2} is {sum(num1,num2)}')
