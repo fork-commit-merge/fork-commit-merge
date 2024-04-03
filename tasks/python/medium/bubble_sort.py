b62dff16bc420441236d937b7bb559e4e53f73c2
GitHub Action <action@github.com>
2024-04-03 16:25:17 +0000

    Revert "Merge pull request #2233 from k-komati/BubbleSort_Python"
    This reverts commit 7912456e2a1a4d98e80211ad9fd69f20b038a066, reversing
    changes made to 1e97730fcf6c62b103bff06f34ccf608762d8489.

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,12 +1,5 @@
 def bubble_sort(arr):
-    n = len(arr)
-    # Traverse through all array elements
-    for i in range(n):
-        for j in range(0, n-i-1):
-            # Traverse the array from 0 to n-i-1 as last i elements are already in place
-            # Swap if the element found is greater than the next element
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+    # TODO: Implement the bubble sort algorithm
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
