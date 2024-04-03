fba7c69e4521e45ab19d435f58c6a11f412c1aab
GitHub Action <action@github.com>
2024-04-03 16:29:46 +0000

    Revert "Merge pull request #2236 from Rabbok/fix-issue"
    This reverts commit fbae04d5be7f7f14c0d54f9c93fd73f90a43d1d2, reversing
    changes made to 20e3b48e65817062b177b7f115badacbe22e04c5.

--- tasks/react/medium/traffic-light/src/TrafficLight.tsx
+++ tasks/react/medium/traffic-light/src/TrafficLight.tsx
@@ -1,40 +1,11 @@
-import React, { useState, useEffect } from "react";
+// React.js - Medium 2
 import "./App.css";
 
-interface LightProps {
-  color: string;
-  active: boolean;
-}
+// TODO: Implement  the TrafficLight component
 
-const Light: React.FC<LightProps> = ({ color, active }) => {
-  return <div className={`light ${color} ${active ? "active" : ""}`} />;
-};
-
-const TRAFFIC_LIGHT_SEQUENCE = [
-  { color: "red", duration: 4000 },
-  { color: "green", duration: 3000 },
-  { color: "yellow", duration: 1000 }
-];
-
-export const TrafficLight: React.FC = () => {
-  const [currentLightIndex, setCurrentLightIndex] = useState<number>(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentLightIndex(prevIndex => (prevIndex + 1) % TRAFFIC_LIGHT_SEQUENCE.length);
-    }, TRAFFIC_LIGHT_SEQUENCE[currentLightIndex].duration);
-    return () => clearInterval(interval);
-  }, [currentLightIndex]);
+export function TrafficLight() {
 
   return (
-    <div className="traffic-light">
-      {TRAFFIC_LIGHT_SEQUENCE.map((light, index) => (
-        <Light
-          key={index}
-          color={light.color}
-          active={index === currentLightIndex}
-        />
-      ))}
-    </div>
+    // TODO: return JSX here
   );
-};
+}
