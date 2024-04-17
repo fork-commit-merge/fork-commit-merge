95f92838867db7107db4835ec5e7657438bbcc65
Onolax <meenamonu2002@gmail.com>
2024-04-17 23:56:52 +0530

    fix-issue

--- tasks/c/medium/read_csv.c
+++ tasks/c/medium/read_csv.c
@@ -1,10 +1,47 @@
 // C - Medium
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+// Driver Code
+int main()
+{
+    FILE *file = fopen("data.csv", "r");
+    if (file == NULL)
+    {
+        printf("Fail to open file.\n");
+        return 1;
+    }
 
-int main() {
-//    TODO: Implement the program requested in the task description
+    char line[1000];
+    if (fgets(line, sizeof(line), file))
+    {
+        printf("Header: %s\n", line);
+    }
+    else
+    {
+        printf("Fail to read the header.\n");
+        fclose(file);
+        return 1;
+    }
+
+    while (fgets(line, sizeof(line), file))
+    {
+        char *token;
+
+        token = strtok(line, "\n");
+        if (token != NULL)
+        {
+            printf("Reading line: %s\n", line);
+            printf("Name: %s\n", strtok(token, ","));
+            token = strtok(NULL, ",");
+            if (token != NULL)
+            {
+                printf("Age: %s\n\n", strtok(token, "\n"));
+            }
+        }
+    }
+
+    fclose(file);
+    return 0;
 }
