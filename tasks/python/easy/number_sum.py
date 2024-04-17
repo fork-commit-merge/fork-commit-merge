ea038e4990a4c77200d88df7867e29f76ab6d66d
David Duirs <61534026+dduirs@users.noreply.github.com>
2024-04-17 12:29:23 +0200

    Update number_sum.py
    Created a function

--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,3 +1,6 @@
-number1 = input("Enter number 1: ")
-number2 = input("Enter number 2: ")
-print(int(number1)+int(number2))
+def SumNumbers():
+  number1 = input("Enter number 1: ")
+  number2 = input("Enter number 2: ")     
+  return(int(number1)+int(number2))
+
+print(SumNumbers())
