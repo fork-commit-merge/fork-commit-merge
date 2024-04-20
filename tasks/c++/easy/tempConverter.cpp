34c416f944a86e01058447e119ecde76730632cc
GitHub Action <action@github.com>
2024-04-20 07:23:08 +0000

    Revert "Merge pull request #2267 from Onolax/fix-issue-Cpp-easy"
    This reverts commit 731fe4d19b36ea6b55365443185a6a67a7caaad6, reversing
    changes made to 6ed4bbf4efd8308bf2220c082cb05571c92c9802.

--- tasks/c++/easy/tempConverter.cpp
+++ tasks/c++/easy/tempConverter.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
-using namespace std;
 
 int main() {
-  float a;
-  cout << "Please give a temperature(C)\n";
-  cin >> a;
-  cout << "Temperature(F): " << ((a * 9 / 5) + 32)<<endl;
-  return 0;
+  // TODO: Implement the temperature converter
 }
