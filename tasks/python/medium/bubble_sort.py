acc35cfc19ac9b7404aa9700a39ab64ec5d0834c
GitHub Action <action@github.com>
2024-04-20 07:24:04 +0000

    Revert "Merge pull request #2268 from dduirs/fix-issue"
    This reverts commit 02d83be9edecb348f0703aa0df9b9a7ec2e210d4, reversing
    changes made to 34c416f944a86e01058447e119ecde76730632cc.

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,15 +1,5 @@
 def bubble_sort(arr):
-    arr_length = len(arr)
-    for position in arr:
-        index = 0
-        while index < arr_length-1:
-            if arr[index] < arr[index+1]:
-                arr.insert(index,arr[index])
-            if arr[index] > arr[index+1]:
-                arr.insert(index+2,arr[index])
-            arr.pop(index)
-            index += 1 
-    return arr
+    # TODO: Implement the bubble sort algorithm
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
