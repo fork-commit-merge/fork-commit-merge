6b896a349af6cd08ab0c541b480f15e80bd64874
GitHub Action <action@github.com>
2024-04-20 07:21:59 +0000

    Revert "Merge pull request #2265 from Onolax/fix-issue-C-easy"
    This reverts commit d3e253a6e29fe2c5e41d209d3cfd650333928b5a, reversing
    changes made to 0de86a551b1dfc0eadcba804bcd962f83c99ff7d.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,16 +1,3 @@
 #include <stdio.h>
 
 // TODO: Implement the sum and main functions
-int sum(int a,int b){
-  return a+b;
-}
-
-int main(){
-  int a;
-  int b;
-  printf("Give int");
-  scanf("%d %d", &a,&b);
-  int k = sum(a,b);
-  printf("your sum = %d",k);
-  return 0;
-}
