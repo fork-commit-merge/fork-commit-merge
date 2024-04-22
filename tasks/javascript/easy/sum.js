af41843ff85f51badcee51b7cd3160827b1fa1c0
Harishankar <harishankarr7@gmail.com>
2024-04-22 01:56:14 -0400

    added the sum function

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -2,3 +2,10 @@
 
 // TODO: Write a function that returns the sum of two numbers
 
+function sum(a, b) {
+    const result = a + b;
+    console.log(`The sum of ${a} and ${b} is ${result}.`);
+}
+
+// Example usage
+sum(1, 2);
