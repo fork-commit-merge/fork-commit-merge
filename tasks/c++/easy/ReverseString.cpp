8cd64d4002e44fe90335527f19583e3a82e563ff
Onolax <meenamonu2002@gmail.com>
2024-04-23 01:19:50 +0530

    Update ReverseString.cpp

--- tasks/c++/easy/ReverseString.cpp
+++ tasks/c++/easy/ReverseString.cpp
@@ -3,9 +3,25 @@
 
 std::string reverseString(const std::string &str) {
     // TODO: Implement the reverseString function
+    int i,len=0;
+        std::string m;
+      len=str.size();
+      for(int i=0;i<len;i++){
+          m+=str[len-i-1];
+      }
+      return m;
+      
 }
 
 int main() {
     // TODO: Implement the main method
+    std::string str;
+    std::cout<<"provide string\n";
+    std::cin>>str;
+    std::string k = reverseString(str);
+    std::cout<<k;
+    return 0;
 }
+
+
 
