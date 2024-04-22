ab81fbd67faa4657ee7ff2b880d3a3d31c2a60d2
Harishankar <harishankarr7@gmail.com>
2024-04-22 02:07:39 -0400

    dart length

--- tasks/dart/easy/count_length.dart
+++ tasks/dart/easy/count_length.dart
@@ -1,7 +1,12 @@
-void main() {
-  // String text = "Example text";
-  // int lengthData = countLength(text);
-  // print('Length: $lengthData');
+int countLength(String input) {
+  int length = 0;
+  for (int i = 0; i < input.length; i++) {
+    length++;
+  }
+  return length;
 }
 
-// TODO: Create a function for counting the length of a string
+void main() {
+  String testString = "Hello, world!";
+  print("The length of the string is: ${countLength(testString)}"); // Output will be 13
+}
