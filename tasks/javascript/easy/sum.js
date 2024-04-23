9a7ed0d905172284acc560e2d87852115fc7c28a
GitHub Action <action@github.com>
2024-04-23 16:21:49 +0000

    Revert "Merge pull request #2273 from harishankarr7/added_sum_function"
    This reverts commit 3905c2fb9054e4dd3c28690a3ada1c86de15ae05, reversing
    changes made to 76884f9ae09de004e8b2570204b2cc40adfd09d5.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -2,10 +2,3 @@
 
 // TODO: Write a function that returns the sum of two numbers
 
-function sum(a, b) {
-    const result = a + b;
-    console.log(`The sum of ${a} and ${b} is ${result}.`);
-}
-
-// Example usage
-sum(1, 2);
