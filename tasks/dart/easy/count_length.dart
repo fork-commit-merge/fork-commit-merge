163e7b18d33c1fc0972169ff7b18b95ec1d537ec
GitHub Action <action@github.com>
2024-04-23 16:25:50 +0000

    Revert "Merge pull request #2276 from harishankarr7/dart_length"
    This reverts commit 90a4b037ac71a2fb37c22dcd9daa28a5e049476d, reversing
    changes made to 9f4dcad0fdd588278b03c210757ff6eae765df72.

--- tasks/dart/easy/count_length.dart
+++ tasks/dart/easy/count_length.dart
@@ -1,12 +1,7 @@
-int countLength(String input) {
-  int length = 0;
-  for (int i = 0; i < input.length; i++) {
-    length++;
-  }
-  return length;
+void main() {
+  // String text = "Example text";
+  // int lengthData = countLength(text);
+  // print('Length: $lengthData');
 }
 
-void main() {
-  String testString = "Hello, world!";
-  print("The length of the string is: ${countLength(testString)}"); // Output will be 13
-}
+// TODO: Create a function for counting the length of a string
