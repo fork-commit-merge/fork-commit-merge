9f4dcad0fdd588278b03c210757ff6eae765df72
GitHub Action <action@github.com>
2024-04-23 16:24:23 +0000

    Revert "Merge pull request #2275 from harishankarr7/dart_datatype"
    This reverts commit c02c27defe44d484060ec7412752304bfe7eeabb, reversing
    changes made to 2015e6f7b5506ef227e34e19967260f852a9143f.

--- tasks/dart/easy/type_data_check.dart
+++ tasks/dart/easy/type_data_check.dart
@@ -1,29 +1,9 @@
-String typeDataCheck(dynamic input) {
-  if (input is int) {
-    return "int";
-  } else if (input is double) {
-    return "double";
-  } else if (input is String) {
-    return "String";
-  } else if (input is bool) {
-    return "bool";
-  } else if (input is List) {
-    return "List";
-  } else if (input is Map) {
-    return "Map";
-  } else if (input is Set) {
-    return "Set";
-  } else {
-    return "Unknown";
-  }
+void main() {
+  // print(typeDataCheck(5));
+  // print(typeDataCheck(3.14));
+  // print(typeDataCheck("Hello"));
+  // print(typeDataCheck(true));
+  // print(typeDataCheck(null));
 }
 
-void main() {
-  print(typeDataCheck(42));            // int
-  print(typeDataCheck(3.14));          // double
-  print(typeDataCheck("hello"));       // String
-  print(typeDataCheck([1, 2, 3]));     // List
-  print(typeDataCheck({"key": "value"})); // Map
-  print(typeDataCheck(true));          // bool
-  print(typeDataCheck({1, 2, 3}));     // Set
-}
+// TODO: Implement a function that accepts a parameter of any type and returns a string describing its data type.
