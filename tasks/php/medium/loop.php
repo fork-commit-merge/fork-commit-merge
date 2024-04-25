0a00664d6e8194b6fda6dcdd53d4b7a2c4d914d7
lchauha <lakhan.ang@gmail.com>
2024-04-25 02:33:25 -0400

    solved-issue

--- tasks/php/medium/loop.php
+++ tasks/php/medium/loop.php
@@ -5,3 +5,8 @@
 $data = ['Pizza', 'Chicken', 'Beef', 'Rice', 'Bread'];
 
 // TODO: Display All Data From The Array
+
+foreach ($data as $item) {
+    echo $item . "\n"; // Print each item on a new line
+}
+?>
