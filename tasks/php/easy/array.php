d5ee5f9b52f381e94938cc6e8796f2fe45b57e86
lchauha <lakhan.ang@gmail.com>
2024-04-25 02:20:42 -0400

    Fixed issue

--- tasks/php/easy/array.php
+++ tasks/php/easy/array.php
@@ -5,3 +5,9 @@
 $data = ['Pizza', 'Chicken', 'Beef', 'Rice', 'Bread'];
 
 // TODO: Display data "Beef" from the array
+foreach ($foods as $food) {
+    if (strtolower($food) == "beef") { // case-insensitive comparison
+        echo "Beef\n"; // Display "Beef" to the console
+    }
+}
+?>
