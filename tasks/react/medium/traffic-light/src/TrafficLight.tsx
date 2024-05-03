6fb17ebabfce7c8cdddf158aeafc381abc2c0d04
GitHub Action <action@github.com>
2024-05-03 12:08:50 +0000

    Revert "Merge pull request #2298 from SourabhRajput123/main"
    This reverts commit 306a559f89705b7049fd90de5b488f55589432ac, reversing
    changes made to 6e7505dc5899f823ac35d3404c5d2a54d80d077f.

--- tasks/react/medium/traffic-light/src/TrafficLight.tsx
+++ tasks/react/medium/traffic-light/src/TrafficLight.tsx
@@ -1,42 +1,11 @@
-// src/TrafficLight.tsx
-
-import React, { useState, useEffect } from "react";
+// React.js - Medium 2
 import "./App.css";
 
-const TrafficLight: React.FC = () => {
-  const [activeLight, setActiveLight] = useState<string>("red");
+// TODO: Implement  the TrafficLight component
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      switch (activeLight) {
-        case "red":
-          setActiveLight("green");
-          setTimeout(() => setActiveLight("yellow"), 4000); // Change to yellow after 4 seconds of red
-          break;
-        case "green":
-          setActiveLight("yellow");
-          setTimeout(() => setActiveLight("red"), 3000); // Change to red after 3 seconds of green
-          break;
-        case "yellow":
-          setActiveLight("red");
-          setTimeout(() => setActiveLight("green"), 1000); // Change to green after 1 second of yellow
-          break;
-        default:
-          setActiveLight("red");
-          break;
-      }
-    }, 8000); // Total cycle duration: 8 seconds (4s red + 3s green + 1s yellow)
-
-    return () => clearInterval(intervalId);
-  }, [activeLight]);
+export function TrafficLight() {
 
   return (
-    <div className="traffic-light">
-      <div className={`light red ${activeLight === "red" ? "active" : ""}`} />
-      <div className={`light yellow ${activeLight === "yellow" ? "active" : ""}`} />
-      <div className={`light green ${activeLight === "green" ? "active" : ""}`} />
-    </div>
+    // TODO: return JSX here
   );
-};
-
-export default TrafficLight;
+}
