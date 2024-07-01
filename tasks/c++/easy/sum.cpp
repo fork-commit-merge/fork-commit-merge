193a1b62288d1f25cae03628fd56b90d91d8f72f
GitHub Action <action@github.com>
2024-07-01 21:25:50 +0000

    Revert "Merge pull request #2329 from varshalohakre/add-sum-program"
    This reverts commit e6c9c04bac1c1223c0cc87862cdf36f4d94a8d20, reversing
    changes made to 3d6474eef7db2047ec896041718fe81b578ef934.

--- tasks/c++/easy/sum.cpp
+++ tasks/c++/easy/sum.cpp
@@ -2,20 +2,7 @@
 
 // TODO: Create a Basic C++ Program to Add Two Numbers
 
-#include <iostream>
-
-
 int main()
 {
- int number1;
- int number2;
- 
- std::cout << "please enter your numbers " <<  std::endl;
- std::cout << "number1 : " ;
- std::cin >> number1 ;
- std::cout << "number2 : " ;
- std::cin >> number2 ;
- std::cout << "sum of given numbers is : " << number1 + number2 << std::endl;
- return 0;
-
+  
 }
