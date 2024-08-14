bfb17949d0e855615c669e4bb84ab925c299cf92
GitHub Action <action@github.com>
2024-08-14 18:20:39 +0000

    Revert "Merge pull request #2340 from Piyush3199/fix-issue"
    This reverts commit 866c16a49d00129fc239d5ee24b2119ea204b672, reversing
    changes made to 5b4fc4adac097c766c6964a707c3cb64289d3f46.

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -4,21 +4,7 @@ package tasks.java.medium;
 
 public class Calculator {
     // TODO: Implement the Calculator class
-    public static float add(float a, float b){
-        return a + b;
-    }
 
-    public static float subtract(float a, float b){
-        return a - b;
-    }
-
-    public static float multiply(float a, float b){
-        return a * b;
-    }
-
-    public static float divide(float a, float b){
-        return a / b;
-    }
     public static void main(String[] args) {
         //! Don't change the tests in the main method!
         Calculator calc = new Calculator();
