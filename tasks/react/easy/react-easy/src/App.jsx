4b0377675b45645a1dfbcf786b3e847953616038
hugosandsjo <hugosandsjo@gmail.com>
2024-08-27 10:43:33 +0200

    card fixed

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -1,10 +1,12 @@
 // React/Vite - Easy
+import Card from "./components/Card";
 import "./index.css";
 
 function App() {
   return (
     <div className="center">
       {/* TODO: Implement the card component */}
+      <Card />
     </div>
   );
 }
