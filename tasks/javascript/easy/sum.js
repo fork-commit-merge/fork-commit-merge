25e9c33db416797a13449f3207185aba62477415
GitHub Action <action@github.com>
2024-08-28 13:40:25 +0000

    Revert "Merge pull request #2351 from HannesAlexandersson/fix-issue"
    This reverts commit 4970bfb5212c11c0ec3e5e5de5f0183b88bf2f74, reversing
    changes made to 9ec8ce8564b5fb9e16281ed162ee58d970c0962b.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -2,13 +2,3 @@
 
 // TODO: Write a function that returns the sum of two numbers
 
-
-function Sum(num1, num2){
-    const sum = num1 + num2;
-
-    return sum;
-}
-const num1 = 1;
-const num2 = 2;
-
-console.log(`The sum of ${num1} and ${num2} is`, Sum(num1, num2));
