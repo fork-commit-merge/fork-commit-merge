d5744ae838c375522bb483ee5e2873c07602a99c
GitHub Action <action@github.com>
2024-08-28 06:08:29 +0000

    Revert "Merge pull request #2348 from hugosandsjo/fix-issue"
    This reverts commit 53f094d437edf0fb6e885592e349115c20a8c165, reversing
    changes made to 78195fd754d1a1b72a80890c610d71514732be81.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -1,12 +1,10 @@
 // React/Vite - Easy
-import Card from "./components/Card";
 import "./index.css";
 
 function App() {
   return (
     <div className="center">
       {/* TODO: Implement the card component */}
-      <Card />
     </div>
   );
 }
