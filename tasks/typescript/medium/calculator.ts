93a6c09722d5bc73934992f66a206fde5cb829d5
GitHub Action <action@github.com>
2024-09-16 20:25:57 +0000

    Revert "Merge pull request #2373 from JSamuelAP/issue-medium-ts"
    This reverts commit 60fa03a3f3ecdd821811e1d26c8bc824cae966af, reversing
    changes made to f005285b27f2898c65f7651f7bae0116f486202d.

--- tasks/typescript/medium/calculator.ts
+++ tasks/typescript/medium/calculator.ts
@@ -1,25 +1,7 @@
 // TypeScript - Medium
 
 namespace Calculator {
-	export function add(n1: number, n2: number): number {
-		return n1 + n2;
-	}
-
-	export function subtract(n1: number, n2: number): number {
-		return n1 - n2;
-	}
-
-	export function multiply(n1: number, n2: number): number {
-		return n1 * n2;
-	}
-
-	export function divide(n1: number, n2: number): number | null {
-		if (n2 === 0) {
-			console.warn("n2 parameter cannot be 0");
-			return null;
-		}
-		return n1 / n2;
-	}
+    // TODO: Implement a Simple TypeScript Calculator
 }
 
 // Test cases
