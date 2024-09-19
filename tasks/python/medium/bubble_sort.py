3563594fad4928efd09acc26cc211b66b67d7e85
Jackziel-Sumoza <sjackziel1234@gmail.com>
2024-09-19 08:22:06 +0300

    Creando una funcion de burbuja para ordenar de menor a mayor una lista de numeros y devolviendo en consola la primera lista y la lista ordenada.

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,5 +1,14 @@
 def bubble_sort(arr):
     # TODO: Implement the bubble sort algorithm
+    for loop in range(len(arr)-1):
+        switch = False
+        for item in range(len(arr)-loop-1):
+            if arr[item] > arr[item+1]:
+                arr[item], arr[item+1] = arr[item+1], arr[item]
+                switch = True
+        if not switch:
+            break
+    return arr
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
