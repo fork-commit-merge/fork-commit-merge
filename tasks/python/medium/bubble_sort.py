928993119d320a5ccf553948db8f91ffad44f969
GitHub Action <action@github.com>
2024-09-19 15:30:57 +0000

    Revert "Merge pull request #2380 from personal-trainer-github/fix-issue"
    This reverts commit d0ead170e20ba1e7ca6428e0bc9b0b201d5dd7b2, reversing
    changes made to 6ea18641df29421e39606dc60ab6579fd7b512e7.

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,18 +1,9 @@
 def bubble_sort(arr):
     # TODO: Implement the bubble sort algorithm
-    for loop in range(len(arr)-1):
-        switch = False
-        for item in range(len(arr)-loop-1):
-            if arr[item] > arr[item+1]:
-                arr[item], arr[item+1] = arr[item+1], arr[item]
-                switch = True
-        if not switch:
-            break
-    return arr
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
-# print("Original array:", arr)
+print("Original array:", arr)
 
 bubble_sort(arr)
 print("Sorted array:", arr)
