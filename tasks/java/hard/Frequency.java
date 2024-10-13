c4947c8b0b30a95b46da6c2254e293612d12b492
GitHub Action <action@github.com>
2024-10-13 19:32:59 +0000

    Revert "Merge pull request #2397 from masahiro0000/Fix-issue#1442"
    This reverts commit f54af294d45cc21055e689d4ffb510d70022db04, reversing
    changes made to ac1d53a0a36b9f532a86fcfc0bf9260dcbf1d639.

--- tasks/java/hard/Frequency.java
+++ tasks/java/hard/Frequency.java
@@ -1,21 +1,8 @@
 // Java - Hard
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Frequency {
 
     public static void main(String[] args) {
         // TODO: Given an array of integers, write a Java program to find the frequency of each element using a Map
-        int[] array = {1, 1, 2, 2, 2, 3, 4, 4, 5, 5, 5, 5};
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-
-        for(int num: array){
-            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
-        }
-
-        for(Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()){
-            System.out.println(entry.getKey() +  ":" + entry.getValue());
-        }
     }
 }
