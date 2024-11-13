cc5326541b3e6e1a79c9a2f05eebd50b9f73907b
Egnodia <egnodera@gmail.com>
2024-11-13 19:41:13 -0400

    Fixed issue

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,4 +1,6 @@
 // JavaScript - Easy
-
+function sum(num1, num2){
+    return `The sum of ${num1} and ${num2} is ${num1 + num2}.`;
+}
 // TODO: Write a function that returns the sum of two numbers
-
+console.log(sum(1, 3));
