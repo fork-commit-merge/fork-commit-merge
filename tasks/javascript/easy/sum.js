f6f9f1a908074e74b7a8b3e643aea3197e4fecce
Arki <107225218+arkeasz@users.noreply.github.com>
2025-01-10 16:04:47 -0500

    Update sum.js

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,4 +1,7 @@
 // JavaScript - Easy
-
+function sum(a,b)  {
+  let re = a + b
+  return `The sum of ${a} and ${b} is ${re}` 
+}
 // TODO: Write a function that returns the sum of two numbers
-
+console.log(sum(1,2))
