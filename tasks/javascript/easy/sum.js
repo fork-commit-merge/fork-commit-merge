0bb0def1332e6b023e75fc81a5a9ab4e66e8704c
GitHub Action <action@github.com>
2025-01-11 09:11:00 +0000

    Revert "Merge pull request #2428 from arkeasz/main"
    This reverts commit 78d5280f66798d512be0170ea710ff0dc24f52da, reversing
    changes made to 43f855fc2bdee7fdc45151332231ae4292d7b101.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,7 +1,4 @@
 // JavaScript - Easy
-function sum(a,b)  {
-  let re = a + b
-  return `The sum of ${a} and ${b} is ${re}` 
-}
+
 // TODO: Write a function that returns the sum of two numbers
-console.log(sum(1,2))
+
