772f6bf447c650013bac24fe53561315bdcb3b22
mieka <miekachan.5656@gmail.com>
2025-01-31 08:03:23 +0900

    First commit

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,8 +1,16 @@
 // Java - Easy
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class GreetingApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // TODO: Implement a Simple Greeting Application
-    }
+        System.out.println("Input your name.");
+        String name = br.readLine();
+        System.out.println(name +"Nice too meet you.");
+    }   
 }
