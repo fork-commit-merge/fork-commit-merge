494ce7b1650e31ea21adc8d6d1af73f122bf8da7
shaian <zshaian@users.noreply.github.com>
2025-03-11 05:46:42 +0800

    feat(task): create the easy task for solid js

--- tasks/solid/easy/src/App.jsx
+++ tasks/solid/easy/src/App.jsx
@@ -1,24 +1,33 @@
-import logo from "./logo.svg";
 import styles from "./App.module.css";
 
 function App() {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
+    <main class={styles.counterContainer}>
+      {/* TODO: Implement the counter state with the name of "count" and a setter named "setCount"  */}
+
+      {/* TODO: Display the current count state in here */}
+      <p class={styles.countNumber}></p>
+
+      {/* TODO: Implement the setter function for incrementing and decrementing the count state. */}
+      <div class={styles.countButtonsContainer}>
+        <button
+          onClick={() => {
+            // Implement the function to increment the counter state here.
+          }}
+          class={styles.countButtons}
         >
-          Hello solid
-        </a>
-      </header>
-    </div>
+          +1
+        </button>
+        <button
+          onClick={() => {
+            // Implement the function to decrement the counter state here.
+          }}
+          class={styles.countButtons}
+        >
+          -1
+        </button>
+      </div>
+    </main>
   );
 }
 
