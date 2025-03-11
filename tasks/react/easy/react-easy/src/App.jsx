b4b00502c81730be7982243f9e0f3a4c119909b2
GitHub Action <action@github.com>
2025-03-11 09:24:07 +0000

    Revert "Merge pull request #2455 from VarishSanghvi/fix-react-card"
    This reverts commit d65ab993c932930145376249bcbd50d64d7bca28, reversing
    changes made to f4d744fd94ef9fa5f74639ca03029eac3ca9769b.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -5,12 +5,6 @@ function App() {
   return (
     <div className="center">
       {/* TODO: Implement the card component */}
-      <h1>React/Vite - Easy</h1>
-      <div className="card">
-        <img src="https://loremflickr.com/320/240" alt="Random" />
-        <h2>Pose</h2>
-        <p>Relaxing while uploading</p>
-      </div>
     </div>
   );
 }
