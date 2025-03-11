75a33d1d83f356762bd37b40fd64fbe55bb329b6
Aman Ahsan <amanahsan01@gmail.com>
2025-03-11 09:47:22 +0300

    Python medium task solved

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,5 +1,13 @@
 def bubble_sort(arr):
-    # TODO: Implement the bubble sort algorithm
+    n = len(arr)
+    for i in range(n - 1, 0, -1):
+        swap = False
+        for j in range(i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swap = True
+        if not swap:
+            break
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
