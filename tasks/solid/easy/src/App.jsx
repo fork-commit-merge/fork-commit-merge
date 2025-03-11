295661fb1103a6051ba012c1782a47c8a27881bf
VarishSanghv <varishsanghvi155@gmail.com>
2025-03-11 13:35:02 +0530

    Implemented Simple Counter in Solid.js

--- tasks/solid/easy/src/App.jsx
+++ tasks/solid/easy/src/App.jsx
@@ -1,18 +1,20 @@
 import styles from "./App.module.css";
-
+import { createSignal } from "solid-js";  
 function App() {
+  const [count, setCount] = createSignal(0);
   return (
     <main class={styles.counterContainer}>
       {/* TODO: Implement the counter state with the name of "count" and a setter named "setCount"  */}
 
       {/* TODO: Display the current count state in here */}
-      <p class={styles.countNumber}></p>
+      <p class={styles.countNumber}>{count()}</p>
 
       {/* TODO: Implement the setter function for incrementing and decrementing the count state. */}
       <div class={styles.countButtonsContainer}>
         <button
           onClick={() => {
             // Implement the function to increment the counter state here.
+            setCount(count() + 1)
           }}
           class={styles.countButtons}
         >
@@ -21,6 +23,7 @@ function App() {
         <button
           onClick={() => {
             // Implement the function to decrement the counter state here.
+            setCount(count() - 1)
           }}
           class={styles.countButtons}
         >
