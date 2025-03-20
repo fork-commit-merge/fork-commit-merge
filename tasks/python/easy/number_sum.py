f556113f184e9b6aaea152d3c32387a566db382d
GitHub Action <action@github.com>
2025-03-20 09:52:17 +0000

    Revert "Merge pull request #2597 from perez-21/python-easy-1"
    This reverts commit 775de55d99938f8b8e221b9ee64255d0ebb4081c, reversing
    changes made to 831dfa5a97580481a8bcb6c6ee3772dbff6364d9.

--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,12 +1,3 @@
 # Python - Easy
 
 # TODO: Write a function that returns the sum of two numbers
-
-
-def add(num1, num2):
-    return num1 + num2
-
-
-num1 = 6
-num2 = 5
-print(f"{num1} + {num2} = {add(num1, num2)}")
