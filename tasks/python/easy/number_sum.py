914a85560052bf3679d20462700fb6d9e165ba70
Perez-salem Tuwan <perezjoshua1811@gmail.com>
2025-03-20 10:44:57 +0100

    complete python easy issue 2

--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,3 +1,12 @@
 # Python - Easy
 
 # TODO: Write a function that returns the sum of two numbers
+
+
+def add(num1, num2):
+    return num1 + num2
+
+
+num1 = 6
+num2 = 5
+print(f"{num1} + {num2} = {add(num1, num2)}")
