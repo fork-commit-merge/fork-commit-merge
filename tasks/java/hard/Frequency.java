1fdf38344536a2877a2a3cd7d1e35860fd620736
harlanenciso112 <harsanenciso@gmail.com>
2025-03-22 13:25:33 -0500

    Frequency resolved

--- tasks/java/hard/Frequency.java
+++ tasks/java/hard/Frequency.java
@@ -1,8 +1,18 @@
 // Java - Hard
 
-public class Frequency {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Frequency {
     public static void main(String[] args) {
-        // TODO: Given an array of integers, write a Java program to find the frequency of each element using a Map
+        Map<Integer, Integer> count = new HashMap<>();
+        int[] array = {4, 2, 4, 3, 2, 4, 5, 1, 3, 2};
+
+        
+        for (int num : array) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        count.forEach((clave, valor) -> System.out.println(clave + " -> " + valor));
     }
 }
