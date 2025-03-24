26875145334e22aa96464137454c6590f796f7e3
GitHub Action <action@github.com>
2025-03-24 05:39:57 +0000

    Revert "Merge pull request #2628 from BotirBakhtiyarov/patch-1"
    This reverts commit 0a1abe7ea715802bff3cad90a9e00b6103987436, reversing
    changes made to cbacb2422ca3d8594c1968667508cf6972ef6447.

--- tasks/flask/easy/app.py
+++ tasks/flask/easy/app.py
@@ -2,8 +2,7 @@ from flask import Flask
 
 app = Flask(__name)
 
-def home():
-    return "Hello, Flask!"  
+# TODO: Implement the task requested in the issue description
 
 if __name__ == "__main__":
     app.run(debug=True)
