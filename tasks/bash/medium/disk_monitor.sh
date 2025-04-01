f0e2ab49c6792b9cfe2e22d9e377ebf69f6142a7
slow-connect <louisdeberlin+github@gmail.com>
2025-04-01 23:57:34 +0200

    Fixed Bash Medium

--- tasks/bash/medium/disk_monitor.sh
+++ tasks/bash/medium/disk_monitor.sh
@@ -3,3 +3,15 @@
 # Bash - Medium
 
 # TODO: Implement the Disk Monitor script
+
+if [ $# -gt 0 ]; then
+    THRESHOLD=$1
+else
+    echo -e "Usage:\nrun ./disk_monitor.sh 20 to check if threshold for low disk space is below 20%"
+    exit 0
+fi
+
+FREESPACE=$(df -Ph | tail -1 | awk '{print $5}' | cut -d'%' -f1)
+if [ $FREESPACE -lt $THRESHOLD ]; then
+    mail -s "DISKSPACE LOW" "me@mail"
+fi
