fa49d9a06fcf0aa713ca7c4e79e96c457077c161
slow-connect <louisdeberlin+github@gmail.com>
2025-04-02 00:01:33 +0200

    Removed solution to Bash easy

--- tasks/bash/easy/create_files.sh
+++ tasks/bash/easy/create_files.sh
@@ -3,11 +3,3 @@
 # Bash - Easy
 
 # TODO: Write the Bash script requested in the issue description
-cd ../../..
-mkdir -p files
-
-for i in $(seq 1 3);
-do
-    echo "Hello Shell!" > files/file$i.txt
-    cat files/file$i.txt
-done
