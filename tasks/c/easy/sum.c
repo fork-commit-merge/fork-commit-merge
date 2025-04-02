894cf0fc6b0533e0565a7fc1d08c5d34210cf2d8
GitHub Action <action@github.com>
2025-04-02 07:18:59 +0000

    Revert "Merge pull request #2782 from slow-connect/C"
    This reverts commit e9da0e89bc5bd1e41bfd589c6b3447cf669c7285, reversing
    changes made to 139136f93e108bffb84bfe6af51a554beb9dfb48.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,15 +1,3 @@
 #include <stdio.h>
 
 // TODO: Implement the sum and main functions
-int sum(int a, int b){
-    return a + b;
-}
-
-int main(){
-    printf("Enter two numbers: ");
-    int a;
-    int b;
-    scanf("%d %d", &a, &b);
-    printf("The sum is: %d\n", sum(a, b));
-    return 0;
-}
