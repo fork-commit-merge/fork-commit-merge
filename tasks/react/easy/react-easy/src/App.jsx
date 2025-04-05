39fdc525d18f6b40557308b87c9f9daccefeeacf
Ahmad Wali Hemat <ahmadwalihemat67@gmail.com>
2025-04-05 22:19:46 +0430

    create Card component

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -3,9 +3,7 @@ import "./index.css";
 
 function App() {
   return (
-    <div className="center">
-      {/* TODO: Implement the card component */}
-    </div>
+    <div className="center">{/* TODO: Implement the card component */}</div>
   );
 }
 
---
+++ tasks/react/easy/react-easy/src/Card.jsx
@@ -0,0 +1,5 @@
+function Card() {
+  return <div className="card"></div>;
+}
+
+export default Card;
