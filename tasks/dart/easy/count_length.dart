15166e2a15572aa3ec8a643f0c07f628a78b8b26
GitHub Action <action@github.com>
2025-04-08 10:57:59 +0000

    Revert "Merge pull request #2799 from Ehsan2009/main"
    This reverts commit 8718acb4526b7169d887458f4f1b1982e8f7c4d6, reversing
    changes made to c13d9b7866fb888524077599d6148b1cef9f551c.

--- tasks/dart/easy/count_length.dart
+++ tasks/dart/easy/count_length.dart
@@ -2,11 +2,6 @@ void main() {
   // String text = "Example text";
   // int lengthData = countLength(text);
   // print('Length: $lengthData');
-  final inputLength = stringLengthCounter('Hello');
-  print(inputLength);
 }
 
 // TODO: Create a function for counting the length of a string
-int stringLengthCounter(String input) {
-  return input.length;
-}
