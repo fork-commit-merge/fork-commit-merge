f34e5fc6556b9693265d6252d036edfbadd559fe
GitHub Action <action@github.com>
2025-04-13 07:31:45 +0000

    Revert "Merge pull request #2805 from flaviosp15/fix-issue"
    This reverts commit 4bb83b1b6c3b90fd170691f15a55b2a700b3fece, reversing
    changes made to 94c64355e0e255e60c34a4d77834dd796fe3842b.

--- tasks/javascript/jest/hard/src/library.js
+++ tasks/javascript/jest/hard/src/library.js
@@ -1,56 +1,47 @@
 // JavaScript - Hard
 
 class Library {
-  constructor() {
-    this.books = [];
-  }
-
-  createBook(title, author) {
-    const book = {
-      title,
-      author,
-      id: this.books.length + 1,
-    };
-    this.books.push(book);
-    return book;
-  }
-
-  getBook(id) {
-    return this.books.find(book => book.id === id);
-  }
-
-  updateBook(id, title, author) {
-    const bookIndex = this.books.findIndex(book => book.id === id);
-    if (bookIndex !== -1) {
-      this.books[bookIndex] = { id, title, author };
+    constructor() {
+        this.books = [];
     }
-    return this.books[bookIndex];
-  }
 
-  deleteBook(id) {
-    const bookIndex = this.books.findIndex(book => book.id === id);
-    if (bookIndex !== -1) {
-      this.books.splice(bookIndex, 1);
+    createBook(title, author) {
+        const book = {
+            title,
+            author,
+            id: this.books.length + 1,
+        };
+        this.books.push(book);
+        return book;
     }
-    return this.books;
-  }
 
-  searchBooks(query) {
-    return this.books.filter(book => {
-      const authorMatch = new RegExp(query, 'i').test(book.author);
-      const titleMatch = new RegExp(query, 'i').test(book.title);
+    getBook(id) {
+        return this.books.find((book) => book.id === id);
+    }
 
-      return authorMatch || titleMatch;
-    });
-  }
+    updateBook(id, title, author) {
+        const bookIndex = this.books.findIndex((book) => book.id === id);
+        if (bookIndex !== -1) {
+            this.books[bookIndex] = { id, title, author };
+        }
+        return this.books[bookIndex];
+    }
 
-  filterBooks(criteria) {
-    const chosenCriteria = criteria.title || criteria.author;
+    deleteBook(id) {
+        const bookIndex = this.books.findIndex((book) => book.id === id);
+        if (bookIndex !== -1) {
+            this.books.splice(bookIndex, 1);
+        }
+        return this.books;
+    }
 
-    if (chosenCriteria) return this.searchBooks(chosenCriteria);
+    searchBooks(query) {
+        // TODO: Implement search functionality
+    }
 
-    return [];
-  }
+    filterBooks(criteria) {
+        // TODO: Implement filter functionality
+    }
 }
 
 module.exports = Library;
