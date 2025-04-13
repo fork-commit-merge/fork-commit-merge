57c9b80f210f23da6dda1c7cc171eba5cefac38d
GitHub Action <action@github.com>
2025-04-13 14:48:24 +0000

    Revert "Merge pull request #2807 from Alan-Augusto/fix-python"
    This reverts commit 274a3fe761bca2da83fb1f3aef68928007e176db, reversing
    changes made to 1fe2b9e104d57fda184e1877746e495edbb3e194.

--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,13 +1,3 @@
-def sum_with_interface():
-    a = int(input("Enter the first number: "))
-    b = int(input("Enter the second number: "))
-    result = a + b
+# Python - Easy
 
-    print("\n╔" + "═" * 30 + "╗")
-    print("║{:^30}║".format("SUM OF TWO NUMBERS"))
-    print("╠" + "═" * 30 + "╣")
-    print("║{:^30}║".format(f"{a} + {b} = {result}"))
-    print("╚" + "═" * 30 + "╝")
-
-# Run the function
-sum_with_interface()
+# TODO: Write a function that returns the sum of two numbers
