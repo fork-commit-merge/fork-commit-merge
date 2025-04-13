e8adf516fa5c4384bdfe26ce73bad5d5050ffe76
flaviosp15 <flaviosp15@outlook.com>
2025-04-13 03:12:48 -0300

    Fixed issue

--- tasks/javascript/jest/hard/src/library.js
+++ tasks/javascript/jest/hard/src/library.js
@@ -1,47 +1,56 @@
 // JavaScript - Hard
 
 class Library {
-    constructor() {
-        this.books = [];
-    }
+  constructor() {
+    this.books = [];
+  }
 
-    createBook(title, author) {
-        const book = {
-            title,
-            author,
-            id: this.books.length + 1,
-        };
-        this.books.push(book);
-        return book;
-    }
+  createBook(title, author) {
+    const book = {
+      title,
+      author,
+      id: this.books.length + 1,
+    };
+    this.books.push(book);
+    return book;
+  }
 
-    getBook(id) {
-        return this.books.find((book) => book.id === id);
-    }
+  getBook(id) {
+    return this.books.find(book => book.id === id);
+  }
 
-    updateBook(id, title, author) {
-        const bookIndex = this.books.findIndex((book) => book.id === id);
-        if (bookIndex !== -1) {
-            this.books[bookIndex] = { id, title, author };
-        }
-        return this.books[bookIndex];
+  updateBook(id, title, author) {
+    const bookIndex = this.books.findIndex(book => book.id === id);
+    if (bookIndex !== -1) {
+      this.books[bookIndex] = { id, title, author };
     }
+    return this.books[bookIndex];
+  }
 
-    deleteBook(id) {
-        const bookIndex = this.books.findIndex((book) => book.id === id);
-        if (bookIndex !== -1) {
-            this.books.splice(bookIndex, 1);
-        }
-        return this.books;
+  deleteBook(id) {
+    const bookIndex = this.books.findIndex(book => book.id === id);
+    if (bookIndex !== -1) {
+      this.books.splice(bookIndex, 1);
     }
+    return this.books;
+  }
 
-    searchBooks(query) {
-        // TODO: Implement search functionality
-    }
+  searchBooks(query) {
+    return this.books.filter(book => {
+      const authorMatch = new RegExp(query, 'i').test(book.author);
+      const titleMatch = new RegExp(query, 'i').test(book.title);
 
-    filterBooks(criteria) {
-        // TODO: Implement filter functionality
-    }
+      return authorMatch || titleMatch;
+    });
+  }
+
+  filterBooks(criteria) {
+    const chosenCriteria = criteria.title || criteria.author;
+
+    if (chosenCriteria) return this.searchBooks(chosenCriteria);
+
+    return [];
+  }
 }
 
 module.exports = Library;
