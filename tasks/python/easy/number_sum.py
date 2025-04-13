449e09b92403341eee9d54e5d3807a157197b040
alan-augusto <augustoalan56@gmail.com>
2025-04-13 11:35:39 -0300

    Fixed issue

--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,3 +1,13 @@
-# Python - Easy
+def sum_with_interface():
+    a = int(input("Enter the first number: "))
+    b = int(input("Enter the second number: "))
+    result = a + b
 
-# TODO: Write a function that returns the sum of two numbers
+    print("\n╔" + "═" * 30 + "╗")
+    print("║{:^30}║".format("SUM OF TWO NUMBERS"))
+    print("╠" + "═" * 30 + "╣")
+    print("║{:^30}║".format(f"{a} + {b} = {result}"))
+    print("╚" + "═" * 30 + "╝")
+
+# Run the function
+sum_with_interface()
