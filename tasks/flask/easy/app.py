e6eca66e5fc0c41d32d6e8a04c7bdc8d6716a42f
GitHub Action <action@github.com>
2025-04-20 05:52:32 +0000

    Revert "Merge pull request #2816 from murugnn/patch-1"
    This reverts commit 7c0ea7929858e5b6061e9623ce16f31a46a98b96, reversing
    changes made to 515f79acef1b10908782bb2255f2bd67eba9d4cf.

--- tasks/flask/easy/app.py
+++ tasks/flask/easy/app.py
@@ -4,10 +4,5 @@ app = Flask(__name)
 
 # TODO: Implement the task requested in the issue description
 
-@app.route("/")
-@app.route("/home")
-def home():
-    return "Hello World"
-
 if __name__ == "__main__":
     app.run(debug=True)
