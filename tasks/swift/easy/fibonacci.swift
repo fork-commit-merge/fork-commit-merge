c7bf9c1fef7bfccd99fd19cbf9f8953e2d7eb14d
GitHub Action <action@github.com>
2025-04-25 05:03:52 +0000

    Revert "Merge pull request #2955 from nomansheikh/feat/swift-fibonacci"
    This reverts commit ef40d48af954b7ab614515067be2bc587a91d0c7, reversing
    changes made to 8ef7abe18781124664bafcac3bedd05623fdd570.

--- tasks/swift/easy/fibonacci.swift
+++ tasks/swift/easy/fibonacci.swift
@@ -3,8 +3,7 @@
 import Foundation
 
 func fibonacci(n: Int) -> Int {
-    if n < 2 { return n }
-    else { return fibonacci(n: n - 1) + fibonacci(n: n - 2) }
+    // TODO: Implement the fibonacci sequence function
 }
 
 // Test cases (Don't touch!)
