b848132f24af0d93f71f5caa0d4943e00e4a19cc
AlexandrAnatoliev <per-1986@list.ru>
2025-04-25 15:08:17 +0300

    feat: add add-method to class

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -3,15 +3,19 @@
 package tasks.java.medium;
 
 public class Calculator {
-    // TODO: Implement the Calculator class
+  // TODO: Implement the Calculator class
 
-    public static void main(String[] args) {
-        //! Don't change the tests in the main method!
-        Calculator calc = new Calculator();
+  public static void main(String[] args) {
+    // ! Don't change the tests in the main method!
+    Calculator calc = new Calculator();
 
-        System.out.println("Addition: " + calc.add(5, 3));
-        System.out.println("Subtraction: " + calc.subtract(5, 3));
-        System.out.println("Multiplication: " + calc.multiply(5, 3));
-        System.out.println("Division: " + calc.divide(5, 3));
-    }
+    System.out.println("Addition: " + calc.add(5, 3));
+    // System.out.println("Subtraction: " + calc.subtract(5, 3));
+    // System.out.println("Multiplication: " + calc.multiply(5, 3));
+    // System.out.println("Division: " + calc.divide(5, 3));
+  }
+
+  public double add(int num1, int num2) {
+    return (double) (num1 + num2);
+  }
 }
