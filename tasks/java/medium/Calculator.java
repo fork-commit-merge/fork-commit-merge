f0fb05a8e14931af07301b8aa09aba1c76907654
AlexandrAnatoliev <per-1986@list.ru>
2025-04-25 21:14:49 +0300

    feat: add double add (double, double) implementation

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -10,6 +10,7 @@ public class Calculator {
     Calculator calc = new Calculator();
 
     System.out.println("Addition: " + calc.add(5, 3));
+    System.out.println("Addition: " + calc.add(5.0, 3.0));
     // System.out.println("Subtraction: " + calc.subtract(5, 3));
     // System.out.println("Multiplication: " + calc.multiply(5, 3));
     // System.out.println("Division: " + calc.divide(5, 3));
@@ -17,5 +18,9 @@ public class Calculator {
 
   public double add(int num1, int num2) {
     return (double) (num1 + num2);
+  }
+
+  public double add(double num1, double num2) {
+    return num1 + num2;
   }
 }
