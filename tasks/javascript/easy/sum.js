1bb3c438e9ec35e86cd13e77000ff4b54910be36
GitHub Action <action@github.com>
2025-04-30 19:26:39 +0000

    Revert "Merge pull request #2969 from Aujooj/fix-issue"
    This reverts commit ab15ef9fe00f9c38ec574c4edf761c44fd2b1b43, reversing
    changes made to 119fd9422dc6c92635703e84311f55271d2d38db.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,7 +1,3 @@
 // JavaScript - Easy
 
-function sum(number1, number2) {
-    return `The sum of ${number1} and ${number2} is ${number1 + number2}.`;
-}
-
-console.log(sum(1, 2));
+// TODO: Write a function that returns the sum of two numbers
--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,26 +1,3 @@
 // JavaScript - Medium
 
-document.addEventListener("DOMContentLoaded", () => {
-    let timeLeft = 10;
-    const timerEl = document.getElementById("timer");
-
-    const updateDisplay = () => {
-        const minutes = Math.floor(timeLeft / 60);
-        const seconds = timeLeft % 60;
-        timerEl.textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-    };
-
-    const tick = () => {
-        if (timeLeft > 0) {
-            timeLeft--;
-            updateDisplay();
-        } else {
-            clearInterval(timerId);
-            timerEl.textContent = "Time's up!";
-        }
-    };
-
-    updateDisplay();
-    const timerId = setInterval(tick, 1000);
-});
-
+// Create functionality for a countdown timer that counts down from 10 seconds to zero using JavaScript
