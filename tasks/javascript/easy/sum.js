f9098c750a506d49130184257c7498ea761f2175
João Santos <jhojhoaugusto@gmail.com>
2025-04-30 15:08:44 -0300

    Javascript-Issue-1514

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,3 +1,7 @@
 // JavaScript - Easy
 
-// TODO: Write a function that returns the sum of two numbers
+function sum(number1, number2) {
+    return `The sum of ${number1} and ${number2} is ${number1 + number2}.`;
+}
+
+console.log(sum(1, 2));
