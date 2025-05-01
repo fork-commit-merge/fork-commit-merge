58a1195734bfe98c4fc1f1ba24ff800071da26e0
GitHub Action <action@github.com>
2025-05-01 16:53:51 +0000

    Revert "Merge pull request #3110 from AbraxasContact/fix-issue"
    This reverts commit e99c506eb8b92322b0875ec07058f06b00c890d9, reversing
    changes made to eee94af1f3e0fb08f3ed96f77d770efd48958596.

--- tasks/java/hard/Frequency.java
+++ tasks/java/hard/Frequency.java
@@ -4,28 +4,5 @@ public class Frequency {
 
     public static void main(String[] args) {
         // TODO: Given an array of integers, write a Java program to find the frequency of each element using a Map
-
-        int[] array = {1, 2, 3, 2, 1, 3, 4, 5, 4, 2, 1};
-
-        // Create a HashMap to store elements and their frequencies
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-
-        // Iterate through the array
-        for (int num : array) {
-            // If the number is already in the map, increment its count
-            if (frequencyMap.containsKey(num)) {
-                frequencyMap.put(num, frequencyMap.get(num) + 1);
-            }
-            // Otherwise, add the number to the map with count 1
-            else {
-                frequencyMap.put(num, 1);
-            }
-        }
-
-        // Print the frequency of each element
-        System.out.println("Element frequencies:");
-        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue() + " times");
-        }
     }
 }
