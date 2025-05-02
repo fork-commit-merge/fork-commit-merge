378e48808f21a9bc32ee394bd5138837a3826e44
GitHub Action <action@github.com>
2025-05-02 18:37:18 +0000

    Revert "Merge pull request #3114 from AlexandrAnatoliev/fix-issue-3055"
    This reverts commit 56d3270483918ca8fb3109e59010595d3fc6a95d, reversing
    changes made to a037a41691c542ae437ea9ca8bfc2f37b82f753b.

--- tasks/java/hard/Frequency.java
+++ tasks/java/hard/Frequency.java
@@ -1,21 +1,8 @@
 // Java - Hard
-import java.util.HashMap;
 
 public class Frequency {
 
     public static void main(String[] args) {
         // TODO: Given an array of integers, write a Java program to find the frequency of each element using a Map
-
-    	int [] array = {1, 2, 2, 2, 5, 6 ,2, 6, 8, 9};
-    	var map = new HashMap<Integer, Integer>();
-
-     	for(int i = 0; i < array.length; i++) {
-    		map.putIfAbsent(array[i], 0);
-    		map.put(array[i], map.get(array[i]) + 1);
-    	}
-     	
-    	map.forEach((k, v) -> {
-    		System.out.println(k + ": " + v);
-    	});
     }
 }
