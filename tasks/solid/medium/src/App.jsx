b805f61fbd49c2ce65bf7f9dfec7a8fc75134985
GitHub Action <action@github.com>
2025-05-02 04:58:09 +0000

    Revert "Merge pull request #3112 from Soham-47/fix-file-issue"
    This reverts commit d192f6388f267d4d9d668499503deadbce1aa579, reversing
    changes made to b5ebee8d795dc54fb16ac5d462e6ebfa1952b575.

--- tasks/solid/medium/src/App.jsx
+++ tasks/solid/medium/src/App.jsx
@@ -1,5 +1,4 @@
 import styles from "./App.module.css";
-import { createSignal, createResource, Switch, Match, Show } from "solid-js";
 
 function App() {
   // API URL to send the request to
@@ -9,26 +8,11 @@ function App() {
   // { slip: { id: 123, advice: "This is an example advice." } }
   const fetchQuote = async () => {
     // TODO: Implement the fetchQuote function to retrieve an advice quote.
-    
-      const response= await fetch("https://api.adviceslip.com/advice")
-      const data=await response.json()
-      return data.slip.advice
-      
-    };
-
-
+  };
 
   // TODO: Implement a signal function to manage the fetchQuote data.
   // TIP: You can use Solid.js's createResource utility function.
   // Documentation: https://docs.solidjs.com/guides/fetching-data
-
-  
-  const [Quote, setQuote] = createSignal()
-  const [quote] = createResource(Quote, fetchQuote)
-
-  
-
-  
 
   return (
     <main class={styles.quoteContainer}>
@@ -37,36 +21,11 @@ function App() {
 
       - Loading: <p class={styles.loadingText}>Fetching quote...</p>
       - Error: <p class={styles.errorText}>Failed to fetch the quote.</p>
-      - Success: <p class={styles.quotes}>Quote appears here.</p> */
-      <div>
-        <Show when={quote.loading}>
-        <p class={styles.loadingText}>Fetching quote...</p>
-      </Show>
-      <Switch>
-        <Match when={quote.error}>
-          <p class={styles.errorText}>Failed to fetch the quote.</p>
-        </Match>
-        <Match when={quote()}>
-          <div>{quote()}</div>
-        </Match>
-      </Switch>
-
-      </div>
-      
-      }
-
+      - Success: <p class={styles.quotes}>Quote appears here.</p> */}
 
       <button
         onClick={() => {
           // Implement the refetch function here to fetch new quotes.
-          const fetchNewQuote=()=>{
-            setQuote(new Date().getTime());
-
-          }
-
-          {fetchNewQuote}
-
-
         }}
         class={styles.newQuoteButton}
       >
