c5840c2df4e89bb38fb7042f06cf4e4bea9b0868
Harsh dabhade <harshdabhade8867@gmail.com>
2025-05-03 23:04:38 +0530

    Solved the Easy Python Problem

--- tasks/python/easy/hello.py
+++ tasks/python/easy/hello.py
@@ -1,3 +1,8 @@
 # Python - Easy
+from tasks.python.easy.number_list import function
 
 # TODO: Write a function that prints "Hello, Python!" to the console
+def hello():
+    print("Hello, Python!")
+
+hello()
