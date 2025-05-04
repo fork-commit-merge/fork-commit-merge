532826f419d1d30373687a99f649a5619a3f2af7
GitHub Action <action@github.com>
2025-05-04 10:32:49 +0000

    Revert "Merge pull request #3120 from dbto/solve-task-python-medium"
    This reverts commit c98fce3c3bb057bb9b75069a78f2a0bf197f228d, reversing
    changes made to 151d1aeb453c4ff95316c9982cffe855d177310a.

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,11 +1,5 @@
 def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        # Letzte i Elemente sind bereits sortiert
-        for j in range(0, n - i - 1):
-            # Wenn das aktuelle Element größer ist als das nächste, tausche sie
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    # TODO: Implement the bubble sort algorithm
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
