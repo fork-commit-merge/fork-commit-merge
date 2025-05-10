d60cedf70c0309b7aab97acafba5719509e9496d
GitHub Action <action@github.com>
2025-05-10 09:44:01 +0000

    Revert "Merge pull request #3128 from teejcoder/teejcoder"
    This reverts commit 06224496d4bd85eb3f92967719fdcccd80161814, reversing
    changes made to 8ec650f5fb502daefe81a2a4d0593376c31cb473.

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,15 +1,3 @@
 // JavaScript - Medium
 
 // Create functionality for a countdown timer that counts down from 10 seconds to zero using JavaScript
-
-var timeLeft = 9
-
-const timer = setInterval(() => {
-    if (timeLeft < 0){
-        clearInterval(timer);
-        document.getElementById('timer').innerHTML = "Time's up!"
-    } else {
-        document.getElementById('timer').innerHTML = `00:0${timeLeft}`
-    }
-    timeLeft -= 1
-}, 1000)
