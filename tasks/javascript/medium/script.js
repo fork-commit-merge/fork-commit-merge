2e42a1e67d1e130528ae7f96aa991be10b9664cb
GitHub Action <action@github.com>
2025-05-13 17:54:17 +0000

    Revert "Merge pull request #3138 from AppiumMal/fix-issue"
    This reverts commit a1a16d4c1f70c98b5e622e6506d36d1cf77d185e, reversing
    changes made to f504a3a0a934eb1e6cd066e4755075abf1904744.

--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,9 +1,3 @@
 // JavaScript - Medium
 
 // Create functionality for a countdown timer that counts down from 10 seconds to zero using JavaScript
-
-
-for(let timer =10;timer > 0 ;timer--)
-{
-console.log(timer)
-}
