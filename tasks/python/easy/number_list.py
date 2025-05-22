f01536be30c5c3418abf08e5145d999d1077e659
GitHub Action <action@github.com>
2025-05-22 10:19:02 +0000

    Revert "Merge pull request #3144 from Dhanalakshmi23s/easy"
    This reverts commit 114b59a6445c24aa7485619196173205f4d8c5cc, reversing
    changes made to 25c2ffc7904437f896a76caf96a203bbc3a4c4fc.

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -5,10 +5,5 @@
 # prints that list to the console and,
 # prints the sum of the numbers of that list
 
-def function():
-    numbers = list(range(1, 11))  # First 10 natural numbers
-    print("List of first 10 natural numbers:", numbers)
-    print("Sum of the numbers:", sum(numbers))
-
-# Call the function
-function()
+def function(numbers):
+    pass
--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,10 +1,3 @@
 # Python - Easy
 
 # TODO: Write a function that returns the sum of two numbers
-
-def add_numbers(a, b):
-    return a + b
-
-# Example usage:
-result = add_numbers(3, 5)
-print("Sum:", result)
