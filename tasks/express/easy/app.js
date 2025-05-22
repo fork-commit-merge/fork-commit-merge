f82c23119c2c83f44f4506636c662f74210b8d5b
Anegouni Karthik <karthikanegouni@gmail.com>
2025-05-22 17:36:56 +0530

    feat: implement simple Express.js server for #3019

--- tasks/express/easy/app.js
+++ tasks/express/easy/app.js
@@ -6,5 +6,9 @@ const app = express();
 const port = 6969;
 
 // TODO: Implement `get` method for / route
+app.get("/",(request,response)=>{
+    response.send("Hello, Express!")
+});
 
 // TODO: Implement `listen` method
+app.listen(6969,()=>console.log("Server is running on port 6969"));
