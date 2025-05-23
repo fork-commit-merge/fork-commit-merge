4f028c878c410795981fe8fc3e98b0e17171b96d
guidosti <guidososa2k17@gmail.com>
2025-05-23 15:43:35 -0300

    Tasks PHP completed

--- tasks/php/easy/array.php
+++ tasks/php/easy/array.php
@@ -5,3 +5,8 @@
 $data = ['Pizza', 'Chicken', 'Beef', 'Rice', 'Bread'];
 
 // TODO: Display data "Beef" from the array
+foreach ($data as $food) {
+	if (strcmp($food, 'Beef') == 0) {
+		print($food);
+	}
+}
