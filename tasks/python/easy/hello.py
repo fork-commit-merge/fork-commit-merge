f29242f415dbaf827639eec4ef0889fce95abd40
itsmekaved <kavin.mitblr2022@learner.manipal.edu>
2025-05-28 16:36:06 +0530

    Fixed Hello World using Flask

--- tasks/python/easy/hello.py
+++ tasks/python/easy/hello.py
@@ -1,3 +1,12 @@
 # Python - Easy
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
 # TODO: Write a function that prints "Hello, Python!" to the console
