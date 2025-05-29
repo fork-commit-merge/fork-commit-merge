7071fa5a7be6fee8c27a95ce37dd38b6f333c9ad
Niko Hoffrén <niko.hoffren@gmail.com>
2025-05-29 18:31:38 +0300

    reset files for next contributor

--- tasks/c++/hard/mystring.cpp
+++ tasks/c++/hard/mystring.cpp
@@ -1,94 +1,42 @@
-// C++ - Hard
-
-#include "mystring.h"
 #include <cstring>
 #include <iostream>
 
-MyString::MyString()
-{
-    data = new char[1];
-    data[0] = '\0';
+MyString::MyString(const char* s) {
+    // TODO: Implement this
 }
 
-MyString::MyString(const char *s)
-{
-    if (s == nullptr)
-    {
-        data = new char[1];
-        data[0] = '\0';
-    }
-    else
-    {
-        data = new char[strlen(s) + 1];
-        std::strcpy(data, s);
-    }
+MyString::MyString(const MyString& other) {
+    // TODO: Implement this
 }
 
-MyString::MyString(const MyString &other)
-{
-    data = new char[strlen(other.data) + 1];
-    std::strcpy(data, other.data);
+int MyString::length() const {
+    // TODO: Implement this
 }
 
-MyString::~MyString()
-{
-    delete[] data;
+const char* MyString::c_str() const {
+    // TODO: Implement this
 }
 
-int MyString::length() const
-{
-    return std::strlen(data);
+void MyString::append(const MyString& other) {
+    // TODO: Implement this
 }
 
-const char *MyString::c_str() const
-{
-    return data;
+int MyString::compare(const MyString& other) const {
+    // TODO: Implement this
 }
 
-void MyString::append(const MyString &other)
-{
-    int new_len = length() + other.length();
-    char *new_data = new char[new_len + 1];
-    std::strcpy(new_data, data);
-    std::strcat(new_data, other.data);
-    delete[] data;
-    data = new_data;
+MyString& MyString::operator=(const MyString& other) {
+    // TODO: Implement this
 }
 
-int MyString::compare(const MyString &other) const
-{
-    return std::strcmp(data, other.data);
+MyString MyString::operator+(const MyString& other) const {
+    // TODO: Implement this
 }
 
-MyString &MyString::operator=(const MyString &other)
-{
-    if (this != &other)
-    {
-        delete[] data;
-        data = new char[strlen(other.data) + 1];
-        std::strcpy(data, other.data);
-    }
-    return *this;
+bool MyString::operator==(const MyString& other) const {
+    // TODO: Implement this
 }
 
-MyString MyString::operator+(const MyString &other) const
-{
-    int new_len = length() + other.length();
-    char *buffer = new char[new_len + 1];
-    std::strcpy(buffer, data);
-    std::strcat(buffer, other.data);
-    MyString result(buffer);
-    delete[] buffer;
-    return result;
-}
-
-bool MyString::operator==(const MyString &other) const
-{
-    return std::strcmp(data, other.data) == 0;
-}
-
-std::ostream &operator<<(std::ostream &os, const MyString &s)
-{
-    os << s.data;
-    return os;
+std::ostream& operator<<(std::ostream& os, const MyString& s) {
+    // TODO: Implement this
 }
--- tasks/c++/hard/mystring.h
+++ tasks/c++/hard/mystring.h
@@ -1,30 +1,8 @@
 // C++ - Hard
 
-#ifndef MYSTRING_H
-#define MYSTRING_H
-
+define MYSTRING_H
 #include <iostream>
 
 class MyString {
-private:
-    char* data;
-
-public:
-    MyString();
-    MyString(const char* s);
-    MyString(const MyString& other);
-    ~MyString();
-
-    int length() const;
-    const char* c_str() const;
-    void append(const MyString& other);
-    int compare(const MyString& other) const;
-
-    MyString& operator=(const MyString& other);
-    MyString operator+(const MyString& other) const;
-    bool operator==(const MyString& other) const;
-
-    friend std::ostream& operator<<(std::ostream& os, const MyString& s);
-};
-
-#endif // MYSTRING_H
+    // TODO: Implement MyString class
+}
