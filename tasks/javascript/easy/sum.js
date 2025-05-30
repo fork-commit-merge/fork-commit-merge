ab3c408ad68c2c18897e1be685ba379c4deb2865
GitHub Action <action@github.com>
2025-05-30 14:41:21 +0000

    Revert "Merge pull request #3296 from Xavi1/main"
    This reverts commit 052bfce5376bf3412176df4e1a31a2f6098684d6, reversing
    changes made to 5d0a79a832c1bb9d99f4693885a02ef94f7f0cdc.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,7 +1,3 @@
-function sum(a, b) {
-  const result = a + b;
-  console.log("The sum of " + a + " and " + b + " is " + result + ".");
-  return result;
-}
+// JavaScript - Easy
 
-sum(1, 2);
+// TODO: Write a function that returns the sum of two numbers
