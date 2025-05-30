80a3e0f0464af1074fbdc52362fa7586a7ab6619
GitHub Action <action@github.com>
2025-05-30 17:56:35 +0000

    Revert "Merge pull request #3297 from OC0NER/greeting-app-feature"
    This reverts commit fcfbb5e67d719ed2589c6ed79527ca6a8c3b59b3, reversing
    changes made to ab3c408ad68c2c18897e1be685ba379c4deb2865.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -3,10 +3,6 @@
 public class GreetingApp {
 
     public static void main(String[] args) {
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-        System.out.print("Please enter your name: ");
-        String name = scanner.nextLine();
-        System.out.println("Hello, " + name + "Welcome to the Java world!");
-        scanner.close();
+
     }
 }
