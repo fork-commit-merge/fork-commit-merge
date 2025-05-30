b532c48a6b9d9c89349b3a617a08b6e451e11468
GitHub Action <action@github.com>
2025-05-30 18:43:44 +0000

    Revert "Merge pull request #3298 from OC0NER/main"
    This reverts commit 7cb3f24dbab931f67b3c54d5afb6010abb6aea52, reversing
    changes made to aebfaf1dd2978ac9ba9c807f78da904b7f63e9e6.

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -4,25 +4,6 @@ package tasks.java.medium;
 
 public class Calculator {
     // TODO: Implement the Calculator class
-    // Method to add two numbers
-    public int add(int a, int b) {
-        return a + b;
-    }
-    //Method to substract two numbers
-    public int subtract(int a, int b) {
-        return a - b;
-    }
-    //Method to multipply two numbers
-    public int multiply(int a, int b) {
-        return a * b;
-    }
-    //Method to divide two numbers
-    public double divide(int a, int b) {
-        if (b == 0) {
-            throw new IllegalArgumentException("Division by zero is not allowed.");
-        }
-        return (double) a / b;
-    }
 
     public static void main(String[] args) {
         //! Don't change the tests in the main method!
