76febff50cbc29550e6702570860c3bd2631e16a
VerneZhong <verne.zhong@gmail.com>
2025-05-31 11:46:14 +0900

    feat: add frequency counter using Map #3200

--- tasks/java/hard/Frequency.java
+++ tasks/java/hard/Frequency.java
@@ -4,6 +4,22 @@ import java.util.Map;
 public class Frequency {
 
     public static void main(String[] args) {
-        // TODO: Given an array of integers, write a Java program to find the frequency of each element using a Map
+        // Given an array of integers
+        int[] nums = {1, 2, 2, 3, 3, 3, 4};
+
+        // Create a Map to store the frequency of each element
+        Map<Integer, Integer> freqMap = new HashMap<>();
+
+        // Iterate through the array and count the frequency of each number
+        for (int num : nums) {
+            // If the number already exists in the map, increment its count
+            // Otherwise, initialize its count to 1
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        // Iterate through the map entries and print the frequency of each element
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
     }
 }
