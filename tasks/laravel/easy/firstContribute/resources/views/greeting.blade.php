519baeb1e8325c6c9f2e5e23b3efca7cf85aa7e8
elmaaref <abdelrahimabuelmaaref@gmail.com>
2025-06-01 18:15:47 +0300

    Add Laravel project files with greeting route

---
+++ tasks/laravel/easy/firstContribute/resources/views/greeting.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Greeting</title>
+</head>
+<body>
+    <h1>Welcome, Contributor!</h1>
+</body>
+</html>
---
+++ tasks/laravel/easy/firstContribute/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/greet', function () {
+    return view('greeting'); 
+});
