46d6401f7d82b7982ebd5ca762296867e3d2a570
GitHub Action <action@github.com>
2025-06-01 09:06:06 +0000

    Revert "Merge pull request #3437 from kroej/master"
    This reverts commit d2e04d7685502fef780555bba7d6e837650d2a4e, reversing
    changes made to 75962a5825e44ec67fa92090dd1277ae21d44da4.

--- tasks/c++/easy/sum.cpp
+++ tasks/c++/easy/sum.cpp
@@ -1,15 +1,8 @@
 // C++ - Easy 2
 
 // TODO: Create a Basic C++ Program to Add Two Numbers
-#include<iostream>
-using namespace std;
-int f(int a=0,int b=0){
-	return a+b;
-}
+
 int main()
 {
-  int a,b;
-  cin>>a>>b;
-  cout<<f(a,b);
-  return 0; 
+  
 }
