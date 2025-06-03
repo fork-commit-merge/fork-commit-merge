9d084528fe9d40c12653d4c4485a5f48878da9d1
maduranga20 <madurangadesign22@gmail.com>
2025-06-03 20:39:52 +0530

    setCounter state

--- tasks/solid/easy/src/App.jsx
+++ tasks/solid/easy/src/App.jsx
@@ -1,18 +1,23 @@
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+
+  const [count,setCount]=useState(0);
+
   return (
     <main class={styles.counterContainer}>
       {/* TODO: Implement the counter state with the name of "count" and a setter named "setCount"  */}
 
       {/* TODO: Display the current count state in here */}
-      <p class={styles.countNumber}></p>
+      <p class={styles.countNumber}>{count}</p>
 
       {/* TODO: Implement the setter function for incrementing and decrementing the count state. */}
       <div class={styles.countButtonsContainer}>
         <button
           onClick={() => {
             // Implement the function to increment the counter state here.
+            setCount(count+1)
           }}
           class={styles.countButtons}
         >
@@ -21,6 +26,8 @@ function App() {
         <button
           onClick={() => {
             // Implement the function to decrement the counter state here.
+                    setCount(count-1)
+    
           }}
           class={styles.countButtons}
         >
