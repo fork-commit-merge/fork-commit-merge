56f3aa6bc37062a305aef8931aa291479329a732
Jake <87922888+JakeOJeff@users.noreply.github.com>
2025-06-06 13:42:43 +0530

    Set the input stream and output stream for converter

--- tasks/lua/easy/temperature_converter.lua
+++ tasks/lua/easy/temperature_converter.lua
@@ -8,4 +8,28 @@ local function fahrenheitToCelsius(fahrenheit)
     return (fahrenheit - 32) * 5/9
 end
 
--- TODO: Implement rest of the Temperature Converter
+local function opt(n)
+    if n == 1 then return "Fahrenheit" else return "Celsius" end
+end
+
+local function func(n, t)
+       if n == 1 then return fahrenheitToCelsius(t) else return celsiusToFahrenheit(t) end
+end
+
+print("Choose type of conversion: (1) Fahrenheit to Celsius or (2) Celsius to Fahrenheit")
+option = io.read("*n")
+temp = 0
+invalid = "Invalid input. Please enter a numeric value."
+
+if (type(option) == "number") then
+    print("Enter temperature in "..opt(option)..":")
+    temp = io.read("*n")
+    if type(temp) == "number" then
+        print("The temperature is "..(func(option, temp)).." degrees "..opt(option) )
+    else
+        print(invalid)
+    end
+else
+    print(invalid)
+end
+    
