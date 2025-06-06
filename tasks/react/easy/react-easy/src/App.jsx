ce2f20565a3d5ff57ace0a8b18e7b26b70fda04c
GitHub Action <action@github.com>
2025-06-06 15:17:46 +0000

    Revert "Merge pull request #3450 from maduranga20/reactcard"
    This reverts commit 2bde7fd61bb58971670c27766e3812670bb5774b, reversing
    changes made to 77ed2b67d71c97272118bd76e9f846bbb420140b.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -4,14 +4,7 @@ import "./index.css";
 function App() {
   return (
     <div className="center">
-      <h1>React/Vite - Easy</h1>
-      <div className="card">
-        <img src="https://loremflickr.com/320/240" alt="card Image" />
-        <h2>Card Title</h2>
-        <p>Card Description</p>
-      </div>
-     
-
+      {/* TODO: Implement the card component */}
     </div>
   );
 }
--- tasks/solid/easy/src/App.jsx
+++ tasks/solid/easy/src/App.jsx
@@ -1,39 +1,31 @@
 import styles from "./App.module.css";
-import  { useState } from 'react';
 
 function App() {
-
-  const [count,setCount]=useState(0);
-
   return (
-    <main className={styles.counterContainer}>
+    <main class={styles.counterContainer}>
       {/* TODO: Implement the counter state with the name of "count" and a setter named "setCount"  */}
 
       {/* TODO: Display the current count state in here */}
-      <p className={styles.countNumber}>{count}</p>
+      <p class={styles.countNumber}></p>
 
       {/* TODO: Implement the setter function for incrementing and decrementing the count state. */}
-      <div className={styles.countButtonsContainer}>
+      <div class={styles.countButtonsContainer}>
         <button
           onClick={() => {
             // Implement the function to increment the counter state here.
-            setCount(count+1)
           }}
-          className={styles.countButtons}
+          class={styles.countButtons}
         >
           +1
         </button>
         <button
           onClick={() => {
             // Implement the function to decrement the counter state here.
-          setCount(count-1)
-    
           }}
-          className={styles.countButtons}
+          class={styles.countButtons}
         >
           -1
         </button>
-     
       </div>
     </main>
   );
