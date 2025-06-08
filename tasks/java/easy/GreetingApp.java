bc5e960648643812b6fa7368758c8daa8539cfb9
GitHub Action <action@github.com>
2025-06-08 14:48:56 +0000

    Revert "Merge pull request #3453 from xankush/main"
    This reverts commit d3ea1cd24d5c14f2f1bb94284ce5a92d6598dbad, reversing
    changes made to a4349d9c0d581963b8fe218b4ee9cfa7aa826b2a.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,13 +1,8 @@
 // Java - Easy
 
-import java.util.Scanner;
-
 public class GreetingApp {
 
     public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your name:");
-        String name = sc.nextLine();
-        System.out.print("Hello, "+name+"! Nice to meet you!");
+
     }
 }
