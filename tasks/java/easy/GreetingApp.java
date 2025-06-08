79eae397c283fa64c68c2821353a1c910c49ded9
xankush <ankushdohare12@gmail.com>
2025-06-08 15:45:24 +0530

    fixed issue

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,8 +1,13 @@
 // Java - Easy
 
+import java.util.Scanner;
+
 public class GreetingApp {
 
     public static void main(String[] args) {
-
+         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name:");
+        String name = sc.nextLine();
+        System.out.print("Hello, "+name+"! Nice to meet you!");
     }
 }
