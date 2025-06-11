fb1d3785b47e15855fa3d85395ef4c1ea5e7d358
GitHub Action <action@github.com>
2025-06-11 07:49:31 +0000

    Revert "Merge pull request #3457 from LeMinhMan2809/fix-issue"
    This reverts commit e9372ad8e608576c7bbdce823bec92bf0700873b, reversing
    changes made to 22b3782c2ea6aedd77bcbb08abb528e44e775b1e.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -4,12 +4,7 @@ import "./index.css";
 function App() {
   return (
     <div className="center">
-      <h1>React/Vite - Easy</h1>
-      <div className="card">
-        <h2 style={{textAlign: "center"}}>Card Title</h2>
-        <img src="https://loremflickr.com/320/240" />
-        <p>Card description. This is some text to explain more about the card</p>
-      </div>
+      {/* TODO: Implement the card component */}
     </div>
   );
 }
