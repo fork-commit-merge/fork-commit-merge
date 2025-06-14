29779f5d6b6bf2395fcb12048ea60ff42509f57c
GitHub Action <action@github.com>
2025-06-14 07:20:00 +0000

    Revert "Merge pull request #3468 from Xavi1/main"
    This reverts commit feb63d59462da0d81094e239ef7fd2693dae4ed6, reversing
    changes made to 9d8b68cc5b68d0bd728e53da3c6240422d0c628a.

--- tasks/react/medium/traffic-light/src/TrafficLight.tsx
+++ tasks/react/medium/traffic-light/src/TrafficLight.tsx
@@ -1,29 +1,11 @@
-import React, { useEffect, useState } from "react";
+// React.js - Medium 2
 import "./App.css";
 
-type Light = "red" | "green" | "yellow";
+// TODO: Implement  the TrafficLight component
 
-const sequence: { light: Light; duration: number }[] = [
-  { light: "red", duration: 4000 },
-  { light: "green", duration: 3000 },
-  { light: "yellow", duration: 1000 },
-];
-
-export const TrafficLight: React.FC = () => {
-  const [index, setIndex] = useState(0);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIndex((prev) => (prev + 1) % sequence.length);
-    }, sequence[index].duration);
-    return () => clearTimeout(timer);
-  }, [index]);
+export function TrafficLight() {
 
   return (
-    <div className="traffic-light">
-      <div className={`light red${sequence[index].light === "red" ? " active" : ""}`}></div>
-      <div className={`light yellow${sequence[index].light === "yellow" ? " active" : ""}`}></div>
-      <div className={`light green${sequence[index].light === "green" ? " active" : ""}`}></div>
-    </div>
+    // TODO: return JSX here
   );
-};
+}
