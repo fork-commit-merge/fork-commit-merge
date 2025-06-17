8eb996b8143e0e1dc0dc77e9943334f115398314
Xavi <xavierkas09@gmail.com>
2025-06-17 10:59:56 +0800

    Refactor SQL comments for clarity and consistency

--- tasks/sql/medium/Select(select).sql
+++ tasks/sql/medium/Select(select).sql
@@ -1,11 +1,9 @@
 -- SQL - Medium
 
--- # Which country has a population that is more than Canada but less than Poland? Show the name and the population.
--- # use SELECT within SELECT.
+-- Which country has a population that is more than Canada but less than Poland? Show the name and the population.
+-- Use SELECT within SELECT.
 
--- sample table :
-
--- World (Name,Continent,Area,Population,GDP);
-
--- |   Name       |  Continent  |     Area    |  Population  |     GDP     |
--- |--------------|-------------|-------------|--------------|-------------|
+SELECT Name, Population
+FROM World
+WHERE Population > (SELECT Population FROM World WHERE Name = 'Canada')
+  AND Population < (SELECT Population FROM World WHERE Name = 'Poland');
