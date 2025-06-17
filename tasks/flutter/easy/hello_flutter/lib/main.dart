b160060c158de57fe64c5bdff3dc3c57ad8a5649
GitHub Action <action@github.com>
2025-06-17 09:12:21 +0000

    Revert "Merge pull request #3476 from jyk0619/main"
    This reverts commit 4dd5d2d3ae4d69eb50466e573483fce167e236c3, reversing
    changes made to 0d0c127f86b4e2ae4753255b0173f246b7b406bf.

--- tasks/flutter/easy/hello_flutter/lib/main.dart
+++ tasks/flutter/easy/hello_flutter/lib/main.dart
@@ -13,11 +13,6 @@ class ForkCommitMerge extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Fork, Commit, Merge'),
-
-        ),
-          body: Text('Hello Flutter!'),
           // TODO: Implement the appbar and body properties
       ),
     );
