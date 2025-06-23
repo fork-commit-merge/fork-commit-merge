11c125533fe49b08b2b762de6749bcce8367386e
GitHub Action <action@github.com>
2025-06-23 15:57:08 +0000

    Revert "Merge pull request #3616 from mnmfullmetal/fix-issue"
    This reverts commit a6171143ef117ae50b9899d896dcbe52ccab51e9, reversing
    changes made to d25cd18736241b92cdbf18f4d6abd517f68d945e.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,36 +1,3 @@
 #include <stdio.h>
 
 // TODO: Implement the sum and main functions
-int sum(int a, int b);
-
-int main() 
-{
-    int num1, num2;
-
-    printf("Enter two numbers: ");
-
-    if (scanf("%d %d", &num1, &num2) != 2) 
-    {
-        printf("Error: Invalid input. Please enter two integers.\n");
-        return 1; 
-    }
-
-    int result = sum(num1, num2);
-
-    printf("The sum is: %d\n", result);
-
-    return 0;
-}
-
-int sum(int a, int b)
-{
-    while (b != 0) 
-    {
-        unsigned int carry = (unsigned int)(a & b) << 1;
-
-        a = a ^ b;
-        
-        b = carry;
-    }
-    return a;
-}
