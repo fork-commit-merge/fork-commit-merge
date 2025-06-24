f24d7c79d621de511988dea115d7f9760c8ceaae
Soumik Saha <sahasoumik1573@gmail.com>
2025-06-24 10:18:04 +0530

    Fixed issue

--- tasks/express/easy/app.js
+++ tasks/express/easy/app.js
@@ -6,5 +6,11 @@ const app = express();
 const port = 6969;
 
 // TODO: Implement `get` method for / route
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
 // TODO: Implement `listen` method
+app.listen(port, () => {
+    console.log('Server is running on Port:', port);
+});
--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,3 +1,7 @@
 // JavaScript - Easy
 
 // TODO: Write a function that returns the sum of two numbers
+
+function sum(a, b) {
+    return a + b;
+}
