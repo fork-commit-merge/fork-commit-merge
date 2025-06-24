085bfec62dbc0204fd2e4cdce41ee81fbd4e07a0
GitHub Action <action@github.com>
2025-06-24 05:33:05 +0000

    Revert "Merge pull request #3624 from soumik-saha/fix-issue"
    This reverts commit b5a1ef35dde81ef707640d9ea48bd0655698cc84, reversing
    changes made to 43503c8fb3e67c4519f973ae79421f53bf661abc.

--- tasks/express/easy/app.js
+++ tasks/express/easy/app.js
@@ -6,11 +6,5 @@ const app = express();
 const port = 6969;
 
 // TODO: Implement `get` method for / route
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
 
 // TODO: Implement `listen` method
-app.listen(port, () => {
-    console.log('Server is running on Port:', port);
-});
--- tasks/javascript/medium/script.js
+++ tasks/javascript/medium/script.js
@@ -1,17 +1,3 @@
 // JavaScript - Medium
 
 // Create functionality for a countdown timer that counts down from 10 seconds to zero using JavaScript
-
-function startCountdown() {
-    let timeLeft = 10;
-    const countdownElement = document.getElementById('countdown');
-    countdownElement.textContent = timeLeft;
-    const countdownInterval = setInterval(() => {
-        timeLeft--;
-        countdownElement.textContent = timeLeft;
-        if (timeLeft <= 0) {
-            clearInterval(countdownInterval);
-            countdownElement.textContent = "Time's up!";
-        }
-    }, 1000);
-}
