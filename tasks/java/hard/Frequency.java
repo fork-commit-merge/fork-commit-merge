24db2984925d9e9b1ef944e83b67870a372107a1
GitHub Action <action@github.com>
2025-06-25 14:42:54 +0000

    Revert "Merge pull request #3628 from shivt-F5/java-branch"
    This reverts commit 7e6333c9d892f8dd4520837440b5958f27da08e1, reversing
    changes made to 82bdda760a0c2c5f91b55baca63cd78e58fda4ce.

---
+++ tasks/java/hard/Frequency.java
@@ -0,0 +1,9 @@
+// Java - Hard
+import java.util.HashMap;
+import java.util.Map;
+public class Frequency {
+
+    public static void main(String[] args) {
+        // TODO: Given an array of integers, write a Java program to find the frequency of each element using a Map
+    }
+}
