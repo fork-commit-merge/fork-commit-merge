fb776c47b9eb798668e2d63294ed63949fd81e2f
Emre Can <emrecan19oo7@gmail.com>
2025-06-27 15:39:50 +0300

    Fixes #3533: Add number frequency counter with HashMap

--- tasks/java/hard/Frequency.java
+++ tasks/java/hard/Frequency.java
@@ -4,6 +4,24 @@ import java.util.Map;
 public class Frequency {
 
     public static void main(String[] args) {
-        // TODO: Given an array of integers, write a Java program to find the frequency of each element using a Map
+        
+    	int[] numbers= {1,2,2,3,3,4,4,4,5,6};
+    	
+    	Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
+    	
+    	for (int number : numbers) {
+			if(frequencyMap.containsKey(number)) {
+				frequencyMap.put(number, frequencyMap.get(number)+1);
+			}
+			else
+			{
+				frequencyMap.put(number, 1);
+			}
+		}
+
+    	frequencyMap.forEach((key, value) -> 
+        System.out.println(key + "	-->	" + value + " times")
+    	);
+    	
     }
 }
