3a40e8d69cd9a5acf74ddbcf57c0c3d2f8607745
GitHub Action <action@github.com>
2025-06-27 19:59:23 +0000

    Revert "Merge pull request #3633 from emrecan15/issue-3507-fix"
    This reverts commit c15b2ae8f96f9e38041e198c92713db69ddf029f, reversing
    changes made to a46ba8e319b30b8031741a9f8ee48eb6092d4b84.

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -3,33 +3,11 @@
 package tasks.java.medium;
 
 public class Calculator {
-    
-	public double add(double i,double k)
-	{
-		double addition = i+k;
-		return addition;
-	}
-	
-	public double subtract(double i,double k)
-	{
-		double subtraction = i-k;
-		return subtraction;
-	}
-	public double multiply(double i,double k)
-	{
-		double multiplication = i*k;
-		return multiplication;
-	}
-	public double divide(double i,double k)
-	{
-		double division = i/k;
-		return division;
-	}
+    // TODO: Implement the Calculator class
 
     public static void main(String[] args) {
         //! Don't change the tests in the main method!
         Calculator calc = new Calculator();
-        
 
         System.out.println("Addition: " + calc.add(5, 3));
         System.out.println("Subtraction: " + calc.subtract(5, 3));
