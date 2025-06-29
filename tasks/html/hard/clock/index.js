1720d5680a063b9ed8f778d8c564ef6862dd8079
GitHub Action <action@github.com>
2025-06-29 17:53:08 +0000

    Revert "Merge pull request #3647 from shivt-F5/issue-3525"
    This reverts commit f7e07077659f68bd8564d309e25b7ec64a81e349, reversing
    changes made to d897e0aa7c60fc040e4140947bade8ff22b70891.

--- tasks/html/hard/clock/index.js
+++ tasks/html/hard/clock/index.js
@@ -1,25 +1,14 @@
-function setRotation(element, rotationRatio) {
-  element.style.setProperty('--rotation', rotationRatio * 360);
-}
+// Implement the setClock function to update the positions of the hour, minute, and second hands on the clock based on the current time.
+setInterval(() => {
+  // TODO: Calculate the current time and update the clock hands' positions here.
+  // You need to calculate the rotation ratios and update the hand rotations.
+}, 1000);
 
-function setClock() {
-  const hourHand = document.querySelector("[data-hour-hand]");
-  const minuteHand = document.querySelector("[data-minute-hand]");
-  const secondHand = document.querySelector("[data-second-hand]");
+const hourHand = document.querySelector("[data-hour-hand]");
+const minuteHand = document.querySelector("[data-minute-hand]");
+const secondHand = document.querySelector("[data-second-hand]");
 
-  const now = new Date();
-  const seconds = now.getSeconds();
-  const minutes = now.getMinutes();
-  const hours = now.getHours();
+// Create setRotation function to rotate a DOM element around its center point. This function will be used to rotate the hour, minute, and second hands on the clock.
+// TODO: Implement the setRotation function to update the rotation of a DOM element based on the provided rotationRatio.
 
-  const secondRotation = seconds / 60;
-  const minuteRotation = (minutes + secondRotation) / 60;
-  const hourRotation = (hours % 12 + minuteRotation) / 12;
-
-  setRotation(hourHand, hourRotation);
-  setRotation(minuteHand, minuteRotation);
-  setRotation(secondHand, secondRotation);
-}
-
-setInterval(setClock, 1000);
-setClock();
+// TODO: You can call the setClock function here to initialize the clock's position when the page loads.
