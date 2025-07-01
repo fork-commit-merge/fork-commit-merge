c8ec018a5b719d09a2b2789444fe17c2ec73ec0a
GitHub Action <action@github.com>
2025-07-01 14:08:55 +0000

    Revert "Merge pull request #3794 from shivt-F5/c-branch"
    This reverts commit 73c7c3567afa33bf1c98af65f1c9d2d0dc806258, reversing
    changes made to 846a726e04f50454ab37fa155040750fb1dc63af.

--- tasks/c/medium/read_csv.c
+++ tasks/c/medium/read_csv.c
@@ -4,34 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUFFER_SIZE 512
 
 int main() {
-FILE *fp = fopen("data.csv", "r");
-    if (!fp) {
-        printf("Could not open file.\n");
-        return 1;
-    }
-
-    char buffer[BUFFER_SIZE];
-    if (fgets(buffer, BUFFER_SIZE, fp)) {
-        buffer[strcspn(buffer, "\r\n")] = 0;
-        printf("Header: %s\n", buffer);
-    }
-
-    while (fgets(buffer, BUFFER_SIZE, fp)) {
-        buffer[strcspn(buffer, "\r\n")] = 0;
-        printf("\nReading line: %s\n", buffer);
-
-        char *name = strtok(buffer, ",");
-        char *age = strtok(NULL, ",");
-
-        if (name && age) {
-            printf("Name: %s\n", name);
-            printf("Age: %s\n", age);
-        }
-    }
-
-    fclose(fp);
-    return 0;
+//    TODO: Implement the program requested in the task description
 }
