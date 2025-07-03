407c82e6e082423ae131abfd238c8da762b876d7
Vi <support@b4dcat.ru>
2025-07-04 00:20:37 +0300

    feat: implement JavaScript function to add two numbers. Closes #3753

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,3 +1,11 @@
 // JavaScript - Easy
 
 // TODO: Write a function that returns the sum of two numbers
+function addNumbers(a, b) {
+    return a + b;
+}
+
+// Test the function
+console.log(addNumbers(5, 3)); // Should output: 8
+console.log(addNumbers(-2, 7)); // Should output: 5
+console.log(addNumbers(0, 0)); // Should output: 0
