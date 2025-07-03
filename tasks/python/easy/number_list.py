fbac1d43f52eb80dddac284e11eb48f897433c09
GitHub Action <action@github.com>
2025-07-03 03:47:17 +0000

    Revert "Merge pull request #3805 from mrvi0/python-easy-number-list"
    This reverts commit d23e4bc98585f5a130dd3f1b11436bbda3cfd328, reversing
    changes made to 36d7680f3c07aa8bdf6cac341a2a740f72d6c364.

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -6,21 +6,4 @@
 # prints the sum of the numbers of that list
 
 def function(numbers):
-    """
-    Function that prints the list of numbers and their sum
-    
-    Args:
-        numbers: list of numbers to process
-    """
-    print("List of numbers:", numbers)
-    total_sum = sum(numbers)
-    print("Sum of numbers:", total_sum)
-    return total_sum
-
-# Create list of first 10 natural numbers (1 to 10)
-natural_numbers = list(range(1, 11))
-
-# Test the function
-if __name__ == "__main__":
-    print("First 10 natural numbers:")
-    result = function(natural_numbers)
+    pass
