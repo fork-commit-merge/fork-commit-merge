8cf819c7da6bc126b29c47b7a601d9d38f43b0b5
GitHub Action <action@github.com>
2025-07-03 03:52:51 +0000

    Revert "Merge pull request #3808 from mrvi0/bash-easy-create-files"
    This reverts commit 05513348b84d136c6492342f8c997e9e69c0440a, reversing
    changes made to 7806ff5c98c47099c713d453c619c7461a223a83.

--- tasks/bash/easy/create_files.sh
+++ tasks/bash/easy/create_files.sh
@@ -2,7 +2,4 @@
 
 # Bash - Easy
 
-# Script to create three files: file1.txt, file2.txt, file3.txt
-
-touch file1.txt file2.txt file3.txt
-echo "Files file1.txt, file2.txt, and file3.txt have been created."
+# TODO: Write the Bash script requested in the issue description
