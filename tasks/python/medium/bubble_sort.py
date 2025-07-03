88627797e4e8ff104911db0dc28643a4ecd2a372
GitHub Action <action@github.com>
2025-07-03 03:48:23 +0000

    Revert "Merge pull request #3806 from mrvi0/python-medium-bubble-sort"
    This reverts commit d147d60b2473bc2c3ca3427e96762bb7043d6d32, reversing
    changes made to fbac1d43f52eb80dddac284e11eb48f897433c09.

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,29 +1,5 @@
 def bubble_sort(arr):
-    """
-    Implementation of bubble sort algorithm
-    Sorts the array in ascending order in-place
-    
-    Args:
-        arr: list to be sorted
-    """
-    n = len(arr)
-    
-    # Traverse through all array elements
-    for i in range(n):
-        # Flag to optimize - if no swapping occurs, array is sorted
-        swapped = False
-        
-        # Last i elements are already in place
-        for j in range(0, n - i - 1):
-            # Compare adjacent elements
-            if arr[j] > arr[j + 1]:
-                # Swap if they are in wrong order
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                swapped = True
-        
-        # If no swapping occurred, array is sorted
-        if not swapped:
-            break
+    # TODO: Implement the bubble sort algorithm
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
