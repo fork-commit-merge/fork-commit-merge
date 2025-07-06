9806723d45c96e96eb688663395ef388c08e0b56
GitHub Action <action@github.com>
2025-07-06 04:46:51 +0000

    Revert "Merge pull request #3838 from gakukinn/patch-1"
    This reverts commit 3930a0d137eeb427b1ed6c27babe070af4eeeb43, reversing
    changes made to 90825436db659d339fab63cac1465672b037a882.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,6 +1,3 @@
 // JavaScript - Easy
-function sum(a, b) {
-    return a + b;
-}
 
-console.log(`The sum of 1 and 2 is ${sum(1, 2)}.`);
+// TODO: Write a function that returns the sum of two numbers
