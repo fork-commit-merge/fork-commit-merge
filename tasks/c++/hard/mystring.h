ecf8002b1c95155ac6459823293d8f44c21521bd
shivani <54018320+shivt-F5@users.noreply.github.com>
2025-07-12 17:44:08 +0400

    c++ hard

--- tasks/c++/hard/mystring.h
+++ tasks/c++/hard/mystring.h
@@ -1,8 +1,23 @@
-// C++ - Hard
-
-define MYSTRING_H
+#ifndef MYSTRING_H
+#define MYSTRING_H
 #include <iostream>
 
 class MyString {
-    // TODO: Implement MyString class
-}
+private:
+    char* str;
+public:
+    MyString();
+    MyString(const char* s);
+    MyString(const MyString& other);
+    ~MyString();
+    int length() const;
+    const char* c_str() const;
+    void append(const MyString& other);
+    int compare(const MyString& other) const;
+    MyString& operator=(const MyString& other);
+    MyString operator+(const MyString& other) const;
+    bool operator==(const MyString& other) const;
+    friend std::ostream& operator<<(std::ostream& os, const MyString& s);
+};
+
+#endif
