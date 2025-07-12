23c4250c795cec341fcf11e570b3c7ab9b0ddac9
GitHub Action <action@github.com>
2025-07-12 13:54:32 +0000

    Revert "Merge pull request #3848 from shivt-F5/july12"
    This reverts commit fe9ec0786b5e89931e0d966a2b2c2f550d4e6907, reversing
    changes made to 87fbd97e8bbfef673af199e3c0bf46f3e867ac14.

--- tasks/c++/hard/mystring.cpp
+++ tasks/c++/hard/mystring.cpp
@@ -1,85 +1,42 @@
 #include <cstring>
 #include <iostream>
-#include "mystring.h"
-
-MyString::MyString() : str(nullptr) {}
 
 MyString::MyString(const char* s) {
-    if (s) {
-        str = new char[strlen(s) + 1];
-        strcpy(str, s);
-    } else {
-        str = nullptr;
-    }
+    // TODO: Implement this
 }
 
 MyString::MyString(const MyString& other) {
-    if (other.str) {
-        str = new char[strlen(other.str) + 1];
-        strcpy(str, other.str);
-    } else {
-        str = nullptr;
-    }
-}
-
-MyString::~MyString() {
-    delete[] str;
+    // TODO: Implement this
 }
 
 int MyString::length() const {
-    return str ? strlen(str) : 0;
+    // TODO: Implement this
 }
 
 const char* MyString::c_str() const {
-    return str ? str : "";
+    // TODO: Implement this
 }
 
 void MyString::append(const MyString& other) {
-    if (other.str) {
-        int newLength = length() + strlen(other.str) + 1;
-        char* newStr = new char[newLength];
-        strcpy(newStr, str ? str : "");
-        strcat(newStr, other.str);
-        delete[] str;
-        str = newStr;
-    }
+    // TODO: Implement this
 }
 
 int MyString::compare(const MyString& other) const {
-    if (!str && !other.str) return 0;
-    if (!str) return -1;
-    if (!other.str) return 1;
-    return strcmp(str, other.str);
+    // TODO: Implement this
 }
 
 MyString& MyString::operator=(const MyString& other) {
-    if (this != &other) {
-        delete[] str;
-        if (other.str) {
-            str = new char[strlen(other.str) + 1];
-            strcpy(str, other.str);
-        } else {
-            str = nullptr;
-        }
-    }
-    return *this;
+    // TODO: Implement this
 }
 
 MyString MyString::operator+(const MyString& other) const {
-    int newLength = length() + other.length() + 1;
-    char* newStr = new char[newLength];
-    strcpy(newStr, str ? str : "");
-    strcat(newStr, other.str ? other.str : "");
-    MyString temp(newStr);
-    delete[] newStr;
-    return temp;
+    // TODO: Implement this
 }
 
 bool MyString::operator==(const MyString& other) const {
-    return compare(other) == 0;
+    // TODO: Implement this
 }
 
 std::ostream& operator<<(std::ostream& os, const MyString& s) {
-    os << (s.str ? s.str : "");
-    return os;
+    // TODO: Implement this
 }
--- tasks/c++/hard/mystring.h
+++ tasks/c++/hard/mystring.h
@@ -1,23 +1,8 @@
-#ifndef MYSTRING_H
-#define MYSTRING_H
+// C++ - Hard
+
+define MYSTRING_H
 #include <iostream>
 
 class MyString {
-private:
-    char* str;
-public:
-    MyString();
-    MyString(const char* s);
-    MyString(const MyString& other);
-    ~MyString();
-    int length() const;
-    const char* c_str() const;
-    void append(const MyString& other);
-    int compare(const MyString& other) const;
-    MyString& operator=(const MyString& other);
-    MyString operator+(const MyString& other) const;
-    bool operator==(const MyString& other) const;
-    friend std::ostream& operator<<(std::ostream& os, const MyString& s);
-};
-
-#endif
+    // TODO: Implement MyString class
+}
