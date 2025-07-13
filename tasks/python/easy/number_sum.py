6f38a6e00f96dbda32ee51fbcb2c3f9d21608cb0
Harsh Sharma <77875433+BeastHarsh047@users.noreply.github.com>
2025-07-14 00:49:54 +0530

    Delete tasks/python/easy/number_sum.py

--- tasks/python/easy/number_sum.py
+++
@@ -1,9 +0,0 @@
-# Python - Easy
-
-# TODO: Write a function that returns the sum of two numbers
-def addnumbers():
- a = int(input("Enter the First Number: "))
- b = int(input("Enter the Second Number: "))
- total=a+b
- print(("Sum of Given Numbers is:"),total)
-addnumbers()
