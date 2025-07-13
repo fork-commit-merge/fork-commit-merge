fcda0485d047e4f6a8bfa6fd801c94a7140a7012
GitHub Action <action@github.com>
2025-07-13 15:48:16 +0000

    Revert "Merge pull request #3987 from BeastHarsh047/fixsecondissue"
    This reverts commit 1fb882770f0680c8a1b533bd00484ddf60ddfee4, reversing
    changes made to 7f3156745deb54f997a2e1fa50d3bc664c881ee9.

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -4,7 +4,6 @@
 # lists the first 10 natural numbers,
 # prints that list to the console and,
 # prints the sum of the numbers of that list
-i=1
-while(i<11):
- print(i)
- i +=1
+
+def function(numbers):
+    pass
