60bc06849b899894d159596f12f1c9e5522befd6
GitHub Action <action@github.com>
2025-07-14 05:28:16 +0000

    Revert "Merge pull request #3993 from nnoonnnn/feature/react-card-task"
    This reverts commit 69361d3314848e200306eda20b2bcce343ac6a85, reversing
    changes made to 4253e7cc0cbd2d64a3d3cf7cfa657e8a2536f369.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -3,14 +3,9 @@ import "./index.css";
 
 function App() {
   return (
-    <>
-   <h1>React/Vite - Easy</h1>
-<div className="card">
-  <img src="https://loremflickr.com/320/240" alt="Random" />
-  <h2>Card Title</h2>
-  <p>Card Description</p>
-</div>
-</>
+    <div className="center">
+      {/* TODO: Implement the card component */}
+    </div>
   );
 }
 
