7a8211678f99d3d0ac70ac86b88e3df7c1c84945
GitHub Action <action@github.com>
2025-07-14 05:27:23 +0000

    Revert "Merge pull request #3992 from nnoonnnn/feature/sum-task"
    This reverts commit 214e8192ce2f6ce9207aa8d015b7e2a13406093b, reversing
    changes made to ee7799d3297f6556b2a1252dfe7b423513fb829b.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,11 +1,3 @@
 // JavaScript - Easy
 
 // TODO: Write a function that returns the sum of two numbers
-/**
- * Returns the sum of two numbers a and b.
- */
-function sum(a, b) {
-  return a + b;
-}
-
-module.exports = sum;
