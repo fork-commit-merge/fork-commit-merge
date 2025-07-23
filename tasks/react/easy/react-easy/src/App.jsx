18d86400f3ca2ae5296803fdb2c3e60f2c086581
GitHub Action <action@github.com>
2025-07-23 15:28:53 +0000

    Revert "Merge pull request #4009 from om272004/feat/create-react-card"
    This reverts commit 00c926015388c49f70fa2250d9f1814d91c92e47, reversing
    changes made to 84b45d166f06129280a248b45c3ba26a6202444b.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="center">
       {/* TODO: Implement the card component */}
-      <h1>React/Vite - Easy</h1>
-      <Card />
     </div>
   );
-}
-
-function Card() {
-  return <div>
-    <img src="https://picsum.photos/320/240" alt="A random image" />
-    <h2>Card title</h2>
-    <p>Card Description</p>
-  </div>
 }
 
 export default App;
