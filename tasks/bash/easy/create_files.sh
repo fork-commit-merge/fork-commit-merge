ac6469168c4d486031ca8c55aaf03e7d8b4731a2
GitHub Action <action@github.com>
2025-07-23 15:30:00 +0000

    Revert "Merge pull request #4010 from shivt-F5/july23"
    This reverts commit 760594e438765416906045c7c5abed26091eea9e, reversing
    changes made to 58678d47689f68f7358ad12759a505fef51d84d4.

--- tasks/bash/easy/create_files.sh
+++ tasks/bash/easy/create_files.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-cd ~/fork-commit-merge/tasks/bash/easy/
-touch file1.txt file2.txt file3.txt
-printf "Hello Shell!\n" > file1.txt
-printf "Hello Shell!\n" > file2.txt
-printf "Hello Shell!\n" > file3.txt
-cat file1.txt file2.txt file3.txt
+# Bash - Easy
+
+# TODO: Write the Bash script requested in the issue description
