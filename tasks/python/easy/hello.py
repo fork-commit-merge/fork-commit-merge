16fce1f33823035a651545c248b567b8a797906b
GitHub Action <action@github.com>
2025-07-25 14:01:33 +0000

    Revert "Merge pull request #4149 from umuttopalak/main"
    This reverts commit e48e0c92991b3cc15b558abe6a2ab6245f56ab03, reversing
    changes made to 635795afe9a0592ee2d68f36842e696b5a6c70c3.

--- tasks/python/easy/hello.py
+++ tasks/python/easy/hello.py
@@ -1,13 +1,3 @@
 # Python - Easy
 
 # TODO: Write a function that prints "Hello, Python!" to the console
-
-def hello_python():
-    """
-    Function that prints "Hello, Python!" to the console.
-    """
-    print("Hello, Python!")
-
-# Call the function to demonstrate the functionality
-if __name__ == "__main__":
-    hello_python()
