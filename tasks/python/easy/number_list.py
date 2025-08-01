e7f76435258c3870a818be1fd348d1853c35c115
GitHub Action <action@github.com>
2025-08-01 14:54:44 +0000

    Revert "Merge pull request #4301 from sagarbharadia17/fix-issue"
    This reverts commit 80c961e5afac25180bb9f6430af6f4c02be543e2, reversing
    changes made to febe86e64dc2168fc8f693af5f2c30399338f7b2.

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -5,18 +5,5 @@
 # prints that list to the console and,
 # prints the sum of the numbers of that list
 
-def sum_of_first_10_natural_numbers():
-    # Create a list of the first 10 natural numbers
-    natural_numbers = list(range(1, 11))
-
-    # Print the created list
-    print("List of the first 10 natural numbers:", natural_numbers)
-
-    # Calculate the sum of the numbers in the list
-    list_sum = sum(natural_numbers)
-
-    # Print the sum of the numbers
-    print("Sum of the numbers in the list:", list_sum)
-    
-
-sum_of_first_10_natural_numbers()
+def function(numbers):
+    pass
