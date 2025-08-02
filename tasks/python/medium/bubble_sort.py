2940b1b56eb28cafad2e71c39f76eed0c491ed8b
GitHub Action <action@github.com>
2025-08-02 09:45:56 +0000

    Revert "Merge pull request #4305 from sagarbharadia17/fix-medium-issue-python"
    This reverts commit 6ca7845f8001db2d41b9fe65cb1fcc9496df2f0d, reversing
    changes made to bbe37e49529c556299abdf614a38fd572542cf56.

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,10 +1,5 @@
 def bubble_sort(arr):
-    n = len(arr)  # Get the length of the array
-    for i in range(n):  
-         for j in range(0, n - i - 1):  
-            if arr[j] > arr[j + 1]:  # Compare adjacent elements
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]  # Swap if out of order
-    
+    # TODO: Implement the bubble sort algorithm
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
