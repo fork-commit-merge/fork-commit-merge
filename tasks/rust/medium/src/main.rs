2a94b3bf59ecaef4396a24a896715e57e01e35dc
GitHub Action <action@github.com>
2025-08-04 08:09:22 +0000

    Revert "Merge pull request #4311 from Dexter2038/fix-issue"
    This reverts commit 19b72e0e45219661c06958f65af62a489dbf9de7, reversing
    changes made to 445aae9844526ffcba6b00131908612f226e942f.

--- tasks/rust/medium/src/main.rs
+++ tasks/rust/medium/src/main.rs
@@ -1,56 +1,7 @@
 // Rust - Medium
 
-use std::io::{self, Write};
+use std::io;
 
-/// Main function
-///
-/// # Panics
-/// 
-/// Panics if there is an error reading user input
-/// Panics if there is an error flushing stdout
-/// Panics if there is an error parsing user input for temperature
-/// Panics if there is an error parsing user input for unit
-///
-fn main() -> Result<(), Box<dyn std::error::Error>> {    
-    // Print instructions for temperature prompt
-    print!("Please enter the temperature value: ");
-    io::stdout().flush()?;
-
-    // Read user input as a string
-    let mut temp = String::new();
-    io::stdin().read_line(&mut temp)?;
-
-    // Validate temperature as a float
-    let temp: f64 = temp.trim().parse()?;
-
-    // Print instructions for unit prompt
-    println!("Please enter the current unit.");
-    print!("Hint: 1/F/f = Fahrenheit, 2/C/c = Celsius: ");
-    io::stdout().flush()?;
-
-    // Read user input as a string
-    let mut unit = String::new();
-    io::stdin().read_line(&mut unit)?;
-
-    // Validate unit against expected values
-    match unit.trim() {
-        "1" | "F" | "f" => {
-            // Convert Fahrenheit to Celsius
-            let converted = (temp - 32.0) * 5.0 / 9.0;
-            println!("{temp}°F = {converted}°C");
-        },
-        "2" | "C" | "c" => {
-            // Convert Celsius to Fahrenheit
-            let converted = (temp * 9.0 / 5.0) + 32.0;
-            println!("{temp}°C = {converted}°F");
-        },
-        // Everything else
-        _ => {
-            // Input doesn't match expected values
-            eprintln!("Invalid input");
-        }
-    }
-
-    // Return success code
-    Ok(())
+fn main() {
+  // TODO: Create a program in Rust that converts temperatures between Fahrenheit and Celsius
 }
