9f40329154af9f4b48776c0559dfa37d9e0dd266
GitHub Action <action@github.com>
2025-08-07 06:40:21 +0000

    Revert "Merge pull request #4319 from sagarbharadia17/easy-2-cpp"
    This reverts commit aabff19ad7eba860e519ce7074c07c8aefa52584, reversing
    changes made to 3e9a81e29e00f05b043004d783434b6749eacaf3.

--- tasks/c++/easy/sum.cpp
+++ tasks/c++/easy/sum.cpp
@@ -1,13 +1,8 @@
 // C++ - Easy 2
 
 // TODO: Create a Basic C++ Program to Add Two Numbers
-#include <iostream>
 
-void Sum(float a, float b){
-    float c = a + b;
-    std::cout << c << std::endl;
-}
 int main()
 {
-  Sum (4, 6);
+  
 }
