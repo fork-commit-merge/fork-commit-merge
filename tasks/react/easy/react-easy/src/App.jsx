b495c88d788287d82fa31c838b8e282ea48b59c8
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2025-08-09 13:36:02 +0530

    Added card component with random image

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -4,9 +4,16 @@ import "./index.css";
 function App() {
   return (
     <div className="center">
-      {/* TODO: Implement the card component */}
+      <h1>React/Vite - Easy</h1>
+      <div className="card">
+        <img src="https://media.istockphoto.com/id/183819034/photo/two-dices.webp?s=1024x1024&w=is&k=20&c=VAzLPaQsBfHhYhEjr3d1OOx7WoOHRmLI08EIDQeJ8iQ=" alt="Random" />
+        <h2>Card Title</h2>
+        <p>Card Description</p>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
