a6d915fba7a586d13feacc98e87705c47e5d3731
Ranjith <ranjithranji935@gmail.com>
2025-08-09 22:03:52 +0530

    Add image loader to Card component

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -1,10 +1,36 @@
-// React/Vite - Easy
+import React, { useState } from "react";
 import "./index.css";
+
+const images = [
+  "https://picsum.photos/200/300?random=1",
+  "https://picsum.photos/200/300?random=2",
+  "https://picsum.photos/200/300?random=3",
+  "https://picsum.photos/200/300?random=4",
+];
+
+function Card() {
+  const randomIndex = Math.floor(Math.random() * images.length);
+  const imageUrl = images[randomIndex];
+  const [loading, setLoading] = useState(true);
+
+  return (
+    <div className="card">
+      {loading && <p>Loading image...</p>}
+      <img
+        src={imageUrl}
+        alt="Random"
+        onLoad={() => setLoading(false)}
+        style={loading ? { display: "none" } : {}}
+      />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="center">
-      {/* TODO: Implement the card component */}
+      <h1>Random Image Card</h1>
+      <Card />
     </div>
   );
 }
