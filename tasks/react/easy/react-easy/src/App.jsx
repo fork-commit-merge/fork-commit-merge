c5fbc5b22738265e71fc5f14af41c6352c465269
GitHub Action <action@github.com>
2025-08-09 16:41:07 +0000

    Revert "Merge pull request #4323 from Ranjith935/main"
    This reverts commit 048abe73926800804ea3dd91dca4cfc970edf297, reversing
    changes made to 4e0c9c218368ce8a7f301c5e83e43131c202c9fe.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -1,36 +1,10 @@
-import React, { useState } from "react";
+// React/Vite - Easy
 import "./index.css";
-
-const images = [
-  "https://picsum.photos/200/300?random=1",
-  "https://picsum.photos/200/300?random=2",
-  "https://picsum.photos/200/300?random=3",
-  "https://picsum.photos/200/300?random=4",
-];
-
-function Card() {
-  const randomIndex = Math.floor(Math.random() * images.length);
-  const imageUrl = images[randomIndex];
-  const [loading, setLoading] = useState(true);
-
-  return (
-    <div className="card">
-      {loading && <p>Loading image...</p>}
-      <img
-        src={imageUrl}
-        alt="Random"
-        onLoad={() => setLoading(false)}
-        style={loading ? { display: "none" } : {}}
-      />
-    </div>
-  );
-}
 
 function App() {
   return (
     <div className="center">
-      <h1>Random Image Card</h1>
-      <Card />
+      {/* TODO: Implement the card component */}
     </div>
   );
 }
