75090e0683cba5e6c84075002cfae521e88e7c77
GitHub Action <action@github.com>
2025-08-09 11:22:59 +0000

    Revert "Merge pull request #4322 from tharani-2006/main"
    This reverts commit 5bf71ff4968d1236c0c4db6e3c99bd8248c91d39, reversing
    changes made to 1230d27d16a3821c69e144076a942976fa9cc36f.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -4,16 +4,9 @@ import "./index.css";
 function App() {
   return (
     <div className="center">
-      <h1>React/Vite - Easy</h1>
-      <div className="card">
-        <img src="https://media.istockphoto.com/id/183819034/photo/two-dices.webp?s=1024x1024&w=is&k=20&c=VAzLPaQsBfHhYhEjr3d1OOx7WoOHRmLI08EIDQeJ8iQ=" alt="Random" />
-        <h2>Card Title</h2>
-        <p>Card Description</p>
-      </div>
+      {/* TODO: Implement the card component */}
     </div>
   );
 }
 
 export default App;
-
-
