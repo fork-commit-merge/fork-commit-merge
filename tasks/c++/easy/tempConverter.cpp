77965252ec1c11b519073af6bccb9d798619313a
sagarbharadia17 <sagarbharadia17@gmail.com>
2025-08-10 09:54:52 +0530

    Fixed issue

--- tasks/c++/easy/tempConverter.cpp
+++ tasks/c++/easy/tempConverter.cpp
@@ -2,4 +2,20 @@
 
 int main() {
   // TODO: Implement the temperature converter
+    float celsius;    
+    float fahrenheit; 
+
+    
+    std::cout << "Enter the temperature in Celsius: "; 
+
+    
+    std::cin >> celsius; 
+
+    
+    fahrenheit = (celsius * 9.0 / 5.0) + 32;
+
+    
+    std::cout << "The temperature in Fahrenheit is: " << fahrenheit << std::endl; 
+
+    return 0; 
 }
