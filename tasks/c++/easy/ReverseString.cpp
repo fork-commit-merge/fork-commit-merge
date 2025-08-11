b7c4f3215ab0e1a440591228dd0f3145f4833330
GitHub Action <action@github.com>
2025-08-11 06:16:33 +0000

    Revert "Merge pull request #4325 from sagarbharadia17/easy-4-cpp"
    This reverts commit fba04ab0c10101b90bbe6b4c01cb6467c134a01e, reversing
    changes made to e4e047b8f9463683bd3fe98a2ab058d1ee428e56.

--- tasks/c++/easy/ReverseString.cpp
+++ tasks/c++/easy/ReverseString.cpp
@@ -3,37 +3,9 @@
 
 std::string reverseString(const std::string &str) {
     // TODO: Implement the reverseString function
-
-    int length = str.length();
-    int i = 0;
-    int j = length - 1; 
-
-    std::string new_string;
-    new_string = str;
-  
-    while (i < j) { 
-      char temp = new_string[i];
-      new_string[i] = new_string[j];
-      new_string[j] = temp;
-      i++;
-      j--;
-    }
-
-   return new_string;
 }
 
 int main() {
     // TODO: Implement the main method
-    std::string input_string;
-    std::string rev_string;
-    
-    std::cout << "Enter a string: ";
-    std::getline(std::cin, input_string);
-
-    rev_string = reverseString(input_string);
-
-    std::cout << "Reversed string: " << rev_string << std::endl;
-
-    return 0;
 }
 
