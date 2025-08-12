e2d044d30394a1a6c4e4bb5f88705a2a1bd9771e
GitHub Action <action@github.com>
2025-08-12 05:35:12 +0000

    Revert "Merge pull request #4329 from sagarbharadia17/medium-1-cpp"
    This reverts commit 35e3efd01ba4e294ebebebb9d23c032facb36293, reversing
    changes made to db007a8c5755da4e66b61280bd81ddfa44a58960.

--- tasks/c++/medium/file_handler/FileHandler.cpp
+++ tasks/c++/medium/file_handler/FileHandler.cpp
@@ -3,56 +3,12 @@
 #include "FileHandler.h"
 #include <fstream>
 #include <sstream>
-#include <iostream>
-#include <cstdio>
 
-std::string FileHandler::ReadFile(const std::string& filePath) {
-    std::ifstream file(filePath); // Open file for reading
+// TODO: Implement the FileHandler::ReadFile method
 
-    if (!file.is_open()) { 
-        std::cerr << "Error: Unable to open file for reading: " << filePath << std::endl;
-        return "";
-    }
+// TODO: Implement the FileHandler::WriteFile method
 
-    std::stringstream buffer;
-    buffer << file.rdbuf(); 
-    file.close(); 
+// TODO: Implement the FileHandler::AppendFile method
 
-    return buffer.str(); 
-}
-
-
-bool FileHandler::WriteFile(const std::string& filePath, const std::string& content) {
-    std::ofstream file(filePath); // Open file for writing, overwrites existing content
-
-    if (!file.is_open()) { 
-        std::cerr << "Error: Unable to open file for writing: " << filePath << std::endl;
-        return false;
-    }
-
-    file << content; 
-    file.close(); 
-    return true;
-}
-
-
-bool FileHandler::AppendFile(const std::string& filePath, const std::string& content) {
-    std::ofstream file(filePath, std::ios::app); // Open file in append mode
-
-    if (!file.is_open()) { 
-        std::cerr << "Error: Unable to open file for appending: " << filePath << std::endl;
-        return false;
-    }
-
-    file << content; 
-    file.close(); 
-
-    return true;
-}
-
-
-bool FileHandler::FileExists(const std::string& filePath) {
-    std::ifstream file(filePath); // Attempt to open the file for reading
-    return file.good(); 
-}
+// TODO: Implement the FileHandler::FileExists method
 
