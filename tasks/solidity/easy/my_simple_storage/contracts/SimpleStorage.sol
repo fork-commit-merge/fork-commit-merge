a71e0dc486fbd5c26d116f2a7ecbec340a6b9b57
Harmeena <harmeenagarg@gmail.com>
2025-08-18 14:47:42 +0530

    fixed issue

--- tasks/solidity/easy/my_simple_storage/contracts/SimpleStorage.sol
+++ tasks/solidity/easy/my_simple_storage/contracts/SimpleStorage.sol
@@ -5,7 +5,12 @@ pragma solidity ^0.8.0;
 
 contract SimpleStorage {
     uint256 _number;
-    // TODO: Implement function to set the number
+    
+    function set(uint256 number) public {
+        _number = number;
+    }
 
-    // TODO: Implement function to get the number
+    function get() public view returns (uint256) {
+        return _number;
+    }
 }
