cf8782c95c32b840ae0becc2d02aadc59b062508
GitHub Action <action@github.com>
2025-08-19 06:32:03 +0000

    Revert "Merge pull request #4477 from sagarbharadia17/hard-cpp"
    This reverts commit bbda6ef7039e44c3d210b6dfded285f0d50c8498, reversing
    changes made to 1541ddcb36cab4f2ec7e4c397a31fe75acd940e6.

--- tasks/c++/hard/mystring.h
+++ tasks/c++/hard/mystring.h
@@ -1,41 +1,8 @@
 // C++ - Hard
 
-#pragma once
+define MYSTRING_H
 #include <iostream>
 
 class MyString {
     // TODO: Implement MyString class
-    private:
-    char* data;  
-    int len;
-
-    public:
-    //  Constructor
-    MyString();
-
-    MyString(const char* s); 
-
-    MyString(const MyString& other);
-
-    // Destructor
-
-    ~MyString();
-
-
-    // Functions
-    int length() const;
-
-    const char* c_str() const;
-
-    void append(const MyString& other);
-
-    int compare(const MyString& other) const;
-
-    MyString& operator=(const MyString& other);
-
-    MyString operator+(const MyString& other) const;
-
-    bool operator==(const MyString& other) const;
-
-    friend std::ostream& operator<<(std::ostream& os, const MyString& str);
-};
+}
