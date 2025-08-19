c72b737b690316d2f84ab8ff5d934e831e132e2d
sagarbharadia17 <sagarbharadia17@gmail.com>
2025-08-19 11:35:50 +0530

    Fixed issue

--- tasks/c++/hard/mystring.h
+++ tasks/c++/hard/mystring.h
@@ -1,8 +1,41 @@
 // C++ - Hard
 
-define MYSTRING_H
+#pragma once
 #include <iostream>
 
 class MyString {
     // TODO: Implement MyString class
-}
+    private:
+    char* data;  
+    int len;
+
+    public:
+    //  Constructor
+    MyString();
+
+    MyString(const char* s); 
+
+    MyString(const MyString& other);
+
+    // Destructor
+
+    ~MyString();
+
+
+    // Functions
+    int length() const;
+
+    const char* c_str() const;
+
+    void append(const MyString& other);
+
+    int compare(const MyString& other) const;
+
+    MyString& operator=(const MyString& other);
+
+    MyString operator+(const MyString& other) const;
+
+    bool operator==(const MyString& other) const;
+
+    friend std::ostream& operator<<(std::ostream& os, const MyString& str);
+};
