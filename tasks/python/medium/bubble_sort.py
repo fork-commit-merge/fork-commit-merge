cd23c5fd2076b9dd5e29583033b17981a436f902
Lincoln-M-Oliveira <lincoln07.oliveira@gmail.com>
2025-08-20 22:07:21 -0300

    Added bubble sort

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,5 +1,20 @@
 def bubble_sort(arr):
     # TODO: Implement the bubble sort algorithm
+    n = len(arr)
+
+    for i in range(n): # for each item on the list arr (n)
+        swapped = False
+
+        for j in range(n - i - 1):
+
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            swapped = True
+
+        if not swapped:
+            break
+    return arr
+
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
@@ -7,3 +22,4 @@ print("Original array:", arr)
 
 bubble_sort(arr)
 print("Sorted array:", arr)
+
