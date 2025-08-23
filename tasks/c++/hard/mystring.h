0a988f523d124dfc913f75594b851e06fde0cd0c
GitHub Action <action@github.com>
2025-08-23 06:25:32 +0000

    Revert "Merge pull request #4488 from Roma3214185/fix-task-2"
    This reverts commit 58441f7fc4d1cf5a9a93a5d9df8aff25c72ff77e, reversing
    changes made to a24e5add876589289394a7f2e50f053e865afb8d.

--- tasks/c++/hard/mystring.h
+++ tasks/c++/hard/mystring.h
@@ -1,23 +1,8 @@
 // C++ - Hard
 
-#pragma once
+define MYSTRING_H
 #include <iostream>
 
 class MyString {
     // TODO: Implement MyString class
-public:
-    //MyString();
-    MyString(const char* s = "");
-    MyString(const MyString& other);
-    int length() const ;
-    const char* c_str() const;
-    void append(const MyString& other);
-    int compare(const MyString& other) const;
-    MyString& operator=(const MyString& other);
-    MyString operator+(const MyString& other) const;
-    bool operator==(const MyString& other) const;
-    friend std::ostream& operator<<(std::ostream& os, const MyString& s);
-    ~MyString();
-private:
-    char* str_;
-};
+}
