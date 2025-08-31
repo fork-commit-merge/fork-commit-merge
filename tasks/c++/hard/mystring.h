a2b679ba451da6ee9eb08914d2ff133d56a7a1a6
Pedro Alvarenga <p.alvarenga.dev@gmail.com>
2025-08-31 01:02:13 -0300

    Fixed issue

--- tasks/c++/hard/mystring.h
+++ tasks/c++/hard/mystring.h
@@ -1,8 +1,36 @@
 // C++ - Hard
 
-define MYSTRING_H
-#include <iostream>
+#ifndef MYSTRING__H__
+#define MYSTRING__H__ 
 
-class MyString {
-    // TODO: Implement MyString class
-}
+#include <iostream>
+#include <cstring>
+#include <stdbool.h>
+
+class MyString 
+{
+	private:
+		char* _data; 
+
+	public: 
+		MyString(const char* cstr = ""); 
+		MyString(const MyString& other); 
+		
+		inline const char* c_str() const; 
+		inline size_t length() const; // size_t is better
+		int compare(const MyString& other) const; 
+	
+		void append(const MyString& other); 
+
+		MyString& operator=(const MyString& other); // returned MyString&
+		MyString operator+(const MyString& other) const;
+		bool operator==(const MyString& other) const;
+		friend std::ostream& operator<<(std::ostream& os, const MyString& str);
+
+		~MyString() 
+		{
+			delete[] _data;
+		}
+}; // Didn't have ; to end class def
+
+#endif
