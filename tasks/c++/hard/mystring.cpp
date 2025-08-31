c4246858d63b8f925c0a991bffebbdd04ea6b48d
GitHub Action <action@github.com>
2025-08-31 05:15:17 +0000

    Revert "Merge pull request #4493 from p-alvarenga/fix-issue"
    This reverts commit ef6dd5efcce065e755843bc090730f81916dfb91, reversing
    changes made to 73f9e302bdd34c4e7ee20d9ee8f9ecbbd66adb13.

--- tasks/c++/hard/mystring.cpp
+++ tasks/c++/hard/mystring.cpp
@@ -1,111 +1,42 @@
-#include "mystring.h"
+#include <cstring>
+#include <iostream>
 
-MyString::MyString(const char* cstr) 
-{
-	size_t len = strlen(cstr);
-
-	_data = new char[len + 1];
-	memcpy(_data, cstr, len);
-	_data[len] = '\0';
+MyString::MyString(const char* s) {
+    // TODO: Implement this
 }
 
-MyString::MyString(const MyString& other) 
-{
-	size_t len = other.length();
-	_data = new char[len + 1]; 
-
-	memcpy(_data, other.c_str(), len); 
-	_data[len] = '\0';
+MyString::MyString(const MyString& other) {
+    // TODO: Implement this
 }
 
-size_t MyString::length() const 
-{
-	return strlen(_data); 
+int MyString::length() const {
+    // TODO: Implement this
 }
 
-inline const char* MyString::c_str() const 
-{
-    return _data; 
+const char* MyString::c_str() const {
+    // TODO: Implement this
 }
 
-void MyString::append(const MyString& other) 
-{
-    size_t this_len, other_len; 
-
-	this_len = strlen(_data); 
-	other_len = other.length();
-
-	char* new_data = new char[this_len + other_len + 1]; 
-
-	memcpy(new_data, _data, this_len);
-	memcpy(new_data + this_len, other.c_str(), other_len);
-
-	new_data[this_len + other_len] = '\0';
-
-	delete[] _data; 
-	_data = new_data; 
+void MyString::append(const MyString& other) {
+    // TODO: Implement this
 }
 
-int MyString::compare(const MyString& other) const 
-{
-	size_t len = strlen(_data); 
-	if (len != other.length()) return false;
-
-	const char* buf = other.c_str(); 
-
-	for (int i = 0; i < len; i++)
-		if (_data[i] != buf[i]) return false;
- 
-	return true; 
+int MyString::compare(const MyString& other) const {
+    // TODO: Implement this
 }
 
-MyString& MyString::operator=(const MyString& other) 
-{
-	if (this == &other) return *this; 
-
-	size_t new_len = other.length(); 
-	char* new_data = new char[new_len + 1]; 
-
-	memcpy(new_data, other.c_str(), new_len);
-	new_data[new_len] = '\0';
-
-	delete[] _data; 
-	_data = new_data;
-
-	return *this; 
+MyString& MyString::operator=(const MyString& other) {
+    // TODO: Implement this
 }
 
-MyString MyString::operator+(const MyString& other) const 
-{
-	size_t this_len = strlen(_data);
-	size_t other_len = other.length(); 
-	char* new_data = new char[this_len + other_len + 1]; 
-
-	memcpy(new_data, _data, this_len); 
-	memcpy(new_data + this_len, other.c_str(), other_len);
-
-	new_data[this_len + other_len] = '\0';
-
-	return MyString(new_data);
+MyString MyString::operator+(const MyString& other) const {
+    // TODO: Implement this
 }
 
-bool MyString::operator==(const MyString& other) const 
-{
-	size_t this_len = strlen(_data);
-
-	if (this == &other) return true; 
-	if (this_len != other.length()) return false; 
-
-	const char* buf = other.c_str(); 
-
-	for (int i = 0; i < this_len; i++)
-		if (_data[i] != buf[i]) return false;
-
-	return true; 
+bool MyString::operator==(const MyString& other) const {
+    // TODO: Implement this
 }
 
-std::ostream& operator<<(std::ostream& os, const MyString& str) // Operator << must be binary
-{
-	os << str._data; 
-	return os;
+std::ostream& operator<<(std::ostream& os, const MyString& s) {
+    // TODO: Implement this
 }
