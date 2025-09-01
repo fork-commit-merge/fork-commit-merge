1e24f465ca9886d3158e6cfa6d0a664bc4ad8474
mariyam-hanna <havvamariyam2001@gmail.com>
2025-09-01 13:44:16 +0530

    Fixed issue

--- tasks/c#/easy/HelloUser/Program.cs
+++ tasks/c#/easy/HelloUser/Program.cs
@@ -1,3 +1,17 @@
 ﻿// C# - Easy
 
 // TODO: Implement the C# program requested in the Issue description
+using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
