88be199c0a6d04fc9edb8ec133492ac1d4a12da4
Prabhu-1409 <msprabhuraj15@gmail.com>
2025-09-01 16:45:28 +0530

    Created Program for Reverse String Using D Language

--- tasks/d/easy/reverseString.d
+++ tasks/d/easy/reverseString.d
@@ -3,5 +3,12 @@
 import std.stdio;
 
 void main() {
-    // TODO: Implement program to reverse a string
+    char[] temp = "Hello World!".dup;
+    char[] ans;
+    int len = cast(int) temp.length;
+    ans.length = len;
+    for(int i=cast(int)temp.length-1;i>=0;i--){
+        ans[(len-i)-1]=cast(char) temp[i];
+    }
+    writeln(ans);
 }
