591db017768ddd3e601e9956ef223c63eb06e716
GitHub Action <action@github.com>
2025-09-01 12:36:41 +0000

    Revert "Merge pull request #4634 from Prabhu-1409/d-reverseString"
    This reverts commit 47bb6f3a85da9241e43d30021d4e87832b66ed8d, reversing
    changes made to 870eeae5871074e1dbb1b171ab0940701a0109c3.

--- tasks/d/easy/reverseString.d
+++ tasks/d/easy/reverseString.d
@@ -3,12 +3,5 @@
 import std.stdio;
 
 void main() {
-    char[] temp = "Hello World!".dup;
-    char[] ans;
-    int len = cast(int) temp.length;
-    ans.length = len;
-    for(int i=cast(int)temp.length-1;i>=0;i--){
-        ans[(len-i)-1]=cast(char) temp[i];
-    }
-    writeln(ans);
+    // TODO: Implement program to reverse a string
 }
