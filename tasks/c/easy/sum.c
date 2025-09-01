dd48f0e8dd21211125948e94420b991a8ff32a10
GitHub Action <action@github.com>
2025-09-01 12:35:05 +0000

    Revert "Merge pull request #4635 from Valcyle/fix-issue"
    This reverts commit fc2ec909071e7953bae251ab32602e23962770a6, reversing
    changes made to ff8f02a4707e66c2c302db13793976e6871f55a9.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -3,15 +3,3 @@
 #include <stdio.h>
 
 // TODO: Implement the sum and main functions
-int sum(int a, int b){
-    return a+b;
-}
-
-int main(int argc, char* argv[]){
-    int a, b, result;
-    printf("Enter two numbers: ");
-    scanf("%d %d", &a, &b);
-    result = sum(a, b);
-    printf("The sum is: %d\n", result);
-    return 0;
-}
