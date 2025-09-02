7a94d51c46ed767ea6a5fe2f7d9697f8ab5eb17a
GitHub Action <action@github.com>
2025-09-02 12:59:23 +0000

    Revert "Merge pull request #4639 from Ossy-em/feature/random-card"
    This reverts commit 09582d24490805f43def08e1dae6c0e0b050afba, reversing
    changes made to 1ee3ca9f6d62efcaf61299d02532b0c0a0197b8d.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -1,26 +1,10 @@
+// React/Vite - Easy
 import "./index.css";
-import React, { useEffect, useState } from "react";
 
 function App() {
-  const [image, setImage] = useState("");
-
-  useEffect(() => {
-
-    fetch("https://picsum.photos/300/200")
-      .then((response) => {
-
-        setImage(response.url);
-      })
-      .catch((err) => console.error(err));
-  }, []);
-
   return (
     <div className="center">
-      {image ? (
-        <img src={image} alt="Random" className="card img" />
-      ) : (
-        <p>Loading...</p>
-      )}
+      {/* TODO: Implement the card component */}
     </div>
   );
 }
