dfe23d0e3e5b5dd0ff459567ac4aa4a0583f55f1
Ushio-Hayase <UshioHayase@gmail.com>
2025-09-03 23:19:40 +0900

    add mystring member function declaration

--- tasks/c++/hard/mystring.cpp
+++ tasks/c++/hard/mystring.cpp
@@ -1,42 +1,62 @@
+#include "mystring.h"
+
 #include <cstring>
 #include <iostream>
 
-MyString::MyString(const char* s) {
+MyString::MyString(const char* s) : content(s)
+{
+    size_t s_len = 0;
+    char* ptr = const_cast<char*>(s);
+    while (ptr != '\0')
+    {
+        ++ptr;
+        ++s_len;
+    }
+    len = s_len;
+}
+
+MyString::MyString(const MyString& other)
+{
+    this->content.reset();
+    this->content;
+}
+
+int MyString::length() const
+{
     // TODO: Implement this
 }
 
-MyString::MyString(const MyString& other) {
+const char* MyString::c_str() const
+{
     // TODO: Implement this
 }
 
-int MyString::length() const {
+void MyString::append(const MyString& other)
+{
     // TODO: Implement this
 }
 
-const char* MyString::c_str() const {
+int MyString::compare(const MyString& other) const
+{
     // TODO: Implement this
 }
 
-void MyString::append(const MyString& other) {
+MyString& MyString::operator=(const MyString& other)
+{
     // TODO: Implement this
 }
 
-int MyString::compare(const MyString& other) const {
+MyString MyString::operator+(const MyString& other) const
+{
     // TODO: Implement this
 }
 
-MyString& MyString::operator=(const MyString& other) {
+bool MyString::operator==(const MyString& other) const
+{
     // TODO: Implement this
 }
 
-MyString MyString::operator+(const MyString& other) const {
-    // TODO: Implement this
-}
-
-bool MyString::operator==(const MyString& other) const {
-    // TODO: Implement this
-}
-
-std::ostream& operator<<(std::ostream& os, const MyString& s) {
+std::ostream& operator<<(std::ostream& os, const MyString& s)
+{
     // TODO: Implement this
 }
--- tasks/c++/hard/mystring.h
+++ tasks/c++/hard/mystring.h
@@ -1,8 +1,37 @@
 // C++ - Hard
-
-define MYSTRING_H
+#ifndef MYSTRING_H
+#define MYSTRING_H
 #include <iostream>
+#include <memory>
 
-class MyString {
-    // TODO: Implement MyString class
-}
+class MyString
+{
+   private:
+    std::unique_ptr<const char> content;
+    size_t len;
+
+   public:
+    MyString();
+
+    MyString(const char* s);
+
+    MyString(const MyString& other);
+
+    int length() const;
+
+    const char* c_str() const;
+
+    void append(const MyString& other);
+
+    int compare(const MyString& other) const;
+
+    MyString& operator=(const MyString& other);
+
+    MyString operator+(const MyString& other) const;
+
+    bool operator==(const MyString& other) const;
+
+    friend std::ostream& operator<<(std::ostream& os, const MyString& s);
+};
+
+#endif
