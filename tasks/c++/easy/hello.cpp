89d55d791254c1fbbc520ba627608dd5a5e59be5
GitHub Action <action@github.com>
2025-09-06 05:17:15 +0000

    Revert "Merge pull request #4644 from mohamedsalem21v/fix-issue"
    This reverts commit 160fb2135b57deacf6c76c18820b1d5dd5766d6a, reversing
    changes made to 9d75b480a4fdcfd6a265f8e2b0f0c0bea22332d5.

--- tasks/c++/easy/hello.cpp
+++ tasks/c++/easy/hello.cpp
@@ -1,10 +1,9 @@
 // C++ - Easy 3
 
 #include <iostream>
-using namespace std;
+
 int main() {
     // TODO: Create a Basic C++ Program that Prints "Hello World"
-    cout << "Hello World";
     return 0;
 }
 
