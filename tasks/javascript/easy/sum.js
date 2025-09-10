25346c227bcefca0ffddf8430199f0109291cbdf
GitHub Action <action@github.com>
2025-09-10 17:09:21 +0000

    Revert "Merge pull request #4647 from SourishBhattacherjee/main"
    This reverts commit 205fa647e9449953529451ff167494f5303d55a6, reversing
    changes made to 849fcaacef32c258a28ab46aa34fd2bb19e83769.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,7 +1,3 @@
 // JavaScript - Easy
 
 // TODO: Write a function that returns the sum of two numbers
-function sum(a, b) {
-  console.log(`Sum of ${a} and ${b} is ${a + b}`);
-}
-sum(1, 2); // Example usage
