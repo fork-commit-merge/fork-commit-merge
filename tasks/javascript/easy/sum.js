13b486a0b86ab28704ded99ed5e5c687616527ab
Sourish Bhattacharjee <sourishbhatt3302@gmail.com>
2025-09-10 18:05:14 +0530

    sum added

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,3 +1,7 @@
 // JavaScript - Easy
 
 // TODO: Write a function that returns the sum of two numbers
+function sum(a, b) {
+  console.log(`Sum of ${a} and ${b} is ${a + b}`);
+}
+sum(1, 2); // Example usage
