9eb4d796503d8ea21d1e2a2fc9f3ae38362f4ea7
GitHub Action <action@github.com>
2025-09-17 06:51:39 +0000

    Revert "Merge pull request #4805 from mike-s-zaugg/patch-9"
    This reverts commit ae62bf21c788869c967ef031d97139de75d7a43c, reversing
    changes made to 0e5d7f71917fc87250b9dac79c14c0742670939d.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,9 +1,3 @@
 // JavaScript - Easy
 
 // TODO: Write a function that returns the sum of two numbers
-function SumNum (num1, num2) {
-  return num1 + num2;
-}
-
-let result = SumNum(2, 8);
-console.log(result);
