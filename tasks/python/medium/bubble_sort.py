17b1c45f721cf0609810186b651341d93e858957
Tushar Narendra Kumar <8tushar.kumar12@gmail.com>
2025-09-17 12:51:32 +0530

    Implementing Bubble Sort

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,6 +1,12 @@
 def bubble_sort(arr):
-    # TODO: Implement the bubble sort algorithm
-
+    for _ in range(len(arr)):
+        i = 0
+        while i < len(arr) - 1:
+            if arr[i] > arr[i+1]:
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+            i+=1
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
 print("Original array:", arr)
