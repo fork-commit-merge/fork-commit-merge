30875a30bb2ca9751e3f3b38764b476c7d411922
GitHub Action <action@github.com>
2025-09-17 15:59:27 +0000

    Revert "Merge pull request #4813 from svuksanova/fix-issue"
    This reverts commit 2b03b95c9fca03e6f58442a4035a7609a91fb515, reversing
    changes made to 07f59cd636a70be986073f3ec87693d5ac3ce705.

--- tasks/c++/hard/mystring.h
+++ tasks/c++/hard/mystring.h
@@ -1,37 +1,8 @@
 // C++ - Hard
 
-#ifndef MYSTRING_H
-
-#define MYSTRING_H
+define MYSTRING_H
 #include <iostream>
 
 class MyString {
-private:
-    char *characters;
-    int n;
-
     // TODO: Implement MyString class
-
-public:
-
-    //constructors
-    MyString(const char* s="");
-    MyString(const MyString& other); //copy
-
-    //operators
-    bool operator==(const MyString& other) const;
-    MyString& operator=(const MyString& other);
-    MyString operator+(const MyString& other) const;
-
-    friend std::ostream& operator<<(std::ostream& os, const MyString& s);
-
-    int length() const;
-    const char* c_str() const;
-    void append(const MyString& other);
-    int compare(const MyString& other) const;
-
-    //destructor
-    ~MyString();
-};
-
-#endif
+}
