6b5f996ff42ffcdb3ba6016ca4f16d211e1de698
Mike Zaugg <sandorzaugg@gmail.com>
2025-09-17 08:45:17 +0200

    Update sum.js

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,3 +1,9 @@
 // JavaScript - Easy
 
 // TODO: Write a function that returns the sum of two numbers
+function SumNum (num1, num2) {
+  return num1 + num2;
+}
+
+let result = SumNum(2, 8);
+console.log(result);
