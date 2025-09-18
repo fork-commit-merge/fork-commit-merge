f5bf7b6d331d7499443da3ff363ae316c7ba6eed
yash-1104github <gyah1104@gmail.com>
2025-09-18 17:10:12 +0530

    resolve Fork, Commit, Merge - Easy Issue (React.js) #4786

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -5,6 +5,13 @@ function App() {
   return (
     <div className="center">
       {/* TODO: Implement the card component */}
+        <h1>React/Vite - Easy</h1>
+        <div className="card">
+             <img  className="card img" src="https://picsum.photos/320/240" alt="Random user" />
+             <h2 style={{alignItems: "center", justifyContent: "center", display: "flex"}} >Card Title</h2>
+             <p style={{alignItems: "center", justifyContent: "center", display: "flex"}}>Card Description</p>
+        </div>
+         
     </div>
   );
 }
