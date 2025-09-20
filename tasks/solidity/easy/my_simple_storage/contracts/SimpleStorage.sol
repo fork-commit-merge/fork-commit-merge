29173ec1107c599d234ad8f237293efa33f0fb80
GitHub Action <action@github.com>
2025-09-20 19:26:10 +0000

    Revert "Merge pull request #4829 from mandip-kamaliya/solve-simple-storage-task"
    This reverts commit e586a6fad4970a8974b4b08e448f94c02edba10e, reversing
    changes made to 097a8262f14356720c9e1f72644cf05e321b15fd.

--- tasks/solidity/easy/my_simple_storage/contracts/SimpleStorage.sol
+++ tasks/solidity/easy/my_simple_storage/contracts/SimpleStorage.sol
@@ -6,12 +6,6 @@ pragma solidity ^0.8.0;
 contract SimpleStorage {
     uint256 _number;
     // TODO: Implement function to set the number
-    function setNumber(uint number) public{
-        _number=number;
-    }
 
     // TODO: Implement function to get the number
-    function getNumber() public view returns(uint){
-        return _number;
-    }
 }
