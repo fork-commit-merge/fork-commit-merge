a1590193b205d152469655233b4093b34ace9a4c
eldred16 <kubanjaelijah2037@gmail.com>
2025-09-23 23:27:44 +0300

    The sum and main functions implemented

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -3,3 +3,21 @@
 #include <stdio.h>
 
 // TODO: Implement the sum and main functions
+
+#include <stdio.h>
+
+// Function to calculate sum of two integers
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int x = 5, y = 7;
+    int result = sum(x, y);
+
+    printf("The sum of %d and %d is %d\n", x, y, result);
+
+    return 0;
+}
