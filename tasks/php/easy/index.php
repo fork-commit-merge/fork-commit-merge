c1bd64cf16ad75105068465b6f021c264bcb6eec
GitHub Action <action@github.com>
2025-09-23 12:25:44 +0000

    Revert "Merge pull request #4836 from Demagalawrence/add-php-file"
    This reverts commit 732ceea20835f837b5a1073c5f0533c7dd93b011, reversing
    changes made to 4227e29362ccefa7d5d757174792e29340aef809.

--- tasks/php/easy/index.php
+++ tasks/php/easy/index.php
@@ -2,14 +2,7 @@
 
 // PHP - Easy 1
 
-$hello = "Hello, ";
+$hello = "Hello";
 $world = "World!";
 
 // TODO: Implement rest of the string concatenation program
-
-// concatenation of a string
-$message = $hello . $world;
-
-// print the result
-
-echo $message;
