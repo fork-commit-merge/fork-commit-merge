380ea3d0fed60ffd7c89aad21e5cc5b9f58657b5
eldred16 <kubanjaelijah2037@gmail.com>
2025-09-23 23:58:53 +0300

    The program in the task description is implemented

--- tasks/c/medium/read_csv.c
+++ tasks/c/medium/read_csv.c
@@ -4,7 +4,45 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define LINE_SIZE 256
 
-int main() {
-//    TODO: Implement the program requested in the task description
+int main()
+{
+    //    TODO: Implement the program requested in the task description
+    FILE *file = fopen("data.csv", "r");
+    if (!file)
+    {
+        perror("Error opening file");
+        return 1;
+    }
+
+    char line[LINE_SIZE];
+
+    // skip header
+    if (fgets(line, sizeof(line), file) == NULL)
+    {
+        printf("Empty file.\n");
+        fclose(file);
+        return 1;
+    }
+    printf("Name\tAGe\n");
+    printf("----\t----\n");
+
+    // Read each line
+    while (fgets(line, sizeof(line), file))
+    {
+        char *name = strtok(line, ",");
+        char *ageStr = strtok(NULL, ",");
+
+        if (name && ageStr)
+        {
+
+            // Remove newline from age if present
+            ageStr[strcspn(ageStr, "\r\n")] = '\0';
+            printf("%s\%ts\n", name, ageStr);
+        }
+    }
+
+    fclose(file);
+    return 0;
 }
