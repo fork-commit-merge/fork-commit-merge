141940a0e50e59cdb27cad53bb05e4168e2d5251
eldred16 <kubanjaelijah2037@gmail.com>
2025-09-24 19:37:00 +0300

    Basic c++ program for adding two  number is created

--- tasks/c++/easy/sum.cpp
+++ tasks/c++/easy/sum.cpp
@@ -1,8 +1,21 @@
 // C++ - Easy 2
 
 // TODO: Create a Basic C++ Program to Add Two Numbers
+#include <iostream>
+using namespace std;
 
 int main()
 {
-  
+    int num1, num2, sum;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    sum - num1 + num2;
+    cout << "Sum = " << sum << endl;
+
+    return 0;
 }
