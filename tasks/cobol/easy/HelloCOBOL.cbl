48d7d3937daea3e39e7eb5cd72718c401750f89e
eldred16 <kubanjaelijah2037@gmail.com>
2025-09-25 12:35:50 +0300

    Hello cobol and addNumber function

--- tasks/cobol/easy/HelloCOBOL.cbl
+++ tasks/cobol/easy/HelloCOBOL.cbl
@@ -1,8 +1,16 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. HelloCOBOL.
-       AUTHOR. Fork, Commit, Merge
+       AUTHOR. Fork, Commit, Merge.
 
       * This is a simple program to print "Hello, COBOL!" to the console.
 
        PROCEDURE DIVISION.
       * TODO: Implement the program logic here
+       
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+
+       PROCEDURE DIVISION.
+       DISPLAY "Hello, CObol!".
+       STOP RUN.
