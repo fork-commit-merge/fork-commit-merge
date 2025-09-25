83c97a8992a507084da5fb4a7add7518b565aeb7
eldred16 <kubanjaelijah2037@gmail.com>
2025-09-25 16:33:49 +0300

    The HTTP server implemented

--- tasks/crystal/easy/server.cr
+++ tasks/crystal/easy/server.cr
@@ -3,3 +3,22 @@
 require "http/server"
 
 # TODO: Implement the HTTP server
+# create a simple HTTP server that handles requests
+server = HTTP::Server.new do |context|
+context.response.content_tupe = "text/plain"
+
+#Handle different routes
+case context.request.path
+when "/"
+context.request.print "Hello, World"
+when "/about"
+context.response.print"About page"
+else
+content.response.status = HTTP::Statu::NOT_FOUND
+content.response.print"Page not found"
+end
+end
+#Sart yhe server on 8080
+address = server.bind_tcp(8080)
+puts "Server running at http://#{address}"
+server.listen 
