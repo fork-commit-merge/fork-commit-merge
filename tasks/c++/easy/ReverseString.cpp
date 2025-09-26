929caf72deb40ed09ae624445f6eb3e6b442b666
Mohamedan Farag <120517880+Mohamedan-Farag@users.noreply.github.com>
2025-09-27 02:03:14 +0300

    Update ReverseString.cpp
    do it the revers

--- tasks/c++/easy/ReverseString.cpp
+++ tasks/c++/easy/ReverseString.cpp
@@ -2,10 +2,19 @@
 #include <string>
 
 std::string reverseString(const std::string &str) {
-    // TODO: Implement the reverseString function
+    std::string reversed;
+    for (int i = str.length() - 1; i >= 0; --i) {
+        reversed += str[i];
+    }
+    return reversed;
 }
 
 int main() {
-    // TODO: Implement the main method
+    std::cout << "Enter a string: ";
+    std::string input;
+    std::getline(std::cin, input);
+    std::string reversed = reverseString(input);
+    std::cout << "Reversed string: " << reversed << std::endl;
+    return 0;
 }
 
