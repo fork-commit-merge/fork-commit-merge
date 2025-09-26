7b527822d29105aea740009cbbbd032a998f6c39
patraarka <113913573+patraarka@users.noreply.github.com>
2025-09-26 11:09:29 +0000

    just added the textboxes to the todolist

--- tasks/react/medium/todo-app/src/App.jsx
+++ tasks/react/medium/todo-app/src/App.jsx
@@ -8,6 +8,15 @@ function App() {
     <div className="App">
       <h1>To-Do List</h1>
       {/* TODO: Add the To-Do list here */}
+      <div className='box'>
+        <div>
+          <input type='text' placeholder='Enter Title'/>
+        </div>
+        <div>
+          <input type='text' placeholder='Enter The Description'/>
+        </div>        
+      </div>
+      
     </div>
   );
 }
