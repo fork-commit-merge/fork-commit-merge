3379fd2bb2b7219f00ddcd9c45be35ffebb633e9
patraarka <113913573+patraarka@users.noreply.github.com>
2025-09-26 20:19:48 +0000

    added the delete button and edit button. all that is left is to make their functionalities.

--- tasks/react/medium/todo-app/src/Components/Items.jsx
+++ tasks/react/medium/todo-app/src/Components/Items.jsx
@@ -1,9 +1,13 @@
 function  Items({title, desc}){
     return(
         <>
-        <div className="boxingitem itemstyle">
-            <h3>{title}</h3>
-            <p>{desc}</p>
+        <div>
+            <div className="boxingitem itemstyle">
+                <h3>{title}</h3>
+                <p>{desc}</p>
+            </div>
+            <button className='but'>Edit</button>
+            <button className='but'>Delete</button>
         </div>
         </>
     )
