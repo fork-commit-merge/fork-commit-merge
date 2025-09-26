c5ecf3ceb6f014538f0adc11d629ec7b78c91f81
GitHub Action <action@github.com>
2025-09-26 05:49:21 +0000

    Revert "Merge pull request #4856 from Demagalawrence/add-counter"
    This reverts commit 7009fb82057d38b6fe8d86920b504470e6624db3, reversing
    changes made to 0a9532c3a9de0f7c523791d198d0490276b172f3.

--- tasks/solid/easy/src/App.jsx
+++ tasks/solid/easy/src/App.jsx
@@ -1,22 +1,18 @@
-
-import { createSignal } from "solid-js";
 import styles from "./App.module.css";
 
 function App() {
-   const [count, setCount] = createSignal(0);
   return (
     <main class={styles.counterContainer}>
       {/* TODO: Implement the counter state with the name of "count" and a setter named "setCount"  */}
-       
+
       {/* TODO: Display the current count state in here */}
-      <p class={styles.countNumber}>{count()}</p>
+      <p class={styles.countNumber}></p>
 
       {/* TODO: Implement the setter function for incrementing and decrementing the count state. */}
       <div class={styles.countButtonsContainer}>
         <button
           onClick={() => {
             // Implement the function to increment the counter state here.
-             setCount(count() + 1);
           }}
           class={styles.countButtons}
         >
@@ -25,7 +21,6 @@ function App() {
         <button
           onClick={() => {
             // Implement the function to decrement the counter state here.
-             setCount(count() - 1);
           }}
           class={styles.countButtons}
         >
