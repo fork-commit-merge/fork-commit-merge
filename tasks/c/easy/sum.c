14bf5b2171d9aed2f920591b455f2ee93086336f
patraarka <113913573+patraarka@users.noreply.github.com>
2025-09-27 13:47:29 +0000

    Made the desired Changes

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,5 +1,21 @@
 // C - Easy
 
 #include <stdio.h>
-
 // TODO: Implement the sum and main functions
+
+int sum(int a, int b){
+    return (a+b);
+}
+
+void main(){
+    int a=0, b=0;
+    printf("Enter Two Numbers::");
+    printf("\nEnter The First Number::--");
+    scanf("%d",&a);
+    printf("Enter The Second Number::--");
+    scanf("%d",&b);
+    printf("The Sum of the numbers is:: %d\n", sum(a,b));
+    printf("\n");
+}
+
+
