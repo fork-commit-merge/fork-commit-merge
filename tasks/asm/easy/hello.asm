54babbc1ef303a8a24599deaf9e0d78f8b0600ee
shivani <54018320+shivt-F5@users.noreply.github.com>
2025-09-28 17:50:26 +0400

    basic machine code!

--- tasks/asm/easy/hello.asm
+++ tasks/asm/easy/hello.asm
@@ -1,1 +1,17 @@
-; TODO: Implement the code requested in the issue description
+    .global _start
+
+    .data
+hello:
+    .asciz "Hello, World!"
+
+    .text
+_start:
+    movl $4, %eax
+    movl $1, %ebx
+    movl $hello, %ecx
+    movl $13, %edx
+    int $0x80
+
+    movl $1, %eax
+    xorl %ebx, %ebx
+    int $0x80
