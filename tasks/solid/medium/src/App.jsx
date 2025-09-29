1186985ecc030d7041cf554c8c9845303ffe5705
GitHub Action <action@github.com>
2025-09-29 10:24:02 +0000

    Revert "Merge pull request #4866 from Demagalawrence/Quote-Generator-App"
    This reverts commit f12a0556b213093cb8f51242de6cc52f21b906a3, reversing
    changes made to 7b957abecf9c90d8e41a8b1117ae2776d9f5ab64.

--- tasks/solid/medium/src/App.jsx
+++ tasks/solid/medium/src/App.jsx
@@ -1,4 +1,3 @@
-import { createResource } from "solid-js";
 import styles from "./App.module.css";
 
 function App() {
@@ -9,16 +8,11 @@ function App() {
   // { slip: { id: 123, advice: "This is an example advice." } }
   const fetchQuote = async () => {
     // TODO: Implement the fetchQuote function to retrieve an advice quote.
-    const res = await fetch(API_URL);
-    if (!res.ok) throw new Error("Failed to fetch quote");
-    const data = await res.json();
-    return data.slip.advice;
   };
 
   // TODO: Implement a signal function to manage the fetchQuote data.
   // TIP: You can use Solid.js's createResource utility function.
   // Documentation: https://docs.solidjs.com/guides/fetching-data
-  const [quote, { refetch }] = createResource(fetchQuote);
 
   return (
     <main class={styles.quoteContainer}>
@@ -28,13 +22,10 @@ function App() {
       - Loading: <p class={styles.loadingText}>Fetching quote...</p>
       - Error: <p class={styles.errorText}>Failed to fetch the quote.</p>
       - Success: <p class={styles.quotes}>Quote appears here.</p> */}
-      {quote.loading && <p class={styles.loadingText}>Fetching quote...</p>}
-      {quote.error && <p class={styles.errorText}>Failed to fetch the quote.</p>}
-      {quote() && <p class={styles.quotes}>{quote()}</p>}
+
       <button
         onClick={() => {
           // Implement the refetch function here to fetch new quotes.
-          refetch();
         }}
         class={styles.newQuoteButton}
       >
