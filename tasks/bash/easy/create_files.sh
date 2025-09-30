ca676416e5cb183d286888cb4e8bbd3c0cdb9e83
GitHub Action <action@github.com>
2025-09-30 05:31:17 +0000

    Revert "Merge pull request #4872 from Demagalawrence/add-bash-file"
    This reverts commit 4673500918b6c5f23693486e531b6a6e587343b1, reversing
    changes made to d53d17dc632cfd9ab37c346d69c5f040eaacf2f6.

--- tasks/bash/easy/create_files.sh
+++ tasks/bash/easy/create_files.sh
@@ -3,18 +3,3 @@
 # Bash - Easy
 
 # TODO: Write the Bash script requested in the issue description
-
-# Navigate into the "files" directory
-cd files || { echo "Directory 'files' not found!"; exit 1; }
-
-# Create three files and write "Hello Shell!" into each
-for i in 1 2 3; do
-  echo "Hello Shell!" > "file$i.txt"
-done
-
-# Print contents of each file to the console
-for i in 1 2 3; do
-  echo "Contents of file$i.txt:"
-  cat "file$i.txt"
-  echo "---------------------"
-done
