d0951a5e039776f1808b05435cc5c81fcc08d7a7
GitHub Action <action@github.com>
2025-09-30 16:44:54 +0000

    Revert "Merge pull request #4878 from eldred16/elijah"
    This reverts commit cca67736f1cedf5da7f1a4e0cd8c01da2818f43a, reversing
    changes made to 1bb5f7bfe25ad21ef8197276b1ed3d0020112ebd.

--- tasks/d/easy/reverseString.d
+++ tasks/d/easy/reverseString.d
@@ -1,18 +1,7 @@
 // D - Easy
 
 import std.stdio;
-import std.string;
 
 void main() {
     // TODO: Implement program to reverse a string
-    writeln("Enter a string to reverse:");
-    string input = readln(). strip();  //Read input and remove newline
-
-    //Reverse the string
-    string reversed = input.reverse;
-    writeln("Reversed string : ", reversed);
-
-
-
-
 }
