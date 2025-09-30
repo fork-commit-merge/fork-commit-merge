bab629e1c1376fa77c3a99358b44f610caa8a0ed
GitHub Action <action@github.com>
2025-09-30 15:00:08 +0000

    Revert "Merge pull request #4875 from kannna5296/fix-issue"
    This reverts commit 31fe93ecc7ffa24f25bfde40941fda0b2287fa0f, reversing
    changes made to 23332259d4a2a70d664389027bccf1899a546d2e.

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,10 +1,5 @@
 def bubble_sort(arr):
-    for i in range(len(arr)):
-        for j in range(len(arr) - (i + 1)):
-            print(i,j)
-            if (arr[j] > arr[j+1]):
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-            
+    # TODO: Implement the bubble sort algorithm
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
