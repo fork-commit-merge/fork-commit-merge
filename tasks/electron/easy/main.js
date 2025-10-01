6227e1019a9f76b5115b5161b7f93bd64471ed12
eldred16 <kubanjaelijah2037@gmail.com>
2025-10-01 18:19:42 +0300

    Updated main.js in electron easy task

--- tasks/electron/easy/main.js
+++ tasks/electron/easy/main.js
@@ -4,6 +4,20 @@ const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
     // TODO: Create a new BrowserWindow that displays the index.html file
+    const mainwindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    });
+
+    //Load the index.html file
+    mainwindow.loadfile("index.html");
+
+    //Open Devtools in development(optional)
+    //mainWindow.webContents.openDevTools();
 }
 
 app.on("ready", createWindow);
