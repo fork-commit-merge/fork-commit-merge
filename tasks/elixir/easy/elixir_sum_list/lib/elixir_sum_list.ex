146a2ec5749b206c7c6f284b571e120ba1dd943d
GitHub Action <action@github.com>
2025-10-01 16:10:48 +0000

    Revert "Merge pull request #5020 from eldred16/elly"
    This reverts commit 504d5e90324e86416b0bb87c2fda5585ad98965f, reversing
    changes made to a084fc30cb47d41911fd4ee5c1185f7c13e9afd1.

--- tasks/elixir/easy/elixir_sum_list/lib/elixir_sum_list.ex
+++ tasks/elixir/easy/elixir_sum_list/lib/elixir_sum_list.ex
@@ -2,12 +2,4 @@
 
 defmodule ElixirSumList do
   # TODO: Implement a Basic Elixir Function to Sum a List of Numbers
-  #function to sum a list of Numbers
-  def sum_list(numbers) do
-    Enum.sum(numbers)
-  end
 end
-
-# Example usage:
-IO.puts(ElixirSumList.sum_list([1,2,3,4,5]))
-# output: 15
