8d4b25baf27c283d3556ac810952c2ab944c0870
GitHub Action <action@github.com>
2025-10-02 09:53:47 +0000

    Revert "Merge pull request #5022 from Demagalawrence/add-vue-todo-list"
    This reverts commit 4d5ec94753fc11b0cab2f6d4064e725b2f7bc410, reversing
    changes made to fedc14297e6fa366917062b3cbd7e8d126428483.

--- tasks/vue/easy/todo-list/src/components/TodoList.vue
+++ tasks/vue/easy/todo-list/src/components/TodoList.vue
@@ -1,68 +1,13 @@
 <template>
     <!-- TODO: Implement the Vue component code here -->
-     <div class="todo-list">
-    <h1>My Todo List</h1>
-
-    <!-- Input field to add new tasks -->
-    <input
-      v-model="newTask"
-      @keyup.enter="addTask"
-      placeholder="Add a new task"
-    />
-    <button @click="addTask">Add</button>
-
-    <!-- Task list -->
-    <ul>
-      <li v-for="(task, index) in tasks" :key="index">
-        <input
-          type="checkbox"
-          v-model="task.completed"
-        />
-        <span :style="{ textDecoration: task.completed ? 'line-through' : 'none' }">
-          {{ task.text }}
-        </span>
-        <button @click="removeTask(index)">Remove</button>
-      </li>
-    </ul>
-  </div>
 </template>
 
 <script>
 export default {
     // TODO: Implement the JavaScript code here
-    name: "TodoList",
-  data() {
-    return {
-      newTask: "",
-      tasks: [],
-    };
-  },
-  methods: {
-    addTask() {
-      if (this.newTask.trim() !== "") {
-        this.tasks.push({ text: this.newTask, completed: false });
-        this.newTask = "";
-      }
-    },
-    removeTask(index) {
-      this.tasks.splice(index, 1);
-    },
-  },
 };
 </script>
 
 <style scoped>
 /* TODO: Implement the CSS code here */
-.todo-list {
-  max-width: 400px;
-  margin: auto;
-  padding: 20px;
-}
-input[type="text"] {
-  margin-right: 10px;
-  padding: 5px;
-}
-button {
-  margin-left: 5px;
-}
 </style>
