54822ab4fd466c4a1e86c3af33e1ccbecd86de66
eldred16 <kubanjaelijah2037@gmail.com>
2025-10-02 16:04:16 +0300

    Issue task implemented

--- tasks/flask/easy/app.py
+++ tasks/flask/easy/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 
-app = Flask(__name)
+app = Flask(__name__)
 
 # TODO: Implement the task requested in the issue description
+# Root route
+@app.route("/")
+def home():
+    return "Hello, Flask!"
 
 if __name__ == "__main__":
     app.run(debug=True)
