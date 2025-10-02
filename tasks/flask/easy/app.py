f4cf014d23b83012485b7514bdb02e761f65a258
GitHub Action <action@github.com>
2025-10-02 14:22:28 +0000

    Revert "Merge pull request #5028 from eldred16/kubanja"
    This reverts commit 761cb5431bb9ae5bdeb23146784b28806291169e, reversing
    changes made to f00be49ea69aa12a72d1273fe80850f1a133e188.

--- tasks/flask/easy/app.py
+++ tasks/flask/easy/app.py
@@ -1,12 +1,8 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name)
 
 # TODO: Implement the task requested in the issue description
-# Root route
-@app.route("/")
-def home():
-    return "Hello, Flask!"
 
 if __name__ == "__main__":
     app.run(debug=True)
