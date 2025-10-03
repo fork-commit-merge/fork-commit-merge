7d2ec90da0c556435617a083533a65204c0d0bda
eldred16 <kubanjaelijah2037@gmail.com>
2025-10-03 22:38:58 +0300

    A simple kotlin program created

--- tasks/kotlin/easy/GreetingApp.kt
+++ tasks/kotlin/easy/GreetingApp.kt
@@ -2,4 +2,14 @@
 
 fun main() {
     // TODO: Create a Simple Kotlin Program to Print User's Input
+    print("Enter your name: ")
+    val name = readLine()
+
+    print("Enter your age: ")
+    val age = readLine()
+
+    println("\n--- User Information ---")
+    println("Nmae: $name")
+    println("Age: $age")
+    
 }
