6fda13373d6282a2af2f04814c5511cd2f78feb7
eldred16 <kubanjaelijah2037@gmail.com>
2025-10-03 23:05:20 +0300

    Factorial function

--- tasks/lisp/easy/factorial.lisp
+++ tasks/lisp/easy/factorial.lisp
@@ -2,4 +2,10 @@
 
 (defun factorial (n)
   ;; TODO: Implement the factorial function
+  (defun factorial(n)
+  ;; Implement the factorial function
+  (if (<= n 1))
+  1
+  (* n (factorial (- n 1)))
+  )
 )
