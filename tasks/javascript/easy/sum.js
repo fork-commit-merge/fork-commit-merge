f3f3be22cc96a711ed86c3c8d1e62cef9e646664
eldred16 <kubanjaelijah2037@gmail.com>
2025-10-03 19:26:03 +0300

    THe fucntion that returns to sum two numbers

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,3 +1,12 @@
 // JavaScript - Easy
 
 // TODO: Write a function that returns the sum of two numbers
+//function to return the sum of two numbers
+function sum(a, b) {
+    return a + b;
+
+}
+
+//Example usage
+console.log(sum(5, 3));// output: 8
+console.log(sum(10, 20)); // output: 30
