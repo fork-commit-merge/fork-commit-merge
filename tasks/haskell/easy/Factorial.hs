bfeaa054d6e6aae86e08c8811ae7e131657dc9c1
eldred16 <kubanjaelijah2037@gmail.com>
2025-10-03 17:21:22 +0300

    factorial implemented

--- tasks/haskell/easy/Factorial.hs
+++ tasks/haskell/easy/Factorial.hs
@@ -3,3 +3,16 @@
 main :: IO ()
 main = do
     -- TODO: Implement a Haskell program that calculates the factorial of a number
+putStrLn "Enter a number to calculate its factorial: "
+input <- getLine
+let n = read input :: Integer
+let  result = factorial n
+putStrLn $ "The factoruial of " ++ shown n ++ "is" ++ show result
+
+--USE PRODUCT AND LIST GENERATION
+factorial':: Integer -> Integer
+factorial' n = product [1..n]
+
+--using foldl
+factorial'' :: Integer -> Integer
+factorial'' n = foldl (*) 1 [1..n]
