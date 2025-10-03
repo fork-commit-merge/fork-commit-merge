90e3a3ad6a80d69e13565bbe432dedcb90b26bf4
eldred16 <kubanjaelijah2037@gmail.com>
2025-10-03 19:05:24 +0300

    GreetinApp implemented in java

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,8 +1,14 @@
 // Java - Easy
 
+import java.util.Scanner;
+
 public class GreetingApp {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "! WElcome to greetingsApp.");
+        scanner.close();
     }
 }
