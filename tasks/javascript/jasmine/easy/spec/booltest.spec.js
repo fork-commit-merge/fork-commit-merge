8afbfa7240f28a9b113b2f3b2dce35d6e34337ff
GitHub Action <action@github.com>
2025-10-04 04:59:59 +0000

    Revert "Merge pull request #5036 from eldred16/kubanja"
    This reverts commit 42b9c6177a8b4f3a00c12bcd4b0fe25f688a0373, reversing
    changes made to 1268e4ed5bcdc08c0fc338a5f570a8579c73ccfe.

--- tasks/javascript/jasmine/easy/spec/booltest.spec.js
+++ tasks/javascript/jasmine/easy/spec/booltest.spec.js
@@ -3,7 +3,5 @@
 describe("Jasmine - Easy - Boolean Test", function () {
     it("should pass this test", function () {
         // TODO: Add `expect` and `toBe` statements to pass this test
-        const isTrue = true; //example boolean value
-        expect(isTrue).toBe(true);
     });
 });
