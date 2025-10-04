45446509705bc9b1b7c917ae81dc40f49e565528
Demagalawrence <demagalawrence8@gmail.com>
2025-10-04 12:12:14 +0300

    Implemented add-numbers function in Clojure

--- tasks/clojure/easy/add-numbers/src/add_numbers/core.clj
+++ tasks/clojure/easy/add-numbers/src/add_numbers/core.clj
@@ -1,5 +1,7 @@
 (ns add-numbers.core)
 
-(defn add-numbers
+(defn add-numbers [x y]
   ;; TODO: Implement the Number addition function here
+   (println (str "The sum of " x " and " y " is: " (+ x y)))
 )
+ 
