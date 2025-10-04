fb45c1e5b46b28c705e91e71a79bfb1a1c637097
GitHub Action <action@github.com>
2025-10-04 05:11:06 +0000

    Revert "Merge pull request #5043 from eldred16/try"
    This reverts commit 0f344289849afc0e9e6f9c8a282bfbe44b98a3dd, reversing
    changes made to 133b40eb7284026d64f387dc6beb26cec195b7cd.

--- tasks/lisp/easy/factorial.lisp
+++ tasks/lisp/easy/factorial.lisp
@@ -2,10 +2,4 @@
 
 (defun factorial (n)
   ;; TODO: Implement the factorial function
-  (defun factorial(n)
-  ;; Implement the factorial function
-  (if (<= n 1))
-  1
-  (* n (factorial (- n 1)))
-  )
 )
