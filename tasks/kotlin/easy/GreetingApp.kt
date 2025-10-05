70d7038f7b7863cdda51763d1af389f08064f61e
GitHub Action <action@github.com>
2025-10-05 08:24:11 +0000

    Revert "Merge pull request #5055 from Demagalawrence/feature/kotlin-greeting"
    This reverts commit f1aeea9f338fe0ff01e5e77d5a83621b745b0df6, reversing
    changes made to 017c80d59335bceb6ed8d21bb59573288d0d07bc.

--- tasks/kotlin/easy/GreetingApp.kt
+++ tasks/kotlin/easy/GreetingApp.kt
@@ -2,12 +2,4 @@
 
 fun main() {
     // TODO: Create a Simple Kotlin Program to Print User's Input
-    print("Enter your name: ")
-    val name = readLine()
-
-    if (name.isNullOrBlank()) {
-        println("Error: Name cannot be empty.")
-    } else {
-        println("Hello, $name!")
-    }
 }
