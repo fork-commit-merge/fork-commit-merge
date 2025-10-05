620f29975a9a12cd5f47b0b959336fe176b9fe14
Demagalawrence <demagalawrence8@gmail.com>
2025-10-05 09:57:56 +0300

    Add Kotlin GreetingApp to print user input

--- tasks/kotlin/easy/GreetingApp.kt
+++ tasks/kotlin/easy/GreetingApp.kt
@@ -2,4 +2,12 @@
 
 fun main() {
     // TODO: Create a Simple Kotlin Program to Print User's Input
+    print("Enter your name: ")
+    val name = readLine()
+
+    if (name.isNullOrBlank()) {
+        println("Error: Name cannot be empty.")
+    } else {
+        println("Hello, $name!")
+    }
 }
