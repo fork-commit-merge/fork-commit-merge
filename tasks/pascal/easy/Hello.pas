d3e4b06ec70d37b950efab3ad939ff43277461fc
eldred16 <kubanjaelijah2037@gmail.com>
2025-10-06 19:29:19 +0300

    hello pascal added

--- tasks/pascal/easy/Hello.pas
+++ tasks/pascal/easy/Hello.pas
@@ -1,3 +1,8 @@
 // Pascal - Easy
 
 // TODO: Create a simple Pascal program that prints "Hello, Pascal!" to the console when executed
+program HelloPascal;
+
+begin
+   writeln('Hello, Pascal!');
+end.
