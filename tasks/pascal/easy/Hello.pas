e51408ececcfa26976879f87899e0472dcde8491
GitHub Action <action@github.com>
2025-10-07 03:36:09 +0000

    Revert "Merge pull request #5063 from eldred16/david"
    This reverts commit 3f230673c8e7ac6b243b136476e79d63d93cf906, reversing
    changes made to 9c1b9c91c77bf8f7d2d51daf4b75329e9b5b0594.

--- tasks/pascal/easy/Hello.pas
+++ tasks/pascal/easy/Hello.pas
@@ -1,8 +1,3 @@
 // Pascal - Easy
 
 // TODO: Create a simple Pascal program that prints "Hello, Pascal!" to the console when executed
-program HelloPascal;
-
-begin
-   writeln('Hello, Pascal!');
-end.
