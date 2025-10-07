3d0316db252a162d1c9e5e0f05ed595ecffcba52
GitHub Action <action@github.com>
2025-10-07 09:34:52 +0000

    Revert "Merge pull request #5066 from Demagalawrence/matlab-addition"
    This reverts commit 67fe958d200d1d00f545d12746dae24b2241e074, reversing
    changes made to d89b14d6b1d163ad5f9c10fe2d7215e546b9bf99.

--- tasks/matlab/easy/addition.m
+++ tasks/matlab/easy/addition.m
@@ -1,12 +1,3 @@
 % MATLAB - Easy
 
 % TODO: Create a MATLAB script that adds two numbers together and prints the result
-% Define two numbers
-a = 5;
-b = 7;
-
-% Calculate the sum
-sum_ab = a + b;
-
-% Display the result
-fprintf('The sum of %d and %d is %d\n', a, b, sum_ab);
