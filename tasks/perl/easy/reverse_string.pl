7956248602556388b2e087980d780ad161bc625a
eldred16 <kubanjaelijah2037@gmail.com>
2025-10-08 17:45:18 +0300

    i've created a perl script.

--- tasks/perl/easy/reverse_string.pl
+++ tasks/perl/easy/reverse_string.pl
@@ -5,3 +5,12 @@ use strict;
 use warnings;
 
 # TODO: Write a Perl script that accepts a string and prints it in reversed order
+print "Enter a string: ";
+my $input = <STDIN>;
+chomp($input);
+
+#Reverse string
+my $reversed = reverse $input;
+
+# Print reversed string
+    print "Reversed string: $reversed\n";
