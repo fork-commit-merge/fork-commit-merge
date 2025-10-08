dd22ddccdc79e60995594fa8e4949b65a89b3387
GitHub Action <action@github.com>
2025-10-08 06:03:47 +0000

    Revert "Merge pull request #5068 from Demagalawrence/crystal-http-server"
    This reverts commit 31545fa2ccfdeb6dd3fcb8470c6718c0d69a227d, reversing
    changes made to 0cad5b26ad40d7f6452dd0bd04d269a69fcce405.

--- tasks/crystal/easy/server.cr
+++ tasks/crystal/easy/server.cr
@@ -3,18 +3,3 @@
 require "http/server"
 
 # TODO: Implement the HTTP server
-# Create a new server instance
-server = HTTP::Server.new do |context|
-  # Respond to every request with "Hello, world!"
-  context.response.content_type = "text/plain"
-  context.response.print "Hello, world!"
-end
-
-# Define the listening address and port
-address = server.bind_tcp "127.0.0.1", 3000
-
-# Display where the server is running
-puts "Listening on http://#{address}"
-
-# Start the server
-server.listen
