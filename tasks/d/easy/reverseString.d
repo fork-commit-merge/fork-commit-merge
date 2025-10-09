2207c870561e481eb2ded9fbf6c81136d7676230
GitHub Action <action@github.com>
2025-10-09 06:34:23 +0000

    Revert "Merge pull request #5072 from Demagalawrence/d-reverse-string"
    This reverts commit 1dcf2d099e332e70d8ea8bead4c881ae6e47d0ab, reversing
    changes made to 94539046507e999b6c339a7b00adc2a691b3c6fc.

--- tasks/d/easy/reverseString.d
+++ tasks/d/easy/reverseString.d
@@ -1,14 +1,7 @@
 // D - Easy
 
 import std.stdio;
-import std.string;
 
 void main() {
     // TODO: Implement program to reverse a string
-
-    string original = "Hello, World!";
-    string reversed = original.retro.joiner.array;
-
-    writeln("Original String: ", original);
-    writeln("Reversed String: ", reversed);
 }
