e523dd76b8b453a8bca9649b686498610b7f1803
GitHub Action <action@github.com>
2025-10-09 18:53:41 +0000

    Revert "Merge pull request #5075 from eldred16/yes"
    This reverts commit 225db623cb13dc0d8cf43a24a26bc8a655d81615, reversing
    changes made to ec271051f94fea103a8daae018cabe39df2b1a18.

--- tasks/swift/easy/fibonacci.swift
+++ tasks/swift/easy/fibonacci.swift
@@ -4,11 +4,6 @@ import Foundation
 
 func fibonacci(n: Int) -> Int {
     // TODO: Implement the fibonacci sequence function
-    if n <= 1 {
-        return n
-    } else {
-        return fibonacci(n: n - 1) + fibonacci(n: n - 2)
-    }
 }
 
 // Test cases (Don't touch!)
