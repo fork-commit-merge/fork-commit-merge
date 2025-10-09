ec271051f94fea103a8daae018cabe39df2b1a18
GitHub Action <action@github.com>
2025-10-09 18:53:07 +0000

    Revert "Merge pull request #5074 from eldred16/law"
    This reverts commit 6d7866d38bb5ee4bd8c135ec74968b261d93cf01, reversing
    changes made to 6a8a29a64edbfb7642d85a7d7a02bdd71838ad53.

--- tasks/rust/easy/fibonacci.rs
+++ tasks/rust/easy/fibonacci.rs
@@ -3,10 +3,3 @@
 use std::io;
 
 // TODO: Implement fibonacci sequence in Rust
-fn fibonacci(n: u32) -> u32 {
-    if n <= 1 {
-        n
-    } else {
-        fibonacci(n - 1) + fibonacci(n - 2)
-    }
-}
--- tasks/svelte/easy/hello/src/routes/+page.svelte
+++ tasks/svelte/easy/hello/src/routes/+page.svelte
@@ -1,5 +1,3 @@
 <!-- Svelte - Easy -->
 
 <!-- TODO: Add a H1 tag with the text "Hello, Svelte!" -->
-
-<h1>Hello, Svelte!</h1>
