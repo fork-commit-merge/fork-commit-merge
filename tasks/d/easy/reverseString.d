53e31db1d7b9cf48432ae8b21327c1c489364205
Demagalawrence <demagalawrence8@gmail.com>
2025-10-09 08:36:22 +0300

    Implement D program to reverse a string

--- tasks/d/easy/reverseString.d
+++ tasks/d/easy/reverseString.d
@@ -1,7 +1,14 @@
 // D - Easy
 
 import std.stdio;
+import std.string;
 
 void main() {
     // TODO: Implement program to reverse a string
+
+    string original = "Hello, World!";
+    string reversed = original.retro.joiner.array;
+
+    writeln("Original String: ", original);
+    writeln("Reversed String: ", reversed);
 }
