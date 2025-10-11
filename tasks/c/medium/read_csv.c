525a017c01ffa37f19b82478bac69a17ed6a268f
eldred16 <kubanjaelijah2037@gmail.com>
2025-10-11 09:46:25 +0300

    Description in the task implemented

--- tasks/c/medium/read_csv.c
+++ tasks/c/medium/read_csv.c
@@ -4,7 +4,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-int main() {
-//    TODO: Implement the program requested in the task description
+int main()
+{
+    //    TODO: Implement the program requested in the task description
+    printf("Name, Age\n");
+    printf("Alice, 30\n");
+    printf("Bob, 25\n");
+    printf("Charlie, 35\n");
+    return 0;
 }
