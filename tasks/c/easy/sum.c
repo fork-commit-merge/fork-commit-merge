94ffdb40b9f39cb4b126be60c79cd12485319ae8
eldred16 <kubanjaelijah2037@gmail.com>
2025-10-11 09:35:45 +0300

    task added

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -3,3 +3,14 @@
 #include <stdio.h>
 
 // TODO: Implement the sum and main functions
+int sum(int a, int b)
+{
+    return a + b;
+}
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d\n", sum(a, b));
+    return 0;
+}
