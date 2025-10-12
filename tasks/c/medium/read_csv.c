5cda383013473aa219fab4f45ddf590bf81dffe7
GitHub Action <action@github.com>
2025-10-12 07:56:02 +0000

    Revert "Merge pull request #5084 from eldred16/patricia"
    This reverts commit 9bf0a8b722e389ca9b28a7c1ceeeb4c204720b15, reversing
    changes made to 66b03f4f83664200c52d6e219093d210892e0913.

--- tasks/c/medium/read_csv.c
+++ tasks/c/medium/read_csv.c
@@ -4,12 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main()
-{
-    //    TODO: Implement the program requested in the task description
-    printf("Name, Age\n");
-    printf("Alice, 30\n");
-    printf("Bob, 25\n");
-    printf("Charlie, 35\n");
-    return 0;
+
+int main() {
+//    TODO: Implement the program requested in the task description
 }
