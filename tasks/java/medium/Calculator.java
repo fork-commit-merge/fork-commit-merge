f687a70871a4413b2e44d2d45921f428f810578f
Sebastian <seb.falter@gmail.com>
2025-10-13 19:45:27 +0200

    Solving task #4909

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -14,4 +14,23 @@ public class Calculator {
         System.out.println("Multiplication: " + calc.multiply(5, 3));
         System.out.println("Division: " + calc.divide(5, 3));
     }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+        return (double) a / b;
+    }
 }
