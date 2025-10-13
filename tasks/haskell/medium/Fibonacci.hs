aee7bdca67a6fe4e1556cc90684791d7a977fd1d
GitHub Action <action@github.com>
2025-10-13 04:34:00 +0000

    Revert "Merge pull request #5088 from Demagalawrence/Fibonacci-hs"
    This reverts commit 0c84ee78cf5c9bb7db1364bc0f724c995b4d3634, reversing
    changes made to 00da7d9a7b3a17aab930f63b84d0f0bc2ec730f9.

--- tasks/haskell/medium/Fibonacci.hs
+++ tasks/haskell/medium/Fibonacci.hs
@@ -1,18 +1,3 @@
 -- Haskell - Medium
 
 -- TODO: Implement a Haskell program that calculates the Fibonacci sequence up to a given number
-
--- Fibonacci.hs
--- Program to generate Fibonacci numbers up to a limit
-
-fibonacci :: Integer -> [Integer]
-fibonacci limit = takeWhile (<= limit) fibs
-  where fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
-
-main :: IO ()
-main = do
-    putStrLn "Enter the limit for Fibonacci numbers:"
-    input <- getLine
-    let limit = read input :: Integer
-    putStrLn "Fibonacci numbers:"
-    print (fibonacci limit)
