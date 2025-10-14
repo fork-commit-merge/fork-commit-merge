167e800bac3e824f31cfcde784257060d587d5d2
GitHub Action <action@github.com>
2025-10-14 05:44:31 +0000

    Revert "Merge pull request #5094 from PsydoV2/fix-issue#4898"
    This reverts commit 36098a3d3fcb1e03fe1da80db1cafa9ea6b6a3d4, reversing
    changes made to 320f4aee57d4967b9abba8d37ae1ca41fdd5c41b.

--- tasks/c#/easy/HelloUser/Program.cs
+++ tasks/c#/easy/HelloUser/Program.cs
@@ -1,24 +1,3 @@
 ﻿// C# - Easy
 
 // TODO: Implement the C# program requested in the Issue description
-using System;
-
-class Program
-{
-    static void Main()
-    {
-        Console.Write("Enter your name: ");
-        string? name = Console.ReadLine()?.Trim();
-
-        if (string.IsNullOrEmpty(name))
-        {
-            Console.WriteLine("That's not a valid name!");
-        }
-        else
-        {
-            Console.WriteLine($"Hello, {name}!");
-        }
-
-        Console.ResetColor();
-    }
-}
