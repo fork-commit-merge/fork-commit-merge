a70e649f3817b6a2488b96f204f64af430ba089e
GitHub Action <action@github.com>
2025-10-14 05:38:54 +0000

    Revert "Merge pull request #5090 from eldred16/dema"
    This reverts commit 0b2b1fbed7e459bedd2f7290fdf36d6dbcba141b, reversing
    changes made to 5e14414b8772167b6501c00a018bbdb115e91713.

--- tasks/c++/easy/ReverseString.cpp
+++ tasks/c++/easy/ReverseString.cpp
@@ -1,23 +1,11 @@
 #include <iostream>
 #include <string>
 
-std::string reverseString(const std::string &str)
-{
+std::string reverseString(const std::string &str) {
     // TODO: Implement the reverseString function
-    std::string reversed;
-    for (int i = str.length() - 1; i >= 0; --i)
-    {
-        reversed += str[i];
-    }
 }
 
-int main()
-{
+int main() {
     // TODO: Implement the main method
-    std::string input;
-    std::cout << "Enter a string: ";
-    std::getline(std::cin, input);
-    std::string reversed = reverseString(input);
-    std::cout << "Reversed string: " << reversed << std::endl;
-    return 0;
 }
+
