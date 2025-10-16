39ccda75d652e7a2dc0044df2e8d4cfd575e5b47
alexli <89558523@qq.com>
2025-10-16 18:52:41 +0200

    Fixed issue

--- tasks/java/hard/Frequency.java
+++ tasks/java/hard/Frequency.java
@@ -5,5 +5,18 @@ public class Frequency {
 
     public static void main(String[] args) {
         // TODO: Given an array of integers, write a Java program to find the frequency of each element using a Map
+        Map<Integer, Integer> frequency = new HashMap<>();
+
+        for (String s : args) {
+            int num = Integer.parseInt(s);
+            Integer count = frequency.get(num);
+            if (count == null) {
+                frequency.put(num, 1);
+            } else {
+                frequency.put(num, count + 1);
+            }
+        }
+
+        System.out.println(frequency);
     }
 }
