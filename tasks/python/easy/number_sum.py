58ac54504659a6895fd4284346dc504160cb7081
GitHub Action <action@github.com>
2025-10-17 11:43:01 +0000

    Revert "Merge pull request #5102 from nabayousuf27/main"
    This reverts commit b80cfa9f3ba3ad141928548f10aa7eb078297973, reversing
    changes made to aba209c20c88b2fb3c6d5cd53130e33de5c89093.

--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,12 +1,3 @@
 # Python - Easy
 
 # TODO: Write a function that returns the sum of two numbers
-# function
-def add_numbers(a, b):
-    return a + b
-
-#call
-result = add_numbers(5, 3)
-
-#print
-print("The sum is:", result)
