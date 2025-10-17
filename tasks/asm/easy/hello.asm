20f39fbdf8736c195e46e6645f22e8785ad6f216
eldred16 <kubanjaelijah2037@gmail.com>
2025-10-17 18:54:58 +0300

    The code request added

--- tasks/asm/easy/hello.asm
+++ tasks/asm/easy/hello.asm
@@ -1,1 +1,8 @@
 ; TODO: Implement the code requested in the issue description
+--- IGNORE ---
+section .data
+    hello db 'Hello, World!',0  ; null-terminated string        
+section .text
+    global _start   
+
+            
