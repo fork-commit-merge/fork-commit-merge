c98e623800b7d8cf8946ff27ca85d739c75404b2
GitHub Action <action@github.com>
2025-10-18 14:35:35 +0000

    Revert "Merge pull request #5107 from arch92/fix-issue#4898"
    This reverts commit 303a68fcf0fd825a4b56278d97d67c44c20bf5af, reversing
    changes made to 6dced6d563d8782eedb15800e94753596c9122a4.

--- tasks/c#/easy/HelloUser/Program.cs
+++ tasks/c#/easy/HelloUser/Program.cs
@@ -1,11 +1,3 @@
-﻿namespace HelloUser;
+﻿// C# - Easy
 
-public class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Please enter your name: ");
-        string userName = Console.ReadLine();
-        Console.WriteLine("Hello, " + userName + "!");
-    }
-}
+// TODO: Implement the C# program requested in the Issue description
