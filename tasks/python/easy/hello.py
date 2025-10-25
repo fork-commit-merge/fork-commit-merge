72226a699d90444f986df7dbb83533b2f1c414c3
kubanjaelijaheldred <kubanjaelijah2037@gmail.com>
2025-10-25 18:38:07 +0300

    Complete Python hello task - implement hello_python function

--- tasks/python/easy/hello.py
+++ tasks/python/easy/hello.py
@@ -1,3 +1,8 @@
 # Python - Easy
 
-# TODO: Write a function that prints "Hello, Python!" to the console
+def hello_python():
+    """Function that prints 'Hello, Python!' to the console"""
+    print("Hello, Python!")
+
+if __name__ == "__main__":
+    hello_python()
