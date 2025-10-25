e426315807c25fff562c448d019cb5842b42f4a2
GitHub Action <action@github.com>
2025-10-25 20:55:32 +0000

    Revert "Merge pull request #5114 from KubanjaElijahEldred/task-java-greeting"
    This reverts commit 62cc1fdda230f4b64637ac003c65c2365668559a, reversing
    changes made to 9a4fd373e36115a45bc5c3f96aa94bf505dadfc6.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -3,12 +3,6 @@
 public class GreetingApp {
 
     public static void main(String[] args) {
-        // Print a greeting message
-        System.out.println("Hello, Java!");
-        
-        // If command line arguments are provided, greet the user
-        if (args.length > 0) {
-            System.out.println("Hello, " + args[0] + "!");
-        }
+
     }
 }
