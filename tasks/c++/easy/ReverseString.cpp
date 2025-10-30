ccfd7f4131a0bf763451b1233bc91cbf37efa16b
kubanjaelijaheldred <kubanjaelijah2037@gmail.com>
2025-10-30 10:37:25 +0300

    Fix-CPP-reverse-string-task

--- tasks/c++/easy/ReverseString.cpp
+++ tasks/c++/easy/ReverseString.cpp
@@ -2,10 +2,23 @@
 #include <string>
 
 std::string reverseString(const std::string &str) {
-    // TODO: Implement the reverseString function
+    std::string reversed = "";
+    for (int i = str.length() - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
 }
 
 int main() {
-    // TODO: Implement the main method
+    std::string input;
+    
+    std::cout << "Enter a string: ";
+    std::cin >> input;
+    
+    std::string reversed = reverseString(input);
+    
+    std::cout << "Reversed string: " << reversed << std::endl;
+    
+    return 0;
 }
 
