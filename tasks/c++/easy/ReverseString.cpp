76c1feaf3d9a9f9aeb8399268f14076a09dc8c11
GitHub Action <action@github.com>
2025-10-30 11:35:53 +0000

    Revert "Merge pull request #5126 from KubanjaElijahEldred/fix-cpp-reverse-string"
    This reverts commit 990a0b7c57945f09beeb2446f8a023f7ac60465d, reversing
    changes made to d9c70f48ce6502d332185700b15f02e47d9c9a3e.

--- tasks/c++/easy/ReverseString.cpp
+++ tasks/c++/easy/ReverseString.cpp
@@ -2,23 +2,10 @@
 #include <string>
 
 std::string reverseString(const std::string &str) {
-    std::string reversed = "";
-    for (int i = str.length() - 1; i >= 0; i--) {
-        reversed += str[i];
-    }
-    return reversed;
+    // TODO: Implement the reverseString function
 }
 
 int main() {
-    std::string input;
-    
-    std::cout << "Enter a string: ";
-    std::cin >> input;
-    
-    std::string reversed = reverseString(input);
-    
-    std::cout << "Reversed string: " << reversed << std::endl;
-    
-    return 0;
+    // TODO: Implement the main method
 }
 
