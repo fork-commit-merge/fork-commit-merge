4c8b17e8b2b69b215d9b663936daef118b12cf3d
GitHub Action <action@github.com>
2025-10-30 05:39:23 +0000

    Revert "Merge pull request #5121 from KubanjaElijahEldred/fix-javascript-sum"
    This reverts commit f9dbb2863c3d9ddc7016224e02a7f1f6b7f4d1e9, reversing
    changes made to e2d97cc00ece8d812cd00e51317e66494547bd01.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,18 +1,3 @@
 // JavaScript - Easy
 
-// Function that returns the sum of two numbers
-function sum(a, b) {
-    return a + b;
-}
-
-// Example usage and testing
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = sum;
-} else {
-    // Browser environment - test the function
-    console.log("Testing sum function:");
-    console.log("sum(2, 3) =", sum(2, 3)); // Expected: 5
-    console.log("sum(-1, 1) =", sum(-1, 1)); // Expected: 0
-    console.log("sum(0, 0) =", sum(0, 0)); // Expected: 0
-    console.log("sum(10.5, 5.5) =", sum(10.5, 5.5)); // Expected: 16
-}
+// TODO: Write a function that returns the sum of two numbers
