88affa927ef89eb8b5b57259aaac9c2f7dc025e6
kubanjaelijaheldred <kubanjaelijah2037@gmail.com>
2025-10-31 10:39:24 +0300

    Fix-Rust-fibonacci-task

--- tasks/rust/easy/fibonacci.rs
+++ tasks/rust/easy/fibonacci.rs
@@ -3,3 +3,27 @@
 use std::io;
 
 // TODO: Implement fibonacci sequence in Rust
+
+fn fibonacci(n: u32) -> u64 {
+    match n {
+        0 => 0,
+        1 => 1,
+        _ => fibonacci(n - 1) + fibonacci(n - 2),
+    }
+}
+
+fn main() {
+    println!("Enter the number of Fibonacci terms to generate:");
+    
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    
+    let n: u32 = input.trim().parse().expect("Please enter a valid number");
+    
+    println!("Fibonacci sequence:");
+    for i in 0..n {
+        println!("{}", fibonacci(i));
+    }
+}
