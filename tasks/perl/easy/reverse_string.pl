f3b5bc5038456700d3044edb11cf8a14f3237a8a
kubanjaelijaheldred <kubanjaelijah2037@gmail.com>
2025-11-01 13:10:18 +0300

    Fix-Perl-reverse-string-task

--- tasks/perl/easy/reverse_string.pl
+++ tasks/perl/easy/reverse_string.pl
@@ -4,4 +4,9 @@
 use strict;
 use warnings;
 
-# TODO: Write a Perl script that accepts a string and prints it in reversed order
+print "Enter a string to reverse: ";
+my $input = <STDIN>;
+chomp($input);
+
+my $reversed = reverse($input);
+print "Reversed string: $reversed\n";
