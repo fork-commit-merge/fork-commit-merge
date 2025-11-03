fbf0c4d22d91205705b037db4d8a86bf11cd5b56
GitHub Action <action@github.com>
2025-11-03 07:17:58 +0000

    Revert "Merge pull request #5292 from KubanjaElijahEldred/fix-erlang-echo"
    This reverts commit 626c03bb586ad2d24cfdc01c5bb54cf1cde38d79, reversing
    changes made to f368e6721b8445aec16757e7622a3af64d50d661.

--- tasks/erlang/easy/echo.erl
+++ tasks/erlang/easy/echo.erl
@@ -4,7 +4,4 @@
 -export([start/0]).
 
 start() ->
-    % Implement the echo program
-    io:format("Enter a message: "),
-    Input = io:get_line(""),
-    io:format("Echo: ~s", [Input]).
+    % TODO: Implement the echo program
