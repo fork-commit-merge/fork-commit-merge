f368e6721b8445aec16757e7622a3af64d50d661
GitHub Action <action@github.com>
2025-11-03 07:17:27 +0000

    Revert "Merge pull request #5291 from KubanjaElijahEldred/fix-d-reverse-string"
    This reverts commit 184a1d7682a1be02cc85d40f88058daa44548f3d, reversing
    changes made to a1f1a6886517ab8476e74d31532da71c70efa77c.

--- tasks/d/easy/reverseString.d
+++ tasks/d/easy/reverseString.d
@@ -1,16 +1,7 @@
 // D - Easy
 
 import std.stdio;
-import std.algorithm.mutation : reverse;
-import std.array;
 
 void main() {
-    // Implement program to reverse a string
-    writeln("Enter a string to reverse:");
-    string input = readln().strip();
-    
-    char[] reversed = input.dup;
-    reverse(reversed);
-    
-    writeln("Reversed string: ", reversed);
+    // TODO: Implement program to reverse a string
 }
