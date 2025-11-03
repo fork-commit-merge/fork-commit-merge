d2aaadcf259ffceb9703f3c8325d55441e5fc5b3
GitHub Action <action@github.com>
2025-11-03 07:15:42 +0000

    Revert "Merge pull request #5289 from KubanjaElijahEldred/fix-crystal-server"
    This reverts commit 2fb93b4aa118d0d6cc7056f795c02563a729be0b, reversing
    changes made to 5e625e1feed2b94b90a194714f35f49e75486ee3.

--- tasks/crystal/easy/server.cr
+++ tasks/crystal/easy/server.cr
@@ -2,12 +2,4 @@
 
 require "http/server"
 
-# Implement a simple HTTP server
-server = HTTP::Server.new do |context|
-  context.response.content_type = "text/plain"
-  context.response.print "Hello, Crystal!"
-end
-
-address = server.bind_tcp 8080
-puts "Listening on http://#{address}"
-server.listen
+# TODO: Implement the HTTP server
