042dc12e6d8619194d7fd0e556c887b27881d5c2
GitHub Action <action@github.com>
2025-11-03 07:18:25 +0000

    Revert "Merge pull request #5293 from KubanjaElijahEldred/fix-fortran-triangle-area"
    This reverts commit 386d5bbf86bb6a898878d2499dfc820a5ce8ec42, reversing
    changes made to fbf0c4d22d91205705b037db4d8a86bf11cd5b56.

--- tasks/fortran/easy/triangle_area.f90
+++ tasks/fortran/easy/triangle_area.f90
@@ -1,16 +1,4 @@
 program triangle_area
-! Implement the triangle_area program
-    implicit none
-    real :: base, height, area
-    
-    print *, "Enter the base of the triangle:"
-    read *, base
-    
-    print *, "Enter the height of the triangle:"
-    read *, height
-    
-    area = 0.5 * base * height
-    
-    print *, "Area of the triangle:", area
-    
+! TODO: Implement the triangle_area program
+
 end program triangle_area
