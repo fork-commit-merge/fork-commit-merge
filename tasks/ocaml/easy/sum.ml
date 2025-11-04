41dc937b7b6d746afcafab486b38b1a3e6de176f
kubanjaelijaheldred <kubanjaelijah2037@gmail.com>
2025-11-04 15:53:54 +0300

    Fix-OCaml-sum-task

--- tasks/ocaml/easy/sum.ml
+++ tasks/ocaml/easy/sum.ml
@@ -1,3 +1,12 @@
 (* OCaml - Easy *)
 
-(* TODO: Implement a function that takes a list of integers and returns the sum of all the integers in the list *)
+(* Implement a function that takes a list of integers and returns the sum of all the integers in the list *)
+let rec sum_list lst =
+  match lst with
+  | [] -> 0
+  | head :: tail -> head + sum_list tail
+
+let () =
+  let numbers = [1; 2; 3; 4; 5] in
+  let result = sum_list numbers in
+  Printf.printf "Sum of list: %d\n" result
