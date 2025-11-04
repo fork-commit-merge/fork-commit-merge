25cba271068a650fadcfe15aff004e86c64f12db
AlexandrAnatoliev <per-1986@list.ru>
2025-11-04 19:02:49 +0300

    fixed issue

--- tasks/bash/medium/disk_monitor.sh
+++ tasks/bash/medium/disk_monitor.sh
@@ -3,3 +3,14 @@
 # Bash - Medium
 
 # TODO: Implement the Disk Monitor script
+
+threshold=10
+
+max_usage=$(df --output=pcent / | awk 'NR>1 {print $1}' | sed 's/%//' \
+  | sort -nr | head -1)
+
+if [ "$max_usage" -gt "$threshold" ]; then
+  echo "Максимальное использование диска: ${max_usage}%"
+  df -h / | mail -s "Максимальное использование диска: ${max_usage}%"\
+    otetzalexandr1986@gmail.com
+fi
