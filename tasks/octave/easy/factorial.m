4577be8967749abc771f0e7165f2f15e745c8eaf
GitHub Action <action@github.com>
2025-11-04 15:55:50 +0000

    Revert "Merge pull request #5308 from KubanjaElijahEldred/fix-octave-factorial"
    This reverts commit 806e8b45ac3794be659821b5c5671e5607ac19c7, reversing
    changes made to 59e53ad8e5f20c8839a935e9c089a241dddbffe5.

--- tasks/octave/easy/factorial.m
+++ tasks/octave/easy/factorial.m
@@ -1,15 +1,3 @@
 % Octave - Easy
 
-% Implement a Octave script for calculating factorial
-function result = factorial_calc(n)
-    if n <= 1
-        result = 1;
-    else
-        result = n * factorial_calc(n - 1);
-    endif
-endfunction
-
-% Test the function
-n = 5;
-result = factorial_calc(n);
-fprintf('Factorial of %d is %d\n', n, result);
+% TODO: Implement a Octave script for calculating factorial
