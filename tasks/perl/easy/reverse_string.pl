9830971540bc2178900aad255c974ec7a108d6dc
GitHub Action <action@github.com>
2025-11-06 06:13:47 +0000

    Revert "Merge pull request #5319 from KubanjaElijahEldred/fix-perl-reverse-string"
    This reverts commit 591816b074fa29e1afecdc910053a0c1e1e83961, reversing
    changes made to 8d29bc10beaf4bf215fd1e7d613079a428934edb.

--- tasks/perl/easy/reverse_string.pl
+++ tasks/perl/easy/reverse_string.pl
@@ -5,8 +5,3 @@ use strict;
 use warnings;
 
 # TODO: Write a Perl script that accepts a string and prints it in reversed order
-print "Enter a string: ";
-my $input = <STDIN>;
-chomp($input);
-my $reversed = reverse($input);
-print "Reversed string: $reversed\n";
