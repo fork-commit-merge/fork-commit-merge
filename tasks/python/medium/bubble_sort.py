f65016225ad67b62415a4059bef6aa788f743dff
Greg Christian <gregpuzzles1@gmail.com>
2025-11-07 12:16:15 -0700

    fix-issue-5246

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,5 +1,12 @@
 def bubble_sort(arr):
-    # TODO: Implement the bubble sort algorithm
+    # Implement the bubble sort algorithm
+    n = len(arr)
+    for i in range(n):
+        # After each pass, the largest element moves to the end
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                # Swap if the element found is greater than the next element
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
