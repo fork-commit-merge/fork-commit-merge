19de3225fdd01dc90afc2f53b9526cf3b2e82fbd
GitHub Action <action@github.com>
2025-11-07 15:56:47 +0000

    Revert "Merge pull request #5325 from KubanjaElijahEldred/fix-csharp-hello-user"
    This reverts commit 01d21a3ea5fb84db2bebd16ead04784ddf9e9f17, reversing
    changes made to 5660fe0a39118bef971eec514b72a7e8470bb2d9.

--- tasks/c#/easy/HelloUser/Program.cs
+++ tasks/c#/easy/HelloUser/Program.cs
@@ -1,6 +1,3 @@
-// C# - Easy
+﻿// C# - Easy
 
-// Program that prompts for user's name and greets them
-Console.Write("What is your name? ");
-string? name = Console.ReadLine();
-Console.WriteLine($"Hello, {name}!");
+// TODO: Implement the C# program requested in the Issue description
