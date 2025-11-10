7da042e03cc5f46ff77dce8d3deffcdfc3e13fc4
GitHub Action <action@github.com>
2025-11-10 21:50:34 +0000

    Revert "Merge pull request #5354 from gregpuzzles1/fix-issue-#5223"
    This reverts commit b8e6b0d975c65f28cbee71454e3d57b1874a892b, reversing
    changes made to 6f38b90cd1d64cec0552e7d3e1e5767e51264551.

--- tasks/html/hard/clock/index.js
+++ tasks/html/hard/clock/index.js
@@ -1,7 +1,7 @@
 // Implement the setClock function to update the positions of the hour, minute, and second hands on the clock based on the current time.
 setInterval(() => {
-  // Calculate the current time and update the clock hands' positions here.
-  setClock();
+  // TODO: Calculate the current time and update the clock hands' positions here.
+  // You need to calculate the rotation ratios and update the hand rotations.
 }, 1000);
 
 const hourHand = document.querySelector("[data-hour-hand]");
@@ -9,28 +9,6 @@ const minuteHand = document.querySelector("[data-minute-hand]");
 const secondHand = document.querySelector("[data-second-hand]");
 
 // Create setRotation function to rotate a DOM element around its center point. This function will be used to rotate the hour, minute, and second hands on the clock.
-// Implement the setRotation function to update the rotation of a DOM element based on the provided rotationRatio.
-function setRotation(element, rotationRatio) {
-  // rotationRatio is between 0 and 1; multiply by 360 to get degrees
-  element.style.setProperty("--rotation", rotationRatio * 360);
-}
+// TODO: Implement the setRotation function to update the rotation of a DOM element based on the provided rotationRatio.
 
-// Compute current time and set the hands
-function setClock() {
-  const currentDate = new Date();
-
-  const seconds = currentDate.getSeconds();
-  const minutes = currentDate.getMinutes();
-  const hours = currentDate.getHours();
-
-  const secondsRatio = seconds / 60;
-  const minutesRatio = (minutes + secondsRatio) / 60;
-  const hoursRatio = ((hours % 12) + minutesRatio) / 12;
-
-  setRotation(secondHand, secondsRatio);
-  setRotation(minuteHand, minutesRatio);
-  setRotation(hourHand, hoursRatio);
-}
-
-// Call the setClock function here to initialize the clock's position when the page loads
-setClock();
+// TODO: You can call the setClock function here to initialize the clock's position when the page loads.
