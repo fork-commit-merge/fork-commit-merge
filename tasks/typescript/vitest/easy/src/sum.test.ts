edb678c32dd527caa944b323a0fd87960793b2a8
GitHub Action <action@github.com>
2025-11-10 18:27:42 +0000

    Revert "Merge pull request #5351 from gregpuzzles1/fix-issue-#5254"
    This reverts commit a559b94820a2bc41dea82baf869fb5471d7e74d6, reversing
    changes made to be566c802dab91415bb0677ce3514a9238a0d041.

--- tasks/typescript/vitest/easy/src/sum.test.ts
+++ tasks/typescript/vitest/easy/src/sum.test.ts
@@ -3,9 +3,4 @@
 import sum from "./sum";
 import { describe, expect, it } from "vitest";
 
-// Test to ensure sum() returns 0 with no numbers
-describe("sum", () => {
-  it("should return 0 when called with no arguments", () => {
-    expect(sum()).toBe(0);
-  });
-});
+// TODO: Create the test required in the task
