336e7d89f4ad0dc66181096be940469d43ed8c49
GitHub Action <action@github.com>
2025-11-13 06:05:02 +0000

    Revert "Merge pull request #5360 from SrDyw/fix-issue1-js-medium"
    This reverts commit a3bb68a038837a7a83c7dc097026dab8f7b256db, reversing
    changes made to 0b8b08fcc2ef0700fc3cbce2fa54dce4b4b568ba.

--- tasks/javascript/medium/countdownTimer/script.js
+++ tasks/javascript/medium/countdownTimer/script.js
@@ -1,30 +1,3 @@
 // JavaScript - Medium
 
 // Create functionality for a countdown timer that counts down from 10 seconds to zero using JavaScript
-
-const START_TIME = 10;
-
-const startTime = (updateFunc, START_TIME) => {
-    let time = START_TIME;
-    updateFunc(START_TIME);
-
-    const interval = setInterval(() => {
-        updateFunc(Math.max(--time, 0));
-        if (time == 0) clearInterval(interval)
-    }, 1000)
-}
-
-const formatTime = seconds => {
-    const m = Math.floor(seconds / 60);
-    const s = seconds % 60
-    return `${m.toString().padStart(2, '0')}:${s.toString().padStart(2, '0')}`
-}
-
-
-const contaier = document.getElementById("timer");
-const updateContainerWithTime = seconds => {
-    contaier.innerHTML = formatTime(seconds);
-}
-
-
-startTime(updateContainerWithTime, START_TIME);
