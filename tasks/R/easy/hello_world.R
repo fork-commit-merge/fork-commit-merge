de600da5fdd6625d2118e94cbba53edbb8e43a5f
GitHub Action <action@github.com>
2025-11-13 20:47:55 +0000

    Revert "Merge pull request #5362 from KubanjaElijahEldred/fix-r-hello-world"
    This reverts commit 2b0908c2094e50fb929ffdab4eb0e623409b588d, reversing
    changes made to 79ba84c96b8648042f64c9c315f605547d7e207f.

--- tasks/R/easy/hello_world.R
+++ tasks/R/easy/hello_world.R
@@ -1,5 +1,3 @@
 # R - Easy
 
-# Write a program that prints the string "Hello, World!" to the console.
-print("Hello, World!")
 # TODO: Write a program that prints the string "Hello, World!" to the console.
