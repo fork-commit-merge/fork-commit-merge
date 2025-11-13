29849bc34dc3fc8afd12e90f54fafe0a946769a2
Greg Christian <gregpuzzles1@gmail.com>
2025-11-13 11:30:00 -0700

    fix-issue-#5182

--- tasks/cobol/easy/HelloCOBOL.cbl
+++ tasks/cobol/easy/HelloCOBOL.cbl
@@ -1,8 +1,9 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. HelloCOBOL.
-       AUTHOR. Fork, Commit, Merge
+       AUTHOR. Greg Christian.
 
       * This is a simple program to print "Hello, COBOL!" to the console.
 
        PROCEDURE DIVISION.
-      * TODO: Implement the program logic here
+           DISPLAY "Hello, COBOL!".
+           STOP RUN.
