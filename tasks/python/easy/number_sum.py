b6a618557eda7ea97d59d94a1a541580d1b246d2
GitHub Action <action@github.com>
2025-11-17 06:43:51 +0000

    Revert "Merge pull request #5370 from KubanjaElijahEldred/fix-python-number-list"
    This reverts commit 9a76d0f915bb2c24021b4c994709fe1b8689e1aa, reversing
    changes made to 821e1ee54169a6c3993a98d299d38bc5e2b36674.

--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,21 +1,3 @@
 # Python - Easy
 
-def add_numbers(a, b):
-    """
-    Returns the sum of two numbers.
-    
-    Args:
-        a (int or float): First number
-        b (int or float): Second number
-        
-    Returns:
-        int or float: Sum of a and b
-    """
-    return a + b
-
-# Example usage
-if __name__ == "__main__":
-    num1 = 5
-    num2 = 3.5
-    result = add_numbers(num1, num2)
-    print(f"The sum of {num1} and {num2} is: {result}")
+# TODO: Write a function that returns the sum of two numbers
