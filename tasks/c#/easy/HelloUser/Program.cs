6f991f7f95192df05db787f1294f54e4ba92fbe5
GitHub Action <action@github.com>
2025-11-18 20:10:32 +0000

    Revert "Merge pull request #5511 from SrDyw/simple-greeting"
    This reverts commit a93d50dd70eb252763cc6a622ca018ff1d171cb2, reversing
    changes made to ec6ef0d368aa7f01fc9e988e3a743068038aa6c2.

--- tasks/c#/easy/HelloUser/Program.cs
+++ tasks/c#/easy/HelloUser/Program.cs
@@ -1,19 +1,3 @@
 ﻿// C# - Easy
 
 // TODO: Implement the C# program requested in the Issue description
-Console.WriteLine("First at all, How's your name?");
-
-var name = "";
-var invalidName = false;
-do
-{
-    Console.Write(">_ ");
-    name = Console.ReadLine();
-    invalidName = string.IsNullOrWhiteSpace(name);
-    if (invalidName)
-    {
-        Console.WriteLine("Please tell me your name, I promise that I don't bite ;)");
-    }
-} while (invalidName);
-
-Console.WriteLine($"Hello, {name}!");
