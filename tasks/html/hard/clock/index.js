a130dbda4381359a750a86c13d23ba3ee6ae20ed
GitHub Action <action@github.com>
2025-11-23 13:37:31 +0000

    Revert "Merge pull request #5521 from sanchezgica/fix-issue"
    This reverts commit bd95edd59657638ac4aa68d44e3adaf3f16702c2, reversing
    changes made to 11f897d495e0026bf70f7141db335e6b8e7e4b9f.

--- tasks/html/hard/clock/index.js
+++ tasks/html/hard/clock/index.js
@@ -1,34 +1,14 @@
 // Implement the setClock function to update the positions of the hour, minute, and second hands on the clock based on the current time.
+setInterval(() => {
+  // TODO: Calculate the current time and update the clock hands' positions here.
+  // You need to calculate the rotation ratios and update the hand rotations.
+}, 1000);
 
 const hourHand = document.querySelector("[data-hour-hand]");
 const minuteHand = document.querySelector("[data-minute-hand]");
 const secondHand = document.querySelector("[data-second-hand]");
 
-function setRotation(hand, rotationRatio) {
-  if (hand) {
-    hand.style.transform = `rotate(${rotationRatio}deg)`;
-  }
-}
-
-function setClock() {
-  const dateNow = new Date();
-
-  const seconds = dateNow.getSeconds();
-  const secondsRotation = seconds * 6;
-  const minutes = dateNow.getMinutes();
-  const minuteRotation = minutes * 6;
-  const hours = dateNow.getHours() % 12;
-  console.log(hours);
-  const hourRotation = hours * 30;
-
-  setRotation(secondHand, secondsRotation);
-  setRotation(minuteHand, minuteRotation);
-  setRotation(hourHand, hourRotation);
-}
-
 // Create setRotation function to rotate a DOM element around its center point. This function will be used to rotate the hour, minute, and second hands on the clock.
 // TODO: Implement the setRotation function to update the rotation of a DOM element based on the provided rotationRatio.
 
 // TODO: You can call the setClock function here to initialize the clock's position when the page loads.
-window.onload = setClock;
-setInterval(setClock, 1000);
