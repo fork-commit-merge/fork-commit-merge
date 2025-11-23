0446b1b8ee790a821e3979119e07544982d3914d
Juan Sanchez <sanchezgica@gmail.com>
2025-11-23 08:06:35 -0300

    implemented setRotation function and set all rotations ratios to  degrees

--- tasks/html/hard/clock/index.js
+++ tasks/html/hard/clock/index.js
@@ -4,29 +4,31 @@ const hourHand = document.querySelector("[data-hour-hand]");
 const minuteHand = document.querySelector("[data-minute-hand]");
 const secondHand = document.querySelector("[data-second-hand]");
 
+function setRotation(hand, rotationRatio) {
+  if (hand) {
+    hand.style.transform = `rotate(${rotationRatio}deg)`;
+  }
+}
+
 function setClock() {
   const dateNow = new Date();
 
   const seconds = dateNow.getSeconds();
   const secondsRotation = seconds * 6;
   const minutes = dateNow.getMinutes();
-  const minuteRotation = minutes * (1 / 60);
-  const hours = dateNow.getHours();
+  const minuteRotation = minutes * 6;
+  const hours = dateNow.getHours() % 12;
   console.log(hours);
-  const hourRotation = hours * (1 / 12);
-  secondHand.style.transform = `rotate(${secondsRotation}deg)`;
-  minuteHand.style.transform = `rotate(${minuteRotation}turn)`;
-  hourHand.style.transform = `rotate(${hourRotation}turn)`;
-}
+  const hourRotation = hours * 30;
 
-setClock();
-setInterval(setClock, 1000);
-setInterval(() => {
-  // TODO: Calculate the current time and update the clock hands' positions here.
-  // You need to calculate the rotation ratios and update the hand rotations.
-}, 1000);
+  setRotation(secondHand, secondsRotation);
+  setRotation(minuteHand, minuteRotation);
+  setRotation(hourHand, hourRotation);
+}
 
 // Create setRotation function to rotate a DOM element around its center point. This function will be used to rotate the hour, minute, and second hands on the clock.
 // TODO: Implement the setRotation function to update the rotation of a DOM element based on the provided rotationRatio.
 
 // TODO: You can call the setClock function here to initialize the clock's position when the page loads.
+window.onload = setClock;
+setInterval(setClock, 1000);
