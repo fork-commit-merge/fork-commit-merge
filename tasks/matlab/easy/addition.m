cfb751dfa00c366f8b0c3ead6d687ba9596c0d65
SuryaGanesh284 <surya285888@gmail.com>
2025-11-26 02:11:48 +0530

    Solve Issue #5509: Create MATLAB Script for Addition of Two Numbers

--- tasks/matlab/easy/addition.m
+++ tasks/matlab/easy/addition.m
@@ -1,3 +1,11 @@
 % MATLAB - Easy
 
 % TODO: Create a MATLAB script that adds two numbers together and prints the result
+
+% Add two numbers
+num1 = 5;
+num2 = 10;
+result = num1 + num2;
+
+% Print the result
+fprintf('The sum of %d and %d is %d\n', num1, num2, result);
