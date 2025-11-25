6d07219e7d1bc933a9be1f9dbf1d551f8bfc8e3b
Sathvikaundala <sathvikareddy33@gmail.com>
2025-11-26 00:59:35 +0530

    Implement basic HTTP server in server.cr
    Implemented a simple HTTP server in Crystal that responds with "Hello, world!" message for all incoming HTTP requests.
    
    The server:
    - Uses Crystal's built-in HTTP::Server module
    - Listens on port 3000
    - Returns "Hello, world!" with content type text/plain
    
    This resolves issue #5507

--- tasks/crystal/easy/server.cr
+++ tasks/crystal/easy/server.cr
@@ -2,4 +2,11 @@
 
 require "http/server"
 
-# TODO: Implement the HTTP server
+server = HTTP::Server.new do |context|
+  context.response.content_type = "text/plain"
+  context.response.print "Hello, world!"
+end
+
+address = server.bind_tcp 3000
+puts "Listening on http://#{address}"
+server.listen
