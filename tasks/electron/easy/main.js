e50526b8555744d2442409a0ecdd6225a7b6fcaa
GitHub Action <action@github.com>
2025-11-26 13:14:04 +0000

    Revert "Merge pull request #5545 from 2400032286/2400032286-patch-2"
    This reverts commit 2506e2ffb4e30f40f6c14833d8375df94f7bd5ad, reversing
    changes made to 2d76f9ae074b8f2b8d59ef974afd468a6602edee.

--- tasks/electron/easy/main.js
+++ tasks/electron/easy/main.js
@@ -3,13 +3,7 @@
 const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
-  win.loadFile('index.html');}
+    // TODO: Create a new BrowserWindow that displays the index.html file
+}
 
 app.on("ready", createWindow);
