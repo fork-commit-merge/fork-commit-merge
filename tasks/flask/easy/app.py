495cd50376c5137d870906de25654aa2d5baf840
GitHub Action <action@github.com>
2025-11-26 13:10:46 +0000

    Revert "Merge pull request #5540 from 2400032286/2400032286-patch-1"
    This reverts commit f38e014fc163a0d506babe7b52019e071be836c0, reversing
    changes made to 1fc4a7c602fe1f2e1340cc8b617fe729f7d70c96.

--- tasks/flask/easy/app.py
+++ tasks/flask/easy/app.py
@@ -4,9 +4,5 @@ app = Flask(__name)
 
 # TODO: Implement the task requested in the issue description
 
-@app.route('/')
-def hello():
-        return 'Hello, Flask!'
-
 if __name__ == "__main__":
     app.run(debug=True)
