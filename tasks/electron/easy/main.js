0cdb929744d27173f4ab0d9e62a483a8cfa58e3b
GitHub Action <action@github.com>
2025-11-26 13:11:57 +0000

    Revert "Merge pull request #5541 from Gokul45-45/solve-issue-5474-electron"
    This reverts commit 7d6c335e2e6884212d3f02279119137ca46ed4a5, reversing
    changes made to 2316f9dcaed8d963960273403724983279a3f6d8.

--- tasks/electron/easy/main.js
+++ tasks/electron/easy/main.js
@@ -4,15 +4,6 @@ const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
     // TODO: Create a new BrowserWindow that displays the index.html file
-        const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
-    
-    mainWindow.loadFile('index.html');
 }
 
 app.on("ready", createWindow);
