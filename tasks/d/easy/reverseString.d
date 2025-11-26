3e37858c7f67f870e491aff80a7a5da8d60723d4
GitHub Action <action@github.com>
2025-11-26 07:47:48 +0000

    Revert "Merge pull request #5533 from Sathvikaundala/Sathvikaundala-patch-2"
    This reverts commit ad7e28513f11b6388fb8526e8b4eafd07acedb63, reversing
    changes made to d624aa22be7dacca6b05c2cb2a3fd6510fac8987.

--- tasks/d/easy/reverseString.d
+++ tasks/d/easy/reverseString.d
@@ -1,16 +1,7 @@
 // D - Easy
 
 import std.stdio;
-import std.algorithm;
-import std.array;
 
 void main() {
-    string originalString = "Hello, World!";
-    
-    // Reverse the string
-    string reversedString = originalString.dup.reverse.array.idup;
-    
-    // Display the results
-    writeln("Original String: ", originalString);
-    writeln("Reversed String: ", reversedString);
+    // TODO: Implement program to reverse a string
 }
