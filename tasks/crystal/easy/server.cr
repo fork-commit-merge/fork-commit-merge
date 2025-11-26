cf7e766e422e7104cb0ebba794a53d916cb14782
GitHub Action <action@github.com>
2025-11-26 07:48:11 +0000

    Revert "Merge pull request #5530 from Sathvikaundala/Sathvikaundala-patch-1"
    This reverts commit cfea9b9958f05a34b25eb7ac0651165e1fa40baa, reversing
    changes made to 3e37858c7f67f870e491aff80a7a5da8d60723d4.

--- tasks/crystal/easy/server.cr
+++ tasks/crystal/easy/server.cr
@@ -2,11 +2,4 @@
 
 require "http/server"
 
-server = HTTP::Server.new do |context|
-  context.response.content_type = "text/plain"
-  context.response.print "Hello, world!"
-end
-
-address = server.bind_tcp 3000
-puts "Listening on http://#{address}"
-server.listen
+# TODO: Implement the HTTP server
