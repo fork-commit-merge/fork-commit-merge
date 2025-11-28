4969f06b753e38a1a8594c98a6f8136fc5be8b41
GitHub Action <action@github.com>
2025-11-28 06:09:49 +0000

    Revert "Merge pull request #5584 from 2400033206/2400033206-patch-1"
    This reverts commit 3f255318e62b3d2f1ecce99cef678627bc92454e, reversing
    changes made to 47eb47a6acf9796eeb6a9e9b64c7ff3c84b6f895.

--- tasks/d/easy/reverseString.d
+++ tasks/d/easy/reverseString.d
@@ -1,25 +1,7 @@
 // D - Easy
-// String Reversal Program
-// This program demonstrates string reversal in D programming language
 
 import std.stdio;
-import std.string : reverse;
-import std.array : array;
 
 void main() {
-    // Original string to reverse
-    string originalString = "Hello, World!";
-    
-    // Convert string to mutable character array
-    char[] charArray = originalString.dup;
-    
-    // Reverse the character array
-    reverse(charArray);
-    
-    // Convert back to string
-    string reversedString = charArray.idup;
-    
-    // Display results
-    writeln("Original String: ", originalString);
-    writeln("Reversed String: ", reversedString);
+    // TODO: Implement program to reverse a string
 }
