56e615e5c75fb677923bd08861ca71172a85ca6f
GitHub Action <action@github.com>
2025-11-29 09:18:30 +0000

    Revert "Merge pull request #5586 from Manthancode28/main"
    This reverts commit f5825a98158f886b57331b227c0f8d0ff94b117c, reversing
    changes made to d03a017d7d4a2144207bcd25386a98d322d015c9.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -1,16 +1,12 @@
+// React/Vite - Easy
+import "./index.css";
+
 function App() {
   return (
     <div className="center">
-      <h1>React/Vite - Easy</h1>
-
-      <div className="card">
-        <img src="https://picsum.photos/320/240" alt="Random" />
-        <h2>Card Title</h2>
-        <p>Card Description</p>
-      </div>
+      {/* TODO: Implement the card component */}
     </div>
   );
 }
 
 export default App;
-
