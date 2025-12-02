47917943f36d9f3602849f201b3c773cd4caad2d
GitHub Action <action@github.com>
2025-12-02 12:02:51 +0000

    Revert "Merge pull request #5733 from prasanth-30011/main"
    This reverts commit 7f838f7632a3c9a2bf14c0a8e7d17e270e9e0ede, reversing
    changes made to 9d4a90eb8fcf816175443937a27ac9c6d50358b0.

--- tasks/javascript/medium/countdownTimer/script.js
+++ tasks/javascript/medium/countdownTimer/script.js
@@ -1,22 +1,3 @@
 // JavaScript - Medium
 
 // Create functionality for a countdown timer that counts down from 10 seconds to zero using JavaScript
-
-let countdownValue = 10;
-const timerDisplay = document.getElementById('timer');
-const timeLeft = document.getElementById('time-left');
-
-function startCountdown() {
-  const interval = setInterval(() => {
-    timerDisplay.textContent = countdownValue;
-    
-    if (countdownValue === 0) {
-      clearInterval(interval);
-      timeLeft.textContent = "Time's up!";
-    } else {
-      countdownValue--;
-    }
-  }, 1000);
-}
-
-startCountdown();
