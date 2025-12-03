c1adf716ceed38f5177d8c786065986954c18f58
GitHub Action <action@github.com>
2025-12-03 07:40:22 +0000

    Revert "Merge pull request #5735 from Anas-16/HardProblem"
    This reverts commit 8d0316b76024b1c1dd28f3763de9fb251812aad3, reversing
    changes made to 246c16ff2ab248f76a49d5719472e18b14ebf92f.

--- tasks/java/hard/Frequency.java
+++ tasks/java/hard/Frequency.java
@@ -1,19 +1,9 @@
-
 // Java - Hard
 import java.util.HashMap;
 import java.util.Map;
-
 public class Frequency {
 
     public static void main(String[] args) {
-
-        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 9, 3, 2, 2, 3, 4, 5 };
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-
-        for (int nums : numbers) {
-            // getOrDefault() returns actual value or defaultValue if it is not mapped
-            frequencyMap.put(nums, frequencyMap.getOrDefault(nums, 0) + 1);
-        }
-        System.out.println(frequencyMap);
+        // TODO: Given an array of integers, write a Java program to find the frequency of each element using a Map
     }
 }
