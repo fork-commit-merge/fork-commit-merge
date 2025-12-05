7966d805e96d4dcbb353ffe6d0038169cfa37a55
Akshay <dhakan70@gmail.com>
2025-12-05 13:29:48 -0500

    solved JS countdownTimer

--- tasks/javascript/medium/countdownTimer/script.js
+++ tasks/javascript/medium/countdownTimer/script.js
@@ -1,3 +1,9 @@
 // JavaScript - Medium
 
 // Create functionality for a countdown timer that counts down from 10 seconds to zero using JavaScript
+
+for (let i=10; i>=0; i--) {
+    setTimeout(() => {
+        console.log(i);
+    }, 1000 * (10-i));
+}
