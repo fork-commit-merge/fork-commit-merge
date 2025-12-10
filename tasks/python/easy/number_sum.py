9d03096b19fc4e2c985d32d73fbc27a757d3d819
Vasileios Kokotakis <vkokotakis@gmail.com>
2025-12-10 19:10:20 +0100

    Easy task commit

--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,3 +1,15 @@
 # Python - Easy
 
 # TODO: Write a function that returns the sum of two numbers
+import random
+
+number_1 = random.random()
+number_2 = random.random()
+
+def print_sum(number_1, number_2):
+    # Print the sum
+    print("Sum:", number_1 + number_2)
+
+print_sum(number_1, number_2)
+
+
