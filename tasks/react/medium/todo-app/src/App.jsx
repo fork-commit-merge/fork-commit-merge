4048e65ebe3bc16bcbd25d62f4a37ca78f4c04d7
GitHub Action <action@github.com>
2025-12-11 13:28:21 +0000

    Revert "Merge pull request #5759 from jikrana-del/main"
    This reverts commit 32d70f9b39a875b3987ddb38c274b95aa17dc397, reversing
    changes made to 7e3b1bf4ab7974f696f69967d4309aea11714fc2.

--- tasks/react/medium/todo-app/src/App.jsx
+++ tasks/react/medium/todo-app/src/App.jsx
@@ -1,72 +1,13 @@
 // React - Medium
 
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState(() => {
-    const saved = localStorage.getItem("todos");
-    return saved ? JSON.parse(saved) : [];
-  });
-  const [input, setInput] = useState("");
-  const [editBtn, setEditBtn] = useState(null);
-  const addTodo = () => {
-    if (!input.trim()) return;
-    if(editBtn !== null){
-      const newTodos = [...todos];
-      newTodos[editBtn] = input;
-      setTodos(newTodos);
-      setEditBtn(null);
-    }
-    else{
-      setTodos([...todos, input]);
-    }
-    setInput("");
-  }
-
-  const deleteTodo = (index) => {
-    const newTodos = todos.filter((_, i) => i !== index);
-    setTodos(newTodos);
-  }
-  const editTodo = (index) => {
-    setInput(todos[index]);
-    setEditBtn(index);
-
-  }
-
+  // TODO: Add the necessary functionality such as hooks here
   return (
     <div className="App">
       <h1>To-Do List</h1>
-      <div>
-        <input type="text"
-          placeholder="Enter To-Do..."
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button onClick={addTodo}>{editBtn !== null ? "Edit" : "Add"}</button>
-      </div>
-      <ul>
-        {todos.map((todo, index) => {
-          return (
-            <li key={index} style={{ marginTop: "10px" }}>
-              {todo}
-              <button
-                onClick={() => editTodo(index)}
-                style={{ marginLeft: "10px" }}
-              >
-                Edit
-              </button>
-
-              <button
-                onClick={() => deleteTodo(index)}
-                style={{ marginLeft: "10px" }}
-              >
-                Delete
-              </button>
-            </li>
-          )
-        })}
-      </ul>
+      {/* TODO: Add the To-Do list here */}
     </div>
   );
 }
--- tasks/react/medium/traffic-light/src/TrafficLight.tsx
+++ tasks/react/medium/traffic-light/src/TrafficLight.tsx
@@ -1,26 +1,11 @@
-import { useEffect, useState } from "react";
-import "./App.css"; // Make sure App.css contains the light styles
+// React.js - Medium 2
+import "./App.css";
+
+// TODO: Implement  the TrafficLight component
 
 export function TrafficLight() {
-  // State to track which light is active
-  const [active, setActive] = useState<"red" | "yellow" | "green">("red");
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      // Traffic Light Sequence
-      if (active === "red") setActive("green");
-      else if (active === "green") setActive("yellow");
-      else setActive("red");
-    }, 2000); // Light changes every 2 seconds
-
-    return () => clearInterval(timer);
-  }, [active]);
 
   return (
-    <div className="traffic-container">
-      <div className={`light red ${active === "red" ? "active" : ""}`}></div>
-      <div className={`light yellow ${active === "yellow" ? "active" : ""}`}></div>
-      <div className={`light green ${active === "green" ? "active" : ""}`}></div>
-    </div>
+    // TODO: return JSX here
   );
 }
