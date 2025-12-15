8dc77ba37fc04b36f4f41866c6a1e33973815d49
GitHub Action <action@github.com>
2025-12-15 18:28:06 +0000

    Revert "Merge pull request #5913 from adityav27/main"
    This reverts commit 87b2794abd1670d3ed1ae015f13c9ee6e4349815, reversing
    changes made to 8689d469044782dffc46c4204ac83be4ded351ed.

--- tasks/flutter/easy/hello_flutter/lib/main.dart
+++ tasks/flutter/easy/hello_flutter/lib/main.dart
@@ -13,24 +13,7 @@ class ForkCommitMerge extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Center(
-            child: Text(
-              'Fork-Commit-Merge',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold),
-            ),
-          ),
-        ),
-        body: const Center(
-          child: Text(
-            'Hello',
-            style: TextStyle(
-                color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-        ),
+          // TODO: Implement the appbar and body properties
       ),
     );
   }
