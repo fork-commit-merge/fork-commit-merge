6190ef960b41c8f467b80bd8704cdf414335d68b
salehcoder <salehmnasra@gmail.com>
2025-12-15 20:22:10 +0200

    easy task c++ tempConverter solution

--- tasks/c++/easy/tempConverter.cpp
+++ tasks/c++/easy/tempConverter.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
-
+using namespace std;
 int main() {
   // TODO: Implement the temperature converter
+double tempC ;
+cin>>tempC;
+double tempF = (tempC*1.8)+32;
+cout<<"the temp in Farhninet is :"<<tempF;
+return tempF;
 }
