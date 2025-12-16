16c20e200ab59ee842ed04993c91e9c09fdfd8f4
GitHub Action <action@github.com>
2025-12-16 08:24:55 +0000

    Revert "Merge pull request #5914 from Salehcodes/salehcodes-c++-easy-tempConverter"
    This reverts commit 2cac36115fac3194a59d4d016c2259872b5e82c8, reversing
    changes made to 1ba91d683435130f085824822f49217a9d84ef42.

--- tasks/c++/easy/tempConverter.cpp
+++ tasks/c++/easy/tempConverter.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
-using namespace std;
+
 int main() {
   // TODO: Implement the temperature converter
-double tempC ;
-cin>>tempC;
-double tempF = (tempC*1.8)+32;
-cout<<"the temp in Farhninet is :"<<tempF;
-return tempF;
 }
