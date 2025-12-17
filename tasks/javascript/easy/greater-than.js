74de1492f9d4a7a2f71ef044c9675796e5328505
abubakar-sadiq-001 <abubakarsadiqi532@gmail.com>
2025-12-17 16:20:58 +0100

    return a new array with all numbers greater than 10 but less than 20

--- tasks/javascript/easy/greater-than.js
+++ tasks/javascript/easy/greater-than.js
@@ -3,4 +3,5 @@
 // TODO: return a new array with all numbers greater than 10 but less than 20;
 
 const array = [2, 5, 8, 10, 12, 15, 19, 20, 25];
-// output: [12, 15, 19];
+
+const filter = array.filter((num) => num > 10 && num < 20); // output: [12, 15, 19];
