f3c422db76aa5dea058b95c4b4a7e91d80cad3f6
GitHub Action <action@github.com>
2025-12-17 06:05:10 +0000

    Revert "Merge pull request #5926 from codyGaurav2/newArray"
    This reverts commit 07a7f328d8f3e8e5c374dac1ff6c7df1e1540d89, reversing
    changes made to d13ac37d362e8ce2e437e6a9f6f5259650e034d4.

--- tasks/javascript/easy/greater-than.js
+++ tasks/javascript/easy/greater-than.js
@@ -3,17 +3,4 @@
 // TODO: return a new array with all numbers greater than 10 but less than 20;
 
 const array = [2, 5, 8, 10, 12, 15, 19, 20, 25];
-
-const result = [];
-
-for(i=0;i<array.length;i++){
-    if(array[i] > 10 && array[i] < 20){
-        result.push(array[i]);
-    }
-}
-
-console.log(result);
-
-
-
 // output: [12, 15, 19];
