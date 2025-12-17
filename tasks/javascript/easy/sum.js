08fe26eb4c275e8eaf50d44162fdda09bca3831c
GitHub Action <action@github.com>
2025-12-17 12:22:25 +0000

    Revert "Merge pull request #5929 from zaibamachhaliya/main"
    This reverts commit 5bf7b3edd553f4b2dd9e07a2e4729324b73433d8, reversing
    changes made to 417825e50a95c71cd4799d4fb077b0582d43b881.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,11 +1,3 @@
 // JavaScript - Easy
 
-
-function Sum(a,b){
-    return a+b;
-}
-
-const result=Sum(1,2);
-console.log(`The sum of 1 and 2 is ${result}.`);
-
 // TODO: Write a function that returns the sum of two numbers
