57edf918a9380cc56d26376b1901bfbac14bb838
Akshay <dhakan70@gmail.com>
2025-12-18 10:21:53 -0500

    new JS medium question

---
+++ tasks/javascript/medium/product-stock.js
@@ -0,0 +1,13 @@
+// JavaScript - Medium
+
+  // TODO: return only products that are in stock using .filter() method;
+  // Extra Challenge: return only the names of products in stock;
+
+const products = [
+    { name: "Book", inStock: true },
+    { name: "Pen", inStock: false },
+    { name: "Phone", inStock: true },
+    { name: "Mug", inStock: false }
+  ];
+  // TODO output = [ { name: 'Book', inStock: true }, { name: 'Phone', inStock: true } ];
+  // Challenge output = ['Book', 'Phone'];
