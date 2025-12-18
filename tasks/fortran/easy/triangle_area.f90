73521edefed92bdf903e5204b766284eccadda5f
Greg Christian <gregpuzzles1@gmail.com>
2025-12-18 01:28:02 +0000

    triangle_area

--- tasks/fortran/easy/triangle_area.f90
+++ tasks/fortran/easy/triangle_area.f90
@@ -1,4 +1,19 @@
 program triangle_area
-! TODO: Implement the triangle_area program
+    implicit none
+    real :: base, height, area
+
+    ! Prompt user for the base of the triangle
+    print *, "Enter the base of the triangle:"
+    read *, base
+
+    ! Prompt user for the height of the triangle
+    print *, "Enter the height of the triangle:"
+    read *, height
+
+    ! Calculate the area of the triangle
+    area = 0.5 * base * height
+
+    ! Display the result
+    print *, "The area of the triangle is:", area
 
 end program triangle_area
