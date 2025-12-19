d9cb6db73701a6a264aa97600e3cae486bf5844c
GitHub Action <action@github.com>
2025-12-19 06:06:10 +0000

    Revert "Merge pull request #5939 from KareemAllam/fix-issue"
    This reverts commit fbca23b64029dd267678bbdef8d63816c7a07c09, reversing
    changes made to 68d5bfa6db0d64162c11873d778ff69eb1f60b78.

--- tasks/javascript/medium/product-stock.js
+++ tasks/javascript/medium/product-stock.js
@@ -1,17 +1,13 @@
 // JavaScript - Medium
 
-// TODO: return only products that are in stock using .filter() method;
-// Extra Challenge: return only the names of products in stock;
+  // TODO: return only products that are in stock using .filter() method;
+  // Extra Challenge: return only the names of products in stock;
 
 const products = [
-  { name: "Book", inStock: true },
-  { name: "Pen", inStock: false },
-  { name: "Phone", inStock: true },
-  { name: "Mug", inStock: false }
-];
-// TODO output = [ { name: 'Book', inStock: true }, { name: 'Phone', inStock: true } ];
-// Challenge output = ['Book', 'Phone'];
-
-const productsInStock = products.filter(product => product.inStock).map(product => product.name);
-console.log(productsInStock)
-return productsInStock
+    { name: "Book", inStock: true },
+    { name: "Pen", inStock: false },
+    { name: "Phone", inStock: true },
+    { name: "Mug", inStock: false }
+  ];
+  // TODO output = [ { name: 'Book', inStock: true }, { name: 'Phone', inStock: true } ];
+  // Challenge output = ['Book', 'Phone'];
