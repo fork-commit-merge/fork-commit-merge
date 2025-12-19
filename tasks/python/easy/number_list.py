355b75fd2a27977b05f5f32ff75a8fed83b85549
Katya Larson <katya_2012@hotmail.com>
2025-12-19 11:19:20 -0500

    Fixed issue

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -5,5 +5,11 @@
 # prints that list to the console and,
 # prints the sum of the numbers of that list
 
-def function(numbers):
-    pass
+def function():
+    num_list=list(range(1,11))
+    print(num_list)
+    
+
+
+if __name__ == '__main__':
+    function()    
