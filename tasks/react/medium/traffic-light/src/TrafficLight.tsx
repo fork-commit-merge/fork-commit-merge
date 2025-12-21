d2dd241ee048890c778c5cbd18b3cdd9f6176bfd
GitHub Action <action@github.com>
2025-12-21 09:18:46 +0000

    Revert "Merge pull request #5943 from masawik/issue-5798"
    This reverts commit 8f556280d233f6677408944fd2bc702eac81b2d6, reversing
    changes made to fab4ae31100e8fa5411430cba4ca2c488040a74f.

--- tasks/react/medium/traffic-light/src/TrafficLight.tsx
+++ tasks/react/medium/traffic-light/src/TrafficLight.tsx
@@ -1,37 +1,11 @@
 // React.js - Medium 2
-import { useEffect, useState } from "react";
 import "./App.css";
 
-const cn = (...classNames: (string | false | undefined)[]) => classNames.filter(c => !!c).join(' ')
-
-interface LightItem {
-  color: 'red' | 'green' | 'yellow'
-  time: number
-}
-
-const SEQUENCE = [
-  { color: 'red', time: 4000 },
-  { color: 'green', time: 3000 },
-  { color: 'yellow', time: 1000 },
-] as const satisfies LightItem[]
+// TODO: Implement  the TrafficLight component
 
 export function TrafficLight() {
-  const [activeSequenceItemIndex, setActiveSequenceItemIndex] = useState<number>(0)
-  const activeLight = SEQUENCE[activeSequenceItemIndex]
-
-  useEffect(() => {
-    const timerId = setTimeout(() => {
-      setActiveSequenceItemIndex((prevIndex) => (prevIndex + 1) % SEQUENCE.length)
-    }, activeLight.time)
-
-    return () => clearTimeout(timerId)
-  }, [activeSequenceItemIndex, activeLight.time])
 
   return (
-    <div className="traffic-light">
-      <div className={cn('light red', activeLight.color === 'red' && 'active')} />
-      <div className={cn('light yellow', activeLight.color === 'yellow' && 'active')} />
-      <div className={cn('light green', activeLight.color === 'green' && 'active')} />
-    </div>
+    // TODO: return JSX here
   );
 }
