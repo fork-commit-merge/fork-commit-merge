fd3b4aa583dcf3d3f1c0de74807ef545e4cc9064
GitHub Action <action@github.com>
2025-12-23 15:12:17 +0000

    Revert "Merge pull request #5948 from jikrana-del/add-filter-task"
    This reverts commit 6a342cce172deeddfd107a527301b099a4ef9b6e, reversing
    changes made to 7a55794c1cf1e0019828f94684f36b5e3f85ceb6.

--- tasks/javascript/medium/product-stock.js
+++ tasks/javascript/medium/product-stock.js
@@ -1,16 +1,13 @@
+// JavaScript - Medium
+
+  // TODO: return only products that are in stock using .filter() method;
+  // Extra Challenge: return only the names of products in stock;
 
 const products = [
-  { name: "Book", inStock: true },
-  { name: "Pen", inStock: false },
-  { name: "Phone", inStock: true },
-  { name: "Mug", inStock: false }
-];
-
-
-const productsFilter = (products) => {
-  return (
-    products.filter(product => product.inStock === true)
-  )
-};
-
-console.log(productsFilter(products));
+    { name: "Book", inStock: true },
+    { name: "Pen", inStock: false },
+    { name: "Phone", inStock: true },
+    { name: "Mug", inStock: false }
+  ];
+  // TODO output = [ { name: 'Book', inStock: true }, { name: 'Phone', inStock: true } ];
+  // Challenge output = ['Book', 'Phone'];
