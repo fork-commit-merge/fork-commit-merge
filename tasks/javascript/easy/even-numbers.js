091d808845039dd7e11255e330265c60c6ea7e73
GitHub Action <action@github.com>
2025-12-24 08:09:48 +0000

    Revert "Merge pull request #5949 from dhruvac29/fix-issue-5922"
    This reverts commit 8f05640315d990cd0668112e489f7c3fa350e7bb, reversing
    changes made to fd3b4aa583dcf3d3f1c0de74807ef545e4cc9064.

--- tasks/javascript/easy/even-numbers.js
+++ tasks/javascript/easy/even-numbers.js
@@ -3,11 +3,4 @@
 // TODO:return a new array containing all even numbers using a .filter() method:
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-// Filter the array to get only even numbers
-// A number is even if it's divisible by 2 (remainder is 0)
-const evenNumbers = array.filter(num => num % 2 === 0);
-
-// Output the result
-console.log(evenNumbers);
 // output: [2, 4, 6, 8, 10]
