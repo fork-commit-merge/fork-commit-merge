1e3074011e548dc6031b2441e372e7a045fe3d00
Abhi-2516 <2516abhi43@gmail.com>
2025-12-30 21:29:14 +0530

    JavaScript Easy: filter numbers between 10 and 20

--- tasks/javascript/easy/greater-than.js
+++ tasks/javascript/easy/greater-than.js
@@ -1,6 +1,9 @@
 // JavaScript - Easy
 
-// TODO: return a new array with all numbers greater than 10 but less than 20;
+// return a new array with all numbers greater than 10 but less than 20;
 
 const array = [2, 5, 8, 10, 12, 15, 19, 20, 25];
-// output: [12, 15, 19];
+
+const result = array.filter(num => num > 10 && num < 20);
+
+console.log(result);
