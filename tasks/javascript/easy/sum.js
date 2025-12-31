37f075b2197906b68f07ac32df529ada9e8f8640
GitHub Action <action@github.com>
2025-12-31 07:44:07 +0000

    Revert "Merge pull request #5963 from 2400032210/lokesh1branch"
    This reverts commit a382075179fc9928c5c138d66048fd2da5278bac, reversing
    changes made to 3e6bc768b7d9cb7f6e5355996343bcc70fdb070f.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,10 +1,3 @@
 // JavaScript - Easy
 
 // TODO: Write a function that returns the sum of two numbers
-
-function sum(a, b) {
-  return a + b;
-}
-
-const result = sum(1, 2);
-console.log(`The sum of 1 and 2 is ${result}.`);
