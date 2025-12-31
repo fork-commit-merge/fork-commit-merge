4a04022015a7c961790833cd595104af9ad115e9
2400032210 <2400032210@kluniversity.in>
2025-12-31 10:12:44 +0530

    Fix issue #5923: Simple Program That Prints The Sum of Two Numbers

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,3 +1,10 @@
 // JavaScript - Easy
 
 // TODO: Write a function that returns the sum of two numbers
+
+function sum(a, b) {
+  return a + b;
+}
+
+const result = sum(1, 2);
+console.log(`The sum of 1 and 2 is ${result}.`);
