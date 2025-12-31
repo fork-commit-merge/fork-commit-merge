0678ae79c14cb5e2dd1b8665a5935b60d48dd3cd
GitHub Action <action@github.com>
2025-12-31 07:41:56 +0000

    Revert "Merge pull request #5961 from 2400032210/lokeshbranch"
    This reverts commit e2c9a102c9a72de7c59adfea46deffad78219280, reversing
    changes made to 6ca67686133a7aad252d25028c9ddfcef265ad28.

--- tasks/javascript/easy/even-numbers.js
+++ tasks/javascript/easy/even-numbers.js
@@ -1,5 +1,6 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// JavaScript - Easy
 
-const evenNumbers = numbers.filter(num => num % 2 === 0);
+// TODO:return a new array containing all even numbers using a .filter() method:
 
-console.log(evenNumbers);
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// output: [2, 4, 6, 8, 10]
