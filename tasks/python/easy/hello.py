f9c7eb56563f2a24d3d754c05f46c39de8056bf3
GitHub Action <action@github.com>
2026-01-03 11:32:12 +0000

    Revert "Merge pull request #6118 from SrideviS-14/fix-issue"
    This reverts commit 83bfb83402a024fe167117e1ea5e7331fc7ae082, reversing
    changes made to e63258804a3524578ca3986b6374aba0626ec925.

--- tasks/python/easy/hello.py
+++ tasks/python/easy/hello.py
@@ -1,5 +1,3 @@
 # Python - Easy
 
 # TODO: Write a function that prints "Hello, Python!" to the console
-def hello_python():
-    print("Hello, Python!")
--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,5 +1,3 @@
 # Python - Easy
 
 # TODO: Write a function that returns the sum of two numbers
-def number_sum(a, b):
-    return a + b
