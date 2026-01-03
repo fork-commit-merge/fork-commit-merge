22f494aefaaf85ee36b18612c8483da128e0648f
GitHub Action <action@github.com>
2026-01-03 11:36:17 +0000

    Revert "Merge pull request #6122 from DaniyalMShehzad/issue-6074-sum-of-two-numbers"
    This reverts commit f2e44fad1ab23ba4edf1a4e2248121161e5137db, reversing
    changes made to 06fa86816ee191a98048ea18becd9e3219d38526.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,10 +1,3 @@
 // JavaScript - Easy
 
 // TODO: Write a function that returns the sum of two numbers
-function sum(a, b) {
-    const result = a + b;
-    console.log(`The sum of ${a} and ${b} is ${result}.`);
-    return result;
-}
-// usage example
-sum(1, 2);
