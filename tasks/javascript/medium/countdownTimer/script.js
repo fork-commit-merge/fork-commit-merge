e63258804a3524578ca3986b6374aba0626ec925
GitHub Action <action@github.com>
2026-01-03 11:31:29 +0000

    Revert "Merge pull request #6117 from gowtham1412-p/main"
    This reverts commit 40390cd3a03b45741495228699ac047dcb2d1765, reversing
    changes made to 8bffca40c7c90187cd2b6bebf771db0645473ccd.

--- tasks/javascript/medium/countdownTimer/script.js
+++ tasks/javascript/medium/countdownTimer/script.js
@@ -1,19 +1,3 @@
 // JavaScript - Medium
 
 // Create functionality for a countdown timer that counts down from 10 seconds to zero using JavaScript
-function countdownTimer() {
-    let seconds = 10;
-    
-    const timer = setInterval(() => {
-        console.log(seconds);
-        seconds--;
-        
-        if (seconds < 0) {
-            console.log("Time's up!");
-            clearInterval(timer);
-        }
-    }, 1000);
-}
-
-// Start the countdown
-countdownTimer();
