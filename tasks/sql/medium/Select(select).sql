e420aec9eab1001a665fa4b4684f784fccf51fc4
DaniyalMShehzad <daniyalshehzad305@gmail.com>
2026-01-04 20:55:24 +0500

    feat: add nested SQL query for population comparison

--- tasks/sql/medium/Select(select).sql
+++ tasks/sql/medium/Select(select).sql
@@ -10,3 +10,16 @@
 
 -- |   Name       |  Continent  |     Area    |  Population  |     GDP     |
 -- |--------------|-------------|-------------|--------------|-------------|
+
+SELECT Name, Population
+FROM World
+WHERE Population > (
+    SELECT Population
+    FROM World
+    WHERE Name = 'Canada'
+)
+AND Population < (
+    SELECT Population
+    FROM World
+    WHERE Name = 'Poland'
+);
