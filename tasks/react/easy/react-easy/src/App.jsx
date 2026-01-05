5195209219af80bd6c6d1b19b9f333b71f98abec
Arjun Rathore <arjun.rathore1812@gmail.com>
2026-01-05 15:05:57 +0530

    Fixed issue

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -4,7 +4,18 @@ import "./index.css";
 function App() {
   return (
     <div className="center">
-      {/* TODO: Implement the card component */}
+      <div className="center">
+        <h1>React/Vite - Easy</h1>
+
+        <div className="card">
+          <img src="https://picsum.photos/320/240" alt="Random scenic view" />
+          <h2>Card Title</h2>
+          <p>
+            A randomly generated image showcasing beautiful scenery, patterns,
+            or everyday moments captured from around the world.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
