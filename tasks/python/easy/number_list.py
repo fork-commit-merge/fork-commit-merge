828d356ba041af72075972146673d0cc0f3da146
tanisha0423 <0120tanisha@gmail.com>
2026-01-06 01:32:17 +0530

    Solve easy python task list

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -5,5 +5,10 @@
 # prints that list to the console and,
 # prints the sum of the numbers of that list
 
-def function(numbers):
-    pass
+def function():
+    numbers = list(range(1,11))
+    print(numbers)
+    print(sum(numbers))
+
+
+function()
