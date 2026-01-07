1adaca4c858e28d43b69138ca32cc3d462cda475
[Tanushree-122] <[tanushreesaxena34@gmail.com]>
2026-01-08 04:23:22 +0530

    Added counter

--- tasks/javascript/medium/countdownTimer/script.js
+++ tasks/javascript/medium/countdownTimer/script.js
@@ -1,3 +1,16 @@
 // JavaScript - Medium
 
 // Create functionality for a countdown timer that counts down from 10 seconds to zero using JavaScript
+let timeLeft = 10;
+
+const timerDisplay = document.getElementById("timer");
+
+const countdown = setInterval(() => {
+  if (timeLeft > 0) {
+    timerDisplay.textContent = timeLeft;
+    timeLeft--;
+  } else {
+    clearInterval(countdown);
+    timerDisplay.textContent = "Time's up!";
+  }
+}, 1000);
