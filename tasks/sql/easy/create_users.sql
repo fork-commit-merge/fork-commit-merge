c4e5b88c7dbe4d6b41a456acab3de1942e633512
GitHub Action <action@github.com>
2026-01-08 07:03:59 +0000

    Revert "Merge pull request #6142 from Akshay1833/sql-easy"
    This reverts commit af580d499a1b70a163626f6dd05fef6045b52a52, reversing
    changes made to 5019b492a17011739fb4642149813f83e82ade8d.

--- tasks/sql/easy/create_users.sql
+++ tasks/sql/easy/create_users.sql
@@ -2,9 +2,5 @@
 
 CREATE TABLE users (
     -- TODO: Add all the requested fields
-    id integer primary key AUTOINCREMENT,
-    name text,
-    email text
 );
 
-INSERT INTO users (name, email) VALUES('John Smith', 'john.smith@gmail.com');
