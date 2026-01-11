47c2e2156123476c7a0594013d17f4e8e5dc73c7
GitHub Action <action@github.com>
2026-01-11 07:25:03 +0000

    Revert "Merge pull request #6154 from Obi1-x/solve-tasks"
    This reverts commit 33da59e5cc81061a0dbaac6f846a26a84130812a, reversing
    changes made to d05bf7c542112f2432d86f6dde01516bbfc7e5ff.

--- tasks/javascript/medium/countdownTimer/script.js
+++ tasks/javascript/medium/countdownTimer/script.js
@@ -1,19 +1,3 @@
 // JavaScript - Medium
 
 // Create functionality for a countdown timer that counts down from 10 seconds to zero using JavaScript
-
-document.addEventListener("DOMContentLoaded", () => {
-    const timerElement = document.getElementById("timer")
-    var time = 10
-
-    const clock = setInterval(() => {
-        time -= 1
-        timerElement.setHTML(`00:0${time}`)
-        console.log(`The time is: 00:0${time}`);
-
-        if(time == 0) {
-            clearInterval(clock)
-            alert("Time's up!")
-        }
-    }, 1000);
-});
