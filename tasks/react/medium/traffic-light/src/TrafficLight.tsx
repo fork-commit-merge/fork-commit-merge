21e25f4068d74a9aaa8b5b0bed1779a85fe94c1a
GitHub Action <action@github.com>
2026-01-11 16:07:38 +0000

    Revert "Merge pull request #6157 from gmnabeel/fix/6085-traffic-light"
    This reverts commit 75f798f51e3fdc57204967ef3d4cdc16a67a6430, reversing
    changes made to 2748c031aaa2e6d875bf8164061ea48b87e7aea1.

--- tasks/react/medium/traffic-light/src/TrafficLight.tsx
+++ tasks/react/medium/traffic-light/src/TrafficLight.tsx
@@ -1,37 +1,11 @@
-import { useEffect, useState } from "react";
+// React.js - Medium 2
 import "./App.css";
 
-const sequence = [
-  { color: "red", duration: 4000 },
-  { color: "yellow", duration: 3000 },
-  { color: "green", duration: 1000 },
-];
+// TODO: Implement  the TrafficLight component
 
 export function TrafficLight() {
-  const [active, setActive] = useState("red");
-
-  useEffect(() => {
-    let index = 0;
-
-    const runTrafficLight = () => {
-      const { color, duration } = sequence[index];
-      setActive(color);
-
-      index = (index + 1) % sequence.length;
-
-      timeoutId = setTimeout(runTrafficLight, duration);
-    };
-
-    let timeoutId = setTimeout(runTrafficLight, sequence[0].duration);
-
-    return () => clearTimeout(timeoutId);
-  }, []);
 
   return (
-    <section className="traffic-light">
-      <div className={`light red ${active === "red" ? "active" : ""}`} />
-      <div className={`light yellow ${active === "yellow" ? "active" : ""}`} />
-      <div className={`light green ${active === "green" ? "active" : ""}`} />
-    </section>
+    // TODO: return JSX here
   );
 }
