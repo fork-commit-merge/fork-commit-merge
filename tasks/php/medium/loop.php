2748c031aaa2e6d875bf8164061ea48b87e7aea1
GitHub Action <action@github.com>
2026-01-11 09:53:43 +0000

    Revert "Merge pull request #6156 from shivt-F5/JanTen"
    This reverts commit b80ddda24b62ab7e6a221bf94d9e13bb394529a1, reversing
    changes made to a003629d70dcd42f5e06fc8d57a3cad161521429.

--- tasks/php/medium/loop.php
+++ tasks/php/medium/loop.php
@@ -1,7 +1,7 @@
 <?php
+
+// PHP - Medium 1
+
 $data = ['Pizza', 'Chicken', 'Beef', 'Rice', 'Bread'];
 
-foreach ($data as $item) {
-    echo $item . "\n";
-}
-?>
+// TODO: Display All Data From The Array
