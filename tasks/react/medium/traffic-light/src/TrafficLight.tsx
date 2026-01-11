2e932a574f15192a80a775699e36c7c5c827f1b3
Nabeel G M <gmnabeel@gmail.com>
2026-01-11 21:25:34 +0530

    fix(Traffic Light): [#6085] Traffic Light Problem solution with hooks + added styles

--- tasks/react/medium/traffic-light/src/TrafficLight.tsx
+++ tasks/react/medium/traffic-light/src/TrafficLight.tsx
@@ -1,11 +1,37 @@
-// React.js - Medium 2
+import { useEffect, useState } from "react";
 import "./App.css";
 
-// TODO: Implement  the TrafficLight component
+const sequence = [
+  { color: "red", duration: 4000 },
+  { color: "yellow", duration: 3000 },
+  { color: "green", duration: 1000 },
+];
 
 export function TrafficLight() {
+  const [active, setActive] = useState("red");
+
+  useEffect(() => {
+    let index = 0;
+
+    const runTrafficLight = () => {
+      const { color, duration } = sequence[index];
+      setActive(color);
+
+      index = (index + 1) % sequence.length;
+
+      timeoutId = setTimeout(runTrafficLight, duration);
+    };
+
+    let timeoutId = setTimeout(runTrafficLight, sequence[0].duration);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
 
   return (
-    // TODO: return JSX here
+    <section className="traffic-light">
+      <div className={`light red ${active === "red" ? "active" : ""}`} />
+      <div className={`light yellow ${active === "yellow" ? "active" : ""}`} />
+      <div className={`light green ${active === "green" ? "active" : ""}`} />
+    </section>
   );
 }
