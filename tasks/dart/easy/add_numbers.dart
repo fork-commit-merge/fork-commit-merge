a6ba8f452819fa2aa44a56a3167ecc975e7b629f
shivani <54018320+shivt-F5@users.noreply.github.com>
2026-01-13 17:30:47 +0400

    dart

--- tasks/dart/easy/add_numbers.dart
+++ tasks/dart/easy/add_numbers.dart
@@ -1,6 +1,8 @@
 void main() {
-  // int sum = addNumbers(5, 10);
-  // print('The sum is: $sum');
+  int sum = addNumbers(5, 10);
+  print('The sum is: $sum');
 }
 
-// TODO: Implement addNumbers function that returns the sum of two numbers
+int addNumbers(int num1, int num2) {
+  return num1 + num2;
+}
