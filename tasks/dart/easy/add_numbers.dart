5cfff1ed3e6b33e7ad3bc9bd6e51dc6d8a500fdc
GitHub Action <action@github.com>
2026-01-14 06:03:47 +0000

    Revert "Merge pull request #6160 from shivt-F5/jan13"
    This reverts commit 278b51e0d71ca8966bc69d38d899fe0c185c3a31, reversing
    changes made to 7950524b4c5e843ab1a9f3e8687e597d3fde28c3.

--- tasks/dart/easy/add_numbers.dart
+++ tasks/dart/easy/add_numbers.dart
@@ -1,8 +1,6 @@
 void main() {
-  int sum = addNumbers(5, 10);
-  print('The sum is: $sum');
+  // int sum = addNumbers(5, 10);
+  // print('The sum is: $sum');
 }
 
-int addNumbers(int num1, int num2) {
-  return num1 + num2;
-}
+// TODO: Implement addNumbers function that returns the sum of two numbers
