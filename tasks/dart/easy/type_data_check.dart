36a46124c4c0e663cb1387fb6a7fe6a1d9e1dfe1
GitHub Action <action@github.com>
2026-01-16 10:58:10 +0000

    Revert "Merge pull request #6164 from shivt-F5/jan16"
    This reverts commit 2fa0c17a7614c88d0e0fe02c8196f2e75c7851c7, reversing
    changes made to 9cd20628386662db34a95e61fd177e5014e39be8.

--- tasks/dart/easy/type_data_check.dart
+++ tasks/dart/easy/type_data_check.dart
@@ -1,23 +1,9 @@
 void main() {
-  print(typeDataCheck(5));       
-  print(typeDataCheck(3.14));    
-  print(typeDataCheck("Hello")); 
-  print(typeDataCheck(true));    
-  print(typeDataCheck(null));    
+  // print(typeDataCheck(5));
+  // print(typeDataCheck(3.14));
+  // print(typeDataCheck("Hello"));
+  // print(typeDataCheck(true));
+  // print(typeDataCheck(null));
 }
 
-String typeDataCheck(dynamic value) {
-  if (value == null) {
-    return "null";
-  } else if (value is int) {
-    return "int";
-  } else if (value is double) {
-    return "double";
-  } else if (value is String) {
-    return "String";
-  } else if (value is bool) {
-    return "bool";
-  } else {
-    return "Unknown type";
-  }
-}
+// TODO: Implement a function that accepts a parameter of any type and returns a string describing its data type.
