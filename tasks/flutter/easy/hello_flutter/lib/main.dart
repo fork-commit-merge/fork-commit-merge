e4c829fe88a5190472e8455f123100e4970e33b5
GitHub Action <action@github.com>
2026-01-20 06:46:19 +0000

    Revert "Merge pull request #6171 from muhammadhassanmahar/main"
    This reverts commit e6e242a45210da8a0aef698901f3f51060e292ad, reversing
    changes made to 71846457f598c14b3c85fb618dd482a657eab9c5.

--- tasks/flutter/easy/hello_flutter/lib/main.dart
+++ tasks/flutter/easy/hello_flutter/lib/main.dart
@@ -1,26 +1,19 @@
+// Flutter - Easy
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ForkCommitMerge());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ForkCommitMerge extends StatelessWidget {
+  const ForkCommitMerge({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Fork, Commit, Merge'),
-        ),
-        body: const Center(
-          child: Text(
-            'Hello, Flutter!',
-            style: TextStyle(fontSize: 24),
-          ),
-        ),
+          // TODO: Implement the appbar and body properties
       ),
     );
   }
