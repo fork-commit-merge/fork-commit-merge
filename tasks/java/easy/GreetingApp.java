7742753f3ed109d9ea43a39947ff673b2585bf76
dagioups <dagioups@csd.auth.gr>
2026-01-21 21:24:38 +0200

    Greeting App Implementation

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,8 +1,13 @@
 // Java - Easy
+import java.util.Scanner;
 
 public class GreetingApp {
-
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+        System.out.println("Hello, " + name + "! Nice to meet you!");
+        input.close();
 
     }
 }
