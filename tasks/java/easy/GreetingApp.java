5edc8497d7aab81f8bcfbad6b14771f319115d54
GitHub Action <action@github.com>
2026-01-21 22:12:43 +0000

    Revert "Merge pull request #6177 from dagioups/issue#6076/greeting_app"
    This reverts commit 4cdd352b724334a79d913d4d9c9244549a6472f6, reversing
    changes made to 397d27fb45312b7fe6d2715b96f1d8274f160944.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,13 +1,8 @@
 // Java - Easy
-import java.util.Scanner;
 
 public class GreetingApp {
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = input.nextLine();
-        System.out.println("Hello, " + name + "! Nice to meet you!");
-        input.close();
 
     }
 }
