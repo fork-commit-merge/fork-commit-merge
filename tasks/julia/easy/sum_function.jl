91dc3a21b645f6fa3ced98b4fe2fa8820438ac9a
GitHub Action <action@github.com>
2026-01-23 15:03:44 +0000

    Revert "Merge pull request #6179 from shivt-F5/jan23"
    This reverts commit 70a67ef4ef9f9ca348955a5b9f4beab5270828de, reversing
    changes made to 820b4d69ed56c39113ea569ceba87416a4ce89fc.

--- tasks/julia/easy/sum_function.jl
+++ tasks/julia/easy/sum_function.jl
@@ -1,14 +1,3 @@
 # Julia - Easy
 
-function print_sum(a, b)
-    sum = a + b
-    println("The sum of $a and $b is: $sum")
-end
-
-println("Enter the first number:")
-a = parse(Float64, readline())
-
-println("Enter the second number:")
-b = parse(Float64, readline())
-
-print_sum(a, b)
+# TODO: Implement the function required in the problem description here
