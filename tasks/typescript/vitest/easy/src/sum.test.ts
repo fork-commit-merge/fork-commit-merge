820b4d69ed56c39113ea569ceba87416a4ce89fc
GitHub Action <action@github.com>
2026-01-23 00:25:04 +0000

    Revert "Merge pull request #6178 from shivt-F5/jan22"
    This reverts commit b604dad5d8a13e59a8cdc3f5a7b3f55fa0f47c3f, reversing
    changes made to 451944dd2bf26c06dc3a0545a3f99aae86ff37a3.

--- tasks/typescript/vitest/easy/src/sum.test.ts
+++ tasks/typescript/vitest/easy/src/sum.test.ts
@@ -3,8 +3,4 @@
 import sum from "./sum";
 import { describe, expect, it } from "vitest";
 
-describe("sum", () => {
-  it("returns 0 when called with no numbers", () => {
-    expect(sum()).toBe(0);
-  });
-});
+// TODO: Create the test required in the task
