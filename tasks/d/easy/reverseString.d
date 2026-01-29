20be2fd86d4ddd8747a92350af987e731e8b9bab
GitHub Action <action@github.com>
2026-01-29 07:06:02 +0000

    Revert "Merge pull request #6184 from shivt-F5/jan282"
    This reverts commit 4d51548b410c780f57569dd2d241ee4b64cc7686, reversing
    changes made to 52d3fe75661f77ef143b29a650f16307c323efd2.

--- tasks/d/easy/reverseString.d
+++ tasks/d/easy/reverseString.d
@@ -3,20 +3,5 @@
 import std.stdio;
 
 void main() {
-import std.stdio;
-import std.algorithm;
-import std.array;
-
-void main()
-{
-    string original = "Hello, World!";
-
-string reversed = original
-        .retro        
-        .array;      
-
-    writeln("Original String: ", original);
-    writeln("Reversed String: ", reversed);
-}
-	
+    // TODO: Implement program to reverse a string
 }
