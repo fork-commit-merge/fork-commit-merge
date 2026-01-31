150be127e45de8815812b051f3434ec97f73b471
GitHub Action <action@github.com>
2026-01-31 14:22:22 +0000

    Revert "Merge pull request #6192 from shivt-F5/jan31"
    This reverts commit 29bba7d3ed75fc8c4c2113ecbdc108d49d8dd0fb, reversing
    changes made to f6765defddf5aeb0d1cb0b9bb9daf78a883c44e2.

--- tasks/lua/easy/temperature_converter.lua
+++ tasks/lua/easy/temperature_converter.lua
@@ -8,35 +8,4 @@ local function fahrenheitToCelsius(fahrenheit)
     return (fahrenheit - 32) * 5/9
 end
 
-print("Choose type of conversion: (1) Fahrenheit to Celsius or (2) Celsius to Fahrenheit")
-local choice = io.read()
-
-if choice == "1" then
-    print("Enter temperature in Fahrenheit:")
-    local input = io.read()
-    local fahrenheit = tonumber(input)
-
-    if not fahrenheit then
-        print("Invalid input. Please enter a numeric value.")
-        return
-    end
-
-    local celsius = fahrenheitToCelsius(fahrenheit)
-    print(string.format("The temperature is %.2f degrees Celsius.", celsius))
-
-elseif choice == "2" then
-    print("Enter temperature in Celsius:")
-    local input = io.read()
-    local celsius = tonumber(input)
-
-    if not celsius then
-        print("Invalid input. Please enter a numeric value.")
-        return
-    end
-
-    local fahrenheit = celsiusToFahrenheit(celsius)
-    print(string.format("The temperature is %.2f degrees Fahrenheit.", fahrenheit))
-
-else
-    print("Invalid choice. Please select 1 or 2.")
-end
+-- TODO: Implement rest of the Temperature Converter
