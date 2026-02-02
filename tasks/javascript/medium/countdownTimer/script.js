ea4e1e62add8a5e101de0fde744f1064326b1065
GitHub Action <action@github.com>
2026-02-02 18:28:33 +0000

    Revert "Merge pull request #6341 from 2d-game-dev/fix-issue-6193"
    This reverts commit a4ac3ef1cb9c3f8da81967c37310d4b78aa15b28, reversing
    changes made to 05926a4238c14aa7a0ad5ca17ad2966be4b5384a.

--- tasks/javascript/medium/countdownTimer/script.js
+++ tasks/javascript/medium/countdownTimer/script.js
@@ -1,13 +1,3 @@
 // JavaScript - Medium
 
 // Create functionality for a countdown timer that counts down from 10 seconds to zero using JavaScript
-const time =document.getElementById('timer');
-let seconds = 10;
-const countdown = setInterval(() => {
-    time.textContent = seconds;
-    seconds--;
-    if (seconds < 0) {
-        clearInterval(countdown);
-        time.textContent = "Time's up!";
-    }
-}, 1000);
