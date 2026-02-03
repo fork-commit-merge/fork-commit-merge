3c295706f328be7e5962a0070fc6b3699447e455
GitHub Action <action@github.com>
2026-02-03 05:28:57 +0000

    Revert "Merge pull request #6342 from Rubick65/fix-issue"
    This reverts commit 7438403337503e270c948900b70f06ee8a3e83de, reversing
    changes made to 9041b51c762d3bda3e538bc0d47f7d59b3869a93.

--- tasks/javascript/medium/product-stock.js
+++ tasks/javascript/medium/product-stock.js
@@ -1,35 +1,13 @@
 // JavaScript - Medium
 
-// TODO: return only products that are in stock using .filter() method;
-// Extra Challenge: return only the names of products in stock;
+  // TODO: return only products that are in stock using .filter() method;
+  // Extra Challenge: return only the names of products in stock;
 
 const products = [
-    {name: "Book", inStock: true},
-    {name: "Pen", inStock: false},
-    {name: "Phone", inStock: true},
-    {name: "Mug", inStock: false}
-];
-// TODO output = [ { name: 'Book', inStock: true }, { name: 'Phone', inStock: true } ];
-// Challenge output = ['Book', 'Phone'];
-
-
-function productsNameWithStock() {
-
-   return products
-        .filter(product => product.inStock)
-        .map(product => product.name);
-
-
-}
-
-console.log(productsNameWithStock());
-
-// inStockProducts = products.filter(product => product.inStock)
-// console.log(inStockProducts)
-//
-// nombre = []
-// inStockProducts.forEach(product => {
-//     nombre.push(product.name)
-// })
-//
-// console.log(nombre)
+    { name: "Book", inStock: true },
+    { name: "Pen", inStock: false },
+    { name: "Phone", inStock: true },
+    { name: "Mug", inStock: false }
+  ];
+  // TODO output = [ { name: 'Book', inStock: true }, { name: 'Phone', inStock: true } ];
+  // Challenge output = ['Book', 'Phone'];
