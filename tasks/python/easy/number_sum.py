e622638eab27ad1f0ce5b98ae0d874ec626f73c1
abdussamad567 <abdussamad5826@gmail.com>
2026-02-05 11:51:34 +0530

    Add function to return sum of two numbers in python

--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,3 +1,8 @@
 # Python - Easy
 
 # TODO: Write a function that returns the sum of two numbers
+def sum_two_numbers(a, b):
+    return a + b
+
+result = sum_two_numbers(3, 5) # type: ignore
+print(result)  # Output should be 8
