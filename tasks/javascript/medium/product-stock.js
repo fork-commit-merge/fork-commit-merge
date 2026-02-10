3774788ae06720acd86e4174e653e4331a68cf08
GitHub Action <action@github.com>
2026-02-10 13:44:25 +0000

    Revert "Merge pull request #6356 from brijesh-dt/fix-issue"
    This reverts commit d05b705f866fb90e9e2e96601bd7d4001287a02e, reversing
    changes made to 6acd500f42d68426437bafd77b2648e8e7e59a98.

--- tasks/javascript/medium/product-stock.js
+++ tasks/javascript/medium/product-stock.js
@@ -1,25 +1,13 @@
 // JavaScript - Medium
 
-// TODO: return only products that are in stock using .filter() method;
-// Extra Challenge: return only the names of products in stock;
+  // TODO: return only products that are in stock using .filter() method;
+  // Extra Challenge: return only the names of products in stock;
 
 const products = [
-  { name: "Book", inStock: true },
-  { name: "Pen", inStock: false },
-  { name: "Phone", inStock: true },
-  { name: "Mug", inStock: false },
-];
-
-const newProducts = products.filter((item) => item.inStock == true);
-const challengeOutput = [];
-
-products.map((item) => {
-  if (item.inStock) {
-    challengeOutput.push(item.name);
-  }
-});
-
-console.log(newProducts);
-console.log(challengeOutput);
-// TODO output = [ { name: 'Book', inStock: true }, { name: 'Phone', inStock: true } ];
-// Challenge output = ['Book', 'Phone'];
+    { name: "Book", inStock: true },
+    { name: "Pen", inStock: false },
+    { name: "Phone", inStock: true },
+    { name: "Mug", inStock: false }
+  ];
+  // TODO output = [ { name: 'Book', inStock: true }, { name: 'Phone', inStock: true } ];
+  // Challenge output = ['Book', 'Phone'];
