d46b2f9f4dec7ff34243d79aec7742266b51100a
GitHub Action <action@github.com>
2026-02-16 19:50:22 +0000

    Revert "Merge pull request #6364 from jikrana-del/sum-number"
    This reverts commit 8e73a7ae2e23aa9d48e1ccaf3070be55dffb55e4, reversing
    changes made to 4b268d24557beb3dea8a821893a1eca174f09520.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,10 +1,3 @@
 // JavaScript - Easy
 
 // TODO: Write a function that returns the sum of two numbers
-
-let a1 = 3;
-let a2 = 5;
-const sum = (a, b) => {
-  return a + b;
-}
-console.log(`The sum of ${a1} and ${a2} is ${sum(a1, a2)}`);
