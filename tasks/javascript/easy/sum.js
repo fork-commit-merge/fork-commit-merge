7f8812bdd2b9f81e007572a06e77210387ddd844
GitHub Action <action@github.com>
2026-02-16 19:53:03 +0000

    Revert "Merge pull request #6366 from Vikram-sardiwal/sum-feature"
    This reverts commit 71f825323937382ec1640a445da804aeff238354, reversing
    changes made to 1d5baecaa2a908404a3ca681ff8fdeb8931c2ace.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,11 +1,3 @@
 // JavaScript - Easy
-a= 10;
-b= 5;
-sum = a+b;
-console.log(sum)
-// TODO: Write a function that returns the sum of two numbers
 
-function sum(a,b){
-    return a+b;
-}
-console.log(sum(5,7))
+// TODO: Write a function that returns the sum of two numbers
