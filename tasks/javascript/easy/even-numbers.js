8e2dc57d1b74ac5c47a0feba9c07f127789db278
GitHub Action <action@github.com>
2026-02-17 11:26:14 +0000

    Revert "Merge pull request #6369 from Shaylapham-05/fix-issue"
    This reverts commit a84476b61aee4d12d43b0fa091d97ae4021d8bef, reversing
    changes made to f276d4c8e28317d82c4f1ab7c457955e65fa2220.

--- tasks/javascript/easy/even-numbers.js
+++ tasks/javascript/easy/even-numbers.js
@@ -4,5 +4,3 @@
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // output: [2, 4, 6, 8, 10]
-const getEvenNumbers = (arr) => arr.filter((num) => num % 2 === 0);
-console.log(getEvenNumbers(array));
