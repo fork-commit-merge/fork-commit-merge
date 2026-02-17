d60998ef34ef7c658b4927ade4d9ff56c4631ae6
GitHub Action <action@github.com>
2026-02-17 16:37:51 +0000

    Revert "Merge pull request #6371 from Vikram-sardiwal/sum-feature"
    This reverts commit ea98432fca6adab48ceb032aa65b6100b1afacc3, reversing
    changes made to d552a64db3aaf4fd8b7a5653106c05bd670046e6.

--- tasks/javascript/easy/greater-than.js
+++ tasks/javascript/easy/greater-than.js
@@ -3,6 +3,4 @@
 // TODO: return a new array with all numbers greater than 10 but less than 20;
 
 const array = [2, 5, 8, 10, 12, 15, 19, 20, 25];
-const result = array.filter(num => num > 10 && num < 20);
-console.log(result);
 // output: [12, 15, 19];
