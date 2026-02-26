d8f686375019f7606492049e45c9cabac5de3afa
GitHub Action <action@github.com>
2026-02-26 20:34:08 +0000

    Revert "Merge pull request #6381 from AliyaRazyapova/easy-julia-sum"
    This reverts commit 2ffa8d1997965d4b003a16730bc2ac1f26588ad9, reversing
    changes made to 8763a1b6a5efbe954824b0cf22cd2267f1744b02.

--- tasks/julia/easy/sum_function.jl
+++ tasks/julia/easy/sum_function.jl
@@ -1,15 +1,3 @@
 # Julia - Easy
-function sum_two_numbers(a, b)
-    return a + b
-end
 
-function test_sum()
-    test_pairs = [(5, 10), (3, 7), (-2, 2), (0, 0), (1.5, 2.5)]
-
-    for (a, b) in test_pairs
-        result = sum_two_numbers(a, b)
-        println("The sum of $a and $b is: $result")
-    end
-end
-
-test_sum()
+# TODO: Implement the function required in the problem description here
