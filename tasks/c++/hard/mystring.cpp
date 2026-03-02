0e15df5c7f1572ffeecb3f7fcf7fdf07640df81b
KINnao087 <3535297641@qq.com>
2026-03-02 16:26:22 +0800

    Implement MyString class

--- tasks/c++/hard/mystring.cpp
+++ tasks/c++/hard/mystring.cpp
@@ -1,42 +1,107 @@
+#include "mystring.h"
+
 #include <cstring>
 #include <iostream>
 
+
+void MyString::__add_data_length(int len)
+{
+    // std::cout << len << std::endl;
+
+    char* new_data = new char[_data_length + len + 1];
+
+    if (_data_length > 0)
+    {
+        memcpy(new_data, _data, _data_length);
+        delete[] _data;
+    }
+
+    _data_length += len + 1;
+
+    _data = new_data;
+}
+
 MyString::MyString(const char* s) {
-    // TODO: Implement this
+    int n = strlen(s);
+
+    __add_data_length(n);
+    _length = n;
+
+    strcpy(_data, s);
+}
+
+MyString::MyString()
+{
 }
 
 MyString::MyString(const MyString& other) {
-    // TODO: Implement this
+    int n = other.length();
+
+    __add_data_length(n);
+    _length = n;
+
+    strcpy(_data, other.c_str());
+}
+
+MyString::~MyString()
+{
+    if (_data_length > 0) delete[] _data;
 }
 
 int MyString::length() const {
-    // TODO: Implement this
+    return _length;
 }
 
 const char* MyString::c_str() const {
-    // TODO: Implement this
+    return _data;
 }
 
 void MyString::append(const MyString& other) {
-    // TODO: Implement this
+    int n = other.length();
+    while (n + _length > _data_length)
+        __add_data_length(_data_length);
+
+    for (int i = 0; i < n; i++) {_data[i + _length] = other[i];}
+    _data[n + _length] = 0;
+    _length = n + _length;
 }
 
 int MyString::compare(const MyString& other) const {
-    // TODO: Implement this
+    return strcmp(_data, other.c_str());
 }
 
 MyString& MyString::operator=(const MyString& other) {
-    // TODO: Implement this
+    while (_data_length < other.length())
+        __add_data_length(_data_length);
+
+    for (int i = 0; i < other.length(); i++)
+        _data[i] = other[i];
+
+    _data[other.length()] = 0;
+    _length = other.length();
+    return *this;
 }
 
 MyString MyString::operator+(const MyString& other) const {
-    // TODO: Implement this
+    MyString result(*this);
+    result.append(other);
+    return result;
 }
 
 bool MyString::operator==(const MyString& other) const {
-    // TODO: Implement this
+    return MyString::compare(other) == 0;
 }
 
 std::ostream& operator<<(std::ostream& os, const MyString& s) {
-    // TODO: Implement this
+    os << s.c_str();
+    return os;
+}
+
+char& MyString::operator[](int i) const
+{
+    return _data[i];
+}
+char& MyString::operator[](int i)
+{
+    return _data[i];
 }
