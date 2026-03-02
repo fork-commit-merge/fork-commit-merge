b39174f6c858d38453ab3b8af5826e1fe67c552f
GitHub Action <action@github.com>
2026-03-02 15:06:52 +0000

    Revert "Merge pull request #6533 from KINnao087/fix-issue"
    This reverts commit 72b1d211237060dd1ac726cfb2405d1397145009, reversing
    changes made to fa352486fedfb340bedcd06d341575d6e0b5836f.

--- tasks/c++/hard/mystring.cpp
+++ tasks/c++/hard/mystring.cpp
@@ -1,107 +1,42 @@
-#include "mystring.h"
-
 #include <cstring>
 #include <iostream>
 
-
-void MyString::__add_data_length(int len)
-{
-    // std::cout << len << std::endl;
-
-    char* new_data = new char[_data_length + len + 1];
-
-    if (_data_length > 0)
-    {
-        memcpy(new_data, _data, _data_length);
-        delete[] _data;
-    }
-
-    _data_length += len + 1;
-
-    _data = new_data;
-}
-
 MyString::MyString(const char* s) {
-    int n = strlen(s);
-
-    __add_data_length(n);
-    _length = n;
-
-    strcpy(_data, s);
-}
-
-MyString::MyString()
-{
+    // TODO: Implement this
 }
 
 MyString::MyString(const MyString& other) {
-    int n = other.length();
-
-    __add_data_length(n);
-    _length = n;
-
-    strcpy(_data, other.c_str());
-}
-
-MyString::~MyString()
-{
-    if (_data_length > 0) delete[] _data;
+    // TODO: Implement this
 }
 
 int MyString::length() const {
-    return _length;
+    // TODO: Implement this
 }
 
 const char* MyString::c_str() const {
-    return _data;
+    // TODO: Implement this
 }
 
 void MyString::append(const MyString& other) {
-    int n = other.length();
-    while (n + _length > _data_length)
-        __add_data_length(_data_length);
-
-    for (int i = 0; i < n; i++) {_data[i + _length] = other[i];}
-    _data[n + _length] = 0;
-    _length = n + _length;
+    // TODO: Implement this
 }
 
 int MyString::compare(const MyString& other) const {
-    return strcmp(_data, other.c_str());
+    // TODO: Implement this
 }
 
 MyString& MyString::operator=(const MyString& other) {
-    while (_data_length < other.length())
-        __add_data_length(_data_length);
-
-    for (int i = 0; i < other.length(); i++)
-        _data[i] = other[i];
-
-    _data[other.length()] = 0;
-    _length = other.length();
-    return *this;
+    // TODO: Implement this
 }
 
 MyString MyString::operator+(const MyString& other) const {
-    MyString result(*this);
-    result.append(other);
-    return result;
+    // TODO: Implement this
 }
 
 bool MyString::operator==(const MyString& other) const {
-    return MyString::compare(other) == 0;
+    // TODO: Implement this
 }
 
 std::ostream& operator<<(std::ostream& os, const MyString& s) {
-    os << s.c_str();
-    return os;
-}
-
-char& MyString::operator[](int i) const
-{
-    return _data[i];
-}
-char& MyString::operator[](int i)
-{
-    return _data[i];
+    // TODO: Implement this
 }
--- tasks/c++/hard/mystring.h
+++ tasks/c++/hard/mystring.h
@@ -1,40 +1,8 @@
 // C++ - Hard
 
-#ifndef MYSTRING_H
-#define MYSTRING_H
+define MYSTRING_H
 #include <iostream>
 
 class MyString {
-    char* _data = 0;
-    int _data_length = 0;
-    int _length = 0;
-
-    void __add_data_length(int len);
-
-public:
-
-    MyString(const char* s) ;
-    MyString(const MyString& other) ;
-    MyString() ;
-
-
-    ~MyString() ;
-
-    int length() const ;
-
-    const char* c_str() const ;
-
-    void append(const MyString& other) ;
-    int compare(const MyString& other) const ;
-
-    MyString& operator=(const MyString& other) ;
-    MyString operator+(const MyString& other) const ;
-    bool operator==(const MyString& other) const ;
-
-    char& operator[](int i) const ;
-    char& operator[](int i) ;
-
-
-};
-std::ostream& operator<<(std::ostream& os, const MyString& s) ;
-#endif
+    // TODO: Implement MyString class
+}
