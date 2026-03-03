a18db7723bc5fb796ba00df47aafeada79aebc31
GitHub Action <action@github.com>
2026-03-03 10:12:46 +0000

    Revert "Merge pull request #6534 from travisbreaks/js-medium-countdown-timer"
    This reverts commit de1846cf89cbd6067193f8e57c90b7f0cd7557d8, reversing
    changes made to dc3af8f4f7976fa12ffa4be2b6e84e2e6b9ba96b.

--- tasks/javascript/medium/countdownTimer/script.js
+++ tasks/javascript/medium/countdownTimer/script.js
@@ -1,19 +1,3 @@
 // JavaScript - Medium
 
-// Countdown timer that counts down from 10 seconds to zero
-
-const timerDisplay = document.getElementById("timer");
-let timeLeft = 10;
-
-const countdown = setInterval(() => {
-  timeLeft--;
-
-  if (timeLeft > 0) {
-    const minutes = String(Math.floor(timeLeft / 60)).padStart(2, "0");
-    const seconds = String(timeLeft % 60).padStart(2, "0");
-    timerDisplay.textContent = `${minutes}:${seconds}`;
-  } else {
-    timerDisplay.textContent = "Time's up!";
-    clearInterval(countdown);
-  }
-}, 1000);
+// Create functionality for a countdown timer that counts down from 10 seconds to zero using JavaScript
