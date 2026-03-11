4cc9c6355f6355b2ad3eeecafdac129bda6bcc91
GitHub Action <action@github.com>
2026-03-11 21:10:52 +0000

    Revert "Merge pull request #6555 from Phourtie-Phour/fix-issue"
    This reverts commit 837c867e794b91ef68fb277bfc71c3ae1dc42311, reversing
    changes made to af2060a32f13d4cf6834652d171227607068dbe9.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,10 +1,3 @@
 // JavaScript - Easy
 
 // TODO: Write a function that returns the sum of two numbers
-function sum(num1, num2){
-  return num1 + num2;
-}
-
-const result = sum(1, 2);
-
-console.log("The sum of 1 and 2 is " + result + ".");
