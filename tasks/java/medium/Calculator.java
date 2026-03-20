4f5c4ddfe452e25645ff5368b8f4464c30b6e10e
GitHub Action <action@github.com>
2026-03-20 19:35:34 +0000

    Revert "Merge pull request #6690 from LLLLLLisaa/main"
    This reverts commit ad9a4c69fc744ebea0b4de93d752c3a0158a71cc, reversing
    changes made to fc3bf13173f4c43d577e790e6e67882cef4f358b.

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -4,21 +4,6 @@ package tasks.java.medium;
 
 public class Calculator {
     // TODO: Implement the Calculator class
-    public double add(double a, double b){
-        return a+b;
-    }
-    public double subtract(double a, double b){
-        return a-b;
-    }
-    public double multiply(double a, double b){
-        return a*b;
-    }
-    public double divide(double a, double b){
-        if (b == 0) {
-            throw new IllegalArgumentException("Cannot divide by zero");
-        }
-        return a / b;
-    }
 
     public static void main(String[] args) {
         //! Don't change the tests in the main method!
