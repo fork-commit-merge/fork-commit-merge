10139b29dff19de4898571b33adb1bc1f6a7cec9
Harshit Gupta <153393197+Ashusf90@users.noreply.github.com>
2026-03-23 18:19:39 +0530

    Update greater-than.js

--- tasks/javascript/easy/greater-than.js
+++ tasks/javascript/easy/greater-than.js
@@ -1,6 +1,10 @@
 // JavaScript - Easy
 
 // TODO: return a new array with all numbers greater than 10 but less than 20;
+const numbers = [5, 10, 12, 15, 20, 25, 19];
 
-const array = [2, 5, 8, 10, 12, 15, 19, 20, 25];
+const result = numbers.filter(num => num > 10 && num < 20);
+
+console.log(result);
+const numbers = [2, 5, 8, 10, 12, 15, 19, 20, 25];
 // output: [12, 15, 19];
