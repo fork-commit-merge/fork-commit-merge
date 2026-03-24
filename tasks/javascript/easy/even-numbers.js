55a2b7fb9ec8c69c90ae3f5529c985ad22adf9c0
GitHub Action <action@github.com>
2026-03-24 12:13:12 +0000

    Revert "Merge pull request #6714 from Ashusf90/patch-3"
    This reverts commit f49264deaf2a409c530f9c8a7f836499be55230e, reversing
    changes made to bc1b17fb4bba9dea21e301ec572ee88073d1bb1c.

--- tasks/javascript/easy/even-numbers.js
+++ tasks/javascript/easy/even-numbers.js
@@ -2,10 +2,5 @@
 
 // TODO:return a new array containing all even numbers using a .filter() method:
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-const evenNumbers = numbers.filter(num => num % 2 === 0);
-
-console.log(evenNumbers);
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // output: [2, 4, 6, 8, 10]
