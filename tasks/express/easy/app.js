b34b58ee11b97e0cd9724c9d1838b93ed72a8c71
navneet-97 <navneet-97@users.noreply.github.com>
2026-03-29 21:19:27 +0530

    feat: implement simple express server

--- tasks/express/easy/app.js
+++ tasks/express/easy/app.js
@@ -1,10 +1,14 @@
 // ExpressJS - Easy
-
-const { get } = require("cheerio/lib/api/traversing");
-const express = require("express");
+import express from 'express'
 const app = express();
 const port = 6969;
 
 // TODO: Implement `get` method for / route
+app.get('/', (req, res) => {
+    res.end("Hello, Express!");
+})
 
 // TODO: Implement `listen` method
+app.listen(port, () => {
+    console.log(`server is running at port:${port}`)
+})
