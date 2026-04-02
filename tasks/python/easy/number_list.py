9f281c88a69b541cd35374247dade234c90c1aa2
GitHub Action <action@github.com>
2026-04-02 20:06:53 +0000

    Revert "Merge pull request #6889 from itsmesihle/features"
    This reverts commit da3947a4ce81e7ae0d229078dbecc9a45f6fe099, reversing
    changes made to 99d7377ae03091dcdd988e8cbeb5b4253cc75288.

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -5,13 +5,5 @@
 # prints that list to the console and,
 # prints the sum of the numbers of that list
 
-def function():
-    sum = 0
-    numbers = []
-    for x in range(10):
-        numbers.append(x + 1)
-        sum += x
-    print(numbers)
-    print(sum)
-
-function()
+def function(numbers):
+    pass
