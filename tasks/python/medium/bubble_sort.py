042ca44838f1ee75e3628469817710fe740d66e5
Sihle <msihlesndlovu97@gmail.com>
2026-04-02 19:30:08 +0200

    feature: completed bubble_sort.py

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,9 +1,21 @@
 def bubble_sort(arr):
-    # TODO: Implement the bubble sort algorithm
+    n = len(arr)
+    
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            '''traverse from index 0 to n-i-1, then swap using tuple unpacking'''
+            # Swap/reassign using tuple unpacking
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+            
+        if not swapped:
+            '''if no swap then, break out of outer loop because arr is sorted'''
+            break
+        
 
-#! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
 print("Original array:", arr)
-
 bubble_sort(arr)
 print("Sorted array:", arr)
