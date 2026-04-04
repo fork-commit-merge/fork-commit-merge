70378690bcb310861f185f5fe7677a97396612e0
Phourtie-Phour <tripleace.2929@gmail.com>
2026-04-04 15:05:05 +0900

    Fixed issue

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -4,7 +4,12 @@ import "./index.css";
 function App() {
   return (
     <div className="center">
-      {/* TODO: Implement the card component */}
+      <h1>React/Vite - Easy</h1>
+        <div className="card">
+          <img src="https://fastly.picsum.photos/id/539/320/240.jpg?hmac=oD0Y9dDFVMYtt6ywF0tBROD8P487YcvHYBvb6glKG5M" alt="" />
+          <h2>Card Title</h2>
+          <p>Card Description</p>
+        </div>
     </div>
   );
 }
