5695190e1c6267822d668e101c3fdd8b97c5577a
zulkiflidev <aganzulkifli@gmail.com>
2026-04-09 20:54:04 +0700

    Solve Easy Issue (JavaScript)

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,3 +1,11 @@
 // JavaScript - Easy
 
 // TODO: Write a function that returns the sum of two numbers
+const a = 1;
+const b = 2;
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log('The sum of 1 and 2 is ' + sum(a, b));
