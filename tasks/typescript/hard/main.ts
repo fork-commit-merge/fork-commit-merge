2104e599a0918d01157e7176060b84bdfe45358c
Phourtie-Phour <tripleace.2929@gmail.com>
2026-04-09 20:43:35 +0900

    Fixed issue

--- tasks/typescript/hard/main.ts
+++ tasks/typescript/hard/main.ts
@@ -9,13 +9,44 @@ interface Product {
 
 let products: Product[] = [];
 
-async function fetchData() {
+async function fetchData(): Promise<void> {
     // TODO: Implement the fetch function
-    displayProducts(products);
+    try{
+    const response = await fetch('https://dummyjson.com/products')
+        if (!response.ok) {
+           throw new Error('response error');
+        } 
+
+        const data = await response.json();
+        products = data.products;
+
+        displayProducts(products);
+
+    } catch (error) {
+        console.error('Fetch error:', error);
+    }
 }
 
 function displayProducts(products: Product[]) {
     // TODO: Implement the display function
+    const productBody = document.getElementById('productBody') as HTMLTableSectionElement;
+
+    productBody.innerHTML='';
+     products.forEach(product => {
+        // create a row
+        const row = document.createElement('tr');
+        
+        // input data 
+        row.innerHTML = `
+            <td>${product.title}</td>
+            <td>${product.price}</td>
+            <td>${product.rating}</td>
+        `;
+        
+        // add a row
+        productBody.appendChild(row);
+    });
+
 }
 
 function applyFilters() {
@@ -34,7 +65,17 @@ function applyFilters() {
 
     const filteredProducts = products.filter(
         // TODO: Implement the filter function
-    );
+        product => {
+        const matchPrice =
+            (isNaN(minPrice) || product.price >= minPrice) &&
+            (isNaN(maxPrice) || product.price <= maxPrice);
+
+        const matchRating =
+            (isNaN(minRating) || product.rating >= minRating) &&
+            (isNaN(maxRating) || product.rating <= maxRating);
+
+        return matchPrice && matchRating;
+    });
 
     displayProducts(filteredProducts);
 }
