71b9c6bac06556df7d48a0e56c36a6cf509c512d
GitHub Action <action@github.com>
2026-04-09 14:46:30 +0000

    Revert "Merge pull request #6929 from zulkiflidev/zuldev-branch"
    This reverts commit 4269e5d75a235e599f984338e5673c87e185e212, reversing
    changes made to 8360f22fe73371347f14aff002331db4bd694b0a.

--- tasks/javascript/easy/sum.js
+++ tasks/javascript/easy/sum.js
@@ -1,12 +1,3 @@
 // JavaScript - Easy
 
 // TODO: Write a function that returns the sum of two numbers
-const a = 1;
-const b = 2;
-
-function sum(a, b) {
-  return a + b;
-}
-
-//result
-console.log('The sum of 1 and 2 is ' + sum(a, b));
