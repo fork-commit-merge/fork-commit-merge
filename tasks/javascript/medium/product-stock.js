8df25b621d539d2deb972156a34ae0d139be4416
GitHub Action <action@github.com>
2026-04-17 14:52:33 +0000

    Revert "Merge pull request #6941 from Aakif-Kohari/task-javascript-medium-3"
    This reverts commit 5e7816b1db51b378e88cc231fb09fc336709c26d, reversing
    changes made to 91ee6f4ddec3512c590388e07e0db5235322098e.

--- tasks/javascript/medium/product-stock.js
+++ tasks/javascript/medium/product-stock.js
@@ -1,6 +1,6 @@
 // JavaScript - Medium
 
-  // Task: return only products that are in stock using .filter() method;
+  // TODO: return only products that are in stock using .filter() method;
   // Extra Challenge: return only the names of products in stock;
 
 const products = [
@@ -9,16 +9,5 @@ const products = [
     { name: "Phone", inStock: true },
     { name: "Mug", inStock: false }
   ];
-
-// only products that are in stock
-const inStockProducts = products.filter(product => product.inStock);
-
-// Extra challenge: only the names of products in stock
-const inStockProductNames = products
-  .filter(product => product.inStock)
-  .map(product => product.name);
-
-console.log(inStockProducts);
-console.log(inStockProductNames);
-  // Task output = [ { name: 'Book', inStock: true }, { name: 'Phone', inStock: true } ];
+  // TODO output = [ { name: 'Book', inStock: true }, { name: 'Phone', inStock: true } ];
   // Challenge output = ['Book', 'Phone'];
