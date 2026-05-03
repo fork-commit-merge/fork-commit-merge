6cb1b3ac798adfbf720331c1c04da61df8de4c40
GitHub Action <action@github.com>
2026-05-03 18:44:26 +0000

    Revert "Merge pull request #7123 from snehalata2027/sum_converter"
    This reverts commit ed62c3a772a39e51ac3b28f6822a0d44530aae3d, reversing
    changes made to e3f1042e07445b29f7fa4546f4540235d2c84f11.

--- tasks/c++/easy/sum.cpp
+++ tasks/c++/easy/sum.cpp
@@ -1,18 +1,8 @@
 // C++ - Easy 2
 
 // TODO: Create a Basic C++ Program to Add Two Numbers
-#include <iostream>;
-using namespace std;
+
 int main()
 {
-    int num1;
-    cout<<"enter the numer1: ";
-    cin>>num1;
-    int num2;
-     cout<<"enter the numer2: ";
-    cin>>num2;
-    int sum=num1 + num2 ;
-    cout<<"sum of two number is : "<< sum;
-
   
 }
