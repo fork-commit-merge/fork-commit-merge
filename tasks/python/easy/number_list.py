1449f8b51fcc027b33834f868e9a4f21a321a6ce
GitHub Action <action@github.com>
2026-05-03 07:26:32 +0000

    Revert "Merge pull request #7115 from lrg913427-dot/fix/python-easy-issue-1-number-list"
    This reverts commit f5d2f83801527bd5361241ccca8a72459b8e0d92, reversing
    changes made to 129c5c654c1a88ede82e7e9756808a4ee9af9e74.

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -1,10 +1,9 @@
 # Python - Easy
 
-# Create a list of the first 10 natural numbers
-numbers = list(range(1, 11))
+# TODO: Create a Simple Python Program that:
+# lists the first 10 natural numbers,
+# prints that list to the console and,
+# prints the sum of the numbers of that list
 
-# Print the list
-print("The list of the first 10 natural numbers is:", numbers)
-
-# Print the sum of the numbers in the list
-print("The sum of the numbers in the list is:", sum(numbers))
+def function(numbers):
+    pass
