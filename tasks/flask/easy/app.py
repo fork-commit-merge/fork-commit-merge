f337bee155f63c7256ea923fe62ad24b07427904
Mohanapriya-sparks <jegadeesantamil@gmail.com>
2026-05-03 13:11:17 +0530

    Add home route to Flask application

--- tasks/flask/easy/app.py
+++ tasks/flask/easy/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 
-app = Flask(__name)
+app = Flask(__name__)
 
-# TODO: Implement the task requested in the issue description
+@app.route("/")
+def home():
+    return "Hello, Flask!"
 
 if __name__ == "__main__":
     app.run(debug=True)
