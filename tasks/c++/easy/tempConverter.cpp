e3f1042e07445b29f7fa4546f4540235d2c84f11
GitHub Action <action@github.com>
2026-05-03 18:43:11 +0000

    Revert "Merge pull request #7122 from snehalata2027/easy_temp_converter"
    This reverts commit d701cb67b573e8525ce622329a795bd377c9ffbc, reversing
    changes made to 689b99de93727d6b444a242539d2cc7f891b7272.

--- tasks/c++/easy/tempConverter.cpp
+++ tasks/c++/easy/tempConverter.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
-using namespace std;
 
 int main() {
   // TODO: Implement the temperature converter
-  int cel_temp;
-  cout<<"Enter the temp in deg celsius to get temp in fah : ";
-  cin>>cel_temp;
-  int fah_temp=(cel_temp*9/5)+32;
-  cout<<"your temp in deg fah is : "<< fah_temp;
 }
