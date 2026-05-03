7b7ec1047b0a9db1aa5812f1a47560c8cd109f73
GitHub Action <action@github.com>
2026-05-03 18:40:05 +0000

    Revert "Merge pull request #7119 from Mohanapriya-sparks/patch-1"
    This reverts commit e4840c32cd9de9b8ab4c56e0e27fd9d840318850, reversing
    changes made to fbfe765cf01863289bfa3bb1ef3dffd30729c7e6.

--- tasks/flask/easy/app.py
+++ tasks/flask/easy/app.py
@@ -1,10 +1,8 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name)
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+# TODO: Implement the task requested in the issue description
 
 if __name__ == "__main__":
     app.run(debug=True)
