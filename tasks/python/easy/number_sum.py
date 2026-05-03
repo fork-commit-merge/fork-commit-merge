8a7bf7b3086fb49d2daf0161abe63daaa342ca0d
GitHub Action <action@github.com>
2026-05-03 07:30:06 +0000

    Revert "Merge pull request #7116 from lrg913427-dot/fix/python-easy-issue-2-number-sum"
    This reverts commit 23ed368cf2c57d69e65cd59a3721b7cdde86821d, reversing
    changes made to e78970e8c599d4e9bad9e6ac028570b0f46409dd.

--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,13 +1,3 @@
 # Python - Easy
 
-# Function that returns the sum of two numbers
-def sum_of_two(a, b):
-    return a + b
-
-
-# Example usage
-if __name__ == "__main__":
-    num1 = 5
-    num2 = 3
-    result = sum_of_two(num1, num2)
-    print(f"The sum of {num1} and {num2} is {result}")
+# TODO: Write a function that returns the sum of two numbers
