3033537069174c880d28ee23c252979be12f8378
GitHub Action <action@github.com>
2026-05-06 19:30:54 +0000

    Revert "Merge pull request #7126 from snehalata2027/rev_word"
    This reverts commit d456305800282b5ee03e7ba974db9134d4ce6304, reversing
    changes made to 618e51cea8d92d87885181ba509551353e8dfad1.

--- tasks/c++/easy/ReverseString.cpp
+++ tasks/c++/easy/ReverseString.cpp
@@ -1,21 +1,11 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
-string reverseString(string rev_word ) {
+std::string reverseString(const std::string &str) {
     // TODO: Implement the reverseString function
-    //  string rev_word;
-    //  cout<<"Enter the word that you want to be get reversed : ";
-    //  cin>>word;
-     for(int i=rev_word.length()-1;i>=0;i--){
-         cout<<rev_word[i]<<" ";
-     }
-
-
 }
 
 int main() {
     // TODO: Implement the main method
-    reverseString("arnav");
 }
 
