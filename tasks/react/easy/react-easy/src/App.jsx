0a022910cc80915fbc84295907a0a10f2286cf1b
GitHub Action <action@github.com>
2026-05-06 19:37:39 +0000

    Revert "Merge pull request #7130 from Poonam-Biradar/react-card-feature"
    This reverts commit a50e0c77d7cae2b26658e1b1635c57c6cb27b933, reversing
    changes made to 4ef581eb179aec1a2d085274e4aa4c58ad14bb5d.

--- tasks/react/easy/react-easy/src/App.jsx
+++ tasks/react/easy/react-easy/src/App.jsx
@@ -2,31 +2,9 @@
 import "./index.css";
 
 function App() {
-  const cardData = {
-    title: "Card Title",
-    description: "Card Description",
-    imageUrl: "https://picsum.photos/320/240",
-  };
-
   return (
     <div className="center">
-      <h1 className="title">React/Vite - Easy</h1>
-
-      <div className="card" role="article">
-        <img
-          src={cardData.imageUrl}
-          alt={cardData.title}
-          className="card-image"
-          onError={(e) =>
-            (e.target.src = "https://via.placeholder.com/320x240")
-          }
-        />
-
-        <div className="card-content">
-          <h2>{cardData.title}</h2>
-          <p>{cardData.description}</p>
-        </div>
-      </div>
+      {/* TODO: Implement the card component */}
     </div>
   );
 }
