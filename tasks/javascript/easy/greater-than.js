ed9766f00754112342d9155410000c60ea2d8191
universe7creator <universe7creator@github.com>
2026-05-08 23:37:00 +0300

    fix(js/easy): filter numbers greater than 10 and less than 20 (Issue #7100)

--- tasks/javascript/easy/greater-than.js
+++ tasks/javascript/easy/greater-than.js
@@ -1,5 +1,8 @@
 // JavaScript - Easy
 
+function filterGreaterThan10LessThan20(array) {
+  return array.filter(num => num > 10 && num < 20);
+}
+
 const array = [2, 5, 8, 10, 12, 15, 19, 20, 25];
-const result = array.filter(num => num > 10 && num < 20);
-console.log(result);
+console.log(filterGreaterThan10LessThan20(array));
