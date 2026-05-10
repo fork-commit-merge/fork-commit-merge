aaa32b2b09807f0ef93f35fc3b765cb5232eeb32
GitHub Action <action@github.com>
2026-05-10 09:54:40 +0000

    Revert "Merge pull request #7145 from snehalata2027/sum_in_c"
    This reverts commit fbba0c3ff99d791b5bc8238ab9583fb74dad22ab, reversing
    changes made to f6319735c200d30b1108bf146e2be77a7cb83db7.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,16 +1,5 @@
 // C - Easy
- int sum(int c, int d){
-    return c+d;
-};
 
 #include <stdio.h>
-int main(){
-   int a;
-   int b;
 
-    printf("Enter two numbers : ");
-    scanf("%d %d",&a , &b);
-    printf("%d",sum(a,b));
-   return 0;
-}
 // TODO: Implement the sum and main functions
