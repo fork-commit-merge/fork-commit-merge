51a4ed3ab9c668aacf2460029e59ddb508a291a6
GitHub Action <action@github.com>
2026-05-10 09:50:10 +0000

    Revert "Merge pull request #7142 from madnessqw/fix-7097-matlab-add"
    This reverts commit b9376aec9fbd458cb08c30b52783675554a8859c, reversing
    changes made to 1f6836e0fedef0275f2b448c6162c36e8e9a42b9.

--- tasks/matlab/easy/addition.m
+++ tasks/matlab/easy/addition.m
@@ -1,12 +1,3 @@
 % MATLAB - Easy
-% Add two numbers and print the result
 
-% Define the numbers
-a = 5;
-b = 3;
-
-% Calculate the sum
-result = a + b;
-
-% Print the result
-fprintf('Result: %g\n', result);
+% TODO: Create a MATLAB script that adds two numbers together and prints the result
