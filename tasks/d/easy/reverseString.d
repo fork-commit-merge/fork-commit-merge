3c5e05390377c57bce0e7b0ff61dedfd5a88d1ef
GitHub Action <action@github.com>
2026-05-10 09:41:34 +0000

    Revert "Merge pull request #7137 from universe7creator/fix/d-reverse-string-7093"
    This reverts commit 128bc6c23d2a55e23e5502c3eee6ccf9c96577d8, reversing
    changes made to a63e2fa269aa771bfbe7a1d0dd46cfd48dd6f8af.

--- tasks/d/easy/reverseString.d
+++ tasks/d/easy/reverseString.d
@@ -1,16 +1,7 @@
 // D - Easy
-// Implement program to reverse a string
 
 import std.stdio;
 
 void main() {
-    string input = "Hello, World!";
-    string reversed;
-    
-    foreach (c; input) {
-        reversed = c ~ reversed;
-    }
-    
-    writeln("Original String: ", input);
-    writeln("Reversed String: ", reversed);
+    // TODO: Implement program to reverse a string
 }
