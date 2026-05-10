0ea1feaecdf869cd2817ee39f3f9b07e08815055
GitHub Action <action@github.com>
2026-05-10 09:43:23 +0000

    Revert "Merge pull request #7138 from universe7creator/fix/crystal-http-server-7095"
    This reverts commit 4af357ce71291304f58960aa74a4e811f0395562, reversing
    changes made to 3c5e05390377c57bce0e7b0ff61dedfd5a88d1ef.

--- tasks/crystal/easy/server.cr
+++ tasks/crystal/easy/server.cr
@@ -1,14 +1,5 @@
 # Crystal - Easy
-# Implement the HTTP server
 
 require "http/server"
 
-port = 8080
-
-server = HTTP::Server.new do |context|
-  context.response.content_type = "text/plain"
-  context.response.print "Hello from Crystal!"
-end
-
-puts "Server listening on port #{port}"
-server.listen(port)
+# TODO: Implement the HTTP server
