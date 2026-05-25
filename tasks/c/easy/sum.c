8acf857efa7b541234adfc5ce6c9c776f97a83a6
GitHub Action <action@github.com>
2026-05-25 14:54:16 +0000

    Revert "Merge pull request #7318 from asmita-saha-07/fix-issue"
    This reverts commit 17136e50e598ba13562f32c3ba935f8e004e4e3b, reversing
    changes made to bb382c2a7c56da3cf7534f1371539606262b05d7.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,13 +1,5 @@
 // C - Easy
 
-#include<stdio.h>
+#include <stdio.h>
 
 // TODO: Implement the sum and main functions
-int main()
-{
-    int a,b;
-    printf("Enter the two numbers to add : ");
-    scanf("%d, %d",&a,&b);
-    printf("sum : %d",a+b);
-    return 0;
-}
--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,10 +1,5 @@
 def bubble_sort(arr):
     # TODO: Implement the bubble sort algorithm
-    for i in range(len(arr)):
-        for j in range(len(arr)-i-1):
-            if arr[j]>arr[j+1]:
-                arr[j+1],arr[j]=arr[j],arr[j+1]
-                
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
@@ -12,4 +7,3 @@ print("Original array:", arr)
 
 bubble_sort(arr)
 print("Sorted array:", arr)
-
