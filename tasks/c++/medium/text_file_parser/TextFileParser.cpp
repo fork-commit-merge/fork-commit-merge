54ffcf91af79b96914cd5690d7eb4d76705d197d
samuel <samuelcuervo1224@gmail.com>
2026-05-26 03:14:39 -0500

    Fixed issue

--- tasks/c++/medium/text_file_parser/TextFileParser.cpp
+++ tasks/c++/medium/text_file_parser/TextFileParser.cpp
@@ -1,8 +1,62 @@
 #include <iostream>
+#include <map>
+#include <sstream>
+#include <algorithm>
+#include <cctype>
+#include <string>
+#include <set>
+#include <vector>
+using namespace std;
 
-// TODO: Create the necessary functions for the program to work
+
+void Process(string line, map<string, int>& wordCount){
+
+    static const set<string> stopWords = {
+        "the", "is", "at", "which", "on", "a", "an", "and", "in", "to", "of", "it"
+    };
+
+    stringstream ss(line);
+    string word;
+    while(ss>>word){
+
+        transform(word.begin(), word.end(), word.begin(), [](unsigned char c) {
+            return tolower(c);
+        });
+
+
+        word.erase(remove_if(word.begin(), word.end(), [](unsigned char c) {
+            return ispunct(c);
+        }), word.end());
+
+
+        if(!word.empty()){
+            if(stopWords.find(word) == stopWords.end()){
+                wordCount[word]++;
+            }
+        }
+    }
+}
+
 
 int main() {
-    // TODO: Implement the main method
+    string line;
+    map<string, int> wordCount; 
+    while(getline(cin, line)){
+        Process(line, wordCount);
+    }
+    
+    
+    vector<pair<string, int>> sortedWords(wordCount.begin(), wordCount.end());
+    
+    stable_sort(sortedWords.begin(), sortedWords.end(), [](const pair<string, int>& a, const pair<string, int>& b) {
+        return a.second > b.second; 
+    });
+    
+    for(const auto& pair : sortedWords){
+        cout << pair.first << " - " << pair.second << endl;
+    }
+    
     return 0;
 }
+
+    
