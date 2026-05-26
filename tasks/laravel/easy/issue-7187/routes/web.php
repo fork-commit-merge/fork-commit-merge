b6c47566ea819908015f58a814d9dea1f6668851
lucasdavid11 <lucasdavid977@gmail.com>
2026-05-25 21:10:54 -0300

    Fixed issue

---
+++ tasks/laravel/easy/issue-7187/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/greet', function () {
+    return view('greeting');
+});
+
