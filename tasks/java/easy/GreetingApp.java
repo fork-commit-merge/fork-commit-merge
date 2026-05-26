d1e76c6b740107f15f570be3c31f099c4ebd0ed2
GitHub Action <action@github.com>
2026-05-26 14:02:52 +0000

    Revert "Merge pull request #7319 from aishvibansal/java-greeting-task"
    This reverts commit 86b7ad6a5b360268c276ebdd3cf6e7ced6283cbe, reversing
    changes made to 7242ca039e523489cc47c626d576fe32cd1ddcab.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,14 +1,8 @@
-import java.util.*;
+// Java - Easy
 
 public class GreetingApp {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-
-        System.out.println("Hello, " + name + "! Nice to meet you!");
-
-        scanner.close();
     }
 }
