76e8dea0122c5542a175be4ed24d8b7938f2be63
GitHub Action <action@github.com>
2026-05-26 14:07:26 +0000

    Revert "Merge pull request #7321 from Krow1224/fix-issue"
    This reverts commit 39fc456b9caa382ab1ff50fd3a3ecbf2e88ca9a4, reversing
    changes made to 697ef6e39ca77d3ecc250cf105de17a1f362ae2e.

--- tasks/c++/medium/text_file_parser/TextFileParser.cpp
+++ tasks/c++/medium/text_file_parser/TextFileParser.cpp
@@ -1,62 +1,8 @@
 #include <iostream>
-#include <map>
-#include <sstream>
-#include <algorithm>
-#include <cctype>
-#include <string>
-#include <set>
-#include <vector>
-using namespace std;
 
-
-void Process(string line, map<string, int>& wordCount){
-
-    static const set<string> stopWords = {
-        "the", "is", "at", "which", "on", "a", "an", "and", "in", "to", "of", "it"
-    };
-
-    stringstream ss(line);
-    string word;
-    while(ss>>word){
-
-        transform(word.begin(), word.end(), word.begin(), [](unsigned char c) {
-            return tolower(c);
-        });
-
-
-        word.erase(remove_if(word.begin(), word.end(), [](unsigned char c) {
-            return ispunct(c);
-        }), word.end());
-
-
-        if(!word.empty()){
-            if(stopWords.find(word) == stopWords.end()){
-                wordCount[word]++;
-            }
-        }
-    }
-}
-
+// TODO: Create the necessary functions for the program to work
 
 int main() {
-    string line;
-    map<string, int> wordCount; 
-    while(getline(cin, line)){
-        Process(line, wordCount);
-    }
-    
-    
-    vector<pair<string, int>> sortedWords(wordCount.begin(), wordCount.end());
-    
-    stable_sort(sortedWords.begin(), sortedWords.end(), [](const pair<string, int>& a, const pair<string, int>& b) {
-        return a.second > b.second; 
-    });
-    
-    for(const auto& pair : sortedWords){
-        cout << pair.first << " - " << pair.second << endl;
-    }
-    
+    // TODO: Implement the main method
     return 0;
 }
-
-    
