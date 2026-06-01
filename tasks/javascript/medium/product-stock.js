933fe7b3c7cefa9ea1c701b470c02d44fa39d541
Brajesh Mohanty <mohantybrajesh4@gmail.com>
2026-06-02 00:20:50 +0530

    fix: filter in-stock products and return their names

--- tasks/javascript/medium/product-stock.js
+++ tasks/javascript/medium/product-stock.js
@@ -1,13 +1,14 @@
 // JavaScript - Medium
 
-  // TODO: return only products that are in stock using .filter() method;
-  // Extra Challenge: return only the names of products in stock;
-
 const products = [
     { name: "Book", inStock: true },
     { name: "Pen", inStock: false },
     { name: "Phone", inStock: true },
     { name: "Mug", inStock: false }
   ];
-  // TODO output = [ { name: 'Book', inStock: true }, { name: 'Phone', inStock: true } ];
-  // Challenge output = ['Book', 'Phone'];
+
+const output = products.filter((product) => product.inStock);
+const challengeOutput = output.map((product) => product.name);
+
+console.log(output);
+console.log(challengeOutput);
