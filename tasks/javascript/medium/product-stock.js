d34718a17cfcd2d12247dfbaeeaf5626deb1a0e2
unknown <joylinmathias30@gmail.com>
2026-06-05 13:35:29 +0530

    Fixed Issue

--- tasks/javascript/medium/product-stock.js
+++ tasks/javascript/medium/product-stock.js
@@ -1,13 +1,17 @@
 // JavaScript - Medium
 
-  // TODO: return only products that are in stock using .filter() method;
-  // Extra Challenge: return only the names of products in stock;
+// Return only products that are in stock using .filter() method
+// Extra Challenge: return only the names of products in stock
 
 const products = [
-    { name: "Book", inStock: true },
-    { name: "Pen", inStock: false },
-    { name: "Phone", inStock: true },
-    { name: "Mug", inStock: false }
-  ];
-  // TODO output = [ { name: 'Book', inStock: true }, { name: 'Phone', inStock: true } ];
-  // Challenge output = ['Book', 'Phone'];
+  { name: "Book", inStock: true },
+  { name: "Pen", inStock: false },
+  { name: "Phone", inStock: true },
+  { name: "Mug", inStock: false }
+];
+
+const inStockProducts = products.filter(product => product.inStock);
+const inStockProductNames = inStockProducts.map(product => product.name);
+
+console.log(inStockProducts);
+console.log(inStockProductNames);
