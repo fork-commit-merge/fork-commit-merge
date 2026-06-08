762a078620d8a040d4ba3fc4fe6023ec777952e3
GitHub Action <action@github.com>
2026-06-08 18:29:17 +0000

    Revert "Merge pull request #7495 from maitriupadhyay03-cell/cpp-hard-mystring-implementation"
    This reverts commit dafe221050431ba4fe573d80312b439f5d795866, reversing
    changes made to c321a03556c994b937be9317fea2b5cd939f8337.

--- tasks/c++/hard/mystring.cpp
+++ tasks/c++/hard/mystring.cpp
@@ -1,110 +1,42 @@
-// C++ - Hard
-
-#include "mystring.h"
 #include <cstring>
 #include <iostream>
-#include <stdexcept>
 
-// Default constructor
-MyString::MyString() : data(nullptr), len(0) {
-    data = new char[1];
-    data[0] = '\0';
+MyString::MyString(const char* s) {
+    // TODO: Implement this
 }
 
-// Parameterized constructor
-MyString::MyString(const char* s) : data(nullptr), len(0) {
-    if (s == nullptr) {
-        data = new char[1];
-        data[0] = '\0';
-        len = 0;
-    } else {
-        len = strlen(s);
-        data = new char[len + 1];
-        strcpy(data, s);
-    }
+MyString::MyString(const MyString& other) {
+    // TODO: Implement this
 }
 
-// Copy constructor
-MyString::MyString(const MyString& other) : data(nullptr), len(other.len) {
-    data = new char[len + 1];
-    strcpy(data, other.data);
-}
-
-// Destructor
-MyString::~MyString() {
-    delete[] data;
-}
-
-// Return length of string
 int MyString::length() const {
-        return len;
+    // TODO: Implement this
 }
 
-// Return C-style string
 const char* MyString::c_str() const {
-        return data;
+    // TODO: Implement this
 }
 
-// Append another MyString
 void MyString::append(const MyString& other) {
-        int newLen = len + other.len;
-    char* newData = new char[newLen + 1];
-    strcpy(newData, data);
-    strcat(newData, other.data);
-    delete[] data;
-    data = newData;
-    len = newLen;
+    // TODO: Implement this
 }
 
-// Compare with another MyString
 int MyString::compare(const MyString& other) const {
-        return strcmp(data, other.data);
+    // TODO: Implement this
 }
 
-// Assignment operator
 MyString& MyString::operator=(const MyString& other) {
-        if (this == &other) {
-        return *this;
-        }
-    delete[] data;
-    len = other.len;
-    data = new char[len + 1];
-    strcpy(data, other.data);
-    return *this;
+    // TODO: Implement this
 }
 
-// Addition operator (concatenation)
 MyString MyString::operator+(const MyString& other) const {
-        int newLen = len + other.len;
-    char* newData = new char[newLen + 1];
-    strcpy(newData, data);
-    strcat(newData, other.data);
-    MyString result(newData);
-    delete[] newData;
-    return result;
+    // TODO: Implement this
 }
 
-// Equality operator
 bool MyString::operator==(const MyString& other) const {
-        return strcmp(data, other.data) == 0;
+    // TODO: Implement this
 }
 
-// Stream insertion operator
 std::ostream& operator<<(std::ostream& os, const MyString& s) {
-        os << s.data;
-    return os;
-}
-
-// Bonus: substring
-MyString MyString::substring(int start, int end) const {
-        if (start < 0 || end > len || start > end) {
-        throw std::out_of_range("Invalid substring indices");
-        }
-    int subLen = end - start;
-    char* subData = new char[subLen + 1];
-    strncpy(subData, data + start, subLen);
-    subData[subLen] = '\0';
-    MyString result(subData);
-    delete[] subData;
-    return result;
+    // TODO: Implement this
 }
--- tasks/c++/hard/mystring.h
+++ tasks/c++/hard/mystring.h
@@ -1,41 +1,8 @@
 // C++ - Hard
 
-#ifndef MYSTRING_H
-#define MYSTRING_H
+define MYSTRING_H
 #include <iostream>
 
 class MyString {
-private:
-    char* data;
-    int len;
-
-public:
-    // Default constructor
-    MyString();
-
-    // Parameterized constructor
-    MyString(const char* s);
-
-    // Copy constructor
-    MyString(const MyString& other);
-
-    // Destructor
-    ~MyString();
-
-    // Member functions
-    int length() const;
-    const char* c_str() const;
-    void append(const MyString& other);
-    int compare(const MyString& other) const;
-
-    // Overloaded operators
-    MyString& operator=(const MyString& other);
-    MyString operator+(const MyString& other) const;
-    bool operator==(const MyString& other) const;
-    friend std::ostream& operator<<(std::ostream& os, const MyString& s);
-
-    // Bonus: substring
-    MyString substring(int start, int end) const;
-};
-
-#endif // MYSTRING_H
+    // TODO: Implement MyString class
+}
