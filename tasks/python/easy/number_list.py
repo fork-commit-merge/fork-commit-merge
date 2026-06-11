27675b04d34906516550d8047f606aceb21feeae
GitHub Action <action@github.com>
2026-06-11 16:28:19 +0000

    Revert "Merge pull request #7500 from HimathKc/easy-python-6607"
    This reverts commit c6b342e8fa463bac27cbf85d24b2c100b77437a1, reversing
    changes made to 04786142b3e4da44a536793191be42f878bb995e.

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -1,4 +1,9 @@
-numbers = list(range(1, 11))  # Creates a list: 1, 2, 3 ... 10
+# Python - Easy
 
-print(numbers)       # Prints the full list
-print(sum(numbers))  # Adds all numbers in the list and prints 55
+# TODO: Create a Simple Python Program that:
+# lists the first 10 natural numbers,
+# prints that list to the console and,
+# prints the sum of the numbers of that list
+
+def function(numbers):
+    pass
