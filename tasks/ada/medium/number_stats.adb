919201e8f1cad2ac8de5d480a7e8632be768170b
GitHub Action <action@github.com>
2026-06-12 17:57:08 +0000

    Revert "Merge pull request #7501 from joylinmhs/Medium-Issue"
    This reverts commit d4235224728038486e3356b82ca9096b3fe63207, reversing
    changes made to 27675b04d34906516550d8047f606aceb21feeae.

--- tasks/ada/medium/number_stats.adb
+++ tasks/ada/medium/number_stats.adb
@@ -1,5 +1,4 @@
 with Ada.Text_IO; use Ada.Text_IO;
-with Ada.Float_Text_IO; use Ada.Float_Text_IO;
 
 procedure Number_Stats is
    Numbers : constant array (1 .. 6) of Integer := (3, 7, 2, 9, 5, 4);
@@ -7,35 +6,15 @@ procedure Number_Stats is
    Min, Max, Sum : Integer;
    Avg : Float;
 begin
-   Sum := 0;
-   Min := Numbers (Numbers'First);
-   Max := Numbers (Numbers'First);
+   -- TODO: Use Numbers in the fixed-size array above to
+   -- implement the calculations for Min, Max, Sum, and Avg
+   
 
-   for Index in Numbers'Range loop
-      Sum := Sum + Numbers (Index);
-      if Numbers (Index) < Min then
-         Min := Numbers (Index);
-      end if;
-      if Numbers (Index) > Max then
-         Max := Numbers (Index);
-      end if;
-   end loop;
+   -- Print the Min, Max, Sum, and Avg on separate lines
+   -- using the statements below
 
-   Avg := Float (Sum) / Float (Numbers'Length);
-
-   Put ("Sum:");
-   Put (Sum, Width => 8);
-   New_Line;
-
-   Put ("Minimum:");
-   Put (Min, Width => 3);
-   New_Line;
-
-   Put ("Maximum:");
-   Put (Max, Width => 3);
-   New_Line;
-
-   Put ("Average: ");
-   Put (Avg, Fore => 1, Aft => 2, Exp => 0);
-   New_Line;
+   Put_Line ("Min:" & Integer'Image (Min));
+   Put_Line ("Max:" & Integer'Image (Max));
+   Put_Line ("Sum:" & Integer'Image (Sum));
+   Put_Line ("Avg:" & Float'Image (Avg));
 end Number_Stats;
