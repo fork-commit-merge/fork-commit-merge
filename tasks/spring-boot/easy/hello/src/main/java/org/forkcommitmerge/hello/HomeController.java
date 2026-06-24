bcf3e5759dddd82a11b4753867956152c51cd435
GitHub Action <action@github.com>
2026-06-24 07:53:37 +0000

    Revert "Merge pull request #7523 from gauravsd-git/springboot-hello-gaurav"
    This reverts commit b0f04c8bf8d5f2b44a87a86989fa3a8e89534007, reversing
    changes made to ba4e2bb09a5534cdc2b73cd6d6f5432a32d39620.

---
+++ tasks/spring-boot/easy/hello/src/main/java/org/forkcommitmerge/hello/HomeController.java
@@ -0,0 +1,20 @@
+package org.forkcommitmerge.hello;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+    /*
+     * TASK: Create a GET mapping for the index page ("/")
+     *
+     * Requirements:
+     * 1. Create a method that handles GET requests to the root path "/"
+     * 2. Add the text "Hello, Spring Boot" to the model with the key "message"
+     * 3. Return the view name "index" to render the template
+     *
+     * The template is already set up to display the message using Thymeleaf
+     * with the expression: th:text="${message}"
+     */
+
+}
