a2141041c9a35403fe083ff5d31dbab5b5f4b0e7
JsQR23 <fabquint23@gmail.com>
2026-06-25 15:35:48 -0600

    traffic lghts activity done

--- tasks/react/medium/traffic-light/src/TrafficLight.tsx
+++ tasks/react/medium/traffic-light/src/TrafficLight.tsx
@@ -2,10 +2,58 @@
 import "./App.css";
 
 // TODO: Implement  the TrafficLight component
-
+import { useEffect, useState } from "react";
 export function TrafficLight() {
 
+  let activeRed:string = "notActive"
+  let activeYellow:string = "notActive"
+  let activeGreen:string = "notActive"
+
+  interface myColours{
+    red:string,
+    yellow:string,
+    green:string
+  }
+  const [colour, setColour] = useState<myColours>({
+    red:"",
+    yellow:"",
+    green:""
+  })
+  useEffect(()=>{
+
+    const loop=()=>{
+      setColour(prev=>({...prev,red:"active",yellow:"",green:""}))
+
+      setTimeout(()=>{
+        setColour({
+          red:"",
+          yellow:"active",
+          green:""
+        })
+      },4000)
+
+      setTimeout(()=>{
+        setColour({
+          red:"",
+          yellow:"",
+          green:"active"
+        })
+      },6000)
+
+    }
+
+    loop()
+    const interval = setInterval(loop,8000)
+    return () => clearInterval(interval);
+
+  },[])
+
+
   return (
-    // TODO: return JSX here
+    <div className="traffic-light">
+        <div className={`light red ${colour.red}`}></div>
+        <div className={`light yellow ${colour.yellow}`}></div>
+        <div className={`light green ${colour.green}`}></div>
+    </div>
   );
 }
