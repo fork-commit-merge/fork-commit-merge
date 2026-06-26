c943eed65c3e896dafbdd67a375ab0e0d728afc1
GitHub Action <action@github.com>
2026-06-26 18:45:25 +0000

    Revert "Merge pull request #7526 from veronica-foltz/main"
    This reverts commit 1cc1c86389a9322e50d41c679e9cd0cca5c0ac5c, reversing
    changes made to e17443b4fec8fce6d3216dd606cc56b404d9aecb.

--- tasks/c/medium/read_csv.c
+++ tasks/c/medium/read_csv.c
@@ -6,36 +6,5 @@
 
 
 int main() {
-    FILE *file = fopen("data.csv", "r");
-
-    if (file == NULL) {
-        printf("Error opening file.\n");
-        return 1;
-    }   
-
-    char line[100];
-
-    if (fgets(line, sizeof(line), file)) {
-        line[strcspn(line, "\n")] = '\0';
-        printf("Header: %s\n\n", line);
-
-    }   
-
-    while (fgets(line, sizeof(line), file)) {
-
-        line[strcspn(line, "\n")] = '\0';
-
-        printf("Reading line: %s\n", line);
-
-        char *name = strtok(line, ",");
-        char *age = strtok(NULL, ",");
-
-        printf("Name: %s\n", name);
-        printf("Age: %s\n\n", age);
-
-    }
-
-        fclose(file);
-        return 0;
-
+//    TODO: Implement the program requested in the task description
 }
