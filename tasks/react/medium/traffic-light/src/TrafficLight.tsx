55ccbc71c4a883a5ebbabad9ee704217889f431b
GitHub Action <action@github.com>
2026-06-26 18:47:49 +0000

    Revert "Merge pull request #7527 from JFQR/JFQR/traffic-lights"
    This reverts commit 54da285739aeb20ca66812d62f5856120df441cb, reversing
    changes made to c943eed65c3e896dafbdd67a375ab0e0d728afc1.

--- tasks/react/medium/traffic-light/src/TrafficLight.tsx
+++ tasks/react/medium/traffic-light/src/TrafficLight.tsx
@@ -2,58 +2,10 @@
 import "./App.css";
 
 // TODO: Implement  the TrafficLight component
-import { useEffect, useState } from "react";
+
 export function TrafficLight() {
 
-  let activeRed:string = "notActive"
-  let activeYellow:string = "notActive"
-  let activeGreen:string = "notActive"
-
-  interface myColours{
-    red:string,
-    yellow:string,
-    green:string
-  }
-  const [colour, setColour] = useState<myColours>({
-    red:"",
-    yellow:"",
-    green:""
-  })
-  useEffect(()=>{
-
-    const loop=()=>{
-      setColour(prev=>({...prev,red:"active",yellow:"",green:""}))
-
-      setTimeout(()=>{
-        setColour({
-          red:"",
-          yellow:"active",
-          green:""
-        })
-      },4000)
-
-      setTimeout(()=>{
-        setColour({
-          red:"",
-          yellow:"",
-          green:"active"
-        })
-      },6000)
-
-    }
-
-    loop()
-    const interval = setInterval(loop,8000)
-    return () => clearInterval(interval);
-
-  },[])
-
-
   return (
-    <div className="traffic-light">
-        <div className={`light red ${colour.red}`}></div>
-        <div className={`light yellow ${colour.yellow}`}></div>
-        <div className={`light green ${colour.green}`}></div>
-    </div>
+    // TODO: return JSX here
   );
 }
