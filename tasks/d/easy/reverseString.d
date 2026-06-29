8ea395fc748ced3b9dad9927ad96c10f0bace57d
GitHub Action <action@github.com>
2026-06-29 19:46:27 +0000

    Revert "Merge pull request #7531 from Khushii1201/main"
    This reverts commit 1e3ae5bf3b5e2b75451627bee35c8fbf493d0c63, reversing
    changes made to 8b7c9b0bd0f094f1a50b0e033c5a6cae3cbfc5dc.

--- tasks/d/easy/reverseString.d
+++ tasks/d/easy/reverseString.d
@@ -1,13 +1,7 @@
+// D - Easy
+
 import std.stdio;
-import std.algorithm;
-import std.array;
 
-void main()
-{
-    string original = "Hello, World!";
-    auto reversed = original.dup;
-    reverse(reversed);
-
-    writeln("Original String: ", original);
-    writeln("Reversed String: ", reversed);
+void main() {
+    // TODO: Implement program to reverse a string
 }
