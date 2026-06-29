7b9e256b5e59b17b6528f5e64b8794ea0e03c2b1
Khushii1201 <khushi.wad78@gmail.com>
2026-06-29 23:02:48 +0530

    Reversed string code in D

--- tasks/d/easy/reverseString.d
+++ tasks/d/easy/reverseString.d
@@ -1,7 +1,13 @@
-// D - Easy
-
 import std.stdio;
+import std.algorithm;
+import std.array;
 
-void main() {
-    // TODO: Implement program to reverse a string
+void main()
+{
+    string original = "Hello, World!";
+    auto reversed = original.dup;
+    reverse(reversed);
+
+    writeln("Original String: ", original);
+    writeln("Reversed String: ", reversed);
 }
