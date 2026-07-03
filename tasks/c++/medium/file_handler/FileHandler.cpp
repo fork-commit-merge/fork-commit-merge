e89b6a6d00cf20068b5f51962540d0b7da9044a2
GitHub Action <action@github.com>
2026-07-03 14:47:41 +0000

    Revert "Merge pull request #7682 from veronica-foltz/main"
    This reverts commit 85da7453f2189e2e652085215bbb5ec099bbea7c, reversing
    changes made to 506afa63154c3c89f7a7ed857a5c530bc236adbf.

--- tasks/c++/medium/file_handler/FileHandler.cpp
+++ tasks/c++/medium/file_handler/FileHandler.cpp
@@ -4,52 +4,11 @@
 #include <fstream>
 #include <sstream>
 
-std::string FileHandler::ReadFile(const std::string& filename)
-{
-    std::ifstream file(filename);
-       
-        if (!file)
-        {
-            return "";
-        }
+// TODO: Implement the FileHandler::ReadFile method
 
-        std::stringstream buffer;
-        buffer << file.rdbuf();
+// TODO: Implement the FileHandler::WriteFile method
 
-        return buffer.str();
-}
+// TODO: Implement the FileHandler::AppendFile method
 
-bool FileHandler::WriteFile(const std::string& filename, const std::string& content)
-{
-    std::ofstream file(filename);
-
-    if (!file)
-    {
-        return false;
-    }
-
-    file << content;
-
-    return true;
-}
-
-bool FileHandler::AppendFile(const std::string& filename, const std::string& content)
-{
-    std::ofstream file(filename, std::ios::app);
-
-    if (!file)
-    {
-        return false;
-    }
-
-    file << content;
-
-    return true;
-}
-
-bool FileHandler::FileExists(const std::string& filename)
-{
-    std::ifstream file(filename);
-    return file.good();
-}
+// TODO: Implement the FileHandler::FileExists method
 
--- tasks/c++/medium/file_handler/FileHandler.h
+++ tasks/c++/medium/file_handler/FileHandler.h
@@ -5,8 +5,5 @@
 
 class FileHandler {
 public:
-    static std::string ReadFile(const std::string& filename);
-    static bool WriteFile(const std::string& filename, const std::string& content);
-    static bool AppendFile(const std::string& filename, const std::string& content);
-    static bool FileExists(const std::string& filename);
-    };
+    // TODO: Implement the FileHandler class
+};
