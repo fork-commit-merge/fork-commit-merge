f0c6a03b2cc2203abd08ea515c8854db3c55766e
GitHub Action <action@github.com>
2026-07-04 09:27:48 +0000

    Revert "Merge pull request #7684 from KhushiVerma021/calculator-java"
    This reverts commit 32e7d386c51b42f1c0896fdc13e5c495963ac0e6, reversing
    changes made to a56259065d6075ad281d9952363f08d6f88b47d3.

--- tasks/java/medium/Calculator.java
+++ tasks/java/medium/Calculator.java
@@ -4,21 +4,6 @@ package tasks.java.medium;
 
 public class Calculator {
     // TODO: Implement the Calculator class
-    public double add(int a, int b){
-        return (double)a+b;
-    }
-    public double subtract(int a, int b){
-        return (double)a-b;
-    }
-    public double multiply(int a, int b){
-        return (double)a*b;
-    }
-    public double divide(int a, int b){
-        if(b==0) {
-            throw new IllegalArgumentException("Cannot divide by zero");
-        }
-        return (double) a/b;
-    }
 
     public static void main(String[] args) {
         //! Don't change the tests in the main method!
