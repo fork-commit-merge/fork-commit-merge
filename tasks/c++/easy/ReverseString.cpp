53b0707fb58e08b39bd51ec642143373ab57d2a2
GitHub Action <action@github.com>
2026-07-06 19:11:03 +0000

    Revert "Merge pull request #7687 from Pratishtha-06/reverse-string"
    This reverts commit 049b09b4782eab0ab0691513226485f8c5d349a8, reversing
    changes made to b6fda99eb1087f10a33b693fbc2670da938d7c28.

--- tasks/c++/easy/ReverseString.cpp
+++ tasks/c++/easy/ReverseString.cpp
@@ -1,26 +1,11 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
-string reverseString(const string &str) {
+std::string reverseString(const std::string &str) {
     // TODO: Implement the reverseString function
-    string reversed;
-    for (int i = str.length() - 1; i >= 0; i--) {
-        reversed += str[i];
-    }
-    return reversed;
 }
 
 int main() {
     // TODO: Implement the main method
-    string input;
-    cout<<"Enter a string: ";
-
-    getline(cin, input);
-
-    string reversed = reverseString(input);
-    cout<<"Reversed string: " << reversed << endl;
-
-return 0;
 }
 
