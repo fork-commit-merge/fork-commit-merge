c79a4192fc7790676c876175f68ba2e43e9fca78
Pratishtha <nandwalpratishtha06@gmail.com>
2026-07-06 22:39:36 +0530

    Solved ReverseString c++ easy issue

--- tasks/c++/easy/ReverseString.cpp
+++ tasks/c++/easy/ReverseString.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
-std::string reverseString(const std::string &str) {
+string reverseString(const string &str) {
     // TODO: Implement the reverseString function
+    string reversed;
+    for (int i = str.length() - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
 }
 
 int main() {
     // TODO: Implement the main method
+    string input;
+    cout<<"Enter a string: ";
+
+    getline(cin, input);
+
+    string reversed = reverseString(input);
+    cout<<"Reversed string: " << reversed << endl;
+
+return 0;
 }
 
