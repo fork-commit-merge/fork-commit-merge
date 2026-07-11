bdcef2e59a4e305b4f5b3900280aa4df3abe4bf9
GitHub Action <action@github.com>
2026-07-11 15:48:52 +0000

    Revert "Merge pull request #7702 from KhushiVerma021/feature/frequency-java"
    This reverts commit 0c2f45192b9cae0734286505dd52b724835dfe58, reversing
    changes made to b3c2f2f1e0a33993cdc7dad07d7b9f4b022f9274.

--- tasks/java/hard/Frequency.java
+++ tasks/java/hard/Frequency.java
@@ -5,15 +5,5 @@ public class Frequency {
 
     public static void main(String[] args) {
         // TODO: Given an array of integers, write a Java program to find the frequency of each element using a Map
-        int[] arr= {1,2,3,2,1,4,2};
-        Map<Integer,Integer> map= new HashMap<>();
-        for(int num: arr){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
-        System.out.println(map);
-    
-    
-    
-    
     }
 }
