e7ab5816b28c2079ac68315db6d7290871f3f98c
GitHub Action <action@github.com>
2026-07-15 06:44:41 +0000

    Revert "Merge pull request #7707 from krsnaSuraj/solve-number-sum-py"
    This reverts commit d882c95b43b6da6adb1d6ccf7b868f7924a5202b, reversing
    changes made to 1c2e5d727a417f413d9e9e3fa238ecf666b766fe.

--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,7 +1,3 @@
 # Python - Easy
 
-def sum_numbers(a, b):
-    return a + b
-
-result = sum_numbers(5, 7)
-print("The sum of 5 and 7 is", result)
+# TODO: Write a function that returns the sum of two numbers
