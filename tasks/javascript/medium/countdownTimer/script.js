4446b4e222d99a1b093a02c4926b8019541c8765
Alisa Ansari <alieeshaathecoder@gmail.com>
2026-07-17 20:58:00 +0530

    Add countdown timer

--- tasks/javascript/medium/countdownTimer/script.js
+++ tasks/javascript/medium/countdownTimer/script.js
@@ -1,3 +1,21 @@
 // JavaScript - Medium
 
 // Create functionality for a countdown timer that counts down from 10 seconds to zero using JavaScript
+const timer = document.getElementById("timer");
+
+function countdownTimer() {
+    let timeLeft = 10;
+
+    const interval = setInterval(() => {
+        timeLeft--;
+
+        if (timeLeft >= 0) {
+            timer.textContent = `00:${String(timeLeft).padStart(2, "0")}`;
+        } else {
+            clearInterval(interval);
+            timer.textContent = "Time's up!";
+        }
+    }, 1000);
+}
+
+countdownTimer();
