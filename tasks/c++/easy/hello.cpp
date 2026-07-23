798f6cf36fb7ee20855f9b4746c1993d492581ea
GitHub Action <action@github.com>
2026-07-23 06:19:41 +0000

    Revert "Merge pull request #7743 from anishkumar-1212/issue-7687-hello-print"
    This reverts commit a0d9a38b8f8bd21642d6ac027f6606e81807d186, reversing
    changes made to 9dda5baae752046cc6999573ccdab995dd3d3cd0.

--- tasks/c++/easy/hello.cpp
+++ tasks/c++/easy/hello.cpp
@@ -1,9 +1,9 @@
 // C++ - Easy 3
+
 #include <iostream>
-using namespace std;
+
 int main() {
-    // TODO: Create a Basic C++ Program that Prints "Hello World
-    cout<<"Hello World";
+    // TODO: Create a Basic C++ Program that Prints "Hello World"
     return 0;
 }
 
