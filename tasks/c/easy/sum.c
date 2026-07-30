e120ebbf607d119aa289e8e7aeb11dfd03fca0b5
Ritwik82 <ritwikpavan82@gmail.com>
2026-07-30 23:01:14 +0530

    Added sum function

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -2,4 +2,14 @@
 
 #include <stdio.h>
 
+int sum(int a, int b){
+    return a + b;
+}
+int main(){
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&a,&b);
+    printf("The sum is: %d",sum(a,b));
+    return 0;
+}
 // TODO: Implement the sum and main functions
