3bc5fb4fe8b7e36574314fbe907310949d67427b
GitHub Action <action@github.com>
2026-08-04 06:51:51 +0000

    Revert "Merge pull request #7905 from abdd8433/fix-issue"
    This reverts commit 6121ddcdf5d2a665bdc57aab5188ca245bc26475, reversing
    changes made to 942265ca684624f7c4f5f6e5801c6525fc9f5265.

--- tasks/python/easy/hello.py
+++ tasks/python/easy/hello.py
@@ -1,7 +1,3 @@
 # Python - Easy
-def hello_python():
-    print("Hello, Python!") 
 
-
-print(hello_python())  
 # TODO: Write a function that prints "Hello, Python!" to the console
--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -5,13 +5,5 @@
 # prints that list to the console and,
 # prints the sum of the numbers of that list
 
-import numbers
-
-
 def function(numbers):
-    print(numbers)
-    print(sum(numbers))
-
-numbers = list(range(1, 11))
-
-print(function(numbers))
+    pass
