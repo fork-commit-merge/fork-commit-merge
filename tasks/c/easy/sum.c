b4377b5d5f2f45f528bbd1322206eb00b4c83924
GitHub Action <action@github.com>
2026-08-06 06:25:12 +0000

    Revert "Merge pull request #7909 from zainabamjadx7/patch-6"
    This reverts commit 1a2946162fa483a80ed57560ba9f4b7787d2abbe, reversing
    changes made to e075ba8e4d2be1772c52c2f6df8e0477ffb4a3dd.

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -1,22 +1,5 @@
+// C - Easy
+
 #include <stdio.h>
 
-// Function to calculate sum
-int sum(int a, int b)
-{
-    return a + b;
-}
-
-int main()
-{
-    int num1, num2;
-
-    printf("Enter first number: ");
-    scanf("%d", &num1);
-
-    printf("Enter second number: ");
-    scanf("%d", &num2);
-
-    printf("Sum = %d\n", sum(num1, num2));
-
-    return 0;
-}
+// TODO: Implement the sum and main functions
