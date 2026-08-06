157bd57aa04274d3f02dca224363b4e289bb555a
GitHub Action <action@github.com>
2026-08-06 06:27:52 +0000

    Revert "Merge pull request #7910 from zainabamjadx7/patch-7"
    This reverts commit 2f0e9e4c7218540ee3dcabb6ae6e7bdcb7e96480, reversing
    changes made to b4377b5d5f2f45f528bbd1322206eb00b4c83924.

--- tasks/c/medium/read_csv.c
+++ tasks/c/medium/read_csv.c
@@ -1,20 +1,10 @@
+// C - Medium
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int main()
-{
-    char names[][20] = {"Alice", "Bob", "Charlie"};
-    int ages[] = {30, 40, 50};
 
-    int n = 3;
-
-    printf("Name\tAge\n");
-
-    for (int i = 0; i < n; i++)
-    {
-        printf("%s\t%d\n", names[i], ages[i]);
-    }
-
-    return 0;
+int main() {
+//    TODO: Implement the program requested in the task description
 }
