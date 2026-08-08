b478929ea0293bd625a5fe710e36c095c91ab05a
GitHub Action <action@github.com>
2026-08-08 08:19:53 +0000

    Revert "Merge pull request #7917 from devmaster1987/fix/javascript-greater-than"
    This reverts commit 3a940de4452adeb0c66f9e6de7e020f8a29c05c4, reversing
    changes made to 96df5f0546aaf17af4bf282a5e317ab67b8c4a98.

--- tasks/javascript/easy/greater-than.js
+++ tasks/javascript/easy/greater-than.js
@@ -1,11 +1,6 @@
 // JavaScript - Easy
 
-// Return a new array with all numbers greater than 10 but less than 20
+// TODO: return a new array with all numbers greater than 10 but less than 20;
 
 const array = [2, 5, 8, 10, 12, 15, 19, 20, 25];
-
-const result = array.filter(number => number > 10 && number < 20);
-
-console.log(result);
-
 // output: [12, 15, 19];
