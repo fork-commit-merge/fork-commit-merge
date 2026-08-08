515e8d058af549e19c0ad5c2743457afedd59c16
GitHub Action <action@github.com>
2026-08-08 08:17:17 +0000

    Revert "Merge pull request #7914 from zainabamjadx7/patch-10"
    This reverts commit 1184fe805f56c0a45010d6f648a8dd04ba5a7c97, reversing
    changes made to 4525c1b1685bd848b41c38be647951a1dfb7057b.

--- tasks/sql/easy/create_users.sql
+++ tasks/sql/easy/create_users.sql
@@ -1,10 +1,6 @@
 -- SQL - Easy
 
 CREATE TABLE users (
-    user_id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    -- TODO: Add all the requested fields
 );
 
