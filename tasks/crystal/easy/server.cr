73ee55bf6462232dd0cfa10a08be4e0f79ec1de2
Noor Fatima <official.noorfatima.9@gmail.com>
2026-08-12 23:45:23 +0500

    feat: add Crystal HTTP server

--- tasks/crystal/easy/server.cr
+++ tasks/crystal/easy/server.cr
@@ -2,4 +2,13 @@
 
 require "http/server"
 
-# TODO: Implement the HTTP server
+server = HTTP::Server.new do |context|
+  context.response.content_type = "text/plain"
+  context.response.print "Hello, world!"
+end
+
+address = server.bind_tcp("127.0.0.1", 3000)
+
+puts "Listening on http://#{address}"
+
+server.listen
