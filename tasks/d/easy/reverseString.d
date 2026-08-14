c280e9cb50cbe031c54d09bc782188da11ced340
GitHub Action <action@github.com>
2026-08-14 06:12:36 +0000

    Revert "Merge pull request #7938 from noor-fatima320/main"
    This reverts commit 66f33a0590cf05090d951ab5058c725620708862, reversing
    changes made to f60c23ee7a1ea8f8a9aaeb0978a60450a8f2418a.

--- tasks/d/easy/reverseString.d
+++ tasks/d/easy/reverseString.d
@@ -3,13 +3,5 @@
 import std.stdio;
 
 void main() {
-    string original = "Hello, World!";
-    string reversed = "";
-
-    foreach_reverse (char c; original) {
-        reversed ~= c;
-    }
-
-    writeln("Original String: ", original);
-    writeln("Reversed String: ", reversed);
+    // TODO: Implement program to reverse a string
 }
