8923f6350370dc72a763caec0db30cfb0b072873
Noor Fatima <official.noorfatima.9@gmail.com>
2026-08-14 17:18:43 +0500

    feat: add Scala factorial calculator

--- tasks/scala/easy/FactorialCalculator.scala
+++ tasks/scala/easy/FactorialCalculator.scala
@@ -1,11 +1,37 @@
 // Scala - Easy
 
+import scala.io.StdIn.readLine
+
 object FactorialCalculator {
   def main(args: Array[String]): Unit = {
-    // TODO: Create a simple Scala program that takes a number from the user and prints the factorial of that number
+    var validInput = false
+
+    while (!validInput) {
+      print("Enter a non-negative integer: ")
+
+      val input = readLine()
+
+      try {
+        val number = input.toInt
+
+        if (number < 0) {
+          println("Invalid input. Please enter a non-negative integer.")
+        } else {
+          println(s"Factorial of $number = ${factorial(number)}")
+          validInput = true
+        }
+      } catch {
+        case _: NumberFormatException =>
+          println("Invalid input. Please enter a valid integer.")
+      }
+    }
   }
 
   def factorial(n: Int): BigInt = {
-    // TODO: Create factorial function
+    if (n == 0) {
+      BigInt(1)
+    } else {
+      BigInt(n) * factorial(n - 1)
+    }
   }
 }
