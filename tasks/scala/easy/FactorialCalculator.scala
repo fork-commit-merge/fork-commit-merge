b05aeae0026c5a928de3d83e020ebd2b98ce6506
GitHub Action <action@github.com>
2026-08-15 17:06:36 +0000

    Revert "Merge pull request #7939 from noor-fatima320/main"
    This reverts commit 95e0e4ad081ad7303256b29e08fb318fe838be8a, reversing
    changes made to c280e9cb50cbe031c54d09bc782188da11ced340.

--- tasks/scala/easy/FactorialCalculator.scala
+++ tasks/scala/easy/FactorialCalculator.scala
@@ -1,37 +1,11 @@
 // Scala - Easy
 
-import scala.io.StdIn.readLine
-
 object FactorialCalculator {
   def main(args: Array[String]): Unit = {
-    var validInput = false
-
-    while (!validInput) {
-      print("Enter a non-negative integer: ")
-
-      val input = readLine()
-
-      try {
-        val number = input.toInt
-
-        if (number < 0) {
-          println("Invalid input. Please enter a non-negative integer.")
-        } else {
-          println(s"Factorial of $number = ${factorial(number)}")
-          validInput = true
-        }
-      } catch {
-        case _: NumberFormatException =>
-          println("Invalid input. Please enter a valid integer.")
-      }
-    }
+    // TODO: Create a simple Scala program that takes a number from the user and prints the factorial of that number
   }
 
   def factorial(n: Int): BigInt = {
-    if (n == 0) {
-      BigInt(1)
-    } else {
-      BigInt(n) * factorial(n - 1)
-    }
+    // TODO: Create factorial function
   }
 }
