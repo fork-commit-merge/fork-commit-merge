213399ef54b42fe30626ef8b9210be74654dd1b5
GitHub Action <action@github.com>
2026-08-15 17:09:19 +0000

    Revert "Merge pull request #7942 from zainabamjadx7/patch-14"
    This reverts commit 38c7eda86684a067eb362f47ed8b131d35362408, reversing
    changes made to 21ac695388064bc72e43e45e7ce7ad652c23c939.

--- tasks/c++/easy/tempConverter.cpp
+++ tasks/c++/easy/tempConverter.cpp
@@ -1,28 +1,5 @@
 #include <iostream>
 
 int main() {
-    int choice;
-    double temp, converted;
-
-    std::cout << "--- Temperature Converter ---\n";
-    std::cout << "1. Celsius to Fahrenheit\n";
-    std::cout << "2. Fahrenheit to Celsius\n";
-    std::cout << "Enter your choice (1 or 2): ";
-    std::cin >> choice;
-
-    if (choice == 1) {
-        std::cout << "Enter temperature in Celsius: ";
-        std::cin >> temp;
-        converted = (temp * 9.0 / 5.0) + 32.0;
-        std::cout << temp << " C = " << converted << " F" << std::endl;
-    } else if (choice == 2) {
-        std::cout << "Enter temperature in Fahrenheit: ";
-        std::cin >> temp;
-        converted = (temp - 32.0) * 5.0 / 9.0;
-        std::cout << temp << " F = " << converted << " C" << std::endl;
-    } else {
-        std::cout << "Invalid choice! Please select 1 or 2." << std::endl;
-    }
-
-    return 0;
+  // TODO: Implement the temperature converter
 }
