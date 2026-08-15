2fec45a9d1b321afb244b5a9febb46a4a7ed44c7
GitHub Action <action@github.com>
2026-08-15 17:09:55 +0000

    Revert "Merge pull request #7943 from zainabamjadx7/patch-15"
    This reverts commit cdb09a8b0dc70307a8dba90923c964de18ab4425, reversing
    changes made to 213399ef54b42fe30626ef8b9210be74654dd1b5.

--- tasks/c++/easy/sum.cpp
+++ tasks/c++/easy/sum.cpp
@@ -2,20 +2,7 @@
 
 // TODO: Create a Basic C++ Program to Add Two Numbers
 
-#include <iostream>
-
-int main() {
-    double num1, num2, sum;
-
-    std::cout << "Enter first number: ";
-    std::cin >> num1;
-
-    std::cout << "Enter second number: ";
-    std::cin >> num2;
-
-    sum = num1 + num2;
-
-    std::cout << "Sum: " << sum << std::endl;
-
-    return 0;
+int main()
+{
+  
 }
