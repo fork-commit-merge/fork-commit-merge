c46a44015f6919ec951253023d4199894b472629
GitHub Action <action@github.com>
2026-08-17 05:12:12 +0000

    Revert "Merge pull request #7948 from zainabamjadx7/patch-17"
    This reverts commit 888144aa9d2ec3f3c691cfd4f7abb46116ad83e1, reversing
    changes made to e3e3de73a412e1530916d295f49112254fafd6a7.

--- tasks/c++/medium/file_handler/FileHandler.h
+++ tasks/c++/medium/file_handler/FileHandler.h
@@ -1,11 +1,9 @@
+// C++ - Medium
+
 #pragma once
 #include <string>
 
 class FileHandler {
 public:
-    
-    static std::string ReadFile(const std::string& filename);
-    static bool WriteFile(const std::string& filename, const std::string& content);
-    static bool AppendFile(const std::string& filename, const std::string& content);
-    static bool FileExists(const std::string& filename);
+    // TODO: Implement the FileHandler class
 };
