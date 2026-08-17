e3241870868178cbab8f5e4d4dd523734507e050
Zainab Amjad <zainabamjad074@gmail.com>
2026-08-17 23:01:17 +0500

    Implement MyString class methods

--- tasks/c++/hard/mystring.cpp
+++ tasks/c++/hard/mystring.cpp
@@ -1,42 +1,79 @@
 #include <cstring>
 #include <iostream>
 
+// Class assumption:
+// class MyString {
+// private:
+//     char* data;
+//     int len;
+// public:
+//     ...
+// };
+
 MyString::MyString(const char* s) {
-    // TODO: Implement this
+    if (s == nullptr) {
+        len = 0;
+        data = new char[1];
+        data[0] = '\0';
+    } else {
+        len = std::strlen(s);
+        data = new char[len + 1];
+        std::strcpy(data, s);
+    }
 }
 
 MyString::MyString(const MyString& other) {
-    // TODO: Implement this
+    len = other.len;
+    data = new char[len + 1];
+    std::strcpy(data, other.data);
 }
 
 int MyString::length() const {
-    // TODO: Implement this
+    return len;
 }
 
 const char* MyString::c_str() const {
-    // TODO: Implement this
+    return data;
 }
 
 void MyString::append(const MyString& other) {
-    // TODO: Implement this
+    int new_len = len + other.len;
+    char* new_data = new char[new_len + 1];
+    
+    std::strcpy(new_data, data);
+    std::strcat(new_data, other.data);
+    
+    delete[] data;
+    data = new_data;
+    len = new_len;
 }
 
 int MyString::compare(const MyString& other) const {
-    // TODO: Implement this
+    return std::strcmp(data, other.data);
 }
 
 MyString& MyString::operator=(const MyString& other) {
-    // TODO: Implement this
+    if (this != &other) { // Self-assignment check
+        delete[] data;
+        
+        len = other.len;
+        data = new char[len + 1];
+        std::strcpy(data, other.data);
+    }
+    return *this;
 }
 
 MyString MyString::operator+(const MyString& other) const {
-    // TODO: Implement this
+    MyString temp(*this);
+    temp.append(other);
+    return temp;
 }
 
 bool MyString::operator==(const MyString& other) const {
-    // TODO: Implement this
+    return std::strcmp(data, other.data) == 0;
 }
 
 std::ostream& operator<<(std::ostream& os, const MyString& s) {
-    // TODO: Implement this
+    os << s.c_str();
+    return os;
 }
