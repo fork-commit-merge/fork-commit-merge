1a22a80e30430b3456f3037e70c14e7a54d52cfa
GitHub Action <action@github.com>
2026-08-17 05:10:43 +0000

    Revert "Merge pull request #7946 from anchal125/feat/todo-list"
    This reverts commit fc467d39df9ecc142bfcbf38139601ba664d36f2, reversing
    changes made to 760e18d053f927f2e82b134ef302fd6e4d34379c.

--- tasks/react/medium/todo-app/src/App.jsx
+++ tasks/react/medium/todo-app/src/App.jsx
@@ -1,56 +1,13 @@
+// React - Medium
+
 import "./App.css";
-import { useState, useEffect } from "react";
 
 function App() {
-  const [input, setInput] = useState("");
-  const [todos, setTodos] = useState(() => {
-    const savedTodos = localStorage.getItem("todos");
-    return savedTodos ? JSON.parse(savedTodos) : [];
-  });
-  const handleTodos = () => {
-    if (!input.trim()) return;
-    setTodos((p) => [...p, { todo: input, id: Date.now() }]);
-    setInput("");
-  };
-  const handleEdit = (index) => {
-    const name = prompt("Enter new todo").trim();
-    if (name) {
-      setTodos((p) => {
-        let cpy = [...p];
-        cpy[index].todo = name;
-        return cpy;
-      });
-    }
-  };
-  const handleDelete = (id) => {
-    setTodos((p) => p.filter((todo) => todo.id != id));
-  };
-
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
-
+  // TODO: Add the necessary functionality such as hooks here
   return (
     <div className="App">
       <h1>To-Do List</h1>
-      <input
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key == "Enter") handleTodos();
-        }}
-      />
-      <button onClick={handleTodos}>ADD</button>
-      <ul>
-        {todos.map(({ todo, id }, index) => (
-          <li key={id} id={id}>
-            <span>{todo}</span>
-            <button onClick={() => handleEdit(index)}>✏️</button>
-            <button onClick={() => handleDelete(id)}>X</button>
-          </li>
-        ))}
-      </ul>
+      {/* TODO: Add the To-Do list here */}
     </div>
   );
 }
