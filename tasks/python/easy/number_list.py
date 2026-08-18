d86f2320a2f23205213998786a8a84919e3bffe0
GitHub Action <action@github.com>
2026-08-18 12:14:33 +0000

    Revert "Merge pull request #7954 from zhenxun00/python-number-list"
    This reverts commit 403b3cb7869fc7a7cac688df3b8b2f4fa242e883, reversing
    changes made to 1e7a3d957762ec149b57412422a1081318fe3963.

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -6,15 +6,4 @@
 # prints the sum of the numbers of that list
 
 def function(numbers):
-    """Calculate the sum of a list of numbers."""
-    return sum(numbers)
-
-# Create a list of the first 10 natural numbers (1 through 10)
-numbers = list(range(1, 11))
-
-# Print the list
-print("List of numbers:", numbers)
-
-# Calculate and print the sum
-total = function(numbers)
-print("Sum of numbers:", total)
+    pass
