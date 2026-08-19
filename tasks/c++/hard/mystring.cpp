0296c3df00b94e2ce1175c1ad2937acb8cdb4431
GitHub Action <action@github.com>
2026-08-19 10:50:30 +0000

    Revert "Merge pull request #7960 from zainabamjadx7/patch-24"
    This reverts commit e4005231e083b862924b4f3d1aa19a08975a41b9, reversing
    changes made to e104461724c740ab2abf3274d61c9d64ae362ee3.

--- tasks/c++/hard/mystring.cpp
+++ tasks/c++/hard/mystring.cpp
@@ -2,69 +2,41 @@
 #include <iostream>
 
 MyString::MyString(const char* s) {
-    if (s == nullptr) {
-        len = 0;
-        data = new char[1];
-        data[0] = '\0';
-    } else {
-        len = std::strlen(s);
-        data = new char[len + 1];
-        std::strcpy(data, s);
-    }
+    // TODO: Implement this
 }
 
 MyString::MyString(const MyString& other) {
-    len = other.len;
-    data = new char[len + 1];
-    std::strcpy(data, other.data);
+    // TODO: Implement this
 }
 
 int MyString::length() const {
-    return len;
+    // TODO: Implement this
 }
 
 const char* MyString::c_str() const {
-    return data;
+    // TODO: Implement this
 }
 
 void MyString::append(const MyString& other) {
-    int new_len = len + other.len;
-    char* new_data = new char[new_len + 1];
-
-    std::strcpy(new_data, data);
-    std::strcat(new_data, other.data);
-
-    delete[] data;
-    data = new_data;
-    len = new_len;
+    // TODO: Implement this
 }
 
 int MyString::compare(const MyString& other) const {
-    return std::strcmp(data, other.data);
+    // TODO: Implement this
 }
 
 MyString& MyString::operator=(const MyString& other) {
-    if (this != &other) { // Self-assignment check
-        delete[] data;
-
-        len = other.len;
-        data = new char[len + 1];
-        std::strcpy(data, other.data);
-    }
-    return *this;
+    // TODO: Implement this
 }
 
 MyString MyString::operator+(const MyString& other) const {
-    MyString temp(*this);
-    temp.append(other);
-    return temp;
+    // TODO: Implement this
 }
 
 bool MyString::operator==(const MyString& other) const {
-    return std::strcmp(data, other.data) == 0;
+    // TODO: Implement this
 }
 
 std::ostream& operator<<(std::ostream& os, const MyString& s) {
-    os << s.c_str();
-    return os;
+    // TODO: Implement this
 }
