ee8385c99c164fd9d37b781c2f8af93f8784e37c
GitHub Action <action@github.com>
2026-08-19 10:51:27 +0000

    Revert "Merge pull request #7961 from noor-fatima320/clojure-add-numbers"
    This reverts commit d4c3bd596943b0d4a662085d960d18b2143152c0, reversing
    changes made to 0296c3df00b94e2ce1175c1ad2937acb8cdb4431.

--- tasks/clojure/easy/add-numbers/src/add_numbers/core.clj
+++ tasks/clojure/easy/add-numbers/src/add_numbers/core.clj
@@ -1,4 +1,5 @@
 (ns add-numbers.core)
 
-(defn add-numbers [x y]
-  (println (str "The sum of " x " and " y " is: " (+ x y))))
+(defn add-numbers
+  ;; TODO: Implement the Number addition function here
+)
