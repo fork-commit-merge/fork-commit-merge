93a226bc13e8ca9c86445a7c993698782d93c635
GitHub Action <action@github.com>
2026-08-24 06:09:04 +0000

    Revert "Merge pull request #7968 from tralethe-dev/patch-1"
    This reverts commit 6a763e49b7b158b109774b95e3222f8bafb8ca6f, reversing
    changes made to e74735a58e565db706e4fed3559d348932a87f24.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -1,14 +1,8 @@
 // Java - Easy
-import java.util.Scanner;
+
 public class GreetingApp {
 
     public static void main(String[] args) {
-        Scanner kb = new Scanner(System.in);
-        String name;
 
-        System.out.print("Enter your name: ");
-        name = kb.nextLine();
-        
-        System.out.println("Hello " + name);
     }
 }
