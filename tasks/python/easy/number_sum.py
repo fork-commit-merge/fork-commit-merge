8a8ddc914aeb1b7d5b0be5a4d42336dee1e7dd82
GitHub Action <action@github.com>
2026-08-24 06:10:05 +0000

    Revert "Merge pull request #7969 from tralethe-dev/patch-2"
    This reverts commit dd109c7ec5b89eba967c1403fe9340dda2fe0c82, reversing
    changes made to 2df353c0e27a2e4c311bac9776b7e6766c06124c.

--- tasks/python/easy/number_sum.py
+++ tasks/python/easy/number_sum.py
@@ -1,5 +1,3 @@
 # Python - Easy
-num1 = input("Enter the first number: ")
-num2 = input("Enter the second number: ")
+
 # TODO: Write a function that returns the sum of two numbers
-print(int(num1)+(num2))
