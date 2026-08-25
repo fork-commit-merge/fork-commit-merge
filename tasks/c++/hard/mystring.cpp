dfeca74f37aa00f891f5744aac6e252505e35daa
GitHub Action <action@github.com>
2026-08-25 10:14:50 +0000

    Revert "Merge pull request #7971 from joylinmhs/Hard-Issue-C++"
    This reverts commit e6989f717885561006b6d31e2d15136dc5462d9b, reversing
    changes made to e5fc7aa2cb7b7da937149063e5b934290fafe8b0.

--- tasks/c++/hard/mystring.cpp
+++ tasks/c++/hard/mystring.cpp
@@ -1,122 +1,42 @@
-#include "mystring.h"
 #include <cstring>
+#include <iostream>
 
-// Default constructor
-MyString::MyString() {
-    size = 0;
-    data = new char[1];
-    data[0] = '\0';
+MyString::MyString(const char* s) {
+    // TODO: Implement this
 }
 
-// Parameterized constructor
-MyString::MyString(const char* str) {
-    if (str == nullptr) {
-        size = 0;
-        data = new char[1];
-        data[0] = '\0';
-        return;
-    }
-
-    size = std::strlen(str);
-    data = new char[size + 1];
-    std::strcpy(data, str);
-}
-
-// Copy constructor
 MyString::MyString(const MyString& other) {
-    size = other.size;
-    data = new char[size + 1];
-    std::strcpy(data, other.data);
+    // TODO: Implement this
 }
 
-// Destructor
-MyString::~MyString() {
-    delete[] data;
-}
-
-// Return length
 int MyString::length() const {
-    return size;
+    // TODO: Implement this
 }
 
-// Return C-style string
 const char* MyString::c_str() const {
-    return data;
+    // TODO: Implement this
 }
 
-// Append another string
 void MyString::append(const MyString& other) {
-    char* newData = new char[size + other.size + 1];
-
-    std::strcpy(newData, data);
-    std::strcat(newData, other.data);
-
-    delete[] data;
-
-    data = newData;
-    size += other.size;
+    // TODO: Implement this
 }
 
-// Compare strings
 int MyString::compare(const MyString& other) const {
-    return std::strcmp(data, other.data);
+    // TODO: Implement this
 }
 
-// Assignment operator
 MyString& MyString::operator=(const MyString& other) {
-    if (this == &other) {
-        return *this;
-    }
-
-    char* newData = new char[other.size + 1];
-    std::strcpy(newData, other.data);
-
-    delete[] data;
-
-    data = newData;
-    size = other.size;
-
-    return *this;
+    // TODO: Implement this
 }
 
-// + operator
 MyString MyString::operator+(const MyString& other) const {
-    MyString result(*this);
-    result.append(other);
-    return result;
+    // TODO: Implement this
 }
 
-// == operator
 bool MyString::operator==(const MyString& other) const {
-    return std::strcmp(data, other.data) == 0;
+    // TODO: Implement this
 }
 
-// << operator
-std::ostream& operator<<(std::ostream& os, const MyString& str) {
-    os << str.data;
-    return os;
-}
-
-// Bonus substring
-MyString MyString::substring(int start, int end) const {
-    if (start < 0 || end < start || start >= size) {
-        return MyString("");
-    }
-
-    if (end >= size) {
-        end = size - 1;
-    }
-
-    int subLength = end - start + 1;
-
-    char* buffer = new char[subLength + 1];
-
-    std::strncpy(buffer, data + start, subLength);
-    buffer[subLength] = '\0';
-
-    MyString result(buffer);
-
-    delete[] buffer;
-
-    return result;
+std::ostream& operator<<(std::ostream& os, const MyString& s) {
+    // TODO: Implement this
 }
