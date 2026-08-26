af83a0a19bd6b9345ced765a6f45f25657b2b86b
Saket7002 <saketsrivastava822@gmail.com>
2026-08-27 00:17:41 +0530

    Fix Ada number statistics

--- tasks/ada/medium/number_stats.adb
+++ tasks/ada/medium/number_stats.adb
@@ -6,9 +6,23 @@ procedure Number_Stats is
    Min, Max, Sum : Integer;
    Avg : Float;
 begin
-   -- TODO: Use Numbers in the fixed-size array above to
-   -- implement the calculations for Min, Max, Sum, and Avg
-   
+   Min := Numbers (Numbers'First);
+   Max := Numbers (Numbers'First);
+   Sum := 0;
+
+   for I in Numbers'Range loop
+      if Numbers (I) < Min then
+         Min := Numbers (I);
+      end if;
+
+      if Numbers (I) > Max then
+         Max := Numbers (I);
+      end if;
+
+      Sum := Sum + Numbers (I);
+   end loop;
+
+   Avg := Float (Sum) / Float (Numbers'Length);
 
    -- Print the Min, Max, Sum, and Avg on separate lines
    -- using the statements below
