dacaf7038ac9d2e7c684fefe8080849d61838623
GitHub Action <action@github.com>
2026-09-10 10:26:12 +0000

    Revert "Merge pull request #8136 from Tanjiro5834/feature/spring-boot-hello"
    This reverts commit 0c0fb07d4bcb216f605c69a9bb58f9be574dd847, reversing
    changes made to f1620ac20331f365b678c79a181754b3b858dbb2.

--- tasks/spring-boot/easy/hello/src/main/java/org/forkcommitmerge/hello/HomeController.java
+++ tasks/spring-boot/easy/hello/src/main/java/org/forkcommitmerge/hello/HomeController.java
@@ -1,8 +1,6 @@
 package org.forkcommitmerge.hello;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
@@ -18,9 +16,5 @@ public class HomeController {
      * The template is already set up to display the message using Thymeleaf
      * with the expression: th:text="${message}"
      */
-    @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("message", "Hello, Spring Boot");
-        return "index";
-    }
+
 }
