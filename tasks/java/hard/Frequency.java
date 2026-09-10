08fbeacaf6f0e02b6633d0edd759a91bbc603ce4
Nathaniel Coronacion <nathanielcoronacion3@gmail.com>
2026-09-10 15:02:29 +0800

    feat: implemented frequency counter using HashMap

--- tasks/java/hard/Frequency.java
+++ tasks/java/hard/Frequency.java
@@ -5,5 +5,14 @@ public class Frequency {
 
     public static void main(String[] args) {
         // TODO: Given an array of integers, write a Java program to find the frequency of each element using a Map
+        int[] arr = {1, 2, 2, 3, 3, 3, 4};
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for(int num : arr){
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        for(Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()){
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
     }
 }
