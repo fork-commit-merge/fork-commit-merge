33753586f7d69c7ec6537c8d8b7f1bf19e056572
GitHub Action <action@github.com>
2026-09-16 08:21:28 +0000

    Revert "Merge pull request #8144 from ibrahimali111/bash-create-files"
    This reverts commit 402a57a0f3c466fb8bda6477162eb1aec5677063, reversing
    changes made to adc7c12de61c347b5ec6fd6e785c173125296831.

--- tasks/bash/easy/create_files.sh
+++ tasks/bash/easy/create_files.sh
@@ -2,10 +2,4 @@
 
 # Bash - Easy
 
-mkdir -p files
-cd files
-
-for i in 1 2 3; do
-    echo "Hello Shell!" > "file$i.txt"
-    cat "file$i.txt"
-done
+# TODO: Write the Bash script requested in the issue description
