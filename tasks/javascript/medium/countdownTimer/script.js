c71a73f20c411406d88df5d2039e194be2dfa608
Aradhy Dev Tiwari <tiwariaradhy1@gmail.com>
2026-09-22 15:02:36 +0530

    adding the countdown function code

--- tasks/javascript/medium/countdownTimer/script.js
+++ tasks/javascript/medium/countdownTimer/script.js
@@ -3,10 +3,12 @@ const timer = document.getElementById("timer");
 let sec = 10;
 function timeout() {
     timer.textContent = `00:${sec.toString().padStart(2, "0")}`;
+
     if (sec == 0) {
         timer.textContent = "Time's up!";
         return;
     }
+
     sec--;
     setTimeout(timeout, 1000);
 }
