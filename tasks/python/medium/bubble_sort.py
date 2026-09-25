f5f691baed6f27495cdb6e3d9ac1c0e75fe72541
fhmid <f.h.middleton@googlemail.com>
2026-09-25 17:21:27 +0100

    fix-issue-#8088

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,5 +1,12 @@
 def bubble_sort(arr):
-    # TODO: Implement the bubble sort algorithm
+    for number in range(len(arr)-1):
+        for number in range(len(arr)-1):
+            if arr[number] > arr[number + 1]:
+                original_number = arr[number]
+                arr[number] = arr[number+1]
+                arr[number+1] = original_number
+    return arr
+
 
 #! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
