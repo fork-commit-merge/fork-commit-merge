8d87577fbeebc770e3ab48614b3a15f241f20223
GitHub Action <action@github.com>
2026-09-26 13:42:10 +0000

    Revert "Merge pull request #8166 from harsh-thakkar7/feature/typescript-vitest-sum"
    This reverts commit 83934740f51ef9d992cd6b1e1c756b1dd857c1c3, reversing
    changes made to b8e2de58227ec40587da4f7a876a9cb6c52fea55.

--- tasks/typescript/vitest/easy/src/sum.test.ts
+++ tasks/typescript/vitest/easy/src/sum.test.ts
@@ -3,16 +3,4 @@
 import sum from "./sum";
 import { describe, expect, it } from "vitest";
 
-describe("sum", () => {
-    it("sums two numbers", () => {
-        expect(sum(1, 2)).toBe(3);
-    });
-
-    it("sums all provided numbers", () => {
-        expect(sum(1, 2, 3, 4, 5)).toBe(15);
-    });
-
-    it("returns 0 when called without arguments", () => {
-        expect(sum()).toBe(0);
-    });
-});
+// TODO: Create the test required in the task
