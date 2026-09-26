dc1eea5ca005f6df95ad21a578873599d0c8107b
GitHub Action <action@github.com>
2026-09-26 13:31:40 +0000

    Revert "Merge pull request #8169 from fhmid/fix-issue"
    This reverts commit 05d4a551d3696c444152e3abd07ffca039774d99, reversing
    changes made to 110d425385b8cc6c82bca6c761f499d228ca8ab4.

--- tasks/python/easy/number_list.py
+++ tasks/python/easy/number_list.py
@@ -5,9 +5,5 @@
 # prints that list to the console and,
 # prints the sum of the numbers of that list
 
-def print_numbers():
-    natural_numbers = list(range(1, 11)) # Create a list of the first natural numbers.
-    print(natural_numbers) # Print the list of natural_numbers
-    print(sum(natural_numbers)) # Print the sum of the natural_numbers
-
-print_numbers() 
+def function(numbers):
+    pass
