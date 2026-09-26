7af0a37f32d74421de8505e47ffc099f29adde45
GitHub Action <action@github.com>
2026-09-26 13:20:45 +0000

    Revert "Merge pull request #8156 from aradhy2006/screen-time-warning"
    This reverts commit 301d02e81cb7670fde9a0d7d473f784db82034b4, reversing
    changes made to 6edba3bf9f2d2d3c3d253c9cc68836b92ba04408.

--- tasks/javascript/medium/screenTimeWarning/screenTimeWarning.js
+++ tasks/javascript/medium/screenTimeWarning/screenTimeWarning.js
@@ -8,10 +8,8 @@
  */
 
 function startScreenTimeWarning(limitMinutes = 30) {
-setTimeout(() => {
-    console.log("You've been here for a while. Take a break and rest your eyes! 😊");
-}, limitMinutes*60*1000);
+
 }
 
 // Example usage
-startScreenTimeWarning(1/60);
+startScreenTimeWarning(10);
